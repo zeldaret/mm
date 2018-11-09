d1d248f2a60e3ddf12153d087286b80ef7e4d62e
rozlette <uberpanzermensch@gmail.com>
2018-11-08 21:39:28 -0600

    boot_0x80081920 can't seem to match...

---
+++ src/boot/boot_0x80081920.c
@@ -0,0 +1,7 @@
+#include <ultra64.h>
+#include <global.h>
+
+// TODO what are these updating the sp even though they call no other functions?
+void func_80081920(UNK_TYPE a0, UNK_TYPE a1, UNK_TYPE a2, UNK_TYPE a3) {}
+void func_8008193C(UNK_TYPE a0, UNK_TYPE a1, UNK_TYPE a2, UNK_TYPE a3) {}
+void func_80081958(UNK_TYPE a0, UNK_TYPE a1, UNK_TYPE a2, UNK_TYPE a3) {}
