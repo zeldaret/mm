5fcbe0a483123b30d3a59f0c27e6c85bbca87ae0
rozlette <uberpanzermensch@gmail.com>
2018-11-08 20:58:39 -0600

    Add slightly better diffing tool

---
+++ asmdiff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MIPS_BINUTILS="mips-linux-gnu-"
+
+OBJDUMP="${MIPS_BINUTILS}objdump -D -z -mmips -EB -j .text"
+
+FORMATTER="sed '/^0/!s/.*://'"
+
+$OBJDUMP build/src/test.o | sed '1,6d; /^0/!s/.*://' > test.dump
+$OBJDUMP $1 | sed '1,6d; /^0/!s/.*://' > comp.dump
+diff -y test.dump comp.dump > diff.dump
+rm test.dump comp.dump
