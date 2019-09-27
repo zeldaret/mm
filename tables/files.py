19418efb9243404bcba631dbe9b955aceb822471
rozlette <uberpanzermensch@gmail.com>
2019-09-26 22:34:24 -0500

    remove .bss in boot address range

--- tables/files.py
+++ tables/files.py
@@ -1,7 +1,7 @@
 # Follows the format of (File Location, Name, Start Address, .data/.rodata Segments (Start, End Inclusive))
 # TODO split .data & .rodata? Add .bss sections?
 {
-    ('baserom/boot', 'boot', 0x80080060, ((0x800969C0, 0x800A5ABF),)),
+    ('baserom/boot', 'boot', 0x80080060, ((0x800969C0, 0x800994FF),)),
     ('decomp/code', 'code', 0x800A5AC0, ((0x80186028, 0x80186A6F),(0x801AAAB0, 0x801E3F9F),)),
     ('decomp/ovl_title', 'ovl_title', 0x80800000, ((0x80800860, 0x8080090F),)),
     ('decomp/ovl_select', 'ovl_select', 0x80800910, ((0x80801C80, 0x80803DEF),)),
