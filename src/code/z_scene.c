69351886bfe39f9533626f1ee2a8138ef9147eec
rozlette <uberpanzermensch@gmail.com>
2020-01-25 00:23:32 -0600

    Fix nonmatching location in z_scene stub

--- src/code/z_scene.c
+++ src/code/z_scene.c
@@ -1,89 +1,89 @@
 #include <ultra64.h>
 #include <global.h>
 
-GLOBAL_ASM("./asm/nonmatching/Scene_LoadObject.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_LoadObject.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_Init.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_Init.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_ReloadUnloadedObjects.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_ReloadUnloadedObjects.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_FindSceneObjectIndex.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_FindSceneObjectIndex.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_IsObjectLoaded.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_IsObjectLoaded.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_DmaAllObjects.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_DmaAllObjects.asm")
 
-GLOBAL_ASM("./asm/nonmatching/func_8012F73C.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/func_8012F73C.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand00.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand00.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand01.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand01.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand02.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand02.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand03.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand03.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand04.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand04.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand06.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand06.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand07.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand07.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand08.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand08.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0A.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0A.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0B.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0B.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0C.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0C.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0D.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0D.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0E.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0E.asm")
 
-GLOBAL_ASM("./asm/nonmatching/func_8012FEBC.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/func_8012FEBC.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand0F.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand0F.asm")
 
-GLOBAL_ASM("./asm/nonmatching/func_8012FF10.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/func_8012FF10.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand11.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand11.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand12.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand12.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand10.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand10.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand05.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand05.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand13.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand13.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand09.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand09.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand15.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand15.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand16.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand16.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand18.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand18.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand17.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand17.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand1B.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand1B.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand1C.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand1C.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand1D.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand1D.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand1E.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand1E.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand19.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand19.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_HeaderCommand1A.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_HeaderCommand1A.asm")
 
-GLOBAL_ASM("./asm/nonmatching/func_801306A4.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/func_801306A4.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_ProcessHeader.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_ProcessHeader.asm")
 
-GLOBAL_ASM("./asm/nonmatching/Scene_CreateEntrance.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/Scene_CreateEntrance.asm")
 
-GLOBAL_ASM("./asm/nonmatching/func_80130784.asm")
+GLOBAL_ASM("./asm/nonmatching/z_scene/func_80130784.asm")
 
