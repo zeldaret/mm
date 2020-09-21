8d917129c9e1e9ce7b69d437312cafe21ab07b44
fullgrowngaming <fullgrowngaming@gmail.com>
2020-09-20 20:41:53 -0700

    Fixed spaces at end of lines in InitVars

--- src/overlays/actors/ovl_En_Torch/z_en_torch.c
+++ src/overlays/actors/ovl_En_Torch/z_en_torch.c
@@ -7,14 +7,14 @@
 void EnTorch_Init(Actor* thisx, GlobalContext* globalCtx);
 
 const ActorInit En_Torch_InitVars = {
-    ACTOR_EN_TORCH,          
-    ACTORTYPE_ITEMACTION, 
-    FLAGS,           
-    GAMEPLAY_KEEP,   
+    ACTOR_EN_TORCH,    
+    ACTORTYPE_ITEMACTION,
+    FLAGS,
+    GAMEPLAY_KEEP,
     sizeof(EnTorch),
-    (ActorFunc)EnTorch_Init, 
+    (ActorFunc)EnTorch_Init,
     (ActorFunc)NULL,      
-    (ActorFunc)NULL, 
+    (ActorFunc)NULL,
     (ActorFunc)NULL
 };
 
