2e5d6872cb2ca29e590b274503272dc130cc6b25
fullgrowngaming <fullgrowngaming@gmail.com>
2020-09-28 20:11:48 -0700

    Made requested changes

--- include/z64actor.h
+++ include/z64actor.h
@@ -185,6 +185,7 @@ typedef struct {
     /* 0x14B */ u8 unk14B;
     /* 0x14C */ UNK_TYPE1 pad14C[0x5];
     /* 0x151 */ u8 unk151;
+    /* 0x152 */ UNK_TYPE1 unk152;
     /* 0x153 */ u8 unk153;
     /* 0x154 */ UNK_TYPE1 pad154[0x1F8];
     /* 0x34C */ Actor* heldActor;
