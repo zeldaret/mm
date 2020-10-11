f92773041808697bcdfd0a8f0743b0f863c66ee8
fullgrowngaming <42490167+fullgrowngaming@users.noreply.github.com>
2020-10-11 16:30:22 -0700

    Arms_Hook OK (#33)
    * All but 2 functions matched
    
    * More work on hookshot
    
    * hookshot work
    
    * 2 functions left (and one is almost done)
    
    * sp issues with draw, but matching otherwise
    
    * hookshot matching
    
    * Changed rodata inclusion for armshook
    
    * Hookshot OK
    
    * Resolve script conflicts
    
    * Rename unk variables to match MM standard
    
    * Made requested changes

--- include/io/controller.h
+++ include/io/controller.h
@@ -3,6 +3,36 @@
 
 #include <PR/ultratypes.h>
 
+#define CONT_A      0x8000
+#define CONT_B      0x4000
+#define CONT_G	    0x2000
+#define CONT_START  0x1000
+#define CONT_UP     0x0800
+#define CONT_DOWN   0x0400
+#define CONT_LEFT   0x0200
+#define CONT_RIGHT  0x0100
+#define CONT_L      0x0020
+#define CONT_R      0x0010
+#define CONT_E      0x0008
+#define CONT_D      0x0004
+#define CONT_C      0x0002
+#define CONT_F      0x0001
+
+#define A_BUTTON	CONT_A
+#define B_BUTTON	CONT_B
+#define L_TRIG		CONT_L
+#define R_TRIG		CONT_R
+#define Z_TRIG		CONT_G
+#define START_BUTTON	CONT_START
+#define U_JPAD		CONT_UP
+#define L_JPAD		CONT_LEFT
+#define R_JPAD		CONT_RIGHT
+#define D_JPAD		CONT_DOWN
+#define U_CBUTTONS	CONT_E
+#define L_CBUTTONS	CONT_C
+#define R_CBUTTONS	CONT_F
+#define D_CBUTTONS	CONT_D
+
 typedef struct {
     /* 0x00 */ u32 ramarray[15];
     /* 0x3C */ u32 pifstatus;
