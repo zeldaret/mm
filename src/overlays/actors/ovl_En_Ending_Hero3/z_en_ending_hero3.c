3b6062c3b53d99ac2130d9f98a4ca543c91458f0
fullgrowngaming <fullgrowngaming@gmail.com>
2020-10-12 18:00:12 -0700

    ending_hero3 OK

--- src/overlays/actors/ovl_En_Ending_Hero3/z_en_ending_hero3.c
+++ src/overlays/actors/ovl_En_Ending_Hero3/z_en_ending_hero3.c
@@ -9,7 +9,9 @@ void EnEndingHero3_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnEndingHero3_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnEndingHero3_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void func_80C23518(EnEndingHero3* this);
+void func_80C23534(EnEndingHero3* this, GlobalContext* globalCtx);
+
 const ActorInit En_Ending_Hero3_InitVars = {
     ACTOR_EN_ENDING_HERO3,
     ACTORTYPE_NPC,
@@ -21,16 +23,48 @@ const ActorInit En_Ending_Hero3_InitVars = {
     (ActorFunc)EnEndingHero3_Update,
     (ActorFunc)EnEndingHero3_Draw
 };
-*/
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/EnEndingHero3_Init.asm")
+extern SkeletonHeader D_06007150;
+extern AnimationHeader D_06000E50;
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/EnEndingHero3_Destroy.asm")
+void EnEndingHero3_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnEndingHero3* this = THIS;
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/func_80C23518.asm")
+    this->actor.unkA0.mass = 0xFF;
+    Actor_SetScale(&this->actor, 0.01f);
+    this->actor.unk1F = 6;
+    this->actor.gravity = -3.0f;
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06007150, &D_06000E50, this->limbDrawTable,
+                     this->transitionDrawTable, 17);
+    Actor_SetDrawParams(&this->actor.shape, 0.0f, func_800B3FC0, 25.0f);
+    func_80C23518(&this->actor);
+}
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/func_80C23534.asm")
+void EnEndingHero3_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+}
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/EnEndingHero3_Update.asm")
+void func_80C23518(EnEndingHero3* this) {
+    this->unk258 = 1;
+    this->actionFunc = func_80C23534;
+}
 
-GLOBAL_ASM("asm/non_matchings/ovl_En_Ending_Hero3_0x80C23460/EnEndingHero3_Draw.asm")
+void func_80C23534(EnEndingHero3* this, GlobalContext* globalCtx) {
+    SkelAnime_FrameUpdateMatrix(&this->skelAnime);
+}
+
+void EnEndingHero3_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnEndingHero3* this = THIS;
+
+    this->actionFunc(this, globalCtx);
+    Actor_SetVelocityAndMoveYRotationAndGravity(&this->actor);
+    func_800B78B8(globalCtx, &this->actor, 20.0f, 20.0f, 50.0f, 0x1D);
+}
+
+void EnEndingHero3_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    EnEndingHero3* this = THIS;
+
+    func_8012C28C(globalCtx->state.gfxCtx);
+    func_8012C2DC(globalCtx->state.gfxCtx);
+    SkelAnime_DrawSV(globalCtx, this->skelAnime.skeleton, this->skelAnime.limbDrawTbl, this->skelAnime.dListCount, 0, 0,
+                     &this->actor);
+}
