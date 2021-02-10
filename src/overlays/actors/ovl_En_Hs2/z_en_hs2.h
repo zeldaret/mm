aad07ed79f89496a66c6dc0c2832344da4dcfc15
fullgrowngaming <fullgrowngaming@gmail.com>
2021-02-10 14:28:31 -0800

    en_hs2 OK

--- src/overlays/actors/ovl_En_Hs2/z_en_hs2.h
+++ src/overlays/actors/ovl_En_Hs2/z_en_hs2.h
@@ -5,9 +5,12 @@
 
 struct EnHs2;
 
+typedef void (*EnHs2ActionFunc)(struct EnHs2*, GlobalContext*);
+
 typedef struct EnHs2 {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x254];
+    /* 0x144 */ char unk_144[0x250];
+    /* 0x394 */ EnHs2ActionFunc actionFunc;
 } EnHs2; // size = 0x398
 
 extern const ActorInit En_Hs2_InitVars;
