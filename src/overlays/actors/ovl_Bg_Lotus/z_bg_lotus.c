c3ceb4169a444ae858d6efbe2f6deccd3bceed77
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-02-26 18:48:44 -0600

    decompile some related bgcheck functions

--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
@@ -9,6 +9,8 @@ void BgLotus_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgLotus_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgLotus_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80AD68DC(BgLotus* this, GlobalContext* globalCtx);
+
 const ActorInit Bg_Lotus_InitVars = {
     ACTOR_BG_LOTUS,
     ACTORCAT_BG,
@@ -28,7 +30,22 @@ u32 D_80AD6D10[] = {
     0x00000000,
 };
 
+extern BgMeshHeader D_06000A20;
+
 #pragma GLOBAL_ASM("asm/non_matchings/ovl_Bg_Lotus_0x80AD6760/BgLotus_Init.asm")
+/*
+void BgLotus_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgLotus* this = THIS;
+    s32 sp2C;
+
+    Actor_ProcessInitChain(&this->dyna.actor, D_80AD6D10);
+    BcCheck3_BgActorInit(&this->dyna, 1);
+    BgCheck3_LoadMesh(globalCtx, &this->dyna, &D_06000A20);
+    this->dyna.actor.bgCheckFlags = func_800C411C(&globalCtx->colCtx.dyna.bgActors[0].prevTransform.scale.z, &this->dyna.actor.floorHeight, &sp2C, this, &this->dyna.actor.world.pos);
+    this->unk160 = 96.0f;
+    this->dyna.actor.world.rot.y = rand() >> 0x10;
+    this->actionFunc = func_80AD68DC;
+}*/
 
 #pragma GLOBAL_ASM("asm/non_matchings/ovl_Bg_Lotus_0x80AD6760/BgLotus_Destroy.asm")
 
--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
@@ -5,9 +5,15 @@
 
 struct BgLotus;
 
+typedef void (*BgLotusActionFunc)(struct BgLotus*, GlobalContext*);
+
 typedef struct BgLotus {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x28];
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ BgLotusActionFunc actionFunc;
+    /* 0x160 */ f32 unk160;
+    /* 0x164 */ u8 unk164;
+    /* 0x166 */ s16 timer;
+    /* 0x168 */ s16 timer2;
 } BgLotus; // size = 0x16C
 
 extern const ActorInit Bg_Lotus_InitVars;
