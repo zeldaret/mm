d98937fa285cbd16ebda735396fd290353abeba4
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-02-27 22:35:27 -0600

    add data structures

--- src/overlays/actors/ovl_Door_Spiral/z_door_spiral.c
+++ src/overlays/actors/ovl_Door_Spiral/z_door_spiral.c
@@ -9,7 +9,6 @@ void DoorSpiral_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void DoorSpiral_Update(Actor* thisx, GlobalContext* globalCtx);
 void DoorSpiral_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
 const ActorInit Door_Spiral_InitVars = {
     ACTOR_DOOR_SPIRAL,
     ACTORCAT_DOOR,
@@ -21,7 +20,50 @@ const ActorInit Door_Spiral_InitVars = {
     (ActorFunc)DoorSpiral_Update,
     (ActorFunc)DoorSpiral_Draw,
 };
-*/
+
+typedef struct {
+    /* 0x00 */ Gfx* doorDList[2];
+    /* 0x08 */ s32 unk8;
+    /* 0x0C */ u8 unkC;
+    /* 0x0D */ u8 unkD;
+    /* 0x0E */ u8 unkE;
+    /* 0x0F */ u8 unkF;
+} SpiralStruct_809A3250;
+
+SpiralStruct_809A3250 D_809A3250[] = {
+    { { NULL, NULL }, 0, 130, 12, 50, 15 },
+    { { 0x050219E0, 0x0501D980 }, 0, 130, 12, 50, 15 },
+    { { 0x06004448, 0x060007A8 }, 0, 130, 12, 50, 15 },
+    { { 0x060051B8, 0x060014C8 }, 0, 130, 12, 50, 15 },
+    { { 0x06009278, 0x06006128 }, 0, 130, 12, 50, 15 },
+    { { 0x06013EA8, 0x06012B70 }, 0, 130, 12, 50, 15 },
+    { { 0x06000EA0, 0x06000590 }, 0, 130, 12, 50, 15 },
+    { { 0x06002110, 0x060012C0 }, 0, 130, 12, 50, 15 },
+};
+
+typedef struct {
+    /* 0x00 */ s16 objectBankId;
+    /* 0x02 */ u8 modelNum;
+} SpiralStruct_809A32D0;
+
+SpiralStruct_809A32D0 D_809A32D0[] = {
+    { GAMEPLAY_KEEP, 0 },    { GAMEPLAY_DANGEON_KEEP, 1 }, { OBJECT_NUMA_OBJ, 2 },      { OBJECT_HAKUGIN_OBJ, 4 },
+    { OBJECT_IKANA_OBJ, 5 }, { OBJECT_DANPEI_OBJECT, 7 },  { OBJECT_IKNINSIDE_OBJ, 6 },
+};
+
+typedef struct {
+    /* 0x00 */ s16 sceneNum;
+    /* 0x02 */ u8 doorType;
+} SpiralStruct_809A32EC;
+
+SpiralStruct_809A32EC D_809A32EC[] = {
+    { SCENE_MITURIN, 2 },     { SCENE_HAKUGIN, 3 },   { SCENE_INISIE_N, 4 }, { SCENE_INISIE_R, 4 },
+    { SCENE_DANPEI2TEST, 5 }, { SCENE_IKNINSIDE, 6 }, { SCENE_CASTLE, 6 },
+};
+
+u32 D_809A3308[] = {
+    0xC0580001, 0xB0FC0FA0, 0xB1000190, 0x31040190, 0x00000000, 0x00000000,
+};
 
 #pragma GLOBAL_ASM("asm/non_matchings/ovl_Door_Spiral_0x809A2B60/func_809A2B60.asm")
 
