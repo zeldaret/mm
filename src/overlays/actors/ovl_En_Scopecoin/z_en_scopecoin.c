b2748ac72311373b5c2996a87624ff80d13b2fc1
kyleburnette <kyle@kyleburnette.com>
2021-03-04 08:35:30 -0800

    Initial scopecoin work

--- src/overlays/actors/ovl_En_Scopecoin/z_en_scopecoin.c
+++ src/overlays/actors/ovl_En_Scopecoin/z_en_scopecoin.c
@@ -9,6 +9,9 @@ void EnScopecoin_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnScopecoin_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnScopecoin_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80BFCFA0(EnScopecoin* this, GlobalContext* globalCtx);
+void func_80BFCFB8(EnScopecoin* this, GlobalContext* globalCtx);
+
 /*
 const ActorInit En_Scopecoin_InitVars = {
     ACTOR_EN_SCOPECOIN,
@@ -23,14 +26,65 @@ const ActorInit En_Scopecoin_InitVars = {
 };
 */
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/func_80BFCFA0.asm")
+void func_80BFCFA0(EnScopecoin* this, GlobalContext* globalCtx) {
+    this->actor.shape.rot.y += 500;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/func_80BFCFB8.asm")
+void func_80BFCFB8(EnScopecoin* this, GlobalContext* globalCtx) {
+    if (Actor_GetCollectibleFlag(globalCtx, (this->actor.params & 0x7F0) >> 4)) {
+        func_800A7730();
+        Actor_MarkForDeath(&this->actor);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/EnScopecoin_Init.asm")
+void EnScopecoin_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnScopecoin* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/EnScopecoin_Destroy.asm")
+    Actor_SetScale(&this->actor, 0.01f);
+    Actor_SetDrawParams(&this->actor.shape, 0.0f, func_800B3FC0, 10.0f);
+    this->unk148 = this->actor.params & 0xF;
 
-#pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/EnScopecoin_Update.asm")
+    if ((this->unk148 < 0) || (this->unk148 >= 8)) {
+        this->unk148 = 0;
+    }
+    if ((globalCtx->actorCtx.unk5 & 2) != 0) {
+        if ((this->unk148 == 2) || (this->unk148 == 6)) {
+
+        } else {
+block_8:
+            this->actionFunc = func_80BFCFA0;
+            this->actor.shape.yOffset = 700.0f;
+            return;
+        }
+        if (Actor_GetCollectibleFlag(globalCtx, (this->actor.params & 0x7F0) >> 4) == 0) {
+            goto block_8;
+        }
+        Actor_MarkForDeath(&this->actor);
+        return;
+    }
+    if (this->unk148 == 2) {
+block_11:
+        if (Actor_GetCollectibleFlag(globalCtx, (this->actor.params & 0x7F0) >> 4) == 0) {
+            this->actor.draw = NULL;
+            this->actionFunc = func_80BFCFB8;
+            return;
+        }
+        Actor_MarkForDeath(&this->actor);
+        return;
+    }
+    if (this->unk148 == 6) {
+        goto block_11;
+    }
+    Actor_MarkForDeath(&this->actor);
+}
+
+void EnScopecoin_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+}
+
+void EnScopecoin_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnScopecoin* this = THIS;
+
+    this->actionFunc(this, globalCtx);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/ovl_En_Scopecoin_0x80BFCFA0/EnScopecoin_Draw.asm")
