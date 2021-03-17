a57518fc5242652a62bbdb6e1b8ca517f6a864af
Isghj <42048411+isghj5@users.noreply.github.com>
2021-03-16 17:35:11 -0700

    Update src/overlays/actors/ovl_En_Ma4/z_en_ma4.h
    Co-authored-by: Rozelette <Rozelette@users.noreply.github.com>

--- src/overlays/actors/ovl_En_Ma4/z_en_ma4.h
+++ src/overlays/actors/ovl_En_Ma4/z_en_ma4.h
@@ -9,7 +9,7 @@ typedef struct EnMa4 {
     /* 0x000 */ Actor actor;
     /* 0x144 */ char unk_144[0x1F4];
     /* 0x338 */ s16 unk338; // modified by EnPoFusen
-    /* 0x33A */ char unk33A[0x8];
+    /* 0x33A */ char unk33A[0x6];
     
 } EnMa4; // size = 0x340
 
