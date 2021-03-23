74527b6fd4c6e76fbad237cdb3e51bed686b8e25
Rozelette <Uberpanzermensch@gmail.com>
2021-03-23 09:33:26 -0500

    Set up file

---
+++ src/code/z_skin_matrix.c
@@ -0,0 +1,50 @@
+#include <ultra64.h>
+#include <global.h>
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MultiplyByVectorXYZW.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MultiplyByVectorXYZ.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_Multiply.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_GetIdentity.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeIdentity.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_Copy.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_Invert.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeScale.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeRotationZYX.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeRotationYXZ.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeTranslation.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeTranslationRotationZYXScale.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeTranslationRotationYXZScale.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeTranslationRotationZYX.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_ToVec3s.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_ToVec3f.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_ToRSPMatrix.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_AppendToPloyOpaDisp.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeRotationAroundUnitVector.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeXRotation.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_XRotation.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeYRotation.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_YRotation.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_skin_matrix/Matrix_MakeZRotation.asm")
