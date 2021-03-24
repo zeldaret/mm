9ec69d8e54324dd7bf4ef9e218ebdbcb69f7f64b
Rozelette <Uberpanzermensch@gmail.com>
2021-03-24 03:41:13 -0500

    Fixup for z_collision_check

--- src/code/z_en_item00.c
+++ src/code/z_en_item00.c
@@ -29,8 +29,8 @@ ActorInit En_Item00_InitVars = {
 };
 
 ColliderCylinderInit enItem00CylinderInit = {
-    { 0x0A, 0x00, 0x09, 0x00, 0x00, 0x01 },
-    { 0x00, { 0x00000000, 0x00, 0x00 }, { 0x00000010, 0x00, 0x00 }, 0x00, 0x01, 0x00 },
+    { COLTYPE_NONE, AT_NONE, AC_ON | AT_TYPE_PLAYER, OC1_NONE, OC2_NONE, COLSHAPE_CYLINDER },
+    { ELEMTYPE_UNK0, { 0x00000000, 0x00, 0x00 }, { 0x00000010, 0x00, 0x00 }, TOUCH_NONE | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_NONE },
     { 10, 30, 0, { 0, 0, 0 } },
 };
 
