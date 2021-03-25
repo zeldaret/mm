60756992b032553fe0279d1002ec2df7a5c4df0b
kyleburnette <kyle@kyleburnette.com>
2021-03-24 19:28:21 -0700

    initial dm_sa work

--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
@@ -9,6 +9,8 @@ void DmSa_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void DmSa_Update(Actor* thisx, GlobalContext* globalCtx);
 void DmSa_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80A2EABC(DmSa* this, GlobalContext* globalCtx);
+
 /*
 const ActorInit Dm_Sa_InitVars = {
     ACTOR_DM_SA,
@@ -23,9 +25,46 @@ const ActorInit Dm_Sa_InitVars = {
 };
 */
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Sa_0x80A2E960/func_80A2E960.asm")
+extern SkeletonHeader D_06013328;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Sa_0x80A2E960/DmSa_Init.asm")
+s32 D_80A2ED00[] = {
+/* 000232 0x80A2ED00 */ 0x0600CC94,
+/* 000233 0x80A2ED04 */ 0x3F800000,
+/* 000234 0x80A2ED08 */ 0x00000000,
+/* 000235 0x80A2ED0C */ 0xBF800000,
+/* 000236 0x80A2ED10 */ 0x00000000,
+/* 000237 0x80A2ED14 */ 0x00000000,
+/* 000238 0x80A2ED18 */ 0x00000000,
+/* 000239 0x80A2ED1C */ 0x00000000
+};
+
+void func_80A2E960(SkelAnime* skelAnime, s32 arg1, s32 arg2) {
+    f32 temp_f0;
+    void *temp_s0;
+    f32 phi_f2;
+
+    temp_s0 = arg1 + ((arg2 & 0xFFFF) * 0x18);
+    temp_f0 = temp_s0->unkC;
+    if (temp_f0 < 0.0f) {
+        phi_f2 = (f32) SkelAnime_GetFrameCount(temp_s0->unk0);
+    } else {
+        phi_f2 = temp_f0;
+    }
+    SkelAnime_ChangeAnim(arg0, (AnimationHeader *) temp_s0->unk0, (bitwise f32) temp_s0->unk4, (bitwise f32) temp_s0->unk8, phi_f2, (?32) temp_s0->unk10, temp_s0->unk14);
+}
+
+void DmSa_Init(Actor* thisx, GlobalContext* globalCtx) {
+    DmSa* this = THIS;
+
+    this->unk2E0 = 0;
+    this->unk2F0 = 0xFF;
+    this->actor.targetArrowOffset = 3000.0f;
+    Actor_SetDrawParams(&this->actor.shape, 0.0f, func_800B3FC0, 24.0f);
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06013328, NULL, 0, 0, 0);
+    func_80A2E960(&this->skelAnime, &D_80A2ED00, 0);
+    Actor_SetScale(&this->actor, 0.01f);
+    this->actionFunc = func_80A2EABC;
+}
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Sa_0x80A2E960/DmSa_Destroy.asm")
 
--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
@@ -5,9 +5,17 @@
 
 struct DmSa;
 
+typedef void (*DmSaActionFunc)(struct DmSa*, GlobalContext*);
+
 typedef struct DmSa {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x1B0];
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x194 */ char unk194[0x108];
+    /* 0x290 */ DmSaActionFunc actionFunc;
+    /* 0x294 */ char unk294[0x4C];
+    /* 0x2E0 */ u16 unk2E0;
+    /* 0x2E2 */ char unk2E2[0xE];
+    /* 0x2F0 */ u32 unk2F0;
 } DmSa; // size = 0x2F4
 
 extern const ActorInit Dm_Sa_InitVars;
