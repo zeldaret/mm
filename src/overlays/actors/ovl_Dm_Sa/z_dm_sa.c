2b80e8a5dc3d2400a1c5c1f603b7f4e6d6f6c521
kyleburnette <kyle@kyleburnette.com>
2021-03-25 09:06:36 -0700

    more dmsa work

--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
@@ -11,6 +11,15 @@ void DmSa_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 void func_80A2EABC(DmSa* this, GlobalContext* globalCtx);
 
+typedef struct {
+    /* 0x00 */ AnimationHeader* animation;
+    /* 0x04 */ f32 playbackSpeed;
+    /* 0x08 */ f32 unk_08;
+    /* 0x0C */ f32 frameCount;
+    /* 0x10 */ u8 unk_10;
+    /* 0x14 */ f32 transitionRate;
+} DmSa_AnimationStruct; // size = 0x18
+
 /*
 const ActorInit Dm_Sa_InitVars = {
     ACTOR_DM_SA,
@@ -26,31 +35,23 @@ const ActorInit Dm_Sa_InitVars = {
 */
 
 extern SkeletonHeader D_06013328;
+extern AnimationHeader D_0600788C;
 
-s32 D_80A2ED00[] = {
-/* 000232 0x80A2ED00 */ 0x0600CC94,
-/* 000233 0x80A2ED04 */ 0x3F800000,
-/* 000234 0x80A2ED08 */ 0x00000000,
-/* 000235 0x80A2ED0C */ 0xBF800000,
-/* 000236 0x80A2ED10 */ 0x00000000,
-/* 000237 0x80A2ED14 */ 0x00000000,
-/* 000238 0x80A2ED18 */ 0x00000000,
-/* 000239 0x80A2ED1C */ 0x00000000
-};
+static DmSa_AnimationStruct animation = { &D_0600788C, 1.0f, 0.0f, 0.0f, 0x00, 0.0f };
 
-void func_80A2E960(SkelAnime* skelAnime, s32 arg1, s32 arg2) {
-    f32 temp_f0;
-    void *temp_s0;
+void func_80A2E960(SkelAnime* skelAnime, DmSa_AnimationStruct* animation1, s32 arg2) {
+    f32 frameCount;
+    AnimationHeader* animationHeader;
     f32 phi_f2;
 
-    temp_s0 = arg1 + ((arg2 & 0xFFFF) * 0x18);
-    temp_f0 = temp_s0->unkC;
-    if (temp_f0 < 0.0f) {
-        phi_f2 = (f32) SkelAnime_GetFrameCount(temp_s0->unk0);
+    animationHeader = &animation1[arg2];
+    frameCount = animation1->frameCount;
+    if (frameCount < 0.0f) {
+        phi_f2 = SkelAnime_GetFrameCount(&animationHeader->genericHeader);
     } else {
-        phi_f2 = temp_f0;
+        phi_f2 = frameCount;
     }
-    SkelAnime_ChangeAnim(arg0, (AnimationHeader *) temp_s0->unk0, (bitwise f32) temp_s0->unk4, (bitwise f32) temp_s0->unk8, phi_f2, (?32) temp_s0->unk10, temp_s0->unk14);
+    SkelAnime_ChangeAnim(skelAnime, animationHeader, animation1->playbackSpeed, animation1->unk_08, phi_f2, animation1->unk_10, animation1->transitionRate);
 }
 
 void DmSa_Init(Actor* thisx, GlobalContext* globalCtx) {
@@ -61,7 +62,7 @@ void DmSa_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->actor.targetArrowOffset = 3000.0f;
     Actor_SetDrawParams(&this->actor.shape, 0.0f, func_800B3FC0, 24.0f);
     SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_06013328, NULL, 0, 0, 0);
-    func_80A2E960(&this->skelAnime, &D_80A2ED00, 0);
+    func_80A2E960(&this->skelAnime, &animation, 0);
     Actor_SetScale(&this->actor, 0.01f);
     this->actionFunc = func_80A2EABC;
 }
