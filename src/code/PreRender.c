6d7b3f7d20a3a5b4c35112476200fb8480136161
Lucas Shaw <lucas.shaw1123@gmail.com>
2021-03-26 11:09:59 -0700

    migration of two files

---
+++ src/code/PreRender.c
@@ -0,0 +1,56 @@
+#include <ultra64.h>
+#include <global.h>
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FCF0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FD2C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FD60.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FD94.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FDB8.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FF70.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8016FF90.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170200.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8017023C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8017057C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801705B4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801705EC.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170730.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170774.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170798.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170AE0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170B28.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80170B4C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_8017160C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801716C4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801717F8.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80171F4C.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80171FA8.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80172078.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801720C4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_801720FC.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/PreRender/func_80172758.asm")
---
+++ src/code/listalloc.c
@@ -0,0 +1,10 @@
+#include <ultra64.h>
+#include <global.h>
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/listalloc/func_80174AA0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/listalloc/func_80174AB4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/listalloc/func_80174B20.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/listalloc/func_80174BA0.asm")
