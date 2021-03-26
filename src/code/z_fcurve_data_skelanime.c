a70fc357af531c6960669aa2aab2ca3edb72ac33
Lucas Shaw <lucas.shaw1123@gmail.com>
2021-03-26 11:20:57 -0700

    z_fcurve_data_skelanime split

---
+++ src/code/z_fcurve_data_skelanime.c
@@ -0,0 +1,16 @@
+#include <ultra64.h>
+#include <global.h>
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F2620.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F2650.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F26C0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F26F0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F2728.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F29A0.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/z_fcurve_data_skelanime/func_800F2CD8.asm")
