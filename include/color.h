fee7a49abc84fbe91116f969a90570cff15decde
Rozelette <Rozelette@users.noreply.github.com>
2021-03-27 14:29:30 -0500

    z_lights fully matched (#80)
    * z_lights OK
    
    * Documentation
    
    * Fixup for z_collision_check
    
    * Improve function types of bind functions
    
    * Fixup for types

--- include/color.h
+++ include/color.h
@@ -4,23 +4,38 @@
 #include <PR/ultratypes.h>
 
 typedef struct {
-    /* 0x0 */ u8 red;
-    /* 0x1 */ u8 green;
-    /* 0x2 */ u8 blue;
-    /* 0x3 */ u8 alpha;
-} ColorRGBA8; // size = 0x4
+    /* 0x0 */ u8 r;
+    /* 0x1 */ u8 g;
+    /* 0x2 */ u8 b;
+} Color_RGB8; // size = 0x3
 
 typedef struct {
-    /* 0x0 */ u8 red;
-    /* 0x1 */ u8 green;
-    /* 0x2 */ u8 blue;
-} RGB; // size = 0x3
+    /* 0x0 */ u8 r;
+    /* 0x1 */ u8 g;
+    /* 0x2 */ u8 b;
+    /* 0x3 */ u8 a;
+} Color_RGBA8; // size = 0x4
+
+// only use when necessary for alignment purposes
+typedef union {
+    struct {
+        u8 r, g, b, a;
+    };
+    u32 rgba;
+} Color_RGBA8_u32;
 
 typedef struct {
-    /* 0x0 */ u8 red;
-    /* 0x1 */ u8 green;
-    /* 0x2 */ u8 blue;
-    /* 0x3 */ u8 alpha;
-} RGBA8; // size = 0x4
+    f32 r, g, b, a;
+} Color_RGBAf;
+
+typedef union {
+    struct {
+        u16 r : 5;
+        u16 g : 5;
+        u16 b : 5;
+        u16 a : 1;
+    };
+    u16 rgba;
+} Color_RGB5A1;
 
 #endif
