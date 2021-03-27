13124082e1e2e11c961535fd1568a1558ef822c4
kyleburnette <kyle@kyleburnette.com>
2021-03-27 11:42:41 -0700

    Changed char arrays in header to be UNK_TYPE1

--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
@@ -10,13 +10,13 @@ typedef void (*DmSaActionFunc)(struct DmSa*, GlobalContext*);
 typedef struct DmSa {
     /* 0x000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;
-    /* 0x188 */ char unk194[0x108];
+    /* 0x188 */ UNK_TYPE1 unk194[0x108];
     /* 0x290 */ DmSaActionFunc actionFunc;
-    /* 0x294 */ char unk294[0x20];
+    /* 0x294 */ UNK_TYPE1 unk294[0x20];
     /* 0x2B4 */ s32 unk2B4;
-    /* 0x2B8 */ char unk2B8[0x28];
+    /* 0x2B8 */ UNK_TYPE1 unk2B8[0x28];
     /* 0x2E0 */ u16 unk2E0;
-    /* 0x2E2 */ char unk2E2[0xE];
+    /* 0x2E2 */ UNK_TYPE1 unk2E2[0xE];
     /* 0x2F0 */ u32 alpha;
 } DmSa; // size = 0x2F4
 
