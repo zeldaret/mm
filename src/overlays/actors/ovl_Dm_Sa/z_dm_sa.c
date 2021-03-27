e15bcea10f166e184cff6eadc728489c6d334f9a
kyleburnette <kyle@kyleburnette.com>
2021-03-27 12:13:01 -0700

    Made all of roz's suggested changes

--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.c
@@ -33,7 +33,7 @@ void func_80A2E960(SkelAnime* arg0, ActorAnimationEntry* animations, u16 index) 
     animations += index;
 
     if (animations->frameCount < 0.0f) {
-        frameCount = (f32)SkelAnime_GetFrameCount(animations->animation);
+        frameCount = SkelAnime_GetFrameCount(animations->animation);
     } else {
         frameCount = animations->frameCount;
     }
@@ -82,7 +82,7 @@ Gfx* func_80A2EB58(GraphicsContext* gfxCtx, u32 alpha) {
     Gfx* dList;
     Gfx* dListHead;
 
-    dList = dListHead = GRAPH_ALLOC(gfxCtx, sizeof(Gfx) * 2);
+    dList = dListHead = GRAPH_ALLOC(gfxCtx, sizeof(Gfx) * 2); //! @bug this does not allocate enough for 3 Gfx commands
     gDPSetRenderMode(dListHead++, G_RM_FOG_SHADE_A, G_RM_AA_ZB_XLU_SURF2);
     gDPSetEnvColor(dListHead++, 0, 0, 0, alpha);
     gSPEndDisplayList(dListHead++);
