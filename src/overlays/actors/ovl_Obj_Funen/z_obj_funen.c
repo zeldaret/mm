c7377f04171a0c81b5cb2f15b309d9d6c249b0b9
kyleburnette <kyle@kyleburnette.com>
2021-03-31 18:39:44 -0700

    obj_funen OK

--- src/overlays/actors/ovl_Obj_Funen/z_obj_funen.c
+++ src/overlays/actors/ovl_Obj_Funen/z_obj_funen.c
@@ -7,7 +7,6 @@
 void ObjFunen_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjFunen_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
 const ActorInit Obj_Funen_InitVars = {
     ACTOR_OBJ_FUNEN,
     ACTORCAT_PROP,
@@ -19,26 +18,27 @@ const ActorInit Obj_Funen_InitVars = {
     (ActorFunc)func_800BDFB0,
     (ActorFunc)ObjFunen_Draw
 };
-*/
 
 extern Gfx D_060000D0[];
 
-f32 D_80A198D0[] = {0.1f, 0.025f};
+f32 D_80A198D0[] = {0.1f, 0.024390244f};
 
 void ObjFunen_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjFunen* this = THIS;
 
-    Actor_SetScale(this, D_80A198D0[this->actor.params & 1]);
+    Actor_SetScale(&this->actor, D_80A198D0[this->actor.params & 1]);
 }
 
 void ObjFunen_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    u32 temp_a3;
-
+    s32 temp_a3;
+    s32 pad[2];
+    
     OPEN_DISPS(globalCtx->state.gfxCtx);
-    func_8012C2DC(globalCtx->state.gfxCtx);
-    SysMatrix_InsertYRotation_s(func_800DFCDC(ACTIVE_CAM) - 0x8000, 1);
 
-    gSPMatrix(POLY_XLU_DISP++, SysMatrix_AppendStateToPolyOpaDisp(globalCtx->state.gfxCtx), G_MTX_MODELVIEW | G_MTX_LOAD | G_MTX_NOPUSH);
+    func_8012C2DC(globalCtx->state.gfxCtx);
+    Matrix_RotateY((s16)(func_800DFCDC(ACTIVE_CAM) - 0x8000), 1);
+
+    gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_MODELVIEW | G_MTX_LOAD | G_MTX_NOPUSH);
 
     temp_a3 = -(globalCtx->unk18840 & 0x7FFFFFFF) & 0x7F;
 
