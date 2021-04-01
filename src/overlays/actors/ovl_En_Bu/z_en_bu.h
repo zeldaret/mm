2e9c2c6ae94a0c115d08212939f4796edfcb7755
kyleburnette <kyle@kyleburnette.com>
2021-04-01 11:32:24 -0700

    made the char array in the header of type UNK_TYPE1

--- src/overlays/actors/ovl_En_Bu/z_en_bu.h
+++ src/overlays/actors/ovl_En_Bu/z_en_bu.h
@@ -10,7 +10,7 @@ typedef void (*EnBuActionFunc)(struct EnBu*, GlobalContext*);
 typedef struct EnBu {
     /* 0x000 */ Actor actor;
     /* 0x144 */ EnBuActionFunc actionFunc;
-    /* 0x148 */ char unk148[0x44];
+    /* 0x148 */ UNK_TYPE1 unk148[0x44];
     /* 0x18C */ s32 unk18C;
 } EnBu; // size = 0x190
 
