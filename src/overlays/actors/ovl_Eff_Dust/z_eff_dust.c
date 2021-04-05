fb23d0f181c1b206670ab32483ebe4bfe1b1b751
Angie <angheloalf95@gmail.com>
2021-04-05 10:44:09 -0400

    init is cursed
    Signed-off-by: Angie <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_Eff_Dust/z_eff_dust.c
+++ src/overlays/actors/ovl_Eff_Dust/z_eff_dust.c
@@ -1,3 +1,9 @@
+/**
+ * File z_eff_dust.c
+ * Overlay: ovl_Eff_Dust
+ * Description: Dust effects
+ */
+
 #include "z_eff_dust.h"
 
 #define FLAGS 0x00000030
@@ -8,6 +14,14 @@ void EffDust_Init(Actor* thisx, GlobalContext* globalCtx);
 void EffDust_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EffDust_Update(Actor* thisx, GlobalContext* globalCtx);
 void EffDust_Draw(Actor* thisx, GlobalContext* globalCtx);
+
+
+void func_80918D64(EffDust* this, GlobalContext* globalCtx);
+void func_80918FE4(EffDust* this, GlobalContext* globalCtx);
+void func_80919230(EffDust* this, GlobalContext* globalCtx);
+
+void func_80919768(Actor* this, GlobalContext* globalCtx);
+void func_809199FC(Actor* this, GlobalContext* globalCtx);
 
 /*
 const ActorInit Eff_Dust_InitVars = {
@@ -23,16 +37,105 @@ const ActorInit Eff_Dust_InitVars = {
 };
 */
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Eff_Dust_0x80918B40/func_80918B40.asm")
+void func_80918B40(EffDust* this) {
+    s32 i;
 
-void EffDust_Init(Actor* thisx, GlobalContext* globalCtx);
+    for (i = 0; i < ARRAY_COUNT(this->distanceTraveled); i++) {
+        this->initialPositions[i].z = 0.0f;
+        this->initialPositions[i].y = 0.0f;
+        this->initialPositions[i].x = 0.0f;
+
+        this->distanceTraveled[i] = 1.0f;
+    }
+    this->index = 0;
+}
+
+/*
+void EffDust_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EffDust* this = THIS;
+    u32 sp18 = this->actor.params;
+
+    func_80918B40(this);
+    switch (sp18) {
+    case 0:
+        this->actionFunc = func_80918D64;
+        this->actor.draw = func_80919768;
+        this->dx = 1.0f;
+        this->dy = 1.0f;
+        this->dz = 1.0f;
+        this->scalingFactor = 0.2f;
+        break;
+    case 1:
+        this->actionFunc = func_80918FE4;
+        this->actor.draw = func_80919768;
+        this->dy = 1.0f;
+        this->dx = 0.8f;
+        this->dz = 0.8f;
+        this->scalingFactor = 0.5f;
+        break;
+    case 2:
+        this->actionFunc = func_80919230;
+        this->actor.draw = func_809199FC;
+        this->actor.room = -1;
+        this->dx = 0.5f;
+        this->scalingFactor = 15.0f;
+        break;
+    case 4:
+        this->actionFunc = func_80919230;
+        this->actor.draw = func_809199FC;
+        this->actor.room = -1;
+        this->dx = 0.5f;
+        this->scalingFactor = 10.0f;
+        break;
+    case 5:
+        this->actionFunc = func_80919230;
+        this->actor.draw = func_809199FC;
+        this->actor.room = -1;
+        this->dx = 0.5f;
+        this->scalingFactor = 20.0f;
+        break;
+    default:
+        StartHeap_Free(this);
+        break;
+    }
+    this->life = 10;
+}
+*/
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Eff_Dust_0x80918B40/EffDust_Init.asm")
 
 void EffDust_Destroy(Actor *thisx, GlobalContext *globalCtx) {
-
 }
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Eff_Dust_0x80918B40/func_80918D64.asm")
+void func_80918D64(EffDust *this, GlobalContext *globalCtx) {
+    s16 theta;
+    s16 fi;
+    s32 i;
+    s32 j;
+    f32* distanceTraveled = this->distanceTraveled;
+
+    for (i = 0; i < 64; i++) {
+        if ((*distanceTraveled) < 1.0f) {
+            *distanceTraveled += 0.05f;
+        }
+        distanceTraveled++;
+    }
+
+    if (this->actor.home.rot.z != 0) {
+        for (j = 0; j < 3; j++) {
+            i = this->index & 0x3F;
+            if (this->distanceTraveled[i] >= 1.0f) {
+                // Spherical coordinate system.
+                fi = randPlusMinusPoint5Scaled(0x10000);
+                theta = Rand_ZeroFloat(0x1000);
+                this->initialPositions[i].x = -(f32) this->actor.home.rot.z * Math_CosS(fi) * Math_CosS(theta);
+                this->initialPositions[i].y = -(f32) this->actor.home.rot.z * Math_SinS(fi) * Math_CosS(theta);
+                this->initialPositions[i].z = -(f32) this->actor.home.rot.z * Math_SinS(theta);
+                this->distanceTraveled[i] = 0.0f;
+                this->index++;
+            }
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Eff_Dust_0x80918B40/func_80918FE4.asm")
 
@@ -41,7 +144,7 @@ void EffDust_Destroy(Actor *thisx, GlobalContext *globalCtx) {
 void EffDust_Update(Actor* thisx, GlobalContext* globalCtx) {
     EffDust* this = THIS;
 
-    this->actionFunc(thisx, globalCtx);
+    this->actionFunc(this, globalCtx);
 }
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Eff_Dust_0x80918B40/func_80919768.asm")
--- src/overlays/actors/ovl_Eff_Dust/z_eff_dust.h
+++ src/overlays/actors/ovl_Eff_Dust/z_eff_dust.h
@@ -9,10 +9,17 @@ typedef void (*EffDustActionFunc)(struct EffDust*, GlobalContext*);
 typedef void (*EffDustDrawFunc)(Actor*, GlobalContext*);
 
 typedef struct EffDust {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x414];
-    /* 0x558 */ EffDustActionFunc actionFunc;
-    /* 0x55C */ EffDustDrawFunc drawFunc;
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ f32 distanceTraveled[64]; // For each particle. Normalized. From 0.0f to 1.0f
+    /* 0x024C */ Vec3f initialPositions[64]; // Array of position for each dust particle.
+    /* 0x0544 */ u8 index;
+    /* 0x0545 */ u8 life; // Only considered if actor.params is 2, 3 or 4.
+    /* 0x0548 */ f32 dx; // Normalized. 0.0f to 1.0f
+    /* 0x054C */ f32 dy; // Normalized. 0.0f to 1.0f
+    /* 0x0550 */ f32 dz; // Normalized. 0.0f to 1.0f
+    /* 0x0554 */ f32 scalingFactor;
+    /* 0x0558 */ EffDustActionFunc actionFunc;
+    /* 0x055C */ EffDustDrawFunc drawFunc;
 } EffDust; // size = 0x560
 
 extern const ActorInit Eff_Dust_InitVars;
