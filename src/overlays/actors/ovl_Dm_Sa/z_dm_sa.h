50d0bbe0876f213dd6ad3441230b988a6c145c25
kyleburnette <kyle@kyleburnette.com>
2021-04-07 15:08:19 -0700

    dm_sa OK (#88)
    * initial dm_sa work
    
    * more dmsa work
    
    * dm_sa OK
    
    * Changed char arrays in header to be UNK_TYPE1
    
    * Made all of roz's suggested changes
    
    * merge

--- src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
+++ src/overlays/actors/ovl_Dm_Sa/z_dm_sa.h
@@ -5,9 +5,19 @@
 
 struct DmSa;
 
+typedef void (*DmSaActionFunc)(struct DmSa*, GlobalContext*);
+
 typedef struct DmSa {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x1B0];
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ UNK_TYPE1 unk194[0x108];
+    /* 0x290 */ DmSaActionFunc actionFunc;
+    /* 0x294 */ UNK_TYPE1 unk294[0x20];
+    /* 0x2B4 */ s32 unk2B4;
+    /* 0x2B8 */ UNK_TYPE1 unk2B8[0x28];
+    /* 0x2E0 */ u16 unk2E0;
+    /* 0x2E2 */ UNK_TYPE1 unk2E2[0xE];
+    /* 0x2F0 */ u32 alpha;
 } DmSa; // size = 0x2F4
 
 extern const ActorInit Dm_Sa_InitVars;
