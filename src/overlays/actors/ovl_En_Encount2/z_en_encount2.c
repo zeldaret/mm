775cc05e07e03313fadcdc343d98ef276ee8c4af
isghj8 <isghj8@gmail.com>
2021-04-10 16:29:31 -0700

    EnEncount2: got some easy functions down, but now I have a really weird unk struct

--- src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
+++ src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
@@ -7,8 +7,15 @@
 void EnEncount2_Init(Actor* thisx, GlobalContext* globalCtx);
 void EnEncount2_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnEncount2_Update(Actor* thisx, GlobalContext* globalCtx);
+//void EnEncount2_Update(EnEncount2* this, GlobalContext* globalCtx);
 void EnEncount2_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+//actionfunc
+void func_808E1714(EnEncount2* this, GlobalContext* globalCtx);
+void func_808E17C4(EnEncount2* this, GlobalContext* globalCtx);
+void func_808E18A8(EnEncount2* this, GlobalContext* globalCtx);
+
+f32 func_808E1A24(EnEncount2* this, Vec3f* vec, s16 arg2);
 
 const ActorInit En_Encount2_InitVars = {
     ACTOR_EN_ENCOUNT2,
@@ -30,6 +37,7 @@ static ColliderJntSphElementInit D_808E1EF0[1] = {
     },
 };
 
+/*
 //glabel D_808E1F14 collider init
 static ColliderJntSphInit D_808E1F14 = {
 //static ColliderJntSphInit sJntSphInit = {
@@ -37,6 +45,7 @@ static ColliderJntSphInit D_808E1F14 = {
     1, D_808E1EF0,
     //1, sJntSphElementsInit,
 };
+*/
 
 DamageTable D_808E1F24[] = {
     0xF0, 0xF0, 0x00, 0xF0, 0xE1, 0xE1, 0x00, 0xE1, 0xF0, 0xF0, 0xF0, 0xE1, 0xE1, 0xE1, 0xF0, 0xF0, 
@@ -50,7 +59,7 @@ UNK_TYPE1 unkpadding[] = {
 };
 
 /*
-// does not fit because THIS
+//issue: collider init is confused
 void EnEncount2_Init(Actor *thisx, GlobalContext *globalCtx) {
     EnEncount2* this = THIS;
     BgMeshHeader* bgMeshHeader = NULL;
@@ -88,7 +97,6 @@ void EnEncount2_Init(Actor *thisx, GlobalContext *globalCtx) {
     this->actor.colChkInfo.damageTable = &D_808E1F24;
     func_808E16FC(this);
 } // */
-
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/EnEncount2_Init.asm")
 
 void EnEncount2_Destroy(Actor *thisx, GlobalContext *globalCtx) {
@@ -97,20 +105,83 @@ void EnEncount2_Destroy(Actor *thisx, GlobalContext *globalCtx) {
     Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E16FC.asm")
+// init extension func
+void func_808E16FC(EnEncount2 *this) {
+    this->unk162 = 0;
+    this->actionFunc = &func_808E1714;
+}
+//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E16FC.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1714.asm")
+//actionfunc
+void func_808E1714(EnEncount2 *this, GlobalContext *globalCtx) {
+    // is this rodata? its not in the asm file as late rodata... extern for now
+    this->unk16C += D_808E1F54;
+    this->actor.velocity.y = Math_SinS((s16) (s32) this->unk16C);
+    Math_SmoothScaleMaxF(&this->unk168, 0.1f, 0.3f, 0.01f);
+    if (((this->collider.base.acFlags & 2) != 0) && (this->actor.colChkInfo.damageEffect == 0xE)) {
+        this->actor.colChkInfo.health = 0;
+        this->unk162 = 1;
+        this->actionFunc = func_808E17C4;
+    }
+}
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E17C4.asm")
+//actionfunc
+void func_808E17C4(EnEncount2 *this, GlobalContext *globalCtx) {
+    s32 i;
+    Vec3f curPos;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E18A8.asm")
+    Math_Vec3f_Copy(&curPos, &this->actor.world);
+    curPos.y += 60.0f;
+    Actor_Spawn(&globalCtx->actorCtx, globalCtx, 0xA2, curPos.x, curPos.y, curPos.z, 0xFF, 0xFF, 0xC8, 1);
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/EnEncount2_Update.asm")
+    for(i = 0; i != 100; i++){
+        func_808E1A24(this, &curPos, 10);
+    }
+
+    Audio_PlayActorSound2(this, 0x2949);
+    this->unk160 = 0x1E; // timer?
+    this->actionFunc = func_808E18A8;
+}
+
+//actionfunc
+void func_808E18A8(EnEncount2 *this, GlobalContext *globalCtx) {
+    if (this->unk160 == 0) {
+        if (this->unk164 >= 0) {
+            Actor_SetSwitchFlag(globalCtx, this->unk164);
+        }
+        Actor_MarkForDeath(&this->actor);
+    }
+}
+
+void EnEncount2_Update(Actor* thisx, GlobalContext *globalCtx) {
+    EnEncount2* this = THIS;
+    s8 pad[4];
+
+    if (this->unk160 != 0) {
+        this->unk160--;
+    }
+
+    this->actor.shape.rot.y = this->actor.world.rot.y;
+    Actor_SetHeight(&this->actor, 30.0f);
+    Actor_SetScale(&this->actor, this->unk168);
+    this->actionFunc(this, globalCtx);
+    Actor_SetVelocityAndMoveYRotationAndGravity(&this->actor);
+    func_808E1B4C(this, globalCtx);
+
+    if (this->unk162 == 0) {
+        Collider_UpdateSpheresElement(&this->collider, 0, &this->actor);
+        CollisionCheck_SetAC(globalCtx, &globalCtx->colCheckCtx, &this->collider);
+        CollisionCheck_SetOC(globalCtx, &globalCtx->colCheckCtx, &this->collider);
+    }
+}
+
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/EnEncount2_Draw.asm")
 
+// issue: we dont know what type these structs are, only that they are size: 0x34
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1A24.asm")
 
+// update extension
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1B4C.asm")
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1C9C.asm")
--- src/overlays/actors/ovl_En_Encount2/z_en_encount2.h
+++ src/overlays/actors/ovl_En_Encount2/z_en_encount2.h
@@ -5,19 +5,39 @@
 
 typedef void (*EnEncount2ActionFunc)(struct EnEncount2*, GlobalContext*);
 
+typedef struct unkEnE2Struct {
+   /* 0x00 */ s8 unk0;
+   /* 0x01 */ UNK_TYPE1 unk1[3];
+   /* 0x04 */ s32 unk4;
+   /* 0x08 */ s32 unk8;
+   /* 0x0C */ s32 unkC;
+   /* 0x10 */ s8  unk10[0x4];
+   /* 0x14 */ s16 unk14;
+   /* 0x16 */ s16 unk16;
+   /* 0x18 */ f32 unk18;
+
+} unkEnE2Struct; // size 0x34
+
 struct EnEncount2;
 
 typedef struct EnEncount2 {
     /* 0x0000 */ Actor actor;
     ///* 0x0144 */ char unk_0144[0x292C];
     /* 0x0144 */ s32 unk144; // index for destroy jnt sph?
-    /* 0x0148 */ UNK_TYPE1 unk_0148[0x1C];
+    /* 0x0148 */ UNK_TYPE1 unk_0148[0x14];
+    /* 0x015C */ EnEncount2ActionFunc actionFunc; // index for destroy jnt sph?
+    /* 0x0160 */ s16 unk160;
+    /* 0x0162 */ s16 unk162; //func_808E16FC
     /* 0x0164 */ s16 unk164; //init from params, ""flag"
     /* 0x0166 */ s16 unk166; //pad
-    /* 0x0168 */ f32 unk168; //from rodata?
-    /* 0x016C */ UNK_TYPE1 unk16C[0x4];
+    /* 0x0168 */ f32 unk168; //gets used for scale in update
+    /* 0x016C */ f32 unk16C;
     /* 0x0170 */ ColliderJntSph collider;
     /* 0x0190 */ ColliderJntSphElement colElement;
+                // wild guess, there are only like 2 structs defined anywhere by anyone at this size
+    ///* 0x01D0 */ EffectSsBlastInitParams unkStructs[3]; //size unk
+
+    // where the heck is the rest of the data? is element that big?
       
     ///* 0x0148 */ UNK_TYPE1 unk_0148[0x28];
 } EnEncount2; // size = 0x2A70
@@ -27,5 +47,7 @@ ColliderJntSphInit D_808E1F14;
 extern const ActorInit En_Encount2_InitVars;
 
 extern BgMeshHeader D_06002420;
+
+extern f32 D_808E1F54; // not found in rodata
 
 #endif // Z_EN_ENCOUNT2_H
