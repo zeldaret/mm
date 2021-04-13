d6191b65ecc7c1aa3d5d2a4acdfbf314c13f8dec
isghj8 <isghj8@gmail.com>
2021-04-12 23:09:07 -0700

    EnEncount2: cleaning/docs

--- src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
+++ src/overlays/actors/ovl_En_Encount2/z_en_encount2.c
@@ -5,20 +5,17 @@
 #define THIS ((EnEncount2*)thisx)
 
 void EnEncount2_Init(Actor* thisx, GlobalContext* globalCtx);
-//void EnEncount2_Init(EnEncount2* this, GlobalContext* globalCtx);
 void EnEncount2_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnEncount2_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnEncount2_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-//actionfunc
 void func_808E1714(EnEncount2* this, GlobalContext* globalCtx);
 void func_808E17C4(EnEncount2* this, GlobalContext* globalCtx);
 void func_808E18A8(EnEncount2* this, GlobalContext* globalCtx);
-
-void func_808E16FC(EnEncount2 *this);
+void func_808E16FC(EnEncount2* this);
 void func_808E1A24(EnEncount2* this, Vec3f* vec, s16 arg2);
-void func_808E1B4C(EnEncount2 *this, GlobalContext *globalCtx);
-void func_808E1C9C(EnEncount2 *this, GlobalContext *globalCtx);
+void func_808E1B4C(EnEncount2* this, GlobalContext* globalCtx);
+void func_808E1C9C(EnEncount2* this, GlobalContext* globalCtx);
 
 const ActorInit En_Encount2_InitVars = {
     ACTOR_EN_ENCOUNT2,
@@ -32,57 +29,45 @@ const ActorInit En_Encount2_InitVars = {
     (ActorFunc)EnEncount2_Draw
 };
 
-static ColliderJntSphElementInit D_808E1EF0[1] = {
-//static ColliderJntSphElementInit sJntSphElementsInit[1] = {
+static ColliderJntSphElementInit sJntSphElementsInit[1] = { // D_808E1EF0
     {
         { ELEMTYPE_UNK0, { 0xF7CFFFFF, 0x00, 0x00 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_NONE | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_ON, },
         { 1, { { 0, 0, 0 }, 0 }, 1 },
     },
 };
 
-/*
-//glabel D_808E1F14 collider init
-static ColliderJntSphInit D_808E1F14 = {
-//static ColliderJntSphInit sJntSphInit = {
+static ColliderJntSphInit sJntSphInit = { // D_808E1F14 
     { COLTYPE_HARD, AT_NONE, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_1, COLSHAPE_JNTSPH, },
-    1, D_808E1EF0,
-    //1, sJntSphElementsInit,
+    1, sJntSphElementsInit,
 };
-*/
 
-DamageTable D_808E1F24[] = {
+DamageTable damageTable[] = {
     0xF0, 0xF0, 0x00, 0xF0, 0xE1, 0xE1, 0x00, 0xE1, 0xF0, 0xF0, 0xF0, 0xE1, 0xE1, 0xE1, 0xF0, 0xF0, 
     0xE1, 0xF0, 0xF0, 0xF0, 0x00, 0x00, 0xE1, 0xF0, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 
 };
 
-UNK_TYPE1 unkpadding[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
-
-///*
-//issue: collider init is confused
 void EnEncount2_Init(Actor *thisx, GlobalContext *globalCtx) {
     EnEncount2* this = THIS;
     s8 pad[4];
     BgMeshHeader* bgMeshHeader = NULL;
 
-    BcCheck3_BgActorInit(&this->actor, 0);
+    BcCheck3_BgActorInit(&this->dynaActor, 0);
     BgCheck_RelocateMeshHeader(&D_06002420, &bgMeshHeader);
-    this->unk144 = BgCheck_AddActorMesh(globalCtx, &globalCtx->colCtx.dyna, &this->actor, bgMeshHeader);
-    ActorShape_Init(&this->actor.shape, 0.0f, func_800B3FC0, 25.0f);
-    this->actor.colChkInfo.mass = 0xFF;
-    Collider_InitAndSetJntSph(globalCtx, &this->collider, &this->actor, &D_808E1F14, &this->colElement);
-    //Collider_InitAndSetJntSph(globalCtx, &this->collider, &this->actor, &sJntSphInit, &this->colElement);
+    this->dynaActor.bgId = BgCheck_AddActorMesh(globalCtx, &globalCtx->colCtx.dyna, &this->dynaActor, bgMeshHeader);
+    ActorShape_Init(&this->dynaActor.actor.shape, 0.0f, func_800B3FC0, 25.0f);
+    this->dynaActor.actor.colChkInfo.mass = 0xFF;
+    Collider_InitAndSetJntSph(globalCtx, &this->collider, &this->dynaActor, &sJntSphInit, &this->colElement);
 
-    this->actor.targetMode = 6;
-    this->actor.colChkInfo.health = 1;
-    //this->unk168 = D_808E1F50;
-    this->unk168 = 0.1;
-    this->unk164 = (s16) (this->actor.params & 0x7F);
+    this->dynaActor.actor.targetMode = 6;
+    this->dynaActor.actor.colChkInfo.health = 1;
+    this->scale = 0.1;
+    this->switchFlag = (s16) (this->dynaActor.actor.params & 0x7F);
 
-    if (this->unk164 == 0x7F) {
-        this->unk164 = -1;
+    if (this->switchFlag == 0x7F) {
+        this->switchFlag = -1;
     }
 
-    if ((this->unk164 >= 0) && (Actor_GetSwitchFlag(globalCtx, this->unk164) != 0)) {
+    if ((this->switchFlag >= 0) && (Actor_GetSwitchFlag(globalCtx, this->switchFlag) != 0)) {
         Actor_MarkForDeath(this);
         return;
     }
@@ -93,61 +78,60 @@ void EnEncount2_Init(Actor *thisx, GlobalContext *globalCtx) {
     this->collider.elements->dim.modelSphere.center.y = -4;
     this->collider.elements->dim.modelSphere.center.z = 0;
 
-    this->actor.colChkInfo.damageTable = &D_808E1F24;
+    this->dynaActor.actor.colChkInfo.damageTable = &damageTable;
     func_808E16FC(this);
-} // */
-//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/EnEncount2_Init.asm")
+}
 
 void EnEncount2_Destroy(Actor *thisx, GlobalContext *globalCtx) {
     EnEncount2* this = THIS;
-    BgCheck_RemoveActorMesh(globalCtx, &globalCtx->colCtx.dyna, this->unk144);
+    BgCheck_RemoveActorMesh(globalCtx, &globalCtx->colCtx.dyna, this->dynaActor.bgId);
     Collider_DestroyJntSph(globalCtx, &this->collider);
 }
 
 // init extension func
 void func_808E16FC(EnEncount2 *this) {
-    this->unk162 = 0;
+    this->statePopped = 0;
     this->actionFunc = &func_808E1714;
 }
 
-//actionfunc
+//actionfunc: idle floating
 void func_808E1714(EnEncount2 *this, GlobalContext *globalCtx) {
-    // is this rodata? its not in the asm file as late rodata... extern for now
-    this->unk16C += D_808E1F54;
-    this->actor.velocity.y = Math_SinS((s16) (s32) this->unk16C);
-    Math_SmoothScaleMaxF(&this->unk168, 0.1f, 0.3f, 0.01f);
-    if (((this->collider.base.acFlags & 2) != 0) && (this->actor.colChkInfo.damageEffect == 0xE)) {
-        this->actor.colChkInfo.health = 0;
-        this->unk162 = 1;
-        this->actionFunc = func_808E17C4;
+    this->oscillationAngle += 1500.0f;
+    this->dynaActor.actor.velocity.y = Math_SinS(this->oscillationAngle);
+    Math_SmoothScaleMaxF(&this->scale, 0.1f, 0.3f, 0.01f);
+    if (((this->collider.base.acFlags & AC_HIT) != 0) && (this->dynaActor.actor.colChkInfo.damageEffect == 0xE)) {
+        this->dynaActor.actor.colChkInfo.health = 0;
+        this->statePopped = 1;
+        this->actionFunc = func_808E17C4; // pop
     }
 }
 
-//actionfunc
+//actionfunc: pop
 void func_808E17C4(EnEncount2 *this, GlobalContext *globalCtx) {
     s32 i;
     Vec3f curPos;
 
-    Math_Vec3f_Copy(&curPos, &this->actor.world.pos);
+    Math_Vec3f_Copy(&curPos, &this->dynaActor.actor.world.pos);
     curPos.y += 60.0f;
-    Actor_Spawn(&globalCtx->actorCtx, globalCtx, 0xA2, curPos.x, curPos.y, curPos.z, 0xFF, 0xFF, 0xC8, 1);
+    Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_CLEAR_TAG, curPos.x, curPos.y, curPos.z,
+       0xFF, 0xFF, 0xC8, 0x0001);
 
     for(i = 0; i != 100; i++){
-        func_808E1A24(this, &curPos, 10);
+        func_808E1A24(this, &curPos, 10); // init particle
     }
 
     Audio_PlayActorSound2(this, 0x2949);
-    this->unk160 = 0x1E; // timer?
-    this->actionFunc = func_808E18A8;
+    this->deathTimer = 30;
+    this->actionFunc = func_808E18A8; // post-pop
 }
 
-//actionfunc
+//actionfunc: after pop, waiting for timer then death
 void func_808E18A8(EnEncount2 *this, GlobalContext *globalCtx) {
-    if (this->unk160 == 0) {
-        if (this->unk164 >= 0) {
-            Actor_SetSwitchFlag(globalCtx, this->unk164);
+    if (this->deathTimer == 0) {
+        if (this->switchFlag >= 0) {
+            Actor_SetSwitchFlag(globalCtx, this->switchFlag);
         }
-        Actor_MarkForDeath(&this->actor);
+        Actor_MarkForDeath(&this->dynaActor.actor);
     }
 }
 
@@ -155,19 +139,19 @@ void EnEncount2_Update(Actor* thisx, GlobalContext *globalCtx) {
     EnEncount2* this = THIS;
     s8 pad[4];
 
-    if (this->unk160 != 0) {
-        this->unk160--;
+    if (this->deathTimer != 0) {
+        this->deathTimer--;
     }
 
-    this->actor.shape.rot.y = this->actor.world.rot.y;
-    Actor_SetHeight(&this->actor, 30.0f);
-    Actor_SetScale(&this->actor, this->unk168);
+    this->dynaActor.actor.shape.rot.y = this->dynaActor.actor.world.rot.y;
+    Actor_SetHeight(&this->dynaActor.actor, 30.0f);
+    Actor_SetScale(&this->dynaActor.actor, this->scale);
     this->actionFunc(this, globalCtx);
-    Actor_SetVelocityAndMoveYRotationAndGravity(&this->actor);
-    func_808E1B4C(this, globalCtx);
+    Actor_SetVelocityAndMoveYRotationAndGravity(&this->dynaActor.actor);
+    func_808E1B4C(this, globalCtx); // update all particles
 
-    if (this->unk162 == 0) {
-        Collider_UpdateSpheresElement(&this->collider, 0, &this->actor);
+    if (! this->statePopped) {
+        Collider_UpdateSpheresElement(&this->collider, 0, &this->dynaActor.actor);
         CollisionCheck_SetAC(globalCtx, &globalCtx->colCheckCtx, &this->collider);
         CollisionCheck_SetOC(globalCtx, &globalCtx->colCheckCtx, &this->collider);
     }
@@ -175,25 +159,24 @@ void EnEncount2_Update(Actor* thisx, GlobalContext *globalCtx) {
 
 void EnEncount2_Draw(Actor *thisx, GlobalContext *globalCtx) {
     EnEncount2* this = THIS;
-    if (this->unk162 != 1) {
+    if (this->statePopped != 1) { // is NOT popped (0 only other option)
         func_800BDFC0(globalCtx, &D_06000A00);
         func_800BDFC0(globalCtx, &D_06000D78);
     }
-    func_808E1C9C(&this->actor, globalCtx);
+    func_808E1C9C(this, globalCtx);
 }
 
-//arg2 is always 10??
 // init all particles
-void func_808E1A24(EnEncount2 *this, Vec3f *vec, s16 arg2) {
+void func_808E1A24(EnEncount2 *this, Vec3f *vec, s16 fadeDelay) {
     s16 i;
-    unkEnE2Struct *sPtr = &this->unkStructs;
+    EnEncount2Particle *sPtr = &this->particles;
 
-    for (i = 0; i < 200; ++i) { //loop_1:
-        if (sPtr->unk0 == 0) {
-            sPtr->unk0 = 1;
+    for (i = 0; i < 200; ++i) {
+        if ( ! sPtr->enabled) {
+            sPtr->enabled = 1;
             sPtr->unk4 = *vec;
-            sPtr->unk16 = arg2;
-            sPtr->unk14 = 0xFF;
+            sPtr->alphaFadeDelay = fadeDelay;
+            sPtr->alpha = 0xFF;
 
             sPtr->unk24.x = (Rand_ZeroOne() - 0.5f) * 10.0f;
             sPtr->unk24.y = (Rand_ZeroOne() - 0.5f) * 10.0f;
@@ -203,51 +186,50 @@ void func_808E1A24(EnEncount2 *this, Vec3f *vec, s16 arg2) {
             sPtr->unk18.y = Rand_ZeroOne() - 0.5f;
             sPtr->unk18.z = Rand_ZeroOne() - 0.5f;
 
-            sPtr->unk30 = (Rand_ZeroFloat(1.0f) * 0.5f) + 2.0f;
+            sPtr->scale = (Rand_ZeroFloat(1.0f) * 0.5f) + 2.0f;
             return;
         }
         sPtr++;
     }
 }
 
-// update extension
 // updates all effect structs
 void func_808E1B4C(EnEncount2 *this, GlobalContext *globalCtx) {
     s32 i;
-    unkEnE2Struct *sPtr = &this->unkStructs;
+    EnEncount2Particle *sPtr = &this->particles;
 
     for(i = 0 ; i < 200; i += 2) {
-        if (sPtr->unk0 != 0) {
+        if (sPtr->enabled) {
             sPtr->unk4.x += sPtr->unk18.x;
             sPtr->unk4.y += sPtr->unk18.y;
             sPtr->unk4.z += sPtr->unk18.z;
             sPtr->unk18.x += sPtr->unk24.x;
             sPtr->unk18.y += sPtr->unk24.y;
             sPtr->unk18.z += sPtr->unk24.z;
-            if (sPtr->unk16 != 0) {
-                sPtr->unk16--;
+            if (sPtr->alphaFadeDelay != 0) {
+                sPtr->alphaFadeDelay--;
             } else {
-                sPtr->unk14 -= 10;
-                if (sPtr->unk14 < 10) {
-                    sPtr->unk0 = 0;
+                sPtr->alpha -= 10;
+                if (sPtr->alpha < 10) {
+                    sPtr->enabled = 0;
                 }
             }
         }
         sPtr++;
 
-        if (sPtr->unk0 != 0) {
+        if (sPtr->enabled) {
             sPtr->unk4.x += sPtr->unk18.x;
             sPtr->unk4.y += sPtr->unk18.y;
             sPtr->unk4.z += sPtr->unk18.z;
             sPtr->unk18.x += sPtr->unk24.x;
             sPtr->unk18.y += sPtr->unk24.y;
             sPtr->unk18.z += sPtr->unk24.z;
-            if (sPtr->unk16 != 0) {
-                sPtr->unk16--;
+            if (sPtr->alphaFadeDelay != 0) {
+                sPtr->alphaFadeDelay--;
             } else {
-                sPtr->unk14 -= 10;
-                if (sPtr->unk14 < 10) {
-                    sPtr->unk0 = 0;
+                sPtr->alpha -= 10;
+                if (sPtr->alpha < 10) {
+                    sPtr->enabled = 0;
                 }
             }
         }
@@ -255,10 +237,12 @@ void func_808E1B4C(EnEncount2 *this, GlobalContext *globalCtx) {
     }
 }
 
-//xlu draw func extension
+// draw particles
+# if COMPLETELY_BROKEN
+// not even close, everything is off
 void func_808E1C9C(EnEncount2* this, GlobalContext* globalCtx) {
     //GraphicsContext *tempGfxC;
-    unkEnE2Struct *sPtr;
+    EnEncount2Particle *sPtr;
     //MtxF *temp_s6;
     f32 tempFramesFloat;
     //u32 tempFrames;
@@ -269,24 +253,23 @@ void func_808E1C9C(EnEncount2* this, GlobalContext* globalCtx) {
 
     OPEN_DISPS(globalCtx->state.gfxCtx);
 
-
     //tempGfxC = globalCtx->state.gfxCtx;
     //func_8012C28C(tempGfxC);
     func_8012C28C(globalCtx->state.gfxCtx);
     func_8012C2DC(globalCtx->state.gfxCtx);
 
-    tmpD_808E1F58 = D_808E1F58; //unk value
-    sPtr = this->unkStructs;
+
+    tmpD_808E1F58 = 0.017453292f; // D_808E1F58
+    sPtr = this->particles;
 
     for (i = 0; i < 200; ++i) { //loop_1:
-        if (sPtr->unk0 != 0) {
+        if (sPtr->enabled) {
             //temp_s6 = &globalCtx->mf_187FC;
-            //temp_f12 = sPtr->unk30;
+            //temp_f12 = sPtr->scale;
 
             // bunch wrong right here
             SysMatrix_InsertTranslation(sPtr->unk4.x, sPtr->unk4.y, sPtr->unk4.z, 0);
-            Matrix_Scale(sPtr->unk30, sPtr->unk30, sPtr->unk30, 1);
-
+            Matrix_Scale(sPtr->scale, sPtr->scale, sPtr->scale, 1);
     
             globalCtx->state.gfxCtx->polyXlu.p = Gfx_CallSetupDL(globalCtx->state.gfxCtx->polyXlu.p, 0x14);
 
@@ -317,15 +300,15 @@ void func_808E1C9C(EnEncount2* this, GlobalContext* globalCtx) {
             //temp_v0_5 = tempGfxC->polyXlu.p;
             //tempGfxC->polyXlu.p = temp_v0_5 + 8;
             //temp_v0_5->words.w0 = 0xFB000000;
-            //temp_v0_5->words.w1 = (sPtr->unk14 & 0xFF) | 0xFAB4FF00;
-            gDPSetEnvColor(POLY_XLU_DISP++, 0xFA, 0xB4, 0xFF, (sPtr->unk14 & 0xFF));
+            //temp_v0_5->words.w1 = (sPtr->alpha & 0xFF) | 0xFAB4FF00;
+            gDPSetEnvColor(POLY_XLU_DISP++, 0xFA, 0xB4, 0xFF, (sPtr->alpha & 0xFF));
 
             SysMatrix_InsertMatrix((z_Matrix *) &globalCtx->mf_187FC, 1);
             //tempFrames = globalCtx->state.frames;
             //tempFramesFloat = (f32) tempFrames;
             tempFramesFloat = globalCtx->state.frames;
             if (globalCtx->state.frames < 0) {
-                tempFramesFloat += 4294967296.0f;
+                tempFramesFloat += 4294967296.0f; // wtf
             }
             SysMatrix_InsertZRotation_f(tempFramesFloat * 20.0f * tmpD_808E1F58, 1);
             //SysMatrix_InsertZRotation_f(tempFramesFloat * 20.0f * D_808E1F58, 1);
@@ -352,4 +335,6 @@ void func_808E1C9C(EnEncount2* this, GlobalContext* globalCtx) {
 
     CLOSE_DISPS(globalCtx->state.gfxCtx);
 }
-//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1C9C.asm")
+#else
+#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Encount2_0x808E1560/func_808E1C9C.asm")
+#endif
--- src/overlays/actors/ovl_En_Encount2/z_en_encount2.h
+++ src/overlays/actors/ovl_En_Encount2/z_en_encount2.h
@@ -5,58 +5,33 @@
 
 typedef void (*EnEncount2ActionFunc)(struct EnEncount2*, GlobalContext*);
 
-typedef struct unkEnE2Struct {
-   /* 0x00 */ u8 unk0;
-   /* 0x01 */ UNK_TYPE1 unk1[3];
-  
-   
-   /* 0x04 */ Vec3f unk4; //sw, but reading from floats? might be vec3f
-   ///* 0x08 */ f32 unk8;
-   ///* 0x0C */ f32 unkC;
+typedef struct EnEncount2Particle{
+   /* 0x00 */ u8 enabled;
+   /* 0x01 */ u8 pad1[3];
+   /* 0x04 */ Vec3f unk4; // pos?
+   /* 0x10 */ u8 pad10[0x4];
+   /* 0x14 */ s16 alpha;
+   /* 0x16 */ s16 alphaFadeDelay; // frame count before alpha fade starts
+   /* 0x18 */ Vec3f unk18; // vel?
+   /* 0x24 */ Vec3f unk24; // accel?
+   /* 0x30 */ f32 scale;
 
-   /* 0x10 */ s8  unk10[0x4];
-   /* 0x14 */ s16 unk14;
-   /* 0x16 */ s16 unk16;
-
-   /* 0x18 */ Vec3f unk18; //vec3f?
-   ///* 0x1C */ f32 unk1C;
-   ///* 0x20 */ f32 unk20;
-
-   /* 0x24 */ Vec3f unk24; //vec3f?
-   ///* 0x28 */ f32 unk28;
-   ///* 0x2C */ f32 unk2C;
-
-   /* 0x30 */ f32 unk30;
-
-} unkEnE2Struct; // size 0x34
+} EnEncount2Particle; // size = 0x34
 
 struct EnEncount2;
 
 typedef struct EnEncount2 {
-    /* 0x0000 */ Actor actor;
-    ///* 0x0144 */ char unk_0144[0x292C];
-    /* 0x0144 */ s32 unk144; // index for destroy jnt sph?
-    /* 0x0148 */ UNK_TYPE1 unk_0148[0x14];
-    /* 0x015C */ EnEncount2ActionFunc actionFunc; // index for destroy jnt sph?
-    /* 0x0160 */ s16 unk160;
-    /* 0x0162 */ s16 unk162; //func_808E16FC
-    /* 0x0164 */ s16 unk164; //init from params, ""flag"
-    /* 0x0166 */ s16 unk166; //pad
-    /* 0x0168 */ f32 unk168; //gets used for scale in update
-    /* 0x016C */ f32 unk16C;
+    /* 0x0000 */ DynaPolyActor dynaActor;
+    /* 0x015C */ EnEncount2ActionFunc actionFunc;
+    /* 0x0160 */ s16 deathTimer;
+    /* 0x0162 */ s16 statePopped;
+    /* 0x0164 */ s16 switchFlag; // auto pad to 168
+    /* 0x0168 */ f32 scale;
+    /* 0x016C */ f32 oscillationAngle;
     /* 0x0170 */ ColliderJntSph collider;
     /* 0x0190 */ ColliderJntSphElement colElement;
-                // wild guess, there are only like 2 structs defined anywhere by anyone at this size
-    ///* 0x01D0 */ EffectSsBlastInitParams unkStructs[3]; //size unk
-    /* 0x01D0 */ unkEnE2Struct unkStructs[200]; //size unk
-
-    // where the heck is the rest of the data? is element that big?
-      
-    ///* 0x0148 */ UNK_TYPE1 unk_0148[0x28];
+    /* 0x01D0 */ EnEncount2Particle particles[200];
 } EnEncount2; // size = 0x2A70
-
-extern ColliderJntSphInit D_808E1F14;
-
 
 extern const ActorInit En_Encount2_InitVars;
 
@@ -64,9 +39,5 @@ extern BgMeshHeader D_06002420;
 
 extern s32 D_06000A00;
 extern s32 D_06000D78;
-
-extern f32 D_808E1F54; // not found in rodata
-
-extern f32 D_808E1F58; // not found in rodata
 
 #endif // Z_EN_ENCOUNT2_H
