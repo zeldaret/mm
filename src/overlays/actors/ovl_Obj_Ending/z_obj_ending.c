4c4c6020e6f4fd6624d9f49288d15db3e6718841
Derek Hensley <hensley.derek58@gmail.com>
2021-04-17 00:00:40 -0700

    Removed inital code comments

--- src/overlays/actors/ovl_Obj_Ending/z_obj_ending.c
+++ src/overlays/actors/ovl_Obj_Ending/z_obj_ending.c
@@ -30,32 +30,7 @@ static InitChainEntry sInitChain[] = {
 */
 
 extern unkStruct D_80C25CE0[];
-/*
-static unkStruct D_80C25CE0[] = {
-    {0x06003440, 0x060031A0, 0x00000000},
-    {0x00000000, 0x060003D0, 0x06001FF8}
-};
-*/
 
-//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ending_0x80C25BC0/ObjEnding_Init.asm")
-/* 
-void ObjEnding_Init(ObjEnding *this, GlobalContext *globalCtx) {
-    ObjEnding *temp_a0;
-    char *temp_t9;
-    void *temp_a0_2;
-
-    temp_a0 = this;
-    this = this;
-    Actor_ProcessInitChain((Actor *) temp_a0, &D_80C25CF8);
-    temp_t9 = (this->actor.params * 0xC) + &D_80C25CE0;
-    this->unk_144 = temp_t9;
-    temp_a0_2 = temp_t9->unk8;
-    if (temp_a0_2 != 0) {
-        this = this;
-        this->unk148 = Lib_SegmentedToVirtual(temp_a0_2);
-    }
-}
-*/
 void ObjEnding_Init(Actor* thisx, GlobalContext *globalCtx) {
     ObjEnding* this = THIS;
     UNK_PTR *tempPtr;
@@ -73,36 +48,6 @@ void ObjEnding_Init(Actor* thisx, GlobalContext *globalCtx) {
 void ObjEnding_Update(Actor *thisx, GlobalContext *globalCtx) {
 }
 
-//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ending_0x80C25BC0/ObjEnding_Draw.asm")
-/*
-void ObjEnding_Draw(ObjEnding *this, GlobalContext *globalCtx) {
-    AnimatedTexture *temp_a1;
-    AnimatedTexture *temp_a3;
-    Gfx *temp_a1_3;
-    char *temp_v0;
-    s32 temp_a1_2;
-    char *phi_v0;
-
-    temp_a3 = this->unk148;
-    temp_a1 = temp_a3;
-    if (temp_a3 != 0) {
-        this = this;
-        SceneProc_DrawAllSceneAnimatedTextures(globalCtx, temp_a1);
-    }
-    temp_v0 = this->unk_144;
-    temp_a1_2 = *temp_v0;
-    phi_v0 = temp_v0;
-    if (temp_a1_2 != 0) {
-        this = this;
-        func_800BDFC0(globalCtx, temp_a1_2);
-        phi_v0 = this->unk_144;
-    }
-    temp_a1_3 = phi_v0->unk4;
-    if (temp_a1_3 != 0) {
-        func_800BE03C(globalCtx, temp_a1_3);
-    }
-}
-*/
 void ObjEnding_Draw(Actor *thisx, GlobalContext *globalCtx) {
     ObjEnding *this = THIS;
     Gfx *dl;
