695beefd824e6d9996046af582b290b2ebcfb014
kyleburnette <kyle@kyleburnette.com>
2021-05-01 13:26:36 -0700

    removed unnecessary pad

--- src/overlays/actors/ovl_Obj_Visiblock/z_obj_visiblock.h
+++ src/overlays/actors/ovl_Obj_Visiblock/z_obj_visiblock.h
@@ -7,7 +7,6 @@ struct ObjVisiblock;
 
 typedef struct ObjVisiblock {
     /* 0x000 */ DynaPolyActor dyna;
-    /* 0x15C */ UNK_TYPE1 unk15C[0x4];
 } ObjVisiblock; // size = 0x160
 
 extern const ActorInit Obj_Visiblock_InitVars;
