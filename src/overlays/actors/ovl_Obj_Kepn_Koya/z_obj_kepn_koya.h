24f04d65c98c73ca45c4f49e99335005f75502dd
kyleburnette <kyle@kyleburnette.com>
2021-05-01 13:23:43 -0700

    actually commited change to header

--- src/overlays/actors/ovl_Obj_Kepn_Koya/z_obj_kepn_koya.h
+++ src/overlays/actors/ovl_Obj_Kepn_Koya/z_obj_kepn_koya.h
@@ -7,7 +7,6 @@ struct ObjKepnKoya;
 
 typedef struct ObjKepnKoya {
     /* 0x000 */ DynaPolyActor dyna;
-    /* 0x15C */ UNK_TYPE1 unk15C;
 } ObjKepnKoya; // size = 0x160
 
 extern const ActorInit Obj_Kepn_Koya_InitVars;
