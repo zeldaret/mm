0bb13c9d65b305874bba0758f5dda18ee2c7406d
Zach Banks <zjbanks@gmail.com>
2021-05-02 20:19:47 -0400

    ovl_Dm_Ravine: respond to PR comments

--- src/overlays/actors/ovl_Dm_Ravine/z_dm_ravine.c
+++ src/overlays/actors/ovl_Dm_Ravine/z_dm_ravine.c
@@ -1,7 +1,7 @@
 /*
  * File: z_dm_ravine.c
  * Overlay: ovl_Dm_Ravine
- * Description: Link Riding Through Lost Woods Cutscene - Tree Trunk
+ * Description: Lost Woods Cutscene - Tree Trunk
  */
 
 #include "z_dm_ravine.h"
@@ -16,31 +16,32 @@ void DmRavine_Action(Actor* thisx, GlobalContext* globalCtx);
 void DmRavine_Update(Actor* thisx, GlobalContext* globalCtx);
 void DmRavine_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-const ActorInit Dm_Ravine_InitVars = { ACTOR_DM_RAVINE,
-                                       ACTORCAT_ITEMACTION,
-                                       FLAGS,
-                                       OBJECT_KEIKOKU_DEMO,
-                                       sizeof(DmRavine),
-                                       (ActorFunc)DmRavine_Init,
-                                       (ActorFunc)DmRavine_Destroy,
-                                       (ActorFunc)DmRavine_Update,
-                                       (ActorFunc)DmRavine_Draw };
+const ActorInit Dm_Ravine_InitVars = {
+    ACTOR_DM_RAVINE,
+    ACTORCAT_ITEMACTION,
+    FLAGS,
+    OBJECT_KEIKOKU_DEMO,
+    sizeof(DmRavine),
+    (ActorFunc)DmRavine_Init,
+    (ActorFunc)DmRavine_Destroy,
+    (ActorFunc)DmRavine_Update,
+    (ActorFunc)DmRavine_Draw,
+};
 
 void DmRavine_Init(Actor* thisx, GlobalContext* globalCtx) {
-    u8 flag;
-
-    flag = gSaveContext.perm.weekEventReg[0];
+    DmRavine* this = THIS;
+    u8 flag = gSaveContext.perm.weekEventReg[0];
     if (((flag & 0x10) | cREG(0)) != 0) {
-        Actor_MarkForDeath(thisx);
+        Actor_MarkForDeath(&this->actor);
         return;
     }
 
-    THIS->loaded = false;
+    this->loaded = false;
     globalCtx->roomContext.unk7A[0] = 1;
     globalCtx->roomContext.unk7A[1] = 0;
-    THIS->state = 0;
-    Actor_SetScale(thisx, 1.0f);
-    THIS->actionFunc = &DmRavine_Action;
+    this->state = 0;
+    Actor_SetScale(&this->actor, 1.0f);
+    this->actionFunc = &DmRavine_Action;
 }
 
 void DmRavine_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -52,25 +53,26 @@ void DmRavine_Action(Actor* thisx, GlobalContext* globalCtx) {
 }
 
 void DmRavine_Update(Actor* thisx, GlobalContext* globalCtx) {
+    DmRavine* this = THIS;
     RoomContext* roomCtx;
 
-    switch (THIS->state) {
+    switch (this->state) {
         case 0:
             return;
         case 1:
-            THIS->loaded = true;
+            this->loaded = true;
             globalCtx->roomContext.unk7A[1]++;
             if (globalCtx->roomContext.unk7A[1] > 254) {
                 globalCtx->roomContext.unk7A[1] = 254;
                 if (globalCtx->csCtx.frames > 700) {
                     globalCtx->roomContext.unk7A[1] = 255;
                     globalCtx->roomContext.unk7A[0] = 0;
-                    THIS->state++;
+                    this->state++;
                 }
             }
             break;
         case 2:
-            Actor_MarkForDeath(thisx);
+            Actor_MarkForDeath(&this->actor);
             break;
     }
 }
--- src/overlays/actors/ovl_Dm_Ravine/z_dm_ravine.h
+++ src/overlays/actors/ovl_Dm_Ravine/z_dm_ravine.h
@@ -5,12 +5,14 @@
 
 struct DmRavine;
 
+typedef void (*DmRavineActionFunc)(Actor*, GlobalContext*);
+
 typedef struct DmRavine {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ void (*actionFunc)(Actor*, GlobalContext*); // unused
+    /* 0x144 */ DmRavineActionFunc actionFunc;
     /* 0x148 */ u8 state;
     /* 0x149 */ u8 loaded;
-    /* 0x14a */ u8 pad_14a[2];
+    /* 0x14A */ u8 pad_14A[2];
 } DmRavine; // size = 0x14C
 
 extern const ActorInit Dm_Ravine_InitVars;
