58773c5f31ad1ae1ea5660563f350ad0d70b7065
angie <angheloalf95@gmail.com>
2021-05-14 13:03:17 -0400

    atans ok

--- src/code/code_0x8017FEB0.c
+++ src/code/code_0x8017FEB0.c
@@ -2,23 +2,62 @@
 #include <global.h>
 
 s16 atans_first_8th(f32 opposite, f32 adjacent) {
-    return atan_first_8th_array[(s32) ((opposite / adjacent) * 1024.0f)];
+    return atan_first_8th_array[(s32)((opposite / adjacent) * 0x400)];
 }
-//#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8017FEB0/atans_first_8th.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8017FEB0/atans.asm")
+s16 atans(f32 opposite, f32 adjacent) {
+    s32 phi_v1;
+
+    if (opposite == 0.0f) {
+        if (adjacent >= 0.0f) {
+            phi_v1 = 0;
+        } else {
+            phi_v1 = 0x8000;
+        }
+    } else if (adjacent == 0.0f) {
+        if (opposite >= 0.0f) {
+            phi_v1 = 0x4000;
+        } else {
+            phi_v1 = 0xC000;
+        }
+    } else if (opposite >= 0.0f) {
+        if (adjacent >= 0.0f) {
+            if (opposite <= adjacent) {
+                phi_v1 = atans_first_8th(opposite, adjacent);
+            } else {
+                phi_v1 = 0x4000 - atans_first_8th(adjacent, opposite);
+            }
+        } else {
+            if (-adjacent < opposite) {
+                phi_v1 = atans_first_8th(-adjacent, opposite) + 0x4000;
+            } else {
+                phi_v1 = 0x8000 - atans_first_8th(opposite, -adjacent);
+            }
+        }
+    } else if (adjacent < 0.0f) {
+        if (-opposite <= -adjacent) {
+            phi_v1 = atans_first_8th(-opposite, -adjacent) + 0x8000;
+        } else {
+            phi_v1 = 0xC000 - atans_first_8th(-adjacent, -opposite);
+        }
+    } else {
+        if (adjacent < -opposite) {
+            phi_v1 = atans_first_8th(adjacent, -opposite) + 0xC000;
+        } else {
+            phi_v1 = -atans_first_8th(-opposite, adjacent);
+        }
+    }
+    return phi_v1;
+}
 
 f32 atan(f32 opposite, f32 adjacent) {
     return atans(opposite, adjacent) * D_801E01E0;
 }
-//#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8017FEB0/atan.asm")
 
 s16 Math_FAtan2F(f32 adjacent, f32 opposite) {
     return atans(opposite, adjacent);
 }
-//#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8017FEB0/Math_FAtan2F.asm")
 
 f32 atan_flip(f32 adjacent, f32 opposite) {
     return atan(opposite, adjacent);
 }
-//#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8017FEB0/atan_flip.asm")
