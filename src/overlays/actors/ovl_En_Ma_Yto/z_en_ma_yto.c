d04de603af278e9d48d915701d865b68459ce1ef
angie <angheloalf95@gmail.com>
2021-05-17 17:16:41 -0400

    EnMaYto_Update

--- src/overlays/actors/ovl_En_Ma_Yto/z_en_ma_yto.c
+++ src/overlays/actors/ovl_En_Ma_Yto/z_en_ma_yto.c
@@ -1,3 +1,9 @@
+/*
+ * File: z_en_ma_yto.c
+ * Overlay: ovl_En_Ma_Yto
+ * Description: Cremia
+ */
+
 #include "z_en_ma_yto.h"
 
 #define FLAGS 0x02100009
@@ -23,6 +29,45 @@ const ActorInit En_Ma_Yto_InitVars = {
 };
 */
 
+
+/*
+void EnMaYto_Init(Actor *thisx, GlobalContext *globalCtx) {
+    ColliderCylinder *sp30;
+    ColliderCylinder *temp_a1;
+
+    thisx->targetMode = 0;
+    thisx->unk200 = 0;
+    thisx->unk310 = (u16)0;
+    thisx->unk320 = (u16)0;
+    thisx->unk31A = (u16)0;
+    if ((((s32) gSaveContext.perm.day % 5) == 1) || ((gSaveContext.unkF0E & 1) != 0)) {
+        func_80B90EC8(thisx, 0, 1);
+    } else {
+        func_80B90EC8(thisx, 5, 2);
+    }
+    thisx->unk31E = (u16)0;
+    thisx->unk31C = (u16)0x64;
+    thisx->unk204 = (s32) ((s32) (thisx->params & 0xF000) >> 0xC);
+    if (func_80B8E6E0(thisx, globalCtx) == 0) {
+        Actor_MarkForDeath(thisx);
+        return;
+    }
+    ActorShape_Init(&thisx->shape, 0.0f, (void (*)(struct Actor *actor, struct Lights *mapper, struct GlobalContext *ctxt)) func_800B3FC0, 18.0f);
+    SkelAnime_InitSV(globalCtx, (SkelAnime *) (thisx + 0x144), (void *)0x6015C28, NULL, thisx + 0x208, thisx + 0x28C, 0x16);
+    func_80B8E84C(thisx, globalCtx);
+    temp_a1 = thisx + 0x18C;
+    sp30 = temp_a1;
+    Collider_InitCylinder(globalCtx, temp_a1);
+    Collider_SetCylinder(globalCtx, temp_a1, thisx, &D_80B91410);
+    CollisionCheck_SetInfo2(&thisx->colChkInfo, DamageTable_Get(0x16), &D_80B9143C);
+    func_800B78B8(globalCtx, thisx, 0.0f, 0.0f, 0.0f, 4);
+    if (func_80B8EABC(thisx, globalCtx) == 1) {
+        func_80B8EBDC(thisx);
+        return;
+    }
+    func_80B8E938(thisx, globalCtx);
+}
+*/
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/EnMaYto_Init.asm")
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B8E6E0.asm")
@@ -35,7 +80,11 @@ const ActorInit En_Ma_Yto_InitVars = {
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B8EABC.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/EnMaYto_Destroy.asm")
+void EnMaYto_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    EnMaYto* this = THIS;
+
+    Collider_DestroyCylinder(globalCtx, &this->collider);
+}
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B8EBDC.asm")
 
@@ -117,8 +166,10 @@ const ActorInit En_Ma_Yto_InitVars = {
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B90C08.asm")
 
+void func_80B90C78(EnMaYto* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B90C78.asm")
 
+void func_80B90D98(EnMaYto* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B90D98.asm")
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B90DF0.asm")
@@ -137,7 +188,14 @@ const ActorInit En_Ma_Yto_InitVars = {
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B9109C.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/EnMaYto_Update.asm")
+void EnMaYto_Update(Actor *thisx, GlobalContext *globalCtx) {
+    EnMaYto* this = THIS;
+
+    this->actionFunc(this, globalCtx);
+    func_80B90D98(this, globalCtx);
+    func_80B90C78(this, globalCtx);
+}
+//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/EnMaYto_Update.asm")
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Ma_Yto_0x80B8E520/func_80B91154.asm")
 
--- src/overlays/actors/ovl_En_Ma_Yto/z_en_ma_yto.h
+++ src/overlays/actors/ovl_En_Ma_Yto/z_en_ma_yto.h
@@ -5,9 +5,14 @@
 
 struct EnMaYto;
 
+typedef void (*EnMaYtoActionFunc)(struct EnMaYto*, GlobalContext*);
+
 typedef struct EnMaYto {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x1E0];
+    /* 0x144 */ char unk_144[0x44];
+    /* 0x188 */ EnMaYtoActionFunc actionFunc;
+    /* 0x18C */ ColliderCylinder collider;
+    /* 0x1D8 */ char unk_1D8[0x14C];
 } EnMaYto; // size = 0x324
 
 extern const ActorInit En_Ma_Yto_InitVars;
