d7932e163030b48ee92a9504899fdcc9bc7afdf3
Parker Burnett <burnettparker@gmail.com>
2021-05-18 21:41:38 -0400

    Moon's Tear OK

--- src/overlays/actors/ovl_Obj_Moon_Stone/z_obj_moon_stone.c
+++ src/overlays/actors/ovl_Obj_Moon_Stone/z_obj_moon_stone.c
@@ -1,3 +1,9 @@
+/*
+ * File z_obj_moon_stone.c
+ * Overlay: ovl_obj_moon_stone
+ * Description: Moon's Tear
+ */
+
 #include "z_obj_moon_stone.h"
 
 #define FLAGS 0x00100010
@@ -18,7 +24,6 @@ void func_80C06768(ObjMoonStone* this, GlobalContext* globalCtx);
 void func_80C0685C(ObjMoonStone* this);
 void func_80C06870(ObjMoonStone* this, GlobalContext* globalCtx);
 
-/*
 const ActorInit Obj_Moon_Stone_InitVars = {
     ACTOR_OBJ_MOON_STONE,
     ACTORCAT_PROP,
@@ -28,16 +33,13 @@ const ActorInit Obj_Moon_Stone_InitVars = {
     (ActorFunc)ObjMoonStone_Init,
     (ActorFunc)ObjMoonStone_Destroy,
     (ActorFunc)ObjMoonStone_Update,
-    (ActorFunc)ObjMoonStone_Draw
+    (ActorFunc)ObjMoonStone_Draw,
 };
-*/
 
 extern AnimatedMaterial D_06001C60;
 extern Gfx D_06000D78[];
 extern Gfx D_06000C80[];
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/ObjMoonStone_Init.asm")
 void ObjMoonStone_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjMoonStone* this = THIS;
 
@@ -59,31 +61,25 @@ void ObjMoonStone_Init(Actor* thisx, GlobalContext* globalCtx) {
             this->actor.flags &= ~1;
             func_80C0673C(this);
         } else {
-            Actor_MarkForDeath(this);
+            Actor_MarkForDeath(&this->actor);
         }
     }
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/ObjMoonStone_Destroy.asm")
 void ObjMoonStone_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
 
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C0662C.asm")
 void func_80C0662C(ObjMoonStone* this) {
     this->actionFunc = func_80C06640;
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C06640.asm")
 void func_80C06640(ObjMoonStone* this, GlobalContext* globalCtx) {
-    Actor* new_var; // could this bee the moon?
+    ActorPlayer* player = PLAYER;
     s16 sp1A;
     s32 phi_v0;
 
-    new_var = globalCtx->actorCtx.actorList[2].first;
     sp1A = this->actor.yawTowardsPlayer - 0x8000;
-    sp1A = sp1A - new_var->shape.rot.y;
+    sp1A -= player->base.shape.rot.y;
     if (func_800B84D0(this, globalCtx)) {
         this->actor.colChkInfo.health = 1;
         func_801518B0(globalCtx, 0x5E3U, this);
@@ -97,14 +93,10 @@ void func_80C06640(ObjMoonStone* this, GlobalContext* globalCtx) {
     }
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C066F8.asm")
 void func_80C066F8(ObjMoonStone* this) {
     this->actionFunc = func_80C0670C;
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C0670C.asm")
 void func_80C0670C(ObjMoonStone* this, GlobalContext* globalCtx) {
     if (func_800B867C(this, globalCtx) != 0) {
         this->actor.colChkInfo.health = 0;
@@ -112,8 +104,6 @@ void func_80C0670C(ObjMoonStone* this, GlobalContext* globalCtx) {
     }
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C0673C.asm")
 void func_80C0673C(ObjMoonStone* this) {
     if ((gSaveContext.perm.weekEventReg[74] & 0x80) == 0) {
         this->actor.draw = NULL;
@@ -121,8 +111,6 @@ void func_80C0673C(ObjMoonStone* this) {
     this->actionFunc = func_80C06768;
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C06768.asm")
 void func_80C06768(ObjMoonStone* this, GlobalContext* globalCtx) {
     if ((gSaveContext.perm.weekEventReg[74] & 0x80)) {
         if (this->actor.draw == 0) {
@@ -144,33 +132,28 @@ void func_80C06768(ObjMoonStone* this, GlobalContext* globalCtx) {
     }
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C0685C.asm")
 void func_80C0685C(ObjMoonStone* this) {
     this->actionFunc = func_80C06870;
 }
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/func_80C06870.asm")
 void func_80C06870(ObjMoonStone* this, GlobalContext* globalCtx) {
     if (func_80152498(&globalCtx->msgCtx) == 6) {
         if (func_80147624(globalCtx)) {
             gSaveContext.perm.weekEventReg[74] |= 0x40;
-            Actor_MarkForDeath(this);
+            Actor_MarkForDeath(&this->actor);
         }
     }
 }
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/ObjMoonStone_Update.asm")
-// void ObjMoonStone_Update(Actor* thisx, GlobalContext* globalCtx) {
-//     ObjMoonStone* this = THIS;
-//     if ((globalCtx->actorCtx.actorList[2].first->unkA6C & 0x10000282) == 0) {
-//         this->actionFunc;
-//     }
-// }
+void ObjMoonStone_Update(Actor* thisx, GlobalContext* globalCtx) {
+    ObjMoonStone* this = THIS;
+    ActorPlayer* player = PLAYER;
 
-// matches
-// #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Moon_Stone_0x80C06510/ObjMoonStone_Draw.asm")
+    if ((player->stateFlags1 & 0x10000282) == 0) {
+        this->actionFunc(this, globalCtx);
+    }
+}
+
 void ObjMoonStone_Draw(Actor* thisx, GlobalContext* globalCtx) {
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
 
