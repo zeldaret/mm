bdc4c5ba476b65a51287a80942882d120c4a896b
angie <angheloalf95@gmail.com>
2021-05-19 20:38:12 -0400

    Suggestions of zbanks

--- tools/warnings_count/update_current_warnings.sh
+++ tools/warnings_count/update_current_warnings.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
 
 # This script should only be used when we need to modify the accepted amount of warnings.
+
+DIR="$(dirname "$(readlink -f "$0")")"
+cd "$DIR/../.."
 
 make distclean
 make setup 2> tools/warnings_count/warnings_setup_current.txt
