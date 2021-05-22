f3e711d47cfa3cdeeba005e8c9e3f91ade0c269f
angie <angheloalf95@gmail.com>
2021-05-21 21:52:04 -0400

    final pass

--- src/overlays/actors/ovl_Obj_Milk_Bin/z_obj_milk_bin.c
+++ src/overlays/actors/ovl_Obj_Milk_Bin/z_obj_milk_bin.c
@@ -9,7 +9,6 @@ void ObjMilkBin_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjMilkBin_Update(Actor* thisx, GlobalContext* globalCtx);
 void ObjMilkBin_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
 const ActorInit Obj_Milk_Bin_InitVars = {
     ACTOR_OBJ_MILK_BIN,
     ACTORCAT_PROP,
@@ -21,12 +20,7 @@ const ActorInit Obj_Milk_Bin_InitVars = {
     (ActorFunc)ObjMilkBin_Update,
     (ActorFunc)ObjMilkBin_Draw,
 };
-*/
 
-// D_80C09050
-extern ColliderCylinderInit sCylinderInit;
-
-/*
 static ColliderCylinderInit sCylinderInit = {
     {
         COLTYPE_HARD,
@@ -46,51 +40,50 @@ static ColliderCylinderInit sCylinderInit = {
     },
     { 12, 30, 0, { 0, 0, 0 } },
 };
-*/
 
 void ObjMilkBin_Init(Actor* thisx, GlobalContext* globalCtx) {
     ObjMilkBin* this = THIS;
 
-    Collider_InitAndSetCylinder(globalCtx, &this->unk_148, &this->actor, &sCylinderInit);
-    Collider_UpdateCylinder(&this->actor, &this->unk_148);
+    Collider_InitAndSetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    Collider_UpdateCylinder(&this->actor, &this->collider);
 
     this->actor.shape.yOffset = 1100.0f;
-    this->unk_194 = 0;
-    this->unk_198 = thisx->params;
+    this->disableDraw = 0;
+    this->type = thisx->params;
 
-    if ((this->unk_198 == 2) && !(gSaveContext.perm.weekEventReg[0x34] & 1)) {
-        this->unk_194 |= 1;
+    if ((this->type == OBJ_MILK_BIN_TYPE_2) && !(gSaveContext.perm.weekEventReg[0x34] & 1)) {
+        this->disableDraw |= 1;
     }
 }
 
 void ObjMilkBin_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     ObjMilkBin* this = THIS;
 
-    Collider_DestroyCylinder(globalCtx, &this->unk_148);
+    Collider_DestroyCylinder(globalCtx, &this->collider);
 }
 
 void ObjMilkBin_Update(Actor* thisx, GlobalContext* globalCtx2) {
     ObjMilkBin* this = THIS;
     GlobalContext* globalCtx = globalCtx2;
 
-    if (this->unk_198 == 1) {
+    if (this->type == OBJ_MILK_BIN_TYPE_1) {
         if (gSaveContext.perm.weekEventReg[0x16] & 1) {
             if (((gSaveContext.perm.day == 2) && (gSaveContext.perm.isNight == 1)) || (gSaveContext.perm.day >= 3)) {
                 Actor_MarkForDeath(&this->actor);
                 return;
             }
         }
-    } else if (this->unk_198 == 2) {
+    } else if (this->type == OBJ_MILK_BIN_TYPE_2) {
         if (gSaveContext.perm.weekEventReg[0x34] & 1) {
-            this->unk_194 &= ~1;
+            this->disableDraw &= ~1;
         } else {
-            this->unk_194 |= 1;
+            this->disableDraw |= 1;
         }
     }
 
-    if (!(this->unk_194 & 1)) {
-        CollisionCheck_SetAC(globalCtx, &globalCtx->colCheckCtx, &this->unk_148.base);
-        CollisionCheck_SetOC(globalCtx, &globalCtx->colCheckCtx, &this->unk_148.base);
+    if (!(this->disableDraw & 1)) {
+        CollisionCheck_SetAC(globalCtx, &globalCtx->colCheckCtx, &this->collider.base);
+        CollisionCheck_SetOC(globalCtx, &globalCtx->colCheckCtx, &this->collider.base);
     }
 }
 
@@ -100,7 +93,7 @@ extern Gfx D_060004B0[];
 void ObjMilkBin_Draw(Actor* thisx, GlobalContext* globalCtx) {
     ObjMilkBin* this = THIS;
 
-    if (!(this->unk_194 & 1)) {
+    if (!(this->disableDraw & 1)) {
         func_800BDFC0(globalCtx, D_060004B0);
     }
 }
