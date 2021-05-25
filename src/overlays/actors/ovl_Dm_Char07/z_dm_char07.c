ced20f906a6599a3333b23667707bfb405df0aea
Derek Hensley <hensley.derek58@gmail.com>
2021-05-25 10:37:50 -0700

    Dm_Char07 Ok

--- src/overlays/actors/ovl_Dm_Char07/z_dm_char07.c
+++ src/overlays/actors/ovl_Dm_Char07/z_dm_char07.c
@@ -9,7 +9,8 @@ void DmChar07_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void DmChar07_Update(Actor* thisx, GlobalContext* globalCtx);
 void DmChar07_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void DmChar07_DoNothing(DmChar07* this, GlobalContext* globalCtx);
+
 const ActorInit Dm_Char07_InitVars = {
     ACTOR_DM_CHAR07,
     ACTORCAT_ITEMACTION,
@@ -21,16 +22,161 @@ const ActorInit Dm_Char07_InitVars = {
     (ActorFunc)DmChar07_Update,
     (ActorFunc)DmChar07_Draw,
 };
-*/
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/func_80AAE9C0.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/DmChar07_Init.asm")
+extern Gfx D_06000100[];
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/DmChar07_Destroy.asm")
+extern Gfx D_06000240[];
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/func_80AAEABC.asm")
+extern Gfx D_06000650[];
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/DmChar07_Update.asm")
+extern Gfx D_06000790[];
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Dm_Char07_0x80AAE9C0/DmChar07_Draw.asm")
+extern Gfx D_06000B80[];
+
+extern Gfx D_06000CC0[];
+
+extern Gfx D_060010D0[];
+
+extern Gfx D_06001210[];
+
+extern Gfx D_060015E0[];
+
+extern Gfx D_060016B8[];
+
+extern Gfx D_06002BA0[];
+
+extern Gfx D_06002CD0[];
+
+extern CollisionHeader D_06006688;
+
+extern Gfx D_060076A0[];
+
+extern Gfx D_06007918[];
+
+extern AnimatedMaterial D_060105F8;
+
+extern Gfx D_06010D68[];
+
+void DmChar07_Setup(DmChar07 *this, DmChar07ActionFunc actionFunc) {
+    this->actionFunc = actionFunc;
+}
+
+void DmChar07_Init(Actor *thisx, GlobalContext *globalCtx) {
+    DmChar07 *this = THIS;
+
+    this->unk2BA = 0;
+    Actor_SetScale(&this->dyna.actor, 1.0f);
+    this->unk2B9 = (thisx->params >> 8) & 0x1F;
+    thisx->params = thisx->params & 0xFF;
+    if (this->dyna.actor.params == 0) {
+        Actor_SetScale(&this->dyna.actor, 0.1f);
+        this->unk2BA = 1;
+        BcCheck3_BgActorInit(&this->dyna, 0);
+        BgCheck3_LoadMesh(globalCtx, &this->dyna, &D_06006688);
+    } else {
+        Actor_SetScale(&this->dyna.actor, 1.0f);
+    }
+    DmChar07_Setup(this, DmChar07_DoNothing);
+}
+
+void DmChar07_Destroy(Actor *thisx, GlobalContext *globalCtx) {
+    DmChar07 *this = THIS;
+    if (this->unk2BA != 0) {
+        BgCheck_RemoveActorMesh(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+    }
+}
+
+void DmChar07_DoNothing(DmChar07* this, GlobalContext* globalCtx) {
+}
+
+void DmChar07_Update(Actor* thisx, GlobalContext* globalCtx) {
+    DmChar07 *this = THIS;
+
+    this->actionFunc(this, globalCtx);
+}
+
+void DmChar07_Draw(Actor *thisx, GlobalContext *globalCtx) {
+    DmChar07 *this = THIS;
+    s32 pad;
+
+    OPEN_DISPS(globalCtx->state.gfxCtx);
+
+    func_8012C28C(globalCtx->state.gfxCtx);
+    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    switch(this->dyna.actor.params) {
+        case 0:
+            gSPDisplayList(POLY_OPA_DISP++, D_06002CD0);
+            break;
+        case 1:
+            AnimatedMat_Draw(globalCtx, Lib_SegmentedToVirtual(&D_060105F8));
+            gSPDisplayList(POLY_OPA_DISP++, D_06007918);
+            gSPDisplayList(POLY_OPA_DISP++, D_06000240);
+            gSPDisplayList(POLY_OPA_DISP++, D_06000790);
+            gSPDisplayList(POLY_OPA_DISP++, D_06000CC0);
+            gSPDisplayList(POLY_OPA_DISP++, D_06001210);
+            gSPDisplayList(POLY_OPA_DISP++, D_060016B8);
+            break;
+        case 2:
+            if (this->unk2B9 & 1) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000240);
+            }
+            if (this->unk2B9 & 2) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000790);
+            }
+            if (this->unk2B9 & 4) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000CC0);
+            }
+            if (this->unk2B9 & 8) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06001210);
+            }
+            if (this->unk2B9 & 16) {
+                gSPDisplayList(POLY_OPA_DISP++, D_060016B8);
+            }
+            break;
+        case 3:
+            gSPDisplayList(POLY_OPA_DISP++, D_06010D68);
+            break;
+        default:
+            break;
+    }
+
+    func_8012C2DC(globalCtx->state.gfxCtx);
+    gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    switch(this->dyna.actor.params) {
+        case 0:
+            gSPDisplayList(POLY_XLU_DISP++, D_06002BA0);
+            break;
+        case 1:
+            gSPDisplayList(POLY_XLU_DISP++, D_060076A0);
+            gSPDisplayList(POLY_XLU_DISP++, D_06000100);
+            gSPDisplayList(POLY_XLU_DISP++, D_06000650);
+            gSPDisplayList(POLY_XLU_DISP++, D_06000B80);
+            gSPDisplayList(POLY_XLU_DISP++, D_060010D0);
+            gSPDisplayList(POLY_XLU_DISP++, D_060015E0);
+            break;
+        case 2:
+            if (this->unk2B9 & 1) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000100);
+            }
+            if (this->unk2B9 & 2) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000650);
+            }
+            if (this->unk2B9 & 4) {
+                gSPDisplayList(POLY_OPA_DISP++, D_06000B80);
+            }
+            if (this->unk2B9 & 8) {
+                gSPDisplayList(POLY_OPA_DISP++, D_060010D0);
+            }
+            if (this->unk2B9 & 16) {
+                gSPDisplayList(POLY_OPA_DISP++, D_060015E0);
+            }
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
+
+    CLOSE_DISPS(globalCtx->state.gfxCtx);
+}
--- src/overlays/actors/ovl_Dm_Char07/z_dm_char07.h
+++ src/overlays/actors/ovl_Dm_Char07/z_dm_char07.h
@@ -5,9 +5,15 @@
 
 struct DmChar07;
 
+typedef void (*DmChar07ActionFunc)(struct DmChar07*, GlobalContext*);
+
 typedef struct DmChar07 {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x178];
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ char pad15C[0x14C];
+    /* 0x2A8 */ DmChar07ActionFunc actionFunc;
+    /* 0x2AC */ char pad2AC[0xD];
+    /* 0x2B9 */ u8 unk2B9;
+    /* 0x2BA */ u8 unk2BA;
 } DmChar07; // size = 0x2BC
 
 extern const ActorInit Dm_Char07_InitVars;
