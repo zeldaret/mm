47b3f941c4631619b077bd4676efbcc323e57b55
Parker Burnett <burnettparker@gmail.com>
2021-05-25 16:48:24 -0400

    some clean up stuff

--- src/overlays/actors/ovl_Obj_Ghaka/z_obj_ghaka.c
+++ src/overlays/actors/ovl_Obj_Ghaka/z_obj_ghaka.c
@@ -9,6 +9,10 @@ void ObjGhaka_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjGhaka_Update(Actor* thisx, GlobalContext* globalCtx);
 void ObjGhaka_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80B3C260(ObjGhaka* this);
+void func_80B3C29C(ObjGhaka* this);
+void func_80B3C2B0(ObjGhaka* this);
+void func_80B3C2C4(ObjGhaka* this, GlobalContext* globalCtx);
 void func_80B3C39C(ObjGhaka* this, GlobalContext* globalCtx);
 void func_80B3C4E0(ObjGhaka* this, GlobalContext* globalCtx);
 void func_80B3C624(ObjGhaka* this, GlobalContext* globalCtx);
@@ -27,16 +31,13 @@ const ActorInit Obj_Ghaka_InitVars = {
 };
 
 
-s32 D_80B3C960[] = { 0x00000000, 0x00000000, 0x00000000 };
-
 // s32 D_80B3C96C[] = { 0x801F0000, 0x3054001E, 0x00000000, 0x00000000, 0x00000000 };
 
-InitChainEntry D_80B3C96C[] = {
+static InitChainEntry D_80B3C96C[] = {
     ICHAIN_U8(targetMode, 0, ICHAIN_CONTINUE),
     ICHAIN_F32(targetArrowOffset, 30, ICHAIN_STOP),
 };
 
-extern Gfx D_06003CD0[];
 extern Gfx D_06001A20[];
 extern Gfx D_06001980[];
 
@@ -46,29 +47,29 @@ void func_80B3C260(ObjGhaka* this) {
     if ((gSaveContext.perm.weekEventReg[20] & 0x20) != 0) {
         this->dyna.actor.world.pos.z = this->dyna.actor.home.pos.z + 100.0f;
     }
-    this->actionFunc2 = func_80B3C39C;
+    this->actionFunc = func_80B3C39C;
 }
 
 // matches
 // #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ghaka_0x80B3C260/func_80B3C29C.asm")
 void func_80B3C29C(ObjGhaka* this) {
-    this->actionFunc2 = func_80B3C4E0;
+    this->actionFunc = func_80B3C4E0;
 }
 
 // matches
 // #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ghaka_0x80B3C260/func_80B3C2B0.asm")
 void func_80B3C2B0(ObjGhaka* this) {
-    this->actionFunc2 = func_80B3C624;
+    this->actionFunc = func_80B3C624;
 }
 
 // matches
 // #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ghaka_0x80B3C260/func_80B3C2C4.asm")
 void func_80B3C2C4(ObjGhaka* this, GlobalContext* globalCtx) {
     if ((gSaveContext.perm.weekEventReg[20] & 0x20) == 0) {
-        Actor_SpawnWithParentAndCutscene(&globalCtx->actorCtx, globalCtx, 0x1FE, 0.0f, 25.0f, 261.0f, 0, 0, 0, 0,
+        Actor_SpawnWithParentAndCutscene(&globalCtx->actorCtx, globalCtx, ACTOR_BG_GORON_OYU, 0.0f, 25.0f, 261.0f, 0, 0, 0, 0,
                                          this->dyna.actor.cutscene, this->dyna.actor.unk20, 0);
     } else {
-        Actor_SpawnWithParentAndCutscene(&globalCtx->actorCtx, globalCtx, 0x1FE, 0.0f, 25.0f, 261.0f, 0, 0, 0, 1, -1,
+        Actor_SpawnWithParentAndCutscene(&globalCtx->actorCtx, globalCtx, ACTOR_BG_GORON_OYU, 0.0f, 25.0f, 261.0f, 0, 0, 0, 1, -1,
                                          this->dyna.actor.unk20, 0);
     }
 }
@@ -138,6 +139,8 @@ void func_80B3C4E0(ObjGhaka* this, GlobalContext* globalCtx) {
     }
 }
 
+
+Vec3f D_80B3C960 = { 0.0f, 0.0f, 0.0f };
 // matches
 // #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Ghaka_0x80B3C260/func_80B3C624.asm")
 void func_80B3C624(ObjGhaka* this, GlobalContext* globalCtx) {
@@ -152,9 +155,9 @@ void func_80B3C624(ObjGhaka* this, GlobalContext* globalCtx) {
         func_80B3C2C4(this, globalCtx);
         gSaveContext.perm.weekEventReg[20] |= 0x20;
         func_80B3C260(this);
-        func_8019F1C0(&D_80B3C960, 0x2835U);
+        func_8019F1C0(&D_80B3C960, 0x2835);
     } else {
-        func_8019F1C0(&D_80B3C960, 0x200AU);
+        func_8019F1C0(&D_80B3C960, 0x200A);
     }
 }
 
@@ -169,11 +172,11 @@ void ObjGhaka_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_ProcessInitChain(&this->dyna.actor, D_80B3C96C);
     Actor_SetScale(&this->dyna.actor, 0.1f);
     BcCheck3_BgActorInit(&this->dyna, 1);
-    BgCheck_RelocateMeshHeader(D_06003CD0, &sp2C);
+    BgCheck_RelocateMeshHeader(&D_06003CD0, &sp2C);
     this->dyna.bgId = BgCheck_AddActorMesh(globalCtx, &globalCtx->colCtx.dyna, &this->dyna, sp2C);
-    func_800B78B8(globalCtx, &this->dyna, 0.0f, 0.0f, 0.0f, 4);
+    func_800B78B8(globalCtx, &this->dyna.actor, 0.0f, 0.0f, 0.0f, 4);
     if (this->dyna.actor.floorPoly == 0) {
-        Actor_MarkForDeath(&this->dyna);
+        Actor_MarkForDeath(&this->dyna.actor);
     }
     if ((gSaveContext.perm.weekEventReg[20] & 0x20) != 0) {
         func_80B3C2C4(this, globalCtx);
@@ -193,7 +196,7 @@ void ObjGhaka_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 void ObjGhaka_Update(Actor* thisx, GlobalContext* globalCtx) {
     ObjGhaka* this = THIS;
 
-    this->actionFunc2(this, globalCtx);
+    this->actionFunc(this, globalCtx);
     thisx->focus.pos.x = thisx->world.pos.x;
     thisx->focus.pos.y = thisx->world.pos.y + 60.0f;
     thisx->focus.pos.z = thisx->world.pos.z;
--- src/overlays/actors/ovl_Obj_Ghaka/z_obj_ghaka.h
+++ src/overlays/actors/ovl_Obj_Ghaka/z_obj_ghaka.h
@@ -6,15 +6,16 @@
 struct ObjGhaka;
 
 typedef void (*ObjGhakaActionFunc)(struct ObjGhaka*, GlobalContext*);
-typedef void (*ObjGhakaActionFunc2)(struct ObjGhaka*, GlobalContext*);
 
 typedef struct ObjGhaka {
     /* 0x000 */ DynaPolyActor dyna;
-    /* 0x15C */ ObjGhakaActionFunc2 actionFunc2;
+    /* 0x15C */ ObjGhakaActionFunc actionFunc;
     /* 0xXXX */ char unkStuff[0x8];
     /* 0x168 */ s16 unk_168;
 } ObjGhaka; // size = 0x16C
 
 extern const ActorInit Obj_Ghaka_InitVars;
+
+extern CollisionHeader D_06003CD0;
 
 #endif // Z_OBJ_GHAKA_H
