fb84cabd4a25efaf26087084c3695773305ff906
Derek Hensley <hensley.derek58@gmail.com>
2021-06-02 15:33:16 -0700

    Ovl_En_Toto OK (#163)
    * Lots of progress
    
    * OK
    
    * Removed casts
    
    * Run format.sh
    
    * cleanup
    
    * More cleanup
    
    * Minor Doc
    
    * More cleanup
    
    * Format
    
    * Add header description
    
    * PR comments
    
    * Convert raw pointers
    
    * Update to new gSaveContext
    
    * PR suggestions

--- include/z64.h
+++ include/z64.h
@@ -932,7 +932,9 @@ typedef struct {
     /* 0x12020 */ u8 unk12020;
     /* 0x12021 */ u8 unk12021;
     /* 0x12022 */ u8 choiceIndex;
-    /* 0x12023 */ UNK_TYPE1 pad12023[0x21];
+    /* 0x12023 */ UNK_TYPE1 pad12023[0x7];
+    /* 0x1202A */ u16 unk1202A;
+    /* 0x1202C */ UNK_TYPE1 pad1202B[0x18];
     /* 0x12044 */ s16 unk12044;
     /* 0x12046 */ UNK_TYPE1 pad12046[0x24];
     /* 0x1206A */ s16 unk1206A;
