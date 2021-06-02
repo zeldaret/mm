06ef43214420637b19ac0a7981a4b81c726710c3
Zelllll <elilee968@gmail.com>
2021-06-02 14:40:03 -0500

    two funcs

--- src/overlays/actors/ovl_Bg_Tobira01/z_bg_tobira01.c
+++ src/overlays/actors/ovl_Bg_Tobira01/z_bg_tobira01.c
@@ -21,11 +21,30 @@ const ActorInit Bg_Tobira01_InitVars = {
     (ActorFunc)BgTobira01_Draw,
 };
 
+extern CollisionHeader D_060011C0;
+
+void func_80B12430(BgTobira01* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Bg_Tobira01_0x80B12430/func_80B12430.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Bg_Tobira01_0x80B12430/BgTobira01_Init.asm")
+void BgTobira01_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgTobira01* this = THIS;
+    s32 pad;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Bg_Tobira01_0x80B12430/BgTobira01_Destroy.asm")
+    BcCheck3_BgActorInit(&this->dyna, 1);
+    BgCheck3_LoadMesh(globalCtx, &this->dyna, &D_060011C0);
+    gSaveContext.weekEventReg[88] &= 0xBF;
+    Actor_SetScale(&this->dyna.actor, 1.0f);
+    this->isNight = gSaveContext.isNight;
+    this->timer = 0;
+    this->actionFunc = func_80B12430;
+}
+
+void BgTobira01_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgTobira01* this = THIS;
+    s32 pad;
+
+    BgCheck_RemoveActorMesh(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+}
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Bg_Tobira01_0x80B12430/BgTobira01_Update.asm")
 
