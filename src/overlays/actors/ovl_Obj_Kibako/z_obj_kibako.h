1defb617d7d422108803c68a8d28205d806806c5
CrookedPoe <DJInsomniaPro@gmail.com>
2021-06-02 15:40:17 -0500

    Clean dist.

--- src/overlays/actors/ovl_Obj_Kibako/z_obj_kibako.h
+++ src/overlays/actors/ovl_Obj_Kibako/z_obj_kibako.h
@@ -5,9 +5,17 @@
 
 struct ObjKibako;
 
+typedef void (*ObjKibakoActionFunc)(struct ObjKibako*, GlobalContext*);
+
 typedef struct ObjKibako {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x58];
+    /* 0x144 */ ObjKibakoActionFunc actionFunc;
+    /* 0x148 */ ColliderCylinder collider;
+    /* 0x194 */ s16 timer;
+    /* 0x196 */ s16 bankIndex;
+    /* 0x198 */ s8 unk198;
+    /* 0x199 */ s8 unk199; /* room? */
+    /* 0x19A */ u8 __pad[2];
 } ObjKibako; // size = 0x19C
 
 extern const ActorInit Obj_Kibako_InitVars;
