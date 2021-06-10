ae2073e119775bd0d029cc5b8bff15e889f37a75
engineer124 <47598039+engineer124@users.noreply.github.com>
2021-06-11 07:34:57 +1000

    Ovl_En_Pametfrog OK with partial documentation (#165)
    * Init, 14 of 70 decompiled
    
    * En_Pametfrog 26/70 functions decompiled
    
    * 35/70 funcs
    
    * Fully Matching!
    
    * Documentation
    
    * Fix merge from master
    
    * PR suggestions
    
    * minor improvements
    
    * Fix functions.h
    
    * Oopsie
    
    * Oopsie Part 2
    
    * Remove Pametfrog from split_asm.py rodata_excluded
    
    * Better use of MACROS + animation docs
    
    Co-authored-by: engineer124 <engineer124enginer124@gmail.com>

--- src/overlays/actors/ovl_En_Bigpamet/z_en_bigpamet.h
+++ src/overlays/actors/ovl_En_Bigpamet/z_en_bigpamet.h
@@ -7,7 +7,9 @@ struct EnBigpamet;
 
 typedef struct EnBigpamet {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x488];
+    /* 0x144 */ char unk_144[0x168];
+    /* 0x2AC */ f32 unk_2AC;
+    /* 0x2B0 */ char unk_2B0[0x31C];
 } EnBigpamet; // size = 0x5CC
 
 extern const ActorInit En_Bigpamet_InitVars;
