29dc0a351e61b866f8c39b85bc8a02b79d6c19ac
Zelllll <elilee968@gmail.com>
2021-07-04 15:46:47 -0700

    typo

--- include/z64.h
+++ include/z64.h
@@ -635,7 +635,7 @@ typedef struct {
     /* 0x168 */ u8* iconItemSegment;
     /* 0x16C */ u8* iconItem24Segment;
     /* 0x170 */ u8* unk_170;
-    /* 0x174 */ u8* unk_174;;
+    /* 0x174 */ u8* unk_174;
     /* 0x178 */ u8* unk_178;
     /* 0x17C */ u8* unk_17C;
     /* 0x180 */ Vtx* itemPageVtx;
