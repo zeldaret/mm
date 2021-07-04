5ae6be88702e2be1b10296ceb0d2928c2a17c532
Zelllll <elilee968@gmail.com>
2021-07-04 13:57:15 -0700

    revert accidental change

--- tools/ZAPD/ZAPD/ZRoom/Commands/SetWind.cpp
+++ tools/ZAPD/ZAPD/ZRoom/Commands/SetWind.cpp
@@ -9,16 +9,16 @@ void SetWind::ParseRawData()
 {
 	ZRoomCommand::ParseRawData();
 	auto& parentRawData = parent->GetRawData();
-	windDir.x = parentRawData.at(rawDataIndex + 0x04);
-	windDir.y = parentRawData.at(rawDataIndex + 0x05);
-	windDir.z = parentRawData.at(rawDataIndex + 0x06);
+	windWest = parentRawData.at(rawDataIndex + 0x04);
+	windVertical = parentRawData.at(rawDataIndex + 0x05);
+	windSouth = parentRawData.at(rawDataIndex + 0x06);
 	clothFlappingStrength = parentRawData.at(rawDataIndex + 0x07);
 }
 
 std::string SetWind::GetBodySourceCode() const
 {
-	return StringHelper::Sprintf("SCENE_CMD_WIND_SETTINGS(%i, %i, %i, %i)", windDir.x, windDir.y,
-	                             windDir.z, clothFlappingStrength);
+	return StringHelper::Sprintf("SCENE_CMD_WIND_SETTINGS(%i, %i, %i, %i)", windWest, windVertical,
+	                             windSouth, clothFlappingStrength);
 }
 
 std::string SetWind::GetCommandCName() const
