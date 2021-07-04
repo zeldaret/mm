f2a9501a29ac10bd90e053084f6fe86a73f26b52
Zelllll <elilee968@gmail.com>
2021-07-03 22:26:13 -0700

    fix2

--- src/overlays/actors/ovl_Bg_Iknv_Obj/z_bg_iknv_obj.c
+++ src/overlays/actors/ovl_Bg_Iknv_Obj/z_bg_iknv_obj.c
@@ -130,7 +130,7 @@ void BgIknvObj_UpdateWaterwheel(BgIknvObj* this, GlobalContext* globalCtx) {
         func_800B9010(&this->dyna.actor, NA_SE_EV_WOOD_WATER_WHEEL - SFX_FLAG);
     }
 
-    if ((globalCtx->csCtx.state != 0) && (gSaveContext.sceneSetupIndex == 1) && (globalCtx->csCtx.unk12 == 4) &&
+    if ((globalCtx->csCtx.state != 0) && (gSaveContext.sceneSetupIndex == 1) && (globalCtx->csCtx.unk_12 == 4) &&
         (globalCtx->csCtx.frames == 0x5D7)) {
         func_8019F128(NA_SE_EV_DOOR_UNLOCK);
     }
