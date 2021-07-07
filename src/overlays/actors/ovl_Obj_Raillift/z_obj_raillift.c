bd9f5741db1a66dc2c81de32a0b979b3ceeebc48
Derek Hensley <d.hensley@tempered.io>
2021-07-06 21:20:51 -0700

    First pass: matched everything with data

--- src/overlays/actors/ovl_Obj_Raillift/z_obj_raillift.c
+++ src/overlays/actors/ovl_Obj_Raillift/z_obj_raillift.c
@@ -9,7 +9,24 @@ void ObjRaillift_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjRaillift_Update(Actor* thisx, GlobalContext* globalCtx);
 void ObjRaillift_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void func_80A1A360(Actor* thisx, GlobalContext* globalCtx);
+void func_80A1A330(Actor* thisx, GlobalContext* globalCtx);
+
+void func_80A19B98(ObjRaillift* this, GlobalContext* globalCtx);
+void func_80A19F18(ObjRaillift* this, GlobalContext* globalCtx);
+void func_80A19910(ObjRaillift* this, s32 arg1);
+void func_80A19F78(ObjRaillift* this, GlobalContext* globalCtx);
+void func_80A19E84(ObjRaillift* this, GlobalContext* globalCtx);
+void func_80A19EE0(ObjRaillift* this, GlobalContext* globalCtx);
+void func_80A19BA8(ObjRaillift* this, GlobalContext* globalCtx);
+
+extern CollisionHeader D_06004FF8;
+extern CollisionHeader D_060048D0;
+extern Gfx D_06004BF0[];
+extern Gfx D_06000208[];
+extern Gfx D_060071B8[];
+
+
 const ActorInit Obj_Raillift_InitVars = {
     ACTOR_OBJ_RAILLIFT,
     ACTORCAT_BG,
@@ -21,30 +38,252 @@ const ActorInit Obj_Raillift_InitVars = {
     (ActorFunc)ObjRaillift_Update,
     (ActorFunc)ObjRaillift_Draw,
 };
-*/
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19910.asm")
+static InitChainEntry sInitChain[] = {
+    ICHAIN_F32(uncullZoneForward, 4000, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneScale, 200, ICHAIN_CONTINUE),
+    ICHAIN_F32(uncullZoneDownward, 400, ICHAIN_CONTINUE),
+    ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
+};
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/ObjRaillift_Init.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/ObjRaillift_Destroy.asm")
+static CollisionHeader* D_80A1A3C0[] = { &D_06004FF8 };
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19B98.asm")
+static CollisionHeader* D_80A1A3C4 = &D_060048D0;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19BA8.asm")
+void func_80A19910(ObjRaillift* this, s32 idx) {
+    Math_Vec3s_ToVec3f(&this->dyna.actor.world.pos, &this->unk170[idx]);
+}
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19E84.asm")
+void ObjRaillift_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ObjRaillift* this = THIS;
+    CollisionHeader** colHeader;
+    Path* path;
+    s32 sp48 = (thisx->params >> 0xF) & 1;
+    s32 sp44 = 0;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19EE0.asm")
+    Actor_ProcessInitChain(thisx, sInitChain);
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19F18.asm")
+    thisx->shape.rot.x = 0;
+    thisx->world.rot.x = 0;
+    thisx->shape.rot.z = 0;
+    thisx->world.rot.z = 0;
+    BcCheck3_BgActorInit(&this->dyna, 1);
+    colHeader = &D_80A1A3C0[sp48];
+    BgCheck3_LoadMesh(globalCtx, &this->dyna, *colHeader);
+    this->unk160 = thisx->home.rot.z * 0.1f;
+    if (this->unk160 < 0.0f) {
+        this->unk160 = -this->unk160;
+        sp44 = 1;
+    }
+    if (colHeader == &D_80A1A3C4) {
+        Actor_SpawnWithParent(&globalCtx->actorCtx, &this->dyna.actor, globalCtx, ACTOR_OBJ_ETCETERA,
+                              this->dyna.actor.world.pos.x, this->dyna.actor.world.pos.y, this->dyna.actor.world.pos.z,
+                              this->dyna.actor.shape.rot.x, this->dyna.actor.shape.rot.y, this->dyna.actor.shape.rot.z,
+                              0);
+        if (sp44 != 0) {
+            this->dyna.actor.draw = func_80A1A360;
+        } else {
+            this->dyna.actor.draw = func_80A1A330;
+        }
+    }
+    if (this->unk160 < 0.01f) {
+        this->actionFunc = func_80A19B98;
+    } else {
+        path = &globalCtx->setupPathList[thisx->params & 0x7F];
+        this->unk168 = (thisx->params >> 7) & 0x1F;
+        this->unk164 = path->count - 1;
+        this->unk16C = 1;
+        this->unk170 = (Vec3s*)Lib_SegmentedToVirtual(path->points);
+        func_80A19910(this, this->unk168);
+        if (((thisx->params >> 0xD) & 1) && !Flags_GetSwitch(globalCtx, thisx->home.rot.x & 0x7F)) {
+            this->actionFunc = func_80A19F18;
+        } else {
+            this->actionFunc = func_80A19BA8;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A19F78.asm")
+void ObjRaillift_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    ObjRaillift* this = THIS;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/ObjRaillift_Update.asm")
+    BgCheck_RemoveActorMesh(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+}
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/ObjRaillift_Draw.asm")
+void func_80A19B98(ObjRaillift* this, GlobalContext* globalCtx) {
+}
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A1A330.asm")
+void func_80A19BA8(ObjRaillift* this, GlobalContext* globalCtx) {
+    int new_var;
+    Vec3f sp48;
+    f32 speed;
+    f32 target;
+    f32 step;
+    s32 flag;
+    Vec3s* temp_v0;
+    Vec3s* temp_v1_2;
+    s32 pad;
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Raillift_0x80A19910/func_80A1A360.asm")
+    if (((&this->dyna.actor)->params >> 0xD) & 1) {
+        if (Flags_GetSwitch(globalCtx, (&this->dyna.actor)->home.rot.x & 0x7F) == 0) {
+            this->actionFunc = func_80A19F18;
+            return;
+        }
+
+        if ((((&this->dyna.actor)->params >> 0xF) & 1) == 1) {
+            func_800B9010(&this->dyna.actor, 0x2903 - 0x800);
+        }
+    }
+
+    Math_Vec3s_ToVec3f(&sp48, &(&this->unk170[this->unk168])[this->unk16C]);
+    Math_Vec3f_Diff(&sp48, &this->dyna.actor.world.pos, &this->dyna.actor.velocity);
+    speed = Math3D_Vec3fMagnitude(&this->dyna.actor.velocity);
+    if ((speed < (this->unk160 * 8.0f)) && (this->unk160 > 2.0f)) {
+        target = ((this->unk160 - 2.0f) * 0.1f) + 2.0f;
+        step = this->unk160 * 0.03f;
+    } else {
+        target = this->unk160;
+        step = this->unk160 * 0.16f;
+    }
+
+    Math_StepToF(&this->dyna.actor.speedXZ, target, step);
+    if ((this->dyna.actor.speedXZ + 0.05f) < speed) {
+        Math_Vec3f_Scale(&this->dyna.actor.velocity, this->dyna.actor.speedXZ / speed);
+        this->dyna.actor.world.pos.x += this->dyna.actor.velocity.x;
+        this->dyna.actor.world.pos.y += this->dyna.actor.velocity.y;
+        this->dyna.actor.world.pos.z += this->dyna.actor.velocity.z;
+    } else {
+        this->unk168 += this->unk16C;
+        if (1) {}
+        this->dyna.actor.speedXZ *= 0.4f;
+        new_var = (this->dyna.actor.params >> 0xC) & 1;
+        flag = 1;
+        if (((this->unk168 >= this->unk164) && (this->unk16C > 0)) || ((this->unk168 <= 0) && (this->unk16C < 0))) {
+            if (!new_var) {
+                this->unk16C = -this->unk16C;
+                this->unk186 = 10;
+                this->actionFunc = func_80A19EE0;
+            } else {
+                temp_v1_2 = &this->unk170[this->unk164];
+                this->unk168 = (this->unk16C > 0) ? (0) : (this->unk164);
+                temp_v0 = this->unk170;
+                if (((temp_v0->x != temp_v1_2->x) || (temp_v0->y != temp_v1_2->y)) || (temp_v0->z != (temp_v1_2->z))) {
+                    this->actionFunc = func_80A19E84;
+                    func_800C62BC(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+                    flag = 0;
+                }
+            }
+        }
+
+        if (flag != 0) {
+            func_80A19910(this, this->unk168);
+        }
+    }
+}
+
+void func_80A19E84(ObjRaillift* this, GlobalContext* globalCtx) {
+    if (!func_800CAF70(&this->dyna)) {
+        func_80A19910(this, this->unk168);
+        func_800C6314(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+        this->actionFunc = func_80A19BA8;
+    }
+}
+
+void func_80A19EE0(ObjRaillift* this, GlobalContext* globalCtx) {
+    this->unk186--;
+    if (this->unk186 <= 0) {
+        this->actionFunc = func_80A19BA8;
+        this->dyna.actor.speedXZ = 0.0f;
+    }
+}
+
+void func_80A19F18(ObjRaillift* this, GlobalContext* globalCtx) {
+    if (Flags_GetSwitch(globalCtx, (&this->dyna.actor)->home.rot.x & 0x7F)) {
+        this->dyna.actor.speedXZ = 0.0f;
+        ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
+        this->actionFunc = func_80A19F78;
+    }
+}
+
+void func_80A19F78(ObjRaillift* this, GlobalContext* globalCtx) {
+    if (ActorCutscene_GetCanPlayNext(this->dyna.actor.cutscene)) {
+        ActorCutscene_StartAndSetUnkLinkFields(this->dyna.actor.cutscene, &this->dyna.actor);
+        this->unk188 = 0x32;
+        this->actionFunc = func_80A19BA8;
+    } else {
+        ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
+    }
+}
+
+void ObjRaillift_Update(Actor* thisx, GlobalContext* globalCtx) {
+    ObjRaillift* this = THIS;
+    f32 target;
+    f32 step;
+
+    this->actionFunc(this, globalCtx);
+    Actor_SetHeight(&this->dyna.actor, 10.0f);
+    if (this->unk188 > 0) {
+        this->unk188--;
+        if (this->unk188 == 0) {
+            ActorCutscene_Stop(this->dyna.actor.cutscene);
+        }
+    }
+    if ((thisx->params >> 0xE) & 1) {
+        this->unk178 = this->unk174;
+        if (func_800CAF70(&this->dyna)) {
+            this->unk174 = 1;
+        } else {
+            this->unk174 = 0;
+        }
+        if ((this->unk174 != this->unk178) && (this->unk180 < 1.0f)) {
+            this->unk184 = -0x8000;
+            this->unk180 = 6.0f;
+        }
+        this->unk184 += 0xCE4;
+        Math_StepToF(&this->unk180, 0.0f, 0.12f);
+        if (this->unk174 != 0) {
+            step = Math_CosS(fabsf(this->unk17C) * 2048.0f) + 0.02f;
+        } else {
+            step = Math_SinS(fabsf(this->unk17C) * 2048.0f) + 0.02f;
+        }
+        if (this->unk174 != 0) {
+            target = -8.0f;
+        } else {
+            target = 0.0f;
+        }
+        Math_StepToF(&this->unk17C, target, step);
+        this->dyna.actor.shape.yOffset = ((Math_SinS(this->unk184) * this->unk180) + this->unk17C) * 10.0f;
+    dummy_label_653499:;
+    }
+    if ((((thisx->params >> 0xF) & 1) == 1) && this->dyna.actor.child != NULL) {
+        if (this->dyna.actor.child->update == NULL) {
+            this->dyna.actor.child = NULL;
+        } else {
+            this->dyna.actor.child->world.pos.x = this->dyna.actor.world.pos.x;
+            this->dyna.actor.child->world.pos.y =
+                this->dyna.actor.world.pos.y + (this->dyna.actor.shape.yOffset * this->dyna.actor.scale.y);
+            this->dyna.actor.child->world.pos.z = this->dyna.actor.world.pos.z;
+        }
+    }
+}
+
+void ObjRaillift_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    s32 pad;
+
+    OPEN_DISPS(globalCtx->state.gfxCtx);
+    func_8012C28C(globalCtx->state.gfxCtx);
+    gSPSegment(POLY_OPA_DISP++, 0x08,
+               Gfx_TwoTexScrollEnvColor(globalCtx->state.gfxCtx, 0, globalCtx->gameplayFrames, 0, 0x20, 0x20, 1, 0, 0,
+                                        0x20, 0x20, 0, 0, 0, 0xA0));
+    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPDisplayList(POLY_OPA_DISP++, D_06004BF0);
+    CLOSE_DISPS(globalCtx->state.gfxCtx);
+}
+
+void func_80A1A330(Actor* thisx, GlobalContext* globalCtx) {
+    func_800BDFC0(globalCtx, D_06000208);
+}
+
+void func_80A1A360(Actor* thisx, GlobalContext* globalCtx) {
+    func_800BDFC0(globalCtx, D_060071B8);
+}
--- src/overlays/actors/ovl_Obj_Raillift/z_obj_raillift.h
+++ src/overlays/actors/ovl_Obj_Raillift/z_obj_raillift.h
@@ -5,9 +5,24 @@
 
 struct ObjRaillift;
 
+typedef void (*ObjRailliftActionFunc)(struct ObjRaillift*, GlobalContext*);
+
 typedef struct ObjRaillift {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x48];
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ ObjRailliftActionFunc actionFunc;
+    /* 0x160 */ f32 unk160;
+    /* 0x164 */ s32 unk164;
+    /* 0x168 */ s32 unk168;
+    /* 0x16C */ s32 unk16C;
+    /* 0x170 */ Vec3s* unk170;
+    /* 0x174 */ s32 unk174;
+    /* 0x178 */ s32 unk178;
+    /* 0x17C */ f32 unk17C;
+    /* 0x180 */ f32 unk180;
+    /* 0x184 */ s16 unk184;
+    /* 0x186 */ s16 unk186;
+    /* 0x188 */ s16 unk188;
+    /* 0x18A */ char unk18A[0x2];
 } ObjRaillift; // size = 0x18C
 
 extern const ActorInit Obj_Raillift_InitVars;
