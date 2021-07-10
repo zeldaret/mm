e50fa54db23e13530bb0854eaf09fc9243af88d1
engineer124 <engineer124engineer124@gmail.com>
2021-07-10 15:34:46 +1000

    Create z_quake.c file

---
+++ src/code/z_quake.c
@@ -0,0 +1,65 @@
+#include "global.h"
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Random.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_UpdateShakeInfo.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback1.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback5.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback6.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback3.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback2.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Callback4.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_GetFreeIndex.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_AddImpl.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Remove.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_GetRequest.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_SetValue.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_SetSpeed.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_SetCountdown.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_GetCountdown.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_SetQuakeValues.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_SetUnkValues.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Init.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Add.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_RemoveFromIdx.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_Calc.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_Init.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_SetCountdown.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_GetCountdown.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_GetType.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_SetType.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_ClearType.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_GetFloorQuake.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake2_Update.asm")
+
+#pragma GLOBAL_ASM("./asm/non_matchings/code/code_0x8012A080/Quake_NumActiveQuakes.asm")
+
+
