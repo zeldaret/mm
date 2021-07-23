a0d5b5ff3acbda8e8e4234b102b72016d6230777
angie <angheloalf95@gmail.com>
2021-07-23 18:35:12 -0400

    boot OK?

--- src/boot/getintmask.c
+++
@@ -1,4 +0,0 @@
-#include <ultra64.h>
-#include <global.h>
-
-#pragma GLOBAL_ASM("asm/non_matchings/boot/getintmask/osGetIntMask.s")
--- src/boot/interrupt.c
+++
@@ -1,6 +0,0 @@
-#include <ultra64.h>
-#include <global.h>
-
-#pragma GLOBAL_ASM("asm/non_matchings/boot/interrupt/__osDisableInt.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/boot/interrupt/__osRestoreInt.s")
