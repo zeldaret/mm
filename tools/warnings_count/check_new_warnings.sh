76294457cec84b28b8f193f2538c8d6253593f87
angie <angheloalf95@gmail.com>
2021-07-29 00:23:04 -0400

    sneeky commit to update warnings tooling

--- tools/warnings_count/check_new_warnings.sh
+++ tools/warnings_count/check_new_warnings.sh
@@ -8,7 +8,7 @@ cd "$DIR/../.."
 
 make distclean
 make setup 2> tools/warnings_count/warnings_setup_new.txt
-make disasm 2>> tools/warnings_count/warnings_setup_new.txt
+make disasm 2> tools/warnings_count/warnings_disasm_new.txt
 make all 2> tools/warnings_count/warnings_build_new.txt
 
 python3 tools/warnings_count/compare_warnings.py tools/warnings_count/warnings_setup_current.txt tools/warnings_count/warnings_setup_new.txt
