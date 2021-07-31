5687be484ab89d3c6d84bb5abb29b4b4081f2b3d
Rylie <31797144+rylieb@users.noreply.github.com>
2021-07-31 17:17:40 -0600

    ObjWarpstone_Init() matching and mostly documented.

--- src/overlays/actors/ovl_Obj_Warpstone/z_obj_warpstone.c
+++ src/overlays/actors/ovl_Obj_Warpstone/z_obj_warpstone.c
@@ -8,6 +8,9 @@ void ObjWarpstone_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjWarpstone_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjWarpstone_Update(Actor* thisx, GlobalContext* globalCtx);
 void ObjWarpstone_Draw(Actor* thisx, GlobalContext* globalCtx);
+void func_80B92B10(Actor* thisx, GlobalContext* globalCtx);
+void func_80B92C00(Actor* thisx, s32 arg1);
+void func_80B92DC4(Actor* thisx, s32 arg1);
 
 #if 0
 const ActorInit Obj_Warpstone_InitVars = {
@@ -37,13 +40,27 @@ static InitChainEntry D_80B9324C[] = {
 };
 
 
+#endif
 extern ColliderCylinderInit D_80B93220;
 extern InitChainEntry D_80B9324C[];
-#endif
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Warpstone_0x80B92B10/func_80B92B10.asm")
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Warpstone_0x80B92B10/ObjWarpstone_Init.asm")
+/*#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Warpstone_0x80B92B10/ObjWarpstone_Init.asm")*/
+
+void ObjWarpstone_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ObjWarpstone* this = THIS;
+
+    Actor_ProcessInitChain(&this->actor, D_80B9324C);
+    Collider_InitAndSetCylinder(globalCtx, &this->collider, &this->actor, &D_80B93220);
+    Actor_SetHeight(&this->actor, 40.0f);
+    if (!IS_OWL_HIT(GET_OWL_ID(this))) {
+        func_80B92B10(&this->actor, func_80B92C00);
+    } else {
+        func_80B92B10(&this->actor, func_80B92DC4);
+        this->unk1AA = 1;
+    }
+}
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_Obj_Warpstone_0x80B92B10/ObjWarpstone_Destroy.asm")
 
--- src/overlays/actors/ovl_Obj_Warpstone/z_obj_warpstone.h
+++ src/overlays/actors/ovl_Obj_Warpstone/z_obj_warpstone.h
@@ -7,9 +7,16 @@ struct ObjWarpstone;
 
 typedef struct ObjWarpstone {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x6C];
+    /* 0x144 */ char unk144[0x18];
+    /* 0x15C */ ColliderCylinder collider; 
+    /* 0x1A8 */ char unk1A8[0x2];
+    /* 0x1AA */ UNK_TYPE1 unk1AA;
+    /* 0x1AC */ UNK_TYPE unk1AC;
 } ObjWarpstone; // size = 0x1B0
 
 extern const ActorInit Obj_Warpstone_InitVars;
+
+#define GET_OWL_ID(this) ((u16)(this->actor.params & 0xF))
+#define IS_OWL_HIT(owlId) (((void)0,gSaveContext.owlsHit) & (u16)gBitFlags[owlId])
 
 #endif // Z_OBJ_WARPSTONE_H
