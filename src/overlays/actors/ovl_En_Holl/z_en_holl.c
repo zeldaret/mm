db543fbc986d1f8a1e3b8ea1b440c165a9ac5949
Rylie <31797144+rylieb@users.noreply.github.com>
2021-08-03 18:06:03 -0600

    EnHoll_Draw() matching and mostly documented.

--- src/overlays/actors/ovl_En_Holl/z_en_holl.c
+++ src/overlays/actors/ovl_En_Holl/z_en_holl.c
@@ -29,6 +29,9 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_F32(uncullZoneDownward, 400, ICHAIN_STOP),
 };
 
+static s32 D_8089A590[] = {
+    0xD7000000, 0xFFFFFFFF, 0xFCFFFFFF, 0xFFFDF638, 0x01004008, &sInitChain, 0x06000204, 0x00000406, 0xDF000000, 0x00000000};
+
 static UNK_PTR D_8089A5B8 = 0;
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Holl_0x80899960/func_80899960.asm")
@@ -72,8 +75,6 @@ void EnHoll_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 
 #pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Holl_0x80899960/func_8089A330.asm")
 
-//#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Holl_0x80899960/EnHoll_Update.asm")
-
 void EnHoll_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnHoll* this = THIS;
     Player* player = PLAYER;
@@ -83,4 +84,32 @@ void EnHoll_Update(Actor* thisx, GlobalContext* globalCtx) {
     }
 }
 
-#pragma GLOBAL_ASM("./asm/non_matchings/overlays/ovl_En_Holl_0x80899960/EnHoll_Draw.asm")
+void EnHoll_Draw(Actor *thisx, GlobalContext *globalCtx) {
+    EnHoll* this = THIS;
+    Gfx* gfxP;
+    u32 dlIndex;
+
+    if (this->opacity != EN_HOLL_INVISIBLE) {
+	OPEN_DISPS(globalCtx->state.gfxCtx);
+        if (this->opacity == EN_HOLL_OPAQUE) {
+            gfxP = POLY_OPA_DISP;
+            dlIndex = 37;
+        } else {
+            gfxP = POLY_XLU_DISP;
+            dlIndex = 0;
+        }
+        gfxP = Gfx_CallSetupDL(gfxP, dlIndex);
+        if (this->alwaysOne == 0) {
+            SysMatrix_InsertYRotation_f(M_PI, MTXMODE_APPLY);
+        }
+	gSPMatrix(gfxP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+	gDPSetPrimColor(gfxP++, 0, 0, 0, 0, 0, this->opacity);
+	gSPDisplayList(gfxP++, D_8089A590);
+        if (this->opacity == EN_HOLL_OPAQUE) {
+            POLY_OPA_DISP = gfxP;
+	} else {
+            POLY_XLU_DISP = gfxP;
+	}
+	CLOSE_DISPS(globalCtx->state.gfxCtx);
+    }
+}
--- src/overlays/actors/ovl_En_Holl/z_en_holl.h
+++ src/overlays/actors/ovl_En_Holl/z_en_holl.h
@@ -1,17 +1,22 @@
 #ifndef Z_EN_HOLL_H
 #define Z_EN_HOLL_H
 
+
 #include <global.h>
 
 struct EnHoll;
 
 typedef void (*EnHollActionFunc)(struct EnHoll* this, GlobalContext* globalCtx);
+typedef enum {
+    /* 0x00 */ EN_HOLL_INVISIBLE,
+    /* 0xFF */ EN_HOLL_OPAQUE = 0xFF
+} EnHollOpacities;
 
 typedef struct EnHoll {
     /* 0x000 */ Actor actor;
     /* 0x144 */ UNK_TYPE1 type;
     /* 0x145 */ u8 opacity;
-    /* 0x146 */ UNK_TYPE1 alwaysOne;
+    /* 0x146 */ u8 alwaysOne;
     /* 0x147 */ UNK_TYPE1 alwaysZero;
     /* 0x148 */ EnHollActionFunc actionFunc;
 } EnHoll; // size = 0x14C
