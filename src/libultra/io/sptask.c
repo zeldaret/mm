d5142ae3617ef0ac5dc723cfcfe12eb6055f3955
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-08-07 22:28:04 +0100

    sptask updated to use libultra macros

--- src/libultra/io/sptask.c
+++ src/libultra/io/sptask.c
@@ -5,7 +5,6 @@
         ptr = (void*)osVirtualToPhysical(ptr); \
     }
 
-
 static OSTask sTmpTask;
 
 OSTask* _VirtualToPhysicalTask(OSTask* intp) {
@@ -24,37 +23,31 @@ OSTask* _VirtualToPhysicalTask(OSTask* intp) {
     return tp;
 }
 
-
 void osSpTaskLoad(OSTask* intp) {
-    OSTask* tp;
+    OSTask* tp = _VirtualToPhysicalTask(intp);
 
-    tp = _VirtualToPhysicalTask(intp);
-
-    if ((tp->t.flags & 0x1) != 0) {
+    if (tp->t.flags & OS_TASK_YIELDED) {
         tp->t.ucode_data = tp->t.yield_data_ptr;
         tp->t.ucode_data_size = tp->t.yield_data_size;
+        intp->t.flags &= ~OS_TASK_YIELDED;
 
-        intp->t.flags = intp->t.flags & 0xFFFFFFFE;
-
-        if ((tp->t.flags & 0x4) != 0) {
-            tp->t.ucode = *(u64**)(((u32)intp->t.yield_data_ptr + 3068) | 0xA0000000);
+        if (tp->t.flags & OS_TASK_LOADABLE) {
+            tp->t.ucode = HW_REG((u32)intp->t.yield_data_ptr + OS_YIELD_DATA_SIZE - 4, u32);
         }
     }
+    osWritebackDCache(tp, sizeof(OSTask));
+    __osSpSetStatus(SP_CLR_SIG0 | SP_CLR_SIG1 | SP_CLR_SIG2 | SP_SET_INTR_BREAK);
 
-    osWritebackDCache((void*)tp, sizeof(OSTask));
-
-    __osSpSetStatus(11008);
-
-    while (__osSpSetPc(0x04001000) == -1) {
+    while (__osSpSetPc((void*)SP_IMEM_START) == -1) {
         ;
     }
-    while (__osSpRawStartDma(1, 0x04000FC0, (void*)tp, sizeof(OSTask)) == -1) {
+    while (__osSpRawStartDma(1, (void*)(SP_IMEM_START - sizeof(*tp)), tp, sizeof(OSTask)) == -1) {
         ;
     }
     while (__osSpDeviceBusy()) {
         ;
     }
-    while (__osSpRawStartDma(1, 0x04001000, (void*)tp->t.ucode_boot, tp->t.ucode_boot_size) == -1) {
+    while (__osSpRawStartDma(1, (void*)SP_IMEM_START, tp->t.ucode_boot, tp->t.ucode_boot_size) == -1) {
         ;
     }
 }
@@ -63,6 +56,5 @@ void osSpTaskStartGo(OSTask* tp) {
     while (__osSpDeviceBusy()) {
         ;
     }
-
-    __osSpSetStatus(293);
+    __osSpSetStatus(SP_SET_INTR_BREAK | SP_CLR_SSTEP | SP_CLR_BROKE | SP_CLR_HALT);
 }
