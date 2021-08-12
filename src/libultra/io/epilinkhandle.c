12b61a887b6605c7ecd238b686c8898746d029bd
Lucas Shaw <lucas.shaw1123@gmail.com>
2021-08-12 17:50:40 -0400

    accidentally put osPfsFreeBlocks in epilinkhandle.c

--- src/libultra/io/epilinkhandle.c
+++ src/libultra/io/epilinkhandle.c
@@ -1,33 +1,12 @@
 #include "global.h"
 
-s32 osPfsFreeBlocks(OSPfs* pfs, s32* leftoverBytes) {
-    s32 j;
-    s32 pages = 0;
-    __OSInode inode;
-    s32 ret = 0;
-    u8 bank;
-    s32 offset;
+s32 osEPiLinkHandle(OSPiHandle* handle) {
+    u32 saveMask = __osDisableInt();
 
-    if (!(pfs->status & PFS_INITIALIZED)) {
-        return (PFS_ERR_INVALID);
-    }
-    if ((ret = __osCheckId(pfs)) != 0) {
-        return ret;
-    }
+    handle->next = __osPiTable;
+    __osPiTable = handle;
 
-    for (bank = PFS_ID_BANK_256K; bank < pfs->banks; bank++) {
-        if ((ret = __osPfsRWInode(pfs, &inode, PFS_READ, bank)) != 0) {
-            return ret;
-        }
+    __osRestoreInt(saveMask);
 
-        offset = ((bank > PFS_ID_BANK_256K) ? 1 : pfs->inodeStartPage);
-        for (j = offset; j < PFS_INODE_SIZE_PER_PAGE; j++) {
-            if (inode.inodePage[j].ipage == PFS_PAGE_NOT_USED) {
-                pages++;
-            }
-        }
-    }
-
-    *leftoverBytes = pages * PFS_ONE_PAGE * BLOCKSIZE;
     return 0;
 }
