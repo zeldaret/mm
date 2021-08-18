fa95f9e6e881457b4a711809a1bf62480cea2c82
EllipticEllipsis <73679967+EllipticEllipsis@users.noreply.github.com>
2021-08-18 16:12:35 +0100

    Migrated a lot of `boot` bss, matched a few NON_MATCHINGs in `boot` (#266)
    * Import bss to boot_main and idle, match last function in idle
    
    * Some pointer cleanup in idle, spec
    
    * Clean up idle, add build.c, add bss to CIC6105
    
    * Import rand bss
    
    * sptask bss and a macro
    
    * sptask updated to use libultra macros
    
    * siacs.c bss
    
    * Macros in CIC and idle
    
    * controller bss
    
    * seteventmesg bss
    
    * spec
    
    * pimgr and piacs bss
    
    * initialize OK, threadsave, initialize, pimgr bss
    
    * Match DmaMgr_Start
    
    * Fix bss reorder using separate file
    
    * Small cleanup of hex/dec in buffer sizes
    
    * Add comment, remove unused bss from spec
    
    * Merge remote-tracking branch 'upstream/master' into boot_bss
    
    * Format
    
    * Delete files
    
    * remove commented-out structs
    
    * Bit more cleanup
    
    * Addressed reviews
    
    * Format

--- include/z64.h
+++ include/z64.h
@@ -390,13 +390,6 @@ typedef struct {
     /* 0xC */ s32 rightX;
 } Viewport; // size = 0x10
 
-typedef struct {
-    /* 0x0 */ unsigned int inst1;
-    /* 0x4 */ unsigned int inst2;
-    /* 0x8 */ unsigned int inst3;
-    /* 0xC */ unsigned int inst4;
-} __osExceptionVector; // size = 0x10
-
 typedef void*(*fault_address_converter_func)(void* addr, void* arg);
 
 typedef void(*fault_client_func)(void* arg1, void* arg2);
