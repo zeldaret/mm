57528dd548a213cac8f416f00e2753e82ddb97d3
Rylie <31797144+rylieb@users.noreply.github.com>
2021-08-21 20:30:51 -0600

    Renamed Vtx and DL.

--- src/overlays/actors/ovl_En_Holl/z_en_holl.c
+++ src/overlays/actors/ovl_En_Holl/z_en_holl.c
@@ -331,7 +331,7 @@ void EnHoll_Draw(Actor* thisx, GlobalContext* globalCtx) {
         }
         gSPMatrix(dl++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gDPSetPrimColor(dl++, 0, 0, 0, 0, 0, this->alpha);
-        gSPDisplayList(dl++, gEnHollCentralRectangleDL);
+        gSPDisplayList(dl++, gEnHollCentralPlaneDL);
         if (this->alpha == 255) {
             POLY_OPA_DISP = dl;
         } else {
