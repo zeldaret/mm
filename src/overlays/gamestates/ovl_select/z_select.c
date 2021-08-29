c9c1aeff5470fdf4ad79e780402c507f011b78ed
angie <angheloalf95@gmail.com>
2021-08-29 00:35:07 -0400

    basic functions

--- src/overlays/gamestates/ovl_select/z_select.c
+++ src/overlays/gamestates/ovl_select/z_select.c
@@ -12,6 +12,7 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/func_80800930.s")
 
+void func_80800A44(SelectContext* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/func_80800A44.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/func_808013B8.s")
@@ -30,11 +31,61 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/func_808019FC.s")
 
+void func_80801A64(SelectContext* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/func_80801A64.s")
 
-void Select_Main(GameState* thisx);
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/Select_Main.s")
+void Select_Main(GameState* thisx) {
+    SelectContext* this = (SelectContext*)thisx;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/Select_Destroy.s")
+    func_80800A44(this);
+    func_80801A64(this);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_select/Select_Init.s")
+void Select_Destroy(GameState* thisx) {
+    ShrinkWindow_Fini();
+}
+
+extern UNK_TYPE D_80801C80[];
+
+void Select_Init(GameState* thisx) {
+    SelectContext* this = (SelectContext*)thisx;
+
+    this->state.main = Select_Main;
+    this->state.destroy = Select_Destroy;
+
+    this->unk_214 = &D_80801C80;
+    this->unk_24C = 0;
+    this->unk_218 = 0;
+    this->unk_220 = 0;
+    this->unk_224 = 0x13;
+    this->unk_228 = 0x25;
+    this->unk_22C = 0x33;
+    this->unk_230 = 0x3B;
+    this->unk_234 = 0x49;
+    this->unk_238 = 0x5B;
+    this->unk_21C = 0;
+    this->unk_248 = 0;
+    this->unk_210 = 0x8F;
+
+    ShrinkWindow_Init();
+    View_Init(&this->view, this->state.gfxCtx);
+    this->view.flags = 0xA;
+    this->unk_25C = 0;
+    this->unk_260 = 0;
+    this->unk_264 = 0;
+    this->unk_268 = 0;
+    this->unk_26C = 0;
+    this->unk_270 = 0;
+    this->unk_274 = 0;
+
+    if ((dREG(80) >= 0) && (dREG(80) < this->unk_210)) {
+        this->unk_218 = dREG(80);
+        this->unk_24C = dREG(81);
+        this->unk_21C = dREG(82);
+    }
+
+    Game_SetFramerateDivisor(this, 1);
+    gSaveContext.cutscene = 0;
+    gSaveContext.playerForm = PLAYER_FORM_HUMAN;
+    gSaveContext.linkAge = 0;
+}
--- src/overlays/gamestates/ovl_select/z_select.h
+++ src/overlays/gamestates/ovl_select/z_select.h
@@ -7,8 +7,37 @@ void Select_Init(GameState* thisx);
 void Select_Destroy(GameState* thisx);
 
 typedef struct {
-    GameState state;
-    char unk_A4[0x1DC];
+    /* 0x000 */ GameState state;
+    /* 0x0A4 */ char unk_0A4[0x4];
+    /* 0x0A8 */ View view;
+    /* 0x210 */ UNK_TYPE4 unk_210;
+    /* 0x214 */ UNK_TYPE4 unk_214;
+    /* 0x218 */ UNK_TYPE4 unk_218;
+    /* 0x21C */ UNK_TYPE4 unk_21C;
+    /* 0x220 */ UNK_TYPE4 unk_220;
+    /* 0x224 */ UNK_TYPE4 unk_224;
+    /* 0x228 */ UNK_TYPE4 unk_228;
+    /* 0x22C */ UNK_TYPE4 unk_22C;
+    /* 0x230 */ UNK_TYPE4 unk_230;
+    /* 0x234 */ UNK_TYPE4 unk_234;
+    /* 0x238 */ UNK_TYPE4 unk_238;
+    /* 0x23C */ UNK_TYPE4 unk_23C;
+    /* 0x240 */ UNK_TYPE4 unk_240;
+    /* 0x244 */ UNK_TYPE4 unk_244;
+    /* 0x248 */ UNK_TYPE4 unk_248;
+    /* 0x24C */ UNK_TYPE4 unk_24C;
+    /* 0x250 */ UNK_TYPE4 unk_250;
+    /* 0x254 */ UNK_TYPE4 unk_254;
+    /* 0x258 */ UNK_TYPE4 unk_258;
+    /* 0x25C */ UNK_TYPE4 unk_25C;
+    /* 0x260 */ UNK_TYPE4 unk_260;
+    /* 0x264 */ UNK_TYPE4 unk_264;
+    /* 0x268 */ UNK_TYPE4 unk_268;
+    /* 0x26C */ UNK_TYPE4 unk_26C;
+    /* 0x270 */ UNK_TYPE4 unk_270;
+    /* 0x274 */ UNK_TYPE4 unk_274;
+    /* 0x278 */ UNK_TYPE4 unk_278;
+    /* 0x27C */ UNK_TYPE4 unk_27C;
 } SelectContext; // size = 0x280
 
 #endif
