7d7313519b374a0c1e7701e4ec4094850238f6f6
Rylie <31797144+rylieb@users.noreply.github.com>
2021-09-08 12:58:58 -0600

    Obj_Syokudai OK, no new warnings, ran formatter.

--- spec
+++ spec
@@ -1076,7 +1076,7 @@ beginseg
     name "ovl_Obj_Syokudai"
     compress
     include "build/src/overlays/actors/ovl_Obj_Syokudai/z_obj_syokudai.o"
-    include "build/data/ovl_Obj_Syokudai/ovl_Obj_Syokudai.reloc.o"
+    include "build/src/overlays/actors/ovl_Obj_Syokudai/ovl_Obj_Syokudai_reloc.o"
 endseg
 
 beginseg
--- src/overlays/actors/ovl_Obj_Syokudai/z_obj_syokudai.c
+++ src/overlays/actors/ovl_Obj_Syokudai/z_obj_syokudai.c
@@ -95,7 +95,7 @@ void ObjSyokudai_Init(Actor* thisx, GlobalContext* globalCtx) {
                             0xFF, 0xB4, -1);
     this->lightNode = LightContext_InsertLight(globalCtx, &globalCtx->lightCtx, &this->lightInfo);
 
-    if (thisx->params & 0x800 || (paramsHigh != 2 || paramsLow != 0x7F) && Flags_GetSwitch(globalCtx, paramsLow)) {
+    if (thisx->params & 0x800 || ((paramsHigh != 2 || paramsLow != 0x7F) && Flags_GetSwitch(globalCtx, paramsLow))) {
         s32 paramsMid = OBJ_SYOKUDAI_GET_PARAMS_MID(thisx);
         this->unk_1DC = -1;
         if (paramsMid != 0) {
@@ -119,15 +119,16 @@ void ObjSyokudai_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
     GlobalContext* globalCtx = globalCtx2;
     ObjSyokudai* this = THIS;
-    s32 pad0[1];
+    s32 paramsMid = OBJ_SYOKUDAI_GET_PARAMS_MID(thisx);
     s32 paramsLow = OBJ_SYOKUDAI_GET_PARAMS_LOW(thisx);
     s32 paramsHigh = OBJ_SYOKUDAI_GET_PARAMS_HIGH(thisx);
-    s32 paramsMid = OBJ_SYOKUDAI_GET_PARAMS_MID(thisx);
+    s32 pad0;
     f32 sp64;
     f32 sp60;
     s32 sp5C = -1;
     u8 sp5B = 0;
-    s32 pad1[2];
+    Player* player;
+    s32 pad1;
 
     if (this->unk1DF != 0) {
         if (ActorCutscene_GetCurrentIndex() != thisx->cutscene) {
@@ -144,7 +145,8 @@ void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
             this->unk1DF = 0;
         }
     } else {
-        if (func_800CA1E8(globalCtx, &globalCtx->colCtx, thisx->world.pos.x, thisx->world.pos.z, &sp60, &sp64) && ((sp60 - thisx->world.pos.y) > 52.0f)) {
+        if (func_800CA1E8(globalCtx, &globalCtx->colCtx, thisx->world.pos.x, thisx->world.pos.z, &sp60, &sp64) &&
+            ((sp60 - thisx->world.pos.y) > 52.0f)) {
             this->unk_1DC = 0;
             if (paramsHigh == 1) {
                 Actor_UnsetSwitchFlag(globalCtx, paramsLow);
@@ -153,9 +155,9 @@ void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
                 }
             }
         } else {
-            u32 collider2HurtboxDmgFlags = 0;
             s32 phi_t0 = 0;
-            Player* player = PLAYER;
+            u32 collider2HurtboxDmgFlags = 0;
+            player = PLAYER;
 
             if (thisx->params & 0x800) {
                 this->unk_1DC = -1;
@@ -175,13 +177,14 @@ void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
                     this->unk_1DC = 0x14;
                 }
             }
-            if ((this->colliderCylinder2.base.acFlags & 2) != 0) {
+            if (this->colliderCylinder2.base.acFlags & 2) {
                 collider2HurtboxDmgFlags = this->colliderCylinder2.info.acHitInfo->toucher.dmgFlags;
                 if ((this->colliderCylinder2.info.acHitInfo->toucher.dmgFlags & 0x820) != 0) {
                     phi_t0 = 1;
                 }
             } else if (player->itemActionParam == 7) {
                 Vec3f posDiffFromSword;
+
                 Math_Vec3f_Diff(&player->swordInfo[0].tip, &thisx->world.pos, &posDiffFromSword);
                 posDiffFromSword.y -= 67.0f;
                 if (SQXYZ(posDiffFromSword) < 400.0f) {
@@ -209,7 +212,8 @@ void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
                             this->unk_1DC = (paramsMid * 50) + 100;
                         }
                     }
-                } else if ((paramsHigh != 0) && (((phi_t0 > 0) && ((collider2HurtboxDmgFlags & 0x800) != 0)) || ((phi_t0 < 0) && (player->unk_B28 != 0)))) {
+                } else if ((paramsHigh != 0) && (((phi_t0 > 0) && ((collider2HurtboxDmgFlags & 0x800) != 0)) ||
+                                                 ((phi_t0 < 0) && (player->unk_B28 != 0)))) {
                     if ((phi_t0 < 0) && (player->unk_B28 < 0xC8)) {
                         player->unk_B28 = 0xC8;
                     }
@@ -243,18 +247,20 @@ void ObjSyokudai_Update(Actor* thisx, GlobalContext* globalCtx2) {
         D_808BCDE0--;
     }
     if (this->unk_1DC != 0) {
+        s32 pad2;
+
         if ((this->unk_1DC < 0) || (this->unk_1DC >= 20)) {
             sp5C = 0xFA;
         } else {
             sp5C = ((this->unk_1DC * 250.0f) / 20.0f);
         }
-        sp5B = (u32) (Rand_ZeroOne() * 0x7F) + 0x80;
+        sp5B = Rand_ZeroOne() * 0x7F;
+        sp5B += 0x80;
         func_800B9010(thisx, 0x2031);
     }
-    Lights_PointSetColorAndRadius(&this->lightInfo, sp5B, (u32) (sp5B * 0.7f) & 0xFF, 0, sp5C);
+    Lights_PointSetColorAndRadius(&this->lightInfo, sp5B, sp5B * 0.7f, 0, sp5C);
     this->unk_1DE++;
 }
-
 
 void ObjSyokudai_Draw(Actor* thisx, GlobalContext* globalCtx) {
     ObjSyokudai* this = THIS;
@@ -277,14 +283,16 @@ void ObjSyokudai_Draw(Actor* thisx, GlobalContext* globalCtx) {
         }
         scaleFactor *= 0.0027f;
         func_8012C2DC(globalCtx->state.gfxCtx);
-	gSPSegment(POLY_XLU_DISP++, 0x08, Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 0, 0, 0x20, 0x40, 1, 0, (this->unk_1DE * -20) & 0x1FF, 0x20, 0x80));
-	gDPSetPrimColor(POLY_XLU_DISP++, 0x80, 0x80, 255, 255, 0, 255);
-	gDPSetEnvColor(POLY_XLU_DISP++, 255, 0, 0, 0);
+        gSPSegment(POLY_XLU_DISP++, 0x08,
+                   Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 0, 0, 0x20, 0x40, 1, 0, (this->unk_1DE * -20) & 0x1FF,
+                                    0x20, 0x80));
+        gDPSetPrimColor(POLY_XLU_DISP++, 0x80, 0x80, 255, 255, 0, 255);
+        gDPSetEnvColor(POLY_XLU_DISP++, 255, 0, 0, 0);
         SysMatrix_InsertTranslation(0.0f, 52.0f, 0.0f, MTXMODE_APPLY);
         Matrix_RotateY(BINANG_ROT180(func_800DFCDC(ACTIVE_CAM) - thisx->shape.rot.y), MTXMODE_APPLY);
         Matrix_Scale(scaleFactor, scaleFactor, scaleFactor, MTXMODE_APPLY);
-	gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-	gSPDisplayList(POLY_XLU_DISP++, D_0407D590);
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPDisplayList(POLY_XLU_DISP++, D_0407D590);
     }
     CLOSE_DISPS(globalCtx->state.gfxCtx);
 }
