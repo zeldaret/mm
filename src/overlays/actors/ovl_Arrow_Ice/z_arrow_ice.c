359e8afe963c0bd136511d2b75bc0ef5bdf9cbe0
tom-overton <tom.overton@outlook.com>
2021-09-16 19:48:11 -0700

    Use decrement operation

--- src/overlays/actors/ovl_Arrow_Ice/z_arrow_ice.c
+++ src/overlays/actors/ovl_Arrow_Ice/z_arrow_ice.c
@@ -106,7 +106,7 @@ void ArrowIce_Hit(ArrowIce* this, GlobalContext* globalCtx) {
 
     timer = this->timer;
     if (timer != 0) {
-        this->timer -= 1;
+        this->timer--;
 
         if (this->timer >= 8) {
             offset = ((this->timer - 8) * (1.0f / 24.0f));
