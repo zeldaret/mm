ed0c85284c38af37c9a5dd437995f6bc0e32d267
angie <angheloalf95@gmail.com>
2021-09-19 15:24:27 -0300

    func_800F470C

--- include/z64.h
+++ include/z64.h
@@ -1765,4 +1765,80 @@ typedef struct {
     /* 0x20 */ u16 fracPart[4][4];
 } MatrixInternal; // size = 0x40
 
+
+
+typedef struct {
+    /* 0x00 */ u16 table[8*8];
+} JpegQuantizationTable; // size = 0x80
+
+typedef struct {
+    /* 0x00 */ u8 codeOffs[16];
+    /* 0x10 */ u16 codesA[16];
+    /* 0x30 */ u16 codesB[16];
+    /* 0x50 */ u8* symbols;
+} JpegHuffmanTable; // size = 0x54
+
+// this struct might be unaccurate but it's not used outside jpegutils.c anyways
+typedef struct {
+    /* 0x000 */ u8 codeOffs[16];
+    /* 0x010 */ u16 dcCodes[120];
+    /* 0x100 */ u16 acCodes[256];
+} JpegHuffmanTableOld; // size = 0x300
+
+typedef struct {
+    /* 0x00 */ u32 address;
+    /* 0x04 */ u32 mbCount;
+    /* 0x08 */ u32 mode;
+    /* 0x0C */ u32 qTableYPtr;
+    /* 0x10 */ u32 qTableUPtr;
+    /* 0x14 */ u32 qTableVPtr;
+    /* 0x18 */ char unk_18[0x8];
+} JpegTaskData; // size = 0x20
+
+typedef struct {
+    /* 0x000 */ JpegTaskData taskData;
+    /* 0x020 */ char yieldData[0x200];
+    /* 0x220 */ JpegQuantizationTable qTableY;
+    /* 0x2A0 */ JpegQuantizationTable qTableU;
+    /* 0x320 */ JpegQuantizationTable qTableV;
+    /* 0x3A0 */ u8 codesLengths[0x110];
+    /* 0x4B0 */ u16 codes[0x108];
+    /* 0x6C0 */ u16 data[4][0x180];
+} JpegWork; // size = 0x12C0
+
+typedef struct {
+    /* 0x00 */ void* imageData;
+    /* 0x04 */ u8 mode;
+    /* 0x05 */ u8 unk_05;
+    /* 0x08 */ JpegHuffmanTable* hTablePtrs[4];
+    /* 0x18 */ u8 unk_18;
+} JpegDecoder; // size = 0x1C
+
+typedef struct {
+    /* 0x00 */ u8 dqtCount;
+    /* 0x04 */ u8* dqtPtr[3];
+    /* 0x10 */ u8 dhtCount;
+    /* 0x14 */ u8* dhtPtr[4];
+    /* 0x24 */ void* imageData;
+    /* 0x28 */ u8 mode; // 0 if Y V0 is 1 and 2 if Y V0 is 2
+    ///* 0x29 */ char unk_29[3];
+    ///* 0x2C */ char unk_2C[4];
+    /* 0x30 */ OSScTask scTask;
+    ///* 0x88 */ char unk_88[0x10];
+    /* 0x98 */ OSMesgQueue mq;
+    /* 0xB0 */ OSMesg msg;
+    /* 0xB4 */ JpegWork* workBuf;
+} JpegContext; // size = 0xB8
+
+typedef struct {
+    /* 0x00 */ u32 byteIdx;
+    /* 0x04 */ u8 bitIdx;
+    /* 0x05 */ u8 dontSkip;
+    /* 0x08 */ u32 curWord;
+    /* 0x0C */ s16 unk_0C;
+    /* 0x0E */ s16 unk_0E;
+    /* 0x10 */ s16 unk_10;
+} JpegDecoderState; // size = 0x14
+
+
 #endif
--- src/code/z_jpeg.c
+++ src/code/z_jpeg.c
@@ -1,6 +1,20 @@
 #include "global.h"
-#include "z64jpeg.h"
 #include "vt.h"
+
+#define MARKER_ESCAPE 0x00
+#define MARKER_SOI 0xD8
+#define MARKER_SOF 0xC0
+#define MARKER_DHT 0xC4
+#define MARKER_DQT 0xDB
+#define MARKER_DRI 0xDD
+#define MARKER_SOS 0xDA
+#define MARKER_APP0 0xE0
+#define MARKER_APP1 0xE1
+#define MARKER_APP2 0xE2
+#define MARKER_COM 0xFE
+#define MARKER_EOI 0xD9
+
+void func_800F42A0(JpegContext* ctx);
 
 #if 0
 extern OSTask_t sJpegTask;
@@ -56,7 +70,6 @@ void func_800F42A0(JpegContext* ctx) {
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_jpeg/func_800F42A0.s")
 #endif
 
-//#pragma GLOBAL_ASM("asm/non_matchings/code/z_jpeg/func_800F43BC.s")
 // Jpeg_CopyToZbuffer
 void func_800F43BC(u16* src, u16* zbuffer, s32 x, s32 y) {
     u16* dst = zbuffer + (((y * SCREEN_WIDTH) + x) * 16);
@@ -87,7 +100,6 @@ void func_800F43BC(u16* src, u16* zbuffer, s32 x, s32 y) {
 
 
 #define Jpeg_GetUnalignedU16 func_800F44F4
-//#pragma GLOBAL_ASM("asm/non_matchings/code/z_jpeg/func_800F44F4.s")
 u16 func_800F44F4(u8* ptr) {
     if (((u32)ptr & 1) == 0) {
         // Read the value normally if it's aligned to a 16-bit address.
@@ -216,246 +228,75 @@ void func_800F4540(u8* ptr, JpegContext* ctx) {
     }
 }
 
-
-#if 0
-// #ifdef MIPS2C_OUTPUT
-// ? func_800F42A0(u8*); // extern
-// ? func_800F43BC(s32, ?, s32, s32); // extern
-// ? func_800F4540(s32, u8*); // extern
-// ? func_8010C1B0(); // extern
-// ? func_801A9B10(s32, s32, ?); // extern
-// s32 func_801A9DCC(s32, ?*, s32, s32, s32); // extern
-// s32 func_801AA020(s32*, s32, ?, s32, ?*); // extern
-
-s32 func_800F470C(s32 arg0, void* arg1, s32 arg2, u32 arg3) {
-    // s32 sp27C;
-    // void* sp278;
-    // OSMesgQueue sp260;
-    // u8 sp1D8;
-
-    JpegContext ctx;
-
-    // UNK_TYPE1 sp184[0x54];
-    // UNK_TYPE1 sp130[0x54];
-    // UNK_TYPE1 spDC[0x54];
-    // UNK_TYPE1 sp88[0x54];
-    JpegHuffmanTable hTables[4];
-
-    // s8 sp84;
-    // UNK_PTR sp80;
-    // UNK_PTR sp7C;
-    // UNK_PTR sp78;
-    // UNK_PTR sp74;
-    // s8 sp71;
-    // u8 sp70;
-    JpegDecoder decoder;
-
-    s32 sp6C;
-    UNK_TYPE1 sp58[0x14];
-    s32 sp44;
-    s32 temp_s0;
-    s32 temp_s0_2;
-    s32 temp_s1;
-    s32 temp_s2;
-    s32 temp_s6;
-    s32 phi_s6;
-    s32 phi_s1;
-    s32 phi_s2;
-    s32 phi_s3;
-    s32 phi_s0;
-    s32 phi_s2_2;
-    s32 phi_s2_3;
-    s32 phi_s3_2;
-    s32 phi_s3_3;
-
-    if (arg3 < 0x12C0U) {
-        return -1;
-    }
-
-    //osCreateMesgQueue(&sp260, &sp278, 1);
-    osCreateMesgQueue(&ctx.mq, &ctx.msg, 1);
-
-    func_8010C1B0();
-    sp27C = arg2;
-    func_800F4540(arg0, &sp1D8);
-    if (sp1D8 != 1) {
-        if (sp1D8 != 2) {
-            if (sp1D8 != 3) {
-                return -1;
-            }
-            func_801A9B10(sp1DC, arg2 + 0x220, 1);
-            func_801A9B10(sp1E0, arg2 + 0x2A0, 1);
-            func_801A9B10(sp1E4, arg2 + 0x320, 1);
-            goto block_10;
-        }
-        func_801A9B10(sp1DC, arg2 + 0x220, 1);
-        func_801A9B10(sp1E0, arg2 + 0x2A0, 1);
-        func_801A9B10(sp1E0, arg2 + 0x320, 1);
-        goto block_10;
-    }
-    func_801A9B10(sp1DC, arg2 + 0x220, 3);
-block_10:
-    if (sp1E8 != 1) {
-        if (sp1E8 != 4) {
-            return -1;
-        }
-        temp_s0 = arg2 + 0x3A0;
-        temp_s1 = arg2 + 0x4B0;
-        if ((func_801A9DCC(sp1EC, &sp88, temp_s0, temp_s1, 1) != 0) || (func_801A9DCC(sp1F0, &spDC, temp_s0, temp_s1, 1) != 0) || (func_801A9DCC(sp1F4, &sp130, temp_s0, temp_s1, 1) != 0) || (func_801A9DCC(sp1F8, &sp184, temp_s0, temp_s1, 1) != 0)) {
-            return -1;
-        }
-        goto block_21;
-    }
-    if (func_801A9DCC(sp1EC, &sp88, arg2 + 0x3A0, arg2 + 0x4B0, 4) != 0) {
-        return -1;
-    }
-block_21:
-    sp74 = &sp88;
-    sp71 = 2;
-    sp78 = &spDC;
-    sp7C = &sp130;
-    sp80 = &sp184;
-    sp84 = 0;
-    sp44 = arg2 + 0x6C0;
-    sp6C = sp1FC;
-    sp70 = sp200;
-    phi_s6 = 0;
-    phi_s2_2 = 0;
-    phi_s2_3 = 0;
-    phi_s3_2 = 0;
-    do {
-        phi_s2 = phi_s2_2;
-        phi_s3 = phi_s3_2;
-        if (func_801AA020(&sp6C, sp44, 4, (phi_s6 != 0) & 0xFF, &sp58) == 0) {
-            func_800F42A0(&sp1D8);
-            phi_s1 = arg2 + 0x6C0;
-            phi_s0 = 0;
-            do {
-                func_800F43BC(phi_s1, arg1, phi_s2, phi_s3);
-                temp_s2 = phi_s2 + 1;
-                temp_s0_2 = phi_s0 + 0x300;
-                phi_s0 = temp_s0_2;
-                phi_s2_3 = temp_s2;
-                phi_s3_3 = phi_s3;
-                if (temp_s2 >= 0x14) {
-                    phi_s3_3 = phi_s3 + 1;
-                }
-                phi_s1 += 0x300;
-                phi_s2 = phi_s2_3;
-                phi_s3 = phi_s3_3;
-                phi_s2_2 = phi_s2_3;
-                phi_s3_2 = phi_s3_3;
-            } while (temp_s0_2 != 0xC00);
-        }
-        temp_s6 = phi_s6 + 4;
-        phi_s6 = temp_s6;
-    } while (temp_s6 != 0x12C);
-    return 0;
-}
-//#else
-//#pragma GLOBAL_ASM("asm/non_matchings/code/z_jpeg/func_800F470C.s")
-//#endif
-#endif
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_jpeg/func_800F470C.s")
-
-#if 0
-// Jpeg_Decode
 s32 func_800F470C(void* data, void* zbuffer, void* work, u32 workSize) {
     s32 y;
     s32 x;
-    u32 j;
-    u32 i;
+    s32 j;
+    s32 i;
     JpegContext ctx;
     JpegHuffmanTable hTables[4];
     JpegDecoder decoder;
     JpegDecoderState state;
-    JpegWork* workBuff;
-    // OSTime diff;
-    // OSTime time;
-    // OSTime curTime;
+    JpegWork* workBuff = work;
 
-    workBuff = work;
-
-    //time = osGetTime();
-    // (?) I guess MB_SIZE=0x180, PROC_OF_MBS=5 which means data is not a part of JpegWork
-    // ASSERT(workSize >= sizeof(JpegWork), "worksize >= sizeof(JPEGWork) + MB_SIZE * (PROC_OF_MBS - 1)", "../z_jpeg.c",
-    //        527);
+    if (workSize < sizeof(JpegWork)) {
+        return -1;
+    }
 
     osCreateMesgQueue(&ctx.mq, &ctx.msg, 1);
-    MsgEvent_SendNullTask();
-
-    //curTime = osGetTime();
-    //diff = curTime - time;
-    //time = curTime;
-    // "Wait for synchronization of fifo buffer"
-    // osSyncPrintf("*** fifoバッファの同期待ち time = %6.3f ms ***\n", OS_CYCLES_TO_USEC(diff) / 1000.0f);
+    // MsgEvent_SendNullTask
+    func_8010C1B0();
 
     ctx.workBuf = workBuff;
-    Jpeg_ParseMarkers(data, &ctx);
 
-    // curTime = osGetTime();
-    // diff = curTime - time;
-    // time = curTime;
-    // "Check markers for each segment"
-    // osSyncPrintf("*** 各セグメントのマーカーのチェック time = %6.3f ms ***\n", OS_CYCLES_TO_USEC(diff) / 1000.0f);
+    func_800F4540(data, &ctx);
 
     switch (ctx.dqtCount) {
         case 1:
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[0], &workBuff->qTableY, 3);
+            func_801A9B10(ctx.dqtPtr[0], &workBuff->qTableY, 3);
             break;
+
         case 2:
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[0], &workBuff->qTableY, 1);
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[1], &workBuff->qTableU, 1);
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[1], &workBuff->qTableV, 1);
+            func_801A9B10(ctx.dqtPtr[0], &workBuff->qTableY, 1);
+            func_801A9B10(ctx.dqtPtr[1], &workBuff->qTableU, 1);
+            func_801A9B10(ctx.dqtPtr[1], &workBuff->qTableV, 1);
             break;
+
         case 3:
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[0], &workBuff->qTableY, 1);
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[1], &workBuff->qTableU, 1);
-            JpegUtils_ProcessQuantizationTable(ctx.dqtPtr[2], &workBuff->qTableV, 1);
+            func_801A9B10(ctx.dqtPtr[0], &workBuff->qTableY, 1);
+            func_801A9B10(ctx.dqtPtr[1], &workBuff->qTableU, 1);
+            func_801A9B10(ctx.dqtPtr[2], &workBuff->qTableV, 1);
             break;
+
+
         default:
             return -1;
     }
-
-    // curTime = osGetTime();
-    // diff = curTime - time;
-    // time = curTime;
-    // "Create quantization table"
-    //osSyncPrintf("*** 量子化テーブル作成 time = %6.3f ms ***\n", OS_CYCLES_TO_USEC(diff) / 1000.0f);
 
     switch (ctx.dhtCount) {
         case 1:
-            if (JpegUtils_ProcessHuffmanTable(ctx.dhtPtr[0], &hTables[0], workBuff->codesLengths, workBuff->codes, 4)) {
-                //osSyncPrintf("Error : Cant' make huffman table.\n");
+            if (func_801A9DCC(ctx.dhtPtr[0], &hTables[0], workBuff->codesLengths, workBuff->codes, 4) != 0) {
+                return -1;
             }
             break;
+
         case 4:
-            if (JpegUtils_ProcessHuffmanTable(ctx.dhtPtr[0], &hTables[0], workBuff->codesLengths, workBuff->codes, 1)) {
-                //osSyncPrintf("Error : Cant' make huffman table.\n");
+            if (func_801A9DCC(ctx.dhtPtr[0], &hTables[0], workBuff->codesLengths, workBuff->codes, 1)) {
+            } else if (func_801A9DCC(ctx.dhtPtr[1], &hTables[1], workBuff->codesLengths, workBuff->codes, 1)){
+            } else if (func_801A9DCC(ctx.dhtPtr[2], &hTables[2], workBuff->codesLengths, workBuff->codes, 1)) {
+            } else if (func_801A9DCC(ctx.dhtPtr[3], &hTables[3], workBuff->codesLengths, workBuff->codes, 1)==0) {
+                break;
             }
-            if (JpegUtils_ProcessHuffmanTable(ctx.dhtPtr[1], &hTables[1], workBuff->codesLengths, workBuff->codes, 1)) {
-                //osSyncPrintf("Error : Cant' make huffman table.\n");
-            }
-            if (JpegUtils_ProcessHuffmanTable(ctx.dhtPtr[2], &hTables[2], workBuff->codesLengths, workBuff->codes, 1)) {
-                //osSyncPrintf("Error : Cant' make huffman table.\n");
-            }
-            if (JpegUtils_ProcessHuffmanTable(ctx.dhtPtr[3], &hTables[3], workBuff->codesLengths, workBuff->codes, 1)) {
-                //osSyncPrintf("Error : Cant' make huffman table.\n");
-            }
-            break;
+            return -1;
+
         default:
             return -1;
     }
-
-    // curTime = osGetTime();
-    // diff = curTime - time;
-    // time = curTime;
-    // "Huffman table creation"
-    // osSyncPrintf("*** ハフマンテーブル作成 time = %6.3f ms ***\n", OS_CYCLES_TO_USEC(diff) / 1000.0f);
 
     decoder.imageData = ctx.imageData;
     decoder.mode = ctx.mode;
     decoder.unk_05 = 2;
+
     decoder.hTablePtrs[0] = &hTables[0];
     decoder.hTablePtrs[1] = &hTables[1];
     decoder.hTablePtrs[2] = &hTables[2];
@@ -463,33 +304,24 @@ s32 func_800F470C(void* data, void* zbuffer, void* work, u32 workSize) {
     decoder.unk_18 = 0;
 
     x = y = 0;
-    for (i = 0; i < 300; i += 4) {
-        if (JpegDecoder_Decode(&decoder, (u16*)workBuff->data, 4, i != 0, &state)) {
-            //osSyncPrintf(VT_FGCOL(RED));
-            //osSyncPrintf("Error : Can't decode jpeg\n");
-            //osSyncPrintf(VT_RST);
-        } else {
-            Jpeg_ScheduleDecoderTask(&ctx);
-            // osInvalDCache(&workBuff->data, sizeof(workBuff->data[0]));
+    for (i = 0; i < 300; i+=4) {
 
-            for (j = 0; j < ARRAY_COUNT(workBuff->data); j++) {
-                Jpeg_CopyToZbuffer(workBuff->data[j], zbuffer, x, y);
-                x++;
+        if (!func_801AA020(&decoder, (u16*)workBuff->data, 4, (i != 0), &state)) {
+            func_800F42A0(&ctx);
 
-                if (x >= 20) {
-                    x = 0;
-                    y++;
-                }
-            }
+            for (j = 0; j < 4; j++) {
+               func_800F43BC(workBuff->data[j], zbuffer, x, y);
+               x++;
+
+               if (x >= 20) {
+                   x = 0;
+                   y++;
+               }
+           }
         }
     }
 
-    //curTime = osGetTime();
-    //diff = curTime - time;
-    //time = curTime;
-    // "Unfold & draw"
-    //osSyncPrintf("*** 展開 & 描画 time = %6.3f ms ***\n", OS_CYCLES_TO_USEC(diff) / 1000.0f);
-
     return 0;
 }
-#endif
+
+
