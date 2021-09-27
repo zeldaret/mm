a3c39f54f68de3355d1096a54263dd6272d438a5
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-09-27 16:33:49 +0100

    Update OK

--- src/code/flg_set.c
+++ src/code/flg_set.c
@@ -112,125 +112,127 @@ FlagSetEntry D_801AE8F0[] = {
     { &gSaveContext.maskMaskBit[0], "mask_mask_bit[0]" },
     { &gSaveContext.maskMaskBit[1], "mask_mask_bit[1]" },
     { &gSaveContext.maskMaskBit[2], "mask_mask_bit[2]" },
-    { 0, 0 },
+    { NULL, "" }, // used in the code
 };
 
-s32 D_801AEC70 = 0;
-u32 D_801AEC74 = 0;
-s32 D_801AEC78 = 0;
-#pragma GLOBAL_ASM("asm/non_matchings/code/flg_set/func_800B32D0.s")
+s32 D_801AEC70 = 0; // entryIdx
+u32 D_801AEC74 = 0; // curBit
+s32 D_801AEC78 = 0; // timer
 
-// void func_800B32D0(GameState* gameState) {
-//     GlobalContext* globalCtx = (GlobalContext*)gameState;
-//     Input* input = &globalCtx->state.input[0];
+void func_800B32D0(GameState* gameState) {
 
-//     if (CHECK_BTN_ALL(input->press.button, BTN_DLEFT)) {
-//         D_801AEC74++;
-//         D_801AEC78 = 10;
-//     }
+    GlobalContext* globalCtx = (GlobalContext*)gameState;
+    Input* input = &globalCtx->state.input[0];
 
-//     if (CHECK_BTN_ALL(input->press.button, BTN_DRIGHT)) {
-//         D_801AEC78 = 10;
-//         D_801AEC74--;
-//     }
+    if (CHECK_BTN_ALL(input->press.button, BTN_DLEFT)) {
+        D_801AEC74++;
+        D_801AEC78 = 10;
+    }
 
-//     if (D_801AEC78 == 0) {
-//         if (CHECK_BTN_ALL(input->cur.button, BTN_DLEFT)) {
-//             D_801AEC78 = 2;
-//             D_801AEC74++;
-//         }
-//         if (CHECK_BTN_ALL(input->cur.button, BTN_DRIGHT)) {
-//             D_801AEC78 = 2;
-//             D_801AEC74--;
-//         }
-//     }
+    if (CHECK_BTN_ALL(input->press.button, BTN_DRIGHT)) {
+        D_801AEC74--;
+        D_801AEC78 = 10;
+    }
 
-//     D_801AEC74 %= 8;
-//     if (CHECK_BTN_ALL(input->press.button, BTN_DUP)) {
-//         D_801AEC70--;
-//         if (D_801AEC70 < 0) {
-//             D_801AEC70 = 0;
-//         }
-//         D_801AEC78 = 10;
-//     }
+    if (D_801AEC78 == 0) {
+        if (CHECK_BTN_ALL(input->cur.button, BTN_DLEFT)) {
+            D_801AEC74++;
+            D_801AEC78 = 2;
+        }
+        if (CHECK_BTN_ALL(input->cur.button, BTN_DRIGHT)) {
+            D_801AEC74--;
+            D_801AEC78 = 2;
+        }
+    }
 
-//     if (CHECK_BTN_ALL(input->press.button, BTN_DDOWN)) {
-//         D_801AEC78 = 10;
-//         D_801AEC70++;
-//         if (!D_801AE8F0[D_801AEC70].value) {
-//             D_801AEC70--;
-//         }
-//     }
+    D_801AEC74 %= 8;
+    if (CHECK_BTN_ALL(input->press.button, BTN_DUP)) {
+        D_801AEC70--;
+        if (D_801AEC70 < 0) {
+            D_801AEC70 = 0;
+        }
+        D_801AEC78 = 10;
+    }
 
-//     if (CHECK_BTN_ALL(input->press.button, BTN_CUP)) {
-//         D_801AEC70 -= 10;
-//         if (D_801AEC70 < 0) {
-//             D_801AEC70 = 0;
-//         }
-//         D_801AEC78 = 10;
-//     }
+    if (CHECK_BTN_ALL(input->press.button, BTN_DDOWN)) {
+        D_801AEC70++;
+        if ((D_801AE8F0[D_801AEC70].value) == NULL) {
+            D_801AEC70--;
+        }
+        D_801AEC78 = 10;
+    }
 
-//     if (CHECK_BTN_ALL(input->press.button, BTN_CDOWN)) {
-//         D_801AEC78 = 10;
-//         D_801AEC70 += 10;
-//         if (((D_801AEC70 <= 100) ^ 1)) {
-//             D_801AEC70 = 100;
-//         }
-//     }
+    if (CHECK_BTN_ALL(input->press.button, BTN_CUP)) {
+        D_801AEC70 -= 10;
+        if (D_801AEC70 < 0) {
+            D_801AEC70 = 0;
+        }
+        D_801AEC78 = 10;
+    }
 
-//     if (D_801AEC78 == 0) {
-//         if (CHECK_BTN_ALL(input->cur.button, BTN_DUP)) {
-//             D_801AEC78 = 2;
-//             D_801AEC70--;
-//             if (D_801AEC70 < 0) {
-//                 D_801AEC70 = 0;
-//             }
-//         } else if (CHECK_BTN_ALL(input->cur.button, BTN_DDOWN)) {
-//             D_801AEC78 = 2;
-//             D_801AEC70++;
-//             if (!D_801AE8F0[D_801AEC70].value) {
-//                 D_801AEC70--;
-//             }
-//         } else if (CHECK_BTN_ALL(input->cur.button, BTN_CUP)) {
-//             D_801AEC78 = 2;
-//             D_801AEC70 -= 10;
-//             if (D_801AEC70 < 0) {
-//                 D_801AEC70 = 0;
-//             }
-//         } else if (CHECK_BTN_ALL(input->cur.button, BTN_CDOWN)) {
-//             D_801AEC78 = 2;
-//             D_801AEC70 += 10;
-//             if (((D_801AEC70 <= 100) ^ 1)) {
-//                 D_801AEC70 = 100;
-//             }
-//         }
-//     }
-//     if (CHECK_BTN_ALL(input->press.button, BTN_A)) {
-//         *D_801AE8F0[D_801AEC70].value ^= (1 << D_801AEC74);
-//     }
+    if (CHECK_BTN_ALL(input->press.button, BTN_CDOWN)) {
+        D_801AEC70 += 10;
+        if (D_801AEC70 > 100) {
+            D_801AEC70 = 100;
+        }
+        D_801AEC78 = 10;
+    }
 
-//     if (D_801AEC78 != 0) {
-//         D_801AEC78--;
-//     }
+    if (D_801AEC78 == 0) {
+        if (CHECK_BTN_ALL(input->cur.button, BTN_DUP)) {
+            D_801AEC70--;
+            if (D_801AEC70 < 0) {
+                D_801AEC70 = 0;
+            }
+            D_801AEC78 = 2;
+        } else if (CHECK_BTN_ALL(input->cur.button, BTN_DDOWN)) {
+            D_801AEC70++;
+            if ((D_801AE8F0[D_801AEC70].value) == NULL) {
+                D_801AEC70--;
+            }
+            D_801AEC78 = 2;
+        } else if (CHECK_BTN_ALL(input->cur.button, BTN_CUP)) {
+            D_801AEC70 -= 10;
+            if (D_801AEC70 < 0) {
+                D_801AEC70 = 0;
+            }
+            D_801AEC78 = 2;
+        } else if (CHECK_BTN_ALL(input->cur.button, BTN_CDOWN)) {
+            D_801AEC70 += 10;
+            if (D_801AEC70 > 100) {
+                D_801AEC70 = 100;
+            }
+            D_801AEC78 = 2;
+        }
+    }
 
-//     if (CHECK_BTN_ALL(input->cur.button, BTN_START)) {
-//         if (CHECK_BTN_ALL(input->press.button, BTN_B)) {
-//             s16 phi_v0;
-//             for (phi_v0 = 0; phi_v0 < 100; phi_v0++) {
-//                 gSaveContext.weekEventReg[phi_v0] = 0;
-//             }
-//             for (phi_v0 = 0; phi_v0 < 8; phi_v0++) {
-//                 gSaveContext.eventInf[phi_v0] = 0;
-//             }
-//         }
-//     } else if (CHECK_BTN_ALL(input->press.button, BTN_B)) {
-//         globalCtx->pauseCtx.debugState = 0;
-//     }
-// }
+    // A toggles a value
+    if (CHECK_BTN_ALL(input->press.button, BTN_A)) {
+        *D_801AE8F0[D_801AEC70].value ^= (1 << D_801AEC74);
+    }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/code/flg_set/D_801DC120.s")
+    if (D_801AEC78 != 0) {
+        D_801AEC78--;
+    }
 
-// void func_800B3644(GameState* gameState);
+    // Start + B will reset the first two flag arrays
+    if (CHECK_BTN_ALL(input->cur.button, BTN_START)) {
+        if (CHECK_BTN_ALL(input->press.button, BTN_B)) {
+            s16 i;
+            for (i = 0; i < 100; i++) {
+                gSaveContext.weekEventReg[i] = 0;
+            }
+            for (i = 0; i < 8; i++) {
+                gSaveContext.eventInf[i] = 0;
+            }
+        }
+
+    // Pressing B will exit
+    } else if (CHECK_BTN_ALL(input->press.button, BTN_B)) {
+        globalCtx->pauseCtx.debugState = 0;
+    }
+}
+
 extern s32 D_801ED890;
 
 void func_800B3644(GameState* gameState) {
