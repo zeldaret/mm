104bb5422bb44f2982ae371529f22325c816c5c3
angie <angheloalf95@gmail.com>
2021-09-28 20:32:18 -0300

    Sram_InitDebugSave

--- src/code/z_sram_NES.c
+++ src/code/z_sram_NES.c
@@ -1,6 +1,11 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80143A10.s")
+void func_80143A10(u8 owlId) {
+    gSaveContext.owlActivationFlags = ((void)0, gSaveContext.owlActivationFlags) | (u16)gBitFlags[owlId];
+    if (gSaveContext.unk_44 == 0xFF) {
+        gSaveContext.unk_44 = owlId;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80143A54.s")
 
@@ -12,13 +17,63 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/Sram_CalcChecksum.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80144628.s")
+void func_80144628(void) {
+    gSaveContext.entranceIndex = 0x1C00;
+    gSaveContext.equippedMask = 0;
+    gSaveContext.unk_05 = 0;
+    gSaveContext.unk_06 = 0;
+    gSaveContext.linkAge = 0;
+    gSaveContext.isNight = false;
+    gSaveContext.unk_14 = 0;
+    gSaveContext.snowheadCleared = 0;
+    gSaveContext.hasTatl = false;
+    gSaveContext.isOwlSave = false;
+    bzero(gSaveContext.newf, 0xFE8);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/Sram_GenerateRandomSaveFields.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80144890.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/Sram_InitDebugSave.s")
+extern UNK_TYPE D_801C6970;
+extern UNK_TYPE D_801C6998;
+extern u8 D_801C6A48[];
+extern u8 D_801C6A50[];
+
+void Sram_InitDebugSave(void) {
+    func_80144628();
+    Lib_MemCpy(gSaveContext.newf, &D_801C6970, 0x28);
+    Lib_MemCpy(&gSaveContext.equips, &D_801C6998, 0x22);
+    Lib_MemCpy(&gSaveContext.inventory, &D_801C69BC, 0x88);
+    Lib_MemCpy(&gSaveContext.checksum, &D_801C6A44, 2);
+
+    if (gSaveContext.playerForm != 4) {
+        gSaveContext.equips.buttonItems[0][2] = D_801C6A48[((void)0, gSaveContext.playerForm & 0xFF)];
+        gSaveContext.equips.cButtonSlots[0][2] = D_801C6A50[((void)0, gSaveContext.playerForm & 0xFF)];
+    }
+
+    gSaveContext.hasTatl = true;
+
+    gSaveContext.horseData.scene = SCENE_F01;
+    gSaveContext.horseData.pos.x = -1420;
+    gSaveContext.horseData.pos.y = 257;
+    gSaveContext.horseData.pos.z = -1285;
+    gSaveContext.horseData.angle = -0x7554;
+
+    gSaveContext.entranceIndex = 0x1C00;
+    gSaveContext.unk_05 = 1;
+
+    gSaveContext.weekEventReg[0xF] |= 0x20;
+    gSaveContext.weekEventReg[0x3B] |= 0x04;
+    gSaveContext.weekEventReg[0x1F] |= 0x04;
+
+    gSaveContext.unk_3F68[4][16] = 1;
+    gSaveContext.roomInf[99][1] = 1;
+    gSaveContext.magicLevel = 0;
+
+    Sram_GenerateRandomSaveFields();
+}
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80144A94.s")
 
@@ -64,4 +119,6 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80147414.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/Sram_nop8014750C.s")
+void Sram_nop8014750C(s32 arg0) {
+
+}
