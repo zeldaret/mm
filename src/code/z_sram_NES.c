c165b8e1c5c94c5e8c0be2a1fbe223027c459500
angie <angheloalf95@gmail.com>
2021-09-28 22:57:44 -0300

    err

--- src/code/z_sram_NES.c
+++ src/code/z_sram_NES.c
@@ -31,7 +31,117 @@ void func_80144628(void) {
     bzero(gSaveContext.newf, 0xFE8);
 }
 
+#ifdef NON_MATCHING
+void Sram_GenerateRandomSaveFields(void) {
+    s16 sp2A;
+    s16 temp_a0;
+    s16 temp_s0;
+    s16 temp_v0;
+    s16 temp_v1;
+    s32 temp_v0_2;
+    s32 temp_v0_3;
+    s16 phi_v0;
+    s16 phi_s0;
+    s16 phi_v1;
+    s16 phi_s0_2;
+    s16 phi_s0_3;
+    s16 phi_v1_2;
+    s16 phi_s0_4;
+    s32 phi_s1;
+    s32 phi_s0_5;
+
+    func_80143A54();
+    gSaveContext.lotteryCodes[0][0] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[0][1] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[0][2] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[1][0] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[1][1] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[1][2] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[2][0] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[2][1] = Rand_S16Offset(0, 0xA);
+    gSaveContext.lotteryCodes[2][2] = Rand_S16Offset(0, 0xA);
+
+/*
+    phi_s0 = 0;
+    do {
+
+//         phi_v0 = 0;
+// loop_2:
+//         phi_v0 = phi_v0 + 1;
+//         if ((s32) phi_v0 < 3) {
+//             goto loop_2;
+//         }
+
+        for (phi_v0 = 0; phi_v0 < 3; phi_v0++) {
+
+        }
+
+        phi_s0 = phi_s0 + 1;
+    } while ((s32) phi_s0 < 3);
+*/
+    for (phi_s0 = 0; phi_s0 < 3; phi_s0++) {
+        for (phi_v0 = 0; phi_v0 < 3; phi_v0++) {
+
+        }
+    }
+
+    phi_s0_2 = 0;
+    sp2A = (s16) (Rand_S16Offset(0, 0x10) & 3);
+    while (phi_s0_2 != 6) {
+        temp_a0 = Rand_S16Offset(0, 0x10) & 3;
+        if (sp2A != temp_a0) {
+            gSaveContext.spiderHouseMaskOrder[phi_s0_2] = (s8) temp_a0;
+            phi_s0_2 = (s16) (phi_s0_2 + 1);
+            sp2A = temp_a0;
+        }
+    }
+
+    do {
+// loop_8:
+//         temp_v0_2 = Rand_S16Offset(0, 6);
+//         if (temp_v0_2 <= 0) {
+//             goto loop_8;
+//         }
+        do {
+            temp_v0_2 = Rand_S16Offset(0, 6);
+        } while (temp_v0_2 <= 0);
+    } while (temp_v0_2 >= 6);
+
+    gSaveContext.bomberCode[0] = (s8) temp_v0_2;
+
+    phi_s0_4 = 1;
+    while (phi_s0_4 != 5) {
+        phi_s1 = 0;
+//loop_12:
+//        temp_v0_3 = Rand_S16Offset(0, 6);
+//        if (temp_v0_3 <= 0) {
+//            goto loop_12;
+//        }
+//        if (temp_v0_3 >= 6) {
+//            goto loop_12;
+//        }
+
+        do {
+            do {
+                temp_v0_3 = Rand_S16Offset(0, 6);
+            } while (temp_v0_3 <= 0);
+        } while (temp_v0_3 >= 6);
+
+        for (phi_v1_2 = 0; phi_v1_2 < phi_s0_4; phi_v1_2++) {
+            if (temp_v0_3 == gSaveContext.bomberCode[phi_v1_2]) {
+                phi_s1 = 1;
+            }
+        }
+
+        if (phi_s1 == 0) {
+            gSaveContext.bomberCode[phi_s0_4] = (s8) temp_v0_3;
+            phi_s0_4 = (s16) (phi_s0_4 + 1);
+        }
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/Sram_GenerateRandomSaveFields.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_sram_NES/func_80144890.s")
 
