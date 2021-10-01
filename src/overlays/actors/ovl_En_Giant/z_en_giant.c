cf697a48e36304a2c738e31b484614b9fdcd4cf9
tom-overton <tom.overton@outlook.com>
2021-09-30 21:54:42 -0700

    EnGiant_Init OK

--- src/overlays/actors/ovl_En_Giant/z_en_giant.c
+++ src/overlays/actors/ovl_En_Giant/z_en_giant.c
@@ -9,6 +9,8 @@ void EnGiant_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnGiant_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnGiant_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80B01990(EnGiant* this, s16 arg1);
+s32 func_80B01A74(EnGiant* this);
 void func_80B023D0(EnGiant* this, GlobalContext* globalCtx);
 void func_80B024AC(EnGiant* this, GlobalContext* globalCtx);
 void func_80B024D8(EnGiant* this, GlobalContext* globalCtx);
@@ -25,9 +27,12 @@ const ActorInit En_Giant_InitVars = {
     (ActorFunc)EnGiant_Draw,
 };
 
-extern UNK_TYPE D_06002168;
+extern AnimationHeader D_06002168;
 extern UNK_TYPE D_06007610;
-extern UNK_TYPE D_060116E4;
+extern FlexSkeletonHeader D_060079B0;
+extern AnimationHeader D_060116E4;
+extern AnimationHeader D_06013004;
+extern AnimationHeader D_06013FE8;
 
 s32 D_80B02950[] = {
     0x06008394, 0x060096E4, 0x060102A4, 0x060116E4, 0x06012A38, 0x06013004, 0x06013FE8, 0x06015334,
@@ -40,7 +45,97 @@ s32 D_80B0298C[] = { 0x06005A80, 0x06006280, 0x06006A80, 0x00000000, 0x00000000 
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Giant/func_80B01A74.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Giant/EnGiant_Init.s")
+void EnGiant_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnGiant* this = THIS;
+    s32 param;
+
+    param = this->actor.params & 0xF;
+    this->actor.uncullZoneForward = 4000.0f;
+    this->actor.uncullZoneScale = 2000.0f;
+    this->actor.uncullZoneDownward = 2400.0f;
+    Actor_SetScale(&this->actor, 0.32f);
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_060079B0, &D_06002168, this->jointTable, this->morphTable, 16);
+    func_80B01990(this, 7);
+    this->unk_24C = 0;
+    this->actionFunc = func_80B024D8;
+    this->actor.draw = NULL;
+    this->unk_24E = 0;
+    this->actor.velocity.y = -10.0f;
+    this->actor.minVelocityY = -10.0f;
+    this->actor.gravity = -5.0f;
+    switch (param) {
+        case 1:
+        case 5:
+        case 9:
+            this->unk_24A = 0x1C6;
+            break;
+        case 2:
+        case 6:
+        case 10:
+            this->unk_24A = 0x1C7;
+            break;
+        case 3:
+        case 7:
+        case 11:
+            this->unk_24A = 0x1C8;
+            break;
+        default:
+            this->unk_24A = 0x1C5;
+            break;
+    }
+
+    if (param >= 4 && param < 8) {
+        if (!(gSaveContext.weekEventReg[0x19] & 2)) {
+            Actor_MarkForDeath(&this->actor);
+            return;
+        }
+        this->unk_24A = 0x1C5;
+        Actor_SetScale(&this->actor, 0.32f);
+        this->actionFunc = func_80B023D0;
+        SkelAnime_ChangeAnim(&this->skelAnime, &D_060116E4, 0.0f, SkelAnime_GetFrameCount(&D_060116E4.common) - 1.0f,
+                             SkelAnime_GetFrameCount(&D_060116E4.common), 2, 0.0f);
+        this->actor.draw = EnGiant_Draw;
+        this->actor.velocity.y = 0.0f;
+        this->actor.minVelocityY = 0.0f;
+        this->actor.gravity = 0.0f;
+    }
+
+    if (param >= 0xC && param < 0x10) {
+        Actor_SetScale(&this->actor, 0.32f);
+        this->actionFunc = func_80B024AC;
+        SkelAnime_ChangeAnim(&this->skelAnime, &D_06013FE8, 1.0f, 0.0f, SkelAnime_GetFrameCount(&D_06013004.common), 0,
+                             0.0f);
+        this->actor.draw = EnGiant_Draw;
+        this->actor.velocity.y = 0.0f;
+        this->actor.minVelocityY = 0.0f;
+        this->actor.gravity = 0.0f;
+        if (func_80B01A74(this)) {
+            Actor_MarkForDeath(&this->actor);
+        }
+    }
+
+    if (param >= 4) {
+        this->unk_24E = 0xFF;
+    }
+
+    this->unk_250 = 0xFFFF;
+    if (param >= 8 && param < 0xC) {
+        switch (gSaveContext.sceneSetupIndex) {
+            case 0:
+            case 10:
+                this->unk_250 = 0x2183;
+                break;
+            case 1:
+            case 2:
+            case 3:
+                this->unk_250 = 0x2182;
+                break;
+            default:
+                this->unk_250 = 0x2181;
+                break;
+        }
+    }
+}
 
 void EnGiant_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 }
--- src/overlays/actors/ovl_En_Giant/z_en_giant.h
+++ src/overlays/actors/ovl_En_Giant/z_en_giant.h
@@ -8,9 +8,17 @@ struct EnGiant;
 typedef void (*EnGiantActionFunc)(struct EnGiant*, GlobalContext*);
 
 typedef struct EnGiant {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x154];
-    /* 0x0298 */ EnGiantActionFunc actionFunc;
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ Vec3s jointTable[16];
+    /* 0x1E8 */ Vec3s morphTable[16];
+    /* 0x248 */ char unk_248[0x2];
+    /* 0x24A */ s16 unk_24A;
+    /* 0x24C */ s16 unk_24C;
+    /* 0x24E */ s16 unk_24E;
+    /* 0x250 */ u16 unk_250;
+    /* 0x252 */ char unk_252[0x46];
+    /* 0x298 */ EnGiantActionFunc actionFunc;
 } EnGiant; // size = 0x29C
 
 extern const ActorInit En_Giant_InitVars;
