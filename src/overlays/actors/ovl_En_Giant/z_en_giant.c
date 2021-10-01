8e487d34ebb5472baa65c0edf779ed721b857fae
tom-overton <tom.overton@outlook.com>
2021-09-30 20:13:32 -0700

    Migrate data to C

--- src/overlays/actors/ovl_En_Giant/z_en_giant.c
+++ src/overlays/actors/ovl_En_Giant/z_en_giant.c
@@ -13,7 +13,6 @@ void func_80B023D0(EnGiant* this, GlobalContext* globalCtx);
 void func_80B024AC(EnGiant* this, GlobalContext* globalCtx);
 void func_80B024D8(EnGiant* this, GlobalContext* globalCtx);
 
-#if 0
 const ActorInit En_Giant_InitVars = {
     ACTOR_EN_GIANT,
     ACTORCAT_NPC,
@@ -26,11 +25,16 @@ const ActorInit En_Giant_InitVars = {
     (ActorFunc)EnGiant_Draw,
 };
 
-#endif
-
 extern UNK_TYPE D_06002168;
 extern UNK_TYPE D_06007610;
 extern UNK_TYPE D_060116E4;
+
+s32 D_80B02950[] = {
+    0x06008394, 0x060096E4, 0x060102A4, 0x060116E4, 0x06012A38, 0x06013004, 0x06013FE8, 0x06015334,
+    0x06017944, 0x0600A1C4, 0x0600D040, 0x0600DE84, 0x0600ACA4, 0x0600B784, 0x0600C5D4,
+};
+
+s32 D_80B0298C[] = { 0x06005A80, 0x06006280, 0x06006A80, 0x00000000, 0x00000000 };
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Giant/func_80B01990.s")
 
