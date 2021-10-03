d1225a5634892d15e770e6aa1fc6c4852c1afdc0
engineer124 <engineer124engineer124@gmail.com>
2021-10-03 17:45:24 +1100

    Cleanup

--- src/libultra/voice/voicestartreaddata.c
+++ src/libultra/voice/voicestartreaddata.c
@@ -2,13 +2,13 @@
 
 // Start voice recognition by the Voice Recognition System
 s32 osVoiceStartReadData(OSVoiceHandle* hd) {
-    s32 temp_v0;
+    s32 ret;
     u8 sp2B;
     s32 sp24;
 
-    temp_v0 = __osVoiceGetStatus(hd->mq, hd->port, &sp2B);
-    if (temp_v0 != 0) {
-        return temp_v0;
+    ret = __osVoiceGetStatus(hd->mq, hd->port, &sp2B);
+    if (ret != 0) {
+        return ret;
     }
 
     if ((sp2B & 2) != 0) {
@@ -20,16 +20,17 @@ s32 osVoiceStartReadData(OSVoiceHandle* hd) {
     }
 
     sp24 = 0x5000000;
-    temp_v0 = __osVoiceContWrite4(hd->mq, hd->port, 0, &sp24);
+    ret = __osVoiceContWrite4(hd->mq, hd->port, 0, &sp24);
 
-    if (temp_v0 == 0) {
-        temp_v0 = __osVoiceCheckResult(hd, &sp2B);
-        if ((temp_v0 & 0xFF00) != 0) {
-            temp_v0 = 5;
+    if (ret == 0) {
+        ret = __osVoiceCheckResult(hd, &sp2B);
+        
+        if ((ret & 0xFF00) != 0) {
+            ret = 5;
         } else {
             hd->mode = 1;
         }
     }
 
-    return temp_v0;
+    return ret;
 }
