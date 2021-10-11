961db34e3bae97d7d35fb1113adad258b9ff9114
tom-overton <tom.overton@outlook.com>
2021-10-11 13:56:31 -0700

    EnElforg_Init OK

--- src/overlays/actors/ovl_En_Elforg/z_en_elforg.c
+++ src/overlays/actors/ovl_En_Elforg/z_en_elforg.c
@@ -9,6 +9,13 @@ void EnElforg_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnElforg_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnElforg_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_80ACCEB0(EnElforg* this, GlobalContext* globalCtx);
+void func_80ACCC98(EnElforg* this, GlobalContext* globalCtx);
+void func_80ACCBD0(EnElforg* this, GlobalContext* globalCtx);
+void func_80ACD6A8(EnElforg* this, GlobalContext* globalCtx);
+void func_80ACD6EC(EnElforg* this, GlobalContext* globalCtx);
+void func_80ACD2E4(EnElforg* this, GlobalContext* globalCtx);
+
 const ActorInit En_Elforg_InitVars = {
     ACTOR_EN_ELFORG,
     ACTORCAT_ITEMACTION,
@@ -23,11 +30,24 @@ const ActorInit En_Elforg_InitVars = {
 
 // static ColliderCylinderInit sCylinderInit = {
 static ColliderCylinderInit D_80ACDA30 = {
-    { COLTYPE_NONE, AT_NONE, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_PLAYER, OC2_TYPE_1, COLSHAPE_CYLINDER, },
-    { ELEMTYPE_UNK0, { 0x00000000, 0x00, 0x00 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_NONE | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_NONE, },
+    {
+        COLTYPE_NONE,
+        AT_NONE,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_PLAYER,
+        OC2_TYPE_1,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK0,
+        { 0x00000000, 0x00, 0x00 },
+        { 0xF7CFFFFF, 0x00, 0x00 },
+        TOUCH_NONE | TOUCH_SFX_NORMAL,
+        BUMP_ON,
+        OCELEM_NONE,
+    },
     { 16, 32, 0, { 0, 0, 0 } },
 };
-
 
 static s32 D_80ACDA5C[] = { 0x00000000, 0xBD4CCCCD, 0x00000000 };
 
@@ -39,7 +59,86 @@ static s32 D_80ACDA88[] = { 0xFF9600FF, 0xFF0000FF, 0x00FF00FF, 0x0000FFFF, 0xFF
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Elforg/func_80ACC470.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Elforg/EnElforg_Init.s")
+void EnElforg_Init(Actor* thisx, GlobalContext* globalCtx) {
+    s32 pad;
+    EnElforg* this = THIS;
+
+    Actor_SetScale(&this->actor, 0.01f);
+    this->unk_214 = 0;
+    this->unk_216 = 0;
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_0402CA98, &D_0402B494, this->jointTable, this->jointTable, 10);
+    this->skelAnime.animPlaybackSpeed = 1.0f;
+    ActorShape_Init(&this->actor.shape, 0.0f, NULL, 0.0f);
+    this->actor.shape.shadowAlpha = 0xFF;
+
+    switch (this->actor.params & 0xF) {
+        case 3:
+            if ((gSaveContext.weekEventReg[8] & 0x80) != 0) {
+                Actor_MarkForDeath(&this->actor);
+                return;
+            }
+            break;
+        case 7:
+            if (Actor_GetCollectibleFlag(globalCtx, (this->actor.params & 0xFE00) >> 9) != 0) {
+                Actor_MarkForDeath(&this->actor);
+                return;
+            }
+            break;
+        default:
+            if (Flags_GetSwitch(globalCtx, (this->actor.params & 0xFE00) >> 9) != 0) {
+                Actor_MarkForDeath(&this->actor);
+                return;
+            }
+            break;
+        case 1:
+        case 2:
+        case 6:
+        case 8:
+            break;
+    }
+
+    if (func_8010A074(globalCtx) != 0) {
+        this->unk_218 = gSaveContext.unk_48C8 + 1;
+    } else {
+        // Needs to be thisx in order to match
+        this->unk_218 = (thisx->params & 0x1C0) >> 6;
+    }
+
+    switch (this->actor.params & 0xF) {
+        case 1:
+            func_80ACC470(this);
+            this->actionFunc = func_80ACCEB0;
+            this->unk_224 = Rand_ZeroFloat(2.0f) + 1.0f;
+            this->unk_228 = Rand_ZeroFloat(100.0f) + 50.0f;
+            break;
+        case 8:
+            func_80ACC470(this);
+            this->actionFunc = func_80ACCC98;
+            this->unk_220 = 0x3C;
+            break;
+        case 2:
+            this->unk_21C = 0;
+            this->actionFunc = func_80ACCBD0;
+            break;
+        case 4:
+            this->actionFunc = func_80ACD6A8;
+            func_80ACD6A8(this, globalCtx);
+            this->actor.draw = NULL;
+            break;
+        case 5:
+            this->actionFunc = func_80ACD6EC;
+            this->actor.draw = NULL;
+            Collider_InitAndSetCylinder(globalCtx, &this->collider, &this->actor, &D_80ACDA30);
+            Collider_UpdateCylinder(&this->actor, &this->collider);
+            break;
+        default:
+            func_80ACC470(this);
+            this->actionFunc = func_80ACD2E4;
+            break;
+    }
+
+    this->actor.shape.rot.y = 0;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Elforg/EnElforg_Destroy.s")
 
--- src/overlays/actors/ovl_En_Elforg/z_en_elforg.h
+++ src/overlays/actors/ovl_En_Elforg/z_en_elforg.h
@@ -8,9 +8,19 @@ struct EnElforg;
 typedef void (*EnElforgActionFunc)(struct EnElforg*, GlobalContext*);
 
 typedef struct EnElforg {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0xE8];
-    /* 0x022C */ EnElforgActionFunc actionFunc;
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ Vec3s jointTable[10];
+    /* 0x1C4 */ ColliderCylinder collider;
+    /* 0x210 */ char unk_210[0x4];
+    /* 0x214 */ s16 unk_214;
+    /* 0x216 */ s16 unk_216;
+    /* 0x218 */ s16 unk_218;
+    /* 0x21C */ s32 unk_21C;
+    /* 0x220 */ s32 unk_220;
+    /* 0x224 */ f32 unk_224;
+    /* 0x228 */ f32 unk_228;
+    /* 0x22C */ EnElforgActionFunc actionFunc;
 } EnElforg; // size = 0x230
 
 extern const ActorInit En_Elforg_InitVars;
