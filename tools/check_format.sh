15300b7199bbe568283b4c1e6dee193c1033445b
Rylie <31797144+rylieb@users.noreply.github.com>
2021-10-14 19:25:16 -0600

    Fail check_format.sh if format.sh fails.

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -2,6 +2,11 @@
 
 STATUSOLD=`git status --porcelain`
 ./format.sh
+if [ $? ]
+then
+   echo "Formatter failed. Exiting."
+   exit -1
+fi
 STATUSNEW=`git status --porcelain`
 
 if [ "${STATUSOLD}" != "${STATUSNEW}" ];
@@ -11,6 +16,7 @@ then
     echo ""
     diff --unified=0  --label "Old git status" <(echo "${STATUSOLD}") --label "New git status" <(echo "${STATUSNEW}")
     echo ""
+    echo "Exiting."
     exit 1
 else
     exit 0
