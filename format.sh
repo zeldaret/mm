0a686cb20bcf3e0ff5c2112c8442779d39b5c708
Rylie <31797144+rylieb@users.noreply.github.com>
2021-10-14 19:17:09 -0600

    Exit nonzero from format.sh if clang-format-11 not installed.

--- format.sh
+++ format.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+FORMAT_VER="11"
 FORMAT_OPTS="-i -style=file"
 TIDY_OPTS="-p . --fix --fix-errors"
 COMPILER_OPTS="-fno-builtin -std=gnu90 -Iinclude -Isrc -D_LANGUAGE_C -DNON_MATCHING"
 
 shopt -s globstar
 
+if [ -z `which clang-format-${FORMAT_VER}` ]
+then
+    echo "clang-format-${FORMAT_VER} not found. Exiting."
+    exit 1
+fi
+
 if (( $# > 0 )); then
     echo "Formatting file(s) $*"
     echo "Running clang-format..."
-    clang-format-11 ${FORMAT_OPTS} "$@"
+    clang-format-${FORMAT_VER} ${FORMAT_OPTS} "$@"
     echo "Running clang-tidy..."
     clang-tidy ${TIDY_OPTS} "$@" -- ${COMPILER_OPTS} &> /dev/null
     echo "Adding missing final new lines..."
@@ -20,7 +27,7 @@ fi
 
 echo "Formatting C files. This will take a bit"
 echo "Running clang-format..."
-clang-format-11 ${FORMAT_OPTS} src/**/*.c
+clang-format-${FORMAT_VER} ${FORMAT_OPTS} src/**/*.c
 echo "Running clang-tidy..."
 clang-tidy ${TIDY_OPTS} src/**/*.c -- ${COMPILER_OPTS} &> /dev/null
 echo "Adding missing final new lines..."
