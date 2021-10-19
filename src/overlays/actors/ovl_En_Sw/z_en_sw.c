c0edd50ab7495b9422b886528e043a37ae1b8bfe
engineer124 <engineer124engineer124@gmail.com>
2021-10-19 23:19:51 +1100

    clean-up part2

--- src/overlays/actors/ovl_En_Sw/z_en_sw.c
+++ src/overlays/actors/ovl_En_Sw/z_en_sw.c
@@ -391,7 +391,6 @@ void func_808D94D0(EnSw* this, GlobalContext* globalCtx, s32 arg2, s32 arg3, s16
     sp64 = 50;
     func_808D90F0(this, arg3, arg4);
     this->actor.speedXZ = this->unk_44C;
-    // pad2 = 0;
     temp_f20 = actor->speedXZ + actor->speedXZ;
 
     sp90.x = this->actor.world.pos.x + (this->unk_368.x * 2.0f);
