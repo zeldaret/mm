f71431d20cc0bed04a0691007fa49b21fab45611
tom-overton <tom.overton@outlook.com>
2021-10-19 20:46:23 -0700

    EnTanron3_Init OK

--- src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.c
+++ src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.c
@@ -17,7 +17,7 @@ void EnTanron3_Draw(Actor* thisx, GlobalContext* globalCtx);
 
 static s32 D_80BB9720[] = { 0x00000000, 0x00000000, 0x00000000 };
 
-static s32 D_80BB972C = 0x00000000;
+static Actor* D_80BB972C = NULL;
 
 const ActorInit En_Tanron3_InitVars = {
     ACTOR_EN_TANRON3,
@@ -33,15 +33,43 @@ const ActorInit En_Tanron3_InitVars = {
 
 // static ColliderCylinderInit sCylinderInit = {
 static ColliderCylinderInit D_80BB9750 = {
-    { COLTYPE_HIT3, AT_ON | AT_TYPE_ENEMY, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_1, COLSHAPE_CYLINDER, },
-    { ELEMTYPE_UNK3, { 0xF7CFFFFF, 0x00, 0x02 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_ON | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_ON, },
+    {
+        COLTYPE_HIT3,
+        AT_ON | AT_TYPE_ENEMY,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_ALL,
+        OC2_TYPE_1,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK3,
+        { 0xF7CFFFFF, 0x00, 0x02 },
+        { 0xF7CFFFFF, 0x00, 0x00 },
+        TOUCH_ON | TOUCH_SFX_NORMAL,
+        BUMP_ON,
+        OCELEM_ON,
+    },
     { 7, 10, -5, { 0, 0, 0 } },
 };
 
 // static ColliderCylinderInit sCylinderInit = {
 static ColliderCylinderInit D_80BB977C = {
-    { COLTYPE_HIT3, AT_ON | AT_TYPE_ENEMY, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_1, COLSHAPE_CYLINDER, },
-    { ELEMTYPE_UNK3, { 0xF7CFFFFF, 0x00, 0x02 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_ON | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_ON, },
+    {
+        COLTYPE_HIT3,
+        AT_ON | AT_TYPE_ENEMY,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_ALL,
+        OC2_TYPE_1,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK3,
+        { 0xF7CFFFFF, 0x00, 0x02 },
+        { 0xF7CFFFFF, 0x00, 0x00 },
+        TOUCH_ON | TOUCH_SFX_NORMAL,
+        BUMP_ON,
+        OCELEM_ON,
+    },
     { 20, 20, -10, { 0, 0, 0 } },
 };
 
@@ -49,11 +77,25 @@ static s32 D_80BB97A8 = 0x643737FF;
 
 static s32 D_80BB97AC = 0x320A0AFF;
 
-extern UNK_TYPE D_0600DAAC;
+extern FlexSkeletonHeader D_0600DA20;
+extern AnimationHeader D_0600DAAC;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Tanron3/func_80BB85A0.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Tanron3/EnTanron3_Init.s")
+void EnTanron3_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnTanron3* this = THIS;
+
+    this->actor.gravity = -1.0f;
+    Collider_InitAndSetCylinder(globalCtx, &this->collider1, &this->actor, &D_80BB9750);
+    Collider_InitAndSetCylinder(globalCtx, &this->collider2, &this->actor, &D_80BB9750);
+    SkelAnime_InitSV(globalCtx, &this->skelAnime, &D_0600DA20, &D_0600DAAC, this->jointTable, this->morphTable, 10);
+    Actor_SetScale(&this->actor, 0.02f);
+    func_80BB897C(this, globalCtx);
+    this->actor.flags &= ~1;
+    this->unk_250 = (s32)Rand_ZeroFloat(500000.0f);
+    this->unk_244 = 430.0f;
+    D_80BB972C = this->actor.parent;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Tanron3/EnTanron3_Destroy.s")
 
--- src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.h
+++ src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.h
@@ -8,9 +8,18 @@ struct EnTanron3;
 typedef void (*EnTanron3ActionFunc)(struct EnTanron3*, GlobalContext*);
 
 typedef struct EnTanron3 {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x1B4];
-    /* 0x02F8 */ EnTanron3ActionFunc actionFunc;
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ Vec3s jointTable[10];
+    /* 0x1C4 */ Vec3s morphTable[10];
+    /* 0x200 */ char unk_200[0x44];
+    /* 0x244 */ f32 unk_244;
+    /* 0x248 */ char unk_248[0x8];
+    /* 0x250 */ s32 unk_250;
+    /* 0x254 */ char unk_254[0xC];
+    /* 0x260 */ ColliderCylinder collider1;
+    /* 0x2AC */ ColliderCylinder collider2;
+    /* 0x2F8 */ EnTanron3ActionFunc actionFunc;
 } EnTanron3; // size = 0x2FC
 
 extern const ActorInit En_Tanron3_InitVars;
