b2747aa8b4186034d6621e8e3eccc44d14ec27a7
Anghelo Carvajal <angheloalf95@gmail.com>
2021-10-24 13:04:36 -0300

    Subrepos updates (#360)
    * git subrepo pull tools/asm-differ --force
    
    subrepo:
      subdir:   "tools/asm-differ"
      merged:   "d218cdf0"
    upstream:
      origin:   "https://github.com/simonlindholm/asm-differ.git"
      branch:   "main"
      commit:   "d218cdf0"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/graphovl --force
    
    subrepo:
      subdir:   "tools/graphovl"
      merged:   "f5fe93d7"
    upstream:
      origin:   "https://github.com/AngheloAlf/graphovl.git"
      branch:   "master"
      commit:   "f5fe93d7"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/ZAPD --force
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "e7a8a48c"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "e7a8a48c"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/graphovl --force
    
    subrepo:
      subdir:   "tools/graphovl"
      merged:   "d14ea084"
    upstream:
      origin:   "https://github.com/AngheloAlf/graphovl.git"
      branch:   "master"
      commit:   "d14ea084"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/ZAPD --force
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "e243634e"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "e243634e"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/decomp-permuter --force
    
    subrepo:
      subdir:   "tools/decomp-permuter"
      merged:   "d1294dfa0"
    upstream:
      origin:   "https://github.com/simonlindholm/decomp-permuter.git"
      branch:   "main"
      commit:   "d1294dfa0"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/graphovl --force
    
    subrepo:
      subdir:   "tools/graphovl"
      merged:   "f5fe93d75"
    upstream:
      origin:   "https://github.com/AngheloAlf/graphovl.git"
      branch:   "master"
      commit:   "f5fe93d75"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull tools/ZAPD --force
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "d0cd6b397"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "d0cd6b397"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * add ExternalXMLFolder config
    
    * git subrepo pull --force tools/decomp-permuter
    
    subrepo:
      subdir:   "tools/decomp-permuter"
      merged:   "a20bac942"
    upstream:
      origin:   "https://github.com/simonlindholm/decomp-permuter.git"
      branch:   "main"
      commit:   "a20bac942"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull --force tools/ZAPD
    
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "4f7b8393e"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "4f7b8393e"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * git subrepo pull --force tools/asm-differ
    
    subrepo:
      subdir:   "tools/asm-differ"
      merged:   "f30d43ace"
    upstream:
      origin:   "https://github.com/simonlindholm/asm-differ.git"
      branch:   "main"
      commit:   "f30d43ace"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"
    
    * Remove Segment="128"

--- tools/ZAPD/ZAPD/ZPlayerAnimationData.h
+++ tools/ZAPD/ZAPD/ZPlayerAnimationData.h
@@ -1,24 +1,28 @@
 #pragma once
 
-#include "ZResource.h"
-#include "tinyxml2.h"
+#include <cstdint>
+#include <vector>
 
-class ZString : public ZResource
+#include "ZResource.h"
+
+class ZPlayerAnimationData : public ZResource
 {
 public:
-	ZString(ZFile* nParent);
+	int16_t frameCount = 0;
+	std::vector<uint16_t> limbRotData;
 
+	ZPlayerAnimationData(ZFile* nParent);
+
+	void ParseXML(tinyxml2::XMLElement* reader) override;
 	void ParseRawData() override;
 
 	Declaration* DeclareVar(const std::string& prefix, const std::string& bodyStr) override;
-	std::string GetBodySourceCode() const override;
 
-	std::string GetSourceOutputHeader(const std::string& prefix) override;
+	std::string GetBodySourceCode() const override;
+	std::string GetDefaultName(const std::string& prefix) const override;
+
 	std::string GetSourceTypeName() const override;
 	ZResourceType GetResourceType() const override;
 
 	size_t GetRawDataSize() const override;
-
-protected:
-	std::vector<uint8_t> strData;
 };
