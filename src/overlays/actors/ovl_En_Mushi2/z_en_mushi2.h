a60c47a970f45363868a868f6ba8ad32b4fd404a
Maide <34639600+Kelebek1@users.noreply.github.com>
2021-10-27 14:28:52 +0100

    En_Fish2 (#292)
    * En_Fish2
    
    * Fix

--- src/overlays/actors/ovl_En_Mushi2/z_en_mushi2.h
+++ src/overlays/actors/ovl_En_Mushi2/z_en_mushi2.h
@@ -9,9 +9,12 @@ typedef void (*EnMushi2ActionFunc)(struct EnMushi2*, GlobalContext*);
 
 typedef struct EnMushi2 {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x1C4];
+    /* 0x0144 */ char unk_144[0x133];
+    /* 0x0277 */ s8 unk_277;
+    /* 0x0278 */ char unk_278[0x90];
     /* 0x0308 */ EnMushi2ActionFunc actionFunc;
-    /* 0x030C */ char unk_30C[0x68];
+    /* 0x030C */ s32 unk_30C;
+    /* 0x0310 */ char unk_310[0x64];
 } EnMushi2; // size = 0x374
 
 extern const ActorInit En_Mushi2_InitVars;
