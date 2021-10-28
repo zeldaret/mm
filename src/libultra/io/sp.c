bf4d0befa9d3024cacbf77c6c51f64c324f93d82
kyleburnette <kyle@kyleburnette.com>
2021-10-27 17:30:29 -0700

    io/sp OK

--- src/libultra/io/sp.c
+++ src/libultra/io/sp.c
@@ -1,3 +1,10 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/boot/sp/__osSpDeviceBusy.s")
+u32 __osSpDeviceBusy(void) {
+    register u32 status = HW_REG(SP_STATUS_REG, u32);
+
+    if (status & (SP_STATUS_DMA_BUSY | SP_STATUS_DMA_FULL | SP_STATUS_IO_FULL)) {
+        return 1;
+    }
+    return 0;
+}
