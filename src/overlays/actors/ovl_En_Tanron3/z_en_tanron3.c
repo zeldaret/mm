fce9048c2d07c12458f401a7238d8166d5bbe83c
tom-overton <tom.overton@outlook.com>
2021-10-30 19:05:01 -0700

    Use 2 * M_PI

--- src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.c
+++ src/overlays/actors/ovl_En_Tanron3/z_en_tanron3.c
@@ -130,8 +130,8 @@ void EnTanron3_SpawnBubbles(EnTanron3* this, GlobalContext* globalCtx) {
     Vec3f acceleration;
 
     for (i = 0; i < 20; i++) {
-        Matrix_InsertYRotation_f(Rand_ZeroFloat(6.2831855f), 0);
-        Matrix_RotateStateAroundXAxis(Rand_ZeroFloat(6.2831855f));
+        Matrix_InsertYRotation_f(Rand_ZeroFloat(2 * M_PI), MTXMODE_NEW);
+        Matrix_RotateStateAroundXAxis(Rand_ZeroFloat(2 * M_PI));
         Matrix_GetStateTranslationAndScaledZ(Rand_ZeroFloat(3.0f) + 2.0f, &velocity);
         acceleration.x = velocity.x * -0.05f;
         acceleration.y = velocity.y * -0.05f;
