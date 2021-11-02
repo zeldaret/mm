e27ef6a62c340339c5e29a0c2e27820a3cf4db49
Kelebek1 <eeeedddccc@hotmail.co.uk>
2021-11-02 04:23:29 +0000

    En_Wiz_Fire

--- src/overlays/actors/ovl_En_Wiz/z_en_wiz.h
+++ src/overlays/actors/ovl_En_Wiz/z_en_wiz.h
@@ -11,7 +11,11 @@ typedef struct EnWiz {
     /* 0x0000 */ Actor actor;
     /* 0x0144 */ char unk_144[0x268];
     /* 0x03AC */ EnWizActionFunc actionFunc;
-    /* 0x03B0 */ char unk_3B0[0x9D0];
+    /* 0x03B0 */ char unk_3B0[0xA];
+    /* 0x03BA */ s16 unk_3BA;
+    /* 0x03BC */ char unk_3BC[0x38E];
+    /* 0x074A */ s16 unk_74A;
+    /* 0x074C */ char unk_74C[0x634];
 } EnWiz; // size = 0xD80
 
 extern const ActorInit En_Wiz_InitVars;
