75e085e587be51423ddfe92361f1fd084c543cae
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-11-06 13:13:06 +0000

    contchannelreset OK

--- src/libultra/io/contchannelreset.c
+++ src/libultra/io/contchannelreset.c
@@ -1,3 +1,27 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/boot/contchannelreset/__osContChannelReset.s")
+s32 __osContChannelReset(OSMesgQueue* mq, s32 count) {
+    s32 i;
+    s32 ret;
+    u8* bufptr = (u8*)&__osPfsPifRam;
+
+    __osSiGetAccess();
+
+    __osPfsPifRam.pifstatus = 1;
+
+    for (i = 0; i < count; i++) {
+        *bufptr++ = 0;
+    }
+
+    *bufptr++ = 0xFD;
+    *bufptr = CONT_CMD_END;
+
+    __osSiRawStartDma(OS_WRITE, &__osPfsPifRam);
+    osRecvMesg(mq, NULL, OS_MESG_BLOCK);
+    ret = __osSiRawStartDma(OS_READ, &__osPfsPifRam);
+    osRecvMesg(mq, NULL, OS_MESG_BLOCK);
+
+    __osSiRelAccess();
+
+    return ret;
+}
