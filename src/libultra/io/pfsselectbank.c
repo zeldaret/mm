6f543d2f3942ff8972274d819f03f08d841c4578
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-11-06 03:12:19 +0000

    pfsselectbank OK

--- src/libultra/io/pfsselectbank.c
+++ src/libultra/io/pfsselectbank.c
@@ -1,3 +1,18 @@
+#include "PR/pfs.h"
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/boot/pfsselectbank/__osPfsSelectBank.s")
+s32 __osPfsSelectBank(OSPfs* pfs, u8 bank) {
+    u8 temp[BLOCKSIZE];
+    s32 i;
+    s32 ret = 0;
+
+    for (i = 0; i < BLOCKSIZE; i++) {
+        temp[i] = bank;
+    }
+
+    ret = __osContRamWrite(pfs->queue, pfs->channel, 0x8000 / BLOCKSIZE, temp, 0);
+    if (ret == 0) {
+        pfs->activebank = bank;
+    }
+    return ret;
+}
