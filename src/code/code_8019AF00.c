158dede3f68891fcf1643af65a9e77b242931432
engineer124 <engineer124engineer124@gmail.com>
2021-11-11 13:47:50 +1100

    Properly split voice files from code

--- src/code/code_8019AF00.c
+++ src/code/code_8019AF00.c
@@ -411,19 +411,3 @@
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A4DF4.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A4E64.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A4EB0.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A4EB8.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A4FD8.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A5080.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A50C0.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A5100.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A510C.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_8019AF00/func_801A5118.s")
