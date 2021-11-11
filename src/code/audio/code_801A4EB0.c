d8bc20f1097ace6dc12f90dde65de00f48c3693b
engineer124 <engineer124engineer124@gmail.com>
2021-11-11 14:29:32 +1100

    Update matching functions in code_801A4EB0 from audio WIP

--- src/code/audio/code_801A4EB0.c
+++ src/code/audio/code_801A4EB0.c
@@ -1,17 +1,33 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A4EB0.s")
+void func_801A5680(u16 arg0);
+void func_801A54D0(u16 arg0);
+
+void func_801A4EB0(void) {
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A4EB8.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A4FD8.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A5080.s")
+void func_801A5080(u16 arg0) {
+    if ((D_801D8E3C != 0) && (arg0 < 6)) {
+        func_801A5680(arg0);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A50C0.s")
+void func_801A50C0(u16 arg0) {
+    if ((D_801D8E3C != 0) && (arg0 < 6)) {
+        func_801A54D0(arg0);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A5100.s")
+u16 func_801A5100(void) {
+    return D_801D8E44;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A510C.s")
+u8 func_801A510C(void) {
+    return D_801D8E3C;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/code_801A4EB0/func_801A5118.s")
