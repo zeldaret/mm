60fd6ac4c79860c013d6f739f4affc32d84f5a78
Derek Hensley <hensley.derek58@gmail.com>
2021-11-11 20:41:56 -0800

    Rename function

--- src/code/z_elf_message.c
+++ src/code/z_elf_message.c
@@ -1,16 +1,16 @@
 #include "global.h"
 
-u16 func_800F05C0(GlobalContext* globalCtx) {
+u16 ElfMessage_GetFirstCycleHint(GlobalContext* globalCtx) {
     if (INV_CONTENT(ITEM_OCARINA) == ITEM_OCARINA) {
         return 0;
     }
     if (CURRENT_DAY <= 0) {
         return 0;
     }
-    if (gSaveContext.weekEventReg[0x58] & 0x20) {
+    if (gSaveContext.weekEventReg[88] & 0x20) {
         return 0;
     }
-    if (gSaveContext.weekEventReg[0x4F] & 0x10) {
+    if (gSaveContext.weekEventReg[79] & 0x10) {
         if (gSaveContext.weekEventReg[8] & 0x40) {
             return 0;
         }
@@ -35,27 +35,27 @@ u16 func_800F05C0(GlobalContext* globalCtx) {
         return 0;
     }
     if (INV_CONTENT(ITEM_MOON_TEAR) == ITEM_MOON_TEAR) {
-        if (gSaveContext.weekEventReg[0x56] & 4) {
+        if (gSaveContext.weekEventReg[86] & 4) {
             return 0x242;
         }
         return 0x243;
     }
-    if (gSaveContext.weekEventReg[0x4A] & 0x20) {
+    if (gSaveContext.weekEventReg[74] & 0x20) {
         return 0x223;
     }
-    if (gSaveContext.weekEventReg[0x49] & 0x80) {
+    if (gSaveContext.weekEventReg[73] & 0x80) {
         return 0x222;
     }
-    if (gSaveContext.weekEventReg[0x49] & 0x20) {
+    if (gSaveContext.weekEventReg[73] & 0x20) {
         return 0x221;
     }
-    if (gSaveContext.weekEventReg[0x4D] & 2) {
-        if (gSaveContext.weekEventReg[0x49] & 0x10) {
+    if (gSaveContext.weekEventReg[77] & 2) {
+        if (gSaveContext.weekEventReg[73] & 0x10) {
             return 0x240;
         }
         return 0x241;
     }
-    if ((gSaveContext.weekEventReg[0x56] & 2) || (gSaveContext.weekEventReg[0x49] & 0x40)) {
+    if ((gSaveContext.weekEventReg[86] & 2) || (gSaveContext.weekEventReg[73] & 0x40)) {
         return 0x23F;
     }
     return 0x220;
