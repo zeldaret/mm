c897b1ceeb9fdcf6c716e118727a48f0a8d1674c
Angie <angheloalf95@gmail.com>
2021-11-14 16:01:29 -0300

    whoops

--- include/z64item.h
+++ include/z64item.h
@@ -327,7 +327,7 @@ typedef enum {
     /* 0x8D */ GI_MASK_BLAST,
     /* 0x8E */ GI_MASK_SCENTS,
     /* 0x8F */ GI_MASK_GIANT,
-    /* 0x96 */ GI_MOON_TEAR,
+    /* 0x96 */ GI_MOON_TEAR = 0x96,
     /* 0x97 */ GI_DEED_LAND,
     /* 0x98 */ GI_DEED_SWAMP,
     /* 0x99 */ GI_DEED_MOUNTAIN,
