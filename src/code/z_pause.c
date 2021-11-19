0bfa87481a3ed7b28265e61a3fb8a1521ccb2afd
Derek Hensley <hensley.derek58@gmail.com>
2021-11-19 07:40:03 -0800

    Rename functions

--- src/code/z_pause.c
+++ src/code/z_pause.c
@@ -1,11 +1,11 @@
 #include "global.h"
 
-void func_80122660(FrameAdvanceContext* frameAdvCtx) {
+void FrameAdvance_Init(FrameAdvanceContext* frameAdvCtx) {
     frameAdvCtx->timer = 0;
     frameAdvCtx->enabled = false;
 }
 
-s32 func_80122670(FrameAdvanceContext* frameAdvCtx, Input* input) {
+s32 FrameAdvance_Update(FrameAdvanceContext* frameAdvCtx, Input* input) {
     if (!frameAdvCtx->enabled || (CHECK_BTN_ALL(input->cur.button, BTN_Z) &&
                                   (CHECK_BTN_ALL(input->press.button, BTN_R) ||
                                    (CHECK_BTN_ALL(input->cur.button, BTN_R) && (++frameAdvCtx->timer >= 9))))) {
