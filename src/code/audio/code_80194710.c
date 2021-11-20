820643d78d8db167a57e8d76dec8790fcf475873
engineer124 <engineer124engineer124@gmail.com>
2021-11-20 11:32:10 +1100

    Add new line after variable declaration

--- src/code/audio/code_80194710.c
+++ src/code/audio/code_80194710.c
@@ -2,12 +2,14 @@
 
 void Audio_osInvalDCache(void* buf, s32 size) {
     OSIntMask prevMask = osSetIntMask(1);
+
     osInvalDCache(buf, size);
     osSetIntMask(prevMask);
 }
 
 void Audio_osWritebackDCache(void* buf, s32 size) {
     OSIntMask prevMask = osSetIntMask(1);
+    
     osWritebackDCache(buf, size);
     osSetIntMask(prevMask);
 }
