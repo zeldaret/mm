79ed96c82332f1b3f3e3edb56f6533662b6e2abb
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-11-22 18:52:33 +0000

    Some review

--- src/overlays/actors/ovl_En_Recepgirl/z_en_recepgirl.c
+++ src/overlays/actors/ovl_En_Recepgirl/z_en_recepgirl.c
@@ -134,16 +134,17 @@ void EnRecepgirl_Talk(EnRecepgirl* this, GlobalContext* globalCtx) {
         if (this->skelAnime.animation == &D_0600A280) {
             Animation_PlayLoop(&this->skelAnime, &D_06001384);
         } else if (this->skelAnime.animation == &D_0600AD98) {
-            if (this->actor.textId == 0x2ADA) { // "The room on your left is The Mayor's office [...]" (meeting ongoing)
+            if (this->actor.textId == 0x2ADA) { // Mayor's office on is the left (meeting ongoing)
                 Animation_MorphToPlayOnce(&this->skelAnime, &D_06000968, 10.0f);
             } else {
                 Animation_MorphToLoop(&this->skelAnime, &D_06009890, 10.0f);
             }
-        } else if (this->actor.textId ==
-                   0x2ADA) { // "The room on your left is The Mayor's office [...]" (meeting ongoing)
-            Animation_MorphToLoop(&this->skelAnime, &D_06009890, 10.0f);
         } else {
-            Animation_MorphToPlayOnce(&this->skelAnime, &D_0600A280, -4.0f);
+            if (this->actor.textId == 0x2ADA) { // Mayor's office on is the left (meeting ongoing)
+                Animation_MorphToLoop(&this->skelAnime, &D_06009890, 10.0f);
+            } else {
+                Animation_MorphToPlayOnce(&this->skelAnime, &D_0600A280, -4.0f);
+            }
         }
     }
 
@@ -155,10 +156,11 @@ void EnRecepgirl_Talk(EnRecepgirl* this, GlobalContext* globalCtx) {
         if (this->actor.textId == 0x2AD9) { // "Welcome..."
             Actor_SetSwitchFlag(globalCtx, this->actor.params);
             Animation_MorphToPlayOnce(&this->skelAnime, &D_0600AD98, 10.0f);
+
             if (gSaveContext.weekEventReg[63] & 0x80) { // showed Couple's Mask to meeting
-                this->actor.textId = 0x2ADF; // "The room on your left is The Mayor's office [...]" (meeting ended)
+                this->actor.textId = 0x2ADF;            // Mayor's office on is the left (meeting ended)
             } else {
-                this->actor.textId = 0x2ADA; // "The room on your left is The Mayor's office [...]" (meeting ongoing)
+                this->actor.textId = 0x2ADA; // Mayor's office on is the left (meeting ongoing)
             }
         } else if (this->actor.textId == 0x2ADC) { // hear directions again?
             Animation_MorphToPlayOnce(&this->skelAnime, &D_0600AD98, 10.0f);
@@ -166,11 +168,10 @@ void EnRecepgirl_Talk(EnRecepgirl* this, GlobalContext* globalCtx) {
         } else {
             Animation_MorphToPlayOnce(&this->skelAnime, &D_06000968, 10.0f);
 
-            if (this->actor.textId == 0x2ADD) { // "So..."
-                this->actor.textId = 0x2ADE;    // Mayor's office on is the left, drawing room on the right
-            } else if (this->actor.textId ==
-                       0x2ADA) {             // "The room on your left is The Mayor's office [...]" (meeting ongoing)
-                this->actor.textId = 0x2ADB; // drawing room on the right
+            if (this->actor.textId == 0x2ADD) {        // "So..."
+                this->actor.textId = 0x2ADE;           // Mayor's office on is the left, drawing room on the right
+            } else if (this->actor.textId == 0x2ADA) { // Mayor's office on is the left (meeting ongoing)
+                this->actor.textId = 0x2ADB;           // drawing room on the right
             } else {
                 this->actor.textId = 0x2AE0; // drawing room on the right, don't go in without an appointment
             }
--- src/overlays/actors/ovl_Obj_Tree/z_obj_tree.h
+++ src/overlays/actors/ovl_Obj_Tree/z_obj_tree.h
@@ -8,13 +8,13 @@ struct ObjTree;
 typedef void (*ObjTreeActionFunc)(struct ObjTree*, GlobalContext*);
 
 typedef struct ObjTree {
-    /* 0x0000 */ DynaPolyActor dyna;
-    /* 0x015C */ ColliderCylinder collider;
-    /* 0x01A8 */ ObjTreeActionFunc actionFunc;
-    /* 0x01AC */ f32 swayAmplitude;
-    /* 0x01B0 */ s16 swayAngle;
-    /* 0x01B2 */ s16 swayVelocity;
-    /* 0x01B4 */ s16 timer;
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ ColliderCylinder collider;
+    /* 0x1A8 */ ObjTreeActionFunc actionFunc;
+    /* 0x1AC */ f32 swayAmplitude;
+    /* 0x1B0 */ s16 swayAngle;
+    /* 0x1B2 */ s16 swayVelocity;
+    /* 0x1B4 */ s16 timer;
 } ObjTree; // size = 0x1B8
 
 extern const ActorInit Obj_Tree_InitVars;
