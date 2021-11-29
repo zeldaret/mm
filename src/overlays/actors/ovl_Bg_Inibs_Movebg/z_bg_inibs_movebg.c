f55e8e70d19121a63d700f26c11a0e43c91da667
retro <psychoticflora@gmail.com>
2021-11-29 06:09:37 +0000

    cleanup

--- src/overlays/actors/ovl_Bg_Inibs_Movebg/z_bg_inibs_movebg.c
+++ src/overlays/actors/ovl_Bg_Inibs_Movebg/z_bg_inibs_movebg.c
@@ -26,9 +26,9 @@ const ActorInit Bg_Inibs_Movebg_InitVars = {
     (ActorFunc)BgInibsMovebg_Draw,
 };
 
-s32 D_80B96560[] = { 0x060062D8, 0x06001DC0 };
-s32 D_80B96568[] = { 0x06006140, 0x06001C10 };
-s32 D_80B96570[] = { 0x06006858, 0x06002598 };
+Gfx* D_80B96560[] = { 0x060062D8, 0x06001DC0 };
+Gfx* D_80B96568[] = { 0x06006140, 0x06001C10 };
+AnimatedMaterial* D_80B96570[] = { 0x06006858, 0x06002598 };
 
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
@@ -39,38 +39,34 @@ void BgInibsMovebg_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DynaPolyActor_Init(&this->dyna, 1);
 
-    //for some reason doesn't match if I don't use the THIS macro twice here
-    THIS->unk_15C = D_80B96560[BGINIBSMOVEBG_GET_F(this->dyna)];
-    this->unk_160 = D_80B96568[BGINIBSMOVEBG_GET_F(this->dyna)];
-    this->unk_164 = D_80B96570[BGINIBSMOVEBG_GET_F(THIS->dyna)];
+    this->unk_15C = D_80B96560[BGINIBSMOVEBG_GET_F(thisx)];
+    this->unk_160 = D_80B96568[BGINIBSMOVEBG_GET_F(thisx)];
+    this->unk_164 = D_80B96570[BGINIBSMOVEBG_GET_F(thisx)];
 }
 
 void BgInibsMovebg_Destroy(Actor* thisx, GlobalContext* globalCtx) {
-    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, THIS->dyna.bgId);
+    BgInibsMovebg* this = THIS;
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
 }
 
 void BgInibsMovebg_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    s32 new_var;
-    s32 temp_a1;
-    BgInibsMovebg* new_var5;
-    s32 new_var4;
-    new_var5 = THIS;
-    new_var = new_var5->unk_164;
+    BgInibsMovebg* this = THIS;
+    AnimatedMaterial* animMat;
+    Gfx* dl1;
+    Gfx* dl2;
 
-    if (new_var != 0) {
-        new_var = new_var5->unk_164;
-        AnimatedMat_Draw(globalCtx, Lib_SegmentedToVirtual((void*)new_var));
+    animMat = this->unk_164;
+    if (animMat != 0) {
+        AnimatedMat_Draw(globalCtx, Lib_SegmentedToVirtual(this->unk_164));
     }
 
-    temp_a1 = new_var5->unk_15C;
-    if (temp_a1 != 0) {
-        func_800BDFC0(globalCtx, (void*)temp_a1);
+    dl1 = this->unk_15C;
+    if (dl1 != NULL) {
+        func_800BDFC0(globalCtx, this->unk_15C);
     }
 
-    new_var4 = new_var5->unk_160;
-    if (((BgInibsMovebg*)thisx)->unk_160 != 0) {
-        func_800BE03C(globalCtx, (void*)new_var4);
+    dl2 = this->unk_160;
+    if (dl2 != NULL) {
+        func_800BE03C(globalCtx, this->unk_160);
     }
-
-    if (new_var5->unk_160 != 0) {}
 }
--- src/overlays/actors/ovl_Bg_Inibs_Movebg/z_bg_inibs_movebg.h
+++ src/overlays/actors/ovl_Bg_Inibs_Movebg/z_bg_inibs_movebg.h
@@ -5,13 +5,13 @@
 
 struct BgInibsMovebg;
 
-#define BGINIBSMOVEBG_GET_F(thisx) (u16)(thisx.actor.params) & 0xF
+#define BGINIBSMOVEBG_GET_F(thisx) ((u16)((thisx)->params) & 0xF)
 
 typedef struct BgInibsMovebg {
     /* 0x000 */ DynaPolyActor dyna;
-    /* 0x15C */ s32 unk_15C;
-    /* 0x160 */ s32 unk_160;
-    /* 0x164 */ s32 unk_164;
+    /* 0x15C */ Gfx* unk_15C;
+    /* 0x160 */ Gfx* unk_160;
+    /* 0x164 */ AnimatedMaterial* unk_164;
 } BgInibsMovebg; // size = 0x168
 
 extern const ActorInit Bg_Inibs_Movebg_InitVars;
