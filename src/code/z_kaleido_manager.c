586b61343ad1fface1b4367ee457700398894ceb
shawlucas <lgs84@cornell.edu>
2021-11-29 15:27:50 -0500

    migrated data and bss for z_kaleido_manager, renamed ovl_Player_Actor to ovl_player_actor to make it work with the data

--- src/code/z_kaleido_manager.c
+++ src/code/z_kaleido_manager.c
@@ -1,15 +1,28 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/D_801DF9C0.s")
+#define KALEIDO_OVERLAY(name)                                                                                \
+    {                                                                                                        \
+        NULL, (u32)_ovl_##name##SegmentRomStart, (u32)_ovl_##name##SegmentRomEnd, _ovl_##name##SegmentStart, \
+            _ovl_##name##SegmentEnd, 0, #name,                                                               \
+    }
+
+KaleidoMgrOverlay gKaleidoMgrOverlayTable[] = {
+    KALEIDO_OVERLAY(kaleido_scope),
+    KALEIDO_OVERLAY(player_actor),
+};
+
+void* sKaleidoAreaPtr = NULL;
+KaleidoMgrOverlay* gKaleidoMgrCurOvl = NULL;
+FaultAddrConvClient sKaleidoAreaFaultClient;
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_80163700.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_80163758.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/KaleidoManager_LoadOvl.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_801637B4.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/KaleidoManager_ClearOvl.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_80163804.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/KaleidoManager_Init.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_8016388C.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/KaleidoManager_Destroy.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/func_801638D8.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_manager/KaleidoManager_GetRamAddr.s")
