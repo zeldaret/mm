10494213afbc4971305dd7b099e9ccda9212adaf
engineer124 <engineer124engineer124@gmail.com>
2021-12-07 10:32:03 +1100

    Name variables

--- src/code/audio/audio_init_params.c
+++ src/code/audio/audio_init_params.c
@@ -1,11 +1,11 @@
 #include "global.h"
 
-const s16 D_801E1100[] = {
+const s16 gAudioTatumInit[] = {
     0x1C00, // unused
     0x30,   // gTatumsPerBeat
 };
 
-const AudioContextInitSizes D_801E1104 = {
+const AudioContextInitSizes gAudioContextInitSizes = {
     0x137F00, // heapSize
     0x1C480,  // initPoolSize
     0x1A000,  // permanentPoolSize
