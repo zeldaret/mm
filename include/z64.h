7bfe411b4c2b25883ff78be32309b010a8a7d360
Derek Hensley <hensley.derek58@gmail.com>
2021-12-06 18:05:13 -0800

    Change char to UNK_TYPE1

--- include/z64.h
+++ include/z64.h
@@ -1461,24 +1461,24 @@ typedef struct {
 
 typedef struct EnHy {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x8];
+    /* 0x144 */ UNK_TYPE1 unk_144[0x8];
     /* 0x14C */ SkelAnime skelAnime;
     /* 0x190 */ s8 unk190;
     /* 0x191 */ s8 unk191;
     /* 0x192 */ s8 unk192;
     /* 0x193 */ s8 animObjIndex;
     /* 0x194 */ ColliderCylinder collider;
-    /* 0x1E0 */ char unk_1E0[0x4];
+    /* 0x1E0 */ UNK_TYPE1 unk_1E0[0x4];
     /* 0x1E4 */ Path* path;
     /* 0x1E8 */ s16 curPoint;
-    /* 0x1EA */ char unk_1EA[0x2];
+    /* 0x1EA */ UNK_TYPE1 unk_1EA[0x2];
     /* 0x1EC */ Vec3f leftFootPos;
     /* 0x1F8 */ Vec3f rightFootPos;
     /* 0x204 */ u8 isLeftFootOnGround;
     /* 0x205 */ u8 isRightFootOnGround;
     /* 0x206 */ Vec3s jointTable[16];
     /* 0x266 */ Vec3s morphTable[16];
-    /* 0x2C6 */ char unk_2C6[0x120];
+    /* 0x2C6 */ UNK_TYPE1 unk_2C6[0x120];
     /* 0x3E6 */ s16 eyeTexIndex;
     /* 0x3E8 */ s16 blinkTimer;
 } EnHy;
