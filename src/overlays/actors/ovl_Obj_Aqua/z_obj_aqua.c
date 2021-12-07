6ae28a789ac6a5a3c4cac69f7ecabc82b0d150e5
engineer124 <47598039+engineer124@users.noreply.github.com>
2021-12-07 13:13:18 +1100

    Macro Clean up (#454)
    * Add OS macros
    
    * MTXMODE cleanup
    
    * format
    
    * More matrix mode macros missed

--- src/overlays/actors/ovl_Obj_Aqua/z_obj_aqua.c
+++ src/overlays/actors/ovl_Obj_Aqua/z_obj_aqua.c
@@ -285,12 +285,12 @@ void ObjAqua_Draw(Actor* thisx, GlobalContext* globalCtx) {
     if (actionFuncTemp) {
         s16 rotation = Math_SinS(this->unk_198) * 8000.0f;
 
-        Matrix_InsertZRotation_s(rotation, 1);
-        Matrix_Scale(1.3f, 1.0f, 1.0f, 1);
-        Matrix_InsertZRotation_s(rotation * -1, 1);
-        Matrix_Scale(10.0f / 13.0f, 1.0f, 1.0f, 1);
+        Matrix_InsertZRotation_s(rotation, MTXMODE_APPLY);
+        Matrix_Scale(1.3f, 1.0f, 1.0f, MTXMODE_APPLY);
+        Matrix_InsertZRotation_s(rotation * -1, MTXMODE_APPLY);
+        Matrix_Scale(10.0f / 13.0f, 1.0f, 1.0f, MTXMODE_APPLY);
     }
-    Matrix_RotateY(yaw, 1);
+    Matrix_RotateY(yaw, MTXMODE_APPLY);
     gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gSPDisplayList(POLY_XLU_DISP++, D_0407D590);
     CLOSE_DISPS(globalCtx->state.gfxCtx);
