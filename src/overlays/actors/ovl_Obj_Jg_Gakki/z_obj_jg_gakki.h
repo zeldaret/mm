527e6940d6e325cb52de0dce580bd016901246bd
Savino Jossi <sjossi@users.noreply.github.com>
2021-12-14 10:04:12 -0500

    clean up old mips_to_c hints
    Co-authored-by: Anghelo Carvajal <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_Obj_Jg_Gakki/z_obj_jg_gakki.h
+++ src/overlays/actors/ovl_Obj_Jg_Gakki/z_obj_jg_gakki.h
@@ -7,9 +7,9 @@ struct ObjJgGakki;
 
 typedef struct ObjJgGakki {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ SkelAnime skelAnime;               /* inferred */
-    /* 0x188 */ ColliderCylinder colliderCylinder; /* maybe part of unk144[2]? */
-} ObjJgGakki;                                      /* size = 0x1D4 */
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ ColliderCylinder colliderCylinder;
+} ObjJgGakki; /* size = 0x1D4 */
 
 extern const ActorInit Obj_Jg_Gakki_InitVars;
 
