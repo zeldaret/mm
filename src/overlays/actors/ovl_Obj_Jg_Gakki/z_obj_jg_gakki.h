f95798e96ff239d4040761d065f629b022d73e55
Savino Jossi <sjossi@users.noreply.github.com>
2021-12-14 10:03:57 -0500

    clean up unnecessary mips_to_c hints
    Co-authored-by: Anghelo Carvajal <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_Obj_Jg_Gakki/z_obj_jg_gakki.h
+++ src/overlays/actors/ovl_Obj_Jg_Gakki/z_obj_jg_gakki.h
@@ -5,20 +5,6 @@
 
 struct ObjJgGakki;
 
-/* typedef struct ObjJgGakki { */
-/*     /1* 0x000 *1/ Actor actor; */
-/*     /1* 0x144 *1/ char pad144[4]; */
-/*     /1* 0x148 *1/ void** unk148;     /1* inferred *1/ */
-/*     /1* 0x14C *1/ char pad14C[0x18]; /1* maybe part of unk148[7]? *1/ */
-/*     /1* 0x164 *1/ Vec3s* unk164;     /1* inferred *1/ */
-/*     /1* 0x168 *1/ char pad168[0x6C]; /1* maybe part of unk164[0x1C]? *1/ */
-/* } ObjJgGakki;        &              /1* size = 0x1D4 *1/ */
-/* typedef struct ObjJgGakki { */
-/*     /1* 0x000 *1/ Actor actor; */
-/*     /1* 0x144 *1/ char pad144[0x44]; */
-/*     /1* 0x188 *1/ ColliderCylinder unk188; /1* inferred *1/ */
-/* } ObjJgGakki; /1* size = 0x1D4 *1/ */
-
 typedef struct ObjJgGakki {
     /* 0x000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;               /* inferred */
