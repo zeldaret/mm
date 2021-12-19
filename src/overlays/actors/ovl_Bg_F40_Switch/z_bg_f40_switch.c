484517479a7520c79324dce47fac8cbd35508b21
immibis <immibis@gmail.com>
2021-12-19 17:10:53 +0100

    Bg_F40_Switch OK and documented

--- src/overlays/actors/ovl_Bg_F40_Switch/z_bg_f40_switch.c
+++ src/overlays/actors/ovl_Bg_F40_Switch/z_bg_f40_switch.c
@@ -15,13 +15,13 @@ void BgF40Switch_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgF40Switch_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgF40Switch_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void func_80BC4B20(BgF40Switch* this, GlobalContext* globalCtx);
-void func_80BC4B94(BgF40Switch* this, GlobalContext* globalCtx);
-void func_80BC4BB8(BgF40Switch* this, GlobalContext* globalCtx);
-void func_80BC4C68(BgF40Switch* this, GlobalContext* globalCtx);
-void func_80BC4D30(BgF40Switch* this, GlobalContext* globalCtx);
+void func_80BC47B0(BgF40Switch* this, GlobalContext* globalCtx);
+void BgF40Switch_ActionBecomeNotPressed(BgF40Switch* this, GlobalContext* globalCtx);
+void BgF40Switch_ActionIdlePressed(BgF40Switch* this, GlobalContext* globalCtx);
+void BgF40Switch_ActionBecomePressed(BgF40Switch* this, GlobalContext* globalCtx);
+void BgF40Switch_ActionWaitToBecomePressed(BgF40Switch* this, GlobalContext* globalCtx);
+void BgF40Switch_ActionIdleNotPressed(BgF40Switch* this, GlobalContext* globalCtx);
 
-#if 0
 const ActorInit Bg_F40_Switch_InitVars = {
     ACTOR_BG_F40_SWITCH,
     ACTORCAT_SWITCH,
@@ -34,37 +34,167 @@ const ActorInit Bg_F40_Switch_InitVars = {
     (ActorFunc)BgF40Switch_Draw,
 };
 
-// static InitChainEntry sInitChain[] = {
-static InitChainEntry D_80BC4E04[] = {
+s32 sBgF40SwitchGlobalsInitialized = false;
+u32 sBgF40SwitchLastUpdateFrame;
+
+extern CollisionHeader object_f40_switch_Colheader_000118;
+extern Gfx object_f40_switch_DL_000438;
+extern Gfx object_f40_switch_DL_000390;
+
+/*
+ * Updates all instances of this actor in the current room, unless it's already been called this frame.
+ */
+void func_80BC47B0(BgF40Switch* this, GlobalContext* globalCtx) {
+    if (globalCtx->gameplayFrames != sBgF40SwitchLastUpdateFrame) {
+        u32 pressedSwitchFlags[4] = { 0 };
+        u32 pad;
+        s32 switchFlag;
+        s32 isPressed;
+        Actor* actor;
+        BgF40Switch* actorAsSwitch;
+        u32 unk40;
+
+        unk40 = func_801233E4(globalCtx);
+        for (actor = globalCtx->actorCtx.actorList[ACTORCAT_SWITCH].first; actor != NULL; actor = actor->next) {
+            if (actor->id == ACTOR_BG_F40_SWITCH && actor->room == this->actor.actor.room && actor->update != NULL) {
+                actorAsSwitch = (BgF40Switch*)actor;
+                actorAsSwitch->wasPressed = actorAsSwitch->isPressed;
+                isPressed = DynaPolyActor_IsInSwitchPressedState(&actorAsSwitch->actor);
+                if (actorAsSwitch->isPressed && actorAsSwitch->actionFunc == BgF40Switch_ActionIdlePressed) {
+                    // Switch is fully pressed - if there's nothing keeping it pressed, wait a short time before
+                    // reverting to unpressed state.
+                    if (isPressed || unk40) {
+                        actorAsSwitch->switchReleaseDelay = 6;
+                    } else {
+                        if (actorAsSwitch->switchReleaseDelay > 0) {
+                            actorAsSwitch->switchReleaseDelay--;
+                        } else {
+                            actorAsSwitch->isPressed = isPressed;
+                        }
+                    }
+                } else {
+                    // No delay when pressing switch, or releasing from a not-fully-pressed state.
+                    actorAsSwitch->isPressed = isPressed;
+                }
+                if (actorAsSwitch->isPressed) {
+                    switchFlag = GET_BGF40SWITCH_SWITCHFLAG(actorAsSwitch);
+                    if (switchFlag >= 0 && switchFlag < 0x80) {
+                        pressedSwitchFlags[(switchFlag & ~0x1F) >> 5] |= 1 << (switchFlag & 0x1F);
+                        if (!actorAsSwitch->wasPressed &&
+                            actorAsSwitch->actionFunc == BgF40Switch_ActionIdleNotPressed &&
+                            !Flags_GetSwitch(globalCtx, switchFlag)) {
+                            actorAsSwitch->isInitiator = true;
+                        }
+                    }
+                }
+            }
+        }
+        for (actor = globalCtx->actorCtx.actorList[ACTORCAT_SWITCH].first; actor != NULL; actor = actor->next) {
+            if (actor->id == ACTOR_BG_F40_SWITCH && actor->room == this->actor.actor.room && actor->update != 0) {
+                switchFlag = GET_BGF40SWITCH_SWITCHFLAG((BgF40Switch*)actor);
+                if (switchFlag >= 0 && switchFlag < 0x80 && Flags_GetSwitch(globalCtx, switchFlag) &&
+                    !(pressedSwitchFlags[(switchFlag & ~0x1F) >> 5] & (1 << (switchFlag & 0x1F)))) {
+                    Actor_UnsetSwitchFlag(globalCtx, switchFlag);
+                }
+            }
+        }
+        sBgF40SwitchLastUpdateFrame = globalCtx->gameplayFrames;
+    }
+}
+
+static InitChainEntry sInitChain[] = {
     ICHAIN_F32(uncullZoneForward, 4000, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneScale, 200, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneDownward, 200, ICHAIN_CONTINUE),
     ICHAIN_VEC3F_DIV1000(scale, 123, ICHAIN_STOP),
 };
 
-#endif
+void BgF40Switch_Init(Actor* thisx, GlobalContext* globalCtx) {
+    BgF40Switch* this = THIS;
 
-extern InitChainEntry D_80BC4E04[];
+    Actor_ProcessInitChain(&this->actor.actor, sInitChain);
+    this->actor.actor.scale.y = 0.165f;
+    this->actionFunc = BgF40Switch_ActionIdleNotPressed;
+    this->actor.actor.world.pos.y = this->actor.actor.home.pos.y + 1.0f;
+    DynaPolyActor_Init(&this->actor, 1);
+    DynaPolyActor_LoadMesh(globalCtx, &this->actor, &object_f40_switch_Colheader_000118);
+    if (!sBgF40SwitchGlobalsInitialized) {
+        sBgF40SwitchLastUpdateFrame = globalCtx->gameplayFrames;
+        sBgF40SwitchGlobalsInitialized = true;
+    }
+}
 
-extern UNK_TYPE D_06000118;
-extern UNK_TYPE D_06000438;
+void BgF40Switch_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    BgF40Switch* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC47B0.s")
+    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->actor.bgId);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/BgF40Switch_Init.s")
+void BgF40Switch_ActionBecomeNotPressed(BgF40Switch* this, GlobalContext* globalCtx) {
+    this->actor.actor.scale.y += 0.0495f;
+    if (this->actor.actor.scale.y >= 0.165f) {
+        Audio_PlayActorSound2(&this->actor.actor, NA_SE_EV_IKANA_BLOCK_SWITCH);
+        this->actionFunc = BgF40Switch_ActionIdleNotPressed;
+        this->actor.actor.scale.y = 0.165f;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/BgF40Switch_Destroy.s")
+void BgF40Switch_ActionIdlePressed(BgF40Switch* this, GlobalContext* globalCtx) {
+    if (!this->isPressed) {
+        this->actionFunc = BgF40Switch_ActionBecomeNotPressed;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC4B20.s")
+void BgF40Switch_ActionBecomePressed(BgF40Switch* this, GlobalContext* globalCtx) {
+    this->actor.actor.scale.y -= 0.0495f;
+    if (this->actor.actor.scale.y <= 0.0165f) {
+        Audio_PlayActorSound2(&this->actor.actor, NA_SE_EV_IKANA_BLOCK_SWITCH);
+        func_8013ECE0(this->actor.actor.xyzDistToPlayerSq, 120, 20, 10);
+        if (this->isInitiator) {
+            ActorCutscene_Stop(this->actor.actor.cutscene);
+            this->isInitiator = false;
+        }
+        this->actionFunc = BgF40Switch_ActionIdlePressed;
+        this->actor.actor.scale.y = 0.0165f;
+        this->switchReleaseDelay = 6;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC4B94.s")
+void BgF40Switch_ActionWaitToBecomePressed(BgF40Switch* this, GlobalContext* globalCtx) {
+    if (this->isInitiator == 0 || this->actor.actor.cutscene == -1) {
+        this->actionFunc = BgF40Switch_ActionBecomePressed;
+        if (this->isInitiator) {
+            Actor_SetSwitchFlag(globalCtx, GET_BGF40SWITCH_SWITCHFLAG(this));
+        }
+    } else {
+        if (ActorCutscene_GetCanPlayNext(this->actor.actor.cutscene)) {
+            ActorCutscene_StartAndSetUnkLinkFields(this->actor.actor.cutscene, &this->actor.actor);
+            this->actionFunc = BgF40Switch_ActionBecomePressed;
+            if (this->isInitiator) {
+                Actor_SetSwitchFlag(globalCtx, GET_BGF40SWITCH_SWITCHFLAG(this));
+            }
+        } else {
+            ActorCutscene_SetIntentToPlay(this->actor.actor.cutscene);
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC4BB8.s")
+void BgF40Switch_ActionIdleNotPressed(BgF40Switch* this, GlobalContext* globalCtx) {
+    if (this->isPressed) {
+        this->actionFunc = BgF40Switch_ActionWaitToBecomePressed;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC4C68.s")
+void BgF40Switch_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgF40Switch* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/func_80BC4D30.s")
+    func_80BC47B0(this, globalCtx);
+    this->actionFunc(this, globalCtx);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/BgF40Switch_Update.s")
+void BgF40Switch_Draw(Actor* thisx, GlobalContext* globalCtx) {
+    BgF40Switch* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_F40_Switch/BgF40Switch_Draw.s")
+    func_800BDFC0(globalCtx, &object_f40_switch_DL_000438);
+    func_800BDFC0(globalCtx, &object_f40_switch_DL_000390);
+}
