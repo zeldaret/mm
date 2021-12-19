038eb997d0de6e51871f6fc5ba3422c6eefcee3d
Tom Overton <tom-overton@users.noreply.github.com>
2021-12-19 11:40:39 -0800

    En_Tanron3 (Gyorg's fishes) OK and somewhat documented (#376)
    * Import data to C
    
    * EnTanron3_Init OK
    
    * EnTanron3_Destroy OK
    
    * func_80BB897C OK
    
    * func_80BB91D4 OK
    
    * func_80BB9288 OK
    
    * EnTanron3_Draw OK
    
    * func_80BB95FC OK
    
    * EnTanron3_Update OK
    
    * func_80BB9308 OK
    
    * func_80BB87D4 OK
    
    * func_80BB85A0 OK
    
    * func_80BB8A48 OK
    
    * Update spec with compiled reloc
    
    * Name most of the functions
    
    * Name a few more things
    
    * Merge animation system changes
    
    * Name remaining unnamed variables
    
    * Finish documentation
    
    * Use modulo for fogTimer
    
    * Move enum/structs to .h
    
    * boss03Parent -> sGyorg
    
    * D_80BB9720 -> sZeroVec
    
    * Move initialization of effectPtr up
    
    * Use 2 * M_PI
    
    * Use MTXMODE_NEW
    
    * 65536.0f -> 0x10000
    
    * timer += 1 -> timer++
    
    * Timer cleanup
    
    * Remove unneccesary parentheses
    
    * When performing bitwise operations on timer, use hex
    
    * Lowercase "fish" in the comment at the top
    
    * Respond to jpburnett's feedback
    
    * Respond to Elliptic and Kenix's reviews
    
    * Undo renaming the Gyorg var, but keep the comment
    
    * Add a little bit more documentation
    
    * Variable renames
    
    * Remove a comment that no longer applies
    
    * Document some swimming behavior
    
    * PICK_DIRECTION -> PICK_NEW_DEVIATION
    
    * Minor comment tweak
    
    * Respond to Elliptic's review
    
    * Remove timer inconsistency

--- src/overlays/actors/ovl_Boss_03/z_boss_03.h
+++ src/overlays/actors/ovl_Boss_03/z_boss_03.h
@@ -9,9 +9,13 @@ typedef void (*Boss03ActionFunc)(struct Boss03*, GlobalContext*);
 
 typedef struct Boss03 {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x1E4];
+    /* 0x0144 */ UNK_TYPE1 unk_144[0x10E];
+    /* 0x0252 */ s8 unk_252; // number of Tanron3 fish that are currently alive, maybe "numSmallFishAlive"?
+    /* 0x0253 */ UNK_TYPE1 unk_253[0xD1];
+    /* 0x0324 */ s16 unk_324;
+    /* 0x0326 */ UNK_TYPE1 unk_326[0x2];
     /* 0x0328 */ Boss03ActionFunc actionFunc;
-    /* 0x032C */ char unk_32C[0x250];
+    /* 0x032C */ UNK_TYPE1 unk_32C[0x250];
 } Boss03; // size = 0x57C
 
 extern const ActorInit Boss_03_InitVars;
