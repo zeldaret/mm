e6c4a78f773890cde50edcb971eb8c73dd5c1541
angie <angheloalf95@gmail.com>
2021-12-22 11:50:07 -0300

    mips2c general pass

--- src/overlays/actors/ovl_En_Nb/z_en_nb.c
+++ src/overlays/actors/ovl_En_Nb/z_en_nb.c
@@ -18,6 +18,12 @@ void EnNb_Draw(Actor* thisx, GlobalContext* globalCtx);
 void func_80BC0D84(EnNb* this, GlobalContext* globalCtx);
 void func_80BC0EAC(EnNb* this, GlobalContext* globalCtx);
 
+void func_80BC08E0(EnNb* this, GlobalContext* globalCtx);
+void func_80BC0978(EnNb* this, GlobalContext* globalCtx);
+
+s32 func_80BC00AC(Actor* this, GlobalContext* globalCtx);
+s32 func_80BC01DC(Actor* this, GlobalContext* globalCtx);
+
 #if 0
 const ActorInit En_Nb_InitVars = {
     ACTOR_EN_NB,
@@ -48,48 +54,66 @@ extern CollisionCheckInfoInit2 D_80BC161C;
 
 extern UNK_TYPE D_06008C40;
 
+Actor* func_80BBFDB0(EnNb* this, GlobalContext* globalCtx, s32 arg2, s16 arg3);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFDB0.s")
 
+void func_80BBFE60(EnNb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFE60.s")
 
+UNK_TYPE func_80BBFE8C(EnNb* this, s32 arg1);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFE8C.s")
 
+void func_80BBFF24(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFF24.s")
 
+Actor* func_80BBFF90(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFF90.s")
 
+s32 func_80BBFFD4(EnNb* this, s16 arg1);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BBFFD4.s")
 
+s8 func_80BC0050(EnNb* this, s32 arg1);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0050.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC00AC.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC01DC.s")
 
+UNK_PTR func_80BC045C(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC045C.s")
 
+s32 func_80BC04FC(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC04FC.s")
 
+void func_80BC05A8(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC05A8.s")
 
+void func_80BC06C4(EnNb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC06C4.s")
 
+void func_80BC0800(EnNb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0800.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC08E0.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0978.s")
 
+UNK_TYPE func_80BC0A18(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0A18.s")
 
+// ?
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0B98.s")
 
+// ?
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0C0C.s")
 
+UNK_TYPE func_80BC0C80(EnNb* this, GlobalContext* globalCtx, u8* arg2);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0C80.s")
 
+// ?
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0D08.s")
 
+void func_80BC0D1C(EnNb* this, GlobalContext* globalCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0D1C.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC0D84.s")
@@ -102,10 +126,16 @@ extern UNK_TYPE D_06008C40;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/EnNb_Update.s")
 
+// OverrideLimbDraw
+s32 func_80BC1174(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC1174.s")
 
+// PostLimbDraw
+void func_80BC11B4(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC11B4.s")
 
+// UnkActorDraw
+void func_80BC1278(GlobalContext* globalCtx, s32 limbIndex, Actor* thisx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/func_80BC1278.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Nb/EnNb_Draw.s")
--- src/overlays/actors/ovl_En_Nb/z_en_nb.h
+++ src/overlays/actors/ovl_En_Nb/z_en_nb.h
@@ -6,12 +6,22 @@
 struct EnNb;
 
 typedef void (*EnNbActionFunc)(struct EnNb*, GlobalContext*);
+typedef s32 (*EnNbActionFunc2)(struct EnNb*, GlobalContext*);
 
 typedef struct EnNb {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x44];
-    /* 0x0188 */ EnNbActionFunc actionFunc;
-    /* 0x018C */ char unk_18C[0x110];
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ EnNbActionFunc actionFunc;
+    /* 0x18C */ EnNbActionFunc unk_18C;
+    /* 0x190 */ ColliderCylinder collider;
+    /* 0x1DC */ u8 unk_1DC;
+    /* 0x1DD */ char unk_1DD[0x3];
+    /* 0x1E0 */ void* unk_1E0;
+    /* 0x1E4 */ s32 unk_1E4;
+    /* 0x1E8 */ Actor* unk_1E8;
+    /* 0x1EC */ char unk_1EC[0xA0];
+    /* 0x28C */ EnNbActionFunc2 unk_28C;
+    /* 0x290 */ char unk_290[0xC];
 } EnNb; // size = 0x29C
 
 extern const ActorInit En_Nb_InitVars;
