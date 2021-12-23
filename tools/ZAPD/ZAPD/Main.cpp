c520c8120eca7503b0a96af87154267af4b4dbe4
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2021-12-23 15:03:11 +0000

    git subrepo pull --force tools/ZAPD
    subrepo:
      subdir:   "tools/ZAPD"
      merged:   "155a463a5"
    upstream:
      origin:   "https://github.com/zeldaret/ZAPD.git"
      branch:   "master"
      commit:   "155a463a5"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

--- tools/ZAPD/ZAPD/Main.cpp
+++ tools/ZAPD/ZAPD/Main.cpp
@@ -48,7 +48,7 @@ void ErrorHandler(int sig)
 	const char* crashEasterEgg[] = {
 		"\tYou've met with a terrible fate, haven't you?",
 		"\tSEA BEARS FOAM. SLEEP BEARS DREAMS. \n\tBOTH END IN THE SAME WAY: CRASSSH!",
-		"ZAPD has fallen and cannot get up."
+		"\tZAPD has fallen and cannot get up.",
 	};
 
 	srand(time(nullptr));
