154211e7a9405ca5a0db9617a3507a5ea797b47d
Parker Burnett <jpburnett@gwu.edu>
2021-12-28 14:11:21 -0700

    getting some matching done

--- src/overlays/actors/ovl_En_Shn/z_en_shn.c
+++ src/overlays/actors/ovl_En_Shn/z_en_shn.c
@@ -2,6 +2,7 @@
  * File: z_en_shn.c
  * Overlay: ovl_En_Shn
  * Description: Swamp Tourist Center Guide
+ * Shashin
  */
 
 #include "z_en_shn.h"
@@ -36,7 +37,11 @@ const ActorInit En_Shn_InitVars = {
 extern UNK_TYPE D_0600B738;
 extern UNK_TYPE D_0600E7D0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE6130.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE6130.s")
+void func_80AE6130(EnShn* this) {
+    this->unk148.playSpeed = this->unk2CC;
+    SkelAnime_Update(&this->unk148);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE615C.s")
 
@@ -58,15 +63,61 @@ extern UNK_TYPE D_0600E7D0;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE68F0.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE69E8.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE69E8.s")
+void func_80AE69E8(EnShn* this, GlobalContext* globalCtx) {
+    Math_ApproachS(&this->actor.shape.rot.y, this->actor.world.rot.y, 3, 0x2AA8);
+    if ((gSaveContext.weekEventReg[0x17] & 8) && (func_80AE61C0(this))) {
+        this->unk1D8 |= 8;
+    } else {
+        this->unk1D8 &= 0xFFF7;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE6A64.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Init.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Init.s")
+void EnShn_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnShn* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Destroy.s")
+    ActorShape_Init(&this->actor.shape, 0.0f, NULL, 0.0f);
+    SkelAnime_InitFlex(globalCtx, &this->unk148, &D_0600E7D0, NULL, &this->unk1FA, &this->unk25A, 16);
+    this->unk2E8 = -1;
+    if ((gSaveContext.weekEventReg[0x17] & 8) != 0) {
+        func_80AE615C(this, 0);
+    } else {
+        func_80AE615C(this, 2);
+    }
+    this->actor.targetMode = 6;
+    Actor_SetScale(&this->actor, 0.01f);
+    this->unk2E0 = 0;
+    this->unk2D8 = 0;
+    this->unk1D8 = 0;
+    if (gSaveContext.entranceIndex != 0xA820) {
+        func_8013AED4(&this->unk1D8, 3, 7);
+        this->unk2BE = 0;
+    } else {
+        func_8013AED4(&this->unk1D8, 4, 7);
+        this->unk2BE = 1;
+    }
+    this->actionFunc = func_80AE69E8;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Update.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Destroy.s")
+void EnShn_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/EnShn_Update.s")
+void EnShn_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnShn* this = THIS;
+
+    func_80AE68F0(this, globalCtx);
+    this->actionFunc(this, globalCtx);
+    func_80AE65F4(this, globalCtx);
+    func_80AE6130(this);
+    func_80AE63A8(this, globalCtx);
+    this->unk2E0 = 0;
+    func_8013C964(&this->actor, globalCtx, 120.0f, 40.0f, 0, this->unk1D8 & 7);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Shn/func_80AE6CF0.s")
 
--- src/overlays/actors/ovl_En_Shn/z_en_shn.h
+++ src/overlays/actors/ovl_En_Shn/z_en_shn.h
@@ -8,10 +8,27 @@ struct EnShn;
 typedef void (*EnShnActionFunc)(struct EnShn*, GlobalContext*);
 
 typedef struct EnShn {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ EnShnActionFunc actionFunc;
-    /* 0x0148 */ char unk_148[0x1A8];
-} EnShn; // size = 0x2F0
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ EnShnActionFunc actionFunc;
+    /* 0x148 */ SkelAnime unk148;                   /* inferred */
+    /* 0x18C */ char pad18C[0x4C];                  /* maybe part of unk148[2]? */
+    /* 0x1D8 */ u16 unk1D8;
+    /* 0x1DA */ char pad1DA[0x20];                  /* maybe part of unk1D8[0x11]? */
+    /* 0x1FA */ Vec3s unk1FA;
+    /* 0x200 */ char pad200[0x5A];                  /* maybe part of unk1FA[0x10]? */
+    /* 0x25A */ Vec3s unk25A;
+    /* 0x260 */ char pad260[0x5E];                  /* maybe part of unk25A[0x10]? */
+    /* 0x2BE */ s16 unk2BE;
+    /* 0x2C0 */ char pad2C0[0xC];                   /* maybe part of unk2BE[7]? */
+    /* 0x2CC */ f32 unk2CC;                         /* inferred */
+    /* 0x2D0 */ char pad2D0[8];                     /* maybe part of unk2CC[3]? */
+    /* 0x2D8 */ s32 unk2D8;
+    /* 0x2DC */ char pad2DC[4];
+    /* 0x2E0 */ s32 unk2E0;
+    /* 0x2E4 */ char pad2E4[4];
+    /* 0x2E8 */ s32 unk2E8;
+    /* 0x2EC */ char pad2EC[4];
+} EnShn;  // size = 0x2F0
 
 extern const ActorInit En_Shn_InitVars;
 
