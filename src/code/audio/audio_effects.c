3db898ea753fba366bb1b5a55e8964c4680be402
engineer124 <engineer124engineer124@gmail.com>
2021-12-30 15:14:07 +1100

    Audio Playback OK

--- src/code/audio/audio_effects.c
+++ src/code/audio/audio_effects.c
@@ -10,12 +10,12 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80196DB4.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80196FEC.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/AudioEffects_NoteVibratoUpdate.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80197048.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/AudioEffects_NoteVibratoInit.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80197138.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/AudioEffects_NotePortamentoInit.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80197164.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/AudioEffects_AdsrInit.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/func_80197188.s")
+#pragma GLOBAL_ASM("asm/non_matchings/code/audio_effects/AudioEffects_AdsrUpdate.s")
