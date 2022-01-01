86dd9c787584a2d9bba9e74a6c9ed1832db0bddf
Isghj8 <isghj8@gmail.com>
2021-12-31 22:16:36 -0800

    EnAni:Start

--- src/overlays/actors/ovl_En_Ani/z_en_ani.c
+++ src/overlays/actors/ovl_En_Ani/z_en_ani.c
@@ -15,6 +15,8 @@ void EnAni_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnAni_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnAni_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_809679D0(EnAni* this, GlobalContext* globalCtx);
+
 #if 0
 const ActorInit En_Ani_InitVars = {
     ACTOR_EN_ANI,
@@ -50,7 +52,17 @@ extern UNK_TYPE D_06000C14;
 extern UNK_TYPE D_060011CC;
 extern UNK_TYPE D_060027A0;
 extern UNK_TYPE D_06009220;
-extern UNK_TYPE D_06009D34;
+extern AnimationHeader D_06009D34;
+
+void func_80967AB4(void *arg0);                       /* static */
+void func_80967DA0(EnAni *this, GlobalContext *arg1); /* static */
+void func_80967E34(Actor *this, GlobalContext *arg1); /* static */
+void func_80968164(EnAni *this, GlobalContext *arg1); /* static */
+extern AnimationHeader D_060007FC;
+extern AnimationHeader D_06001D48;
+extern FlexSkeletonHeader D_060028A0;
+extern Vec3f D_809686A4;
+extern UNK_TYPE D_809686B0;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Ani/func_809679D0.s")
 
@@ -58,7 +70,40 @@ extern UNK_TYPE D_06009D34;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Ani/func_80967AB4.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Ani/EnAni_Init.s")
+void EnAni_Init(Actor *thisx, GlobalContext *globalCtx) {
+    EnAni *this = THIS;
+    s32 pad;
+
+    Actor_ProcessInitChain(&this->actor, D_8096869C);
+    ActorShape_Init(&this->actor.shape, 0.0f, func_800B3FC0, 24.0f);
+    SkelAnime_InitFlex(globalCtx, &this->unk1DC, &D_060028A0, (AnimationHeader *) &D_06009D34, &this->unk220, &this->unk280, 0x10);
+    Animation_PlayOnce(&this->unk1DC, (AnimationHeader *) &D_06009D34);
+    Collider_InitAndSetCylinder(globalCtx, &this->unk144, &this->actor, &D_80968670);
+    Collider_InitAndSetCylinder(globalCtx, &this->unk190, &this->actor, &D_80968670);
+    Collider_UpdateCylinder(&this->actor, &this->unk190);
+    this->actor.colChkInfo.mass = MASS_IMMOVABLE;
+    this->unk2EC = 0;
+    this->unk2EE = 0;
+    this->unk2F0 = 0;
+    this->actionFunc = func_809679D0;
+    if ((this->actor.params & 0xFF) == 1) {
+        Animation_Change(&this->unk1DC, &D_06001D48, 1.0f, 0.0f, 
+              Animation_GetLastFrame(&D_06001D48), 2, 0.0f);
+        this->unk2FC = func_80968164;
+        this->actor.velocity.y = 0.0f;
+        this->actor.minVelocityY = 0.0f;
+        this->actor.gravity = 0.0f;
+        this->actor.flags |= 0x10;
+        this->unk2EC |= 4;
+        gSaveContext.eventInf[1] &= 0xEF;
+    }else {
+        this->unk190.dim.radius = 0x3C;
+        this->unk2FC = func_80967DA0;
+        this->actor.velocity.y = -25.0f;
+        this->actor.minVelocityY = -25.0f;
+        this->actor.gravity = -5.0f;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Ani/EnAni_Destroy.s")
 
--- src/overlays/actors/ovl_En_Ani/z_en_ani.h
+++ src/overlays/actors/ovl_En_Ani/z_en_ani.h
@@ -8,10 +8,25 @@ struct EnAni;
 typedef void (*EnAniActionFunc)(struct EnAni*, GlobalContext*);
 
 typedef struct EnAni {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x1B0];
-    /* 0x02F4 */ EnAniActionFunc actionFunc;
-    /* 0x02F8 */ char unk_2F8[0x8];
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ ColliderCylinder unk144;            /* inferred */
+    /* 0x190 */ ColliderCylinder unk190;            /* inferred */
+    /* 0x1DC */ SkelAnime unk1DC;                   /* inferred */
+    /* 0x220 */ Vec3s unk220;                       /* inferred */
+    /* 0x226 */ char pad226[0x5A];                  /* maybe part of unk220[0x10]? */
+    /* 0x280 */ Vec3s unk280;                       /* inferred */
+    /* 0x286 */ char pad286[0x5A];                  /* maybe part of unk280[0x10]? */
+    /* 0x2E0 */ Vec3s unk2E0;                       /* inferred */
+    /* 0x2E6 */ Vec3s unk2E6;                       /* inferred */
+    /* 0x2EC */ u16 unk2EC;                         /* inferred */
+    /* 0x2EE */ s16 unk2EE;                         /* inferred */
+    /* 0x2F0 */ s16 unk2F0;                         /* inferred */
+    /* 0x2F2 */ char pad2F2[2];
+    /* 0x2F4 */ EnAniActionFunc actionFunc;
+    /* 0x2F8 */ s16 unk2F8;                         /* inferred */
+    /* 0x2FA */ s16 unk2FA;                         /* inferred */
+    ///* 0x2FC */ void (*unk2FC)(EnAni *, GlobalContext *); /* inferred */
+    /* 0x2FC */ void* unk2FC; /* inferred */
 } EnAni; // size = 0x300
 
 extern const ActorInit En_Ani_InitVars;
