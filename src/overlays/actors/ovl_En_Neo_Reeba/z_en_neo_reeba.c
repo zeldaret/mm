8447de1b33482b46292873599f2462a088592596
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-12-31 21:22:59 -0700

    match some functions

--- src/overlays/actors/ovl_En_Neo_Reeba/z_en_neo_reeba.c
+++ src/overlays/actors/ovl_En_Neo_Reeba/z_en_neo_reeba.c
@@ -76,57 +76,375 @@ static ColliderCylinderInit D_80B7E520 = {
 extern DamageTable D_80B7E500;
 extern ColliderCylinderInit D_80B7E520;
 
-extern UNK_TYPE D_060001E4;
+extern AnimationHeader D_060001E4;
+extern SkeletonHeader D_06001EE8;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Init.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Init.s")
+void func_80B7CA34(EnNeoReeba*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Destroy.s")
+void EnNeoReeba_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnNeoReeba* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CA34.s")
+    ActorShape_Init(&this->actor.shape, 0.0f, func_800B3FC0, 0.0f);
+    SkelAnime_Init(globalCtx, &this->skelAnime, &D_06001EE8, &D_060001E4, this->jointTable, this->morphTable, 18);
+    if (!(this->actor.params & 0x8000)) {
+        Actor_SetScale(&this->actor, 0.04f);
+        this->actor.colChkInfo.mass = 0x5A;
+        this->actor.colChkInfo.health = 1;
+    } else {
+        Actor_SetScale(&this->actor, 0.05f);
+        this->actor.colChkInfo.mass = 0xB4;
+        this->actor.colChkInfo.health = 3;
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CA70.s")
+    this->actor.colChkInfo.damageTable = &D_80B7E500;
+    this->actor.targetMode = 2;
+    this->actor.hintId = 0x47;
+    this->actor.gravity = -0.5f;
+    this->unk_2B0 = gZeroVec3f;
+    this->unk_2BC = gZeroVec3f;
+    this->unk_308 = 0;
+    this->unk_30C = 0;
+    this->unk_30E = 0;
+    this->unk_30A = 0;
+    this->unk_2F8 = 0.0f;
+    this->unk_2FC = 0.0f;
+    Collider_InitCylinder(globalCtx, &this->collider);
+    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &D_80B7E520);
+    func_80B7CA34(this);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CB3C.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Destroy.s")
+void EnNeoReeba_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    EnNeoReeba* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CB88.s")
+    Collider_DestroyCylinder(globalCtx, &this->collider);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CCE0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CA34.s")
+void func_80B7CA70(EnNeoReeba*, GlobalContext*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CD28.s")
+void func_80B7CA34(EnNeoReeba* this) {
+    this->actor.flags &= -2;
+    this->actor.draw = NULL;
+    this->unk_30A = 0xA;
+    this->actionFunc = func_80B7CA70;
+    this->actor.shape.yOffset = -2000.0f;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CE34.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CA70.s")
+void func_80B7CE34(EnNeoReeba*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CE94.s")
+void func_80B7CA70(EnNeoReeba* this, GlobalContext* globalCtx) {
+    Player* player = GET_PLAYER(globalCtx);
+    s32 pad;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CFA0.s")
+    if ((Actor_XZDistanceToPoint(&player->actor, &this->actor.home.pos) < 200.0f) &&
+        (Player_GetMask(globalCtx) != PLAYER_MASK_STONE) && (fabsf(this->actor.playerHeightRel) < 100.0f)) {
+        func_80B7CE34(this);
+    }
+    if (this->unk_30A == 0) {
+        this->actor.world.pos = this->actor.home.pos;
+        this->unk_30A = -1;
+    } else {
+        this->unk_30A--;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CFFC.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CB3C.s")
+void func_80B7CB88(EnNeoReeba*, GlobalContext*); /* extern */
+void func_80B7CFFC(EnNeoReeba*, GlobalContext*);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D130.s")
+void func_80B7CB3C(EnNeoReeba* this) {
+    this->actor.shape.yOffset = 0.0f;
+    if (this->actionFunc == func_80B7CFFC) {
+        this->unk_30A = 0x14;
+    } else {
+        this->unk_30A = 0xA;
+    }
+    this->actionFunc = func_80B7CB88;
+    this->skelAnime.playSpeed = 1.0f;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D150.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CB88.s")
+void func_80B7CCE0(EnNeoReeba*); /* extern */
+void func_80B7CFA0(EnNeoReeba*); /* extern */
+void func_80B7D130(EnNeoReeba*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D254.s")
+void func_80B7CB88(EnNeoReeba* this, GlobalContext* globalCtx) {
+    Player* player;
+    f32 temp_f0;
+    f32 temp_f2;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D2E4.s")
+    player = GET_PLAYER(globalCtx);
+    temp_f0 = Actor_XZDistanceToPoint(&player->actor, &this->actor.home.pos);
+    if ((temp_f0 > 200.0f) || (fabsf(this->actor.playerHeightRel) > 100.0f)) {
+        func_80B7CCE0(this);
+    } else {
+        if (this->unk_30A == 0) {
+            if ((temp_f0 < 140.0f) && (fabsf(this->actor.playerHeightRel) < 100.0f)) {
+                this->unk_2B0 = player->actor.world.pos;
+                this->unk_2B0.x += 10.0f * player->actor.speedXZ * Math_SinS(player->actor.world.rot.y);
+                this->unk_2B0.z += 10.0f * player->actor.speedXZ * Math_CosS(player->actor.world.rot.y);
+                func_80B7CFA0(this);
+            } else {
+                func_80B7D130(this);
+            }
+        }
+    }
+    if (this->unk_30A != 0) {
+        this->unk_30A--;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D360.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CCE0.s")
+void func_80B7CD28(EnNeoReeba*, GlobalContext*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D398.s")
+void func_80B7CCE0(EnNeoReeba* this) {
+    this->unk_2F8 = 0.0f;
+    this->skelAnime.playSpeed = 2.0f;
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_AKINDONUTS_HIDE);
+    this->actionFunc = func_80B7CD28;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D3EC.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CD28.s")
+void func_80B7CD28(EnNeoReeba* this, GlobalContext* globalCtx) {
+    if (Math_SmoothStepToF(&this->actor.shape.yOffset, -2000.0f, 0.5f, this->unk_2F8, 10.0f) == 0.0f) {
+        func_80B7CA34(this);
+    } else if ((globalCtx->gameplayFrames % 4) == 0) {
+        func_800BBDAC(globalCtx, &this->actor, &this->actor.world.pos, this->actor.shape.shadowScale, 1, 8.0f, 500, 10,
+                      1);
+    }
+    if (this->unk_2F8 < 300.0f) {
+        this->unk_2F8 += 20.0f;
+    }
+    Math_ApproachF(&this->actor.shape.shadowScale, 0.0f, 1.0f, 1.0f);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D47C.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CE34.s")
+void func_80B7CE94(EnNeoReeba*, GlobalContext*); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D4FC.s")
+void func_80B7CE34(EnNeoReeba* this) {
+    this->actor.draw = EnNeoReeba_Draw;
+    this->unk_2F8 = 300.0f;
+    this->skelAnime.playSpeed = 2.0f;
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_STALKID_APPEAR);
+    this->actor.flags |= 1;
+    this->actionFunc = func_80B7CE94;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D5A4.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CE94.s")
+void func_80B7CB3C(EnNeoReeba*); /* extern */
+
+void func_80B7CE94(EnNeoReeba* this, GlobalContext* globalCtx) {
+    if (Math_SmoothStepToF(&this->actor.shape.yOffset, 0.0f, 0.5f, this->unk_2F8, 10.0f) == 0.0f) {
+        func_80B7CB3C(this);
+    } else if ((globalCtx->gameplayFrames & 3) == 0) {
+        func_800BBDAC(globalCtx, &this->actor, &this->actor.world.pos, this->actor.shape.shadowScale, 1, 8.0f, 500, 10,
+                      1);
+    }
+    if (this->unk_2F8 > 20.0f) {
+        this->unk_2F8 = this->unk_2F8 - 10.0f;
+    }
+    Math_ApproachF(&this->actor.shape.shadowScale, 12.0f, 1.0f, 1.0f);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CFA0.s")
+void func_80B7CFA0(EnNeoReeba* this) {
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_RIVA_MOVE);
+    this->unk_30C = 0xA;
+    this->unk_30A = 0x3C;
+    this->actionFunc = func_80B7CFFC;
+    this->skelAnime.playSpeed = 2.0f;
+    this->actor.speedXZ = 14.0f;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7CFFC.s")
+void func_80B7CFFC(EnNeoReeba* this, GlobalContext* globalCtx) {
+    f32 sp3C = Math_Vec3f_StepToXZ(&this->actor.world.pos, &this->unk_2B0, this->actor.speedXZ);
+
+    func_800BBDAC(globalCtx, &this->actor, &this->actor.world.pos, this->actor.shape.shadowScale, 1, 4.0f, (s16)0xFA,
+                  (s16)0xA, (u8)1);
+    if (sp3C < 2.0f) {
+        func_80B7CB3C(this);
+    } else if (sp3C < 40.0f && this->actor.speedXZ > 3.0f) {
+        this->actor.speedXZ -= 2.0f;
+    }
+    if (this->unk_30C == 0) {
+        Audio_PlayActorSound2(&this->actor, NA_SE_EN_RIVA_MOVE);
+        this->unk_30C = 0xA;
+    } else {
+        this->unk_30C--;
+    }
+    if (this->unk_30A == 0) {
+        func_80B7CB3C(this);
+    } else {
+        this->unk_30A--;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D130.s")
+void func_80B7D150(EnNeoReeba*, GlobalContext*); /* extern */
+
+void func_80B7D130(EnNeoReeba* this) {
+    this->actionFunc = func_80B7D150;
+    this->actor.speedXZ = 6.0f;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D150.s")
+void func_80B7D150(EnNeoReeba* this, GlobalContext* globalCtx) {
+    Player* player = GET_PLAYER(globalCtx);
+    f32 temp_f0;
+    s32 pad;
+
+    temp_f0 = Math_Vec3f_StepToXZ(&this->actor.world.pos, &this->actor.home.pos, this->actor.speedXZ);
+    if (temp_f0 < 2.0f) {
+        func_80B7CB3C(this);
+    } else if (temp_f0 < 40.0f && this->actor.speedXZ > 3.0f) {
+        this->actor.speedXZ -= 1.0f;
+    }
+    if ((Actor_XZDistanceToPoint(&player->actor, &this->actor.home.pos) > 200.0f) ||
+        (fabsf(this->actor.playerHeightRel) > 100.0f)) {
+        func_80B7CCE0(this);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D254.s")
+void func_80B7D2E4(EnNeoReeba*, GlobalContext*); /* extern */
+
+void func_80B7D254(EnNeoReeba* this) {
+    this->unk_30A = 0x3C;
+    this->unk_2B0 = this->actor.world.pos;
+    this->unk_2B0.x -= 20.0f * Math_SinS(this->actor.yawTowardsPlayer);
+    this->unk_2B0.z -= 20.0f * Math_CosS(this->actor.yawTowardsPlayer);
+    this->actionFunc = func_80B7D2E4;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D2E4.s")
+void func_80B7D2E4(EnNeoReeba* this, GlobalContext* globalCtx) {
+    if (Math_Vec3f_StepToXZ(&this->actor.world.pos, &this->unk_2B0, this->actor.speedXZ) < 2.0f) {
+        func_80B7CB3C(this);
+    }
+    if (this->unk_30A == 0) {
+        func_80B7CB3C(this);
+    } else {
+        this->unk_30A--;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D360.s")
+void func_80B7D398(EnNeoReeba*, GlobalContext*);
+void func_80B7D360(EnNeoReeba* this) {
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_COMMON_FREEZE);
+    this->actionFunc = func_80B7D398;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D398.s")
+void func_80B7D398(EnNeoReeba* this, GlobalContext* globalCtx) {
+    if (this->unk_30E > 0) {
+        this->unk_30E--;
+    } else {
+        this->unk_300 = 0.0f;
+        this->unk_304 = 0.0f;
+    }
+    if (this->actor.colorFilterTimer == 0) {
+        this->unk_30E = 0;
+        func_80B7CB3C(this);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D3EC.s")
+void func_80B7D47C(EnNeoReeba*, GlobalContext*);
+void func_80B7D3EC(EnNeoReeba* this) {
+    this->unk_310 = 0xB;
+    this->unk_304 = 0.5f;
+    this->unk_300 = 1.0f;
+    if (this->actor.params & 0x8000) {
+        this->unk_304 *= 1.5f;
+    }
+    if (this->actor.colChkInfo.health != 0) {
+        this->unk_30E = 0x50;
+    } else {
+        this->unk_30E = 0xC;
+    }
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_COMMON_FREEZE);
+    this->actionFunc = func_80B7D47C;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D47C.s")
+void func_80B7D4FC(EnNeoReeba*);
+void func_80B7D6D0(EnNeoReeba*);                 /* extern */
+void func_80B7E0BC(EnNeoReeba*, GlobalContext*); /* extern */
+
+void func_80B7D47C(EnNeoReeba* this, GlobalContext* globalCtx) {
+    if (this->unk_30E == 0) {
+        this->unk_30E = 0;
+        this->unk_304 = 0.0f;
+        this->unk_300 = 0.0f;
+        if (this->actor.colChkInfo.health != 0) {
+            func_80B7D4FC(this);
+        } else {
+            func_80B7D6D0(this);
+        }
+    } else if (this->unk_30E == 1) {
+        this->unk_30E--;
+        func_80B7E0BC(this, globalCtx);
+    } else {
+        this->unk_30E--;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D4FC.s")
+void func_80B7D5A4(EnNeoReeba*, GlobalContext*);
+
+void func_80B7D4FC(EnNeoReeba* this) {
+    this->unk_30A = 0xA;
+    this->skelAnime.playSpeed = 1.0f;
+    this->unk_2BC.x = Math_SinS(this->actor.yawTowardsPlayer) * -12.0f;
+    this->unk_2BC.z = Math_CosS(this->actor.yawTowardsPlayer) * -12.0f;
+    this->unk_2FC = 4551.0f;
+    func_800BCB70(&this->actor, 0x4000, 255, 0, 25);
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_RIVA_DAMAGE);
+    this->actionFunc = func_80B7D5A4;
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D5A4.s")
+extern f32 D_80B7E54C[];
+extern f32 D_80B7E578[];
+
+void func_80B7D5A4(EnNeoReeba* this, GlobalContext* globalCtx) {
+    f32 temp_f0;
+
+    if (this->unk_30A == 0) {
+        this->unk_2FC = 0.0f;
+        func_80B7CB3C(this);
+    } else {
+        this->unk_308 += -0x1F40;
+        Math_SmoothStepToF(&this->unk_2FC, 0.0f, 0.5f, 182.0f, 18.0f);
+        this->actor.scale.x = this->actor.scale.z = D_80B7E54C[this->unk_30A];
+        this->actor.scale.y = D_80B7E578[this->unk_30A];
+        if (this->actor.params & 0x8000) {
+            this->actor.scale.x *= 1.5f;
+            this->actor.scale.y *= 1.5f;
+            this->actor.scale.z *= 1.5f;
+        }
+        func_800BBDAC(globalCtx, &this->actor, &this->actor.world.pos, this->actor.shape.shadowScale, 1, 4.0f,
+                      250, 10, 1);
+        this->unk_30A += -1;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D6D0.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D788.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7D9B8.s")
+void func_80B7D360(EnNeoReeba*); /* extern */
+void func_80B7D3EC(EnNeoReeba*); /* extern */
+void func_80B7D5A4(EnNeoReeba*, GlobalContext*);
+void func_80B7D788(EnNeoReeba*, GlobalContext*);
+void func_80B7D47C(EnNeoReeba*, GlobalContext*);
+void func_80B7D398(EnNeoReeba*, GlobalContext*);
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7DC80.s")
 
@@ -138,8 +456,29 @@ extern UNK_TYPE D_060001E4;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7E260.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Update.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Update.s")
+void func_80B7D9B8(EnNeoReeba*, GlobalContext*); /* extern */
+void func_80B7DC80(EnNeoReeba*, GlobalContext*); /* extern */
+void func_80B7E260(EnNeoReeba*, GlobalContext*); /* extern */
+void func_80B7D398(EnNeoReeba*, GlobalContext*);
+void func_80B7D47C(EnNeoReeba*, GlobalContext*);
 
+void EnNeoReeba_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EnNeoReeba* this = THIS;
+
+    if ((this->actor.params & 0x8000)) {
+        this->collider.dim.radius = 0x1B;
+        this->collider.dim.height = 0x2D;
+    }
+    this->actionFunc(this, globalCtx);
+    if ((this->actionFunc != func_80B7CA70) && (this->actionFunc != func_80B7D398) &&
+        (this->actionFunc != func_80B7D47C)) {
+        SkelAnime_Update(&this->skelAnime);
+    }
+    func_80B7D9B8(this, globalCtx);
+    func_80B7DC80(this, globalCtx);
+    func_80B7E260(this, globalCtx);
+}
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/func_80B7E378.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Neo_Reeba/EnNeoReeba_Draw.s")
--- src/overlays/actors/ovl_En_Neo_Reeba/z_en_neo_reeba.h
+++ src/overlays/actors/ovl_En_Neo_Reeba/z_en_neo_reeba.h
@@ -9,9 +9,23 @@ typedef void (*EnNeoReebaActionFunc)(struct EnNeoReeba*, GlobalContext*);
 
 typedef struct EnNeoReeba {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x168];
+    /* 0x0144 */ SkelAnime skelAnime;
+    /* 0x0188 */ ColliderCylinder collider;
+    /* 0x01d4 */ Vec3s jointTable[18];
+    /* 0x0240 */ Vec3s morphTable[18];
     /* 0x02AC */ EnNeoReebaActionFunc actionFunc;
-    /* 0x02B0 */ char unk_2B0[0x64];
+    /* 0x02B0 */ Vec3f unk_2B0;
+    /* 0x02BC */ Vec3f unk_2BC;
+    /* 0x02C8 */ char unk_2C8[0x30];
+    /* 0x02F8 */ f32 unk_2F8;
+    /* 0x02FC */ f32 unk_2FC;
+    /* 0x0300 */ f32 unk_300;
+    /* 0x0304 */ f32 unk_304;
+    /* 0x0308 */ s16 unk_308;
+    /* 0x030A */ s16 unk_30A;
+    /* 0x030C */ s16 unk_30C;
+    /* 0x030E */ s16 unk_30E;
+    /* 0x0310 */ s8 unk_310;
 } EnNeoReeba; // size = 0x314
 
 extern const ActorInit En_Neo_Reeba_InitVars;
