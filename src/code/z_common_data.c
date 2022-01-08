c3c5b76f520976019ef20310c17d2d5e97cbe051
angie <angheloalf95@gmail.com>
2022-01-08 20:36:08 -0300

    small change

--- src/code/z_common_data.c
+++ src/code/z_common_data.c
@@ -3,7 +3,7 @@
 SaveContext gSaveContext;
 
 void SaveContext_Init(void) {
-    bzero(&gSaveContext, sizeof(gSaveContext));
+    bzero(&gSaveContext, sizeof(SaveContext));
 
     gSaveContext.save.playerForm = 0;
     gSaveContext.seqIndex = (u8)NA_BGM_DISABLED;
