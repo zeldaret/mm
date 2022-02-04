ad2cafc6e52fba6fd4c01891adf64acf05291a58
Parker Burnett <burnettparker@gmail.com>
2022-02-04 11:18:33 -0500

    fixing DL and changing to thisx

--- src/overlays/actors/ovl_Obj_Kinoko/z_obj_kinoko.c
+++ src/overlays/actors/ovl_Obj_Kinoko/z_obj_kinoko.c
@@ -7,7 +7,7 @@
 #include "z_obj_kinoko.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 
-#define FLAGS 0x00000010
+#define FLAGS (ACTOR_FLAG_10)
 
 #define THIS ((ObjKinoko*)thisx)
 
@@ -37,35 +37,34 @@ void ObjKinoko_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 
 void ObjKinoko_Update(Actor* thisx, GlobalContext* globalCtx) {
     f32 phi_f2;
-    ObjKinoko* this = THIS;
     Player* player = GET_PLAYER(globalCtx);
 
     if (player->currentMask != PLAYER_MASK_SCENTS) {
-        this->actor.draw = NULL;
-        this->actor.hintId = 0xFF;
-        this->actor.flags &= ~0x1;
+        thisx->draw = NULL;
+        thisx->hintId = 0xFF;
+        thisx->flags &= ~ACTOR_FLAG_1;
     } else {
-        this->actor.draw = ObjKinoko_Draw;
-        this->actor.hintId = 0x64;
-        this->actor.flags |= 0x1;
-        if (Actor_HasParent(&this->actor, globalCtx) != 0) {
-            Flags_SetCollectible(globalCtx, OBJ_KINOKO_PARAMS(this));
-            Actor_MarkForDeath(&this->actor);
+        thisx->draw = ObjKinoko_Draw;
+        thisx->hintId = 0x64;
+        thisx->flags |= ACTOR_FLAG_1;
+        if (Actor_HasParent(thisx, globalCtx) != 0) {
+            Flags_SetCollectible(globalCtx, OBJ_KINOKO_PARAMS(thisx));
+            Actor_MarkForDeath(thisx);
             return;
         }
-        Actor_PickUp(&this->actor, globalCtx, GI_MAX, 20.0f, 10.0f);
-        if (Math_SmoothStepToF(&this->actor.speedXZ, 0.0f, 0.04f, 2.0f, 0.5f) < 0.5f) {
-            this->actor.scale.x = 0.0f;
-            this->actor.speedXZ = 110.0f;
-            this->actor.velocity.x = 0.2f;
+        Actor_PickUp(thisx, globalCtx, GI_MAX, 20.0f, 10.0f);
+        if (Math_SmoothStepToF(&thisx->speedXZ, 0.0f, 0.04f, 2.0f, 0.5f) < 0.5f) {
+            thisx->scale.x = 0.0f;
+            thisx->speedXZ = 110.0f;
+            thisx->velocity.x = 0.2f;
         }
-        if (Math_SmoothStepToF(&this->actor.scale.x, this->actor.velocity.x, 0.04f, 0.004f, 0.001f) <
-            ((this->actor.velocity.x == 0.0f) ? 0.0f : 0.05f)) {
-            this->actor.velocity.x = 0.0f;
+        if (Math_SmoothStepToF(&thisx->scale.x, thisx->velocity.x, 0.04f, 0.004f, 0.001f) <
+            ((thisx->velocity.x == 0.0f) ? 0.0f : 0.05f)) {
+            thisx->velocity.x = 0.0f;
         }
-        this->actor.scale.y = this->actor.scale.x;
-        this->actor.scale.z = this->actor.scale.x;
-        this->actor.shape.rot.y = func_800DFCDC(globalCtx->cameraPtrs[globalCtx->activeCamera]) + 0x8000;
+        thisx->scale.y = thisx->scale.x;
+        thisx->scale.z = thisx->scale.x;
+        thisx->shape.rot.y = func_800DFCDC(globalCtx->cameraPtrs[globalCtx->activeCamera]) + 0x8000;
     }
 }
 
@@ -79,10 +78,10 @@ void ObjKinoko_Draw(Actor* thisx, GlobalContext* globalCtx) {
     gDPSetEnvColor(&gfx[1], 110, 44, 200, 100);
     gDPSetRenderMode(&gfx[2], G_RM_PASS, G_RM_ZB_CLD_SURF2);
     gSPMatrix(&gfx[3], Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(&gfx[4], D_04029D20);
+    gSPDisplayList(&gfx[4], &gameplay_keep_DL_029D10[2]);
     Matrix_InsertXRotation_s(-0x4000, 1);
     gSPMatrix(&gfx[5], Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-    gSPDisplayList(&gfx[6], D_04029D20);
+    gSPDisplayList(&gfx[6], &gameplay_keep_DL_029D10[2]);
     POLY_XLU_DISP = &gfx[7];
     CLOSE_DISPS(globalCtx->state.gfxCtx);
 }
