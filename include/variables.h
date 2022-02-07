5beb6c4baabec21fc830f725e93d654b2d6988dd
angie <angheloalf95@gmail.com>
2022-02-06 23:40:48 -0300

    Fix bss

--- include/variables.h
+++ include/variables.h
@@ -3340,6 +3340,7 @@ extern FaultAddrConvClient sGraphFaultAddrConvClient;
 extern FaultClient sGraphFaultClient;
 extern GfxMasterList* gGfxMasterDL;
 extern CfbInfo sGraphCfbInfos[3];
+extern OSTime sGraphTaskStartTime;
 extern OSMesgQueue sSiIntMsgQ;
 extern OSMesg sSiIntMsgBuf[1];
 extern u32 gSegments[NUM_SEGMENTS];
