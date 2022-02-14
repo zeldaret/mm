9c669e6ee364f3d35e79e81127548aa3d1fdf478
Tom Overton <tom.overton@outlook.com>
2022-02-13 23:30:50 -0800

    Actor flags

--- src/overlays/actors/ovl_Bg_Numa_Hana/z_bg_numa_hana.c
+++ src/overlays/actors/ovl_Bg_Numa_Hana/z_bg_numa_hana.c
@@ -8,7 +8,7 @@
 #include "objects/object_numa_obj/object_numa_obj.h"
 #include "objects/object_syokudai/object_syokudai.h"
 
-#define FLAGS 0x00000410
+#define FLAGS (ACTOR_FLAG_10 | ACTOR_FLAG_400)
 
 #define THIS ((BgNumaHana*)thisx)
 
