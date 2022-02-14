395e4a0685351b0db54005c2d83d41d93420332d
Derek Hensley <hensley.derek58@gmail.com>
2022-02-13 19:54:05 -0800

    Doc bba01

--- src/overlays/actors/ovl_En_Bba_01/z_en_bba_01.c
+++ src/overlays/actors/ovl_En_Bba_01/z_en_bba_01.c
@@ -16,9 +16,9 @@ void EnBba01_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnBba01_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnBba01_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-void func_809CC370(EnHy* this, GlobalContext* globalCtx);
-void func_809CC3A8(EnHy* this, GlobalContext* globalCtx);
-void func_809CC3CC(EnHy* this, GlobalContext* globalCtx);
+void EnBba01_Walk(EnHy* this, GlobalContext* globalCtx);
+void EnBba01_FaceFoward(EnHy* this, GlobalContext* globalCtx);
+void EnBba01_Talk(EnHy* this, GlobalContext* globalCtx);
 
 typedef enum { BBA01_DMG_EFF_NONE } EnBba01DamageEffect;
 
@@ -34,18 +34,29 @@ const ActorInit En_Bba_01_InitVars = {
     (ActorFunc)EnBba01_Draw,
 };
 
-// static ColliderCylinderInit sCylinderInit = {
-static ColliderCylinderInit D_809CCC80 = {
-    { COLTYPE_HIT0, AT_NONE, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_1, COLSHAPE_CYLINDER, },
-    { ELEMTYPE_UNK1, { 0x00000000, 0x00, 0x00 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_NONE | TOUCH_SFX_NORMAL, BUMP_ON, OCELEM_ON, },
+static ColliderCylinderInit sCylinderInit = {
+    {
+        COLTYPE_HIT0,
+        AT_NONE,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_ALL,
+        OC2_TYPE_1,
+        COLSHAPE_CYLINDER,
+    },
+    {
+        ELEMTYPE_UNK1,
+        { 0x00000000, 0x00, 0x00 },
+        { 0xF7CFFFFF, 0x00, 0x00 },
+        TOUCH_NONE | TOUCH_SFX_NORMAL,
+        BUMP_ON,
+        OCELEM_ON,
+    },
     { 18, 64, 0, { 0, 0, 0 } },
 };
 
-// sColChkInfoInit
-static CollisionCheckInfoInit2 D_809CCCAC = { 0, 0, 0, 0, MASS_IMMOVABLE };
+static CollisionCheckInfoInit2 sColChkInfoInit = { 0, 0, 0, 0, MASS_IMMOVABLE };
 
-// static DamageTable sDamageTable = {
-static DamageTable D_809CCCB8 = {
+static DamageTable sDamageTable = {
     /* Deku Nut       */ DMG_ENTRY(0, BBA01_DMG_EFF_NONE),
     /* Deku Stick     */ DMG_ENTRY(0, BBA01_DMG_EFF_NONE),
     /* Horse trample  */ DMG_ENTRY(0, BBA01_DMG_EFF_NONE),
@@ -82,8 +93,7 @@ static DamageTable D_809CCCB8 = {
 
 u16 D_809CCCD8[] = { 4000, 4, 1, 3, 6000, 4, 1, 6, 4000, 4, 1, 3, 6000, 4, 1, 6 };
 
-// UpdateModel
-void func_809CC060(EnBba01* this, GlobalContext* globalCtx) {
+void EnBba01_UpdateModel(EnBba01* this, GlobalContext* globalCtx) {
     Player* player = GET_PLAYER(globalCtx);
     Vec3f focus;
 
@@ -107,8 +117,7 @@ void func_809CC060(EnBba01* this, GlobalContext* globalCtx) {
     EnHy_UpdateCollider(&this->enHy, globalCtx);
 }
 
-// TestIsTalking
-s32 func_809CC1D4(EnBba01* this, GlobalContext* globalCtx) {
+s32 EnBba01_TestIsTalking(EnBba01* this, GlobalContext* globalCtx) {
     s32 isTalking = false;
 
     if (Actor_ProcessTalkRequest(&this->enHy.actor, &globalCtx->state)) {
@@ -118,7 +127,7 @@ s32 func_809CC1D4(EnBba01* this, GlobalContext* globalCtx) {
         this->enHy.tmpHeadRot = this->enHy.headRot;
         this->enHy.tmpTorsoRot = this->enHy.torsoRot;
         this->enHy.tmpActionFunc = this->enHy.actionFunc;
-        this->enHy.actionFunc = func_809CC3CC;
+        this->enHy.actionFunc = EnBba01_Talk;
     }
     return isTalking;
 }
@@ -135,34 +144,30 @@ s32 func_809CC270(EnBba01* this, GlobalContext* globalCtx) {
     return true;
 }
 
-//FinishInit
-void func_809CC2F0(EnHy* this, GlobalContext* globalCtx) {
+void EnBba01_FinishInit(EnHy* this, GlobalContext* globalCtx) {
     if (EnHy_Init(this, globalCtx, &object_bba_Skel_005EF0, ENHY_ANIMATION_BBA_6)) {
         this->actor.flags |= ACTOR_FLAG_1;
         this->actor.draw = EnBba01_Draw;
         this->waitingOnInit = false;
         if (ENBBA01_GET_PATH(&this->actor) == ENBBA01_NO_PATH) {
-            this->actionFunc = func_809CC3A8;
+            this->actionFunc = EnBba01_FaceFoward;
         } else {
-            this->actionFunc = func_809CC370;
+            this->actionFunc = EnBba01_Walk;
         }
     }
 }
 
-//Walk
-void func_809CC370(EnHy* this, GlobalContext* globalCtx) {
+void EnBba01_Walk(EnHy* this, GlobalContext* globalCtx) {
     if (EnHy_MoveForwards(this, 1.0f)) {
         this->curPoint = 0;
     }
 }
 
-//FaceFoward
-void func_809CC3A8(EnHy* this, GlobalContext* globalCtx) {
+void EnBba01_FaceFoward(EnHy* this, GlobalContext* globalCtx) {
     this->actor.shape.rot = this->actor.world.rot;
 }
 
-//Talk
-void func_809CC3CC(EnHy* this, GlobalContext* globalCtx) {
+void EnBba01_Talk(EnHy* this, GlobalContext* globalCtx) {
     s16 yaw;
     u8 talkstate;
 
@@ -204,13 +209,13 @@ void EnBba01_Init(Actor* thisx, GlobalContext* globalCtx) {
     }
     this->enHy.actor.draw = NULL;
     Collider_InitCylinder(globalCtx, &this->enHy.collider);
-    Collider_SetCylinder(globalCtx, &this->enHy.collider, &this->enHy.actor, &D_809CCC80);
-    CollisionCheck_SetInfo2(&this->enHy.actor.colChkInfo, &D_809CCCB8, &D_809CCCAC);
+    Collider_SetCylinder(globalCtx, &this->enHy.collider, &this->enHy.actor, &sCylinderInit);
+    CollisionCheck_SetInfo2(&this->enHy.actor.colChkInfo, &sDamageTable, &sColChkInfoInit);
     this->enHy.actor.flags &= ~ACTOR_FLAG_1;
     this->enHy.path = func_8013D648(globalCtx, ENBBA01_GET_PATH(&this->enHy.actor), ENBBA01_NO_PATH);
     this->enHy.waitingOnInit = true;
     Actor_SetScale(&this->enHy.actor, 0.01f);
-    this->enHy.actionFunc = func_809CC2F0;
+    this->enHy.actionFunc = EnBba01_FinishInit;
 }
 
 void EnBba01_Destroy(Actor* thisx, GlobalContext* globalCtx) {
@@ -222,15 +227,14 @@ void EnBba01_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 void EnBba01_Update(Actor* thisx, GlobalContext* globalCtx) {
     EnBba01* this = THIS;
 
-    func_809CC1D4(this, globalCtx);
+    EnBba01_TestIsTalking(this, globalCtx);
     this->enHy.actionFunc(&this->enHy, globalCtx);
     Actor_UpdateBgCheckInfo(globalCtx, &this->enHy.actor, 0.0f, 0.0f, 0.0f, 4);
-    func_809CC060(this, globalCtx);
+    EnBba01_UpdateModel(this, globalCtx);
     func_809CC270(this, globalCtx);
 }
 
-//OverrideLimbDraw
-s32 func_809CC6F0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot,
+s32 EnBba01_OverrideLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot,
                              Actor* thisx) {
     EnBba01* this = THIS;
     s8 bodyPart;
@@ -264,8 +268,7 @@ s32 func_809CC6F0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* p
         Matrix_InsertTranslation(40.0f, 0.0f, 0.0f, MTXMODE_APPLY);
     }
 
-    if ((limbIndex == 8) || (limbIndex == 9) ||
-        (limbIndex == 12)) {
+    if ((limbIndex == 8) || (limbIndex == 9) || (limbIndex == 12)) {
         rot->y += (s16)(Math_SinS(this->enHy.limbRotTableY[limbIndex]) * 200.0f);
         rot->z += (s16)(Math_CosS(this->enHy.limbRotTableZ[limbIndex]) * 200.0f);
     }
@@ -273,8 +276,7 @@ s32 func_809CC6F0(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3f* p
     return false;
 }
 
-//PostLimbDraw
-void func_809CC984(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
+void EnBba01_PostLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
     EnBba01* this = THIS;
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
     Vec3f zeroVec = { 0.0f, 0.0f, 0.0f };
@@ -291,8 +293,7 @@ void func_809CC984(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* 
     }
 }
 
-//TransformLimbDraw
-void func_809CCA5C(GlobalContext* globalCtx, s32 limbIndex, Actor* thisx) {
+void EnBba01_TransformLimbDraw(GlobalContext* globalCtx, s32 limbIndex, Actor* thisx) {
 }
 
 void EnBba01_Draw(Actor* thisx, GlobalContext* globalCtx) {
@@ -308,8 +309,8 @@ void EnBba01_Draw(Actor* thisx, GlobalContext* globalCtx) {
     gSPSegment(POLY_OPA_DISP++, 0x09, Gfx_EnvColor(globalCtx->state.gfxCtx, 55, 55, 255, 0));
     gDPPipeSync(POLY_OPA_DISP++);
     SkelAnime_DrawTransformFlexOpa(globalCtx, this->enHy.skelAnime.skeleton, this->enHy.skelAnime.jointTable,
-                                   this->enHy.skelAnime.dListCount, func_809CC6F0, func_809CC984,
-                                   func_809CCA5C, &this->enHy.actor);
+                                   this->enHy.skelAnime.dListCount, EnBba01_OverrideLimbDraw, EnBba01_PostLimbDraw,
+                                   EnBba01_TransformLimbDraw, &this->enHy.actor);
     Matrix_InsertXRotation_s(0, MTXMODE_NEW);
 
     for (i = 0, shadowTexIter = shadowTex; i < (s32)sizeof(u8[64][64]); i++) {
