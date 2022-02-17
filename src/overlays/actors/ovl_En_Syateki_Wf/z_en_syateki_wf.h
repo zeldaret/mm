5432eac3a7501e5fe758dd4daeaf322b52d3f29f
Tom Overton <tom.overton@outlook.com>
2022-02-17 04:35:25 -0800

    Delete struct padding

--- src/overlays/actors/ovl_En_Syateki_Wf/z_en_syateki_wf.h
+++ src/overlays/actors/ovl_En_Syateki_Wf/z_en_syateki_wf.h
@@ -14,25 +14,22 @@ typedef struct EnSyatekiWf {
     /* 0x144 */ SkelAnime skelAnime;
     /* 0x188 */ Vec3s jointTable[22];
     /* 0x20C */ Vec3s morphTable[22];
-    /* 0x290 */ char unk_290[0x4];
+    /* 0x290 */ UNK_TYPE1 unk_290[0x4];
     /* 0x294 */ EnSyatekiWfActionFunc actionFunc;
     /* 0x298 */ s16 unk_298;
     /* 0x29A */ s16 unk_29A;
     /* 0x29C */ s16 unk_29C;
-    /* 0x29E */ char unk_29E[0x2];
     /* 0x2A0 */ Vec3s* unk_2A0;
     /* 0x2A4 */ s16 unk_2A4;
     /* 0x2A6 */ s16 unk_2A6;
     /* 0x2A8 */ s16 unk_2A8;
-    /* 0x2A8 */ char unk_2AA[0x2];
     /* 0x2AC */ f32 unk_2AC;
     /* 0x2B0 */ u8 unk_2B0;
-    /* 0x2B1 */ char unk_2B1[0x3];
     /* 0x2B4 */ ColliderCylinder unk_2B4;
     /* 0x300 */ ColliderCylinder unk_300;
     /* 0x34C */ ColliderJntSph unk_34C;
     /* 0x36C */ ColliderJntSphElement unk_36C;
-    /* 0x3AC */ char unk_3AC[0x20];
+    /* 0x3AC */ UNK_TYPE1 unk_3AC[0x20];
 } EnSyatekiWf; // size = 0x3CC
 
 extern const ActorInit En_Syateki_Wf_InitVars;
