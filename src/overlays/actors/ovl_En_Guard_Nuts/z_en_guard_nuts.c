28a4e672a6bd971e9aa3a9a1b0f348bb8e2e18ff
Parker Burnett <burnettparker@gmail.com>
2022-02-19 23:40:32 -0500

    pr feedback

--- src/overlays/actors/ovl_En_Guard_Nuts/z_en_guard_nuts.c
+++ src/overlays/actors/ovl_En_Guard_Nuts/z_en_guard_nuts.c
@@ -79,6 +79,20 @@ static TexturePtr sEyeTextures[] = {
     gDekuPalaceGuardEyeClosedTex,
 };
 
+typedef enum {
+    /* 0 */ WAIT_HEAD_TILT_ANIM,
+    /* 1 */ WALK_ANIM,
+    /* 2 */ DIG_ANIM,
+    /* 3 */ WALK_ANIM_2,
+} EnGuardNutsAnim;
+
+typedef enum {
+    /* 0 */ GUARD_NUTS_WAIT_STATE,
+    /* 1 */ GUARD_NUTS_TALKING_STATE,
+    /* 3 */ GUARD_NUTS_BURROWED_STATE = 3,
+    /* 4 */ GUARD_NUTS_UNK_STATE
+} EnGuardNutsState;
+
 void EnGuardNuts_Init(Actor* thisx, GlobalContext* globalCtx) {
     EnGuardNuts* this = THIS;
 
@@ -89,8 +103,8 @@ void EnGuardNuts_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->actor.targetMode = 1;
     Collider_InitAndSetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
     Actor_SetScale(&this->actor, 0.015f);
-    Math_Vec3f_Copy(&this->unk228, &this->actor.world.pos);
-    this->unk21E = D_80ABBE1C;
+    Math_Vec3f_Copy(&this->guardPos, &this->actor.world.pos);
+    this->guardCount = D_80ABBE1C;
     D_80ABBE1C++;
 
     // If you have returned deku princess guards will init burrowed.
@@ -121,8 +135,8 @@ void EnGuardNuts_ChangeAnim(EnGuardNuts* this, s32 index) {
 }
 
 void EnGuardNuts_SetupWait(EnGuardNuts* this) {
-    EnGuardNuts_ChangeAnim(this, WAIT_ANIM);
-    this->unk21C = 0;
+    EnGuardNuts_ChangeAnim(this, WAIT_HEAD_TILT_ANIM);
+    this->state = GUARD_NUTS_WAIT_STATE;
     this->actionFunc = EnGuardNuts_Wait;
 }
 
@@ -142,19 +156,19 @@ void EnGuardNuts_Wait(EnGuardNuts* this, GlobalContext* globalCtx) {
     }
     if (player->transformation == PLAYER_FORM_DEKU) {
         // this is the palace of...
-        this->textId = 0;
+        this->guardTextIndex = 0;
         if ((gSaveContext.weekEventReg[17] & 4) && (!this->hasCompletedConversation)) {
             // I told you not to enter!!
-            this->textId = 7;
+            this->guardTextIndex = 7;
         } else if (gSaveContext.weekEventReg[12] & 0x40) {
             // come to see the monkey again?
-            this->textId = 4;
+            this->guardTextIndex = 4;
         }
     } else {
         // No place for an outsider
-        this->textId = 3;
+        this->guardTextIndex = 3;
     }
-    this->actor.textId = sTextIDs[this->textId];
+    this->actor.textId = sTextIDs[this->guardTextIndex];
     phi_a1 = this->actor.world.rot.y;
     if (D_80ABBE20 == 2) {
         EnGuardNuts_Burrow(this, globalCtx);
@@ -174,10 +188,10 @@ void EnGuardNuts_Wait(EnGuardNuts* this, GlobalContext* globalCtx) {
     } else {
         Math_SmoothStepToS(&this->actor.shape.rot.y, phi_a1, 1, 0xBB8, 0);
     }
-    if (this->unk23A.x == 0) {
+    if (this->targetHeadPos.x == 0) {
         if (fabsf((f32)this->actor.world.rot.y - (f32)this->actor.yawTowardsPlayer) < 10000.0f) {
-            this->unk23A.y = this->actor.world.rot.y - this->actor.yawTowardsPlayer;
-            this->unk23A.y = -this->unk23A.y;
+            this->targetHeadPos.y = this->actor.world.rot.y - this->actor.yawTowardsPlayer;
+            this->targetHeadPos.y = -this->targetHeadPos.y;
         }
     }
     func_800B8614(&this->actor, globalCtx, 70.0f);
@@ -185,10 +199,9 @@ void EnGuardNuts_Wait(EnGuardNuts* this, GlobalContext* globalCtx) {
 
 void func_80ABB540(EnGuardNuts* this) {
     EnGuardNuts_ChangeAnim(this, WALK_ANIM);
-    this->unk23A.y = 0;
-    this->unk23A.x = this->unk23A.y;
+    this->targetHeadPos.x = this->targetHeadPos.y = 0;
     this->timer = 16;
-    this->unk21C = 1;
+    this->state = GUARD_NUTS_TALKING_STATE;
     this->actionFunc = func_80ABB590;
 }
 
@@ -202,7 +215,7 @@ void func_80ABB590(EnGuardNuts* this, GlobalContext* globalCtx) {
     if (fabsf((this->actor.shape.rot.y - yaw)) < 100.0f) {
         this->actor.shape.rot.y = yaw;
         curFrame = this->skelAnime.curFrame;
-        if ((curFrame < this->animFrameCount) || (this->textId >= 7)) {
+        if ((curFrame < this->animFrameCount) || (this->guardTextIndex >= 7)) {
             SkelAnime_Update(&this->skelAnime);
         }
     } else {
@@ -210,31 +223,31 @@ void func_80ABB590(EnGuardNuts* this, GlobalContext* globalCtx) {
         Math_SmoothStepToS(&this->actor.shape.rot.y, yaw, 1, 0xBB8, 0);
     }
     if (Message_GetState(&globalCtx->msgCtx) == 5) {
-        this->unk23A.y = 0;
-        this->unk23A.x = 0;
-        if ((this->textId == 3) && (this->animIndex == WAIT_ANIM)) {
-            EnGuardNuts_ChangeAnim(this, WAIT_ANIM);
+        this->targetHeadPos.y = 0;
+        this->targetHeadPos.x = 0;
+        if ((this->guardTextIndex == 3) && (this->animIndex == WAIT_HEAD_TILT_ANIM)) {
+            EnGuardNuts_ChangeAnim(this, WAIT_HEAD_TILT_ANIM);
         }
         if (func_80147624(globalCtx) != 0) {
-            if (D_80ABBE38[this->textId] != 1) {
-                if (D_80ABBE38[this->textId] == 2) {
+            if (D_80ABBE38[this->guardTextIndex] != 1) {
+                if (D_80ABBE38[this->guardTextIndex] == 2) {
                     func_801477B4(globalCtx);
                     D_80ABBE20 = 2;
                     gSaveContext.weekEventReg[12] |= 0x40;
                     EnGuardNuts_Burrow(this, globalCtx);
                 } else {
-                    this->textId++;
-                    func_801518B0(globalCtx, sTextIDs[this->textId], &this->actor);
-                    if (D_80ABBE38[this->textId] == 2) {
+                    this->guardTextIndex++;
+                    func_801518B0(globalCtx, sTextIDs[this->guardTextIndex], &this->actor);
+                    if (D_80ABBE38[this->guardTextIndex] == 2) {
                         D_80ABBE20 = 1;
                     }
                     this->timer = 16;
                 }
-            } else if (this->textId != 3) {
-                this->unk23A.x = 0;
-                this->unk23A.y = this->unk23A.x;
+            } else if (this->guardTextIndex != 3) {
+                this->targetHeadPos.x = 0;
+                this->targetHeadPos.y = this->targetHeadPos.x;
                 func_801477B4(globalCtx);
-                this->unk21C = 4;
+                this->state = GUARD_NUTS_UNK_STATE;
                 this->actionFunc = EnGuardNuts_Unburrow;
             } else {
                 func_801477B4(globalCtx);
@@ -242,14 +255,14 @@ void func_80ABB590(EnGuardNuts* this, GlobalContext* globalCtx) {
             }
         }
     } else if ((Message_GetState(&globalCtx->msgCtx) >= 3) && (D_80ABBE20 == 0)) {
-        if ((this->textId == 0) || (this->textId == 3) || (this->textId >= 7)) {
+        if ((this->guardTextIndex == 0) || (this->guardTextIndex == 3) || (this->guardTextIndex >= 7)) {
             if (this->timer == 0) {
                 this->timer = 2;
-                this->unk23A.y ^= 0x2000;
+                this->targetHeadPos.y ^= 0x2000;
             }
         } else if (this->timer == 0) {
             this->timer = 2;
-            this->unk23A.x ^= 0x2000;
+            this->targetHeadPos.x ^= 0x2000;
         }
     }
 }
@@ -261,9 +274,9 @@ void EnGuardNuts_Burrow(EnGuardNuts* this, GlobalContext* globalCtx) {
     Math_Vec3f_Copy(&digPos, &this->actor.world.pos);
     digPos.y = this->actor.floorHeight;
     EffectSsHahen_SpawnBurst(globalCtx, &digPos, 4.0f, 0, 10, 3, 15, -1, 10, NULL);
-    this->unk23A.y = 0;
+    this->targetHeadPos.y = 0;
     this->actor.flags |= ACTOR_FLAG_8000000;
-    this->unk23A.x = this->unk23A.y;
+    this->targetHeadPos.x = this->targetHeadPos.y;
     Actor_PlaySfxAtPos(&this->actor, NA_SE_EN_NUTS_DOWN);
     Actor_PlaySfxAtPos(&this->actor, NA_SE_EN_NUTS_UP);
     this->actionFunc = EnGuardNuts_SetupUnburrow;
@@ -274,7 +287,7 @@ void EnGuardNuts_SetupUnburrow(EnGuardNuts* this, GlobalContext* globalCtx) {
 
     SkelAnime_Update(&this->skelAnime);
     if (this->animFrameCount <= curFrame) {
-        this->unk21C = 3;
+        this->state = GUARD_NUTS_BURROWED_STATE;
         this->actionFunc = EnGuardNuts_Unburrow;
         this->actor.world.rot.y = this->actor.home.rot.y;
         this->actor.shape.rot.y = this->actor.home.rot.y;
@@ -294,7 +307,7 @@ void EnGuardNuts_Unburrow(EnGuardNuts* this, GlobalContext* globalCtx) {
             EffectSsHahen_SpawnBurst(globalCtx, &digPos, 4.0f, 0, 10, 3, 15, -1, 10, NULL);
             Actor_PlaySfxAtPos(&this->actor, NA_SE_EN_NUTS_UP);
             D_80ABBE20 = 0;
-            if (this->textId == 9) {
+            if (this->guardTextIndex == 9) {
                 this->hasCompletedConversation = true;
             }
             this->actor.flags &= ~ACTOR_FLAG_8000000;
@@ -308,8 +321,7 @@ void EnGuardNuts_Update(Actor* thisx, GlobalContext* globalCtx) {
     s32 pad;
 
     if (this->blinkTimer == 0) {
-        this->eyeState++;
-        if (this->eyeState >= 3) {
+        if (++(this->eyeState) >= 3) {
             this->eyeState = 0;
             this->blinkTimer = (s16)Rand_ZeroFloat(60.0f) + 20;
         }
@@ -321,8 +333,8 @@ void EnGuardNuts_Update(Actor* thisx, GlobalContext* globalCtx) {
 
     this->actionFunc(this, globalCtx);
     Actor_SetFocus(&this->actor, 40.0f);
-    Math_SmoothStepToS(&this->headRot.x, this->unk23A.x, 1, 0xBB8, 0);
-    Math_SmoothStepToS(&this->headRot.y, this->unk23A.y, 1, 0xBB8, 0);
+    Math_SmoothStepToS(&this->headRot.x, this->targetHeadPos.x, 1, 0xBB8, 0);
+    Math_SmoothStepToS(&this->headRot.y, this->targetHeadPos.y, 1, 0xBB8, 0);
 
     if (this->blinkTimer != 0) {
         this->blinkTimer--;
@@ -333,7 +345,7 @@ void EnGuardNuts_Update(Actor* thisx, GlobalContext* globalCtx) {
 
     Actor_MoveWithGravity(&this->actor);
     Actor_UpdateBgCheckInfo(globalCtx, &this->actor, 20.0f, 20.0f, 60.0f, 0x1D);
-    if ((this->unk21C != 3) && (this->unk21C != 4)) {
+    if ((this->state != GUARD_NUTS_BURROWED_STATE) && (this->state != GUARD_NUTS_UNK_STATE)) {
         Collider_UpdateCylinder(&this->actor, &this->collider);
         CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
     }
@@ -362,7 +374,7 @@ void EnGuardNuts_Draw(Actor* thisx, GlobalContext* globalCtx) {
 
     SkelAnime_DrawOpa(globalCtx, this->skelAnime.skeleton, this->skelAnime.jointTable, EnGuardNuts_OverrideLimbDraw,
                       NULL, &this->actor);
-    Matrix_InsertTranslation(this->unk228.x, this->actor.floorHeight, this->unk228.z, MTXMODE_NEW);
+    Matrix_InsertTranslation(this->guardPos.x, this->actor.floorHeight, this->guardPos.z, MTXMODE_NEW);
     Matrix_Scale(0.015f, 0.015f, 0.015f, 1);
 
     gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
--- src/overlays/actors/ovl_En_Guard_Nuts/z_en_guard_nuts.h
+++ src/overlays/actors/ovl_En_Guard_Nuts/z_en_guard_nuts.h
@@ -8,13 +8,6 @@ struct EnGuardNuts;
 
 typedef void (*EnGuardNutsActionFunc)(struct EnGuardNuts*, GlobalContext*);
 
-typedef enum {
-    /* 0 */ WAIT_ANIM,
-    /* 1 */ WALK_ANIM,
-    /* 2 */ DIG_ANIM,
-    /* 3 */ WALK_ANIM_2,
-} EnGuardNutAnim;
-
 typedef struct EnGuardNuts {
     /* 0x000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;
@@ -25,13 +18,13 @@ typedef struct EnGuardNuts {
     /* 0x212 */ s16 blinkTimer;
     /* 0x214 */ s16 timer;
     /* 0x218 */ s32 animIndex;
-    /* 0x21C */ s16 unk21C;
-    /* 0x21E */ s16 unk21E;
-    /* 0x220 */ s32 textId;
+    /* 0x21C */ s16 state;
+    /* 0x21E */ s16 guardCount; // Unused - Counter for how many guards have spawned
+    /* 0x220 */ s32 guardTextIndex;
     /* 0x224 */ u8 hasCompletedConversation; // Flag set when player has gone through all text.
-    /* 0x228 */ Vec3f unk228;
+    /* 0x228 */ Vec3f guardPos;
     /* 0x234 */ Vec3s headRot;
-    /* 0x23A */ Vec3s unk23A;
+    /* 0x23A */ Vec3s targetHeadPos;
     /* 0x240 */ f32 animFrameCount;
     /* 0x244 */ ColliderCylinder collider;
 } EnGuardNuts; // size = 0x290
