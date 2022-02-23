1745baaa41d0fd524fcd7024a0ce630abf80cde9
Derek Hensley <hensley.derek58@gmail.com>
2022-02-22 19:50:13 -0800

    Fix FireObjLightParams struct

--- include/z64.h
+++ include/z64.h
@@ -171,8 +171,8 @@ typedef struct FireObjColors {
 typedef struct FireObjLightParams {
     /* 0x0 */ s16 radius;
     /* 0x2 */ Color_RGB8 color;
-    /* 0x5 */ Color_RGB8 randMultiplier
-} FireObjLightParams;
+    /* 0x5 */ Color_RGB8 randMultiplier;
+} FireObjLightParams; // size = 0x8
 
 #define FONT_CHAR_TEX_WIDTH  16
 #define FONT_CHAR_TEX_HEIGHT 16
