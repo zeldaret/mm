f15311c9b36953f12ba295749d41f4f68763b684
Parker Burnett <burnettparker@gmail.com>
2022-02-24 00:16:51 -0500

    this is the guay

--- src/overlays/actors/ovl_En_Crow/z_en_crow.c
+++ src/overlays/actors/ovl_En_Crow/z_en_crow.c
@@ -15,6 +15,7 @@ void EnCrow_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnCrow_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnCrow_Draw(Actor* thisx, GlobalContext* globalCtx);
 
+void func_8099AC58(EnCrow* this);
 void func_8099AC8C(EnCrow* this, GlobalContext* globalCtx);
 void func_8099B0CC(EnCrow* this, GlobalContext* globalCtx);
 void func_8099B584(EnCrow* this, GlobalContext* globalCtx);
@@ -22,7 +23,6 @@ void func_8099B6C4(EnCrow* this, GlobalContext* globalCtx);
 void func_8099B838(EnCrow* this, GlobalContext* globalCtx);
 void func_8099B9E8(EnCrow* this, GlobalContext* globalCtx);
 
-#if 0
 const ActorInit En_Crow_InitVars = {
     ACTOR_EN_CROW,
     ACTORCAT_ENEMY,
@@ -38,15 +38,30 @@ const ActorInit En_Crow_InitVars = {
 // static ColliderJntSphElementInit sJntSphElementsInit[1] = {
 static ColliderJntSphElementInit D_8099C070[1] = {
     {
-        { ELEMTYPE_UNK0, { 0xF7CFFFFF, 0x00, 0x08 }, { 0xF7CFFFFF, 0x00, 0x00 }, TOUCH_ON | TOUCH_SFX_HARD, BUMP_ON, OCELEM_ON, },
+        {
+            ELEMTYPE_UNK0,
+            { 0xF7CFFFFF, 0x00, 0x08 },
+            { 0xF7CFFFFF, 0x00, 0x00 },
+            TOUCH_ON | TOUCH_SFX_HARD,
+            BUMP_ON,
+            OCELEM_ON,
+        },
         { 1, { { 0, 0, 0 }, 20 }, 100 },
     },
 };
 
 // static ColliderJntSphInit sJntSphInit = {
 static ColliderJntSphInit D_8099C094 = {
-    { COLTYPE_HIT3, AT_ON | AT_TYPE_ENEMY, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_ALL, OC2_TYPE_1, COLSHAPE_JNTSPH, },
-    1, D_8099C070, // sJntSphElementsInit,
+    {
+        COLTYPE_HIT3,
+        AT_ON | AT_TYPE_ENEMY,
+        AC_ON | AC_TYPE_PLAYER,
+        OC1_ON | OC1_TYPE_ALL,
+        OC2_TYPE_1,
+        COLSHAPE_JNTSPH,
+    },
+    1,
+    D_8099C070, // sJntSphElementsInit,
 };
 
 // sColChkInfoInit
@@ -88,6 +103,8 @@ static DamageTable D_8099C0AC = {
     /* Powder Keg     */ DMG_ENTRY(1, 0x0),
 };
 
+s32 D_8099C0CC = 0;
+
 // static InitChainEntry sInitChain[] = {
 static InitChainEntry D_8099C0D0[] = {
     ICHAIN_F32(uncullZoneForward, 3000, ICHAIN_CONTINUE),
@@ -95,8 +112,6 @@ static InitChainEntry D_8099C0D0[] = {
     ICHAIN_F32_DIV1000(gravity, -500, ICHAIN_CONTINUE),
     ICHAIN_F32(targetArrowOffset, 2000, ICHAIN_STOP),
 };
-
-#endif
 
 extern ColliderJntSphElementInit D_8099C070[1];
 extern ColliderJntSphInit D_8099C094;
@@ -106,11 +121,36 @@ extern InitChainEntry D_8099C0D0[];
 
 extern UNK_TYPE D_060000F0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/EnCrow_Init.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/EnCrow_Init.s")
+void EnCrow_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EnCrow* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/EnCrow_Destroy.s")
+    Actor_ProcessInitChain(&this->actor, D_8099C0D0);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, &object_crow_Skel_0010C0, &D_060000F0, &this->unk194, &this->unk1CA,
+                       9);
+    Collider_InitAndSetJntSph(globalCtx, &this->unk200, &this->actor, &D_8099C094, &this->unk220);
+    this->unk200.elements->dim.worldSphere.radius = D_8099C094.elements[0].dim.modelSphere.radius;
+    CollisionCheck_SetInfo(&this->actor.colChkInfo, &D_8099C0AC, &D_8099C0A4);
+    ActorShape_Init(&this->actor.shape, 2000.0f, ActorShadow_DrawCircle, 20.0f);
+    D_8099C0CC = 0;
+    if (this->actor.parent != 0) {
+        this->actor.flags &= -2;
+    }
+    func_8099AC58(this);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/func_8099AC58.s")
+void EnCrow_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+    EnCrow* this = THIS;
+
+    Collider_DestroyJntSph(globalCtx, &this->unk200);
+}
+
+void func_8099AC58(EnCrow* this) {
+    this->unk18E = 0x64;
+    this->unk200.base.acFlags |= 1;
+    this->actionFunc = func_8099AC8C;
+    this->skelAnime.playSpeed = 1.0f;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/func_8099AC8C.s")
 
@@ -134,9 +174,53 @@ extern UNK_TYPE D_060000F0;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/func_8099B8EC.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/func_8099B9E8.s")
+void func_8099B9E8(EnCrow* this, GlobalContext* globalCtx) {
+    s16 temp_v0;
+    f32 phi_f0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/func_8099BAB4.s")
+    DECR(this->unk18E);
+    if (this->unk18E == 0) {
+        SkelAnime_Update(&this->skelAnime);
+        this->actor.draw = EnCrow_Draw;
+        if (this->actor.params != 0) {
+            phi_f0 = 0.03f;
+        } else {
+            phi_f0 = 0.01f;
+        }
+        if (Math_StepToF(&this->actor.scale.x, phi_f0, phi_f0 * 0.1f)) {
+            this->actor.flags |= 1;
+            this->actor.flags &= ~0x10;
+            this->actor.colChkInfo.health = 1;
+            func_8099AC58(this);
+        }
+        this->actor.scale.y = this->actor.scale.x;
+        this->actor.scale.z = this->actor.scale.x;
+    }
+}
+
+void func_8099BAB4(EnCrow* this, GlobalContext* globalCtx) {
+
+    if (this->unk200.base.acFlags & 2) {
+        this->unk200.base.acFlags &= ~AT_HIT;
+        Actor_SetDropFlag(&this->actor, &this->unk200.elements->info);
+
+        if (this->actor.colChkInfo.damageEffect == 1) {
+            func_8099B778(this);
+
+        } else if (this->actor.colChkInfo.damageEffect == 5) {
+            this->unk18C = 0x1F;
+            this->unk290 = 2.0f;
+            this->unk294 = 0.5f;
+            func_8099B778(this);
+
+        } else {
+            this->actor.colChkInfo.health = 0;
+            this->actor.flags &= -2;
+            Enemy_StartFinishingBlow(globalCtx, &this->actor);
+            func_8099B384(this, globalCtx);
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Crow/EnCrow_Update.s")
 
--- src/overlays/actors/ovl_En_Crow/z_en_crow.h
+++ src/overlays/actors/ovl_En_Crow/z_en_crow.h
@@ -2,6 +2,7 @@
 #define Z_EN_CROW_H
 
 #include "global.h"
+#include "objects/object_crow/object_crow.h"
 
 struct EnCrow;
 
@@ -9,9 +10,22 @@ typedef void (*EnCrowActionFunc)(struct EnCrow*, GlobalContext*);
 
 typedef struct EnCrow {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x44];
+    /* 0x0144 */ SkelAnime skelAnime;
     /* 0x0188 */ EnCrowActionFunc actionFunc;
-    /* 0x018C */ char unk_18C[0x110];
+    /* 0x18C */ s8 unk18C;                          /* inferred */
+    /* 0x18D */ char pad18D[1];
+    /* 0x18E */ s16 unk18E;
+    /* 0x190 */ char pad190[4];                     /* maybe part of unk18E[3]? */
+    /* 0x194 */ Vec3s unk194;
+    /* 0x19A */ char pad19A[0x30];                  /* maybe part of unk194[9]? */
+    /* 0x1CA */ Vec3s unk1CA;
+    /* 0x1D0 */ char pad1D0[0x30];                  /* maybe part of unk1CA[9]? */
+    /* 0x200 */ ColliderJntSph unk200;
+    /* 0x220 */ ColliderJntSphElement unk220;
+    /* 0x260 */ char pad260[0x30];
+    /* 0x290 */ f32 unk290;                         /* inferred */
+    /* 0x294 */ f32 unk294;                         /* inferred */
+    /* 0x298 */ char pad298[4];
 } EnCrow; // size = 0x29C
 
 extern const ActorInit En_Crow_InitVars;
