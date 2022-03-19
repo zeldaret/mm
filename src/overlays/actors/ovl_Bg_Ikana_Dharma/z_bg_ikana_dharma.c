81d71e537707d1e9eeaf34dd8919a056e9fbeebc
immibis <immibis@gmail.com>
2022-03-19 23:11:42 +0100

    Bg_Ikana_Dharma more stuff

--- src/overlays/actors/ovl_Bg_Ikana_Dharma/z_bg_ikana_dharma.c
+++ src/overlays/actors/ovl_Bg_Ikana_Dharma/z_bg_ikana_dharma.c
@@ -15,12 +15,12 @@ void BgIkanaDharma_Init(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaDharma_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaDharma_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaDharma_Draw(Actor* thisx, GlobalContext* globalCtx);
-void func_80BEC5C4(BgIkanaDharma* this);
-void func_80BEC5E0(BgIkanaDharma* this, GlobalContext* globalCtx);
-void func_80BEC758(BgIkanaDharma* this);
-void func_80BEC790(BgIkanaDharma* this, GlobalContext* globalCtx);
-void func_80BEC7EC(BgIkanaDharma* this);
-void func_80BEC808(BgIkanaDharma* this, GlobalContext* globalCtx);
+void BgIkanaDharma_BeginNormalState(BgIkanaDharma* this);
+void BgIkanaDharma_UpdateNormalState(BgIkanaDharma* this, GlobalContext* globalCtx);
+void BgIkanaDharma_BeginWaitToStartCutscene(BgIkanaDharma* this);
+void BgIkanaDharma_WaitToStartCutscene(BgIkanaDharma* this, GlobalContext* globalCtx);
+void BgIkanaDharma_BeginCutsceneState(BgIkanaDharma* this);
+void BgIkanaDharma_UpdateCutscene(BgIkanaDharma* this, GlobalContext* globalCtx);
 
 const ActorInit Bg_Ikana_Dharma_InitVars = {
     ACTOR_BG_IKANA_DHARMA,
@@ -61,9 +61,9 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_F32_DIV1000(gravity, -1100, ICHAIN_STOP),
 };
 
-static BgIkanaDharma* D_80BECBD0;
+static BgIkanaDharma* g_firstHitBgIkanaDharma;
 
-void func_80BEC240(BgIkanaDharma* this, GlobalContext* globalCtx) {
+void BgIkanaDharma_CreateParticles(BgIkanaDharma* this, GlobalContext* globalCtx) {
     u32 pad;
     Vec3f pos;
     Vec3f velocity;
@@ -97,10 +97,9 @@ void func_80BEC240(BgIkanaDharma* this, GlobalContext* globalCtx) {
 }
 
 void BgIkanaDharma_Init(Actor* thisx, GlobalContext* globalCtx) {
-    ColliderCylinder* temp_s1;
-    s16 temp_v0;
+    s16 params;
     BgIkanaDharma* this = THIS;
-    f32 phi_f20;
+    f32 segmentY;
     GlobalContext* globalCtx2 = globalCtx;
     s32 i;
 
@@ -110,139 +109,137 @@ void BgIkanaDharma_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->dyna.actor.scale.z = 0.3f;
     DynaPolyActor_Init(&this->dyna, 0);
     DynaPolyActor_LoadMesh(globalCtx, &this->dyna, &object_ikana_obj_Colheader_000C50);
-    Collider_InitCylinder(globalCtx, &this->unk15C);
-    Collider_SetCylinder(globalCtx2, &this->unk15C, &this->dyna.actor, &sCylinderInit);
+    Collider_InitCylinder(globalCtx, &this->collider);
+    Collider_SetCylinder(globalCtx2, &this->collider, &this->dyna.actor, &sCylinderInit);
     i = 0;
-    temp_v0 = this->dyna.actor.params;
-    if ((((s32)temp_v0 >> 5) & 1) == 0) {
-        phi_f20 = this->dyna.actor.world.pos.y;
-        if (i < (temp_v0 & 0xF)) {
+    params = this->dyna.actor.params;
+    if (((params >> 5) & 1) == 0) {
+        segmentY = this->dyna.actor.world.pos.y;
+        if (i < (params & 0xF)) {
             do {
-                phi_f20 += 60.0f;
-                Actor_SpawnAsChildAndCutscene(
-                    &globalCtx2->actorCtx, globalCtx2, 0x273, this->dyna.actor.world.pos.x, phi_f20,
-                    this->dyna.actor.world.pos.z, (s16)(s32)this->dyna.actor.shape.rot.x,
-                    (s16)(s32)this->dyna.actor.shape.rot.y, (s16)(s32)this->dyna.actor.shape.rot.z, 0x20,
-                    (u32)this->dyna.actor.cutscene, (s32)this->dyna.actor.unk20, NULL);
+                segmentY += 60.0f;
+                Actor_SpawnAsChildAndCutscene(&globalCtx2->actorCtx, globalCtx2, ACTOR_BG_IKANA_DHARMA,
+                                              this->dyna.actor.world.pos.x, segmentY, this->dyna.actor.world.pos.z,
+                                              this->dyna.actor.shape.rot.x, this->dyna.actor.shape.rot.y,
+                                              this->dyna.actor.shape.rot.z, BGIKANADHARMA_FLAG_IS_CHILD,
+                                              this->dyna.actor.cutscene, this->dyna.actor.unk20, NULL);
                 i++;
-            } while (i != (temp_v0 & 0xF));
+            } while (i != (params & 0xF));
         }
         this->dyna.actor.bgCheckFlags |= 1;
     }
-    func_80BEC5C4(this);
+    BgIkanaDharma_BeginNormalState(this);
 }
 
 void BgIkanaDharma_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgIkanaDharma* this = THIS;
 
     DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
-    Collider_DestroyCylinder(globalCtx, &this->unk15C);
-    if (this == D_80BECBD0) {
-        D_80BECBD0 = NULL;
+    Collider_DestroyCylinder(globalCtx, &this->collider);
+    if (g_firstHitBgIkanaDharma == this) {
+        g_firstHitBgIkanaDharma = NULL;
     }
 }
 
-void func_80BEC5C4(BgIkanaDharma* this) {
-    this->actionFunc = &func_80BEC5E0;
+void BgIkanaDharma_BeginNormalState(BgIkanaDharma* this) {
+    this->actionFunc = &BgIkanaDharma_UpdateNormalState;
     this->dyna.actor.speedXZ = 0.0f;
 }
 
-void func_80BEC5E0(BgIkanaDharma* thisx, GlobalContext* globalCtx) {
+void BgIkanaDharma_UpdateNormalState(BgIkanaDharma* thisx, GlobalContext* globalCtx) {
     s32 phi_v0;
     Actor* temp_a3;
-    s32 temp_v0_2;
-    s16 temp_v0_3;
     BgIkanaDharma* this = thisx;
+    s32 temp_v0_3;
 
     temp_a3 = globalCtx->actorCtx.actorLists[2].first;
-    phi_v0 = (this->unk15C.base.acFlags & AC_HIT) != 0;
+    phi_v0 = (this->collider.base.acFlags & AC_HIT) != 0;
     if (phi_v0) {
-        this->unk15C.base.acFlags &= ~AC_HIT;
+        this->collider.base.acFlags &= ~AC_HIT;
     }
-    if (phi_v0 && D_80BECBD0 == 0) { // TODO: ! or ==NULL?
-        D_80BECBD0 = this;
+    if (phi_v0 && g_firstHitBgIkanaDharma == NULL) {
+        s32 temp_v0_2;
+        g_firstHitBgIkanaDharma = this;
         Flags_SetSwitch(globalCtx, BGIKANADHARMA_GET_SWITCHFLAG(&this->dyna.actor));
-        temp_v0_3 = this->dyna.actor.yawTowardsPlayer + 0x8000;
+        temp_v0_3 = (s16)(this->dyna.actor.yawTowardsPlayer + 0x8000);
         temp_v0_2 = ((s16)(temp_a3->shape.rot.y - temp_v0_3) >> 1);
         this->dyna.actor.world.rot.y = temp_v0_3 + temp_v0_2 + 0xF000;
         this->dyna.actor.speedXZ = 20.0f;
-        Actor_PlaySfxAtPos(&this->dyna.actor, 0x29BDU);
-        func_80BEC758(this);
-    } else if (((this->dyna.actor.flags & 0x40) == 0x40) && (D_80BECBD0 == 0) &&
-               (this->dyna.actor.xzDistToPlayer < 420.0f)) {
-        phi_v0 = (s16)(this->dyna.actor.yawTowardsPlayer - temp_a3->shape.rot.y);
-        if ((s32)phi_v0 < 0) {
-            phi_v0 = -phi_v0;
-            if (phi_v0) {}
+        Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_DARUMA_VANISH);
+        BgIkanaDharma_BeginWaitToStartCutscene(this);
+    } else if ((this->dyna.actor.flags & 0x40) == 0x40 && g_firstHitBgIkanaDharma == NULL &&
+               this->dyna.actor.xzDistToPlayer < 420.0f) {
+        temp_v0_3 = (s16)(this->dyna.actor.yawTowardsPlayer - temp_a3->shape.rot.y);
+        if (temp_v0_3 < 0) {
+            temp_v0_3 = -temp_v0_3; // this line is the reason temp_v0_3 isn't s16
         }
-        if (phi_v0 >= 0x4001) {
-            Collider_UpdateCylinder(&this->dyna.actor, &this->unk15C);
-            CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &thisx->unk15C.base);
+        if (temp_v0_3 >= 0x4001) {
+            Collider_UpdateCylinder(&this->dyna.actor, &this->collider);
+            CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &thisx->collider.base);
         }
     }
 }
 
-void func_80BEC758(BgIkanaDharma* arg0) {
-    ActorCutscene_SetIntentToPlay((s16)arg0->dyna.actor.cutscene);
-    arg0->actionFunc = func_80BEC790;
+void BgIkanaDharma_BeginWaitToStartCutscene(BgIkanaDharma* this) {
+    ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
+    this->actionFunc = BgIkanaDharma_WaitToStartCutscene;
 }
 
-void func_80BEC790(BgIkanaDharma* this, GlobalContext* globalCtx) {
-    if (ActorCutscene_GetCanPlayNext(this->dyna.actor.cutscene) != 0) {
+void BgIkanaDharma_WaitToStartCutscene(BgIkanaDharma* this, GlobalContext* globalCtx) {
+    if (ActorCutscene_GetCanPlayNext(this->dyna.actor.cutscene)) {
         ActorCutscene_StartAndSetUnkLinkFields(this->dyna.actor.cutscene, &this->dyna.actor);
-        func_80BEC7EC(this);
+        BgIkanaDharma_BeginCutsceneState(this);
     } else {
         ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
     }
 }
 
-void func_80BEC7EC(BgIkanaDharma* this) {
-    this->unk_1AC = 0xA;
-    this->actionFunc = &func_80BEC808;
+void BgIkanaDharma_BeginCutsceneState(BgIkanaDharma* this) {
+    this->cutsceneFramesRemaining = 0xA;
+    this->actionFunc = &BgIkanaDharma_UpdateCutscene;
 }
 
-void func_80BEC808(BgIkanaDharma* this, GlobalContext* arg1) {
-    if (this->unk_1AC > 0) {
-        this->unk_1AC--;
-        if (this->unk_1AC == 0) {
-            if (D_80BECBD0 == this) {
-                D_80BECBD0 = NULL;
+void BgIkanaDharma_UpdateCutscene(BgIkanaDharma* this, GlobalContext* globalCtx) {
+    if (this->cutsceneFramesRemaining > 0) {
+        this->cutsceneFramesRemaining--;
+        if (this->cutsceneFramesRemaining == 0) {
+            if (g_firstHitBgIkanaDharma == this) {
+                g_firstHitBgIkanaDharma = NULL;
             }
             ActorCutscene_Stop(this->dyna.actor.cutscene);
         }
     }
-    if (Math_StepToF(&this->dyna.actor.scale.y, 0.0f, 0.0033333334f) != 0) {
+    if (Math_StepToF(&this->dyna.actor.scale.y, 0.0f, 1.0f / 300.0f) != 0) {
         Actor_MarkForDeath(&this->dyna.actor);
         return;
     }
     this->dyna.actor.scale.x = this->dyna.actor.scale.y * 3.0f;
     this->dyna.actor.scale.z = this->dyna.actor.scale.y * 3.0f;
-    func_80BEC240(this, arg1);
+    BgIkanaDharma_CreateParticles(this, globalCtx);
 }
 
 void BgIkanaDharma_Update(Actor* thisx, GlobalContext* globalCtx) {
     BgIkanaDharma* this = (BgIkanaDharma*)thisx;
 
     this->actionFunc(this, globalCtx);
-    if (this->actionFunc == func_80BEC5E0) {
-        DynaPolyActor* temp_v0;
+    if (this->actionFunc == BgIkanaDharma_UpdateNormalState) {
+        DynaPolyActor* actorBelow;
         u32 pad, pad2;
-        s16 temp_v0_2;
         func_800B4AEC(globalCtx, &this->dyna.actor, 30.0f);
-        temp_v0 = DynaPoly_GetActor(&globalCtx->colCtx, this->dyna.actor.floorBgId);
-        if (temp_v0 == 0) {
+        actorBelow = DynaPoly_GetActor(&globalCtx->colCtx, this->dyna.actor.floorBgId);
+        if (actorBelow == NULL) {
             Actor_MoveWithGravity(&this->dyna.actor);
-            Actor_UpdateBgCheckInfo(globalCtx, &this->dyna.actor, 0.0f, 0.0f, 0.0f, 4U);
-            if ((this->dyna.actor.bgCheckFlags & 2) != 0) {
-                temp_v0_2 = Quake_Add(globalCtx->cameraPtrs[globalCtx->activeCamera], 3U);
-                Quake_SetSpeed(temp_v0_2, 0x5420);
-                Quake_SetQuakeValues(temp_v0_2, 4, 0, 0, 0);
-                Quake_SetCountdown(temp_v0_2, 0xC);
-                Actor_PlaySfxAtPos(&this->dyna.actor, 0x2835U); // TODO look up constant
+            Actor_UpdateBgCheckInfo(globalCtx, &this->dyna.actor, 0.0f, 0.0f, 0.0f, 4);
+            if (this->dyna.actor.bgCheckFlags & 2) {
+                s16 quake = Quake_Add(globalCtx->cameraPtrs[globalCtx->activeCamera], 3);
+                Quake_SetSpeed(quake, 21536);
+                Quake_SetQuakeValues(quake, 4, 0, 0, 0);
+                Quake_SetCountdown(quake, 12);
+                Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_BLOCK_BOUND);
             }
         } else {
-            if (temp_v0->actor.id == 0x273) { // TODO look up constant
-                this->dyna.actor.world.pos.y = temp_v0->actor.world.pos.y + 60.0f;
+            if (actorBelow->actor.id == ACTOR_BG_IKANA_DHARMA) {
+                this->dyna.actor.world.pos.y = actorBelow->actor.world.pos.y + 60.0f;
             } else {
                 this->dyna.actor.world.pos.y = this->dyna.actor.floorHeight;
             }
@@ -254,7 +251,7 @@ void BgIkanaDharma_Update(Actor* thisx, GlobalContext* globalCtx) {
             phi_f0 = 2.0f;
         }
         Actor_MoveWithGravity(&this->dyna.actor);
-        Actor_UpdateBgCheckInfo(globalCtx, &this->dyna.actor, 20.0f, phi_f0, 0.0f, 5U);
+        Actor_UpdateBgCheckInfo(globalCtx, &this->dyna.actor, 20.0f, phi_f0, 0.0f, 5);
     }
     Actor_SetFocus(&this->dyna.actor, 40.0f);
 }
