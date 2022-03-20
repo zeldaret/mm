711dcd10d3ea9f2e18b3b3be6909971c98e8a93e
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-03-20 21:09:47 +0000

    Obj_Grass_Carry

--- src/overlays/actors/ovl_Obj_Grass_Carry/z_obj_grass_carry.h
+++ src/overlays/actors/ovl_Obj_Grass_Carry/z_obj_grass_carry.h
@@ -2,6 +2,7 @@
 #define Z_OBJ_GRASS_CARRY_H
 
 #include "global.h"
+#include "overlays/actors/ovl_Obj_Grass/z_obj_grass.h"
 
 struct ObjGrassCarry;
 
@@ -9,7 +10,11 @@ typedef void (*ObjGrassCarryActionFunc)(struct ObjGrassCarry*, GlobalContext*);
 
 typedef struct ObjGrassCarry {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x58];
+    /* 0x0144 */ ColliderCylinder collider;
+    /* 0x0190 */ ObjGrass* unk_190;
+    /* 0x0194 */ ObjGrassStruct3* unk_194;
+    /* 0x0198 */ s16 unk_198;
+    /* 0x019A */ s16 unk_19A;
     /* 0x019C */ ObjGrassCarryActionFunc actionFunc;
 } ObjGrassCarry; // size = 0x1A0
 
