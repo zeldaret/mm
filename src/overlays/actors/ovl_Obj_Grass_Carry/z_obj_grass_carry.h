d18810a9cc3004262f18c81e50ca9fd4860e35f9
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-03-20 21:18:10 +0000

    Fix?

--- src/overlays/actors/ovl_Obj_Grass_Carry/z_obj_grass_carry.h
+++ src/overlays/actors/ovl_Obj_Grass_Carry/z_obj_grass_carry.h
@@ -5,14 +5,15 @@
 #include "overlays/actors/ovl_Obj_Grass/z_obj_grass.h"
 
 struct ObjGrassCarry;
+struct ObjGrass;
 
 typedef void (*ObjGrassCarryActionFunc)(struct ObjGrassCarry*, GlobalContext*);
 
 typedef struct ObjGrassCarry {
     /* 0x0000 */ Actor actor;
     /* 0x0144 */ ColliderCylinder collider;
-    /* 0x0190 */ ObjGrass* unk_190;
-    /* 0x0194 */ ObjGrassStruct3* unk_194;
+    /* 0x0190 */ struct ObjGrass* unk_190;
+    /* 0x0194 */ struct ObjGrassStruct3* unk_194;
     /* 0x0198 */ s16 unk_198;
     /* 0x019A */ s16 unk_19A;
     /* 0x019C */ ObjGrassCarryActionFunc actionFunc;
