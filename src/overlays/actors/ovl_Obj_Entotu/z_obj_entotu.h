f832299123d0c8efe04d157a633db5ced320ee24
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-03-21 20:19:09 +0000

    PR

--- src/overlays/actors/ovl_Obj_Entotu/z_obj_entotu.h
+++ src/overlays/actors/ovl_Obj_Entotu/z_obj_entotu.h
@@ -8,7 +8,7 @@ struct ObjEntotu;
 typedef struct ObjEntotu {
     /* 0x000 */ Actor actor;
     /* 0x144 */ UNK_TYPE1 unk144[4];
-    /* 0x148 */ u8 unk_148[7][16];
+    /* 0x148 */ Vtx unk_148[7];
     /* 0x1B8 */ Vec3f unk_1B8;
     /* 0x1C4 */ u8 unk_1C4;
     /* 0x1C6 */ s16 unk_1C6;
