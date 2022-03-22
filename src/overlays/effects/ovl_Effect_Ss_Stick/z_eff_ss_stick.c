87d5ab4303e6800812d9c26c0e146f891d5310c1
Derek Hensley <hensley.derek58@gmail.com>
2022-03-21 18:22:17 -0700

    cleanup

--- src/overlays/effects/ovl_Effect_Ss_Stick/z_eff_ss_stick.c
+++ src/overlays/effects/ovl_Effect_Ss_Stick/z_eff_ss_stick.c
@@ -7,6 +7,9 @@
 #include "z_eff_ss_stick.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 
+#define rObjBankIdx regs[0]
+#define rYaw regs[1]
+
 #define PARAMS ((EffectSsStickInitParams*)initParamsx)
 
 u32 EffectSsStick_Init(GlobalContext* globalCtx, u32 index, EffectSs* this, void* initParamsx);
@@ -19,16 +22,16 @@ const EffectSsInit Effect_Ss_Stick_InitVars = {
 };
 
 u32 EffectSsStick_Init(GlobalContext* globalCtx, u32 index, EffectSs* this, void* initParamsx) {
-    EffectSsStickInitParams* params = PARAMS;
+    EffectSsStickInitParams* initParams = PARAMS;
     Vec3f pos;
 
-    this->regs[0] = Object_GetIndex(&globalCtx->objectCtx, GAMEPLAY_KEEP);
-    pos = params->pos;
+    this->rObjBankIdx = Object_GetIndex(&globalCtx->objectCtx, GAMEPLAY_KEEP);
+    pos = initParams->pos;
     this->pos = pos;
     this->vec = pos;
-    this->regs[1] = params->yaw;
-    this->velocity.x = Math_SinS(params->yaw) * 6.0f;
-    this->velocity.z = Math_CosS(params->yaw) * 6.0f;
+    this->rYaw = initParams->yaw;
+    this->velocity.x = Math_SinS(initParams->yaw) * 6.0f;
+    this->velocity.z = Math_CosS(initParams->yaw) * 6.0f;
     this->life = 20;
     this->draw = EffectSsStick_Draw;
     this->update = EffectSsStick_Update;
@@ -42,14 +45,16 @@ void EffectSsStick_Draw(GlobalContext* globalCtx, u32 index, EffectSs* this) {
     GraphicsContext* gfxCtx = globalCtx->state.gfxCtx;
 
     OPEN_DISPS(gfxCtx);
+
     Matrix_InsertTranslation(this->pos.x, this->pos.y, this->pos.z, MTXMODE_NEW);
     Matrix_Scale(0.01f, 0.0025f, 0.01f, MTXMODE_APPLY);
-    Matrix_InsertRotation(0, this->regs[1], 0, MTXMODE_APPLY);
+    Matrix_InsertRotation(0, this->rYaw, 0, MTXMODE_APPLY);
     gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     func_8012C28C(gfxCtx);
-    gSPSegment(POLY_OPA_DISP++, 0x06, globalCtx->objectCtx.status[this->regs[0]].segment);
+    gSPSegment(POLY_OPA_DISP++, 0x06, globalCtx->objectCtx.status[this->rObjBankIdx].segment);
     gSPSegment(POLY_OPA_DISP++, 0x0C, D_801C0850);
-    gSPDisplayList(POLY_OPA_DISP++, gameplay_keep_DL_0032B0);
+    gSPDisplayList(POLY_OPA_DISP++, gDekuStickDL);
+
     CLOSE_DISPS(gfxCtx);
 }
 
