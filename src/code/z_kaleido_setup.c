9691abe84879dc32d7c33238b9f49ec0c4077b64
angie <angheloalf95@gmail.com>
2022-03-27 00:31:24 -0300

    func_800F4C0C

--- src/code/z_kaleido_setup.c
+++ src/code/z_kaleido_setup.c
@@ -2,7 +2,59 @@
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_setup/func_800F4A10.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_kaleido_setup/func_800F4C0C.s")
+void func_800F4C0C(GlobalContext* globalCtx) {
+    Input* input;
+    MessageContext* msgCtx; // sp24
+    Player* player;
+    PauseContext* pauseCtx; // sp1C
+
+
+    player = GET_PLAYER(globalCtx);
+
+
+    input = CONTROLLER1(globalCtx);
+    msgCtx = &globalCtx->msgCtx;
+    pauseCtx = &globalCtx->pauseCtx;
+
+    if (~(input->cur.button | ~0x10) == 0) {
+        if (msgCtx && msgCtx) {}
+    }
+
+    if (pauseCtx->state == 0) {
+        if (pauseCtx->debugState == 0) {
+            if ((globalCtx->gameOverCtx.state == 0) && (globalCtx->sceneLoadFlag == 0) && (globalCtx->unk_18B4A == 0) && (gSaveContext.cutscene < 0xFFF0)) {
+                if (gSaveContext.nextCutsceneIndex < 0xFFF0) {
+                    if ((func_801690CC(globalCtx) == 0) || (( (msgCtx->msgMode != 0)) && (msgCtx->unk11F04 == 0xFF))) {
+                        if ((globalCtx->unk_1887C < 2)) {
+                            if ((gSaveContext.unk_3F28 != 8) && (gSaveContext.unk_3F28 != 9)) {
+                                if (((gSaveContext.eventInf[1] & 0x80) == 0) && ((player->stateFlags1 & 0x20) == 0)) {
+                                    if (((globalCtx->actorCtx.unk5 & 2) == 0) && ((globalCtx->actorCtx.unk5 & 4) == 0)) {
+                                        if ((globalCtx->actorCtx.unk268 == 0) && (~(input->press.button | ~0x1000) == 0)) {
+                                            gSaveContext.unk_3F26 = gSaveContext.unk_3F22;
+                                            pauseCtx->unk_2B9 = 0;
+                                            pauseCtx->state = 1;
+                                            func_800F4A10(globalCtx);
+                                            pauseCtx->mode = (pauseCtx->pageIndex * 2) + 1;
+                                            func_801A3A7C(1);
+                                        }
+                                        if (pauseCtx->state == 1) {
+                                            Game_SetFramerateDivisor(&globalCtx->state, 2);
+                                            if (ShrinkWindow_GetLetterboxTarget() != 0) {
+                                                ShrinkWindow_SetLetterboxTarget(0);
+                                            }
+                                            func_801A3AEC(1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 void KaleidoSetup_Init(GlobalContext* globalCtx) {
     PauseContext* pauseCtx = &globalCtx->pauseCtx;
