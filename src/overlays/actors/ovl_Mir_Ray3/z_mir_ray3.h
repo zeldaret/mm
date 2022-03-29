b4842eb16c046ee4efedc889bdf52f1a818b3e3f
Maide <34639600+Kelebek1@users.noreply.github.com>
2022-03-29 22:26:56 +0100

    Mir_Ray3 (1 non-matching) (#695)

--- src/overlays/actors/ovl_Mir_Ray3/z_mir_ray3.h
+++ src/overlays/actors/ovl_Mir_Ray3/z_mir_ray3.h
@@ -5,9 +5,18 @@
 
 struct MirRay3;
 
+#define MIRRAY3_GET_F(thisx) ((thisx)->params & 0xF)
+
+#define MIRRAY3_F_1 1
+
 typedef struct MirRay3 {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x120];
+    /* 0x144 */ ColliderQuad colliderQuad;
+    /* 0x1C4 */ ColliderCylinder colliderCylinder;
+    /* 0x210 */ u16 unk_210;
+    /* 0x214 */ f32 unk_214;
+    /* 0x218 */ Vec3f unk_218[6];
+    /* 0x260 */ f32 unk_260;
 } MirRay3; // size = 0x264
 
 extern const ActorInit Mir_Ray3_InitVars;
