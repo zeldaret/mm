8a8649b5eb02b2940e86c1a56f1e180d70ad5387
nckwntzl <nckwntzl@gmail.com>
2022-04-08 15:10:10 -0400

    ran format

--- src/overlays/actors/ovl_Obj_Blockstop/z_obj_blockstop.c
+++ src/overlays/actors/ovl_Obj_Blockstop/z_obj_blockstop.c
@@ -15,8 +15,7 @@ void ObjBlockstop_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjBlockstop_Update(Actor* thisx, GlobalContext* globalCtx);
 
 void ObjBlockstop_CheckCollision(ObjBlockstop* this, GlobalContext* globalCtx);
-void ObjBlockstop_TryPlayCutscene(ObjBlockstop *arg0, GlobalContext *arg1);
-
+void ObjBlockstop_TryPlayCutscene(ObjBlockstop* arg0, GlobalContext* arg1);
 
 const ActorInit Obj_Blockstop_InitVars = {
     ACTOR_OBJ_BLOCKSTOP,
@@ -30,56 +29,48 @@ const ActorInit Obj_Blockstop_InitVars = {
     (ActorFunc)NULL,
 };
 
-
-
-void ObjBlockstop_Init(Actor *thisx, GlobalContext *globalCtx) {
-    ObjBlockstop *this = THIS;
-    if (Flags_GetSwitch(globalCtx, (s32) this->actor.params)) {
+void ObjBlockstop_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ObjBlockstop* this = THIS;
+    if (Flags_GetSwitch(globalCtx, (s32)this->actor.params)) {
         Actor_MarkForDeath(&this->actor);
     }
     this->actionFunc = ObjBlockstop_CheckCollision;
 }
 
-
-
-
-
-//checks that the position of the push block is within 20 units and activates, since the block falls and stops on the blockstop
-void ObjBlockstop_CheckCollision(ObjBlockstop *this, GlobalContext *globalCtx) {
-    Actor *tempActor;
+// checks that the position of the push block is within 20 units and activates, since the block falls and stops on the
+// blockstop
+void ObjBlockstop_CheckCollision(ObjBlockstop* this, GlobalContext* globalCtx) {
+    Actor* tempActor;
     tempActor = globalCtx->actorCtx.actorLists[6].first;
-        while (tempActor){
-            if ((tempActor->id == 0x7A) && //check if oshihiki (push block)
-            (fabsf(tempActor->world.pos.x - this->actor.world.pos.x) < 20.0f) && //check coords for collision
-            (fabsf(tempActor->world.pos.z - this->actor.world.pos.z) < 20.0f) && 
+    while (tempActor) {
+        if ((tempActor->id == 0x7A) &&                                           // check if oshihiki (push block)
+            (fabsf(tempActor->world.pos.x - this->actor.world.pos.x) < 20.0f) && // check coords for collision
+            (fabsf(tempActor->world.pos.z - this->actor.world.pos.z) < 20.0f) &&
             (fabsf(tempActor->world.pos.y - this->actor.world.pos.y) < 20.0f)) {
-                 
-                s32 params = OBJOSHIHIKI_GET_F(tempActor);
-                if(params < 3){
-                    ActorCutscene_SetIntentToPlay((s16) this->actor.cutscene);
-                    this->actionFunc = ObjBlockstop_TryPlayCutscene;
-                }
+
+            s32 params = OBJOSHIHIKI_GET_F(tempActor);
+            if (params < 3) {
+                ActorCutscene_SetIntentToPlay((s16)this->actor.cutscene);
+                this->actionFunc = ObjBlockstop_TryPlayCutscene;
             }
-            tempActor = tempActor->next;
         }
-    
+        tempActor = tempActor->next;
+    }
 }
 
-
-
-void ObjBlockstop_TryPlayCutscene(ObjBlockstop *arg0, GlobalContext *arg1) {
-    if (ActorCutscene_GetCanPlayNext((s16) arg0->actor.cutscene) != 0) {
-        Flags_SetSwitch(arg1, (s32) arg0->actor.params);
-        if (ActorCutscene_GetLength((s16) arg0->actor.cutscene) != -1) {
-            ActorCutscene_StartAndSetUnkLinkFields((s16) arg0->actor.cutscene, &arg0->actor);
+void ObjBlockstop_TryPlayCutscene(ObjBlockstop* arg0, GlobalContext* arg1) {
+    if (ActorCutscene_GetCanPlayNext((s16)arg0->actor.cutscene) != 0) {
+        Flags_SetSwitch(arg1, (s32)arg0->actor.params);
+        if (ActorCutscene_GetLength((s16)arg0->actor.cutscene) != -1) {
+            ActorCutscene_StartAndSetUnkLinkFields((s16)arg0->actor.cutscene, &arg0->actor);
         }
         Actor_MarkForDeath(&arg0->actor);
         return;
     }
-    ActorCutscene_SetIntentToPlay((s16) arg0->actor.cutscene);
+    ActorCutscene_SetIntentToPlay((s16)arg0->actor.cutscene);
 }
 
-void ObjBlockstop_Update(Actor *thisx, GlobalContext *globalCtx) {
-    ObjBlockstop *this = (ObjBlockstop *) thisx;
+void ObjBlockstop_Update(Actor* thisx, GlobalContext* globalCtx) {
+    ObjBlockstop* this = (ObjBlockstop*)thisx;
     this->actionFunc(this, globalCtx);
 }
