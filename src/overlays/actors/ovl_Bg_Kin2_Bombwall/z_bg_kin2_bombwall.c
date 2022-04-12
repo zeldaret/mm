0d14388908ab4e800175af66c57a912eba99a420
TrulyNaN <trulynan@gmail.com>
2022-04-11 22:08:52 -0400

    init for OSH bombable wall started. put back Actor* in function declarations.

--- src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
+++ src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
@@ -56,7 +56,44 @@ extern UNK_TYPE D_06000490;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E090.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/BgKin2Bombwall_Init.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/BgKin2Bombwall_Init.s")
+
+typedef struct BgKin2Bombwall {
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ char pad144[0x18];
+    /* 0x15C */ ColliderCylinder unk15C;            /* inferred */
+    /* 0x1A8 */ void (*actionFunc)(BgKin2Bombwall *, GlobalContext *);
+    /* 0x1AC */ char pad1AC[4];
+} BgKin2Bombwall;                                   /* size = 0x1B0 */
+
+? func_80B6E4B8(BgKin2Bombwall *);                  /* extern */
+ColliderCylinderInit D_80B6E6F0 = {
+    { 0xA, 0, 9, 0, 0, 1 },
+    { 0, { 0, 0, 0 }, { 8, 0, 0 }, 0, 1, 0 },
+    { 0x3C, 0x3C, 0, { 0, 0, 0 } },
+};
+InitChainEntry D_80B6E748[6];                       /* unable to generate initializer */
+
+void BgKin2Bombwall_Init(Actor* thisx, GlobalContext *globalCtx) {
+    BgKin2Bombwall* this = THIS;
+    ColliderCylinder *sp24;
+    ColliderCylinder *temp_a1;
+
+    Actor_ProcessInitChain(&this->actor, D_80B6E748);
+    DynaPolyActor_Init((DynaPolyActor *) this, 0);
+    temp_a1 = &this->unk15C;
+    sp24 = temp_a1;
+    Collider_InitCylinder(globalCtx, temp_a1);
+    if (Flags_GetSwitch(globalCtx, this->actor.params & 0x7F) != 0) {
+        Actor_MarkForDeath(&this->actor);
+        return;
+    }
+    DynaPolyActor_LoadMesh(globalCtx, (DynaPolyActor *) this, (CollisionHeader *) &D_06000490);
+    Collider_SetCylinder(globalCtx, sp24, &this->actor, &D_80B6E6F0);
+    Collider_UpdateCylinder(&this->actor, sp24);
+    Actor_SetHeight(&this->actor, 60.0f);
+    func_80B6E4B8(this);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/BgKin2Bombwall_Destroy.s")
 
