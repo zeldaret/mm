5a8db617d5fa7b1bfe2c3d9ff94ae83e6bf628ed
TrulyNaN <trulynan@gmail.com>
2022-04-13 21:04:05 -0400

    BgKin2Bombwall_Init is matching.

--- src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.h
+++ src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.h
@@ -9,10 +9,19 @@ typedef void (*BgKin2BombwallActionFunc)(struct BgKin2Bombwall*, GlobalContext*)
 
 typedef struct BgKin2Bombwall {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x64];
+    /* 0x0144 */ char unk_144[0x18];
+    /* 0x15C */ ColliderCylinder unk15C;  
     /* 0x01A8 */ BgKin2BombwallActionFunc actionFunc;
     /* 0x01AC */ char unk_1AC[0x4];
 } BgKin2Bombwall; // size = 0x1B0
+
+//typedef struct BgKin2Bombwall {
+//    /* 0x000 */ Actor actor;
+//    /* 0x144 */ char pad144[0x18];
+//    /* 0x15C */ ColliderCylinder unk15C;            /* inferred */
+//    /* 0x1A8 */ void (*actionFunc)(BgKin2Bombwall *, GlobalContext *);
+//    /* 0x1AC */ char pad1AC[4];
+//} BgKin2Bombwall;   
 
 extern const ActorInit Bg_Kin2_Bombwall_InitVars;
 
