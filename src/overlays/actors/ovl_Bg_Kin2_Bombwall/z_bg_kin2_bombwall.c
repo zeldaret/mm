d26505690d9486e6b299d68114c866b3ec7f6d70
TrulyNaN <trulynan@gmail.com>
2022-04-18 19:09:38 -0400

    func_80B6E020 from ovl_Bg_Kin2_Bombwall matches.

--- src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
+++ src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
@@ -44,9 +44,95 @@ extern UNK_TYPE D_06000490;
 extern ColliderCylinderInit D_80B6E6F0;
 extern InitChainEntry D_80B6E748[];
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E020.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E020.s")
+
+s32 func_80B6E020(BgKin2Bombwall *arg0, GlobalContext *arg1) {
+    Actor *temp_v0;
+
+    if ((arg0->unk15C.base.acFlags & 2) != 0) {
+        temp_v0 = arg0->unk15C.base.ac;
+        if ((temp_v0 != 0) && (Math3D_Vec3fDistSq(&arg0->actor.world.pos, &temp_v0->world.pos) < 6400.0f)) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E090.s")
+// extern Color_RGBA8 D_80B6E71C;
+// extern Color_RGBA8 D_80B6E720;
+// extern Vec3f D_80B6E724;
+// extern s8 D_80B6E730;
+// extern s8 D_80B6E735; //? type before
+// extern s8 D_80B6E738; //? type before
+
+// void func_80B6E090(BgKin2Bombwall *arg0, GlobalContext *arg1) {
+//     Vec3f spF0;
+//     Vec3f spE4;
+//     f32 spE0;
+//     f32 spDC;
+//     f32 spD8;
+//     f32 spD4;
+//     f32 spD0;
+//     f32 spCC;
+//     s32 spAC;
+//     Vec3f *temp_s5;
+//     s32 temp_a0;
+//     s32 temp_s2;
+//     s32 temp_s3;
+//     s8 *temp_s4;
+//     s32 phi_v0;
+//     s8 *phi_s4;
+//     s32 phi_s3;
+//     s32 phi_s0;
+//     s32 phi_s1;
+//     s32 phi_s0_2;
+
+//     Matrix_RotateY(arg0->actor.shape.rot.y, 0);
+//     temp_s5 = &spF0;
+//     phi_v0 = 0;
+//     phi_s3 = 0;
+//     do {
+//         temp_a0 = phi_v0 + 1;
+//         spAC = temp_a0;
+//         phi_s4 = &D_80B6E730;
+// loop_2:
+//         temp_s3 = (phi_s3 + 1) & 7;
+//         spD8 = (f32) ((Rand_Next() >> 0x1C) + *phi_s4);
+//         spDC = ((Rand_ZeroOne() - 0.5f) * 15.0f) + ((f32) temp_a0 * 15.0f);
+//         spE0 = (Rand_ZeroOne() * 20.0f) - 10.0f;
+//         spCC = (2.0f * (Rand_ZeroOne() - 0.5f)) + (spD8 * 0.018461538f);
+//         spD0 = (Rand_ZeroOne() * 7.0f) + 4.0f;
+//         spD4 = spE0 * 0.3f;
+//         Matrix_MultiplyVector3fByState((Vec3f *) &spD8, temp_s5);
+//         Matrix_MultiplyVector3fByState((Vec3f *) &spCC, &spE4);
+//         // spF0 = (bitwise f32) spF0 + arg0->actor.world.pos.x;
+//         spF0.x += arg0->actor.world.pos.x; //guessed
+//         spF0.y += arg0->actor.world.pos.y;
+//         spF0.z += arg0->actor.world.pos.z;
+//         phi_s3 = temp_s3;
+//         phi_s3 = temp_s3;
+//         if ((Rand_Next() & 3) == 0) {
+//             phi_s0_2 = 0x20;
+//         } else {
+//             phi_s0_2 = 0x40;
+//         }
+//         phi_s0 = phi_s0_2;
+//         if ((temp_s3 < 2) || (phi_s1 = 0, (Rand_Next() > 0))) {
+//             temp_s2 = Rand_Next();
+//             func_800B0E48(arg1, temp_s5, &D_801D15B0, &D_80B6E724, &D_80B6E71C, &D_80B6E720, (s16) (((u32) temp_s2 >> 0x1B) + 0x46), (s16) ((Rand_Next() >> 0x1A) + 0x3C));
+//             phi_s0 = (s32) (s16) (phi_s0_2 | 1);
+//             phi_s1 = 1;
+//         }
+//         EffectSsKakera_Spawn(arg1, temp_s5, &spE4, temp_s5, (s16) -0x226, (s16) phi_s0, (s16) 0x1E, (s16) 0, (s16) 0, (s16) (s32) *(&D_80B6E738 + (temp_s3 * 2)), (s16) phi_s1, (s16) 0, 0x32, (s16) -1, (s16) 0x1F5, (Gfx *) &D_06000128);
+//         temp_s4 = phi_s4 + 1;
+//         phi_s4 = temp_s4;
+//         if (temp_s4 != &D_80B6E735) {
+//             goto loop_2;
+//         }
+//         phi_v0 = spAC;
+//     } while (spAC != 6);
+// }
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/BgKin2Bombwall_Init.s")
     
 void func_80B6E4B8(BgKin2Bombwall *);                  /* extern */
