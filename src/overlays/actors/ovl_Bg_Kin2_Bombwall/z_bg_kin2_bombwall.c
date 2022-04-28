26fab85a2f14dd91fa5815ae328263d37320864f
TrulyNaN <trulynan@gmail.com>
2022-04-27 21:26:15 -0400

    Put in the changes made by EllipticEllipsis and hensldm for ovl_Bg_Kin2_Bombwall's func_8066E090.

--- src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
+++ src/overlays/actors/ovl_Bg_Kin2_Bombwall/z_bg_kin2_bombwall.c
@@ -38,7 +38,7 @@ static InitChainEntry D_80B6E748[] = {
     ICHAIN_VEC3F_DIV1000(scale, 1000, ICHAIN_STOP),
 };
 #endif
-extern Gfx D_06000128;
+extern Gfx D_06000128[];
 extern UNK_TYPE D_06000360;
 extern UNK_TYPE D_06000490;
 extern ColliderCylinderInit D_80B6E6F0;
@@ -48,8 +48,8 @@ extern InitChainEntry D_80B6E748[];
 extern Color_RGBA8 D_80B6E71C;
 extern Color_RGBA8 D_80B6E720;
 extern Vec3f D_80B6E724;
-extern s8 D_80B6E730;
-extern s8 D_80B6E735; //? type before
+extern s8 D_80B6E730[];
+extern s8 D_80B6E735[5]; //? type before
 extern s16 D_80B6E738[]; //? type before. should be an s16 array
 
 //from Draw function :
@@ -69,86 +69,67 @@ s32 func_80B6E020(BgKin2Bombwall *arg0, GlobalContext *arg1) {
     return 0;
 }
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E090.s")
-void func_80B6E090(BgKin2Bombwall *arg0, GlobalContext *arg1) {
-    BgKin2Bombwall *this = arg0;
-    GlobalContext *globalCtx = arg1;
-    Vec3f spF0; 
-    Vec3f spE4;
-    f32 spE0;
-    f32 spDC; 
-    Vec3f spD8; 
-    f32 spD4; 
-    f32 spD0; 
-    Vec3f spCC;
-    s32 i;
-    //Vec3f *temp_s5; //confirmed fake
-    s32 temp_a0;
-    s32 phi_s0; //s0
-    s32 phi_s1;
-    s32 temp_s2;
-    s32 temp_s3;
-    //s8 *temp_s4; //confirmed fake
-    //s32 phi_v0; //fake?
-    s8 *phi_s4; //$f6 that should be $f16
-    //s32 phi_s3; //confirmed fake
-    
-    //s32 phi_s0_2;
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/func_80B6E090.s")
+// #define ARRAY_COUNT(arr) (s32)(sizeof(arr) / sizeof(arr[0]))
 
-    Matrix_RotateY(this->actor.shape.rot.y, 0); //s6 should be actor.
-    i = 0;
-    temp_s3 = 0;
-    
-    do {
-        temp_a0 = i + 1; //result should go into a0.
-        //i = temp_a0; //should be stored into a0 for next function.
-        
-        phi_s4 = &D_80B6E730;//should be 0x4170 in at
-loop_2:
-        temp_s3 = (temp_s3 + 1) & 7;
-        spD8.x = (f32) (((Rand_Next()) >> 0x1C) + *phi_s4); //spD8 should be $f6?
-        spD8.y = ((Rand_ZeroOne() - 0.5f) * 15.0f) + ((f32) temp_a0 * 15.0f);  //temp_a0 should be $f22
-        spD8.z = (Rand_ZeroOne() * 20.0f) - 10.0f;//must find $f10 (is spD8 for now)
-        spCC.x = (2.0f * (Rand_ZeroOne() - 0.5f)) + (spD8.x * 0.018461538f);//must find $f10.
-        spCC.y = (Rand_ZeroOne() * 7.0f) + 4.0f;
-        spCC.z = spD8.z * 0.3f; //should be $f10?
-        //spD8 = this->actor.shape.rot.z;
-        
-        Matrix_MultiplyVector3fByState( &spD8, &spE4);
-        Matrix_MultiplyVector3fByState( &spCC, (Vec3f *) &spE4);
+// void func_80B6E090(BgKin2Bombwall *this, GlobalContext *globalCtx) {
+//     s32 i;
+//     Vec3f spF0; 
+//     Vec3f spE4;
+//     Vec3f spD8; 
+//     Vec3f spCC;
+//     s32 j;
+//     s32 temp_s3;
+//     f32 temp_a0;
+//     s32 temp_s2;
+//     s16 phi_s0; //s0
+//     s16 phi_s1;
 
-        spF0.x += this->actor.world.pos.x; //guessed
-        spF0.y += this->actor.world.pos.y;
-        spF0.z += this->actor.world.pos.z;
-        //phi_s3 = temp_s3;
-        //phi_s3 = temp_s3;
-        if (Rand_Next() % 4 == 0) {
-            phi_s0 = 0x20;
-        } else {
-            phi_s0 = 0x40;
-        }
-        //phi_s0 = phi_s0_2;
-        if (temp_s3 < 2 ||  Rand_Next() > 0) {
-            phi_s0 = (s32) (s16) (phi_s0 | 1);
-            phi_s1 = 1;
-            func_800B0E48(globalCtx, &spF0, &D_801D15B0, &D_80B6E724, &D_80B6E71C, &D_80B6E720, 
-                             (((u32) Rand_Next() >> 0x1B) + 0x46), 
-                             (( ( (u32) Rand_Next() ) >> 0x1A) + 0x3C));
-        } else {
-            phi_s1 = 0;
-        }
-        //temp_s3 = temp_s3*2;
-        EffectSsKakera_Spawn(globalCtx, &spF0, &spE4, &spF0, -0x226, phi_s0, 0x1E, 0, 
-                             0,  D_80B6E738[temp_s3], phi_s1, 0, 0x32, -1, 
-                             0x1F5,  (Gfx *) &D_06000128);
-        
-        //phi_s4 = temp_s4;
-        if (phi_s4 = phi_s4 + 1, phi_s4 != &D_80B6E735) {
-            goto loop_2;
-        }
-        i=i+1;
-    } while (i != 6);
-}
+
+//     Matrix_RotateY(this->actor.shape.rot.y, 0);
+    
+//     for(i = 0, temp_s3 = 0; i < 6; i++) {
+//         temp_a0 = (i + 1) * 15.f;   
+//         for (j = 0; j < ARRAY_COUNT(D_80B6E735); j++) {
+//             temp_s3++;
+//             temp_s3 &= 7;
+            
+//             spD8.x = D_80B6E730[j] + (s32)(((u32)Rand_Next()) >> 0x1C);
+//             spD8.y = ((Rand_ZeroOne() - 0.5f) * 15.0f) + temp_a0;  
+//             spD8.z = (Rand_ZeroOne() * 20.0f) - 10.0f;
+            
+//             spCC.x = (2.0f * (Rand_ZeroOne() - 0.5f)) + (spD8.x * 0.018461538f);
+//             spCC.y = (Rand_ZeroOne() * 7.0f) + 4.0f;
+//             spCC.z = spD8.z * 0.3f;
+            
+//             Matrix_MultiplyVector3fByState(&spD8, &spF0);
+//             Matrix_MultiplyVector3fByState(&spCC, &spE4);
+    
+//             spF0.x += this->actor.world.pos.x;
+//             spF0.y += this->actor.world.pos.y;
+//             spF0.z += this->actor.world.pos.z;
+         
+//             if (Rand_Next() % 4 == 0) {
+//                 phi_s0 = 0x20;
+//             } else {
+//                 phi_s0 = 0x40;
+//             }
+            
+//             if (temp_s3 < 2 ||  Rand_Next() > 0) {
+//                 phi_s0 |= 1;
+//                 phi_s1 = 1;
+//                 func_800B0E48(globalCtx, &spF0, &D_801D15B0, &D_80B6E724, &D_80B6E71C, &D_80B6E720, 
+//                                  (((u32) Rand_Next() >> 0x1B) + 0x46), 
+//                                  (( ( (u32) Rand_Next() ) >> 0x1A) + 0x3C));
+//             } else {
+//                 phi_s1 = 0;
+//             }
+//             EffectSsKakera_Spawn(globalCtx, &spF0, &spE4, &spF0, -0x226, phi_s0, 0x1E, 0, 
+//                                  0,  D_80B6E738[temp_s3], phi_s1, 0, 0x32, -1, 
+//                                  0x1F5,  D_06000128);
+//         }
+//     }
+// }
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Kin2_Bombwall/BgKin2Bombwall_Init.s")
     
