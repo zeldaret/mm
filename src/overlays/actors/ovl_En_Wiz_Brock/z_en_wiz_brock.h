04ab29ba3641e777a04831afeeb1614822860b88
Parker Burnett <burnettparker@gmail.com>
2022-05-03 00:04:50 -0600

    fixing text

--- src/overlays/actors/ovl_En_Wiz_Brock/z_en_wiz_brock.h
+++ src/overlays/actors/ovl_En_Wiz_Brock/z_en_wiz_brock.h
@@ -11,7 +11,7 @@ typedef struct EnWizBrock {
     /* 0x0000 */ DynaPolyActor dyna;
     /* 0x015C */ char unk_15C[0x44];
     /* 0x01A0 */ EnWizBrockActionFunc actionFunc;
-    /* 0x1A4 */ s16 despawnCounter; // Counts up (Max of 0x25)
+    /* 0x1A4 */ s16 despawnCounter; // Counter for despawing blocks (Max of 0x25)
     /* 0x1A6 */ s16 unk1A6;
     /* 0x1A8 */ s16 platformNum; // Numeric identifier for platform
     /* 0x1AA */ s16 platformType; // Determines element type for platform (ice/fire)
