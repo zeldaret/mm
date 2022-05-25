f7bba61a474638903f6ff1c75fa071b971895c13
Isghj8 <isghj8@gmail.com>
2022-05-24 23:00:26 -0700

    DmZl: start

--- src/overlays/actors/ovl_Dm_Zl/z_dm_zl.c
+++ src/overlays/actors/ovl_Dm_Zl/z_dm_zl.c
@@ -32,12 +32,30 @@ const ActorInit Dm_Zl_InitVars = {
 
 #endif
 
+
+
 extern UNK_TYPE D_0600DE08;
 extern UNK_TYPE D_0600E038;
 
+// dont yet know what this data is supposed to be other than 0x18
+extern DmZl_UnkStruct D_80A387F0[];
+
+void func_80A38190(SkelAnime *skelAnime, void *arg1, s32 arg2);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Zl/func_80A38190.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Zl/DmZl_Init.s")
+void DmZl_Init(Actor *thisx, GlobalContext *globalCtx) {
+    s32 pad;
+    DmZl *this = THIS;
+
+    this->unk2B0 = 0;
+    this->unk2BA = 0;
+    this->actor.targetArrowOffset = 1000.0f;
+    ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 24.0f);
+    SkelAnime_InitFlex(globalCtx, &this->skelAnime, (FlexSkeletonHeader *) &D_0600E038, NULL, NULL, NULL, 0);
+    func_80A38190(&this->skelAnime, &D_80A387F0[this->unk2B0], 0);
+    Actor_SetScale(&this->actor, 0.01f);
+    this->actionFunc = func_80A382FC;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Zl/DmZl_Destroy.s")
 
--- src/overlays/actors/ovl_Dm_Zl/z_dm_zl.h
+++ src/overlays/actors/ovl_Dm_Zl/z_dm_zl.h
@@ -7,11 +7,28 @@ struct DmZl;
 
 typedef void (*DmZlActionFunc)(struct DmZl*, GlobalContext*);
 
+typedef struct {
+    /* 0 */ u8 pad[0x18];
+} DmZl_UnkStruct; // size = 0x18
+
+
 typedef struct DmZl {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x11C];
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ char pad188[0xD8];                  /* maybe part of unk144[4]? */
     /* 0x0260 */ DmZlActionFunc actionFunc;
-    /* 0x0264 */ char unk_264[0x70];
+    /* 0x264 */ char pad264[0x4C];                  /* maybe part of actionFunc[0x14]? */
+    /* 0x2B0 */ s16 unk2B0;                         /* inferred */
+    /* 0x2B2 */ u8 unk2B2;                          /* inferred */
+    /* 0x2B3 */ u8 unk2B3;                          /* inferred */
+    /* 0x2B4 */ u8 unk2B4;                          /* inferred */
+    /* 0x2B5 */ u8 unk2B5;                          /* inferred */
+    /* 0x2B6 */ u8 unk2B6;                          /* inferred */
+    /* 0x2B7 */ char pad2B7[1];
+    /* 0x2B8 */ s16 unk2B8;                         /* inferred */
+    /* 0x2BA */ s16 unk2BA;                         /* inferred */
+    /* 0x2BC */ char pad2BC[0x18];                  /* maybe part of unk2BA[0xD]? */
+
 } DmZl; // size = 0x2D4
 
 extern const ActorInit Dm_Zl_InitVars;
