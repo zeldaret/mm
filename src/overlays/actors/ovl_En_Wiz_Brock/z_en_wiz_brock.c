52fc70440f6fae1a2d2edd7fc2a14444082c64b7
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-05-26 03:48:37 +0100

    Format

--- src/overlays/actors/ovl_En_Wiz_Brock/z_en_wiz_brock.c
+++ src/overlays/actors/ovl_En_Wiz_Brock/z_en_wiz_brock.c
@@ -109,7 +109,7 @@ void EnWizBrock_Draw(Actor* thisx, GlobalContext* globalCtx) {
     OPEN_DISPS(globalCtx->state.gfxCtx);
 
     Matrix_Translate(this->dyna.actor.world.pos.x, this->dyna.actor.world.pos.y, this->dyna.actor.world.pos.z,
-                             MTXMODE_NEW);
+                     MTXMODE_NEW);
     Matrix_Scale(this->dyna.actor.scale.x, this->dyna.actor.scale.y, this->dyna.actor.scale.z, MTXMODE_APPLY);
     if (this->dyna.actor.colChkInfo.health != 0) {
         Scene_SetRenderModeXlu(globalCtx, 0, 1);
