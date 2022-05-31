54499c329e854457db36e888b6812a14873fb845
Isghj8 <isghj8@gmail.com>
2022-05-30 17:25:38 -0700

    DmZl: OK

--- src/overlays/actors/ovl_Dm_Zl/z_dm_zl.c
+++ src/overlays/actors/ovl_Dm_Zl/z_dm_zl.c
@@ -59,12 +59,12 @@ void DmZl_Init(Actor *thisx, GlobalContext *globalCtx) {
     s32 pad;
     DmZl *this = THIS;
 
-    this->unk2B0 = 0;
-    this->unk2BA = 0;
+    this->animationIndex = 0;
+    this->unused2BA = 0;
     this->actor.targetArrowOffset = 1000.0f;
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 24.0f);
     SkelAnime_InitFlex(globalCtx, &this->skelAnime, (FlexSkeletonHeader *) &gZl4Skeleton, NULL, NULL, NULL, 0);
-    func_80A38190(&this->skelAnime, &D_80A387F0[this->unk2B0], 0);
+    func_80A38190(&this->skelAnime, &D_80A387F0[this->animationIndex], 0);
     Actor_SetScale(&this->actor, 0.01f);
     this->actionFunc = func_80A382FC;
 }
@@ -73,19 +73,128 @@ void DmZl_Destroy(Actor *thisx, GlobalContext *globalCtx) {
     DmZl *this = (DmZl *) thisx;
 }
 
-//do nothing
+// DmZl_DoNothing
 void func_80A382FC(DmZl *this, GlobalContext *globalCtx) {
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Zl/func_80A3830C.s")
+// weird cutscene func
+void func_80A3830C(DmZl *this, GlobalContext *globalCtx) {
+    s32 actionIndex;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Zl/func_80A38468.s")
+    if (Cutscene_CheckActorAction(globalCtx, 0x66) != 0) {
+        actionIndex = Cutscene_GetActorActionIndex(globalCtx, 0x66);
+        if (globalCtx->csCtx.frames == globalCtx->csCtx.actorActions[actionIndex]->startFrame) {
+            s16 newUnk2B0;
+            newUnk2B0 = 0;
+            switch (globalCtx->csCtx.actorActions[actionIndex]->action) {
+                case 2:
+                    newUnk2B0 = 1;
+                    break;
+                case 3:
+                    newUnk2B0 = 3;
+                    break;
+                case 4:
+                    newUnk2B0 = 5;
+                    break;
+                case 1:
+                default:
+                    break;
+            }
+            if (newUnk2B0 != this->animationIndex) {
+                this->animationIndex = newUnk2B0;
+                func_80A38190(&this->skelAnime, &D_80A387F0[this->animationIndex], 0);
+            }
+        }
+
+        Cutscene_ActorTranslateAndYaw(&this->actor, globalCtx, actionIndex);
+    }
+
+    if ((Animation_OnFrame(&this->skelAnime, this->skelAnime.endFrame) != 0) &&
+           (((actionIndex = this->animationIndex) == 1) || (actionIndex == 3) || (actionIndex == 5))) {
+        this->animationIndex++;
+        func_80A38190(&this->skelAnime, &D_80A387F0[this->animationIndex], 0);
+    }
+}
+
+// eye control?
+void func_80A38468(DmZl *this) {
+    if (this->timer > 0) {
+        this->timer--;
+    } else {
+        this->timer = 0;
+    }
+
+    if (this->timer < 3) {
+        this->unk2B3 = this->timer;
+        this->unk2B2 = this->timer;
+    }
+
+    switch (this->unk2B5) {
+        case 0:
+            if (this->timer == 0) {
+                this->timer = Rand_S16Offset(0x1E, 0x1E);
+            }
+            break;
+        case 1:
+            if (this->timer == 0) {
+                this->unk2B2 = this->unk2B3 = 2;
+            }
+            break;
+        case 2:
+            if (this->timer == 0) {
+                this->unk2B2 = 5;
+                this->unk2B3 = 6;
+            }
+            break;
+        case 3:
+            if (this->timer == 0) {
+                this->unk2B2 = 6;
+                this->unk2B3 = 5;
+            }
+            break;
+        case 4:
+            if (this->timer == 0) {
+                this->unk2B2 = this->unk2B3 = 3;
+            }
+            break;
+        case 5:
+            if (this->timer == 0) {
+                this->unk2B2 = this->unk2B3 = 4;
+            }
+            break;
+        case 6:
+            if (this->timer >= 3) {
+                this->timer = 0;
+            }
+            break;
+    }
+
+    switch (this->unk2B6) {
+        default:
+            this->mouthTextureIndex = 0;
+            break;
+        case 1:
+            this->mouthTextureIndex = 1;
+            break;
+        case 2:
+            this->mouthTextureIndex = 2;
+            break;
+        case 3:
+            this->mouthTextureIndex = 3;
+            break;
+    }
+
+    if (this->animationIndex == 6) {
+        this->unk2B2 = this->unk2B3 = 2;
+    }
+}
+
 
 void DmZl_Update(Actor *thisx, GlobalContext *globalCtx) {
     DmZl *this = THIS;
     func_80A38468(this);
     SkelAnime_Update(&this->skelAnime);
-    func_80A3830C(&this->actor, globalCtx);
+    func_80A3830C(this, globalCtx);
     this->actionFunc(this, globalCtx);
 }
 
@@ -99,7 +208,7 @@ void func_80A38648(GlobalContext* globalCtx, s32 limbIndex, Gfx** dList, Vec3s* 
     DmZl* this = THIS;
 
     if (limbIndex == 0x10) {
-        if ((this->unk2B0 >= 3) && (this->unk2B0 < 7)) {
+        if ((this->animationIndex >= 3) && (this->animationIndex < 7)) {
             OPEN_DISPS(globalCtx->state.gfxCtx);
 
             gSPDisplayList(POLY_OPA_DISP++, &gDl4OcarinaDl);
@@ -119,7 +228,7 @@ void DmZl_Draw(Actor *thisx, GlobalContext *globalCtx) {
 
     gSPSegment(POLY_OPA_DISP++, 0x09, Lib_SegmentedToVirtual(D_80A388A8[this->unk2B2]));
 
-    gSPSegment(POLY_OPA_DISP++, 0x0A, Lib_SegmentedToVirtual(D_80A38898[this->unk2B4]));
+    gSPSegment(POLY_OPA_DISP++, 0x0A, Lib_SegmentedToVirtual(D_80A38898[this->mouthTextureIndex]));
 
     func_8012C28C(globalCtx->state.gfxCtx);
     SkelAnime_DrawFlexOpa(globalCtx, this->skelAnime.skeleton, this->skelAnime.jointTable, this->skelAnime.dListCount, func_80A3862C, func_80A38648, &this->actor);
--- src/overlays/actors/ovl_Dm_Zl/z_dm_zl.h
+++ src/overlays/actors/ovl_Dm_Zl/z_dm_zl.h
@@ -15,19 +15,18 @@ typedef struct {
 typedef struct DmZl {
     /* 0x0000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;
-    /* 0x188 */ char pad188[0xD8];                  /* maybe part of unk144[4]? */
+    /* 0x188 */ char pad188[0xD8]; // unused by DmZl
     /* 0x0260 */ DmZlActionFunc actionFunc;
-    /* 0x264 */ char pad264[0x4C];                  /* maybe part of actionFunc[0x14]? */
-    /* 0x2B0 */ s16 unk2B0;                         /* inferred */
-    /* 0x2B2 */ u8 unk2B2;                          /* inferred */
-    /* 0x2B3 */ u8 unk2B3;                          /* inferred */
-    /* 0x2B4 */ u8 unk2B4;                          /* inferred */
-    /* 0x2B5 */ u8 unk2B5;                          /* inferred */
-    /* 0x2B6 */ u8 unk2B6;                          /* inferred */
-    /* 0x2B7 */ char pad2B7[1];
-    /* 0x2B8 */ s16 unk2B8;                         /* inferred */
-    /* 0x2BA */ s16 unk2BA;                         /* inferred */
-    /* 0x2BC */ char pad2BC[0x18];                  /* maybe part of unk2BA[0xD]? */
+    /* 0x264 */ char pad264[0x4C]; // unused by DmZl
+    /* 0x2B0 */ s16 animationIndex;
+    /* 0x2B2 */ u8 unk2B2; // eye tex id
+    /* 0x2B3 */ u8 unk2B3; // exe tex id
+    /* 0x2B4 */ u8 mouthTextureIndex;
+    /* 0x2B5 */ u8 unk2B5; // used to control eye state, but not set by our actor, outside of actor?
+    /* 0x2B6 */ u8 unk2B6; // used to control mouth state, but not set by our actor, outside of actor?
+    /* 0x2B8 */ s16 timer;
+    /* 0x2BA */ s16 unused2BA; // init to 0
+    /* 0x2BC */ char pad2BC[0x18]; // unused by DmZl
 
 } DmZl; // size = 0x2D4
 
