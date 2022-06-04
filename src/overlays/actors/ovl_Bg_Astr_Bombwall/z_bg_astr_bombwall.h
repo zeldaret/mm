c8744d336fefa18c48310b788f4c6e397acec9a5
SonicDcer <noreply@github.com>
2022-06-03 23:48:24 -0300

    decompiled

--- src/overlays/actors/ovl_Bg_Astr_Bombwall/z_bg_astr_bombwall.h
+++ src/overlays/actors/ovl_Bg_Astr_Bombwall/z_bg_astr_bombwall.h
@@ -6,14 +6,17 @@
 struct BgAstrBombwall;
 
 typedef void (*BgAstrBombwallActionFunc)(struct BgAstrBombwall*, GlobalContext*);
-
 typedef struct BgAstrBombwall {
-    /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x18];
-    /* 0x015C */ BgAstrBombwallActionFunc actionFunc;
-    /* 0x0160 */ char unk_160[0xDC];
-} BgAstrBombwall; // size = 0x23C
-
+    /* 0x000 */ Actor actor;
+    /* 0x144 */ s32 unk144;                         /* inferred */
+    /* 0x148 */ char pad148[0x14];                  /* maybe part of unk144[6]? */
+    /* 0x15C */ BgAstrBombwallActionFunc actionFunc;
+    /* 0x160 */ ColliderTris unk160;                /* inferred */
+    /* 0x180 */ ColliderTrisElement unk180;         /* inferred */
+    /* 0x1DC */ char pad1DC[0x5C];
+    /* 0x238 */ s16 unk238;                         /* inferred */
+    /* 0x23A */ char pad23A[2];
+} BgAstrBombwall;
 extern const ActorInit Bg_Astr_Bombwall_InitVars;
 
 #endif // Z_BG_ASTR_BOMBWALL_H
