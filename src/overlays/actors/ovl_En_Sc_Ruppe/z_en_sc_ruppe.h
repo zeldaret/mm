74a6527b6c3dbcc9241eb1654660b74ecb2a4877
SonicDcer <noreply@github.com>
2022-06-07 02:31:28 -0300

    z_en_sc_ruppe decompiled

--- src/overlays/actors/ovl_En_Sc_Ruppe/z_en_sc_ruppe.h
+++ src/overlays/actors/ovl_En_Sc_Ruppe/z_en_sc_ruppe.h
@@ -9,9 +9,10 @@ typedef void (*EnScRuppeActionFunc)(struct EnScRuppe*, GlobalContext*);
 
 typedef struct EnScRuppe {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x4C];
+    /* 0x0144 */ ColliderCylinder collider;
     /* 0x0190 */ EnScRuppeActionFunc actionFunc;
-    /* 0x0194 */ char unk_194[0x4];
+    /* 0x194 */ s16 rupeeDisplayTime;
+    /* 0x196 */ s16 rupeeIndex;
 } EnScRuppe; // size = 0x198
 
 extern const ActorInit En_Sc_Ruppe_InitVars;
