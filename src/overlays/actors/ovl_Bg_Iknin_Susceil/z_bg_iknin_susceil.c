49723b56b3a3c29de2d6bfe2f2c0080e8ad58031
Anghelo Carvajal <angheloalf95@gmail.com>
2022-06-07 23:07:48 -0400

    Update src/overlays/actors/ovl_Bg_Iknin_Susceil/z_bg_iknin_susceil.c
    Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>

--- src/overlays/actors/ovl_Bg_Iknin_Susceil/z_bg_iknin_susceil.c
+++ src/overlays/actors/ovl_Bg_Iknin_Susceil/z_bg_iknin_susceil.c
@@ -78,7 +78,7 @@ void func_80C0A86C(BgIkninSusceil* this, GlobalContext* globalCtx, s16 verticalM
     if (arg4 == 1) {
         Rumble_Request(10000.0f, 255, 20, 150);
     } else if (arg4 == 2) {
-        Rumble_Request(10000.0f, 180, 20, 100);
+        Rumble_Request(SQ(100.0f), 180, 20, 100);
     } else if (arg4 == 3) {
         Rumble_Request(SQ(100.0f), 120, 20, 10);
     }
