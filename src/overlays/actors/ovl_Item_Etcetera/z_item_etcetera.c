e47d146431fed4fe2d0c4c57c4d9f28e1135728d
SonicDcer <noreply@github.com>
2022-06-10 09:50:26 -0300

    ovl_Item_Etcetera decompiled

--- src/overlays/actors/ovl_Item_Etcetera/z_item_etcetera.c
+++ src/overlays/actors/ovl_Item_Etcetera/z_item_etcetera.c
@@ -14,11 +14,13 @@ void ItemEtcetera_Init(Actor* thisx, GlobalContext* globalCtx);
 void ItemEtcetera_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ItemEtcetera_Update(Actor* thisx, GlobalContext* globalCtx);
 
-void func_80920044(ItemEtcetera* this, GlobalContext* globalCtx);
-
 void ItemEtcetera_SetupAction(ItemEtcetera* this, ItemEtceteraActionFunc actionFunc);
+void func_80920044(ItemEtcetera* this, GlobalContext* globalCtx);
+void func_8092009C(ItemEtcetera* this, GlobalContext* globalCtx);
+void func_809200F8(ItemEtcetera* this, GlobalContext* globalCtx);
+void func_80920164(ItemEtcetera* this, GlobalContext* globalCtx);
+void func_809201BC(ItemEtcetera* this, GlobalContext* globalCtx);
 
-#if 0
 const ActorInit Item_Etcetera_InitVars = {
     ACTOR_ITEM_ETCETERA,
     ACTORCAT_PROP,
@@ -31,22 +33,99 @@ const ActorInit Item_Etcetera_InitVars = {
     (ActorFunc)NULL,
 };
 
-#endif
+static s16 D_80920230[14] = {
+    0x9E, 0x9E, 0x9E, 0x9E, 0x9E, 0x9E, 0x86, 0x121, 0x13F, 0x13F, 0x13F, 0x13F, 0x96, 0x86,
+};
+static s16 D_8092024C[14] = { 0, 0, 0, 0, 0, 0, 1, 0x47, 0x4F, 0x50, 0x51, 0x53, 0x13, 1 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/ItemEtcetera_SetupAction.s")
+static s16 D_80920268[20] = {
+    0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x3C, 0x25,
+};
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/ItemEtcetera_Init.s")
+void ItemEtcetera_SetupAction(ItemEtcetera* this, ItemEtceteraActionFunc actionFunc) {
+    this->actionFunc3 = actionFunc;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/ItemEtcetera_Destroy.s")
+void ItemEtcetera_Init(Actor* thisx, GlobalContext* globalCtx) {
+    ItemEtcetera* this = THIS;
+    s32 pad;
+    s32 params;
+    s32 index;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/func_80920044.s")
+    params = ITEMETCETERA_GET_FF(&this->actor);
+    index = Object_GetIndex(&globalCtx->objectCtx, D_80920230[params]);
+    if (index >= 0) {
+        this->unk_14C = index;
+    }
+    this->unk_148 = D_8092024C[params];
+    this->unk_14A = D_80920268[params];
+    this->actionFunc = func_8092009C;
+    this->actionFunc2 = func_809201BC;
+    Actor_SetScale(&this->actor, 0.25f);
+    ItemEtcetera_SetupAction(this, func_80920044);
+    switch (params) {
+        case 7:
+            Actor_SetScale(&this->actor, 0.5f);
+            this->actor.draw = NULL;
+            this->actor.shape.yOffset = 50.0f;
+            return;
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+            Actor_SetScale(&this->actor, 0.5f);
+            this->actionFunc = func_809200F8;
+            this->actionFunc2 = func_80920164;
+            this->actor.world.pos.y += 15.0f;
+            /* fallthrough */
+        default:
+            return;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/func_8092009C.s")
+void ItemEtcetera_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/func_809200F8.s")
+void func_80920044(ItemEtcetera* this, GlobalContext* globalCtx) {
+    if (Object_IsLoaded(&globalCtx->objectCtx, this->unk_14C)) {
+        this->actor.objBankIndex = (u8)this->unk_14C;
+        this->actor.draw = (void (*)(Actor*, GlobalContext*))this->actionFunc2;
+        this->actionFunc3 = this->actionFunc;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/ItemEtcetera_Update.s")
+void func_8092009C(ItemEtcetera* this, GlobalContext* globalCtx) {
+    if (Actor_HasParent(&this->actor, globalCtx)) {
+        Actor_MarkForDeath(&this->actor);
+        return;
+    }
+    Actor_PickUp(&this->actor, globalCtx, (s32)this->unk_14A, 30.0f, 50.0f);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/func_80920164.s")
+void func_809200F8(ItemEtcetera* this, GlobalContext* globalCtx) {
+    if (Flags_GetTreasure(globalCtx, ITEMETCETERA_GET_TREASURE(&this->actor))) {
+        Actor_MarkForDeath(&this->actor);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Item_Etcetera/func_809201BC.s")
+void ItemEtcetera_Update(Actor* thisx, GlobalContext* globalCtx) {
+    ItemEtcetera* this = THIS;
+
+    this->actionFunc3(this, globalCtx);
+}
+
+void func_80920164(ItemEtcetera* this, GlobalContext* globalCtx) {
+    if (globalCtx->actorCtx.unk4 == 100) {
+        func_800B8050(&this->actor, globalCtx, 0);
+        func_800B8118(&this->actor, globalCtx, 0);
+        GetItem_Draw(globalCtx, this->unk_148);
+    }
+}
+
+void func_809201BC(ItemEtcetera* this, GlobalContext* globalCtx) {
+    func_800B8050(&this->actor, globalCtx, 0);
+    func_800B8118(&this->actor, globalCtx, 0);
+    GetItem_Draw(globalCtx, this->unk_148);
+}
--- src/overlays/actors/ovl_Item_Etcetera/z_item_etcetera.h
+++ src/overlays/actors/ovl_Item_Etcetera/z_item_etcetera.h
@@ -5,12 +5,20 @@
 
 struct ItemEtcetera;
 
+#define ITEMETCETERA_GET_FF(thisx) ((thisx)->params & 0xFF)
+#define ITEMETCETERA_GET_TREASURE(thisx) (((thisx)->params >> 8) & 0x1F)
+
 typedef void (*ItemEtceteraActionFunc)(struct ItemEtcetera*, GlobalContext*);
 
 typedef struct ItemEtcetera {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x10];
-    /* 0x0154 */ ItemEtceteraActionFunc actionFunc;
+    /* 0x0144 */ ItemEtceteraActionFunc actionFunc;
+    /* 0x148 */ s16 unk_148;
+    /* 0x14A */ s16 unk_14A;
+    /* 0x14C */ u8 unk_14C;
+    /* 0x14D */ char pad_14D[3];
+    /* 0x0150 */ ItemEtceteraActionFunc actionFunc2;
+    /* 0x0154 */ ItemEtceteraActionFunc actionFunc3;
 } ItemEtcetera; // size = 0x158
 
 extern const ActorInit Item_Etcetera_InitVars;
