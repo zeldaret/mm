d4683938048b0231825aff01948591daf31f3a0c
SonicDcer <noreply@github.com>
2022-06-18 20:27:20 -0300

    ovl_Eff_Zoraband decompiled

--- src/overlays/actors/ovl_Eff_Zoraband/z_eff_zoraband.c
+++ src/overlays/actors/ovl_Eff_Zoraband/z_eff_zoraband.c
@@ -5,6 +5,7 @@
  */
 
 #include "z_eff_zoraband.h"
+#include "objects/object_zoraband/object_zoraband.h"
 
 #define FLAGS (ACTOR_FLAG_10 | ACTOR_FLAG_20)
 
@@ -13,11 +14,10 @@
 void EffZoraband_Init(Actor* thisx, GlobalContext* globalCtx);
 void EffZoraband_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EffZoraband_Update(Actor* thisx, GlobalContext* globalCtx);
-void EffZoraband_Draw(Actor* thisx, GlobalContext* globalCtx);
+void EffZoraband_Draw(Actor* thisx, GlobalContext* globalCtx2);
 
-void func_80C07790(EffZoraband* this, GlobalContext* globalCtx);
+void EffZoraband_Mikau_FadeOut(EffZoraband* this, GlobalContext* globalCtx);
 
-#if 0
 const ActorInit Eff_Zoraband_InitVars = {
     ACTOR_EFF_ZORABAND,
     ACTORCAT_ITEMACTION,
@@ -30,16 +30,71 @@ const ActorInit Eff_Zoraband_InitVars = {
     (ActorFunc)EffZoraband_Draw,
 };
 
-#endif
+void EffZoraband_Init(Actor* thisx, GlobalContext* globalCtx) {
+    EffZoraband* this = THIS;
 
-extern UNK_TYPE D_06000F38;
+    Actor_SetScale(&this->actor, 1.0f);
+    this->actionFunc = EffZoraband_Mikau_FadeOut;
+    this->transparency = 0;
+    this->actor.shape.rot.z = 0;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Zoraband/EffZoraband_Init.s")
+void EffZoraband_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Zoraband/EffZoraband_Destroy.s")
+void EffZoraband_Mikau_FadeOut(EffZoraband* this, GlobalContext* globalCtx) {
+    if (Cutscene_CheckActorAction(globalCtx, 0x20F)) {
+        if ((EFFZORABAND_GET_F(&this->actor) + 2) ==
+            globalCtx->csCtx.actorActions[Cutscene_GetActorActionIndex(globalCtx, 0x20F)]->action) {
+            this->transpInit |= 2;
+        }
+        if (globalCtx->csCtx.actorActions[Cutscene_GetActorActionIndex(globalCtx, 0x20F)]->action == 7) {
+            this->actor.draw = NULL;
+        } else {
+            this->actor.draw = EffZoraband_Draw;
+        }
+    }
+    if ((this->actor.home.rot.z != 0) && (this->actor.draw != NULL)) {
+        func_800B9010(&this->actor, NA_SE_EV_UFO_LIGHT_BEAM - SFX_FLAG);
+    }
+    if (this->transpInit & 2) {
+        if (this->transparency < 240) {
+            this->transparency += 25;
+        } else {
+            this->transparency = 255;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Zoraband/func_80C07790.s")
+void EffZoraband_Update(Actor* thisx, GlobalContext* globalCtx) {
+    EffZoraband* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Zoraband/EffZoraband_Update.s")
+    this->actionFunc(this, globalCtx);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Zoraband/EffZoraband_Draw.s")
+void EffZoraband_Draw(Actor* thisx, GlobalContext* globalCtx2) {
+    GlobalContext* globalCtx = globalCtx2;
+    EffZoraband* this = THIS;
+
+    if (this->transparency) {
+        OPEN_DISPS(globalCtx->state.gfxCtx);
+
+        func_8012C2DC(globalCtx->state.gfxCtx);
+        Matrix_RotateYS((Camera_GetCamDirYaw(globalCtx->cameraPtrs[globalCtx->activeCamera]) + 0x8000), MTXMODE_APPLY);
+        AnimatedMat_DrawXlu(globalCtx, Lib_SegmentedToVirtual(object_zoraband_Matanimheader_000F38));
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        if (this->actor.home.rot.z) {
+            gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, 255, 255, 100, this->transparency);
+            gDPSetEnvColor(POLY_XLU_DISP++, 255, 200, 0, 255);
+        } else {
+            gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, 0, 100, 255, this->transparency);
+            gDPSetEnvColor(POLY_XLU_DISP++, 0, 0, 100, 255);
+        }
+        gSPDisplayList(POLY_XLU_DISP++, object_zoraband_DL_000180);
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0xFF, 200, 255, 255, this->transparency);
+        gDPSetEnvColor(POLY_XLU_DISP++, 0, 100, 255, 255);
+        gSPDisplayList(POLY_XLU_DISP++, object_zoraband_DL_0002A8);
+
+        CLOSE_DISPS(globalCtx->state.gfxCtx);
+    }
+}
