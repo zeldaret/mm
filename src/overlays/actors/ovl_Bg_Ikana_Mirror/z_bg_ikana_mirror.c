7367acb9c73488d71fa805f4ac1fe8ac697e766f
TrulyNaN <trulynan@gmail.com>
2022-06-18 15:26:14 -0400

    Matched func_80B7F730 in ovl_Bg_Ikana_Mirror. Progress on its _Init done too.

--- src/overlays/actors/ovl_Bg_Ikana_Mirror/z_bg_ikana_mirror.c
+++ src/overlays/actors/ovl_Bg_Ikana_Mirror/z_bg_ikana_mirror.c
@@ -14,7 +14,7 @@ void BgIkanaMirror_Init(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaMirror_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaMirror_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgIkanaMirror_Draw(Actor* thisx, GlobalContext* globalCtx);
-//first commit
+// first commit
 #if 0
 const ActorInit Bg_Ikana_Mirror_InitVars = {
     ACTOR_BG_IKANA_MIRROR,
@@ -98,18 +98,18 @@ static InitChainEntry D_80B8021C[] = {
 
 #endif
 
-void func_80B7FA9C(BgIkanaMirror *arg0, GlobalContext *arg1); /* static */
-void func_80B7FBA4(BgIkanaMirror *arg0, GlobalContext *arg1);
-
+void func_80B7F730(BgIkanaMirror *);
+void func_80B7FA9C(BgIkanaMirror* arg0, GlobalContext* arg1); /* static */
+void func_80B7FBA4(BgIkanaMirror* arg0, GlobalContext* arg1);
 
 extern ColliderTrisElementInit D_80B7FF50[9];
 extern ColliderTrisInit D_80B8016C;
-extern ColliderQuadInit D_80B8017C[];
-extern Vec3f D_80B8019C;
-extern Vec3f D_80B801A8;
-extern Vec3f D_80B801B4;
-extern Vec3f D_80B801C0;
-extern ColliderQuadInit D_80B801FC;
+extern ColliderQuadInit D_80B8017C[2];
+// extern Vec3f D_80B8019C[2];
+// extern Vec3f D_80B801A8[2];
+// extern Vec3f D_80B801B4[2];
+// extern Vec3f D_80B801C0[2];
+// extern ColliderQuadInit D_80B801FC;
 extern InitChainEntry D_80B8021C[];
 
 extern Gfx* D_060014B0;
@@ -117,33 +117,145 @@ extern Gfx* D_06001880;
 extern Gfx* D_06001E18;
 extern CollisionHeader D_06002358;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/func_80B7F730.s")
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/func_80B7F730.s")
+
+// typedef struct {
+//                Vec3f quad[4];
+//                Vec3s dcMid;
+//                Vec3s baMid;
+//                f32 acDist;
+// } ColliderQuadDim;
+// typedef struct {
+//                Vec3f quad[4];
+// } ColliderQuadDimInit;
+// typedef struct {
+//                Collider base;
+//                ColliderInfo info;
+//                ColliderQuadDim dim;
+// } ColliderQuad;
+// typedef struct {
+//                ColliderInit base;
+//                ColliderInfoInit info;
+//                ColliderQuadDimInit dim;
+// } ColliderQuadInit;
+
+void func_80B7F730(BgIkanaMirror *arg0){
+  ColliderQuad *new_var;
+  s32 i;
+  Vec3f sp84;
+  Vec3f sp78;
+  Vec3f sp6C;
+  Vec3f sp60;
+    ColliderQuadDimInit* dim;
+  Matrix_Push();
+  Matrix_SetTranslateRotateYXZ(arg0->actor.world.pos.x, arg0->actor.world.pos.y, arg0->actor.world.pos.z, &arg0->actor.shape.rot);
+  for (i = 0; i < 2; i++)
+  {
+      dim = &D_80B8017C[i].dim;
+    Matrix_MultVec3f(&dim->quad[0], &sp84);
+    Matrix_MultVec3f(&dim->quad[1], &sp78);
+    Matrix_MultVec3f(&dim->quad[2], &sp6C);
+    Matrix_MultVec3f(&dim->quad[3], &sp60);
+    new_var = &arg0->unk4B8[i];
+    Collider_SetQuadVertices(new_var, &sp84, &sp78, &sp6C, &sp60);
+  }
+
+  Matrix_Pop();
+}
+
+// void func_80B7F730(BgIkanaMirror *arg0)
+// {
+//   // Vec3f sp78;
+//   // Vec3f sp6C;
+//   ColliderQuad *new_var;
+//   // Vec3f sp84;
+//   s32 i;
+//   Vec3f sp60[4];
+//   Matrix_Push();
+//   Matrix_SetTranslateRotateYXZ(arg0->actor.world.pos.x, arg0->actor.world.pos.y, arg0->actor.world.pos.z, &arg0->actor.shape.rot);
+//   for (i = 0; i < 2; i++)
+//   {
+//     Matrix_MultVec3f(&D_80B8019C[i].dim.quad[0], &sp60[3]);
+//     new_var = &arg0->unk4B8[i];
+//     Matrix_MultVec3f(&D_80B801C0[i].dim.quad[1], &sp60[2]);
+//     Matrix_MultVec3f(&D_80B801B4[i].dim.quad[2], &sp60[1]);
+//     Matrix_MultVec3f(&D_80B8019C[i].dim.quad[3], &sp60[0]);
+//     Collider_SetQuadVertices(new_var, &sp60[3], &sp60[2], &sp60[1], &sp60[0]);
+//   }
+
+//   Matrix_Pop();
+// }
+
+
+// void func_80B7F730(BgIkanaMirror *arg0)
+// {
+//   Vec3f sp84;
+//   Vec3f sp78;
+//   Vec3f sp6C;
+//   Vec3f *new_var4;
+//   Vec3f sp60;
+//   Vec3f *phi_s1;
+//   Vec3f *phi_s0;
+//   Vec3f *phi_s4;
+//   Vec3f *phi_s3;
+//   Vec3f *new_var3;
+//   ColliderQuad *phi_s2;
+  
+//   Matrix_Push();
+//   Matrix_SetTranslateRotateYXZ(arg0->actor.world.pos.x, arg0->actor.world.pos.y, arg0->actor.world.pos.z, &arg0->actor.shape.rot);
+ 
+//  phi_s1 = D_80B8019C; 
+//  phi_s2 = arg0->unk4B8;
+//   new_var4 = (Vec3f *) ((&D_80B801A8) + 0xA0);
+//  phi_s3 = D_80B801C0; 
+//  phi_s4 = D_80B801B4; 
+//  phi_s0 = D_80B801A8;
+//   do {
+//     // new_var2 = phi_s2;
+//     Matrix_MultVec3f(phi_s1, &sp84);
+//     Matrix_MultVec3f(phi_s0, &sp78);
+//     Matrix_MultVec3f(phi_s4, &sp6C);
+//     Matrix_MultVec3f(phi_s3, &sp60);
+//     // new_var = &sp6C;
+//     Collider_SetQuadVertices(phi_s2, &sp84, &sp78, &sp6C, &sp60);
+//     phi_s1 = (Vec3f*) (((s32) phi_s1)+0x50);
+//     phi_s0 = (Vec3f*) (((s32) phi_s0)+0x50);
+//     phi_s4 = (Vec3f*) (((s32) phi_s4)+0x50);
+//     phi_s3 = (Vec3f*) (((s32) phi_s3)+0x50);
+//     phi_s2 = (ColliderQuad*) (((s32) phi_s2)+0x80);
+//   }
+//   while ((new_var3 = new_var4) != phi_s0);
+//   Matrix_Pop();
+// }
+
+
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/BgIkanaMirror_Init.s")
 
-void func_80B7F730(BgIkanaMirror *);                   /* extern */
-void func_80B7FA84(BgIkanaMirror *arg0);
-void func_80B7FB84(BgIkanaMirror *arg0);
-extern UNK_TYPE D_06001678; //AnimatedMaterial
-extern UNK_TYPE D_06001AD8; //AnimatedMaterial
+void func_80B7F730(BgIkanaMirror*); /* extern */
+void func_80B7FA84(BgIkanaMirror* arg0);
+void func_80B7FB84(BgIkanaMirror* arg0);
+extern UNK_TYPE D_06001678; // AnimatedMaterial
+extern UNK_TYPE D_06001AD8; // AnimatedMaterial
 
-//ColliderTrisInit D_80B8016C = { { 0xA, 0, 0x29, 0, 0, 2 }, 9, D_80B7FF50 };
+// ColliderTrisInit D_80B8016C = { { 0xA, 0, 0x29, 0, 0, 2 }, 9, D_80B7FF50 };
 
-void BgIkanaMirror_Init(Actor *thisx, GlobalContext *globalCtx2) {
+void BgIkanaMirror_Init(Actor* thisx, GlobalContext* globalCtx2) {
     GlobalContext* globalCtx = globalCtx2;
-    BgIkanaMirror *this =((BgIkanaMirror*)thisx);
-    Vec3f *phi_s1;
+    BgIkanaMirror* this = ((BgIkanaMirror*)thisx);
+    Vec3f* phi_s1;
     Vec3f sp60[3];
     ColliderTrisElementInit* temp;
     s32 i;
     s32 j;
- 
+
     Actor_ProcessInitChain(&this->actor, D_80B8021C);
-    DynaPolyActor_Init((DynaPolyActor *) this, 0);
-    DynaPolyActor_LoadMesh(globalCtx, (DynaPolyActor *) this, &D_06002358);
+    DynaPolyActor_Init((DynaPolyActor*)this, 0);
+    DynaPolyActor_LoadMesh(globalCtx, (DynaPolyActor*)this, &D_06002358);
     Collider_InitTris(globalCtx, &this->unk15C);
     Collider_SetTris(globalCtx, &this->unk15C, &this->actor, &D_80B8016C, &this->unk17C);
-    Matrix_SetTranslateRotateYXZ(this->actor.world.pos.x, this->actor.world.pos.y, this->actor.world.pos.z, &this->actor.shape.rot);
+    Matrix_SetTranslateRotateYXZ(this->actor.world.pos.x, this->actor.world.pos.y, this->actor.world.pos.z,
+                                 &this->actor.shape.rot);
 
     for (i = 0; i < 9; i++) {
         temp = &D_80B8016C.elements[i];
@@ -153,12 +265,12 @@ void BgIkanaMirror_Init(Actor *thisx, GlobalContext *globalCtx2) {
             phi_s1++;
         }
         Collider_SetTrisVertices(&this->unk15C, i, &sp60[0], &sp60[1], &sp60[2]);
-    } 
-    
+    }
+
     for (i = 0; i < 2; i++) {
         Collider_InitQuad(globalCtx, &this->unk4B8[i]);
         Collider_SetQuad(globalCtx, &this->unk4B8[i], &this->actor, &D_80B8017C[i]);
-    } 
+    }
 
     func_80B7F730(this);
     this->unk5BC = Lib_SegmentedToVirtual(&D_06001678);
@@ -172,7 +284,7 @@ void BgIkanaMirror_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     BgIkanaMirror* this = THIS;
     ColliderQuad* var_s1;
     s32 var_s0;
-    
+
     DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->unk144);
     Collider_DestroyTris(globalCtx, &this->unk15C);
     for (var_s0 = 0; var_s0 < 2; var_s0++) {
@@ -182,7 +294,7 @@ void BgIkanaMirror_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/func_80B7FA84.s")
 
-void func_80B7FA84(BgIkanaMirror *arg0) {
+void func_80B7FA84(BgIkanaMirror* arg0) {
     arg0->unk5C8 = 0;
     arg0->actionFunc = func_80B7FA9C;
 }
@@ -191,7 +303,7 @@ void func_80B7FA84(BgIkanaMirror *arg0) {
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/func_80B7FB84.s")
 
-void func_80B7FB84(BgIkanaMirror *arg0) {
+void func_80B7FB84(BgIkanaMirror* arg0) {
     arg0->actor.flags |= 0x20;
     arg0->actionFunc = func_80B7FBA4;
 }
@@ -200,95 +312,107 @@ void func_80B7FB84(BgIkanaMirror *arg0) {
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/BgIkanaMirror_Update.s")
 
-void BgIkanaMirror_Update(Actor *thisx, GlobalContext *globalCtx) {
-    BgIkanaMirror *this = (BgIkanaMirror *) thisx;
+void BgIkanaMirror_Update(Actor* thisx, GlobalContext* globalCtx) {
+    BgIkanaMirror* this = (BgIkanaMirror*)thisx;
     this->actionFunc(this, globalCtx);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/BgIkanaMirror_Draw.s")
-void BgIkanaMirror_Draw(Actor *thisx, GlobalContext *globalCtx)
-{
-  Gfx *sp4C;
-  Gfx *sp40;
-  f32 sp38;
-  Gfx *sp24;
-  Gfx *temp_v0;
-  Gfx *temp_v0_10;
-  Gfx *temp_v0_2;
-  Gfx *temp_v0_3;
-  Gfx *temp_v0_4;
-  Gfx *temp_v0_5;
-  Gfx *temp_v0_7;
-  Gfx *temp_v0_8;
-  Gfx *temp_v0_9;
-  s32 new_var;
-  GraphicsContext *temp_a0;
-  GraphicsContext *temp_s0;
-  f32 temp_ft1;
-  f32 temp_fv0;
-  s8 temp_v0_6;
-  f32 phi_ft1;
-  BgIkanaMirror *this = (BgIkanaMirror *) thisx;
-  new_var = this->unk5C6;
-  temp_a0 = globalCtx->state.gfxCtx;
-  temp_s0 = temp_a0;
-  func_8012C28C(temp_a0);
-  func_8012C2DC(globalCtx->state.gfxCtx);
-  temp_v0 = temp_s0->polyOpa.p;
-  temp_s0->polyOpa.p = temp_v0 + 8;
-  temp_v0->words.w0 = 0xDA380003;
-  sp4C = temp_v0;
-  sp4C->words.w1 = Matrix_NewMtx(globalCtx->state.gfxCtx);
-  temp_v0_2 = temp_s0->polyOpa.p;
-  temp_s0->polyOpa.p = temp_v0_2 + 8;
-  temp_v0_2->words.w1 = (u32) (&D_06001E18);
-  temp_v0_2->words.w0 = 0xDE000000;
-  if (((s32) new_var) > 0)
-  {
-    AnimatedMat_Draw(globalCtx, this->unk5BC);
-    temp_v0_3 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_3 + 8;
-    temp_v0_3->words.w0 = 0xFA000080;
-    temp_v0_3->words.w1 = new_var | (~0xFF);
-    temp_v0_4 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_4 + 8;
-    temp_v0_4->words.w0 = 0xDA380003;
-    sp40 = temp_v0_4;
-    sp40->words.w1 = Matrix_NewMtx(globalCtx->state.gfxCtx);
-    temp_v0_5 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_5 + 8;
-    temp_v0_5->words.w1 = (u32) (&D_060014B0);
-    temp_v0_5->words.w0 = 0xDE000000;
-  }
-  temp_v0_6 = this->unk5C7;
-  if (((s32) temp_v0_6) > 0)
-  {
-    temp_ft1 = (f32) temp_v0_6;
-    phi_ft1 = temp_ft1;
-    if ((((s32) temp_v0_6) < 0) != 0)
-    {
-      phi_ft1 = temp_ft1 + 4294967296.0f;
-    }
-    temp_fv0 = phi_ft1 * 0.003921569f;
-    sp38 = temp_fv0;
-    AnimatedMat_Draw(globalCtx, this->unk5C0);
-    temp_v0_7 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_7 + 8;
-    phi_ft1 = temp_fv0 * 123.0f;
-    temp_v0_7->words.w0 = 0xFA000080;
-    temp_v0_7->words.w1 = (((s32) phi_ft1) & 0xFF) | (~0xFF);
-    temp_v0_8 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_8 + 8;
-    temp_v0_8->words.w0 = 0xFB000000;
-    temp_v0_8->words.w1 = (((s32) (temp_fv0 * 185.0f)) & 0xFF) | 0xD7D7FF00;
-    temp_v0_9 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_9 + 8;
-    temp_v0_9->words.w0 = 0xDA380003;
-    sp24 = temp_v0_9;
-    sp24->words.w1 = Matrix_NewMtx(globalCtx->state.gfxCtx);
-    temp_v0_10 = temp_s0->polyXlu.p;
-    temp_s0->polyXlu.p = temp_v0_10 + 8;
-    temp_v0_10->words.w1 = (u32) (&D_06001880);
-    temp_v0_10->words.w0 = 0xDE000000;
-  }
-}
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Ikana_Mirror/BgIkanaMirror_Draw.s")
+
+// void BgIkanaMirror_Draw(Actor* thisx, GlobalContext* globalCtx) {
+//     GraphicsContext* new_var8;
+//     AnimatedMaterial* new_var6;
+//     s32 new_var;
+//     f32 sp38;
+//     AnimatedMaterial* new_var3;
+//     Gfx* temp_v0_10;
+//     Gfx* temp_v0;
+//     Gfx* temp_v0_2;
+//     Gfx* temp_v0_3;
+//     f32 new_var7;
+//     Gfx* temp_v0_4;
+//     GlobalContext* new_var5;
+//     Gfx* temp_v0_5;
+//     Gfx* temp_v0_7;
+//     Gfx* temp_v0_8;
+//     Gfx* temp_v0_9;
+//     GraphicsContext* new_var2;
+//     GraphicsContext* temp_a0;
+//     GraphicsContext* temp_s0;
+//     Gfx** new_var11;
+//     f32 temp_ft1;
+//     s8 temp_v0_6;
+//     f32 phi_ft1;
+//     BgIkanaMirror* this = (BgIkanaMirror*)thisx;
+//     new_var = this->unk5C6;
+//     temp_a0 = ((0, globalCtx))->state.gfxCtx;
+//     temp_s0 = temp_a0;
+//     func_8012C28C(temp_s0);
+//     func_8012C2DC(globalCtx->state.gfxCtx);
+//     temp_v0 = temp_s0->polyOpa.p;
+//     new_var8 = temp_s0;
+//     temp_s0->polyOpa.p = temp_s0->polyOpa.p + 8;
+//     temp_v0->words.w0 = 0xDA380003;
+//     if (1) {
+//         new_var6 = this->unk5C0;
+//         temp_v0->words.w1 = Matrix_NewMtx(globalCtx->state.gfxCtx);
+//     }
+//     temp_v0_2 = temp_s0->polyOpa.p;
+//     temp_s0->polyOpa.p = 8 + temp_v0_2;
+//     temp_v0_2->words.w1 = (u32)(&D_06001E18);
+//     temp_v0_2->words.w0 = 0xDE000000;
+//     if (((s32)new_var) > 0) {
+//         new_var3 = this->unk5BC;
+//         AnimatedMat_Draw(globalCtx, new_var3);
+//         temp_v0_3 = new_var8->polyXlu.p;
+//         new_var8->polyXlu.p = temp_v0_3 + 8;
+//         temp_v0_3->words.w0 = 0xFA000080;
+//         new_var5 = globalCtx;
+//         temp_v0_3->words.w1 = new_var | (~0xFF);
+//         temp_v0_4 = new_var8->polyXlu.p;
+//         new_var8->polyXlu.p = temp_v0_4 + 8;
+//         temp_v0_4->words.w0 = 0xDA380003;
+//         temp_v0_4->words.w1 = Matrix_NewMtx(new_var5->state.gfxCtx);
+//     dummy_label_941816:;
+
+//         if (1) {
+//             temp_v0_5 = new_var8->polyXlu.p;
+//             new_var8->polyXlu.p = temp_v0_5 + 8;
+//             temp_v0_5->words.w1 = (u32)(&D_060014B0);
+//         }
+//         temp_v0_5->words.w0 = 0xDE000000;
+//     }
+//     temp_v0_6 = this->unk5C7;
+//     if (((s32)temp_v0_6) > 0) {
+//         temp_ft1 = (f32)temp_v0_6;
+//         phi_ft1 = (f32)temp_v0_6;
+//         if (((s32)temp_v0_6) < 0) {
+//             phi_ft1 = 4294967296.0f + temp_ft1;
+//         }
+//         new_var7 = phi_ft1 * 0.003921569f;
+//         AnimatedMat_Draw(new_var5, new_var6);
+//         temp_v0_7 = new_var8->polyXlu.p;
+//         temp_s0->polyXlu.p = temp_v0_7;
+//         temp_s0->polyXlu.p = temp_s0->polyXlu.p + 8;
+//         new_var2 = temp_s0;
+//         phi_ft1 = new_var7 * 123.0f;
+//         temp_v0_7->words.w0 = 0xFA000080;
+//         temp_v0_7->words.w1 = (((s32)phi_ft1) & 0xFF) | (~0xFF);
+//         if ((temp_v0_9 && temp_v0_9) && temp_v0_9) {}
+//         temp_v0_8 = temp_s0->polyXlu.p;
+//         new_var2->polyXlu.p = temp_v0_8 + 8;
+//         temp_v0_8->words.w0 = 0xFB000000;
+//         temp_v0_8->words.w1 = (((s32)(new_var7 * 185.0f)) & 0xFF) | 0xD7D7FF00;
+//         temp_v0_9 = new_var2->polyXlu.p;
+//         new_var2->polyXlu.p = temp_v0_9 + 8;
+//         temp_v0_9->words.w0 = 0xDA380003;
+//         new_var11 = &D_06001880;
+//         temp_v0_9->words.w1 = Matrix_NewMtx(globalCtx->state.gfxCtx);
+//         if (0, (&new_var5->state) && (&new_var5->state)) {}
+//         temp_v0_10 = new_var2->polyXlu.p;
+//         new_var2->polyXlu.p = temp_s0->polyXlu.p;
+//         new_var2->polyXlu.p = new_var2->polyXlu.p + 8;
+//         temp_v0_10->words.w1 = (u32)new_var11;
+//         temp_v0_10->words.w0 = 0xDE000000;
+//     }
+// }
