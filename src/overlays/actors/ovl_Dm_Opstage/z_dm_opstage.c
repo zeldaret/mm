7631411d87d83008a09d791ca1c43aba62369021
Isghj8 <isghj8@gmail.com>
2022-06-21 21:34:59 -0700

    object_keikoku_demo: Doc'd

--- src/overlays/actors/ovl_Dm_Opstage/z_dm_opstage.c
+++ src/overlays/actors/ovl_Dm_Opstage/z_dm_opstage.c
@@ -1,7 +1,7 @@
 /*
  * File: z_dm_opstage.c
  * Overlay: ovl_Dm_Opstage
- * Description: Lost Woods cutscene objects
+ * Description: Lost Woods Cutscene Trees and Floor
  */
 
 #include "z_dm_opstage.h"
@@ -44,12 +44,14 @@ void DmOpstage_Init(Actor* thisx, GlobalContext* globalCtx) {
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DmOpstage_SetupAction(this, DmOpstage_FollowCutsceneScript);
     Actor_SetScale(&this->dyna.actor, 0.1f);
-    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DM_OPSTAGE_TYPE_FLOOR) {
+
+    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DMOPSTAGE_TYPE_FLOOR) {
         DynaPolyActor_Init(&this->dyna, 0);
-        DynaPolyActor_LoadMesh(globalCtx, &this->dyna, &object_keikoku_demo_Colheader_001C98);
+        DynaPolyActor_LoadMesh(globalCtx, &this->dyna, &gKeikokuDemoFloorColliderHeader);
     }
-    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) > DM_OPSTAGE_TYPE_FLOOR) {
-        this->pos.x = this->dyna.actor.world.pos.x;
+
+    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) > DMOPSTAGE_TYPE_FLOOR) {
+        this->pos.x = this->dyna.actor.world.pos.x; // this offset lets us draw at center, but official pos is zero
         this->pos.y = this->dyna.actor.world.pos.y;
         this->pos.z = this->dyna.actor.world.pos.z;
         this->dyna.actor.world.pos.x = 0.0f;
@@ -61,7 +63,7 @@ void DmOpstage_Init(Actor* thisx, GlobalContext* globalCtx) {
 void DmOpstage_Destroy(Actor* thisx, GlobalContext* globalCtx) {
     DmOpstage* this = THIS;
 
-    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DM_OPSTAGE_TYPE_FLOOR) {
+    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DMOPSTAGE_TYPE_FLOOR) {
         DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
     }
 }
@@ -69,7 +71,7 @@ void DmOpstage_Destroy(Actor* thisx, GlobalContext* globalCtx) {
 void DmOpstage_FollowCutsceneScript(DmOpstage* this, GlobalContext* globalCtx) {
     s32 actionIndex;
 
-    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DM_OPSTAGE_TYPE_FLOOR) {
+    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) == DMOPSTAGE_TYPE_FLOOR) {
         if (Cutscene_CheckActorAction(globalCtx, 0x73)) {
             actionIndex = Cutscene_GetActorActionIndex(globalCtx, 0x73);
             if (globalCtx->csCtx.actorActions[actionIndex]->action == 2) {
@@ -81,10 +83,10 @@ void DmOpstage_FollowCutsceneScript(DmOpstage* this, GlobalContext* globalCtx) {
             }
             Cutscene_ActorTranslateAndYaw(&this->dyna.actor, globalCtx, actionIndex);
         }
-    } else if (Cutscene_CheckActorAction(globalCtx, DMOPSTAGE_GET_08(&this->dyna.actor) + 0x74)) {
+    } else if (Cutscene_CheckActorAction(globalCtx, DMOPSTAGE_GET_ACTORACTION(&this->dyna.actor) + 0x74)) {
         Cutscene_ActorTranslateAndYaw(
             &this->dyna.actor, globalCtx,
-            Cutscene_GetActorActionIndex(globalCtx, DMOPSTAGE_GET_08(&this->dyna.actor) + 0x74));
+            Cutscene_GetActorActionIndex(globalCtx, DMOPSTAGE_GET_ACTORACTION(&this->dyna.actor) + 0x74));
     }
 }
 
@@ -94,6 +96,8 @@ void DmOpstage_Update(Actor* thisx, GlobalContext* globalCtx) {
     this->actionFunc(this, globalCtx);
     if ((globalCtx->sceneNum == SCENE_SPOT00) && (gSaveContext.sceneSetupIndex == 0) &&
         (globalCtx->csCtx.frames == 480)) {
+        // This actor is responsible for playing the fairy sound during the exposition in the intro,
+        // During the transition to Lost Woods, before Ocarina gets stolen.
         func_8019F128(NA_SE_EV_NAVY_FLY_REBIRTH);
     }
 }
@@ -101,31 +105,33 @@ void DmOpstage_Update(Actor* thisx, GlobalContext* globalCtx) {
 void DmOpstage_Draw(Actor* thisx, GlobalContext* globalCtx) {
     DmOpstage* this = THIS;
 
-    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) > DM_OPSTAGE_TYPE_FLOOR) {
+    if (DMOPSTAGE_GET_TYPE(&this->dyna.actor) > DMOPSTAGE_TYPE_FLOOR) {
+        // offset draw from scene center (0,0,0) to our location
         Matrix_Translate(this->dyna.actor.world.pos.x + this->pos.x, this->dyna.actor.world.pos.y + this->pos.y,
                          this->dyna.actor.world.pos.z + this->pos.z, MTXMODE_NEW);
         Matrix_RotateYS(this->dyna.actor.world.rot.y, MTXMODE_APPLY);
         Matrix_Scale(0.1f, 0.1f, 0.1f, MTXMODE_APPLY);
     }
+
     switch (DMOPSTAGE_GET_TYPE(&this->dyna.actor)) {
-        case DM_OPSTAGE_TYPE_FLOOR:
-            Gfx_DrawDListOpa(globalCtx, object_keikoku_demo_DL_000978);
-            Gfx_DrawDListXlu(globalCtx, object_keikoku_demo_DL_000970);
+        case DMOPSTAGE_TYPE_FLOOR:
+            Gfx_DrawDListOpa(globalCtx, gKeikokuDemoFloorDL);
+            Gfx_DrawDListXlu(globalCtx, gKeikokuDemoFloorEmptyDL);
             break;
 
-        case DM_OPSTAGE_TYPE_TREE1:
-            Gfx_DrawDListOpa(globalCtx, object_keikoku_demo_DL_002878);
-            Gfx_DrawDListXlu(globalCtx, object_keikoku_demo_DL_002870);
+        case DMOPSTAGE_TYPE_ROOT_TREE:
+            Gfx_DrawDListOpa(globalCtx, gKeikokuDemoTallTreeWithRootBaseDL);
+            Gfx_DrawDListXlu(globalCtx, gKeikokuDemoTallTreeWithRootBaseEmptyDL);
             break;
 
-        case DM_OPSTAGE_TYPE_TREE2:
-            Gfx_DrawDListOpa(globalCtx, object_keikoku_demo_DL_003068);
-            Gfx_DrawDListXlu(globalCtx, object_keikoku_demo_DL_003060);
+        case DMOPSTAGE_TYPE_CUT_TREE:
+            Gfx_DrawDListOpa(globalCtx, gKeikokuDemoTallTreeCutDL);
+            Gfx_DrawDListXlu(globalCtx, gKeikokuDemoTallTreeCutEmptyDL);
             break;
 
-        case DM_OPSTAGE_TYPE_TREE3:
-            Gfx_DrawDListOpa(globalCtx, object_keikoku_demo_DL_003728);
-            Gfx_DrawDListXlu(globalCtx, object_keikoku_demo_DL_003720);
+        case DMOPSTAGE_TYPE_STRAIGHT_TREE:
+            Gfx_DrawDListOpa(globalCtx, gKeikokuDemoTallTreeStraightDL);
+            Gfx_DrawDListXlu(globalCtx, gKeikokuDemoTallTreeStraightEmptyDL);
             break;
     }
 }
