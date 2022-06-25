876761d1f8f124b7a6533bba3b6ee04366d9d650
andzura <andzura@andzura.fr>
2022-06-25 21:16:09 +0200

    func_8014C70C matched

--- src/code/z_message.c
+++ src/code/z_message.c
@@ -777,8 +777,6 @@ void func_8014AAD0(GlobalContext* globalCtx) {
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_message/func_8014ADBC.s")
 
-void func_8014C70C(GlobalContext*, u16, s16);
-#ifdef NON_MATCHING
 void func_80178E3C(u8*, u32, UNK_PTR, u32); /* extern */
 extern s16 D_801CFF70[];
 extern s16 D_801CFF7C[];
@@ -786,11 +784,13 @@ extern s16 D_801CFF88[];
 
 void func_8014C70C(GlobalContext* globalCtx, u16 arg1, s16 arg2) {
     MessageContext* msgCtx = &globalCtx->msgCtx;
+    u16 *new_var2 = &arg1;
+    
     if (arg1 == 0x83) {
         msgCtx->unk12010 = (msgCtx->unk11FF8 - D_801CFF88[gSaveContext.options.language]);
         msgCtx->unk12012 = (arg2 + 0xA);
         msgCtx->unk12014 = 0x10;
-    } else if ((arg1 >= 0x84) && (arg1 < 0x8B)) {
+    } else  if ((arg1 >= 0x84) && (arg1 < 0x8B)) {
         msgCtx->unk12010 = (msgCtx->unk11FF8 - D_801CFF88[gSaveContext.options.language]);
         msgCtx->unk12012 = (arg2 + 0xA);
         msgCtx->unk12014 = 0x10;
@@ -831,15 +831,14 @@ void func_8014C70C(GlobalContext* globalCtx, u16 arg1, s16 arg2) {
         func_80178E3C(SEGMENT_ROM_START(icon_item_24_static_test), (arg1 - 0x6E), (u8*)msgCtx->unk11EF8 + 0x1000,
                       0x900U);
     }
+
+    if(1){}
     if (globalCtx->pauseCtx.unk_1F0 != 0) {
         msgCtx->unk12010 = ((msgCtx->unk12010 * 1.4f) + 2.0f);
         msgCtx->unk12014 = (msgCtx->unk12014 * 1.4f);
     }
     msgCtx->unk12022 = 1;
 }
-#else
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_message/func_8014C70C.s")
-#endif
 
 void Message_LoadChar(GlobalContext* globalCtx, u16 codePointIndex, s32* offset, f32* arg3, s16 decodedBufPos) {
     MessageContext* msgCtx = &globalCtx->msgCtx;
