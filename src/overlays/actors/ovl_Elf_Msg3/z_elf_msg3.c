51ddca26633d9cc5f129be22d555a32a47b3adda
sonicdcer <alejandro.asenjo88@gmail.com>
2022-06-25 20:50:14 -0300

    pr review changes

--- src/overlays/actors/ovl_Elf_Msg3/z_elf_msg3.c
+++ src/overlays/actors/ovl_Elf_Msg3/z_elf_msg3.c
@@ -42,7 +42,7 @@ void ElfMsg3_SetupAction(ElfMsg3* this, ElfMsg3ActionFunc actionFunc) {
 
 s32 func_80A2CD1C(ElfMsg3* this, GlobalContext* globalCtx) {
     if ((this->actor.home.rot.y > 0) && (this->actor.home.rot.y < 0x81) &&
-        (Flags_GetSwitch(globalCtx, this->actor.home.rot.y - 1) != 0)) {
+        (Flags_GetSwitch(globalCtx, this->actor.home.rot.y - 1))) {
         if (ELFMSG3_GET_SWITCH(&this->actor) != 0x7F) {
             Flags_SetSwitch(globalCtx, ELFMSG3_GET_SWITCH(&this->actor));
         }
@@ -50,7 +50,7 @@ s32 func_80A2CD1C(ElfMsg3* this, GlobalContext* globalCtx) {
         return true;
     }
     if (this->actor.home.rot.y == 0x81) {
-        if (Flags_GetClear(globalCtx, this->actor.room) != 0) {
+        if (Flags_GetClear(globalCtx, this->actor.room)) {
             if (ELFMSG3_GET_SWITCH(&this->actor) != 0x7F) {
                 Flags_SetSwitch(globalCtx, ELFMSG3_GET_SWITCH(&this->actor));
             }
@@ -61,7 +61,7 @@ s32 func_80A2CD1C(ElfMsg3* this, GlobalContext* globalCtx) {
     if (ELFMSG3_GET_SWITCH(&this->actor) == 0x7F) {
         return false;
     }
-    if (Flags_GetSwitch(globalCtx, ELFMSG3_GET_SWITCH(&this->actor)) != 0) {
+    if (Flags_GetSwitch(globalCtx, ELFMSG3_GET_SWITCH(&this->actor))) {
         Actor_MarkForDeath(&this->actor);
         return true;
     }
@@ -70,7 +70,7 @@ s32 func_80A2CD1C(ElfMsg3* this, GlobalContext* globalCtx) {
 
 void ElfMsg3_Init(Actor* thisx, GlobalContext* globalCtx) {
     ElfMsg3* this = THIS;
-    s32 absolute;
+    s32 baseRange;
 
     if (func_80A2CD1C(this, globalCtx) == 0) {
         Actor_ProcessInitChain(&this->actor, sInitChain);
@@ -78,9 +78,9 @@ void ElfMsg3_Init(Actor* thisx, GlobalContext* globalCtx) {
             this->actor.scale.z = 0.4f;
             this->actor.scale.x = 0.4f;
         } else {
-            absolute = ABS_ALT(this->actor.home.rot.x);
-            this->actor.scale.z = absolute * 0.04f;
-            this->actor.scale.x = absolute * 0.04f;
+            baseRange = ABS_ALT(this->actor.home.rot.x);
+            this->actor.scale.z = baseRange * 0.04f;
+            this->actor.scale.x = baseRange * 0.04f;
         }
         if (this->actor.home.rot.z == 0) {
             this->actor.scale.y = 0.4f;
@@ -106,7 +106,7 @@ s32 func_80A2CF50(ElfMsg3* this) {
 
 void func_80A2CF7C(ElfMsg3* this, GlobalContext* globalCtx) {
     Player* player = GET_PLAYER(globalCtx);
-    EnElf* elf = (EnElf*)player->tatlActor;
+    EnElf* tatl = (EnElf*)player->tatlActor;
 
     if (((((player->tatlActor != NULL) &&
            (fabsf(player->actor.world.pos.x - this->actor.world.pos.x) < (100.0f * this->actor.scale.x))) &&
@@ -115,7 +115,7 @@ void func_80A2CF7C(ElfMsg3* this, GlobalContext* globalCtx) {
         (fabsf(player->actor.world.pos.z - this->actor.world.pos.z) < (100.0f * this->actor.scale.z))) {
         player->tatlTextId = func_80A2CF50(this);
         ActorCutscene_SetIntentToPlay(0x7C);
-        elf->elfMsg = &this->actor;
+        tatl->elfMsg = &this->actor;
         if (this->actor.cutscene == -1) {
             this->actor.cutscene = 0x7C;
         }
@@ -123,7 +123,7 @@ void func_80A2CF7C(ElfMsg3* this, GlobalContext* globalCtx) {
             if (ActorCutscene_GetCurrentIndex() == 0x7D) {
                 ActorCutscene_Stop(0x7D);
                 ActorCutscene_SetIntentToPlay(this->actor.cutscene);
-            } else if (ActorCutscene_GetCanPlayNext(this->actor.cutscene) != 0) {
+            } else if (ActorCutscene_GetCanPlayNext(this->actor.cutscene)) {
                 ActorCutscene_Start(this->actor.cutscene, &this->actor);
                 func_800E0348(globalCtx->cameraPtrs[0]);
             } else {
@@ -137,13 +137,13 @@ void ElfMsg3_Update(Actor* thisx, GlobalContext* globalCtx) {
     ElfMsg3* this = THIS;
 
     if (func_80A2CD1C(this, globalCtx) == 0) {
-        if (Actor_ProcessTalkRequest(&this->actor, &globalCtx->state) != 0) {
+        if (Actor_ProcessTalkRequest(&this->actor, &globalCtx->state)) {
             if (ELFMSG3_GET_SWITCH(thisx) != 0x7F) {
                 Flags_SetSwitch(globalCtx, ELFMSG3_GET_SWITCH(thisx));
             }
             Actor_MarkForDeath(&this->actor);
         } else if ((this->actor.home.rot.y >= 0) || (this->actor.home.rot.y < -0x80) ||
-                   (Flags_GetSwitch(globalCtx, -1 - this->actor.home.rot.y) != 0)) {
+                   (Flags_GetSwitch(globalCtx, -1 - this->actor.home.rot.y))) {
             this->actionFunc(this, globalCtx);
         }
     }
