acd4145fe2c6e6e634f1685fcfcb6e11cca90c07
sonicdcer <alejandro.asenjo88@gmail.com>
2022-06-26 14:59:24 -0300

    PlayState

--- src/overlays/actors/ovl_Bg_Open_Shutter/z_bg_open_shutter.c
+++ src/overlays/actors/ovl_Bg_Open_Shutter/z_bg_open_shutter.c
@@ -44,8 +44,8 @@ static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
 
-f32 func_80ACAB10(GlobalContext* this, Actor* actor, f32 arg2, f32 arg3, f32 arg4) {
-    Player* actorPos = GET_PLAYER(this);
+f32 func_80ACAB10(PlayState* play, Actor* actor, f32 arg2, f32 arg3, f32 arg4) {
+    Player* actorPos = GET_PLAYER(play);
     Vec3f point;
     Vec3f offset;
 
@@ -61,13 +61,13 @@ f32 func_80ACAB10(GlobalContext* this, Actor* actor, f32 arg2, f32 arg3, f32 arg
     }
 }
 
-s8 func_80ACABA8(BgOpenShutter* this, GlobalContext* globalCtx) {
-    Player* player = GET_PLAYER(globalCtx);
+s8 func_80ACABA8(BgOpenShutter* this, PlayState* play) {
+    Player* player = GET_PLAYER(play);
     f32 temp_fv0;
     s16 temp_v0;
 
-    if (!Player_InCsMode(&globalCtx->state) && (this->dyna.actor.xzDistToPlayer < 100.0f)) {
-        temp_fv0 = func_80ACAB10(globalCtx, &this->dyna.actor, 0.0f, 65.0f, 15.0f);
+    if (!Player_InCsMode(&play->state) && (this->dyna.actor.xzDistToPlayer < 100.0f)) {
+        temp_fv0 = func_80ACAB10(play, &this->dyna.actor, 0.0f, 65.0f, 15.0f);
         if (fabsf(temp_fv0) < 50.0f) {
             temp_v0 = player->actor.shape.rot.y - this->dyna.actor.shape.rot.y;
             if (temp_fv0 > 0.0f) {
@@ -86,36 +86,36 @@ s8 func_80ACABA8(BgOpenShutter* this, GlobalContext* globalCtx) {
     return 0;
 }
 
-void BgOpenShutter_Init(Actor* thisx, GlobalContext* globalCtx) {
+void BgOpenShutter_Init(Actor* thisx, PlayState* play) {
     BgOpenShutter* this = THIS;
 
     Actor_ProcessInitChain(&this->dyna.actor, sInitChain);
     DynaPolyActor_Init(&this->dyna, 1);
-    DynaPolyActor_LoadMesh(globalCtx, &this->dyna, &object_open_obj_Colheader_001640);
+    DynaPolyActor_LoadMesh(play, &this->dyna, &object_open_obj_Colheader_001640);
     this->actionFunc = func_80ACAD88;
 }
 
-void BgOpenShutter_Destroy(Actor* thisx, GlobalContext* globalCtx) {
+void BgOpenShutter_Destroy(Actor* thisx, PlayState* play) {
     s32 params = (u16)BGOPENSHUTTER_GET_A(thisx);
     BgOpenShutter* this = THIS;
 
-    globalCtx->doorCtx.transitionActorList[params].id = -globalCtx->doorCtx.transitionActorList[params].id;
-    DynaPoly_DeleteBgActor(globalCtx, &globalCtx->colCtx.dyna, this->dyna.bgId);
+    play->doorCtx.transitionActorList[params].id = -play->doorCtx.transitionActorList[params].id;
+    DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
 }
 
-void func_80ACAD88(BgOpenShutter* this, GlobalContext* globalCtx) {
+void func_80ACAD88(BgOpenShutter* this, PlayState* play) {
     s32 quake;
 
     if (this->unk_15C != 0) {
-        Player* player = GET_PLAYER(globalCtx);
+        Player* player = GET_PLAYER(play);
         Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_SLIDE_DOOR_OPEN);
-        func_800DFFAC(globalCtx->cameraPtrs[0], &this->dyna.actor, player->unk_3BA, 0.0f, 12, 15, 10);
+        func_800DFFAC(play->cameraPtrs[0], &this->dyna.actor, player->unk_3BA, 0.0f, 12, 15, 10);
         this->unk_164 = 0;
         this->actionFunc = func_80ACAE5C;
         this->dyna.actor.velocity.y = 0.0f;
     } else {
-        Player* player = GET_PLAYER(globalCtx);
-        quake = func_80ACABA8(this, globalCtx);
+        Player* player = GET_PLAYER(play);
+        quake = func_80ACABA8(this, play);
         if (quake > 0) {
             player->doorType = 2;
             player->doorDirection = quake;
@@ -125,7 +125,7 @@ void func_80ACAD88(BgOpenShutter* this, GlobalContext* globalCtx) {
     }
 }
 
-void func_80ACAE5C(BgOpenShutter* this, GlobalContext* globalCtx) {
+void func_80ACAE5C(BgOpenShutter* this, PlayState* play) {
     Math_StepToF(&this->dyna.actor.velocity.y, 15.0f, 3.0f);
     if (Math_StepToF(&this->dyna.actor.world.pos.y, this->dyna.actor.home.pos.y + 120.0f,
                      this->dyna.actor.velocity.y)) {
@@ -137,17 +137,16 @@ void func_80ACAE5C(BgOpenShutter* this, GlobalContext* globalCtx) {
     }
 }
 
-void func_80ACAEF0(BgOpenShutter* this, GlobalContext* globalCtx) {
+void func_80ACAEF0(BgOpenShutter* this, PlayState* play) {
     s32 pad;
     s16 quake;
 
     Math_StepToF(&this->dyna.actor.velocity.y, 20.0f, 8.0f);
     if (Math_StepToF(&this->dyna.actor.world.pos.y, this->dyna.actor.home.pos.y, this->dyna.actor.velocity.y)) {
         this->dyna.actor.floorHeight = this->dyna.actor.home.pos.y;
-        Actor_SpawnFloorDustRing(globalCtx, &this->dyna.actor, &this->dyna.actor.world.pos, 60.0f, 10, 8.0f, 500, 10,
-                                 true);
+        Actor_SpawnFloorDustRing(play, &this->dyna.actor, &this->dyna.actor.world.pos, 60.0f, 10, 8.0f, 500, 10, true);
         Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_BIGWALL_BOUND);
-        quake = Quake_Add(Play_GetCamera(globalCtx, CAM_ID_MAIN), 3);
+        quake = Quake_Add(Play_GetCamera(play, CAM_ID_MAIN), 3);
         Quake_SetSpeed(quake, -0x7F18);
         Quake_SetQuakeValues(quake, 2, 0, 0, 0);
         Quake_SetCountdown(quake, 10);
@@ -157,30 +156,30 @@ void func_80ACAEF0(BgOpenShutter* this, GlobalContext* globalCtx) {
     }
 }
 
-void BgOpenShutter_Update(Actor* thisx, GlobalContext* globalCtx2) {
+void BgOpenShutter_Update(Actor* thisx, PlayState* play2) {
     BgOpenShutter* this = THIS;
-    GlobalContext* globalCtx = globalCtx2;
+    PlayState* play = play2;
     s32 index;
 
-    if (Cutscene_CheckActorAction(globalCtx, 0x7C)) {
-        index = Cutscene_GetActorActionIndex(globalCtx, 0x7C);
-        if (globalCtx->csCtx.actorActions[index]->action == DOOR_OPEN) {
+    if (Cutscene_CheckActorAction(play, 0x7C)) {
+        index = Cutscene_GetActorActionIndex(play, 0x7C);
+        if (play->csCtx.actorActions[index]->action == DOOR_OPEN) {
             if (this->actionFunc == func_80ACAD88) {
                 Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_SLIDE_DOOR_OPEN);
                 this->actionFunc = func_80ACAE5C;
                 this->dyna.actor.velocity.y = 0.0f;
             }
             this->unk_164 = 0;
-        } else if (globalCtx->csCtx.actorActions[index]->action == DOOR_CLOSED) {
+        } else if (play->csCtx.actorActions[index]->action == DOOR_CLOSED) {
             if (this->actionFunc == func_80ACAE5C) {
                 Actor_PlaySfxAtPos(&this->dyna.actor, NA_SE_EV_SLIDE_DOOR_CLOSE);
                 this->actionFunc = func_80ACAEF0;
             }
         }
     }
-    this->actionFunc(this, globalCtx);
+    this->actionFunc(this, play);
 }
 
-void BgOpenShutter_Draw(Actor* thisx, GlobalContext* globalCtx) {
-    Gfx_DrawDListOpa(globalCtx, object_open_obj_DL_0003E8);
+void BgOpenShutter_Draw(Actor* thisx, PlayState* play) {
+    Gfx_DrawDListOpa(play, object_open_obj_DL_0003E8);
 }
