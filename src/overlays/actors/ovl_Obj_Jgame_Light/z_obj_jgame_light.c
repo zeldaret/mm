efc4805c20cb412cfc8caec3432ca9e07e66d592
sonicdcer <alejandro.asenjo88@gmail.com>
2022-06-26 14:47:41 -0300

    playstate

--- src/overlays/actors/ovl_Obj_Jgame_Light/z_obj_jgame_light.c
+++ src/overlays/actors/ovl_Obj_Jgame_Light/z_obj_jgame_light.c
@@ -66,13 +66,13 @@ void ObjJgameLight_Init(Actor* thisx, PlayState* play) {
     LightInfo* lights = &this->lightInfo;
 
     Actor_SetScale(&this->actor, 1.0f);
-    func_800B4AEC(globalCtx, &this->actor, 50.0f);
+    func_800B4AEC(play, &this->actor, 50.0f);
     ActorShape_Init(&this->actor.shape, 0.0f, func_800B4B50, 1.0f);
-    Collider_InitCylinder(globalCtx, &this->collider);
-    Collider_SetCylinder(globalCtx, &this->collider, &this->actor, &sCylinderInit);
+    Collider_InitCylinder(play, &this->collider);
+    Collider_SetCylinder(play, &this->collider, &this->actor, &sCylinderInit);
     Lights_PointGlowSetInfo(lights, this->actor.world.pos.x, (this->actor.world.pos.y + 70.0f), this->actor.world.pos.z,
                             255, 255, 180, -1);
-    this->lightNode = LightContext_InsertLight(globalCtx, &globalCtx->lightCtx, &this->lightInfo);
+    this->lightNode = LightContext_InsertLight(play, &play->lightCtx, &this->lightInfo);
     Actor_SetFocus(&this->actor, 60.0f);
     this->actor.colChkInfo.health = 0;
     this->unk_1B8 = 0;
@@ -86,8 +86,8 @@ void ObjJgameLight_Init(Actor* thisx, PlayState* play) {
 void ObjJgameLight_Destroy(Actor* thisx, PlayState* play) {
     ObjJgameLight* this = THIS;
 
-    Collider_DestroyCylinder(globalCtx, &this->collider);
-    LightContext_RemoveLight(globalCtx, &globalCtx->lightCtx, this->lightNode);
+    Collider_DestroyCylinder(play, &this->collider);
+    LightContext_RemoveLight(play, &play->lightCtx, this->lightNode);
 }
 
 void func_80C15474(ObjJgameLight* this, PlayState* play) {
@@ -131,8 +131,8 @@ void func_80C15474(ObjJgameLight* this, PlayState* play) {
 
 void ObjJgameLight_UpdateCollision(ObjJgameLight* this, PlayState* play) {
     Collider_UpdateCylinder(&this->actor, &this->collider);
-    CollisionCheck_SetOC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
-    CollisionCheck_SetAC(globalCtx, &globalCtx->colChkCtx, &this->collider.base);
+    CollisionCheck_SetOC(play, &play->colChkCtx, &this->collider.base);
+    CollisionCheck_SetAC(play, &play->colChkCtx, &this->collider.base);
 }
 
 void func_80C15718(ObjJgameLight* this, PlayState* play) {
@@ -159,9 +159,9 @@ void func_80C15718(ObjJgameLight* this, PlayState* play) {
 void ObjJgameLight_Update(Actor* thisx, PlayState* play) {
     ObjJgameLight* this = THIS;
 
-    func_80C15718(this, globalCtx);
-    func_80C15474(this, globalCtx);
-    ObjJgameLight_UpdateCollision(this, globalCtx);
+    func_80C15718(this, play);
+    func_80C15474(this, play);
+    ObjJgameLight_UpdateCollision(this, play);
     this->unk_1AE++;
 }
 
@@ -169,19 +169,19 @@ void ObjJgameLight_Draw(Actor* thisx, PlayState* play) {
     s32 pad;
     ObjJgameLight* this = THIS;
 
-    OPEN_DISPS(globalCtx->state.gfxCtx);
+    OPEN_DISPS(play->state.gfxCtx);
 
-    func_8012C28C(globalCtx->state.gfxCtx);
-    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    func_8012C28C(play->state.gfxCtx);
+    gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gSPDisplayList(POLY_OPA_DISP++, &gObjectSyokudaiTypeSwitchCausesFlameDL);
     if (this->alpha > 0) {
-        func_8012C2DC(globalCtx->state.gfxCtx);
+        func_8012C2DC(play->state.gfxCtx);
         if (this->alpha > 255) {
             gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 210, 64, 32, 255);
         } else {
             gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 210, 64, 32, this->alpha);
         }
-        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         if (this->signal == CORRECT) {
             gSPDisplayList(POLY_XLU_DISP++, gObjJgameLightCorrectDL);
         } else if (this->signal == INCORRECT) {
@@ -191,20 +191,20 @@ void ObjJgameLight_Draw(Actor* thisx, PlayState* play) {
     if (this->unk_1A8 != 0.0f) {
         f32 scale;
 
-        func_8012C2DC(globalCtx->state.gfxCtx);
+        func_8012C2DC(play->state.gfxCtx);
         scale = (this->unk_1A8 * 27.0f) / 10000.0f;
         gSPSegment(POLY_XLU_DISP++, 0x08,
-                   Gfx_TwoTexScroll(globalCtx->state.gfxCtx, 0, 0, 0, 0x20, 0x40, 1, 0, (this->unk_1AE * -0x14) & 0x1FF,
+                   Gfx_TwoTexScroll(play->state.gfxCtx, 0, 0, 0, 0x20, 0x40, 1, 0, (this->unk_1AE * -0x14) & 0x1FF,
                                     0x20, 0x80));
         gDPSetPrimColor(POLY_XLU_DISP++, 0x80, 0x80, 255, 255, 0, 255);
         gDPSetEnvColor(POLY_XLU_DISP++, 255, 0, 0, 0);
         Matrix_Translate(0.0f, 52.0f, 0.0f, MTXMODE_APPLY);
         Matrix_RotateYS(
-            ((Camera_GetCamDirYaw(globalCtx->cameraPtrs[globalCtx->activeCamera]) - this->actor.shape.rot.y) + 0x8000),
+            ((Camera_GetCamDirYaw(play->cameraPtrs[play->activeCamera]) - this->actor.shape.rot.y) + 0x8000),
             MTXMODE_APPLY);
         Matrix_Scale(scale, scale, scale, MTXMODE_APPLY);
-        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(globalCtx->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPDisplayList(POLY_XLU_DISP++, gGameplayKeepDrawFlameDL);
     }
-    CLOSE_DISPS(globalCtx->state.gfxCtx);
+    CLOSE_DISPS(play->state.gfxCtx);
 }
