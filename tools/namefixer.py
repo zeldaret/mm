06a4a3ac7cd76c9a28d5c6c436acad18e4065164
Tom Overton <tom.overton@outlook.com>
2022-06-26 21:11:06 -0700

    namefixer

--- tools/namefixer.py
+++ tools/namefixer.py
@@ -546,6 +546,19 @@ wordReplace = {
     "func_800E8FA4": "Actor_TrackPoint",
     "func_800E9250": "Actor_TrackPlayer",
 
+    "func_8010A000": "Map_GetDungeonOrBossAreaIndex",
+    "func_8010A074": "Map_IsInDungeonOrBossArea",
+    "func_8010A0F0": "Map_GetDungeonAreaIndex",
+    "func_8010A164": "Map_IsInDungeonArea",
+    "func_8010A194": "Map_GetBossAreaIndex",
+    "func_8010A208": "Map_IsInBossArea",
+    "func_8010A2DC": "Map_SavePlayerInitialInfo",
+    "func_8010A33C": "Map_InitRoomData",
+    "func_8010A410": "Map_Destroy",
+    "func_8010A430": "Map_Init",
+    "func_8010A54C": "Minimap_Draw",
+    "func_8010A580": "Map_Update",
+
     # Structs
     "ActorAnimationEntry": "AnimationInfo",
     "ActorAnimationEntryS": "AnimationInfoS",
