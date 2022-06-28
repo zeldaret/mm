94127337fa9f0069041717d70dda8038c8011e03
sonicdcer <alejandro.asenjo88@gmail.com>
2022-06-28 02:43:52 -0300

    pr review changes

--- src/overlays/actors/ovl_Obj_Demo/z_obj_demo.c
+++ src/overlays/actors/ovl_Obj_Demo/z_obj_demo.c
@@ -31,34 +31,32 @@ const ActorInit Obj_Demo_InitVars = {
 
 void ObjDemo_Init(Actor* thisx, PlayState* play) {
     ObjDemo* this = THIS;
-    s16 temp_v0;
 
     thisx->params &= 0xFF;
     if ((thisx->params != 0xFF) && (Flags_GetSwitch(play, thisx->params))) {
         Actor_MarkForDeath(&this->actor);
-    } else {
-        if (thisx->shape.rot.x < 0) {
-            this->unk_148 = 1;
-            thisx->shape.rot.x = -thisx->shape.rot.x;
-        }
-        if (thisx->shape.rot.x == 0) {
-            this->unk_14C = 80.0f;
-        } else {
-            this->unk_14C = thisx->shape.rot.x * 4.0f;
-            thisx->shape.rot.x = 0;
-            thisx->world.rot.x = 0;
-        }
-        temp_v0 = thisx->shape.rot.z;
-        if (temp_v0 == 0) {
-            this->unk_150 = 60.0f;
-        } else {
-            thisx->shape.rot.z = 0;
-            thisx->world.rot.z = 0;
-            this->unk_150 = temp_v0 * 4.0f;
-        }
-        func_80983634(play);
-        this->actionFunc = func_80983678;
+        return;
     }
+    if (thisx->shape.rot.x < 0) {
+        this->unk_148 = 1;
+        thisx->shape.rot.x = -thisx->shape.rot.x;
+    }
+    if (thisx->shape.rot.x == 0) {
+        this->xzRange = 80.0f;
+    } else {
+        this->xzRange = thisx->shape.rot.x * 4.0f;
+        thisx->shape.rot.x = 0;
+        thisx->world.rot.x = 0;
+    }
+    if (thisx->shape.rot.z == 0) {
+        this->yRange = 60.0f;
+    } else {
+        this->yRange = thisx->shape.rot.z * 4.0f;
+        thisx->shape.rot.z = 0;
+        thisx->world.rot.z = 0;
+    }
+    func_80983634(play);
+    this->actionFunc = func_80983678;
 }
 
 void func_80983634(PlayState* play) {
@@ -69,7 +67,7 @@ void func_80983634(PlayState* play) {
 
 void func_80983678(ObjDemo* this, PlayState* play) {
     func_80983634(play);
-    if ((this->actor.xzDistToPlayer < this->unk_14C) && (fabsf(this->actor.playerHeightRel) < this->unk_150)) {
+    if ((this->actor.xzDistToPlayer < this->xzRange) && (fabsf(this->actor.playerHeightRel) < this->yRange)) {
         if (this->unk_148 == 1) {
             ActorCutscene_Stop(0x7D);
         }
@@ -82,37 +80,37 @@ void func_80983704(ObjDemo* this, PlayState* play) {
     if ((this->unk_148 == 1) && (ActorCutscene_GetCurrentIndex() == 0x7D)) {
         ActorCutscene_Stop(0x7D);
         ActorCutscene_SetIntentToPlay(this->actor.cutscene);
-        return;
-    }
-    if (ActorCutscene_GetCanPlayNext(this->actor.cutscene) != 0) {
-        if (this->unk_148 == 1) {
-            ActorCutscene_Start(this->actor.cutscene, &this->actor);
-            func_800E0348(play->cameraPtrs[0]);
-        } else {
-            ActorCutscene_StartAndSetUnkLinkFields(this->actor.cutscene, &this->actor);
-        }
-        if (play->sceneNum == SCENE_CASTLE) {
-            Audio_QueueSeqCmd(NA_BGM_IKANA_CASTLE | 0x8000);
-        }
-        this->actor.cutscene = ActorCutscene_GetAdditionalCutscene(this->actor.cutscene);
-        if (this->actor.cutscene == -1) {
-            if (this->actor.params != 0xFF) {
-                Flags_SetSwitch(play, this->actor.params);
-            }
-            Actor_MarkForDeath(&this->actor);
-        }
     } else {
-        ActorCutscene_SetIntentToPlay(this->actor.cutscene);
-        func_80983634(play);
+        if (ActorCutscene_GetCanPlayNext(this->actor.cutscene)) {
+            if (this->unk_148 == 1) {
+                ActorCutscene_Start(this->actor.cutscene, &this->actor);
+                func_800E0348(play->cameraPtrs[0]);
+            } else {
+                ActorCutscene_StartAndSetUnkLinkFields(this->actor.cutscene, &this->actor);
+            }
+            if (play->sceneNum == SCENE_CASTLE) {
+                Audio_QueueSeqCmd(NA_BGM_IKANA_CASTLE | 0x8000);
+            }
+            this->actor.cutscene = ActorCutscene_GetAdditionalCutscene(this->actor.cutscene);
+            if (this->actor.cutscene == -1) {
+                if (this->actor.params != 0xFF) {
+                    Flags_SetSwitch(play, this->actor.params);
+                }
+                Actor_MarkForDeath(&this->actor);
+            }
+        } else {
+            ActorCutscene_SetIntentToPlay(this->actor.cutscene);
+            func_80983634(play);
+        }
     }
 }
 
 void ObjDemo_Update(Actor* thisx, PlayState* play) {
     ObjDemo* this = THIS;
 
-    if ((this->actor.params != 0xFF) && (Flags_GetSwitch(play, this->actor.params) != 0)) {
+    if ((this->actor.params != 0xFF) && Flags_GetSwitch(play, this->actor.params)) {
         Actor_MarkForDeath(&this->actor);
-    } else {
-        this->actionFunc(this, play);
+        return;
     }
+    this->actionFunc(this, play);
 }
--- src/overlays/actors/ovl_Obj_Demo/z_obj_demo.h
+++ src/overlays/actors/ovl_Obj_Demo/z_obj_demo.h
@@ -11,8 +11,8 @@ typedef struct ObjDemo {
     /* 0x000 */ Actor actor;
     /* 0x144 */ ObjDemoActionFunc actionFunc;
     /* 0x148 */ s32 unk_148;
-    /* 0x14C */ f32 unk_14C;
-    /* 0x150 */ f32 unk_150;
+    /* 0x14C */ f32 xzRange;
+    /* 0x150 */ f32 yRange;
 } ObjDemo; // size = 0x154
 
 extern const ActorInit Obj_Demo_InitVars;
