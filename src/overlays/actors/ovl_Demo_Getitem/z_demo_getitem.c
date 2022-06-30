c5ff29c15d608b8152116f77c7bb71778e1966df
sonicdcer <alejandro.asenjo88@gmail.com>
2022-06-30 05:31:46 -0300

    renaming

--- src/overlays/actors/ovl_Demo_Getitem/z_demo_getitem.c
+++ src/overlays/actors/ovl_Demo_Getitem/z_demo_getitem.c
@@ -16,7 +16,7 @@ void DemoGetitem_Update(Actor* thisx, PlayState* play);
 
 void func_80A4FB10(DemoGetitem* this, PlayState* play);
 void func_80A4FB68(DemoGetitem* this, PlayState* play2);
-void func_80A4FCF0(Actor* thisx, PlayState* play);
+void DemoGetitem_Draw(Actor* thisx, PlayState* play);
 
 const ActorInit Demo_Getitem_InitVars = {
     ACTOR_DEMO_GETITEM,
@@ -34,7 +34,7 @@ static s16 sObjectBankIndices[] = { OBJECT_GI_MASK14, OBJECT_GI_SWORD_4 };
 
 static s16 sGetItemDraws[] = { GID_MASK_GREAT_FAIRY, GID_SWORD_GREAT_FAIRY };
 
-static u16 csActionIndex[] = { 0x6E, 0x236 };
+static u16 sCsActionIndices[] = { 0x6E, 0x236 };
 
 void DemoGetitem_Init(Actor* thisx, PlayState* play) {
     s32 pad;
@@ -49,7 +49,7 @@ void DemoGetitem_Init(Actor* thisx, PlayState* play) {
     Actor_SetScale(&this->actor, 0.25f);
     this->actionFunc = func_80A4FB10;
     this->item = sGetItemDraws[itemIndex];
-    this->csAction = csActionIndex[itemIndex];
+    this->csAction = sCsActionIndices[itemIndex];
     objIndex = Object_GetIndex(&play->objectCtx, sObjectBankIndices[itemIndex]);
     if (objIndex < 0) {
         Actor_MarkForDeath(&this->actor);
@@ -79,15 +79,15 @@ void func_80A4FB68(DemoGetitem* this, PlayState* play2) {
         }
         switch (play->csCtx.actorActions[Cutscene_GetActorActionIndex(play, this->csAction)]->action) {
             case 2:
-                this->actor.draw = func_80A4FCF0;
+                this->actor.draw = DemoGetitem_Draw;
                 Cutscene_ActorTranslate(&this->actor, play, Cutscene_GetActorActionIndex(play, this->csAction));
-                this->actor.shape.rot.y += 1000;
+                this->actor.shape.rot.y += 0x3E8;
                 return;
             case 3:
                 Actor_MarkForDeath(&this->actor);
                 return;
             case 4:
-                this->actor.draw = func_80A4FCF0;
+                this->actor.draw = DemoGetitem_Draw;
                 Cutscene_ActorTranslateAndYaw(&this->actor, play, Cutscene_GetActorActionIndex(play, this->csAction));
                 this->actor.shape.yOffset = Math_SinS(sp22) * 15.0f;
                 return;
@@ -104,7 +104,7 @@ void DemoGetitem_Update(Actor* thisx, PlayState* play) {
     this->actionFunc(this, play);
 }
 
-void func_80A4FCF0(Actor* thisx, PlayState* play) {
+void DemoGetitem_Draw(Actor* thisx, PlayState* play) {
     DemoGetitem* this = THIS;
 
     func_800B8050(&this->actor, play, 0);
