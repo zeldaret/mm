f400ea61393bcd9d892a370092b1a9a05ceaf36d
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-01 23:16:50 -0300

    all functions matching

--- src/overlays/actors/ovl_En_Sth2/z_en_sth2.h
+++ src/overlays/actors/ovl_En_Sth2/z_en_sth2.h
@@ -5,9 +5,18 @@
 
 struct EnSth2;
 
+typedef void (*EnSth2ActionFunc)(struct EnSth2*, PlayState*);
+
 typedef struct EnSth2 {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x10C];
+    /* 0x144 */ SkelAnime unk144;
+    /* 0x188 */ Vec3s unk188;
+    /* 0x18E */ char pad18E[0x5A];
+    /* 0x1E8 */ Vec3s unk1E8;
+    /* 0x1EE */ char pad1EE[0x5A];
+    /* 0x248 */ s16 unk248;
+    /* 0x24A */ u8 unk24A;
+    /* 0x24C */ EnSth2ActionFunc actionFunc;
 } EnSth2; // size = 0x250
 
 extern const ActorInit En_Sth2_InitVars;
