941cdeb696563ba3d07c4562ef4ddf592db9c941
ordlucas <benning.luke@gmail.com>
2022-07-04 23:51:10 -0400

    Suggested changes

--- src/overlays/actors/ovl_En_Part/z_en_part.c
+++ src/overlays/actors/ovl_En_Part/z_en_part.c
@@ -30,38 +30,28 @@ const ActorInit En_Part_InitVars = {
     (ActorFunc)EnPart_Draw,
 };
 
-Vec3f D_80865900 = { 0.0f, 0.0f, 0.0f };
-
-static void (*D_8086590C[5])(EnPart*, PlayState*) = {
-    func_80865390, func_808654C4, 0x00000000, 0x00000000, 0x00000000,
-};
-
 void EnPart_Init(Actor* thisx, PlayState* play) {
-    EnPart* this = THIS;
 }
 
 void EnPart_Destroy(Actor* thisx, PlayState* play) {
-    EnPart* this = THIS;
 }
 
 void func_80865390(EnPart* this, PlayState* play) {
     f32 randVar;
 
     this->unk144 = 1;
-    this->actor.world.rot.y = (s16)(Rand_ZeroOne() * 20000.0f);
+    this->actor.world.rot.y = (Rand_ZeroOne() * 20000.0f);
     switch (this->actor.params) {
-        case 1:
-
-        case 4:
+        case PARAMS_1:
+        case PARAMS_4:
             randVar = Rand_ZeroOne() * 17.0f;
             this->unk146 += (s16)randVar + 5;
             this->actor.velocity.y = (Rand_ZeroOne() * 5.0f) + 4.0f;
             this->actor.gravity = (-0.6f) - (Rand_ZeroOne() * 0.5f);
             this->unk14C = 0.15f;
             break;
-
-        case 15:
-            this->actor.world.rot.y = this->actor.parent->shape.rot.y + 32768;
+        case PARAMS_15:
+            this->actor.world.rot.y = this->actor.parent->shape.rot.y + 0x8000;
             this->unk146 = 100;
             this->actor.velocity.y = 7.0f;
             this->actor.speedXZ = 2.0f;
@@ -71,81 +61,82 @@ void func_80865390(EnPart* this, PlayState* play) {
 }
 
 void func_808654C4(EnPart* this, PlayState* play) {
-    s16 tempVar;
-    Vec3f sp88;
-    Vec3f sp74;
-    s32 counter;
-    Vec3f sp64;
+    s16 effectScale;
+    Vec3f effectPos;
+    Vec3f effectVelocity = { 0.0f, 0.0f, 0.0f };
+    s32 i;
+    Vec3f pos;
 
-    sp74 = D_80865900;
-
-    if (this->actor.params == 15) {
-        this->unk146 -= 1;
+    if (this->actor.params == PARAMS_15) {
+        this->unk146--;
         if (this->unk146 > 0) {
-            this->actor.shape.rot.x += 15000;
+            this->actor.shape.rot.x += 0x3a98;
             this->actor.shape.rot.y = this->actor.shape.rot.y;
             this->actor.shape.rot.z = this->actor.shape.rot.z;
             if (BgCheck_SphVsFirstPoly(&play->colCtx, &this->actor.world.pos, 20.0f)) {
                 this->unk146 = 0;
             }
         } else {
-            Math_Vec3f_Copy(&sp64, &this->actor.world.pos);
-            sp64.y = this->actor.floorHeight;
-            func_800B3030(play, &sp64, &gZeroVec3f, &gZeroVec3f, (s16)(this->actor.scale.y * 1400.0f), 7, 0);
-            SoundSource_PlaySfxAtFixedWorldPos(play, &this->actor.world.pos, 10, 14456);
+            Math_Vec3f_Copy(&pos, &this->actor.world.pos);
+            pos.y = this->actor.floorHeight;
+            func_800B3030(play, &pos, &gZeroVec3f, &gZeroVec3f, this->actor.scale.y * 1400.0f, 7, 0);
+            SoundSource_PlaySfxAtFixedWorldPos(play, &this->actor.world.pos, 10, NA_SE_EN_EXTINCT);
             Actor_MarkForDeath(&this->actor);
         }
     } else {
         if (this->unk146 <= 0) {
             switch (this->actor.params) {
-                case 1:
-                    func_800B3030(play, &this->actor.world.pos, &gZeroVec3f, &gZeroVec3f,
-                                  (s16)(this->actor.scale.y * 4000.0f), 7, 1);
-                    SoundSource_PlaySfxAtFixedWorldPos(play, &this->actor.world.pos, 10, 14456);
+                case PARAMS_1:
+                    func_800B3030(play, &this->actor.world.pos, &gZeroVec3f, &gZeroVec3f, this->actor.scale.y * 4000.0f,
+                                  7, 1);
+                    SoundSource_PlaySfxAtFixedWorldPos(play, &this->actor.world.pos, 10, NA_SE_EN_EXTINCT);
                     break;
-                case 4:
-                    for (counter = 7; counter >= 0; counter--) {
-                        sp88.x = randPlusMinusPoint5Scaled(60.0f) + this->actor.world.pos.x;
-                        sp88.y = randPlusMinusPoint5Scaled(50.0f) +
-                                 (this->actor.world.pos.y + (this->actor.shape.yOffset * this->actor.scale.y));
-                        sp88.z = randPlusMinusPoint5Scaled(60.0f) + this->actor.world.pos.z;
-                        sp74.y = Rand_ZeroOne() + 1.0f;
-                        tempVar = Rand_S16Offset(80, 100);
-                        EffectSsDtBubble_SpawnColorProfile(play, &sp88, &sp74, &gZeroVec3f, tempVar, 25, 0, 1);
+                case PARAMS_4:
+                    for (i = 7; i >= 0; i--) {
+                        effectPos.x = randPlusMinusPoint5Scaled(60.0f) + this->actor.world.pos.x;
+                        effectPos.y = randPlusMinusPoint5Scaled(50.0f) +
+                                      (this->actor.world.pos.y + (this->actor.shape.yOffset * this->actor.scale.y));
+                        effectPos.z = randPlusMinusPoint5Scaled(60.0f) + this->actor.world.pos.z;
+                        effectVelocity.y = Rand_ZeroOne() + 1.0f;
+                        effectScale = Rand_S16Offset(80, 100);
+                        EffectSsDtBubble_SpawnColorProfile(play, &effectPos, &effectVelocity, &gZeroVec3f, effectScale,
+                                                           25, 0, 1);
                     }
                     break;
             }
             Actor_MarkForDeath(&this->actor);
         } else {
-            this->unk146 -= 1;
-            this->unk148 += this->unk14C;
+            this->unk146--;
+            this->zRot += this->unk14C;
         }
     }
 }
 
+EnPartActionFunc D_8086590C[] = { func_80865390, func_808654C4 };
+
 void EnPart_Update(Actor* thisx, PlayState* play) {
-    EnPart* this = (EnPart*)thisx;
+    EnPart* this = THIS;
 
     Actor_MoveWithGravity(&this->actor);
-    D_8086590C[this->unk144](this, play);
+    (*D_8086590C[this->unk144])(this, play);
 }
 
 void EnPart_Draw(Actor* thisx, PlayState* play) {
-    EnPart* this = (EnPart*)thisx;
+    EnPart* this = THIS;
 
     OPEN_DISPS(play->state.gfxCtx);
 
-    if (this->actor.params > 0) {
-        Matrix_RotateZF(this->unk148, MTXMODE_APPLY);
+    if (this->actor.params > PARAMS_0) {
+        Matrix_RotateZF(this->zRot, MTXMODE_APPLY);
     }
     func_8012C28C(play->state.gfxCtx);
     func_800B8050(&this->actor, play, 0);
-    if (this->actor.params == 15) {
+    if (this->actor.params == PARAMS_15) {
         gSPSegment(POLY_OPA_DISP++, 0x0C, gEmptyDL);
     }
-    if (this->unk_150 != NULL) {
+    if (this->displayList != NULL) {
         gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-        gSPDisplayList(POLY_OPA_DISP++, this->unk_150);
+        gSPDisplayList(POLY_OPA_DISP++, this->displayList);
     }
 
     CLOSE_DISPS(play->state.gfxCtx);
--- src/overlays/actors/ovl_En_Part/z_en_part.h
+++ src/overlays/actors/ovl_En_Part/z_en_part.h
@@ -5,14 +5,22 @@
 
 struct EnPart;
 
+typedef void (*EnPartActionFunc)(struct EnPart*, PlayState*);
+
+typedef enum {
+    PARAMS_0 = 0,
+    PARAMS_1 = 1,
+    PARAMS_4 = 4,
+    PARAMS_15 = 15
+} ActorParams;
+
 typedef struct EnPart {
     /* 0x000 */ Actor actor;
     /* 0x144 */ u8 unk144;
-    /* 0x145 */ s8 unk145;
     /* 0x146 */ s16 unk146;
-    /* 0x148 */ f32 unk148;
+    /* 0x148 */ f32 zRot;
     /* 0x14C */ f32 unk14C;
-    /* 0x150 */ Gfx* unk_150;
+    /* 0x150 */ Gfx* displayList;
 } EnPart; // size = 0x154
 
 extern const ActorInit En_Part_InitVars;
