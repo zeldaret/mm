1626cf3a411152cedafe3534f366d1a601dc978c
engineer124 <engineer124engineer124@gmail.com>
2022-07-08 00:06:14 +1000

    Begin Kaleido

--- src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_item.c
+++ src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_item.c
@@ -5,10 +5,9 @@
  */
 
 #include "z_kaleido_scope.h"
+#include "interface/parameter_static/parameter_static.h"
 
 extern UNK_TYPE D_02001360;
-extern UNK_TYPE D_020044A0;
-extern UNK_TYPE D_02004AA0;
 extern UNK_TYPE D_0200B998;
 extern UNK_TYPE D_08062000;
 extern UNK_TYPE D_08064340;
@@ -16,10 +15,70 @@ extern UNK_TYPE D_0B000000;
 extern UNK_TYPE D_0C000000;
 extern UNK_TYPE D_0C006C00;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_8081B240.s")
+Gfx* func_8010CD98(Gfx* displayListHead, void* texture, s16 textureWidth, s16 textureHeight, s16 rectLeft, s16 rectTop,
+                   s16 rectWidth, s16 rectHeight, u16 dsdx, u16 dtdy);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_8081B6BC.s")
+extern u8 gSlotAgeReqs[4][24];
+extern s16 D_8082B444[];
+extern s16 D_8082B454[];
 
+void KaleidoScope_DrawAmmoCount(PauseContext* pauseCtx, GraphicsContext* gfxCtx, s16 item, u16 arg3) {
+    s16 i;
+    s16 ammo;
+
+    OPEN_DISPS(gfxCtx);
+
+    if (item == ITEM_PICTO_BOX) {
+        if (!(((void)0, gSaveContext.save.inventory.questItems) & (gBitFlags[QUEST_UNK_19]))) {
+            ammo = 0;
+        } else {
+            ammo = 1;
+        }
+    } else {
+        ammo = AMMO(item);
+    }
+
+    gDPPipeSync(POLY_OPA_DISP++);
+
+    if (gSlotAgeReqs[((void)0, gSaveContext.save.playerForm)][gItemSlots[item]] == 0) {
+        gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 100, 100, 100, pauseCtx->alpha);
+    } else {
+        gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 255, 255, 255, pauseCtx->alpha);
+        if (ammo == 0) {
+            gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 130, 130, 130, pauseCtx->alpha);
+        } else if (((item == ITEM_BOMB) && (AMMO(item) == CUR_CAPACITY(UPG_BOMB_BAG))) ||
+                   ((item == ITEM_BOW) && (AMMO(item) == CUR_CAPACITY(UPG_QUIVER))) ||
+                   ((item == ITEM_STICK) && (AMMO(item) == CUR_CAPACITY(UPG_STICKS))) ||
+                   ((item == ITEM_NUT) && (AMMO(item) == CUR_CAPACITY(UPG_NUTS))) ||
+                   ((item == ITEM_BOMBCHU) && (AMMO(item) == CUR_CAPACITY(UPG_BOMB_BAG))) ||
+                   ((item == ITEM_POWDER_KEG) && (ammo == 1)) || ((item == ITEM_PICTO_BOX) && (ammo == 1)) ||
+                   ((item == ITEM_MAGIC_BEANS) && (ammo == 20))) {
+            gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 120, 255, 0, pauseCtx->alpha);
+        }
+    }
+
+    for (i = 0; ammo >= 10; i++) {
+        ammo -= 10;
+    }
+
+    gDPPipeSync(POLY_OPA_DISP++);
+
+    if (i != 0) {
+        POLY_OPA_DISP = func_8010CD98(POLY_OPA_DISP, ((u8*)gAmmoDigit0Tex + (8 * 8 * i)), 8, 8, D_8082B444[arg3],
+                                      D_8082B454[arg3], 8, 8, 1 << 10, 1 << 10);
+    }
+    POLY_OPA_DISP = func_8010CD98(POLY_OPA_DISP, ((u8*)gAmmoDigit0Tex + (8 * 8 * ammo)), 8, 8, D_8082B444[arg3] + 6,
+                                  D_8082B454[arg3], 8, 8, 1 << 10, 1 << 10);
+
+    CLOSE_DISPS(gfxCtx);
+}
+
+void KaleidoScope_SetCursorVtx(PauseContext* pauseCtx, u16 index, Vtx* vtx) {
+    pauseCtx->cursorVtx[0].v.ob[0] = vtx[index].v.ob[0];
+    pauseCtx->cursorVtx[0].v.ob[1] = vtx[index].v.ob[1];
+}
+
+void func_8081B6EC(GraphicsContext* gfxCtx);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_8081B6EC.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_8081BCA8.s")
--- src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope_NES.c
+++ src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope_NES.c
@@ -16,15 +16,62 @@ extern UNK_TYPE D_0B000000;
 extern UNK_TYPE D_0C000000;
 extern UNK_TYPE D_0C006C00;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80821900.s")
+void func_80178E3C(u8*, s32, s32, s32);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_8082192C.s")
+void func_80821900(s32 arg0, s32 arg1) {
+    func_80178E3C(SEGMENT_ROM_START(map_name_static), arg1, arg0, 0x400);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80821958.s")
+void func_8082192C(s32 arg0, s32 arg1) {
+    func_80178E3C(SEGMENT_ROM_START(map_name_static), arg1, arg0, 0xA00);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80821984.s")
+void func_80821958(s32 arg0, s32 arg1) {
+    func_80178E3C(SEGMENT_ROM_START(item_name_static), arg1, arg0, 0x400);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80821A04.s")
+void func_80821984(PlayState* play, s16 arg1) {
+    PauseContext* pauseCtx = &play->pauseCtx;
+
+    pauseCtx->unk_268[pauseCtx->pageIndex] = 0;
+    pauseCtx->unk_258 = arg1;
+    pauseCtx->unk_25A = 0;
+
+    play_sound(NA_SE_SY_DECIDE);
+
+    gSaveContext.buttonStatus[0] = 0;
+    gSaveContext.buttonStatus[1] = 0xFF;
+    gSaveContext.buttonStatus[2] = 0xFF;
+    gSaveContext.buttonStatus[3] = 0xFF;
+    gSaveContext.buttonStatus[4] = 0xFF;
+
+    gSaveContext.unk_3F22 = 0;
+    Interface_ChangeAlpha(50);
+}
+
+extern u8 D_801C6A98[5][5];
+
+void func_80821A04(PlayState* play) {
+    PauseContext* pauseCtx = &play->pauseCtx;
+
+    pauseCtx->unk_280 = 0;
+    pauseCtx->unk_258 = 0;
+
+    play_sound(NA_SE_SY_CURSOR);
+
+    pauseCtx->unk_298 = 4.0f;
+
+    // TODO: indexing of D_801C6A98 is likely wrong
+    gSaveContext.buttonStatus[0] = 0;
+    gSaveContext.buttonStatus[1] = D_801C6A98[pauseCtx->pageIndex + 1][1];
+    gSaveContext.buttonStatus[2] = D_801C6A98[pauseCtx->pageIndex + 1][1];
+    gSaveContext.buttonStatus[3] = D_801C6A98[pauseCtx->pageIndex + 1][1];
+    gSaveContext.buttonStatus[4] = 0;
+
+    gSaveContext.unk_3F22 = 0;
+
+    Interface_ChangeAlpha(50);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80821AD4.s")
 
@@ -62,7 +109,30 @@ extern UNK_TYPE D_0C006C00;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/KaleidoScope_Draw.s")
 
+#ifdef NON_MATCHING
+void func_808286D8(u32* arg0, u16 arg1) {
+    u8 temp_t1;
+    u32 var_a1;
+    u16 i;
+    u32 temp_a1_2;
+    s32 j;
+
+    if (!arg0) {}
+
+    for (i = 0; i < arg1; i++) {
+        j = i;
+        if (arg0[j] & ~0xFF) {
+            var_a1 = arg0[j];
+            temp_a1_2 = var_a1 >> 8;
+            if (j) {}
+            temp_t1 = ((((temp_a1_2 & 0xFF0000) >> 0x10) + ((temp_a1_2 & 0xFF00) >> 7) + (temp_a1_2 & 0xFF)) / 7) & 0xFF;
+            arg0[j] = (arg0[j] & 0xFF) | (((((temp_t1 << 8) | temp_t1) << 8) | temp_t1) << 8);
+        }
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_808286D8.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_kaleido_scope/func_80828788.s")
 
