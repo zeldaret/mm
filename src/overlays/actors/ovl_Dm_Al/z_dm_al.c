e174316957beae3f7ad18cdad1b5782f1f127c7b
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-06 21:35:14 -0300

    cleanup in progress

--- src/overlays/actors/ovl_Dm_Al/z_dm_al.c
+++ src/overlays/actors/ovl_Dm_Al/z_dm_al.c
@@ -16,13 +16,6 @@ void DmAl_Destroy(Actor* thisx, PlayState* play);
 void DmAl_Update(Actor* thisx, PlayState* play);
 void DmAl_Draw(Actor* thisx, PlayState* play);
 
-void func_80C1BDD8(DmAl* this, PlayState* play);
-
-s32 func_80C1BD90(DmAl* this, PlayState* play);
-s32 func_80C1C028(PlayState* play, s32 limbIndex, Gfx** gfx, Vec3f* rot, Vec3s* pos, Actor* thisx);
-void func_80C1C064(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx);
-
-#if 1
 const ActorInit Dm_Al_InitVars = {
     ACTOR_EN_AL,
     ACTORCAT_NPC,
@@ -35,44 +28,24 @@ const ActorInit Dm_Al_InitVars = {
     (ActorFunc)DmAl_Draw,
 };
 
-#endif
-
-/*
-static AnimationInfoS D_80C1C270[] = {
+static AnimationInfoS sAnimationSittingBarstool[] = {
     &object_al_Anim_00DBE0,
     1.0f,
+    0,
     -1,
-    0,
-    0,
-    0
-};*/
-
-static AnimationInfoS D_80C1C270[] = {
-    0x0600DBE0,
-    0x3F800000,
-    0x0000FFFF,
-    0,
     0,
     0
 };
 
-//extern AnimationInfoS D_80C1C270;
-//extern UNK_TYPE D_80C1C280[];
-//extern s32 D_80C1C294[];
-//extern s32 D_80C1C2AC;
+s32 func_80C1BD90(DmAl* this, s32* animationIndex) {
+    s32 ret = 0;
 
-s32 func_80C1BD90(DmAl* this, PlayState* play) {
-    s32 phi_v1;
-
-    phi_v1 = 0;
-    if (play != this->unk454) {
-        this->unk454 = play;
-        phi_v1 = SubS_ChangeAnimationByInfoS(&this->unk144, &D_80C1C270, (s32)play);
+    if (animationIndex != this->animationIndex) {
+        this->animationIndex = animationIndex;
+        ret = SubS_ChangeAnimationByInfoS(&this->skelAnime, sAnimationSittingBarstool, animationIndex);
     }
-    return phi_v1;
+    return ret;
 }
-
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Al/func_80C1BDD8.s")
 
 void func_80C1BDD8(DmAl *this, PlayState *play) {
     s32 D_80C1C280[] = {
@@ -83,35 +56,31 @@ void func_80C1BDD8(DmAl *this, PlayState *play) {
 
     if (play->csCtx.state != 0) {
         if (this->unk45C == 0) {
-            this->unk450 = 0xFF;
+            this->action = 0xFF;
             this->unk45C = 1;
-            this->unk458 = this->unk454;
+            this->animationIndex2 = this->animationIndex;
         }
         if (Cutscene_CheckActorAction(play, 0x232)) {
             actionIndex = Cutscene_GetActorActionIndex(play, 0x232);
             action = play->csCtx.actorActions[actionIndex]->action;
-            if (this->unk450 != (u8)action) {
-                this->unk450 = action;
+            if (this->action != (u8)action) {
+                this->action = action;
                 func_80C1BD90(this, D_80C1C280[action]);
-                //func_80C1BD90(this, (sp + (action * 4))->unk2C);
-
             }
             Cutscene_ActorTranslateAndYaw(&this->actor, play, actionIndex);
         }
     } else if (this->unk45C != 0) {
         this->unk45C = 0;
-        func_80C1BD90(this, this->unk458);
+        func_80C1BD90(this, this->animationIndex2);
     }
 }
 
-//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Al/DmAl_Init.s")
-
 void DmAl_Init(Actor* thisx, PlayState* play) {
-    DmAl* this = (DmAl*)thisx;
+    DmAl* this = THIS;
 
     ActorShape_Init(&this->actor.shape, 0.0f, NULL, 0.0f);
-    SkelAnime_InitFlex(play, &this->unk144, &object_al_Skel_00A0D8, NULL, &this->unk30C, &this->unk3AE, 0x1B);
-    this->unk454 = (PlayState*)-1;
+    SkelAnime_InitFlex(play, &this->skelAnime, &object_al_Skel_00A0D8, NULL, &this->morphTable, &this->unk3AE, 0x1B);
+    this->animationIndex = -1;
     func_80C1BD90(this, NULL);
     this->actor.flags &= -2;
     Actor_SetScale(&this->actor, 0.01f);
@@ -125,7 +94,7 @@ void DmAl_Update(Actor* thisx, PlayState* play) {
     DmAl* this = THIS;
 
     this->actionFunc(this, play);
-    SkelAnime_Update(&this->unk144);
+    SkelAnime_Update(&this->skelAnime);
     Actor_UpdateBgCheckInfo(play, &this->actor, 30.0f, 12.0f, 0.0f, 4);
 }
 
@@ -177,12 +146,12 @@ s32 D_80C1C294[6] = { 0x06006598, 0x06005920, 0x06005878, 0x060057D0, 0x06005728
 s32 D_80C1C2AC = 0;
 
 void DmAl_Draw(Actor* thisx, PlayState* play) {
-    s32 i;
-    DmAl* this = (DmAl*)thisx;
+    u32 i;
+    DmAl* this = THIS;
  
     OPEN_DISPS(play->state.gfxCtx);
     func_8012C28C(play->state.gfxCtx);
-    SkelAnime_DrawTransformFlexOpa(play, this->unk144.skeleton, this->unk144.jointTable, this->unk144.dListCount,
+    SkelAnime_DrawTransformFlexOpa(play, this->skelAnime.skeleton, this->skelAnime.jointTable, this->skelAnime.dListCount,
                                    func_80C1C028, func_80C1C064, func_80C1C11C, &this->actor);
     for (i = 0; i < (((sizeof(this->unkMtx)) / (sizeof(this->unkMtx[0])))); i++) {
         Matrix_Put(&this->unkMtx[i]);
--- src/overlays/actors/ovl_Dm_Al/z_dm_al.h
+++ src/overlays/actors/ovl_Dm_Al/z_dm_al.h
@@ -9,17 +9,17 @@ typedef void (*DmAlActionFunc)(struct DmAl*, PlayState*);
 
 typedef struct DmAl {
     /* 0x0000 */ Actor actor;
-    /* 0x144 */ SkelAnime unk144;
+    /* 0x144 */ SkelAnime skelAnime;
     /* 0x188 */ DmAlActionFunc actionFunc;
     /* 0x18C */ MtxF unkMtx[6];
-    /* 0x30C */ Vec3s unk30C;
+    /* 0x30C */ Vec3s morphTable;
     /* 0x312 */ char pad312[0x9C];
     /* 0x3AE */ Vec3s unk3AE;
     /* 0x3B4 */ char pad3B4[0x9C];
-    /* 0x450 */ u8 unk450;
+    /* 0x450 */ u8 action;
     /* 0x451 */ char pad451[3];
-    /* 0x454 */ PlayState* unk454;
-    /* 0x458 */ PlayState* unk458;
+    /* 0x454 */ s32* animationIndex;
+    /* 0x458 */ s32* animationIndex2;
     /* 0x45C */ s32 unk45C;
 } DmAl; // size = 0x460
 
