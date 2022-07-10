05d34acabcba459258629aaab3f3b0b78fc70974
Anghelo Carvajal <angheloalf95@gmail.com>
2022-07-10 17:49:45 -0400

    Update src/overlays/actors/ovl_Obj_Um/z_obj_um.c
    Co-authored-by: EllipticEllipsis <elliptic.ellipsis@gmail.com>

--- src/overlays/actors/ovl_Obj_Um/z_obj_um.c
+++ src/overlays/actors/ovl_Obj_Um/z_obj_um.c
@@ -1000,7 +1000,7 @@ void ObjUm_RanchWait(ObjUm* this, PlayState* play) {
     SkelAnime_Update(&this->skelAnime);
     ObjUm_UpdateAnim(this, play, 2);
     this->flags |= OBJ_UM_FLAG_WAITING;
-    if (gSaveContext.save.time > CLOCK_TIME(18, 0) && gSaveContext.save.time <= CLOCK_TIME(19, 0)) {
+    if ((gSaveContext.save.time > CLOCK_TIME(18, 0)) && (gSaveContext.save.time <= CLOCK_TIME(19, 0))) {
         if (!(player->stateFlags1 & 0x800000)) {
             func_80B7984C(play, this, 0, &this->unk_2B4);
         }
