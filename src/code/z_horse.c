24bb845eac317e26e9e6d3c189ef318c872c66f8
angie <angheloalf95@gmail.com>
2022-07-10 23:34:40 -0400

    func_800F3940

--- src/code/z_horse.c
+++ src/code/z_horse.c
@@ -1,6 +1,23 @@
 #include "global.h"
+#include "overlays/actors/ovl_Bg_Umajump/z_bg_umajump.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_horse/func_800F3940.s")
+s32 func_800F3940(PlayState* play) {
+    Actor* actor;
+
+    for (actor = NULL; ; actor = actor->next) {
+        actor = SubS_FindActor(play, actor, ACTORCAT_PROP, ACTOR_BG_UMAJUMP);
+
+        if (actor == NULL) {
+            break;
+        }
+
+        if (actor->params == 2) {
+            return ((BgUmajump*)actor)->unk_160;
+        }
+    }
+
+    return -1;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/code/z_horse/func_800F39B4.s")
 
--- src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.h
+++ src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.h
@@ -7,7 +7,9 @@ struct BgUmajump;
 
 typedef struct BgUmajump {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x28];
+    /* 0x144 */ char unk_144[0x1C];
+    /* 0x160 */ s32 unk_160;
+    /* 0x164 */ char unk_164[0xC];
 } BgUmajump; // size = 0x16C
 
 extern const ActorInit Bg_Umajump_InitVars;
