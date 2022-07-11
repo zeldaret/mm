370f1babe67bd1ebaf0c5ba3d7fe75f7e4f1267d
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-11 08:04:40 -0300

    ovl_Dm_Char04 Decompiled

--- src/overlays/actors/ovl_Dm_Char04/z_dm_char04.h
+++ src/overlays/actors/ovl_Dm_Char04/z_dm_char04.h
@@ -7,26 +7,17 @@ struct DmChar04;
 
 typedef void (*DmChar04ActionFunc)(struct DmChar04*, PlayState*);
 
-typedef struct unkStruct {
-    f32 unk_240;
-    f32 unk_244;
-    f32 unk_248;
-    f32 unk_24C;
-} unkStruct;
-
 typedef struct DmChar04 {
     /* 0x0000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;
-    /* 0x188 */ Vec3s jointTable;
-    /* 0x18E */ char pad18E[0x54];
-    /* 0x1E2 */ Vec3s morphTable;
-    /* 0x1E8 */ char pad1E8[0x54];
+    /* 0x188 */ Vec3s jointTable[15];
+    /* 0x1E2 */ Vec3s morphTable[15];
     /* 0x023C */ DmChar04ActionFunc actionFunc;
-    /* 0x240 */ unkStruct str; // 240, 244, 248, 24C
-    /* 0x250 */ unkStruct str2; // 250, 254, 258, 25C
-    /* 0x260 */ u8 unk260;
-    /* 0x261 */ u8 unk261;
-    /* 0x262 */ u16 unk262;
+    /* 0x240 */ Color_RGBAf primColors;
+    /* 0x250 */ Color_RGBAf envColors;
+    /* 0x260 */ u8 csAction;
+    /* 0x261 */ u8 animationIndex;
+    /* 0x262 */ u16 unk_262;
 } DmChar04; // size = 0x264
 
 extern const ActorInit Dm_Char04_InitVars;
