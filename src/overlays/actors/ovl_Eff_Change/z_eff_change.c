b8020afce297c991b562ba5a86849d6762793e51
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-13 01:26:34 -0300

    all functions matching

--- src/overlays/actors/ovl_Eff_Change/z_eff_change.c
+++ src/overlays/actors/ovl_Eff_Change/z_eff_change.c
@@ -5,6 +5,7 @@
  */
 
 #include "z_eff_change.h"
+#include "objects/gameplay_keep/gameplay_keep.h"
 
 #define FLAGS (ACTOR_FLAG_10)
 
@@ -13,6 +14,10 @@
 void EffChange_Init(Actor* thisx, PlayState* play);
 void EffChange_Destroy(Actor* thisx, PlayState* play);
 void EffChange_Update(Actor* thisx, PlayState* play);
+
+void func_80A4C578(EffChange* this, s32 arg1);        /* static */
+void func_80A4C5CC(EffChange* this, PlayState* play); /* static */
+void func_80A4C7B0(Actor* thisx, PlayState* play);
 
 #if 0
 const ActorInit Eff_Change_InitVars = {
@@ -26,17 +31,137 @@ const ActorInit Eff_Change_InitVars = {
     (ActorFunc)EffChange_Update,
     (ActorFunc)NULL,
 };
-
+static struct _struct_D_80A4C920_0x6 D_80A4C920[8] = {
+    { 0xFF, 0xFF, 0xAA, 0, 0x64, 0 },
+    { 0xFF, 0xFF, 0xAA, 0xC8, 0, 0 },
+    { 0xAA, 0xFF, 0xFF, 0, 0x64, 0xFF },
+    { 0xFF, 0xFF, 0xAA, 0xC8, 0x96, 0 },
+    { 0xFF, 0xFF, 0xAA, 0, 0x64, 0 },
+    { 0xFF, 0xFF, 0xAA, 0, 0x64, 0 },
+    { 0xFF, 0xFF, 0xAA, 0, 0x64, 0 },
+    { 0xFF, 0xFF, 0xAA, 0, 0x64, 0 },
+};
 #endif
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Init.s")
+extern UNK_TYPE D_040281DC;
+extern UNK_TYPE D_04028FEC;
+extern SkeletonInfo* D_0402900C;
+extern UNK_TYPE D_80A4C920[];
+extern f32 D_80A4C950;
+extern f32 D_80A4C954;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Destroy.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Init.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C578.s")
+void EffChange_Init(Actor* thisx, PlayState* play) {
+    EffChange* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C5CC.s")
+    this->actionFunc = func_80A4C5CC;
+    this->actor.draw = func_80A4C7B0;
+    func_80A4C578(this, this->actor.params & 7);
+    Actor_SetScale(&this->actor, 0.075f);
+    this->unk1BE[3] = 0;
+    func_80183430(&this->skeletonInfo, &D_0402900C, &D_040281DC, &this->unk174, &this->unk198, NULL);
+    func_801834A8(&this->skeletonInfo, &D_040281DC);
+    this->unk1BC = 0;
+    this->actor.shape.rot.y = 0;
+    this->skeletonInfo.frameCtrl.unk_C = 0.6666667;
+    ActorCutscene_SetIntentToPlay(0x7B);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Update.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Destroy.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C7B0.s")
+void EffChange_Destroy(Actor* thisx, PlayState* play) {
+    EffChange* this = THIS;
+
+    func_8018349C(&this->skeletonInfo);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C578.s")
+
+void func_80A4C578(EffChange* this, s32 arg1) {
+    u8* temp_v0;
+    arg1 *= 6;
+    temp_v0 = &(((u8*)D_80A4C920)[arg1]);
+    this->unk1BE[0] = temp_v0[0];
+    this->unk1BE[1] = temp_v0[1];
+    this->unk1BE[2] = temp_v0[2];
+    this->unk1C2[0] = temp_v0[3];
+    this->unk1C2[1] = temp_v0[4];
+    this->unk1C2[2] = temp_v0[5];
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C5CC.s")
+
+void func_80A4C5CC(EffChange* this, PlayState* play) {
+    f32 phi_fv0;
+
+    if (func_80183DE0(&this->skeletonInfo) != 0) {
+        Actor_MarkForDeath(&this->actor);
+        ActorCutscene_Stop(0x7B);
+        func_800FD2B4(play, 0.0f, 850.0f, 0.2f, 0.0f);
+    } else {
+        this->unk1BC++;
+        if (this->skeletonInfo.frameCtrl.unk_10 < 20.0f) {
+            if ((this->unk1BE[3]) < 0xF2) {
+                this->unk1BE[3] += 0xD;
+            } else {
+                this->unk1BE[3] = 0xFF;
+            }
+        } else if (this->skeletonInfo.frameCtrl.unk_10 > 70.0f) {
+            if (((s32)this->unk1BE[3]) >= 0xE) {
+                this->unk1BE[3] -= 0xD;
+            } else {
+                this->unk1BE[3] = 0;
+            }
+        } else {
+            this->unk1BE[3] = 0xFF;
+        }
+
+        phi_fv0 = this->unk1BE[3] * 0.003921569f;
+        if (phi_fv0 > 1.0f) {
+            phi_fv0 = 1.0f;
+        } else if (phi_fv0 < 0.0f) {
+            phi_fv0 = 0.0f;
+        }
+        func_800FD2B4(play, phi_fv0, 850.0f, 0.2f, 0.0f);
+        if (ActorCutscene_GetCurrentIndex() != 0x7B) {
+            if (ActorCutscene_GetCanPlayNext(0x7B) != 0) {
+                ActorCutscene_Start(0x7B, &this->actor);
+            } else {
+                ActorCutscene_SetIntentToPlay(0x7B);
+            }
+        }
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/EffChange_Update.s")
+
+void EffChange_Update(Actor* thisx, PlayState* play) {
+    EffChange* this = THIS;
+
+    this->actionFunc(this, play);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Eff_Change/func_80A4C7B0.s")
+
+void func_80A4C7B0(Actor* thisx, PlayState* play) {
+    s32 pad;
+    Mtx* mtx;
+    EffChange* this = THIS;
+
+    AnimatedMat_DrawStepXlu(play, Lib_SegmentedToVirtual(&D_04028FEC), (u32)this->unk1BC);
+    mtx = GRAPH_ALLOC(play->state.gfxCtx, ALIGN16(this->skeletonInfo.unk_18->unk_1 * sizeof(Mtx)));
+
+    if (mtx != NULL) {
+        func_8012C2DC(play->state.gfxCtx);
+        Matrix_RotateYS((Camera_GetCamDirYaw(GET_ACTIVE_CAM(play)) + 0x8000), MTXMODE_APPLY);
+
+        OPEN_DISPS(play->state.gfxCtx);
+
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, this->unk1BE[0], this->unk1BE[1], this->unk1BE[2], this->unk1BE[3]);
+        gDPSetEnvColor(POLY_XLU_DISP++, this->unk1C2[0], this->unk1C2[1], this->unk1C2[2], 0xFF);
+        func_8018450C(play, &this->skeletonInfo, mtx, NULL, NULL, &this->actor);
+
+        CLOSE_DISPS(play->state.gfxCtx);
+    }
+}
--- src/overlays/actors/ovl_Eff_Change/z_eff_change.h
+++ src/overlays/actors/ovl_Eff_Change/z_eff_change.h
@@ -7,9 +7,25 @@ struct EffChange;
 
 typedef void (*EffChangeActionFunc)(struct EffChange*, PlayState*);
 
+struct _struct_D_80A4C920_0x6 {
+    /* 0x0 */ u8 unk0;
+    /* 0x1 */ u8 unk1;
+    /* 0x2 */ u8 unk2;
+    /* 0x3 */ u8 unk3;
+    /* 0x4 */ u8 unk4;
+    /* 0x5 */ u8 unk5;
+};
+
 typedef struct EffChange {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x84];
+    /* 0x144 */ SkeletonInfo skeletonInfo;
+    /* 0x174 */ Vec3s unk174;
+    /* 0x17A */ char pad17A[30];
+    /* 0x198 */ Vec3s unk198;
+    /* 0x19E */ char pad19E[30];
+    /* 0x1BC */ s16 unk1BC;
+    /* 0x1BE */ u8 unk1BE[4];
+    /* 0x1C2 */ u8 unk1C2[4];
     /* 0x1C8 */ EffChangeActionFunc actionFunc;
 } EffChange; // size = 0x1CC
 
