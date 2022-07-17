a5cc57c0bdfba930913d755c2acec6c042bdb93d
Derek Hensley <hensley.derek58@gmail.com>
2022-07-17 11:16:34 -0700

    z64

--- include/z64.h
+++ include/z64.h
@@ -100,13 +100,13 @@ typedef struct {
     /* 0x06 */ s16   unk_06;
     /* 0x08 */ Gfx*  opa;
     /* 0x0C */ Gfx*  xlu;
-} PolygonDlist2; // size = 0x8
+} PolygonDlist2; // size = 0x10
 
 typedef struct {
-    /* 0x00 */ u8    type;
-    /* 0x01 */ u8    num; // number of dlist entries
-    /* 0x04 */ void* start;
-    /* 0x08 */ void* end;
+    /* 0x0 */ u8    type;
+    /* 0x1 */ u8    num; // number of dlist entries
+    /* 0x4 */ void* start;
+    /* 0x8 */ void* end;
 } PolygonType2; // size = 0xC
 
 typedef struct {
@@ -121,40 +121,40 @@ typedef struct {
 } CameraStateParams; // size = 0xC
 
 typedef struct {
-    /* 0x0 */ s16 x;
-    /* 0x2 */ s16 y;
-    /* 0x4 */ s16 z;
-    /* 0x6 */ s16 reserved;
-    /* 0x8 */ s16 s;
-    /* 0xA */ s16 t;
-    /* 0xC */ s8 r;
-    /* 0xD */ s8 g;
-    /* 0xE */ s8 b;
-    /* 0xF */ s8 a;
+    /* 0x00 */ s16 x;
+    /* 0x02 */ s16 y;
+    /* 0x04 */ s16 z;
+    /* 0x06 */ s16 reserved;
+    /* 0x08 */ s16 s;
+    /* 0x0A */ s16 t;
+    /* 0x0C */ s8 r;
+    /* 0x0D */ s8 g;
+    /* 0x0E */ s8 b;
+    /* 0x0F */ s8 a;
 } F3DVertexColor; // size = 0x10
 
 typedef struct {
-    /* 0x0 */ s16 x;
-    /* 0x2 */ s16 y;
-    /* 0x4 */ s16 z;
-    /* 0x6 */ s16 reserved;
-    /* 0x8 */ s16 s;
-    /* 0xA */ s16 t;
-    /* 0xC */ s8 normalX;
-    /* 0xD */ s8 normalY;
-    /* 0xE */ s8 normalZ;
-    /* 0xF */ s8 a;
+    /* 0x00 */ s16 x;
+    /* 0x02 */ s16 y;
+    /* 0x04 */ s16 z;
+    /* 0x06 */ s16 reserved;
+    /* 0x08 */ s16 s;
+    /* 0x0A */ s16 t;
+    /* 0x0C */ s8 normalX;
+    /* 0x0D */ s8 normalY;
+    /* 0x0E */ s8 normalZ;
+    /* 0x0F */ s8 a;
 } F3DVertexNormal; // size = 0x10
 
 typedef struct {
-    /* 0x0 */ f32 size;
-    /* 0x4 */ f32 dynamicSizeStep;
-    /* 0x8 */ u8 state;
-    /* 0x9 */ u8 sizeGrowsCos2;
-    /* 0xA */ u8 colorsIndex;
-    /* 0xB */ u8 flags;
-    /* 0xC */ u8 lightParamsIndex;
-} FireObjInitParams; // size = 0xD
+    /* 0x00 */ f32 size;
+    /* 0x04 */ f32 dynamicSizeStep;
+    /* 0x08 */ u8 state;
+    /* 0x09 */ u8 sizeGrowsCos2;
+    /* 0x0A */ u8 colorsIndex;
+    /* 0x0B */ u8 flags;
+    /* 0x0C */ u8 lightParamsIndex;
+} FireObjInitParams; // size = 0x10
 
 typedef struct FireObjColors {
     /* 0x0 */ Color_RGBA8 primColor;
@@ -200,17 +200,17 @@ typedef struct {
 } GameInfo; // size = 0x15D4
 
 typedef struct {
-    /* 0x0000 */ u32    size;
-    /* 0x0004 */ void*  bufp;
-    /* 0x0008 */ void*  head;
-    /* 0x000C */ void*  tail;
+    /* 0x00 */ u32    size;
+    /* 0x04 */ void*  bufp;
+    /* 0x08 */ void*  head;
+    /* 0x0C */ void*  tail;
 } TwoHeadArena; // size = 0x10
 
 typedef struct {
-    /* 0x0000 */ u32    size;
-    /* 0x0004 */ Gfx*   bufp;
-    /* 0x0008 */ Gfx*   p;
-    /* 0x000C */ Gfx*   d;
+    /* 0x00 */ u32    size;
+    /* 0x04 */ Gfx*   bufp;
+    /* 0x08 */ Gfx*   p;
+    /* 0x0C */ Gfx*   d;
 } TwoHeadGfxArena; // size = 0x10
 
 typedef struct {
@@ -311,10 +311,10 @@ typedef struct {
 } ShrinkWindowContext; // size = 0x4
 
 typedef struct {
-    /* 0x0 */ s32 topY;
-    /* 0x4 */ s32 bottomY;
-    /* 0x8 */ s32 leftX;
-    /* 0xC */ s32 rightX;
+    /* 0x00 */ s32 topY;
+    /* 0x04 */ s32 bottomY;
+    /* 0x08 */ s32 leftX;
+    /* 0x0C */ s32 rightX;
 } Viewport; // size = 0x10
 
 typedef void(*osCreateThread_func)(void*);
@@ -715,29 +715,29 @@ typedef struct {
 } EnvironmentContext; // size = 0x100
 
 typedef struct {
-    /* 0x00000 */ View view;
-    /* 0x00168 */ void* skyboxStaticSegment[4];
-    /* 0x00178 */ void* skyboxPaletteStaticSegment;
-    /* 0x0017C */ Gfx* dListBuf;
-    /* 0x00180 */ Gfx* roomDL;
-    /* 0x00184 */ Vtx* roomVtx;
-    /* 0x00188 */ DmaRequest unk188;
-    /* 0x001A8 */ DmaRequest unk1A8;
-    /* 0x001C8 */ DmaRequest unk1C8;
-    /* 0x001E8 */ OSMesgQueue loadQueue;
-    /* 0x00200 */ OSMesg loadMsg;
-    /* 0x00204 */ s16 skyboxShouldDraw;
-    /* 0x00208 */ f32 rotX;
-    /* 0x0020C */ f32 rotY;
-    /* 0x00210 */ f32 rotZ;
-    /* 0x00214 */ Vec3f eye;
-    /* 0x00220 */ s16 angle;
-    /* 0x00222 */ u8 primR;
-    /* 0x00223 */ u8 primG;
-    /* 0x00224 */ u8 primB;
-    /* 0x00225 */ u8 envR;
-    /* 0x00226 */ u8 envG;
-    /* 0x00227 */ u8 envB;
+    /* 0x000 */ View view;
+    /* 0x168 */ void* skyboxStaticSegment[4];
+    /* 0x178 */ void* skyboxPaletteStaticSegment;
+    /* 0x17C */ Gfx* dListBuf;
+    /* 0x180 */ Gfx* roomDL;
+    /* 0x184 */ Vtx* roomVtx;
+    /* 0x188 */ DmaRequest unk188;
+    /* 0x1A8 */ DmaRequest unk1A8;
+    /* 0x1C8 */ DmaRequest unk1C8;
+    /* 0x1E8 */ OSMesgQueue loadQueue;
+    /* 0x200 */ OSMesg loadMsg;
+    /* 0x204 */ s16 skyboxShouldDraw;
+    /* 0x208 */ f32 rotX;
+    /* 0x20C */ f32 rotY;
+    /* 0x210 */ f32 rotZ;
+    /* 0x214 */ Vec3f eye;
+    /* 0x220 */ s16 angle;
+    /* 0x222 */ u8 primR;
+    /* 0x223 */ u8 primG;
+    /* 0x224 */ u8 primB;
+    /* 0x225 */ u8 envR;
+    /* 0x226 */ u8 envG;
+    /* 0x227 */ u8 envB;
 } SkyboxContext; // size = 0x228
 
 typedef enum {
@@ -745,8 +745,8 @@ typedef enum {
 } SkyboxId;
 
 typedef struct ListAlloc {
-    /* 0x00 */ struct ListAlloc* prev;
-    /* 0x04 */ struct ListAlloc* next;
+    /* 0x0 */ struct ListAlloc* prev;
+    /* 0x4 */ struct ListAlloc* next;
 } ListAlloc; // size = 0x8
 
 typedef struct {
@@ -792,9 +792,9 @@ typedef struct {
 } PreRenderParams; // size = 0x28
 
 typedef struct {
-    /* 0x00 */ u8 unk00;
-    /* 0x01 */ u8 unk01;
-} MsgCtx11F00;
+    /* 0x0 */ u8 unk00;
+    /* 0x1 */ u8 unk01;
+} MsgCtx11F00; // size = 0x2
 
 typedef struct {
     /* 0x00000 */ View view;
@@ -895,10 +895,10 @@ typedef struct FaultAddrConvClient {
 } FaultAddrConvClient; // size = 0xC
 
 typedef struct FaultClient {
-    /* 0x0 */ struct FaultClient* next;
-    /* 0x4 */ void (*callback)(void*, void*);
-    /* 0x8 */ void* param0;
-    /* 0xC */ void* param1;
+    /* 0x00 */ struct FaultClient* next;
+    /* 0x04 */ void (*callback)(void*, void*);
+    /* 0x08 */ void* param0;
+    /* 0x0C */ void* param1;
 } FaultClient; // size = 0x10
 
 typedef struct {
@@ -943,10 +943,10 @@ typedef struct {
 } GameStateOverlay; // size = 0x30
 
 typedef struct GameAllocEntry {
-    /* 0x0 */ struct GameAllocEntry* next;
-    /* 0x4 */ struct GameAllocEntry* prev;
-    /* 0x8 */ size_t size;
-    /* 0xC */ u32 unk_0C;
+    /* 0x00 */ struct GameAllocEntry* next;
+    /* 0x04 */ struct GameAllocEntry* prev;
+    /* 0x08 */ size_t size;
+    /* 0x0C */ u32 unk_0C;
 } GameAllocEntry; // size = 0x10
 
 typedef struct GameAlloc {
@@ -1108,7 +1108,7 @@ struct FireObjLight {
     /* 0x00 */ LightNode* light;
     /* 0x04 */ LightInfo lightInfo;
     /* 0x12 */ u8 lightParamsIndex;
-}; // size = 0x13
+}; // size = 0x14
 
 #define OS_SC_RETRACE_MSG       1
 #define OS_SC_DONE_MSG          2
@@ -1167,21 +1167,21 @@ struct FireObj {
 }; // size = 0x8B
 
 typedef struct {
-    /* 0x00 */ u8   seqIndex;
-    /* 0x01 */ u8   nightSeqIndex;
-    /* 0x02 */ u8   unk_02;
+    /* 0x0 */ u8   seqIndex;
+    /* 0x1 */ u8   nightSeqIndex;
+    /* 0x2 */ u8   unk_02;
 } SoundContext; // size = 0x3
 
 typedef struct {
-    /* 0x00 */ s32 enabled;
-    /* 0x04 */ s32 timer;
+    /* 0x0 */ s32 enabled;
+    /* 0x4 */ s32 timer;
 } FrameAdvanceContext; // size = 0x8
 
 typedef enum {
-    /* 00 */ GAMEOVER_INACTIVE,
-    /* 01 */ GAMEOVER_DEATH_START,
-    /* 02 */ GAMEOVER_DEATH_WAIT_GROUND,    // wait for player to fall and hit the ground
-    /* 03 */ GAMEOVER_DEATH_FADE_OUT,       // wait before fading out
+    /*  0 */ GAMEOVER_INACTIVE,
+    /*  1 */ GAMEOVER_DEATH_START,
+    /*  2 */ GAMEOVER_DEATH_WAIT_GROUND,    // wait for player to fall and hit the ground
+    /*  3 */ GAMEOVER_DEATH_FADE_OUT,       // wait before fading out
 
     /* 20 */ GAMEOVER_REVIVE_START = 20,
     /* 21 */ GAMEOVER_REVIVE_RUMBLE,
@@ -1191,8 +1191,8 @@ typedef enum {
 } GameOverState;
 
 typedef struct {
-    /* 0x00 */ u16 state;
-} GameOverContext; // size = 0x02
+    /* 0x0 */ u16 state;
+} GameOverContext; // size = 0x2
 
 struct PlayState {
     /* 0x00000 */ GameState state;
@@ -1342,9 +1342,9 @@ typedef struct {
 } TargetRangeParams; // size = 0x8
 
 typedef struct {
-    /* 0x00 */ u8* value;
-    /* 0x04 */ const char* name;
-} FlagSetEntry; // size = 0x08
+    /* 0x0 */ u8* value;
+    /* 0x4 */ const char* name;
+} FlagSetEntry; // size = 0x8
 
 // TODO: Dedicated Header?
 #define FRAM_BASE_ADDRESS 0x08000000           // FRAM Base Address in Cart Memory
