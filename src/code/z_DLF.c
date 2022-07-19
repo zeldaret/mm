9ae892374c3a9b12d0b28b4e123dbd8ab4e9b6d9
louist103 <louist103@gmail.com>
2022-07-19 15:25:56 -0400

    matched both

--- src/code/z_DLF.c
+++ src/code/z_DLF.c
@@ -2,6 +2,113 @@
 #include "system_malloc.h"
 #include "z64load.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_DLF/Overlay_LoadGameState.s")
+void Overlay_LoadGameState(GameStateOverlay* gameState) {
+    void* temp;
+    void* vrStart;
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/z_DLF/Overlay_FreeGameState.s")
+    if (gameState->loadedRamAddr != NULL) {
+        return;
+    }
+    vrStart = gameState->vramStart;
+    if (vrStart == NULL) {
+        gameState->unk_28 = 0;
+    } else {
+        gameState->loadedRamAddr = Load2_AllocateAndLoad(gameState->vromStart, gameState->vromEnd, (uintptr_t)vrStart,
+                                                         (uintptr_t)gameState->vramEnd);
+        if (gameState->loadedRamAddr == NULL) {
+            return;
+        }
+
+        if (gameState->unk_14 != NULL) {
+            temp = (void*)((uintptr_t)gameState->unk_14 -
+                           (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+        } else {
+            temp = NULL;
+        }
+        gameState->unk_14 = temp;
+        if (gameState->init != NULL) {
+            temp = (void*)((uintptr_t)gameState->init -
+                           (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+        } else {
+            temp = NULL;
+        }
+        gameState->init = temp;
+
+        if (gameState->destroy != NULL) {
+            temp = (void*)((uintptr_t)gameState->destroy -
+                           (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+        } else {
+            temp = NULL;
+        }
+        gameState->destroy = temp;
+
+        if (gameState->unk_20 != NULL) {
+            temp = (void*)((uintptr_t)gameState->unk_20 -
+                           (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+        } else {
+            temp = NULL;
+        }
+        gameState->unk_20 = temp;
+
+        if (gameState->unk_24 != NULL) {
+            temp = (void*)((uintptr_t)gameState->unk_24 -
+                           (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+        } else {
+            temp = NULL;
+        }
+        gameState->unk_24 = temp;
+
+        gameState->unk_28 = 0;
+    }
+}
+
+void Overlay_FreeGameState(GameStateOverlay* gameState) {
+    s32 var_v0;
+    void* temp;
+
+    if (gameState->loadedRamAddr != NULL) {
+        var_v0 = gameState->unk_28 != 0 ? -1 : 0;
+        if (var_v0 == 0) {
+            if (gameState->unk_14 != NULL) {
+                temp = (void*)((uintptr_t)gameState->unk_14 +
+                               (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+            } else {
+                temp = NULL;
+            }
+            gameState->unk_14 = temp;
+            if (gameState->init != NULL) {
+                temp = (void*)((uintptr_t)gameState->init +
+                               (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+            } else {
+                temp = NULL;
+            }
+            gameState->init = temp;
+
+            if (gameState->destroy != NULL) {
+                temp = (void*)((uintptr_t)gameState->destroy +
+                               (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+            } else {
+                temp = NULL;
+            }
+            gameState->destroy = temp;
+
+            if (gameState->unk_20 != NULL) {
+                temp = (void*)((uintptr_t)gameState->unk_20 +
+                               (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+            } else {
+                temp = NULL;
+            }
+            gameState->unk_20 = temp;
+
+            if (gameState->unk_24 != NULL) {
+                temp = (void*)((uintptr_t)gameState->unk_24 +
+                               (intptr_t)((uintptr_t)gameState->vramStart - (uintptr_t)gameState->loadedRamAddr));
+            } else {
+                temp = NULL;
+            }
+            gameState->unk_24 = temp;
+            SystemArena_Free(gameState->loadedRamAddr);
+            gameState->loadedRamAddr = NULL;
+        }
+    }
+}
