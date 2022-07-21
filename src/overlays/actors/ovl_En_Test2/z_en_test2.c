c947a2b40b687a93b0585374f1a21dd725dec567
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-21 16:29:27 -0300

    data problem

--- src/overlays/actors/ovl_En_Test2/z_en_test2.c
+++ src/overlays/actors/ovl_En_Test2/z_en_test2.c
@@ -13,10 +13,16 @@
 void EnTest2_Init(Actor* thisx, PlayState* play);
 void EnTest2_Update(Actor* thisx, PlayState* play);
 
-void func_80A3E4EC(Actor* actor, PlayState* play);
+void func_80A3E4EC(Actor* thisx, PlayState* play);
 void func_80A3E524(Actor* thisx, PlayState* play);
 
-#if 0
+typedef struct UnkStruct {
+    Gfx* unk0;
+    Gfx* unk4;
+    AnimatedMaterial* unk8;
+} UnkStruct;
+
+#if 1
 const ActorInit En_Test2_InitVars = {
     ACTOR_EN_TEST2,
     ACTORCAT_BG,
@@ -29,6 +35,23 @@ const ActorInit En_Test2_InitVars = {
     (ActorFunc)NULL,
 };
 
+// UnkStruct* D_80A3E6B0 = 0x06000040;
+UnkStruct D_80A3E6B0[] = {
+    { (Gfx*)0x06000040, NULL, NULL },
+    { (Gfx*)0x06001820, NULL, NULL },
+    { (Gfx*)0x060008C0, NULL, NULL },
+    { (Gfx*)0x060016D8, NULL, (AnimatedMaterial*)0x060017A8 },
+    { (Gfx*)0x06002018, NULL, (AnimatedMaterial*)0x060020E8 },
+    { (Gfx*)0x06005268, NULL, (AnimatedMaterial*)0x06005338 },
+    { (Gfx*)0x06000110, (Gfx*)0x06000080, NULL },
+    { (Gfx*)0x06000F70, NULL, NULL },
+    { (Gfx*)0x06001200, NULL, NULL },
+    { (Gfx*)0x06004928, NULL, (AnimatedMaterial*)0x060049F8 },
+    { (Gfx*)0x06002978, NULL, (AnimatedMaterial*)0x06002A58 },
+    { (Gfx*)0x06000D38, NULL, (AnimatedMaterial*)0x06000E48 },
+    { (Gfx*)0x060035A8, NULL, (AnimatedMaterial*)0x06003888 },
+};
+
 // static InitChainEntry sInitChain[] = {
 static InitChainEntry D_80A3E74C[] = {
     ICHAIN_VEC3F(scale, 1, ICHAIN_CONTINUE),
@@ -36,34 +59,19 @@ static InitChainEntry D_80A3E74C[] = {
     ICHAIN_F32(uncullZoneScale, 800, ICHAIN_CONTINUE),
     ICHAIN_F32(uncullZoneDownward, 2500, ICHAIN_STOP),
 };
-s32 D_80A3E6B0 = 0x06000040;
-static s16 D_80A3E75C[0x12] = {
-    0x180,
-    0x17F,
-    0x76,
-    0x190,
-    0x190,
-    0x190,
-    0x1BC,
-    0x1E0,
-    0x1E0,
-    0x190,
-    0x190,
-    0x190,
-    0x190,
-    0,
-    0,
-    0,
-    0,
-    0,
+
+static s16 D_80A3E75C[] = {
+    OBJECT_DEKUCITY_ANA_OBJ, OBJECT_SICHITAI_OBJ,  OBJECT_YUKIMURA_OBJ, OBJECT_HAKUGIN_OBJ, OBJECT_HAKUGIN_OBJ,
+    OBJECT_HAKUGIN_OBJ,      OBJECT_MEGANEANA_OBJ, OBJECT_HAKA_OBJ,     OBJECT_HAKA_OBJ,    OBJECT_HAKUGIN_OBJ,
+    OBJECT_HAKUGIN_OBJ,      OBJECT_HAKUGIN_OBJ,   OBJECT_HAKUGIN_OBJ,
 };
 
 #endif
 
-extern InitChainEntry D_80A3E74C[];
-extern u32 D_80A3E6B0[][3];
-extern s16 D_80A3E75C[];
-extern s32 D_80A3E6B4[][3];
+ //extern InitChainEntry D_80A3E74C[];
+ //extern UnkStruct D_80A3E6B0[][];
+ //extern s16 D_80A3E75C[];
+ //extern s32 D_80A3E6B4[][];
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Test2/EnTest2_Init.s")
 
@@ -81,8 +89,8 @@ void EnTest2_Init(Actor* thisx, PlayState* play) {
 void EnTest2_Update(Actor* thisx, PlayState* play) {
     s32 pad;
     s32 objectIndex;
-    u32* temp_v0_2;
-    EnTest2* this = (EnTest2*)thisx;
+    UnkStruct* temp_v0_2;
+    EnTest2* this = THIS;
     objectIndex = Object_GetIndex(&play->objectCtx, D_80A3E75C[this->actor.params]);
 
     if (objectIndex < 0) {
@@ -90,12 +98,12 @@ void EnTest2_Update(Actor* thisx, PlayState* play) {
         return;
     }
     if (Object_IsLoaded(&play->objectCtx, objectIndex) != 0) {
-        temp_v0_2 = D_80A3E6B0[this->actor.params];
+        temp_v0_2 = &D_80A3E6B0[this->actor.params].unk0;
         this->actor.objBankIndex = objectIndex;
         this->actor.draw = func_80A3E524;
-        if ((temp_v0_2[2]) != 0) {
+        if ((temp_v0_2->unk8) != 0) {
             Actor_SetObjectDependency(play, &this->actor);
-            this->unk144 = Lib_SegmentedToVirtual(temp_v0_2[2]);
+            this->unk144 = Lib_SegmentedToVirtual(temp_v0_2->unk8);
         }
         if (play->roomCtx.currRoom.unk5 != 0) {
             this->actor.update = func_80A3E4EC;
@@ -120,7 +128,7 @@ void func_80A3E4EC(Actor* thisx, PlayState* play) {
 void func_80A3E524(Actor* thisx, PlayState* play) {
     s32 pad;
     EnTest2* this = (EnTest2*)thisx;
-    Gfx* temp_a3 = D_80A3E6B0[this->actor.params][1];
+    UnkStruct* temp_a3 = D_80A3E6B0[this->actor.params].unk0;
 
     if (this->unk144 != NULL) {
         AnimatedMat_Draw(play, this->unk144);
@@ -134,12 +142,12 @@ void func_80A3E524(Actor* thisx, PlayState* play) {
         if (temp_a3 != 0) {
             gSPDisplayList(POLY_XLU_DISP++, temp_a3);
         }
-        gSPDisplayList(POLY_XLU_DISP++, D_80A3E6B0[this->actor.params][0]);
+        gSPDisplayList(POLY_XLU_DISP++, D_80A3E6B0[this->actor.params].unk0);
         CLOSE_DISPS(play->state.gfxCtx);
         return;
     }
     if (temp_a3 != 0) {
         Gfx_DrawDListXlu(play, temp_a3);
     }
-    Gfx_DrawDListOpa(play, D_80A3E6B0[this->actor.params][0]);
+    Gfx_DrawDListOpa(play, D_80A3E6B0[this->actor.params].unk0);
 }
