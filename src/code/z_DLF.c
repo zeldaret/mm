2b3185d13af911bd7e481fbbd69d62faa15b5dab
louist103 <louist103@gmail.com>
2022-07-21 13:29:15 -0400

    PR fixes and change other overlay loading functions

--- src/code/z_DLF.c
+++ src/code/z_DLF.c
@@ -3,75 +3,71 @@
 #include "z64load.h"
 
 void Overlay_LoadGameState(GameStateOverlay* gameState) {
-    void* temp;
-    void* vrStart;
+    void* vramStart;
 
     if (gameState->loadedRamAddr != NULL) {
         return;
     }
-    vrStart = gameState->vramStart;
-    if (vrStart == NULL) {
+    vramStart = gameState->vramStart;
+    if (vramStart == NULL) {
         gameState->unk_28 = 0;
-    } else {
-        gameState->loadedRamAddr = Load2_AllocateAndLoad(gameState->vromStart, gameState->vromEnd, (uintptr_t)vrStart,
-                                                         (uintptr_t)gameState->vramEnd);
-        if (gameState->loadedRamAddr == NULL) {
-            return;
-        }
+        return;
+    }
+    gameState->loadedRamAddr = Load2_AllocateAndLoad(gameState->vromStart, gameState->vromEnd, (uintptr_t)vramStart,
+                                                     (uintptr_t)gameState->vramEnd);
+    if (gameState->loadedRamAddr != NULL) {
 
-        gameState->unk_14 = (uintptr_t)((gameState->unk_14 != NULL) ? (void*)(-OVERLAY_RELOCATION_OFFSET(gameState) + (uintptr_t)gameState->unk_14) : NULL);
-        gameState->init = (uintptr_t)((gameState->init != NULL) ? (void*)(-OVERLAY_RELOCATION_OFFSET(gameState) + (uintptr_t)gameState->init) : NULL);
-        gameState->destroy = (uintptr_t)((gameState->destroy != NULL) ? (void*)(-OVERLAY_RELOCATION_OFFSET(gameState) + (uintptr_t)gameState->destroy) : NULL);
-        gameState->unk_20 = (uintptr_t)((gameState->unk_20 != NULL) ? (void*)(-OVERLAY_RELOCATION_OFFSET(gameState) + (uintptr_t)gameState->unk_20) : NULL);
-        gameState->unk_24 = (uintptr_t)((gameState->unk_24 != NULL) ? (void*)(-OVERLAY_RELOCATION_OFFSET(gameState) + (uintptr_t)gameState->unk_24) : NULL);
+        gameState->unk_14 =
+            (uintptr_t)((gameState->unk_14 != NULL)
+                            ? (void*)((uintptr_t)gameState->unk_14 - (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                            : NULL);
+        gameState->init =
+            (uintptr_t)((gameState->init != NULL)
+                            ? (void*)((uintptr_t)gameState->init - (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                            : NULL);
+        gameState->destroy =
+            (uintptr_t)((gameState->destroy != NULL)
+                            ? (void*)((uintptr_t)gameState->destroy - (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                            : NULL);
+        gameState->unk_20 =
+            (uintptr_t)((gameState->unk_20 != NULL)
+                            ? (void*)((uintptr_t)gameState->unk_20 - (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                            : NULL);
+        gameState->unk_24 =
+            (uintptr_t)((gameState->unk_24 != NULL)
+                            ? (void*)((uintptr_t)gameState->unk_24 - (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                            : NULL);
 
         gameState->unk_28 = 0;
     }
 }
 
-// This is probably a fake match
-// https://decomp.me/scratch/qZKzC
-
 void Overlay_FreeGameState(GameStateOverlay* gameState) {
     s32 var_v0;
-    void* temp;
 
     if (gameState->loadedRamAddr != NULL) {
         var_v0 = gameState->unk_28 != 0 ? -1 : 0;
         if (var_v0 == 0) {
-            if (gameState->unk_14 != NULL) {
-                temp = (void*)((uintptr_t)gameState->unk_14 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState));
-            } else {
-                temp = NULL;
-            }
-            gameState->unk_14 = temp;
-            if (gameState->init != NULL) {
-                temp = (void*)((uintptr_t)gameState->init + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState));
-            } else {
-                temp = NULL;
-            }
-            gameState->init = temp;
-
-            if (gameState->destroy != NULL) {
-                temp = (void*)((uintptr_t)gameState->destroy + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState));
-            } else {
-                temp = NULL;
-            }
-            gameState->destroy = temp;
-
-            if (gameState->unk_20 != NULL) {
-                temp = (void*)((uintptr_t)gameState->unk_20 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState));
-            } else {
-                temp = NULL;
-            }
-            gameState->unk_20 = temp;
-
-            if (gameState->unk_24 != NULL) {
-                temp = (void*)((uintptr_t)gameState->unk_24 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState));
-            } else {
-                temp = NULL;
-            }
-            gameState->unk_24 = temp;
+            gameState->unk_14 =
+                (uintptr_t)((gameState->unk_14 != NULL)
+                                ? (void*)((uintptr_t)gameState->unk_14 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                                : NULL);
+            gameState->init =
+                (uintptr_t)((gameState->init != NULL)
+                                ? (void*)((uintptr_t)gameState->init + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                                : NULL);
+            gameState->destroy = (uintptr_t)(
+                (gameState->destroy != NULL)
+                    ? (void*)((uintptr_t)gameState->destroy + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                    : NULL);
+            gameState->unk_20 =
+                (uintptr_t)((gameState->unk_20 != NULL)
+                                ? (void*)((uintptr_t)gameState->unk_20 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                                : NULL);
+            gameState->unk_24 =
+                (uintptr_t)((gameState->unk_24 != NULL)
+                                ? (void*)((uintptr_t)gameState->unk_24 + (intptr_t)OVERLAY_RELOCATION_OFFSET(gameState))
+                                : NULL);
             SystemArena_Free(gameState->loadedRamAddr);
             gameState->loadedRamAddr = NULL;
         }
