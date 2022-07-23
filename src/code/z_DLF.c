07c9c0690c3edafc3abf356aeab249e862732848
louist103 <louist103@gmail.com>
2022-07-23 14:11:30 -0400

    re format

--- src/code/z_DLF.c
+++ src/code/z_DLF.c
@@ -17,26 +17,26 @@ void Overlay_LoadGameState(GameStateOverlay* overlayEntry) {
                                                         (uintptr_t)vramStart, (uintptr_t)overlayEntry->vramEnd);
     if (overlayEntry->loadedRamAddr != NULL) {
 
-        overlayEntry->unk_14 =
-            (uintptr_t)((overlayEntry->unk_14 != NULL)
-                            ? (void*)((uintptr_t)overlayEntry->unk_14 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
-                            : NULL);
+        overlayEntry->unk_14 = (uintptr_t)(
+            (overlayEntry->unk_14 != NULL)
+                ? (void*)((uintptr_t)overlayEntry->unk_14 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
+                : NULL);
         overlayEntry->init =
             (uintptr_t)((overlayEntry->init != NULL)
                             ? (void*)((uintptr_t)overlayEntry->init - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
                             : NULL);
-        overlayEntry->destroy =
-            (uintptr_t)((overlayEntry->destroy != NULL)
-                            ? (void*)((uintptr_t)overlayEntry->destroy - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
-                            : NULL);
-        overlayEntry->unk_20 =
-            (uintptr_t)((overlayEntry->unk_20 != NULL)
-                            ? (void*)((uintptr_t)overlayEntry->unk_20 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
-                            : NULL);
-        overlayEntry->unk_24 =
-            (uintptr_t)((overlayEntry->unk_24 != NULL)
-                            ? (void*)((uintptr_t)overlayEntry->unk_24 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
-                            : NULL);
+        overlayEntry->destroy = (uintptr_t)(
+            (overlayEntry->destroy != NULL)
+                ? (void*)((uintptr_t)overlayEntry->destroy - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
+                : NULL);
+        overlayEntry->unk_20 = (uintptr_t)(
+            (overlayEntry->unk_20 != NULL)
+                ? (void*)((uintptr_t)overlayEntry->unk_20 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
+                : NULL);
+        overlayEntry->unk_24 = (uintptr_t)(
+            (overlayEntry->unk_24 != NULL)
+                ? (void*)((uintptr_t)overlayEntry->unk_24 - (intptr_t)OVERLAY_RELOCATION_OFFSET(overlayEntry))
+                : NULL);
 
         overlayEntry->unk_28 = 0;
     }
