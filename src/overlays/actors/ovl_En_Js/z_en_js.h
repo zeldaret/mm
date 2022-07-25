afd4f8e79580bd5cc58dcc0581af922133f78a60
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-25 14:14:11 -0300

    remove comments

--- src/overlays/actors/ovl_En_Js/z_en_js.h
+++ src/overlays/actors/ovl_En_Js/z_en_js.h
@@ -9,14 +9,14 @@ typedef void (*EnJsActionFunc)(struct EnJs*, PlayState*);
 
 typedef struct EnJs {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ SkelAnime skelAnime;                   /* inferred */
-    /* 0x188 */ ColliderCylinder collider;            /* inferred */
-    /* 0x1D4 */ Vec3s unk1D4;                       /* inferred */
-    /* 0x1DA */ char pad1DA[0x66];                  /* maybe part of unk1D4[0x12]? */
-    /* 0x240 */ Vec3s unk240;                       /* inferred */
-    /* 0x246 */ char pad246[0x66];                  /* maybe part of unk240[0x12]? */
-    /* 0x2AC */ Path *unk2AC;                       /* inferred */
-    /* 0x2B0 */ s32 unk2B0;                         /* inferred */
+    /* 0x144 */ SkelAnime skelAnime;        
+    /* 0x188 */ ColliderCylinder collider;  
+    /* 0x1D4 */ Vec3s unk1D4;               
+    /* 0x1DA */ char pad1DA[0x66];          
+    /* 0x240 */ Vec3s unk240;               
+    /* 0x246 */ char pad246[0x66];          
+    /* 0x2AC */ Path *unk2AC;               
+    /* 0x2B0 */ s32 unk2B0;                 
     /* 0x2B4 */ f32 unk2B4;
     /* 0x2B8 */ u16 unk2B8;
     /* 0x2BA */ s16 unk2BA;
