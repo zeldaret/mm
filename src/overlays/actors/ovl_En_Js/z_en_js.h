9d5887fec978df4aaa0b8ffefe370cf4d4b5d18a
sonicdcer <alejandro.asenjo88@gmail.com>
2022-07-26 17:26:29 -0300

    final details

--- src/overlays/actors/ovl_En_Js/z_en_js.h
+++ src/overlays/actors/ovl_En_Js/z_en_js.h
@@ -2,6 +2,11 @@
 #define Z_EN_JS_H
 
 #include "global.h"
+#include "objects/object_ob/object_ob.h"
+
+#define ENJS_GET_F(thisx) ((thisx)->params & 0xF)
+#define ENJS_GET_FC00(thisx) ((thisx)->params & 0xFC00)
+#define ENJS_GET_3F0(thisx)  (((thisx)->params & 0x3F0) >> 4)
 
 struct EnJs;
 
@@ -11,13 +16,13 @@ typedef struct EnJs {
     /* 0x000 */ Actor actor;
     /* 0x144 */ SkelAnime skelAnime;
     /* 0x188 */ ColliderCylinder collider;
-    /* 0x1D4 */ Vec3s jointTable[18];
-    /* 0x240 */ Vec3s morphTable[18];
-    /* 0x2AC */ Path* unk_2AC;
+    /* 0x1D4 */ Vec3s jointTable[OBJECT_OB_LIMB_MAX];
+    /* 0x240 */ Vec3s morphTable[OBJECT_OB_LIMB_MAX];
+    /* 0x2AC */ Path* path;
     /* 0x2B0 */ s32 unk_2B0;
     /* 0x2B4 */ f32 unk_2B4;
     /* 0x2B8 */ u16 unk_2B8;
-    /* 0x2BA */ s16 unk_2BA;
+    /* 0x2BA */ s16 maskDlSelector;
     /* 0x2BC */ s16 unk_2BC;
     /* 0x2BE */ s16 cutscenes[2];
     /* 0x2C2 */ s16 cutsceneIndex;
