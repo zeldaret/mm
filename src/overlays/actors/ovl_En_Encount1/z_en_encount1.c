de0362db272a90f8172ed32080c58804bb39fb7f
sonicdcer <alejandro.asenjo88@gmail.com>
2022-08-01 02:16:42 -0300

    all functions matching

--- src/overlays/actors/ovl_En_Encount1/z_en_encount1.c
+++ src/overlays/actors/ovl_En_Encount1/z_en_encount1.c
@@ -67,78 +67,81 @@ void EnEncount1_Init(Actor* thisx, PlayState* play) {
     this->actionFunc = func_808E0954;
 }
 
-void func_808E0954(EnEncount1* this, PlayState* play) {
-    Player* sp74 = GET_PLAYER(play);
-    Vec3f sp68;
+void func_808E0954(EnEncount1 *this, PlayState *play) {
+    Player *sp74 = (Player *) play->actorCtx.actorLists[ACTORCAT_PLAYER].first;
+    Vec3f spawnPos;
     f32 sp64;
+    f32 temp_fv0_2;
     s16 sp5E;
+    s32 temp;
     CollisionPoly* sp54;
     s32 sp50;
-    f32 sp48;
-    Path* temp_a0_3;
-    f32 temp_fv0_2;
-    f32 temp_fv0_3;
 
-    if ((((this->unk_14E < this->unk14C) &&
-          ((!(this->unk160 > 0.0f)) || (!(this->unk160 < this->actor.xzDistToPlayer)))) &&
-         ((this->unk154 <= 0) || (this->unk152 < this->unk154))) &&
-        ((this->unk156 == 0) || ((this->unk156 = this->unk156 + 1, (this->unk156 < this->unk158) == 0)))) {
-        this->unk156 = 0;
-        switch (this->unk150) {
-            case 0:
-                sp64 = randPlusMinusPoint5Scaled(40.0f) + 200.0f;
-                sp5E = sp74->actor.shape.rot.y;
-                if (this->unk_14E & 1) {
-                    sp5E = sp74->actor.shape.rot.y * (-1);
-                    sp64 = randPlusMinusPoint5Scaled(20.0f) + 100.0f;
-                }
-                sp48 = Math_SinS(sp5E);
-                sp68.x = randPlusMinusPoint5Scaled(40.0f) + (sp74->actor.world.pos.x + (sp48 * sp64));
-                sp68.y = sp74->actor.floorHeight + 120.0f;
-                sp48 = Math_CosS(sp5E);
-                sp68.z = randPlusMinusPoint5Scaled(40.0f) + (sp74->actor.world.pos.z + (sp48 * sp64));
-                temp_fv0_2 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &sp68);
-                if ((!(temp_fv0_2 <= (-32000.0f))) &&
-                    (((sp74->actor.depthInWater == (-32000.0f))) ||
-                     (!(temp_fv0_2 < (sp74->actor.world.pos.y - sp74->actor.depthInWater))))) {
-                    sp68.y = temp_fv0_2;
-                }
-                break;
-
-            case 1:
-                Math_Vec3f_Copy(&sp68, &sp74->actor.world.pos);
-                break;
-
-            case 2:
-                sp64 = randPlusMinusPoint5Scaled(250.0f) + 500.0f;
-                sp5E = sp74->actor.shape.rot.y;
-                sp48 = Math_SinS(sp5E);
-                sp68.x = randPlusMinusPoint5Scaled(40.0f) + (sp74->actor.world.pos.x + (sp48 * sp64));
-                sp68.y = sp74->actor.world.pos.y - Rand_ZeroFloat(20.0f);
-                sp48 = Math_CosS(sp5E);
-                sp68.z = randPlusMinusPoint5Scaled(40.0f) + (sp74->actor.world.pos.z + (sp48 * sp64));
-                temp_fv0_3 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &sp68);
-                if ((((sp74->stateFlags1 & 0x08000000)) && (!(temp_fv0_3 <= (-32000.0f)))) &&
-                    (!(sp74->actor.depthInWater < temp_fv0_3))) {
-                    break;
-
-                    case 3:
-                        temp_a0_3 = this->unk148;
-                        if ((temp_a0_3 != NULL) && (SubS_CopyPointFromPath(temp_a0_3, 0, &sp68) == 0)) {
-                            Actor_MarkForDeath(&this->actor);
-                        }
-                }
-            default:
-                if (Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, D_808E0DF0[this->unk150], sp68.x, sp68.y,
-                                       sp68.z, 0, 0, 0, D_808E0DF8[this->unk150]) != NULL) {
-                    this->unk_14E += 1;
-                    if (this->unk154 > 0) {
-                        this->unk152 += 1;
-                    }
-                }
-                if ((this->unk_14E >= this->unk14C) && (this->unk158 != 0)) {
-                    this->unk156 = 1;
-                }
+    if (((this->unk_14E >= this->unk14C) ||
+        ((0.0f < this->unk160) && (this->unk160 < this->actor.xzDistToPlayer)) ||
+        ((0 < this->unk154 ) && (this->unk154 <= this->unk152)))) {
+        return;
+    } else if(this->unk156 != 0) {
+        this->unk156++;
+        if (this->unk156 < this->unk158) {
+            return;
+        }
+    }
+    this->unk156 = 0;
+    switch (this->unk150) {
+        case 0:
+            sp64 = randPlusMinusPoint5Scaled(40.0f) + 200.0f;
+            sp5E = sp74->actor.shape.rot.y;
+            if (this->unk_14E & 1) {
+                sp5E = -sp5E;
+                sp64 = randPlusMinusPoint5Scaled(20.0f) + 100.0f;
+            }
+            spawnPos.x = sp74->actor.world.pos.x + (Math_SinS(sp5E) * sp64) + randPlusMinusPoint5Scaled(40.0f);
+            spawnPos.y = sp74->actor.floorHeight + 120.0f;
+            spawnPos.z = sp74->actor.world.pos.z + (Math_CosS(sp5E) * sp64)+ randPlusMinusPoint5Scaled(40.0f);
+            temp_fv0_2 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &spawnPos);
+            if ((temp_fv0_2 <= -32000.0f) ||
+                ((sp74->actor.depthInWater != -32000.0f) &&
+                 (temp_fv0_2 < (sp74->actor.world.pos.y - sp74->actor.depthInWater)))) {
+                return;
+            }
+            spawnPos.y = temp_fv0_2;
+            break;
+        
+        case 1:
+            Math_Vec3f_Copy(&spawnPos, &sp74->actor.world.pos);
+            break;
+        
+        case 2:
+            sp64 = randPlusMinusPoint5Scaled(250.0f) + 500.0f;
+            sp5E = sp74->actor.shape.rot.y;
+            spawnPos.x = sp74->actor.world.pos.x + Math_SinS(sp5E) * sp64 + randPlusMinusPoint5Scaled(40.0f);
+            spawnPos.y = sp74->actor.world.pos.y - Rand_ZeroFloat(20.0f);
+            spawnPos.z = sp74->actor.world.pos.z + (Math_CosS(sp5E) * sp64) + randPlusMinusPoint5Scaled(40.0f);
+            temp_fv0_2 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &spawnPos);
+            if ((!(sp74->stateFlags1 & 0x08000000) || (temp_fv0_2 <= (-32000.0f))||
+                (sp74->actor.depthInWater < temp_fv0_2))) {
+                return;
+            }
+            break;
+        
+        case 3:
+            if ((this->unk148 != NULL) && (SubS_CopyPointFromPath(this->unk148, 0, &spawnPos) == 0)) {
+                Actor_MarkForDeath(&this->actor);
+            }
+            break;
+    }
+    
+    temp = D_808E0DF8[this->unk150];
+    if (Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, D_808E0DF0[this->unk150], spawnPos.x, spawnPos.y,
+                           spawnPos.z, 0, 0, 0, temp) != NULL) {
+        this->unk_14E++;
+        if (this->unk154 > 0) {
+            this->unk152++;
+        }
+    
+        if ((this->unk_14E >= this->unk14C) && (this->unk158 != 0)) {
+            this->unk156 = 1;
         }
     }
 }
@@ -149,5 +152,4 @@ void EnEncount1_Update(Actor* thisx, PlayState* play) {
     EnEncount1* this = THIS;
 
     this->actionFunc(this, play);
-    return;
 }
