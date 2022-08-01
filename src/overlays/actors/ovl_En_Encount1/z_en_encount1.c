82ffa64d02e1946ad506bfb2e1f42743546265eb
sonicdcer <alejandro.asenjo88@gmail.com>
2022-08-01 16:27:40 -0300

    Documenting

--- src/overlays/actors/ovl_En_Encount1/z_en_encount1.c
+++ src/overlays/actors/ovl_En_Encount1/z_en_encount1.c
@@ -27,7 +27,12 @@ const ActorInit En_Encount1_InitVars = {
     (ActorFunc)NULL,
 };
 
-static s16 sActorList[] = { ACTOR_EN_GRASSHOPPER, ACTOR_EN_WALLMAS, ACTOR_EN_PR2, ACTOR_EN_PR2 };
+static s16 sActorList[] = {
+    ACTOR_EN_GRASSHOPPER,
+    ACTOR_EN_WALLMAS,
+    ACTOR_EN_PR2,
+    ACTOR_EN_PR2,
+};
 
 static s16 sActorParams[] = { 1, 0, 1, 3 };
 
@@ -39,53 +44,53 @@ void EnEncount1_Init(Actor* thisx, PlayState* play) {
         return;
     }
 
-    this->unk150 = (this->actor.params >> 0xB) & 0x1F;
-    this->unk14C = (this->actor.params >> 6) & 0x1F;
-    this->unk154 = ENENCOUNT1_GET_3F(&this->actor);
-    this->unk158 = this->actor.world.rot.x;
-    this->unk15C = this->actor.world.rot.y;
-    this->unk160 = (this->actor.world.rot.z * 40.0f) + 120.0f;
+    this->actorType = ENENCOUNT1_GET_TYPE(&this->actor);
+    this->unk_14C = ENENCOUNT1_GET_7C0(&this->actor);
+    this->unk_154 = ENENCOUNT1_GET_PATH(&this->actor);
+    this->unk_158 = this->actor.world.rot.x;
+    this->unk_15C = this->actor.world.rot.y;
+    this->unk_160 = (this->actor.world.rot.z * 40.0f) + 120.0f;
 
-    if (this->unk154 >= 0x3F) {
-        this->unk154 = -1;
+    if (this->unk_154 >= 0x3F) {
+        this->unk_154 = -1;
     }
     if (this->actor.world.rot.z < 0) {
-        this->unk160 = -1.0f;
+        this->unk_160 = -1.0f;
     }
-    if (this->unk150 == 3) {
-        this->unk_15A = ENENCOUNT1_GET_3F(&this->actor);
-        this->unk148 = SubS_GetPathByIndex(play, this->unk_15A, 0x3F);
-        this->unk154 = -1;
-        this->unk160 = -1.0f;
+    if (this->actorType == SKULLFISH_2) {
+        this->unk_15A = ENENCOUNT1_GET_PATH(&this->actor);
+        this->path = SubS_GetPathByIndex(play, this->unk_15A, 0x3F);
+        this->unk_154 = -1;
+        this->unk_160 = -1.0f;
     }
-    this->actor.flags &= -2;
+    this->actor.flags &= ~ACTOR_FLAG_1;
     this->actionFunc = func_808E0954;
 }
 
-void func_808E0954(EnEncount1 *this, PlayState *play) {
-    Player *player = GET_PLAYER(play);
+void func_808E0954(EnEncount1* this, PlayState* play) {
+    Player* player = GET_PLAYER(play);
     Vec3f spawnPos;
     f32 sp64;
     f32 temp_fv0_2;
     s16 sp5E;
+    s16 actorList;
     s32 actorParams;
     CollisionPoly* sp54;
     s32 sp50;
 
-    if (((this->unk_14E >= this->unk14C) ||
-        ((0.0f < this->unk160) && (this->unk160 < this->actor.xzDistToPlayer)) ||
-        ((0 < this->unk154) && (this->unk154 <= this->unk152)))) {
+    if (((this->unk_14E >= this->unk_14C) || ((0.0f < this->unk_160) && (this->unk_160 < this->actor.xzDistToPlayer)) ||
+         ((0 < this->unk_154) && (this->unk_154 <= this->unk_152)))) {
         return;
-    } else if(this->unk156 != 0) {
-        this->unk156++;
-        if (this->unk156 < this->unk158) {
+    } else if (this->unk_156 != 0) {
+        this->unk_156++;
+        if (this->unk_156 < this->unk_158) {
             return;
         }
     }
 
-    this->unk156 = 0;
-    switch (this->unk150) {
-        case 0:
+    this->unk_156 = 0;
+    switch (this->actorType) {
+        case GRASSHOPPER:
             sp64 = randPlusMinusPoint5Scaled(40.0f) + 200.0f;
             sp5E = player->actor.shape.rot.y;
             if (this->unk_14E & 1) {
@@ -94,50 +99,51 @@ void func_808E0954(EnEncount1 *this, PlayState *play) {
             }
             spawnPos.x = player->actor.world.pos.x + (Math_SinS(sp5E) * sp64) + randPlusMinusPoint5Scaled(40.0f);
             spawnPos.y = player->actor.floorHeight + 120.0f;
-            spawnPos.z = player->actor.world.pos.z + (Math_CosS(sp5E) * sp64)+ randPlusMinusPoint5Scaled(40.0f);
+            spawnPos.z = player->actor.world.pos.z + (Math_CosS(sp5E) * sp64) + randPlusMinusPoint5Scaled(40.0f);
             temp_fv0_2 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &spawnPos);
-            if ((temp_fv0_2 <= -32000.0f) ||
-                ((player->actor.depthInWater != -32000.0f) &&
+            if ((temp_fv0_2 <= BGCHECK_Y_MIN) ||
+                ((player->actor.depthInWater != BGCHECK_Y_MIN) &&
                  (temp_fv0_2 < (player->actor.world.pos.y - player->actor.depthInWater)))) {
                 return;
             }
             spawnPos.y = temp_fv0_2;
             break;
-        
-        case 1:
+
+        case WALLMASTER:
             Math_Vec3f_Copy(&spawnPos, &player->actor.world.pos);
             break;
-        
-        case 2:
+
+        case SKULLFISH:
             sp64 = randPlusMinusPoint5Scaled(250.0f) + 500.0f;
             sp5E = player->actor.shape.rot.y;
             spawnPos.x = player->actor.world.pos.x + Math_SinS(sp5E) * sp64 + randPlusMinusPoint5Scaled(40.0f);
             spawnPos.y = player->actor.world.pos.y - Rand_ZeroFloat(20.0f);
             spawnPos.z = player->actor.world.pos.z + (Math_CosS(sp5E) * sp64) + randPlusMinusPoint5Scaled(40.0f);
             temp_fv0_2 = BgCheck_EntityRaycastFloor5(&play->colCtx, &sp54, &sp50, &this->actor, &spawnPos);
-            if ((!(player->stateFlags1 & 0x08000000) || (temp_fv0_2 <= (-32000.0f))||
-                (player->actor.depthInWater < temp_fv0_2))) {
+            if ((!(player->stateFlags1 & 0x8000000) || (temp_fv0_2 <= (BGCHECK_Y_MIN)) ||
+                 (player->actor.depthInWater < temp_fv0_2))) {
                 return;
             }
             break;
-        
-        case 3:
-            if ((this->unk148 != NULL) && (SubS_CopyPointFromPath(this->unk148, 0, &spawnPos) == 0)) {
+
+        case SKULLFISH_2:
+            if ((this->path != NULL) && (!SubS_CopyPointFromPath(this->path, 0, &spawnPos))) {
                 Actor_MarkForDeath(&this->actor);
             }
             break;
     }
-    
-    actorParams = sActorParams[this->unk150];
-    if (Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, sActorList[this->unk150], spawnPos.x, spawnPos.y,
-                           spawnPos.z, 0, 0, 0, actorParams) != NULL) {
+
+    actorList = sActorList[this->actorType];
+    actorParams = sActorParams[this->actorType];
+    if (Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, actorList, spawnPos.x, spawnPos.y, spawnPos.z, 0, 0, 0,
+                           actorParams) != NULL) {
         this->unk_14E++;
-        if (this->unk154 > 0) {
-            this->unk152++;
+        if (this->unk_154 > 0) {
+            this->unk_152++;
         }
-    
-        if ((this->unk_14E >= this->unk14C) && (this->unk158 != 0)) {
-            this->unk156 = 1;
+
+        if ((this->unk_14E >= this->unk_14C) && (this->unk_158 != 0)) {
+            this->unk_156 = 1;
         }
     }
 }
--- src/overlays/actors/ovl_En_Encount1/z_en_encount1.h
+++ src/overlays/actors/ovl_En_Encount1/z_en_encount1.h
@@ -3,7 +3,16 @@
 
 #include "global.h"
 
-#define ENENCOUNT1_GET_3F(thisx) ((thisx)->params & 0x3F)
+#define ENENCOUNT1_GET_TYPE(thisx) (((thisx)->params >> 11) & 0x1F)
+#define ENENCOUNT1_GET_7C0(thisx) (((thisx)->params >> 6) & 0x1F)
+#define ENENCOUNT1_GET_PATH(thisx) ((thisx)->params & 0x3F)
+
+typedef enum EnEncount1Enemy {
+    /* 0x0 */ GRASSHOPPER,
+    /* 0x1 */ WALLMASTER,
+    /* 0x2 */ SKULLFISH,
+    /* 0x3 */ SKULLFISH_2,
+} EnEncount1Enemy;
 
 struct EnEncount1;
 
@@ -12,17 +21,17 @@ typedef void (*EnEncount1ActionFunc)(struct EnEncount1*, PlayState*);
 typedef struct EnEncount1 {
     /* 0x000 */ Actor actor;
     /* 0x144 */ EnEncount1ActionFunc actionFunc;
-    /* 0x148 */ Path* unk148;
-    /* 0x14C */ s16 unk14C;
+    /* 0x148 */ Path* path;
+    /* 0x14C */ s16 unk_14C;
     /* 0x14E */ s16 unk_14E;
-    /* 0x150 */ s16 unk150;
-    /* 0x152 */ s16 unk152;
-    /* 0x154 */ s16 unk154;
-    /* 0x156 */ s16 unk156;
-    /* 0x158 */ s16 unk158;
+    /* 0x150 */ s16 actorType;
+    /* 0x152 */ s16 unk_152;
+    /* 0x154 */ s16 unk_154;
+    /* 0x156 */ s16 unk_156;
+    /* 0x158 */ s16 unk_158;
     /* 0x15A */ s16 unk_15A;
-    /* 0x15C */ s32 unk15C;
-    /* 0x160 */ f32 unk160;
+    /* 0x15C */ s32 unk_15C;
+    /* 0x160 */ f32 unk_160;
 } EnEncount1; // size = 0x164
 
 extern const ActorInit En_Encount1_InitVars;
