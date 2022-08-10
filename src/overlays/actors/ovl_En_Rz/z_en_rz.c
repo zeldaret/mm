de97b415384d78b28cc7f0fd04fdf4fbcafc4107
sonicdcer <alejandro.asenjo88@gmail.com>
2022-08-10 04:42:14 -0300

    4 functions left

--- src/overlays/actors/ovl_En_Rz/z_en_rz.c
+++ src/overlays/actors/ovl_En_Rz/z_en_rz.c
@@ -32,11 +32,14 @@ void func_80BFC8F8(EnRz* this, PlayState* play);
 void func_80BFB780(EnRz* this, Lights* mapper, PlayState* play);
 void func_80BFB864(PlayState*, EnRz*, s32, s32, f32);
 void func_80BFBA50(EnRz* this, PlayState*);
-s32 func_80BFBDA0(EnRz* this, PlayState*);
+Actor* func_80BFBDA0(EnRz* this, PlayState*);
 s32 func_80BFBCEC(EnRz* this, PlayState* play);
 void func_80BFBD54(EnRz* this, PlayState* play);
 s32 func_80BFBE70(EnRz* this, PlayState* play);
 s32 func_80BFBFAC(EnRz* this, PlayState* play);
+s32 func_80BFBB44(EnRz* this);
+void func_80BFCAD0(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx);
+
 #if 0
 const ActorInit En_Rz_InitVars = {
     ACTOR_EN_RZ,
@@ -57,6 +60,12 @@ static ColliderCylinderInit D_80BFCCF4 = {
     { 20, 40, 0, { 0, 0, 0 } },
 };
 
+static Vec3f D_80BFCD60 = { 500.0f, -500.0f, 0.0f };
+
+static void *D_80BFCCE0[3] = { (void *)0x0600BC50, (void *)0x0600C190, (void *)0x0600C590 };
+static void *D_80BFCCEC = (void *)0x0600C990;
+static void *D_80BFCCF0 = (void *)0x0600CD90;
+
 #endif
 
 extern ColliderCylinderInit D_80BFCCF4;
@@ -65,6 +74,11 @@ extern UNK_TYPE D_06003A20;
 extern UNK_TYPE D_0600D768;
 
 extern FlexSkeletonHeader D_0600D8D8;
+extern Vec3f D_80BFCD60;
+
+extern void* D_80BFCCE0[3];
+extern void* D_80BFCCEC;
+extern void* D_80BFCCF0;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/EnRz_Init.s")
 
@@ -174,12 +188,26 @@ void func_80BFBD54(EnRz* this, PlayState* play) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBDA0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBDA0.s")
+
+Actor* func_80BFBDA0(EnRz* this, PlayState* play) {
+    Actor* npc = play->actorCtx.actorLists[ACTORCAT_NPC].first;
+
+    while (npc != NULL) {
+        if ((npc->id == ACTOR_EN_RZ) && (EN_RZ_GET_PARAM_F(&this->actor) == EN_RZ_GET_PARAM_F(npc))) {
+            if (&this->actor != npc) {
+                return npc;
+            }
+        }
+        npc = npc->next;
+    }
+    return NULL;
+}
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBDFC.s")
 
 void func_80BFBDFC(PlayState* play) {
-    if ((gSaveContext.save.weekEventReg[0x4B] & 0x80) != 0) {
+    if ((gSaveContext.save.weekEventReg[0x4B] & 0x80)) {
         func_80151BB4(play, 0x27);
     }
     func_80151BB4(play, 0xC);
@@ -193,43 +221,40 @@ void EnRz_Destroy(Actor* thisx, PlayState* play) {
     Collider_DestroyCylinder(play, &this->collider);
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBE70.s")
-/*
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBE70.s")
+
 s32 func_80BFBE70(EnRz* this, PlayState* play) {
     u16 action;
-
     if (((this->actor.params & 0x8000) == 0) && (this->unk422 == 4)) {
         func_800B9010(&this->actor, 0x2198);
     }
-    if (Cutscene_CheckActorAction(play, this->unk426) != 0) {
+    if (Cutscene_CheckActorAction(play, this->unk426)) {
         Cutscene_ActorTranslateAndYaw(&this->actor, play, Cutscene_GetActorActionIndex(play, this->unk426));
         action = play->csCtx.actorActions[Cutscene_GetActorActionIndex(play, this->unk426)]->action;
-        if (action != this->unk428) {
+        if (this->unk428 != action) {
             this->unk428 = action;
             switch (action & 0xFFFF) {
                 case 1:
                     func_80BFBA1C(play, this, 1);
-                    return 1;
+                    break;
+
                 case 2:
                     func_80BFBA1C(play, this, 5);
-                    return 1;
+                    break;
+
                 case 3:
                     func_80BFBA1C(play, this, 4);
-                    return 1;
+                    break;
+
                 case 4:
                     func_80BFBA1C(play, this, 8);
-                    return 1;
-                default:
-                    return 1;
+                    break;
             }
-        } else {
-            return 0;
         }
-    } else {
-        return 0;
+        return true;
     }
+    return false;
 }
-*/
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFBFAC.s")
 
@@ -256,28 +281,30 @@ void func_80BFC058(EnRz* this, PlayState* play) {
     func_80BFBD54(this, play);
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC078.s")
-/*
-void func_80BFC078(EnRz* this, PlayState* play) {
-    Vec3f sp28;
-    u16 temp_v1;
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC078.s")
 
+void func_80BFC078(EnRz* this, PlayState* play) {
+    s32 pad;
+    Vec3f sp28;
     func_80BFBD54(this, play);
     if ((Message_GetState(&play->msgCtx) == 5) && Message_ShouldAdvance(play)) {
-        // if (1)
-        temp_v1 = play->msgCtx.currentTextId;
-        if ((temp_v1 == 0x2927) || (temp_v1 == 0x2928)) {
-            func_80151938(play, (temp_v1 + 1) & 0xFFFF);
-            gSaveContext.save.weekEventReg[0x4D] |= 4;
-        } else {
-            func_801477B4(play);
-            this->actionFunc = func_80BFC3F8;
-            if (this->unk422 != 8) {
-                func_80BFB9E4(play, this, 6);
-                if (this->unk40C != NULL) {
-                    func_80BFB9E4(play, this->unk40C, 6);
+        switch (play->msgCtx.currentTextId) {
+            case 0x2927:
+            case 0x2928:
+                func_80151938(play, play->msgCtx.currentTextId + 1);
+                gSaveContext.save.weekEventReg[0x4D] |= 4;
+                break;
+
+            default:
+                func_801477B4(play);
+                this->actionFunc = func_80BFC3F8;
+                if (this->unk422 != 8) {
+                    func_80BFB9E4(play, this, 6);
+                    if (this->unk40C != NULL) {
+                        func_80BFB9E4(play, this->unk40C, 6);
+                    }
                 }
-            }
+                break;
         }
     }
     if ((this->actor.params & 0x8000) == 0) {
@@ -287,52 +314,285 @@ void func_80BFC078(EnRz* this, PlayState* play) {
         func_801A1FB4(3, &sp28, 0x2A, 900.0f);
     }
 }
-*/
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC19C.s")
 
-void func_80BFC19C(EnRz *this, PlayState *play) {
+void func_80BFC19C(EnRz* this, PlayState* play) {
     func_80BFBD54(this, play);
     if (!func_80BFBE70(this, play)) {
         this->actionFunc = func_80BFC3F8;
         func_80BFBA1C(play, this, 8);
         if (this->unk40C != NULL) {
-            func_80BFBA1C(play, (EnRz *) this->unk40C, 8);
+            func_80BFBA1C(play, (EnRz*)this->unk40C, 8);
         }
     }
 }
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC214.s")
 
-void func_80BFC214(EnRz *this, PlayState *play) {
+void func_80BFC214(EnRz* this, PlayState* play) {
     func_80BFBD54(this, play);
     if (!func_80BFBE70(this, play) && func_80BFBFAC(this, play)) {
         this->actionFunc = func_80BFC19C;
     }
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC270.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC270.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC2F4.s")
+void func_80BFC270(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if (Actor_HasParent(&this->actor, play)) {
+        this->actor.parent = NULL;
+        this->actionFunc = func_80BFC214;
+        func_80BFBDFC(play);
+    } else {
+        Actor_PickUp(&this->actor, play, 0xC, 2000.0f, 1000.0f);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC36C.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC2F4.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC3F8.s")
+void func_80BFC2F4(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if (!func_80BFBE70(this, play)) {
+        func_801477B4(play);
+        Actor_PickUp(&this->actor, play, 0xC, 2000.0f, 1000.0f);
+        this->actionFunc = func_80BFC270;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC608.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC36C.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC674.s")
+void func_80BFC36C(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if (func_80BFBFAC(this, play)) {
+        gSaveContext.save.weekEventReg[0x4D] |= 4;
+        if ((gSaveContext.save.weekEventReg[0x4B] & 0x80)) {
+            this->actionFunc = func_80BFC214;
+        } else {
+            this->actionFunc = func_80BFC2F4;
+            gSaveContext.save.weekEventReg[0x4B] |= 0x80;
+        }
+        this->actor.cutscene = this->unk42C;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC728.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC3F8.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC7E0.s")
+void func_80BFC3F8(EnRz* this, PlayState* play) {
+    s32 pad;
+    Vec3f sp30;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC8AC.s")
+    func_80BFBD54(this, play);
+    if (this->unk40C == NULL) {
+        this->unk40C = func_80BFBDA0(this, play);
+    }
+    if (!func_80BFBE70(this, play)) {
+        if (Actor_ProcessTalkRequest(&this->actor, &play->state)) {
+            this->actionFunc = func_80BFC078;
+            if ((this->actor.flags & 0x10000) == 0x10000) {
+                this->actionFunc = func_80BFC36C;
+                this->actor.cutscene = this->unk42A;
+                this->actor.flags &= 0xFFFEFFFF;
+            } else if (Player_GetMask(play) == 0xE) {
+                if ((gSaveContext.save.weekEventReg[0x4D] & 4) != 0) {
+                    Message_StartTextbox(play, 0x2925, &this->actor);
+                    func_80BFBDFC(play);
+                } else {
+                    Message_StartTextbox(play, 0x2926, &this->actor);
+                    func_80BFBDFC(play);
+                }
+            } else {
+                Message_StartTextbox(play, 0x2926, &this->actor);
+                func_80BFBDFC(play);
+            }
+        } else if (func_80BFBC78(this, play) != 0) {
+            if ((func_80BFBCEC(this, play) != 0) && ((gSaveContext.save.weekEventReg[0x4D] & 4) == 0) &&
+                (this->unk40C != NULL)) {
+                this->actor.flags |= 0x10000;
+                func_800B8500(&this->actor, play, 1000.0f, 1000.0f, -1);
+            } else {
+                this->actor.flags &= 0xFFFEFFFF;
+                func_800B8614(&this->actor, play, 120.0f);
+            }
+        }
+        if ((this->actor.params & 0x8000) == 0) {
+            sp30.x = this->actor.projectedPos.x;
+            sp30.y = this->actor.projectedPos.y;
+            sp30.z = this->actor.projectedPos.z;
+            func_801A1FB4(3, &sp30, 0x2A, 900.0f);
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC8F8.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC608.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/EnRz_Update.s")
+void func_80BFC608(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if ((Message_GetState(&play->msgCtx) == 5) && Message_ShouldAdvance(play)) {
+        func_801477B4(play);
+        this->actionFunc = func_80BFC674;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFCAD0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC674.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/EnRz_Draw.s")
+void func_80BFC674(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if (Actor_ProcessTalkRequest(&this->actor, &play->state)) {
+        this->actionFunc = func_80BFC608;
+        if (Player_GetMask(play) == 0xE) {
+            Message_StartTextbox(play, 0x2925, &this->actor);
+            return;
+        }
+        Message_StartTextbox(play, 0x2924, &this->actor);
+        return;
+    }
+    if (func_80BFBC78(this, play)) {
+        func_800B8614(&this->actor, play, 120.0f);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC728.s")
+
+void func_80BFC728(EnRz* this, PlayState* play) {
+    func_80BFBD54(this, play);
+    if ((Message_GetState(&play->msgCtx) == 5) && Message_ShouldAdvance(play)) {
+        func_801477B4(play);
+        this->actionFunc = func_80BFC7E0;
+        this->actor.textId++;
+        if ((this->actor.params & 0x8000) == 0) {
+            if (this->actor.textId >= 0x2920) {
+                this->actor.textId = 0x291C;
+            }
+        } else if (this->actor.textId >= 0x2924) {
+            this->actor.textId = 0x2920;
+        }
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC7E0.s")
+
+void func_80BFC7E0(EnRz* this, PlayState* play) {
+    s32 new_var;
+
+    func_80BFBD54(this, play);
+    new_var = this->unk424;
+    if (new_var > 0) {
+        this->unk424--;
+    } else {
+        this->actionFunc = func_80BFC8F8;
+        func_80BFB9E4(play, this, 2);
+    }
+    if (Actor_ProcessTalkRequest(&this->actor, &play->state)) {
+        this->actionFunc = func_80BFC728;
+        func_80BFB9E4(play, this, 0);
+        this->actor.speedXZ = 0.0f;
+        func_80BFBDFC(play);
+        return;
+    }
+    if (func_80BFBC78(this, play)) {
+        func_800B8614(&this->actor, play, 120.0f);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC8AC.s")
+
+void func_80BFC8AC(EnRz* this, PlayState* play) {
+    this->unk424 = 0x64;
+    this->actionFunc = func_80BFC7E0;
+    this->actor.speedXZ = 0.0f;
+    func_80BFB9E4(play, this, 0);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFC8F8.s")
+
+void func_80BFC8F8(EnRz* this, PlayState* play) {
+    s32 temp_v0;
+
+    func_80BFBD54(this, play);
+    this->actor.speedXZ = 1.5f;
+    temp_v0 = func_80BFBB44(this);
+    if (temp_v0 != 1) {
+        if (temp_v0 == 2) {
+            func_80BFBA50(this, play);
+            func_80BFC8AC(this, play);
+        }
+    } else {
+        func_80BFC8AC(this, play);
+    }
+    if (Actor_ProcessTalkRequest(&this->actor, &play->state)) {
+        this->actionFunc = func_80BFC728;
+        func_80BFB9E4(play, this, 0);
+        this->actor.speedXZ = 0.0f;
+        func_80BFBDFC(play);
+    } else if (func_80BFBC78(this, play)) {
+        func_800B8614(&this->actor, play, 120.0f);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/EnRz_Update.s")
+
+void EnRz_Update(Actor* thisx, PlayState* play) {
+    s32 phi_v0;
+    s32 pad;
+    EnRz* this = THIS;
+
+    Collider_UpdateCylinder(&this->actor, &this->collider);
+    CollisionCheck_SetOC(play, &play->colChkCtx, &this->collider.base);
+    Actor_MoveWithGravity(&this->actor);
+    Actor_UpdateBgCheckInfo(play, &this->actor, 40.0f, 25.0f, 40.0f, 5U);
+    this->actionFunc(this, play);
+    if (this->unk412 == 0) {
+        phi_v0 = false;
+    } else {
+        this->unk412--;
+        phi_v0 = this->unk412;
+    }
+    if (!phi_v0) {
+        this->unk412 = Rand_S16Offset(0x3C, 0x3C);
+    }
+    this->unk410 = this->unk412;
+    if (this->unk410 >= 3) {
+        this->unk410 = 0;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/func_80BFCAD0.s")
+
+void func_80BFCAD0(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
+    EnRz* this = THIS;
+
+    if (limbIndex == 0xB) {
+        Matrix_MultVec3f(&D_80BFCD60, &thisx->focus.pos);
+    }
+    if (limbIndex == 3) {
+        Matrix_MultVec3f(&gZeroVec3f, &this->unk414);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Rz/EnRz_Draw.s")
+
+void EnRz_Draw(Actor* thisx, PlayState* play) {
+    EnRz* this = THIS;
+
+    OPEN_DISPS(play->state.gfxCtx);
+
+    func_8012C28C(play->state.gfxCtx);
+    if (!(this->actor.params & 0x8000)) {
+        AnimatedMat_DrawStepOpa(play, Lib_SegmentedToVirtual(&D_0600D768), 0U);
+    } else {
+        AnimatedMat_DrawStepOpa(play, Lib_SegmentedToVirtual(&D_0600D768), 1U);
+    }
+    if (this->unk422 == 4) {
+        gSPSegment(POLY_OPA_DISP++, 0x09, Lib_SegmentedToVirtual(D_80BFCCF0));
+    } else if (this->unk420 & 2) {
+        gSPSegment(POLY_OPA_DISP++, 0x09, Lib_SegmentedToVirtual(D_80BFCCEC));
+    } else {
+        gSPSegment(POLY_OPA_DISP++, 0x09, Lib_SegmentedToVirtual(D_80BFCCE0[this->unk410]));
+    }
+    SkelAnime_DrawFlexOpa(play, this->skelanime.skeleton, this->skelanime.jointTable, this->skelanime.dListCount, NULL,
+                          func_80BFCAD0, &this->actor);
+
+    CLOSE_DISPS(play->state.gfxCtx);
+}
--- src/overlays/actors/ovl_En_Rz/z_en_rz.h
+++ src/overlays/actors/ovl_En_Rz/z_en_rz.h
@@ -3,6 +3,8 @@
 
 #include "global.h"
 
+#define EN_RZ_GET_PARAM_F(thisx) ((thisx)->params & 0xF)
+
 struct EnRz;
 
 typedef void (*EnRzActionFunc)(struct EnRz*, PlayState*);
@@ -14,15 +16,18 @@ typedef struct EnRz {
     /* 0x3B8 */ Path *unk3B8;
     /* 0x3BC */ s32 unk3BC;
     /* 0x3C0 */ ColliderCylinder collider;
-    /* 0x40C */ Actor *unk40C;                       /* inferred */
-    /* 0x410 */ char pad410[4];
+    /* 0x40C */ Actor *unk40C;
+    /* 0x410 */ s16 unk410;                         /* inferred */
+    /* 0x412 */ s16 unk412;                         /* inferred */
     /* 0x414 */ Vec3f unk414;
     /* 0x420 */ u16 unk420;
     /* 0x422 */ s16 unk422;
-    /* 0x424 */ char pad424[2];
+    /* 0x424 */ s16 unk424;
     /* 0x426 */ u16 unk426;
     /* 0x428 */ u16 unk428;
-    /* 0x42A */ char pad42A[6];
+    /* 0x42A */ s16 unk42A;
+    /* 0x42C */ s16 unk42C;
+    /* 0x42E */ char pad42E[2];
     /* 0x430 */ EnRzActionFunc actionFunc;
 } EnRz; // size = 0x434
 
