64425d2878bdab97a4f7d377c974e7c43303d581
angie <angheloalf95@gmail.com>
2022-08-11 21:48:10 -0400

    func_8083BF54 NON_MATCHING

--- src/overlays/actors/ovl_player_actor/z_player.c
+++ src/overlays/actors/ovl_player_actor/z_player.c
@@ -7519,8 +7519,145 @@ void func_8083BB4C(PlayState* play, Player* this) {
     }
 }
 
+#ifdef NON_MATCHING
+// float regalloc
+void func_8083BF54(PlayState* play, Player* this) {
+    Vec3f sp84;
+    s32 temp_v0; // real
+    s32 var_a2; // sp7C
+
+    this->actor.terminalVelocity = -20.0f;
+    this->actor.gravity = gGameInfo->data[0x44] / 100.0f;
+    var_a2 = 0;
+    temp_v0 = func_808340D4(D_80862B08);
+
+    if ((temp_v0 != 0) || (((var_a2 = (D_80862B08 == 0xE) || (D_80862B08 == 0xF))) || (D_80862B08 == 0xD))) {
+        f32 temp_fv1_2;
+        f32 var_fa1;
+        f32 var_ft4;
+        f32 var_fv0;
+        s32 temp_a0; // ?
+        u16 var_a1; // real?
+
+        temp_a0 = D_80862B08 == 0xE;
+        var_ft4 = fabsf(this->linearVelocity + D_80862B3C) * 20.0f;
+        if (temp_v0 != 0) {
+            if (D_80862B08 == 4) {
+                var_fa1 = 1300.0f;
+                var_a1 = 0xE9;
+            } else if (D_80862B08 == 7) {
+                var_fa1 = 20000.0f;
+                var_a1 = 0xE9;
+                var_ft4 = 0.0f;
+            } else {
+                var_ft4 *= 1.6f;
+                var_fa1 = 10000.0f;
+                var_a1 = 0xE9;
+            }
+        } else if (var_a2 != 0) {
+            if (temp_a0 != 0) {
+                var_fa1 = 400.0f;
+                var_a1 = 0xEA;
+                var_ft4 *= 10.0f;
+            } else {
+                var_fa1 = 1300.0f;
+                var_a1 = 0xEA;
+                var_ft4 = 0.0f;
+            }
+        } else {
+            var_a1 = 0xE9;
+            var_fa1 = (this->transformation == PLAYER_FORM_GORON) ? 10000.0f : 1000.0f;
+            var_ft4 = 0.0f;
+        }
+
+        temp_fv1_2 = this->unk_AB8;
+        var_fa1 = CLAMP_MIN(var_fa1, temp_fv1_2);
+
+        var_fv0 = (temp_a0 != 0) ? 200.0f : (var_fa1 - temp_fv1_2) * 0.02f;
+        var_fv0 = CLAMP(var_fv0, 0.0f, 300.0f);
+
+        this->unk_AB8 = temp_fv1_2 + (var_fv0 - var_ft4);
+        //this->unk_AB8 = CLAMP(this->unk_AB8, 0.0f, var_fa1);
+        var_ft4 = var_fa1;
+        this->unk_AB8 = CLAMP(this->unk_AB8, 0.0f, var_ft4);
+
+        if ((this->linearVelocity == 0.0f) && (fabsf(this->unk_AB8 - temp_fv1_2) > 2.0f)) {
+            func_800B8F98(&this->actor, var_a1);
+        }
+
+        this->actor.gravity -= this->unk_AB8 * 0.004f;
+    } else {
+        this->unk_AB8 = 0.0f;
+    }
+
+    if ((this->stateFlags3 & 0x10) && (this->actor.bgCheckFlags & 0x20)) {
+        if (this->actor.depthInWater < 50.0f) {
+            Vec3f* var_v1; // sp60
+            f32 var_fa0_3; // sp5C
+            f32 var_ft4_2;
+            s32 var_t0; // sp54
+            f32 temp_fv1_5;
+
+            var_ft4_2 = (fabsf(this->bodyPartsPos[0].x - this->unk_D6C.x) + fabsf(this->bodyPartsPos[0].y - this->unk_D6C.y)) + fabsf(this->bodyPartsPos[0].z - this->unk_D6C.z);
+            var_ft4_2 = CLAMP_MAX(var_ft4_2, 4.0f);
+
+            this->unk_AEC += var_ft4_2;
+            if (this->unk_AEC > 15.0f) {
+                this->unk_AEC = 0.0f;
+                sp84.x = (Rand_ZeroOne() * 10.0f) + this->actor.world.pos.x;
+                sp84.y = this->actor.world.pos.y + this->actor.depthInWater;
+                sp84.z = (Rand_ZeroOne() * 10.0f) + this->actor.world.pos.z;
+                EffectSsGRipple_Spawn(play, &sp84, 0x64, 0x1F4, 0);
+
+                if ((this->linearVelocity > 4.0f) && (func_801242B4(this) == 0) && ((this->actor.world.pos.y + this->actor.depthInWater) < this->bodyPartsPos[0].y)) {
+                    func_80837730(play, this, 20.0f, (s32) ((fabsf(this->linearVelocity) * 50.0f) + (this->actor.depthInWater * 5.0f)));
+                } else if (this->stateFlags3 & 0x8000) {
+                    var_fa0_3 = (this->actor.world.pos.y + this->actor.depthInWater) - 5.0f;
+                    var_v1 = this->bodyPartsPos;
+
+                    for (var_t0 = 0; var_t0 < PLAYER_BODYPART_MAX; var_t0++, var_v1++) {
+                        temp_fv1_5 = var_v1->y - var_fa0_3;
+
+                        if (temp_fv1_5 > 0.0f) {
+                            func_80837730(play, this, 20.0f, (fabsf(this->linearVelocity) * 20.0f + (temp_fv1_5 * 10.0f)));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (this->ageProperties->unk_2C < this->actor.depthInWater) {
+            s32 var_v1_2; // sp50
+            s32 var_v0; // sp4C
+            f32 var_fv1;
+
+            var_v1_2 = 0;
+
+            var_fv1 = (this->stateFlags1 & 0x04000000) ? -fabsf(this->linearVelocity) : ((func_80850D68 == this->actionFunc) ? (ABS_ALT(this->unk_B8A) * -0.004f) + (this->unk_B48 * -0.38f) : this->actor.velocity.y);
+
+            if (!D_80862B08) {}
+
+            if ((var_fv1 > -1.0f) || ((this->currentBoots == 5) && (this->actor.bgCheckFlags & 1))) {
+                if (Rand_ZeroOne() < 0.2f) {
+                    var_v1_2 = 1;
+                }
+            } else {
+                var_v1_2 = var_fv1 * -0.3f;
+                if (var_v1_2 >= 9) {
+                    var_v1_2 = 8;
+                }
+            }
+
+            for (var_v0 = 0; var_v0 < var_v1_2; var_v0++) {
+                EffectSsBubble_Spawn(play, &this->actor.world.pos, 20.0f, 10.0f, 20.0f, 0.13f);
+            }
+        }
+    }
+}
+#else
 void func_8083BF54(PlayState* play, Player* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_player_actor/func_8083BF54.s")
+#endif
 
 s32 func_8083C62C(Player* this, s32 arg1) {
     Actor* actor = this->unk_730;
