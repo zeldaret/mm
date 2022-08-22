e3d35d88f514a561afc06aec4f0f6310f988ff02
sonicdcer <alejandro.asenjo88@gmail.com>
2022-08-22 18:55:37 -0300

    all functions matching

--- src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.c
+++ src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.c
@@ -14,6 +14,13 @@ void BgUmajump_Init(Actor* thisx, PlayState* play);
 void BgUmajump_Destroy(Actor* thisx, PlayState* play);
 void BgUmajump_Update(Actor* thisx, PlayState* play);
 
+void func_8091A5A0(BgUmajump* this, PlayState* play);
+void func_8091A0B8(BgUmajump* this, PlayState* play);
+void func_8091A044(BgUmajump* this, PlayState* play);
+void func_80919FC8(BgUmajump* this, PlayState* play);
+void func_80919F30(BgUmajump* this, PlayState* play); /* extern */
+void func_8091A7B0(Actor* thisx, PlayState* play);
+
 #if 0
 const ActorInit Bg_Umajump_InitVars = {
     ACTOR_BG_UMAJUMP,
@@ -37,24 +44,206 @@ static InitChainEntry D_8091A810[] = {
 #endif
 
 extern InitChainEntry D_8091A810[];
+extern CollisionHeader D_06001438;
 
-extern UNK_TYPE D_06001220;
+extern Gfx* D_06001220;
 extern UNK_TYPE D_06001558;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_80919F30.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_80919F30.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_80919FC8.s")
+void func_80919F30(BgUmajump* this, PlayState* play) {
+    if (this->unk160 >= 0x5A) {
+        this->unk160 = 0;
+        this->unk160 = 0;
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A044.s")
+    this->unk160++;
+    this->dyna.actor.shape.yOffset = Math_SinS((this->unk160 / 90.0f) * 65536.0f) * (20.0f / this->dyna.actor.scale.y);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A0B8.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_80919FC8.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Init.s")
+void func_80919FC8(BgUmajump* this, PlayState* play) {
+    if ((play->csCtx.frames >= 6) && (this->unk164 == 0)) {
+        this->unk164 = 1;
+        play_sound(0x2844);
+    }
+    if (!play->csCtx.state) {
+        ActorCutscene_Stop(this->dyna.actor.cutscene);
+        this->dyna.actor.update = Actor_Noop;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Destroy.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A044.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Update.s")
+void func_8091A044(BgUmajump* this, PlayState* play) {
+    if (ActorCutscene_GetCanPlayNext(this->dyna.actor.cutscene)) {
+        ActorCutscene_StartAndSetUnkLinkFields(this->dyna.actor.cutscene, &this->dyna.actor);
+        gSaveContext.save.weekEventReg[0x59] |= 0x20;
+        this->actionFunc = func_80919FC8;
+    } else {
+        ActorCutscene_SetIntentToPlay(this->dyna.actor.cutscene);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A5A0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A0B8.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A7B0.s")
+void func_8091A0B8(BgUmajump* this, PlayState* play) {
+    Player* player = GET_PLAYER(play);
+
+    if ((this->horse != NULL) && (player != NULL) &&
+        (Math3D_XZDistance(this->horse->world.pos.x, this->horse->world.pos.z, player->actor.world.pos.x,
+                           player->actor.world.pos.z) < 1400.0f)) {
+        this->actionFunc = func_8091A044;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Init.s")
+
+void BgUmajump_Init(Actor* thisx, PlayState* play) {
+    BgUmajump* this = THIS;
+
+    Actor_ProcessInitChain(&this->dyna.actor, D_8091A810);
+
+    this->actionFunc = NULL;
+    this->horse = NULL;
+    this->unk164 = 0;
+
+    DynaPolyActor_Init(&this->dyna, 0);
+
+    this->unk160 = (thisx->params >> 8) & 0xFF;
+    thisx->params = thisx->params & 0xFF;
+
+    if ((this->dyna.actor.params == 2)) {
+        if ((((play->sceneNum == SCENE_F01) && (!(gSaveContext.save.weekEventReg[0x59] & 0x20))) &&
+             (!CHECK_QUEST_ITEM(QUEST_SONG_EPONA))) &&
+            (this->dyna.actor.cutscene != -1)) {
+            this->actionFunc = func_8091A0B8;
+            this->dyna.actor.update = func_8091A5A0;
+            this->dyna.actor.flags |= 0x10;
+            this->horse = SubS_FindActor(play, this->horse, ACTORCAT_BG, ACTOR_EN_HORSE);
+        } else {
+            this->dyna.actor.update = Actor_Noop;
+        }
+    } else {
+        this->unk160 = Object_GetIndex(&play->objectCtx, 0xD2);
+        if (this->unk160 < 0) {
+            Actor_MarkForDeath(&this->dyna.actor);
+        }
+        if ((this->dyna.actor.params == 3) && CHECK_QUEST_ITEM(QUEST_SONG_EPONA)) {
+            Actor_Spawn(&play->actorCtx, play, ACTOR_EN_KANBAN, this->dyna.actor.world.pos.x,
+                        this->dyna.actor.world.pos.y, this->dyna.actor.world.pos.z, this->dyna.actor.shape.rot.x,
+                        this->dyna.actor.shape.rot.y, this->dyna.actor.shape.rot.z, 0x3E);
+            Actor_MarkForDeath(&this->dyna.actor);
+        }
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Destroy.s")
+
+void BgUmajump_Destroy(Actor* thisx, PlayState* play) {
+    BgUmajump* this = THIS;
+
+    DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/BgUmajump_Update.s")
+
+void BgUmajump_Update(Actor* thisx, PlayState* play) {
+    BgUmajump* this = (BgUmajump*)thisx;
+    if (Object_IsLoaded(&play->objectCtx, this->unk160) != 0) {
+        this->dyna.actor.objBankIndex = this->unk160;
+        this->dyna.actor.draw = func_8091A7B0;
+        Actor_SetObjectDependency(play, &this->dyna.actor);
+        if (this->dyna.actor.params == 5) {
+            if ((gSaveContext.save.weekEventReg[0x16] & 1) != 0) {
+                DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+            }
+        } else if (this->dyna.actor.params == 6) {
+            if ((gSaveContext.save.weekEventReg[0x16] & 1) == 0) {
+                DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+            }
+        } else {
+            if ((this->dyna.actor.params == 4) || (this->dyna.actor.params == 3)) {
+                DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+            } else {
+                DynaPolyActor_LoadMesh(play, &this->dyna, &D_06001438);
+            }
+        }
+        if (this->dyna.actor.params == 1) {
+            this->unk160 = 0;
+            this->actionFunc = func_80919F30;
+            this->dyna.actor.update = func_8091A5A0;
+            return;
+        }
+        if (this->dyna.actor.params == 3) {
+            this->dyna.actor.update = func_8091A5A0;
+            return;
+        }
+        if ((this->dyna.actor.params == 5) || (this->dyna.actor.params == 6)) {
+            if (this->dyna.actor.params == 5) {
+                if ((this->dyna.bgId == -1) && ((gSaveContext.save.weekEventReg[0x16] & 1) != 0)) {
+                    DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+                }
+            } else if ((this->dyna.actor.params == 6) && (this->dyna.bgId == -1) &&
+                       (((gSaveContext.save.weekEventReg[0x16] & 1) == 0) ||
+                        ((gSaveContext.save.day == 2) && (gSaveContext.save.isNight == 1) &&
+                         ((gSaveContext.save.time >= 0x3AAA) && (gSaveContext.save.time <= 0x4000))))) {
+                DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+            }
+            func_800C641C(play, &play->colCtx.dyna, this->dyna.bgId);
+            this->dyna.actor.update = func_8091A5A0;
+            return;
+        }
+        if (this->dyna.actor.params == 4) {
+            func_800C641C(play, &play->colCtx.dyna, this->dyna.bgId);
+            this->dyna.actor.update = Actor_Noop;
+        } else {
+            this->dyna.actor.update = Actor_Noop;
+        }
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A5A0.s")
+
+void func_8091A5A0(BgUmajump* this, PlayState* play) {
+    if (this->actionFunc != NULL) {
+        this->actionFunc(this, play);
+    }
+    if ((this->dyna.actor.params == 3) && (CHECK_QUEST_ITEM(QUEST_SONG_EPONA))) {
+        Actor_Spawn(&play->actorCtx, play, ACTOR_EN_KANBAN, this->dyna.actor.world.pos.x, this->dyna.actor.world.pos.y,
+                    this->dyna.actor.world.pos.z, this->dyna.actor.shape.rot.x, this->dyna.actor.shape.rot.y,
+                    this->dyna.actor.shape.rot.z, 0x3E);
+        Actor_MarkForDeath(&this->dyna.actor);
+    }
+    if (this->dyna.actor.params == 5) {
+        if ((this->dyna.bgId == -1) && (gSaveContext.save.weekEventReg[0x16] & 1)) {
+            DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+        }
+        else if ((this->dyna.bgId != -1) && !(gSaveContext.save.weekEventReg[0x16] & 1)) {
+            DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
+        }
+    } else if (this->dyna.actor.params == 6) {
+        if ((this->dyna.bgId == -1) &&
+            (((gSaveContext.save.weekEventReg[0x16] & 1) == 0) ||
+             ((gSaveContext.save.day == 2) && (gSaveContext.save.isNight == 1) &&
+              (((gSaveContext.save.time >= 0x3AAA))) && (gSaveContext.save.time <= 0x4000)))) {
+            DynaPolyActor_LoadMesh(play, &this->dyna, (CollisionHeader*)&D_06001558);
+        }
+        else if ((this->dyna.bgId != -1) && ((gSaveContext.save.weekEventReg[0x16] & 1) != 0) &&
+            ((gSaveContext.save.day != 2) || (gSaveContext.save.isNight != 1) ||
+             ( ((gSaveContext.save.time < 0x3AAA))) || (gSaveContext.save.time > 0x4000))) {
+            DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
+        }
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Umajump/func_8091A7B0.s")
+
+void func_8091A7B0(Actor* thisx, PlayState* play) {
+    BgUmajump* this = THIS;
+
+    if (this->dyna.bgId != -1) {
+        Gfx_DrawDListOpa(play, &D_06001220);
+    }
+}
--- src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.h
+++ src/overlays/actors/ovl_Bg_Umajump/z_bg_umajump.h
@@ -5,9 +5,14 @@
 
 struct BgUmajump;
 
+typedef void (*BgUmajumpActionFunc)(struct BgUmajump*, PlayState*);
+
 typedef struct BgUmajump {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x28];
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ BgUmajumpActionFunc actionFunc;
+    /* 0x160 */ s32 unk160;
+    /* 0x164 */ s32 unk164;
+    /* 0x168 */ Actor* horse;
 } BgUmajump; // size = 0x16C
 
 extern const ActorInit Bg_Umajump_InitVars;
