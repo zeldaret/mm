82b0c849c87e7096d99531560c9c00436300c171
Derek Hensley <hensley.derek58@gmail.com>
2022-08-21 19:15:20 -0700

    Doc WIP

--- src/overlays/actors/ovl_En_M_Thunder/z_en_m_thunder.c
+++ src/overlays/actors/ovl_En_M_Thunder/z_en_m_thunder.c
@@ -6,6 +6,7 @@
 
 #include "z_en_m_thunder.h"
 #include "z64rumble.h"
+#include "overlays/actors/ovl_Eff_Dust/z_eff_dust.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 
 #define FLAGS (ACTOR_FLAG_10)
@@ -38,8 +39,7 @@ const ActorInit En_M_Thunder_InitVars = {
     (ActorFunc)EnMThunder_Draw,
 };
 
-// static ColliderCylinderInit sCylinderInit = {
-static ColliderCylinderInit D_808B7120 = {
+static ColliderCylinderInit sCylinderInit = {
     {
         COLTYPE_NONE,
         AT_ON | AT_TYPE_PLAYER,
@@ -59,15 +59,22 @@ static ColliderCylinderInit D_808B7120 = {
     { 200, 200, 0, { 0, 0, 0 } },
 };
 
-u8 D_808B714C[] = { 1, 2, 3, 4, 1, 2, 3, 4 };
-u16 D_808B7154[] = { NA_SE_IT_ROLLING_CUT_LV2, NA_SE_IT_ROLLING_CUT_LV1, NA_SE_IT_ROLLING_CUT_LV2, NA_SE_IT_ROLLING_CUT_LV1 };
-f32 D_808B715C[] = { 0.1f, 0.15f, 0.2f, 0.25f, 0.3f, 0.25f, 0.2f, 0.15f, 0.0f };
+static u8 sDamages[] = { 1, 2, 3, 4, 1, 2, 3, 4 };
+
+static u16 sSounds[] = {
+    NA_SE_IT_ROLLING_CUT_LV2,
+    NA_SE_IT_ROLLING_CUT_LV1,
+    NA_SE_IT_ROLLING_CUT_LV2,
+    NA_SE_IT_ROLLING_CUT_LV1,
+};
+
+static f32 sScales[] = { 0.1f, 0.15f, 0.2f, 0.25f, 0.3f, 0.25f, 0.2f, 0.15f, 0.0f };
 
 void func_808B53C0(EnMThunder* this, PlayState* play) {
     Player* player = GET_PLAYER(play);
 
     this->actor.update = func_808B65BC;
-    this->unk1C2 = 0;
+    this->unk1C2 = false;
     this->unk1BE = 1;
     this->unk1C1 = 2;
     this->actionFunc = func_808B6310;
@@ -83,20 +90,21 @@ void EnMThunder_Init(Actor* thisx, PlayState* play) {
     EnMThunder* this = THIS;
     Player* player = GET_PLAYER(play);
 
-    Collider_InitCylinder(play, &this->unk144);
-    Collider_SetCylinder(play, &this->unk144, &this->actor, &D_808B7120);
+    Collider_InitCylinder(play, &this->collider);
+    Collider_SetCylinder(play, &this->collider, &this->actor, &sCylinderInit);
     this->unk1BF = ENMTHUNDER_GET_UNK1BF(&this->actor);
-    Lights_PointNoGlowSetInfo(&this->unk194, this->actor.world.pos.x, this->actor.world.pos.y, this->actor.world.pos.z,
-                              255, 255, 255, 0);
-    this->unk190 = LightContext_InsertLight(play, &play->lightCtx, &this->unk194);
+    Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.world.pos.x, this->actor.world.pos.y,
+                              this->actor.world.pos.z, 255, 255, 255, 0);
+    this->lightNode = LightContext_InsertLight(play, &play->lightCtx, &this->lightInfo);
 
     if (this->unk1BF == 0x80) {
         func_808B53C0(this, play);
         return;
     }
-    this->unk144.dim.radius = 0;
-    this->unk144.dim.height = 40;
-    this->unk144.dim.yShift = -20;
+
+    this->collider.dim.radius = 0;
+    this->collider.dim.height = 40;
+    this->collider.dim.yShift = -20;
     this->unk1BC = 8;
     this->unk1AC = 0.0f;
     this->actor.world.pos = player->bodyPartsPos[0];
@@ -106,7 +114,7 @@ void EnMThunder_Init(Actor* thisx, PlayState* play) {
     this->actor.shape.rot.x = -this->actor.world.rot.x;
     this->actor.room = -1;
     Actor_SetScale(&this->actor, 0.1f);
-    this->unk1C2 = 0;
+    this->unk1C2 = false;
 
     if (player->stateFlags2 & PLAYER_STATE2_20000) {
         if (!gSaveContext.save.playerData.magicAcquired || (gSaveContext.unk_3F28 != 0) ||
@@ -119,11 +127,13 @@ void EnMThunder_Init(Actor* thisx, PlayState* play) {
             Actor_MarkForDeath(&this->actor);
             return;
         }
+
         player->stateFlags2 &= ~PLAYER_STATE2_20000;
-        this->unk1C2 = 0;
+        this->unk1C2 = false;
+
         if (gSaveContext.save.weekEventReg[23] & 2) {
             player->unk_B08[0] = 1.0f;
-            this->unk144.info.toucher.damage = D_808B714C[this->unk1BF + 4];
+            this->collider.info.toucher.damage = sDamages[this->unk1BF + 4];
             this->unk1BE = 0;
             if (this->unk1BF == 3) {
                 this->unk1C1 = 6;
@@ -134,7 +144,7 @@ void EnMThunder_Init(Actor* thisx, PlayState* play) {
             }
         } else {
             player->unk_B08[0] = 0.5f;
-            this->unk144.info.toucher.damage = D_808B714C[this->unk1BF];
+            this->collider.info.toucher.damage = sDamages[this->unk1BF];
             this->unk1BE = 1;
             if (this->unk1BF == 3) {
                 this->unk1C1 = 4;
@@ -144,35 +154,40 @@ void EnMThunder_Init(Actor* thisx, PlayState* play) {
                 this->unk1C1 = 2;
             }
         }
-        if (player->meleeWeaponAnimation < 30) {
+
+        if (player->meleeWeaponAnimation < PLAYER_MWA_SPIN_ATTACK_1H) {
             this->unk1BE += 2;
             this->actionFunc = func_808B60D4;
             this->unk1BC = 1;
             this->unk1C1 = 12;
-            this->unk144.info.toucher.dmgFlags = 0x02000000;
-            this->unk144.info.toucher.damage = 3;
+            this->collider.info.toucher.dmgFlags = 0x02000000;
+            this->collider.info.toucher.damage = 3;
         } else {
             this->actionFunc = func_808B5F68;
             this->unk1BC = 8;
         }
+
         Audio_PlaySfxGeneral(NA_SE_IT_ROLLING_CUT_LV1, &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
                              &gSfxDefaultReverb);
+
         this->unk1A4 = 1.0f;
     } else {
         this->actionFunc = func_808B5984;
     }
+
     this->actor.child = NULL;
 }
 
 void EnMThunder_Destroy(Actor* thisx, PlayState* play) {
     EnMThunder* this = THIS;
 
-    if (this->unk1C2 != 0) {
+    if (this->unk1C2) {
         func_80115D5C(&play->state);
     }
-    Collider_DestroyCylinder(play, &this->unk144);
+
+    Collider_DestroyCylinder(play, &this->collider);
     func_808B5890(play, 0.0f);
-    LightContext_RemoveLight(play, &play->lightCtx, this->unk190);
+    LightContext_RemoveLight(play, &play->lightCtx, this->lightNode);
 }
 
 void func_808B5890(PlayState* play, f32 arg1) {
@@ -183,7 +198,7 @@ void func_808B58CC(EnMThunder* this, PlayState* play) {
     Player* player = GET_PLAYER(play);
 
     if (player->stateFlags2 & PLAYER_STATE2_20000) {
-        if (player->meleeWeaponAnimation >= 30) {
+        if (player->meleeWeaponAnimation >= PLAYER_MWA_SPIN_ATTACK_1H) {
             Audio_PlaySfxGeneral(NA_SE_IT_ROLLING_CUT, &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
                                  &gSfxDefaultReverb);
             Audio_PlaySfxGeneral(NA_SE_IT_SWORD_SWING_HARD, &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
@@ -203,7 +218,7 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
     this->actor.world.pos = player->bodyPartsPos[0];
     this->actor.shape.rot.y = player->actor.shape.rot.y + 0x8000;
 
-    if ((this->unk1C2 == 0) && (player->unk_B08[0] >= 0.1f)) {
+    if (!this->unk1C2 && (player->unk_B08[0] >= 0.1f)) {
         if ((gSaveContext.unk_3F28 != 0) || ((ENMTHUNDER_GET_MAGIC_COST(&this->actor) != 0) &&
                                              !func_80115DB4(play, ENMTHUNDER_GET_MAGIC_COST(&this->actor), 4))) {
             func_808B58CC(this, play);
@@ -213,7 +228,7 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
             this->unk1A4 = 0.0f;
             return;
         }
-        this->unk1C2 = 1;
+        this->unk1C2 = true;
     }
 
     if (player->unk_B08[0] >= 0.1f) {
@@ -224,8 +239,9 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
         if ((child != NULL) && (child->update != NULL)) {
             child->parent = NULL;
         }
+
         if (player->unk_B08[0] <= 0.15f) {
-            if ((player->unk_B08[0] >= 0.1f) && (player->meleeWeaponAnimation >= 30)) {
+            if ((player->unk_B08[0] >= 0.1f) && (player->meleeWeaponAnimation >= PLAYER_MWA_SPIN_ATTACK_1H)) {
                 Audio_PlaySfxGeneral(NA_SE_IT_ROLLING_CUT, &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
                                      &gSfxDefaultReverb);
                 Audio_PlaySfxGeneral(NA_SE_IT_SWORD_SWING_HARD, &player->actor.projectedPos, 4, &D_801DB4B0,
@@ -234,12 +250,15 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
             Actor_MarkForDeath(&this->actor);
             return;
         }
+
         player->stateFlags2 &= ~PLAYER_STATE2_20000;
+
         if (ENMTHUNDER_GET_MAGIC_COST(&this->actor) != 0) {
             gSaveContext.unk_3F28 = 1;
         }
+
         if (player->unk_B08[0] < 0.85f) {
-            this->unk144.info.toucher.damage = D_808B714C[this->unk1BF];
+            this->collider.info.toucher.damage = sDamages[this->unk1BF];
             this->unk1BE = 1;
             if (this->unk1BF == 3) {
                 this->unk1C1 = 4;
@@ -249,7 +268,7 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
                 this->unk1C1 = 2;
             }
         } else {
-            this->unk144.info.toucher.damage = D_808B714C[this->unk1BF + 4];
+            this->collider.info.toucher.damage = sDamages[this->unk1BF + 4];
             this->unk1BE = 0;
             if (this->unk1BF == 3) {
                 this->unk1C1 = 6;
@@ -259,7 +278,8 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
                 this->unk1C1 = 3;
             }
         }
-        if (player->meleeWeaponAnimation < 30) {
+
+        if (player->meleeWeaponAnimation < PLAYER_MWA_SPIN_ATTACK_1H) {
             this->unk1BE += 2;
             this->actionFunc = func_808B60D4;
             this->unk1BC = 1;
@@ -267,9 +287,12 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
             this->actionFunc = func_808B5F68;
             this->unk1BC = 8;
         }
-        Audio_PlaySfxGeneral(D_808B7154[this->unk1BE], &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
+
+        Audio_PlaySfxGeneral(sSounds[this->unk1BE], &player->actor.projectedPos, 4, &D_801DB4B0, &D_801DB4B0,
                              &gSfxDefaultReverb);
+
         this->unk1A4 = 1.0f;
+
         return;
     }
 
@@ -282,18 +305,20 @@ void func_808B5984(EnMThunder* this, PlayState* play) {
     }
 
     if (player->unk_B08[0] > 0.15f) {
-        this->unk1C0 = 0xFF;
+        this->unk1C0 = 255;
         if (this->actor.child == NULL) {
             Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, ACTOR_EFF_DUST, this->actor.world.pos.x,
-                               this->actor.world.pos.y, this->actor.world.pos.z, 0, this->actor.shape.rot.y, 0, 2);
+                               this->actor.world.pos.y, this->actor.world.pos.z, 0, this->actor.shape.rot.y, 0,
+                               EFF_DUST_TYPE_2);
         }
-        this->unk1B4 += (((player->unk_B08[0] - 0.15f) * 1.5f) - this->unk1B4) * 0.5f;
+        this->unk1B4 += (((player->unk_B08[0] - 0.15f) * 1.5f) - this->unk1B4) * 0.5f; // LERP
     } else if (player->unk_B08[0] > .1f) {
         this->unk1C0 = (s32)((player->unk_B08[0] - .1f) * 255.0f * 20.0f);
         this->unk1A4 = (player->unk_B08[0] - .1f) * 10.0f;
     } else {
         this->unk1C0 = 0;
     }
+
     if (player->unk_B08[0] > 0.85f) {
         func_8019F900(&player->actor.projectedPos, 2);
     } else if (player->unk_B08[0] > 0.15f) {
@@ -333,9 +358,9 @@ void func_808B5F68(EnMThunder* this, PlayState* play) {
     } else {
         Math_SmoothStepToF(&this->actor.scale.x, (s32)this->unk1C1, 0.6f, 0.8f, 0.0f);
         Actor_SetScale(&this->actor, this->actor.scale.x);
-        this->unk144.dim.radius = this->actor.scale.x * 30.0f;
-        Collider_UpdateCylinder(&this->actor, &this->unk144);
-        CollisionCheck_SetAT(play, &play->colChkCtx, &this->unk144.base);
+        this->collider.dim.radius = this->actor.scale.x * 30.0f;
+        Collider_UpdateCylinder(&this->actor, &this->collider);
+        CollisionCheck_SetAT(play, &play->colChkCtx, &this->collider.base);
     }
 
     if (this->unk1BC > 0) {
@@ -366,6 +391,7 @@ void func_808B60D4(EnMThunder* this, PlayState* play) {
     } else {
         this->unk1A8 = this->unk1A4 * (10.0f / 9.0f);
     }
+
     if (Math_StepToF(&this->unk1A4, 0.0f, 0.05f)) {
         Actor_MarkForDeath(&this->actor);
     } else {
@@ -378,23 +404,25 @@ void func_808B60D4(EnMThunder* this, PlayState* play) {
         Math_SmoothStepToF(&this->actor.scale.x, this->unk1C1, 0.6f, 2.0f, 0.0f);
         Actor_SetScale(&this->actor, this->actor.scale.x);
 
-        this->unk144.dim.radius = this->actor.scale.x * 5.0f;
+        this->collider.dim.radius = this->actor.scale.x * 5.0f;
 
-        this->unk144.dim.pos.x = this->actor.world.pos.x;
-        this->unk144.dim.pos.y = this->actor.world.pos.y;
-        this->unk144.dim.pos.z = this->actor.world.pos.z;
+        this->collider.dim.pos.x = this->actor.world.pos.x;
+        this->collider.dim.pos.y = this->actor.world.pos.y;
+        this->collider.dim.pos.z = this->actor.world.pos.z;
 
-        this->unk144.dim.pos.x =
+        this->collider.dim.pos.x =
             (Math_SinS(this->actor.shape.rot.y) * -5.0f * this->actor.scale.x) + this->actor.world.pos.x;
-        this->unk144.dim.pos.y = this->actor.world.pos.y;
-        this->unk144.dim.pos.z =
+        this->collider.dim.pos.y = this->actor.world.pos.y;
+        this->collider.dim.pos.z =
             (Math_CosS(this->actor.shape.rot.y) * -5.0f * this->actor.scale.z) + this->actor.world.pos.z;
 
-        CollisionCheck_SetAT(play, &play->colChkCtx, &this->unk144.base);
+        CollisionCheck_SetAT(play, &play->colChkCtx, &this->collider.base);
     }
+
     if (this->unk1BC > 0) {
         this->unk1BC--;
     }
+
     func_808B5EEC(this, play);
 }
 
@@ -411,6 +439,7 @@ void func_808B6310(EnMThunder* this, PlayState* play) {
     } else {
         this->unk1A8 = this->unk1A4 * (10.0f / 6.0f);
     }
+
     func_808B5EEC(this, play);
 }
 
@@ -419,18 +448,18 @@ void EnMThunder_Update(Actor* thisx, PlayState* play) {
 
     this->actionFunc(this, play);
     func_808B5890(play, this->unk1B4);
-    Lights_PointNoGlowSetInfo(&this->unk194, this->actor.world.pos.x, this->actor.world.pos.y, this->actor.world.pos.z,
-                              this->unk1A4 * 255.0f, this->unk1A4 * 255.0f, this->unk1A4 * 100.0f,
-                              this->unk1A4 * 800.0f);
+    Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.world.pos.x, this->actor.world.pos.y,
+                              this->actor.world.pos.z, this->unk1A4 * 255.0f, this->unk1A4 * 255.0f,
+                              this->unk1A4 * 100.0f, this->unk1A4 * 800.0f);
 }
 
 void func_808B65BC(Actor* thisx, PlayState* play) {
     EnMThunder* this = THIS;
 
     this->actionFunc(this, play);
-    Lights_PointNoGlowSetInfo(&this->unk194, this->actor.world.pos.x, this->actor.world.pos.y, this->actor.world.pos.z,
-                              this->unk1A4 * 255.0f, this->unk1A4 * 255.0f, this->unk1A4 * 100.0f,
-                              this->unk1A4 * 800.0f);
+    Lights_PointNoGlowSetInfo(&this->lightInfo, this->actor.world.pos.x, this->actor.world.pos.y,
+                              this->actor.world.pos.z, this->unk1A4 * 255.0f, this->unk1A4 * 255.0f,
+                              this->unk1A4 * 100.0f, this->unk1A4 * 800.0f);
 }
 
 void EnMThunder_Draw(Actor* thisx, PlayState* play2) {
@@ -438,7 +467,7 @@ void EnMThunder_Draw(Actor* thisx, PlayState* play2) {
     EnMThunder* this = THIS;
     Player* player = GET_PLAYER(play);
     f32 scale;
-    s32 spA4;
+    s32 y2Scroll;
 
     OPEN_DISPS(play->state.gfxCtx);
 
@@ -506,16 +535,17 @@ void EnMThunder_Draw(Actor* thisx, PlayState* play2) {
         Matrix_Scale(-0.7f, -0.6f, -0.4f, MTXMODE_APPLY);
         Matrix_RotateXS(0x4000, MTXMODE_APPLY);
     }
+
     if (this->unk1B0 >= 0.85f) {
-        scale = (D_808B715C[play->gameplayFrames & 7] * 6.0f) + 1.0f;
+        scale = (sScales[play->gameplayFrames & 7] * 6.0f) + 1.0f;
         gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, 255, 255, 170, this->unk1C0);
         gDPSetEnvColor(POLY_XLU_DISP++, 255, 100, 0, 128);
-        spA4 = 40;
+        y2Scroll = 40;
     } else {
-        scale = (D_808B715C[play->gameplayFrames & 7] * 2.0f) + 1.0f;
+        scale = (sScales[play->gameplayFrames & 7] * 2.0f) + 1.0f;
         gDPSetPrimColor(POLY_XLU_DISP++, 0, 0x80, 170, 255, 255, this->unk1C0);
         gDPSetEnvColor(POLY_XLU_DISP++, 0, 100, 255, 128);
-        spA4 = 20;
+        y2Scroll = 20;
     }
 
     Matrix_Scale(1.0f, scale, scale, MTXMODE_APPLY);
@@ -523,7 +553,7 @@ void EnMThunder_Draw(Actor* thisx, PlayState* play2) {
     gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     gSPSegment(POLY_XLU_DISP++, 0x09,
                Gfx_TwoTexScroll(play->state.gfxCtx, 0, (play->gameplayFrames * 5) & 0xFF, 0, 32, 32, 1,
-                                (play->gameplayFrames * 20) & 0xFF, (play->gameplayFrames * spA4) & 0xFF, 8, 8));
+                                (play->gameplayFrames * 20) & 0xFF, (play->gameplayFrames * y2Scroll) & 0xFF, 8, 8));
     gSPDisplayList(POLY_XLU_DISP++, gameplay_keep_DL_0268F0);
 
     CLOSE_DISPS(play->state.gfxCtx);
--- src/overlays/actors/ovl_En_M_Thunder/z_en_m_thunder.h
+++ src/overlays/actors/ovl_En_M_Thunder/z_en_m_thunder.h
@@ -12,23 +12,21 @@ typedef void (*EnMThunderActionFunc)(struct EnMThunder*, PlayState*);
 
 typedef struct EnMThunder {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ ColliderCylinder unk144;
-    /* 0x190 */ LightNode* unk190;
-    /* 0x194 */ LightInfo unk194;
-    /* 0x1A2 */ char pad1A2[2];
-    /* 0x1A4 */ f32 unk1A4;
-    /* 0x1A8 */ f32 unk1A8;
-    /* 0x1AC */ f32 unk1AC;
-    /* 0x1B0 */ f32 unk1B0;
-    /* 0x1B4 */ f32 unk1B4;
+    /* 0x144 */ ColliderCylinder collider;
+    /* 0x190 */ LightNode* lightNode;
+    /* 0x194 */ LightInfo lightInfo;
+    /* 0x1A4 */ f32 unk1A4; // ligh color scale/proportion
+    /* 0x1A8 */ f32 unk1A8; // some alpha scale/proportion
+    /* 0x1AC */ f32 unk1AC; // scroll param
+    /* 0x1B0 */ f32 unk1B0; // same as unk_B08[0] (should be just unk_B08)
+    /* 0x1B4 */ f32 unk1B4; // func_800FD2B4 arg1
     /* 0x1B8 */ EnMThunderActionFunc actionFunc;
-    /* 0x1BC */ u16 unk1BC;
-    /* 0x1BE */ u8 unk1BE;
-    /* 0x1BF */ u8 unk1BF;
-    /* 0x1C0 */ u8 unk1C0;
-    /* 0x1C1 */ u8 unk1C1;
-    /* 0x1C2 */ u8 unk1C2;
-    /* 0x1C3 */ char pad1C3[1];
+    /* 0x1BC */ u16 unk1BC; // timer of some kind
+    /* 0x1BE */ u8 unk1BE; // choses sound/graphics
+    /* 0x1BF */ u8 unk1BF; // type? comes from params
+    /* 0x1C0 */ u8 unk1C0; // alpha of some kind
+    /* 0x1C1 */ u8 unk1C1; // scaleTarget
+    /* 0x1C2 */ u8 unk1C2; // boolean
 } EnMThunder; // size = 0x1C4
 
 extern const ActorInit En_M_Thunder_InitVars;
