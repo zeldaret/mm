e595c0b95c1132501d3df954a0d70fc556e6b957
mzxrules <mzxrules@gmail.com>
2022-09-14 14:07:42 -0400

    sys_cmpdata OK

--- src/code/sys_cmpdma.c
+++ src/code/sys_cmpdma.c
@@ -1,39 +1,6 @@
 #include "global.h"
 #include "system_malloc.h"
 
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178750.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178818.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178978.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_801789D4.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/SysCfb_GetFbPtr.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/SysCfb_GetZBuffer.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178A24.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178A34.s")
-
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178A94.s")
-
-typedef union {
-    struct {
-        u8 r;
-        u8 g;
-        u8 b;
-        u8 a;
-    };
-    s32 rgba;
-} Struct1_80178AC0;
-
-typedef union {
-    u16 a, b;
-    s32 ab;
-} Struct2_80178AC0;
-
 typedef struct {
     union {
         u32 dmaWord[2];
@@ -46,133 +13,105 @@ typedef struct {
     };
 } CmpDmaBuffer;
 
-// static CmpDmaData sCmpDmaBuffer;
-extern CmpDmaBuffer sCmpDmaBuffer;
+static CmpDmaBuffer sDmaBuffer;
 
-#ifdef NON_MATCHING
-// Some constants re-ordered at the top
-void func_80178AC0(u16* src, void* dst, u32 size) {
-    Struct1_80178AC0 spC;
-    Struct2_80178AC0 sp8;
-    Struct2_80178AC0 sp4;
-    s32* phi_v0 = dst;
-    u16* phi_v1 = src;
-    u32 tempsp4;
-    u32 tempsp8;
-    s32* phi_a0;
+void func_80178AC0(u16 *src, void *dst, size_t size) {
+  Color_RGBA8_u32 spC;
+  Color_RGBA16_2 tc;
+  Color_RGBA14 tc2;
+  u32 *dstCur = dst;
+  u16 *src16 = src;
 
-    if ((u32)((u8*)dst - (size & 0xFFFFFFFFFFFFFFFF)) >= (u32)dst) {
-        return;
+  while(((u32) dstCur) -  size < ((u32) dst)) {
+    tc.rgba = *(src16++);
+    if (tc.a == 1) {
+      spC.r = (tc.r * 255) / 31;
+      spC.g = (tc.g * 255) / 31;
+      spC.b = (tc.b * 255) / 31;
+      spC.a = 255;
+    } else if (tc.rgba == 0) {
+      spC.rgba = 0;
+    } else {
+      tc2.rgba = tc.rgba;
+      tc.rgba = *(src16++);
+      spC.r = (tc.r << 3) | tc2.r;
+      spC.g = (tc.g << 3) | tc2.g;
+      spC.b = (tc.b << 3) | tc2.b;
+      spC.a = ((tc.rgba & 1) << 7) | ((tc2.a * 127) / 63);
     }
-
-    phi_a0 = (s32*)((u8*)dst - size);
-
-    do {
-        sp8.a = *(phi_v1++);
-        phi_a0++;
-
-        if ((sp8.a & 1) == 1) {
-            tempsp8 = sp8.ab;
-
-            spC.r = ((tempsp8 >> 0x1B) * 0xFF) / 31;
-            spC.g = (((tempsp8 << 5) >> 0x1B) * 0xFF) / 31;
-            spC.b = (((tempsp8 << 0xA) >> 0x1B) * 0xFF) / 31;
-            spC.a = 0xFF;
-
-        } else if (sp8.a == 0) {
-            spC.rgba = 0;
-
-        } else {
-            sp4.a = sp8.a;
-            sp8.a = *(phi_v1++);
-
-            tempsp4 = sp4.ab;
-            tempsp8 = sp8.ab;
-
-            spC.r = ((tempsp8 >> 0x1B) * 8) | (tempsp4 >> 0x1D);
-            spC.g = (((tempsp8 << 5) >> 0x1B) * 8) | ((tempsp4 * 8) >> 0x1D);
-            spC.b = (((tempsp8 << 0xA) >> 0x1B) * 8) | ((tempsp4 << 6) >> 0x1D);
-            tempsp8 = sp8.a;
-            spC.a = ((tempsp8 & 1) << 7) | ((((tempsp4 << 9) >> 0x1B) * 0x7F) / 63);
-        }
-
-        *(phi_v0++) = spC.rgba;
-    } while ((u32)phi_a0 < (u32)dst);
+    *(dstCur++) = spC.rgba;
+  } 
 }
-#else
-void func_80178AC0(u16* src, void* dst, u32 size);
-#pragma GLOBAL_ASM("asm/non_matchings/code/sys_cmpdma/func_80178AC0.s")
-#endif
 
-void func_80178C80(u8* fileRomStart, s32 id, s32* start, s32* size, s32* flag) {
+void CmpDma_GetFileInfo(u8* segmentRom, s32 id, uintptr_t* outFileRom, size_t* size, s32* flag) {
     u32 dataStart;
     u32 refOff;
 
-    DmaMgr_DmaRomToRam(fileRomStart, (void*)&sCmpDmaBuffer.dataStart, sizeof(sCmpDmaBuffer.dataStart));
+    DmaMgr_DmaRomToRam(segmentRom, &sDmaBuffer.dataStart, sizeof(sDmaBuffer.dataStart));
 
-    dataStart = sCmpDmaBuffer.dataStart;
+    dataStart = sDmaBuffer.dataStart;
     refOff = id * sizeof(u32);
 
     // if id is >= idMax
     if (refOff > (dataStart - 4)) {
-        *start = fileRomStart;
+        *outFileRom = segmentRom;
         *size = 0;
     } else if (refOff == 0) {
         // get offset start of next file, i.e. size of first file
-        DmaMgr_DmaRomToRam(fileRomStart + 4, (void*)&sCmpDmaBuffer.dataSize, sizeof(sCmpDmaBuffer.dataSize));
-        *start = fileRomStart + dataStart;
-        *size = sCmpDmaBuffer.dataSize;
+        DmaMgr_DmaRomToRam(segmentRom + 4, &sDmaBuffer.dataSize, sizeof(sDmaBuffer.dataSize));
+        *outFileRom = segmentRom + dataStart;
+        *size = sDmaBuffer.dataSize;
     } else {
         // get offset start, end from dataStart
-        DmaMgr_DmaRomToRam(refOff + fileRomStart, (void*)&sCmpDmaBuffer.offset, sizeof(sCmpDmaBuffer.offset));
-        *start = sCmpDmaBuffer.offset.start + fileRomStart + dataStart;
-        *size = sCmpDmaBuffer.offset.end - sCmpDmaBuffer.offset.start;
+        DmaMgr_DmaRomToRam(refOff + segmentRom, &sDmaBuffer.offset, sizeof(sDmaBuffer.offset));
+        *outFileRom = sDmaBuffer.offset.start + segmentRom + dataStart;
+        *size = sDmaBuffer.offset.end - sDmaBuffer.offset.start;
     }
     *flag = 0;
 }
 
-void func_80178D7C(s32 romStart, s32 size, void* dst) {
+void CmpDma_Decompress(uintptr_t romStart, size_t size, void* dst) {
     if (size != 0) {
         Yaz0_Decompress(romStart, dst, size);
     }
 }
 
-void func_80178DAC(u32 rom, s32 id, void* dst, s32 size) {
-    s32 romStart;
-    s32 compressedSize;
+void CmpDma_LoadFileImpl(uintptr_t segmentRom, s32 id, void* dst, size_t size) {
+    uintptr_t romStart;
+    size_t compressedSize;
     s32 flag;
 
-    func_80178C80(rom, id, &romStart, &compressedSize, &flag);
+    CmpDma_GetFileInfo(segmentRom, id, &romStart, &compressedSize, &flag);
     if (flag & 1) {
-        void* temp = SystemArena_Malloc(0x1000);
+        void* tempBuf = SystemArena_Malloc(0x1000);
 
-        func_80178D7C(romStart, compressedSize, temp);
-        func_80178AC0(temp, dst, size);
-        SystemArena_Free(temp);
+        CmpDma_Decompress(romStart, compressedSize, tempBuf);
+        func_80178AC0(tempBuf, dst, size);
+        SystemArena_Free(tempBuf);
     } else {
-        func_80178D7C(romStart, compressedSize, dst);
+        CmpDma_Decompress(romStart, compressedSize, dst);
     }
 }
 
-void func_80178E3C(uintptr_t vrom, s32 id, void* dst, s32 size) {
-    func_80178DAC(DmaMgr_TranslateVromToRom(vrom), id, dst, size);
+void CmpDma_LoadFile(uintptr_t segmentVrom, s32 id, void* dst, size_t size) {
+    CmpDma_LoadFileImpl(DmaMgr_TranslateVromToRom(segmentVrom), id, dst, size);
 }
 
-void func_80178E7C(uintptr_t vrom, void* dst, s32 arg2) {
-    uintptr_t rom = DmaMgr_TranslateVromToRom(vrom);
+void CmpDma_LoadAllFiles(uintptr_t segmentVrom, void* dst, size_t size) {
+    uintptr_t rom = DmaMgr_TranslateVromToRom(segmentVrom);
     u32 i;
     u32 end;
     void* nextDst;
-    u32 var;
+    u32 dataStart;
 
-    DmaMgr_DmaRomToRam(rom, (void*)&sCmpDmaBuffer.dmaWord[0], sizeof(sCmpDmaBuffer.dmaWord[0]));
+    DmaMgr_DmaRomToRam(rom, &sDmaBuffer.dataStart, sizeof(sDmaBuffer.dataStart));
 
-    var = sCmpDmaBuffer.dmaWord[0];
+    dataStart = sDmaBuffer.dataStart;
     nextDst = dst;
-    end = (var >> 2) - 1;
+    end = (dataStart / sizeof(u32)) - 1;
 
     for (i = 0; i < end; i++) {
-        func_80178DAC(rom, i, nextDst, 0);
+        CmpDma_LoadFileImpl(rom, i, nextDst, 0);
         nextDst = gYaz0DecompressDstEnd;
     }
 }
