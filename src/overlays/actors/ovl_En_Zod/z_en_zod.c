3bda64704e5286ca66c667c0c05cd3c2cfb77e2f
sonicdcer <alejandro.asenjo88@gmail.com>
2022-09-20 05:47:57 -0300

    func_80BAF1EC matching

--- src/overlays/actors/ovl_En_Zod/z_en_zod.c
+++ src/overlays/actors/ovl_En_Zod/z_en_zod.c
@@ -15,6 +15,12 @@ void EnZod_Destroy(Actor* thisx, PlayState* play);
 void EnZod_Update(Actor* thisx, PlayState* play);
 void EnZod_Draw(Actor* thisx, PlayState* play);
 
+void func_80BAF338(EnZod* this, u32 arg1, u32 arg2, u16 arg3);
+void func_80BAF99C(EnZod* this, PlayState* play); /* extern */
+void func_80BAFB84(EnZod* this, PlayState* play); /* extern */
+void func_80BAFDB4(EnZod* this, PlayState* play); /* extern */
+void func_80BAFF14(EnZod* this, PlayState* play);
+
 #if 0
 const ActorInit En_Zod_InitVars = {
     ACTOR_EN_ZOD,
@@ -38,15 +44,117 @@ static ColliderCylinderInit D_80BB0540 = {
 #endif
 
 extern ColliderCylinderInit D_80BB0540;
+extern FlexSkeletonHeader D_0600D658;
 
 extern UNK_TYPE D_06000D94;
 extern UNK_TYPE D_06007650;
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Init.s")
+/*
+void EnZod_Init(Actor* thisx, PlayState* play) {
+    s32 pad;
+    struct EnZod* temp_v0_3;
+    EnZod* this = THIS;
+    ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 60.0f);
+    this->actor.colChkInfo.mass = 0xFF;
+    Actor_SetScale(&this->actor, 0.01f);
+    SkelAnime_InitFlex(play, &this->skelAnime, &D_0600D658, (AnimationHeader*)&D_06000D94, &this->morphTable,
+                       &this->JointTable, 0xA);
+    Animation_PlayLoop(&this->skelAnime, (AnimationHeader*)&D_06000D94);
+    Collider_InitAndSetCylinder(play, &this->collider, &this->actor, &D_80BB0540);
+    this->unk260 = 0;
+    this->unk266 = 0x12C;
+    this->unk25E = this->unk260;
+    this->unk25C = this->unk260;
+    this->unk256 = 0;
+    this->unk25A = -1;
+    this->unk258 = -1;
+    this->actor.textId = 0;
+    this->unk298 = 0;
+    this->actor.terminalVelocity = -4.0f;
+    this->actor.gravity = -4.0f;
+    this->unk264 = this->unk266;
+    this->unk262 = this->unk266;
+    temp_v0_3 = this + (2 * 4);
+    this->unk280 = 0.01f;
+    this->unk284 = 0.01f;
+    this->unk26C = 0.0f;
+    this->unk268 = 0.0f;
+    temp_v0_3->unk28C = 0.01f;
+    temp_v0_3->unk274 = 0.0f;
+    temp_v0_3->unk288 = 0.01f;
+    temp_v0_3->unk270 = 0.0f;
+    temp_v0_3->unk284 = 0.01f;
+    temp_v0_3->unk26C = 0.0f;
+    temp_v0_3->unk268 = 0.0f;
+    temp_v0_3->unk280 = 0.01f;
+    func_80BAF338(this, 3, 2, -1);
+    this->actionFunc = func_80BAF99C;
+    switch (this->actor.params & 0xF) {
+        case 1:
+            if ((gSaveContext.save.weekEventReg[0x4E] & 1) != 0) {
+                this->actionFunc = func_80BAFDB4;
+                func_80BAF338(this, 0, 2, -1);
+                this->actor.flags |= 0x10;
+                ActorCutscene_SetIntentToPlay((s16)this->actor.cutscene);
+                return;
+            }
+            this->actionFunc = func_80BAFB84;
+            if ((gSaveContext.save.weekEventReg[0x37] & 0x80) == 0) {
+                Actor_MarkForDeath(&this->actor);
+                return;
+            }
+            return;
+        case 2:
+            this->actionFunc = func_80BAFF14;
+            this->unk29A = -1;
+            this->unk256 |= 2;
+            return;
+        default:
+            if ((gSaveContext.save.weekEventReg[0x37] & 0x80) != 0) {
+                Actor_MarkForDeath(&this->actor);
+            }
+            this->actor.flags |= 0x10;
+            break;
+    }
+}
+*/
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Destroy.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Destroy.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAF1EC.s")
+void EnZod_Destroy(Actor* thisx, PlayState* play) {
+    EnZod* this = THIS;
+
+    Collider_DestroyCylinder(play, &this->collider);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAF1EC.s")
+
+void func_80BAF1EC(EnZod* this, PlayState* play) {
+    u16 phi_a3;
+    if (gSaveContext.save.playerForm != 2) {
+        phi_a3 = 0x1227;
+        if ((gSaveContext.save.weekEventReg[0x20] & 8) != 0) {
+            phi_a3 = 0x1229;
+        } else {
+            gSaveContext.save.weekEventReg[0x20] |= 8;
+        }
+    } else if ((this->unk256 & 1) != 0) {
+        phi_a3 = 0x1225;
+    } else {
+        phi_a3 = 0x1219;
+        if ((gSaveContext.save.weekEventReg[0x20] & 0x10) != 0) {
+            phi_a3 = 0x1226;
+        } else {
+            gSaveContext.save.weekEventReg[0x20] |= 0x10;
+        }
+        this->unk256 |= 1;
+    }
+    func_80BAF338(this, 0, 2, phi_a3);
+    Message_StartTextbox(play, phi_a3, &this->actor);
+}
+
+
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAF2B4.s")
 
--- src/overlays/actors/ovl_En_Zod/z_en_zod.h
+++ src/overlays/actors/ovl_En_Zod/z_en_zod.h
@@ -9,7 +9,35 @@ typedef void (*EnZodActionFunc)(struct EnZod*, PlayState*);
 
 typedef struct EnZod {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x158];
+    /* 0x144 */ Vec3s morphTable;          /* inferred */
+    /* 0x14A */ char pad14A[0x36];         /* maybe part of unk144[0xA]? */
+    /* 0x180 */ Vec3s JointTable;          /* inferred */
+    /* 0x186 */ char pad186[0x36];         /* maybe part of unk180[0xA]? */
+    /* 0x1BC */ SkelAnime skelAnime;       /* inferred */
+    /* 0x200 */ ColliderCylinder collider; /* inferred */
+    /* 0x24C */ char pad24C[0xA];
+    /* 0x256 */ u16 unk256;
+    /* 0x258 */ s16 unk258;
+    /* 0x25A */ s16 unk25A;
+    /* 0x25C */ s16 unk25C;
+    /* 0x25E */ s16 unk25E;
+    /* 0x260 */ s16 unk260;
+    /* 0x262 */ s16 unk262;
+    /* 0x264 */ s16 unk264;
+    /* 0x266 */ s16 unk266;
+    /* 0x268 */ f32 unk268;
+    /* 0x26C */ f32 unk26C;
+    /* 0x270 */ f32 unk270;
+    /* 0x274 */ f32 unk274;
+    /* 0x278 */ char pad278[0x2];
+    /* 0x280 */ f32 unk280;
+    /* 0x284 */ f32 unk284;
+    /* 0x288 */ f32 unk288;
+    /* 0x28C */ f32 unk28C;
+    char pad[0x2];
+
+    /* 0x298 */ s16 unk298;
+    /* 0x29A */ s16 unk29A; /* inferred */
     /* 0x29C */ EnZodActionFunc actionFunc;
 } EnZod; // size = 0x2A0
 
