aed251eebff8de3130b8cc7ffd0491f601967aa0
sonicdcer <alejandro.asenjo88@gmail.com>
2022-09-21 02:55:10 -0300

    progress

--- src/overlays/actors/ovl_En_Zod/z_en_zod.c
+++ src/overlays/actors/ovl_En_Zod/z_en_zod.c
@@ -15,10 +15,10 @@ void EnZod_Destroy(Actor* thisx, PlayState* play);
 void EnZod_Update(Actor* thisx, PlayState* play);
 void EnZod_Draw(Actor* thisx, PlayState* play);
 
-void func_80BAF338(EnZod* this, s16 arg1, u8 arg2);
-void func_80BAF99C(EnZod* this, PlayState* play); 
-void func_80BAFB84(EnZod* this, PlayState* play); 
-void func_80BAFDB4(EnZod* this, PlayState* play); 
+void func_80BAF338(EnZod* this, s16 arg1, u8 mode);
+void func_80BAF99C(EnZod* this, PlayState* play);
+void func_80BAFB84(EnZod* this, PlayState* play);
+void func_80BAFDB4(EnZod* this, PlayState* play);
 void func_80BAFF14(EnZod* this, PlayState* play);
 s32 func_80BAF2B4(EnZod* this, PlayState* play);
 void func_80BAF3E0(EnZod* this);
@@ -66,6 +66,55 @@ static void *D_80BB062C[5] = { (void *)0x06005E50, (void *)0x06006650, (void *)0
 
 static Vec3f D_80BB0580 = { 1300.0f, 1100.0f, 0.0f };
 
+static f32 D_80BB058C[0xA] = {
+    0.0f,
+    -2690.0f,
+    2310.0f,
+    3888.0f,
+    -4160.0f,
+    -2200.0f,
+    -463.0f,
+    1397.0f,
+    3413.0f,
+    389.0f,
+};
+static f32 D_80BB05B4[0xA] = {
+    0.0f,
+    6335.0f,
+    6703.0f,
+    5735.0f,
+    3098.0f,
+    3349.0f,
+    3748.0f,
+    3718.0f,
+    2980.0f,
+    1530.0f,
+};
+static f32 D_80BB05DC[0xA] = {
+    0.0f,
+    4350.0f,
+    3200.0f,
+    1555.0f,
+    2874.0f,
+    3901.0f,
+    4722.0f,
+    4344.0f,
+    3200.0f,
+    3373.0f,
+};
+static u32 D_80BB0604[0xA] = {
+    0x0600A460,
+    0x0600A550,
+    0x0600A5E0,
+    0x0600A670,
+    0x0600A700,
+    0x0600A8F8,
+    0x0600AAF0,
+    0x0600ACE8,
+    0x0600AEE0,
+    0x0600B0D8,
+};
+
 #endif
 
 extern ColliderCylinderInit D_80BB0540;
@@ -76,50 +125,36 @@ extern UNK_TYPE D_06000D94;
 extern TexturePtr D_06007650;
 extern Vec3f D_80BB0580;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Init.s")
-/*
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Init.s")
 void EnZod_Init(Actor* thisx, PlayState* play) {
-    s32 pad;
-    struct EnZod* temp_v0_3;
-    EnZod* this = THIS;
+    s32 i;
+    EnZod* this = (EnZod*)thisx;
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 60.0f);
     this->actor.colChkInfo.mass = 0xFF;
     Actor_SetScale(&this->actor, 0.01f);
-    SkelAnime_InitFlex(play, &this->skelAnime, &D_0600D658, (AnimationHeader*)&D_06000D94, &this->morphTable,
+    SkelAnime_InitFlex(play, &this->skelAnime, &D_0600D658, (AnimationHeader*)(&D_06000D94), &this->morphTable,
                        &this->JointTable, 0xA);
-    Animation_PlayLoop(&this->skelAnime, (AnimationHeader*)&D_06000D94);
+    Animation_PlayLoop(&this->skelAnime, (AnimationHeader*)(&D_06000D94));
     Collider_InitAndSetCylinder(play, &this->collider, &this->actor, &D_80BB0540);
-    this->unk260 = 0;
-    this->unk266 = 0x12C;
-    this->unk25E = this->unk260;
-    this->unk25C = this->unk260;
+    this->unk25C = this->unk25E = this->unk260 = 0;
+    this->actor.gravity = this->actor.terminalVelocity = -4.0f;
+    this->unk262 = this->unk264 = this->unk266 = 0x12C;
     this->unk256 = 0;
     this->unk25A = -1;
     this->unk258 = -1;
     this->actor.textId = 0;
     this->unk298 = 0;
-    this->actor.terminalVelocity = -4.0f;
-    this->actor.gravity = -4.0f;
-    this->unk264 = this->unk266;
-    this->unk262 = this->unk266;
-    temp_v0_3 = this + (2 * 4);
-    this->unk280 = 0.01f;
-    this->unk284 = 0.01f;
-    this->unk26C = 0.0f;
-    this->unk268 = 0.0f;
-    temp_v0_3->unk28C = 0.01f;
-    temp_v0_3->unk274 = 0.0f;
-    temp_v0_3->unk288 = 0.01f;
-    temp_v0_3->unk270 = 0.0f;
-    temp_v0_3->unk284 = 0.01f;
-    temp_v0_3->unk26C = 0.0f;
-    temp_v0_3->unk268 = 0.0f;
-    temp_v0_3->unk280 = 0.01f;
-    func_80BAF338(this, 3, 2);
+
+    for(i = 0; i < 6; i++) {
+        this->unk268[i] = 0.0f;
+        this->unk280[i] = 0.01;
+    }
+
+    func_80BAF338(this, 3, 2U);
     this->actionFunc = func_80BAF99C;
     switch (this->actor.params & 0xF) {
         case 1:
-            if ((gSaveContext.save.weekEventReg[0x4E] & 1) != 0) {
+            if ((gSaveContext.save.weekEventReg[0x4E] & 1)) {
                 this->actionFunc = func_80BAFDB4;
                 func_80BAF338(this, 0, 2);
                 this->actor.flags |= 0x10;
@@ -127,16 +162,18 @@ void EnZod_Init(Actor* thisx, PlayState* play) {
                 return;
             }
             this->actionFunc = func_80BAFB84;
-            if ((gSaveContext.save.weekEventReg[0x37] & 0x80) == 0) {
+            if (!(gSaveContext.save.weekEventReg[0x37] & 0x80)) {
                 Actor_MarkForDeath(&this->actor);
                 return;
             }
             return;
+
         case 2:
             this->actionFunc = func_80BAFF14;
             this->unk29A = -1;
             this->unk256 |= 2;
             return;
+
         default:
             if ((gSaveContext.save.weekEventReg[0x37] & 0x80) != 0) {
                 Actor_MarkForDeath(&this->actor);
@@ -145,7 +182,6 @@ void EnZod_Init(Actor* thisx, PlayState* play) {
             break;
     }
 }
-*/
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Destroy.s")
 
@@ -193,11 +229,11 @@ s32 func_80BAF2B4(EnZod* this, PlayState* play) {
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAF338.s")
 
-void func_80BAF338(EnZod* this, s16 arg1, u8 arg2) {
+void func_80BAF338(EnZod* this, s16 arg1, u8 mode) {
     if ((arg1 < 0) || (arg1 >= 5)) {
         arg1 = 3;
     }
-    Animation_Change(&this->skelAnime, D_80BB056C[arg1], 1.0f, 0.0f, Animation_GetLastFrame(D_80BB056C[arg1]), arg2,
+    Animation_Change(&this->skelAnime, D_80BB056C[arg1], 1.0f, 0.0f, Animation_GetLastFrame(D_80BB056C[arg1]), mode,
                      -5.0f);
     this->unk258 = arg1;
     this->unk25A = arg1;
@@ -311,7 +347,7 @@ void func_80BAF99C(EnZod* this, PlayState* play) {
     sp20.y = this->actor.projectedPos.y;
     sp20.z = this->actor.projectedPos.z;
 
-    func_801A1FB4(3, &sp20, 0x6DU, 700.0f);
+    func_801A1FB4(3, &sp20, 0x6D, 700.0f);
 }
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAFA44.s")
@@ -369,7 +405,10 @@ void func_80BAFB84(EnZod* this, PlayState* play) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAFC00.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAFC00.s")
+
+void func_80BAFC00(EnZod *this, PlayState *play) {
+}
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BAFC10.s")
 
@@ -506,7 +545,7 @@ void EnZod_Update(Actor* thisx, PlayState* play) {
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BB0128.s")
 
-s32 func_80BB0128(PlayState *play, s32 limbIndex, Gfx **dList, Vec3f *pos, Vec3s *rot, Actor *thisx) {
+s32 func_80BB0128(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx) {
     EnZod* this = THIS;
 
     if (limbIndex == 3) {
@@ -518,13 +557,132 @@ s32 func_80BB0128(PlayState *play, s32 limbIndex, Gfx **dList, Vec3f *pos, Vec3s
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BB0170.s")
 
-void func_80BB0170(PlayState *play, s32 limbIndex, Gfx **dList, Vec3s *rot, Actor *thisx) {
+void func_80BB0170(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* rot, Actor* thisx) {
     if (limbIndex == 3) {
         Matrix_MultVec3f(&D_80BB0580, &thisx->focus.pos);
     }
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BB01B0.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/func_80BB01B0.s")
+
+typedef struct unkStruct {
+    f32 unk_0;
+    f32 unk_4;
+    f32 unk_8;
+    f32 unk_C;
+} unkStruct;
+extern f32 D_80BB058C[];
+extern f32 D_80BB05B4[];
+extern f32 D_80BB05DC[];
+extern u32 D_80BB0604[];
+void func_80BB01B0(EnZod *this, PlayState *play) {
+    f32 spC4;
+    f32 sp9C;
+    f32 sp74;
+    Gfx *temp_v0;
+    Gfx *temp_v0_2;
+    GraphicsContext *temp_s2;
+    unkStruct *temp_t0;
+    unkStruct *temp_t1;
+    unkStruct *temp_t2;
+    unkStruct *temp_t6;
+    unkStruct *temp_t7;
+    unkStruct *temp_t7_2;
+    f32 temp_fa0;
+    s32 temp_s1;
+    unkStruct *phi_t7;
+    f32 *phi_t6;
+    f32 *phi_t2;
+    f32 *phi_t1;
+    f32 *phi_t7_2;
+    f32 *phi_t0;
+    f32 *phi_s4;
+    f32 *phi_s5;
+    f32 *phi_s6;
+    s32 phi_s1;
+    s32 phi_s3;
+    u32 *phi_s7;
+
+    phi_t7 = D_80BB058C;
+    phi_t6 = &spC4;
+    do {
+        temp_t7 = phi_t7 + 0xC;
+        temp_t6 = phi_t6 + 0xC;
+        temp_t6->unk_C =  *phi_t7;
+        temp_t6->unk_8 = (s32) temp_t7->unk_8;
+        temp_t6->unk_4 = (s32) temp_t7->unk_4;
+        phi_t7 = temp_t7;
+        phi_t6 = temp_t6;
+    } while (temp_t7 != (D_80BB058C + 0x24));
+    temp_t6->unk_0 = temp_t7->unk_0;
+    phi_t2 = D_80BB05B4;
+    phi_t1 = &sp9C;
+    do {
+        temp_t2 = phi_t2 + 0xC;
+        temp_t1 = phi_t1 + 0xC;
+        temp_t1->unk_C = (f32) *phi_t2;
+        temp_t1->unk_8 = (s32) temp_t2->unk_8;
+        temp_t1->unk_4 = (s32) temp_t2->unk_4;
+        phi_t2 = temp_t2;
+        phi_t1 = temp_t1;
+    } while (temp_t2 != (D_80BB05B4 + 0x24));
+    temp_t1->unk_0 = temp_t2->unk_0;
+    phi_t7_2 = D_80BB05DC;
+    phi_t0 = &sp74;
+    do {
+        temp_t7_2 = phi_t7_2 + 0xC;
+        temp_t0 = phi_t0 + 0xC;
+        temp_t0->unk_C = (f32) *phi_t7_2;
+        temp_t0->unk_8 = (s32) temp_t7_2->unk_8;
+        temp_t0->unk_4 = (s32) temp_t7_2->unk_4;
+        phi_t7_2 = temp_t7_2;
+        phi_t0 = temp_t0;
+    } while (temp_t7_2 != (D_80BB05DC + 0x24));
+    temp_t0->unk_0 = temp_t7_2->unk_0;
+    temp_s2 = play->state.gfxCtx;
+    phi_s4 = &spC4;
+    phi_s5 = &sp9C;
+    phi_s6 = &sp74;
+    phi_s1 = 0;
+    phi_s3 = 0;
+    phi_s7 = D_80BB0604;
+    do {
+        Matrix_Push();
+        Matrix_Translate(*phi_s4, *phi_s5, *phi_s6, MTXMODE_APPLY);
+        switch (phi_s1) {
+        case 1:
+        case 2:
+        case 3:
+            Matrix_RotateXS((this + (phi_s1 * 2))->unk25A, MTXMODE_APPLY);
+            break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            temp_fa0 = (this + -phi_s3)->unk28C + 1.0f;
+            Matrix_Scale(temp_fa0, temp_fa0, temp_fa0, MTXMODE_APPLY);
+            break;
+        }
+        temp_v0 = temp_s2->polyOpa.p;
+        temp_s2->polyOpa.p = temp_v0 + 8;
+        temp_v0->words.w0 = 0xDA380003;
+        temp_v0->words.w1 = Matrix_NewMtx(play->state.gfxCtx);
+        temp_v0_2 = temp_s2->polyOpa.p;
+        temp_s2->polyOpa.p = temp_v0_2 + 8;
+        temp_v0_2->words.w0 = 0xDE000000;
+        temp_v0_2->words.w1 = *phi_s7;
+        Matrix_Pop();
+        temp_s1 = phi_s1 + 1;
+        phi_s4 += 4;
+        phi_s5 += 4;
+        phi_s6 += 4;
+        phi_s1 = temp_s1;
+        phi_s3 += 4;
+        phi_s7 += 4;
+    } while (temp_s1 != 0xA);
+}
 
 //#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Zod/EnZod_Draw.s")
 
@@ -537,16 +695,16 @@ void EnZod_Draw(Actor* thisx, PlayState* play) {
     func_8012C28C(play->state.gfxCtx);
 
     if (this->unk256 & 2) {
-        POLY_OPA_DISP = Gfx_SetFog(POLY_OPA_DISP, 0, 0, 0, 0, this->unk29A, 0x3E8);
+        POLY_OPA_DISP = Gfx_SetFog(POLY_OPA_DISP, 0, 0, 0, 0, this->unk29A, 1000);
     }
 
     gfxP = POLY_OPA_DISP;
-    
+
     gSPSegment(gfxP, 0x08, Lib_SegmentedToVirtual(D_80BB062C[this->unk24C]));
     gSPSegment(gfxP + 1, 0x09, Lib_SegmentedToVirtual(&D_06007650));
-    
+
     POLY_OPA_DISP = gfxP + 2;
-    
+
     func_80BB01B0(this, play);
     SkelAnime_DrawFlexOpa(play, this->skelAnime.skeleton, this->skelAnime.jointTable, this->skelAnime.dListCount,
                           func_80BB0128, func_80BB0170, &this->actor);
