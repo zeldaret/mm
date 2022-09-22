ad7d598c87fd1aa1fd1775c0059aa78ffe1eeb36
sonicdcer <alejandro.asenjo88@gmail.com>
2022-09-21 23:15:20 -0300

    Magic_Reset

--- src/overlays/actors/ovl_Oceff_Wipe5/z_oceff_wipe5.c
+++ src/overlays/actors/ovl_Oceff_Wipe5/z_oceff_wipe5.c
@@ -40,7 +40,7 @@ void OceffWipe5_Init(Actor* thisx, PlayState* play) {
 void OceffWipe5_Destroy(Actor* thisx, PlayState* play) {
     OceffWipe5* this = THIS;
 
-    func_80115D5C(&play->state);
+    Magic_Reset(play);
     play->msgCtx.unk120B0 = 0;
 }
 
