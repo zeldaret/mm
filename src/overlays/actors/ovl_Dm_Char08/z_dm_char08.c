bff934cef5fc33ad72dbb11d7cbde414f243f525
Derek Hensley <hensley.derek58@gmail.com>
2022-09-28 20:47:50 -0700

    Update src/overlays/actors/ovl_Dm_Char08/z_dm_char08.c
    Co-authored-by: Anghelo Carvajal <angheloalf95@gmail.com>

--- src/overlays/actors/ovl_Dm_Char08/z_dm_char08.c
+++ src/overlays/actors/ovl_Dm_Char08/z_dm_char08.c
@@ -835,7 +835,7 @@ void func_80AB032C(DmChar08* this, PlayState* play) {
                         this->eyeMode = TURTLE_EYEMODE_BLINK_LEFT;
                         this->unk_208++;
                     }
-                    if ((Message_GetState(&play->msgCtx) == 6) && Message_ShouldAdvance(play)) {
+                    if ((Message_GetState(&play->msgCtx) == TEXT_STATE_DONE) && Message_ShouldAdvance(play)) {
                         this->unk_208 = 3;
                     }
                     break;
