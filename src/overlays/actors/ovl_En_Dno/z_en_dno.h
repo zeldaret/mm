15b9f83bd8980cbc3fc27373cba72826946395da
Tom Overton <tom-overton@users.noreply.github.com>
2022-09-30 13:17:34 -0700

    Document object_dnj (Deku Butler) (#1100)

--- src/overlays/actors/ovl_En_Dno/z_en_dno.h
+++ src/overlays/actors/ovl_En_Dno/z_en_dno.h
@@ -2,6 +2,7 @@
 #define Z_EN_DNO_H
 
 #include "global.h"
+#include "objects/object_dnj/object_dnj.h"
 
 struct EnDno;
 
@@ -21,10 +22,10 @@ typedef struct EnDno {
     /* 0x144 */ EnDnoActionFunc actionFunc;
     /* 0x148 */ SkelAnime skelAnime;
     /* 0x18C */ ColliderCylinder collider;
-    /* 0x1D8 */ Vec3s jointTable[28];
-    /* 0x280 */ Vec3s morphTable[28];
+    /* 0x1D8 */ Vec3s jointTable[DEKU_BUTLER_LIMB_MAX];
+    /* 0x280 */ Vec3s morphTable[DEKU_BUTLER_LIMB_MAX];
     /* 0x328 */ s32 unk_328;
-    /* 0x32C */ s32 unk_32C;
+    /* 0x32C */ s32 animIndex;
     /* 0x330 */ UNK_TYPE1 unk_330[0x4];
     /* 0x334 */ Vec3f unk_334;
     /* 0x340 */ ActorPathing actorPath;
