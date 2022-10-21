02a6464319954686991ca61b4910e3481592f9f1
Anghelo Carvajal <angheloalf95@gmail.com>
2022-10-20 21:40:52 -0300

    Update src/overlays/fbdemos/ovl_fbdemo_wipe5/z_fbdemo_wipe5.c
    Co-authored-by: Derek Hensley <hensley.derek58@gmail.com>

--- src/overlays/fbdemos/ovl_fbdemo_wipe5/z_fbdemo_wipe5.c
+++ src/overlays/fbdemos/ovl_fbdemo_wipe5/z_fbdemo_wipe5.c
@@ -82,7 +82,7 @@ void TransitionWipe5_Draw(void* thisx, Gfx** gfxP) {
     s32 height = gScreenHeight;
     s32 sp50 = D_801FBB90;
     TransitionWipe5* this = THIS;
-    s32 alpha = ((1.0f - this->unk_0C) * 255.0f);
+    s32 alpha = (1.0f - this->unk_0C) * 255.0f;
 
     gDPPipeSync(gfx++);
     gSPLoadUcodeL(gfx++, gspS2DEX2_fifo);
