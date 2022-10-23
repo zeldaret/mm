f26acc4884c6c4a2cc25f103e3eab02ba73cf4f5
engineer124 <engineer124engineer124@gmail.com>
2022-10-23 10:01:27 -0400

    fix bss

--- include/variables.h
+++ include/variables.h
@@ -2461,7 +2461,6 @@ extern TriNorm D_801EDBB0;
 // extern UNK_TYPE1 D_801EDBD8;
 // extern UNK_TYPE1 D_801EDBDC;
 // extern UNK_TYPE1 D_801EDBE0;
-extern s16 D_801EDBF0;
 // extern UNK_TYPE1 D_801EDBF4;
 // extern UNK_TYPE1 D_801EDBF8;
 // extern UNK_TYPE1 D_801EDC00;
