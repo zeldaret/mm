2da84adb4b9e111fa26e712649b0158cacd6ca74
petrie911 <petrie911@users.noreply.github.com>
2022-10-26 15:41:16 -0500

    whitespace

--- src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.h
+++ src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.h
@@ -7,16 +7,16 @@ typedef struct ObjBoyo {
     /* 0x000 */ Actor actor;
     /* 0x144 */ ColliderCylinder unk_144; 
     /* 0x190 */ AnimatedMaterial *unk_190;
-    /* 0x194 */ s16 unk_194;              
-    /* 0x196 */ s16 unk_196;              
-    /* 0x198 */ f32 unk_198;              
-    /* 0x19C */ f32 unk_19C;              
-    /* 0x1A0 */ f32 unk_1A0;              
-    /* 0x1A4 */ s16 unk_1A4;              
-    /* 0x1A6 */ s16 unk_1A6;              
-    /* 0x1A8 */ s16 unk_1A8;              
-    /* 0x1AA */ s16 unk_1AA;              
-    /* 0x1AC */ s16 unk_1AC;              
+    /* 0x194 */ s16 unk_194;
+    /* 0x196 */ s16 unk_196;
+    /* 0x198 */ f32 unk_198;
+    /* 0x19C */ f32 unk_19C;
+    /* 0x1A0 */ f32 unk_1A0;
+    /* 0x1A4 */ s16 unk_1A4;
+    /* 0x1A6 */ s16 unk_1A6;
+    /* 0x1A8 */ s16 unk_1A8;
+    /* 0x1AA */ s16 unk_1AA;
+    /* 0x1AC */ s16 unk_1AC;
 } ObjBoyo; // size = 0x1B0
 
 #endif // Z_OBJ_BOYO_H
