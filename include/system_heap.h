2cf3ad567dfb6772a379843f33b2481bdbf84ef4
Derek Hensley <hensley.derek58@gmail.com>
2023-02-17 19:59:50 -0800

    Add malloc and free to header

--- include/system_heap.h
+++ include/system_heap.h
@@ -4,6 +4,8 @@
 #include "libc/stddef.h"
 #include "PR/ultratypes.h"
 
+void* SystemHeap_Malloc(size_t size);
+void SystemHeap_Free(void* ptr);
 void SystemHeap_Init(void* start, size_t size);
 
 #endif
