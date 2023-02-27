79f930c78b82f6bd886e5b5501b66dfd24e5c50d
Angie <angheloalf95@gmail.com>
2023-02-27 12:13:05 -0300

    z64game_over.h

--- include/z64.h
+++ include/z64.h
@@ -35,6 +35,7 @@
 #include "z64cutscene.h"
 #include "z64dma.h"
 #include "z64effect.h"
+#include "z64game_over.h"
 #include "z64interface.h"
 #include "z64item.h"
 #include "z64light.h"
@@ -757,22 +758,6 @@ typedef struct {
     /* 0x0 */ s32 enabled;
     /* 0x4 */ s32 timer;
 } FrameAdvanceContext; // size = 0x8
-
-typedef enum {
-    /*  0 */ GAMEOVER_INACTIVE,
-    /*  1 */ GAMEOVER_DEATH_START,
-    /*  2 */ GAMEOVER_DEATH_WAIT_GROUND,    // wait for player to fall and hit the ground
-    /*  3 */ GAMEOVER_DEATH_FADE_OUT,       // wait before fading out
-    /* 20 */ GAMEOVER_REVIVE_START = 20,
-    /* 21 */ GAMEOVER_REVIVE_RUMBLE,
-    /* 22 */ GAMEOVER_REVIVE_WAIT_GROUND,   // wait for player to fall and hit the ground
-    /* 23 */ GAMEOVER_REVIVE_WAIT_FAIRY,    // wait for the fairy to rise all the way up out of player's body
-    /* 24 */ GAMEOVER_REVIVE_FADE_OUT       // fade out the game over lights as player is revived and gets back up
-} GameOverState;
-
-typedef struct {
-    /* 0x0 */ u16 state;
-} GameOverContext; // size = 0x2
 
 typedef struct PlayState {
     /* 0x00000 */ GameState state;
