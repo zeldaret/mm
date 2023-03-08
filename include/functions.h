184f5858ff6bf0c3c7bd399efe592e5af497e6d9
Derek Hensley <hensley.derek58@gmail.com>
2023-03-07 21:06:49 -0800

    Debug mode (#1185)
    * OK
    
    * Docs
    
    * PR
    
    * Format
    
    * Update include/z64debug_display.h
    
    Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>
    
    * Update include/z64debug_display.h
    
    Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: engineer124 <47598039+engineer124@users.noreply.github.com>

--- include/functions.h
+++ include/functions.h
@@ -1227,15 +1227,6 @@ s32 Actor_TrackPlayerSetFocusHeight(PlayState* play, Actor* actor, Vec3s* headRo
 s32 Actor_TrackPlayer(PlayState* play, Actor* actor, Vec3s* headRot, Vec3s* torsoRot, Vec3f focusPos);
 void SaveContext_Init(void);
 void GameInfo_Init(void);
-DebugDispObject* DebugDisplay_Init(void);
-void DebugDisplay_DrawObjects(PlayState* play);
-DebugDispObject* DebugDisplay_AddObject(f32 posX, f32 posY, f32 posZ, s16 rotX, s16 rotY, s16 rotZ, f32 scaleX, f32 scaleY, f32 scaleZ, u8 red, u8 green, u8 blue, u8 alpha, s16 type, GraphicsContext* gfxCtx);
-// void func_800E9C90(void);
-// void func_800E9CA0(s32 param_1, UNK_TYPE1 param_2, s8* param_3);
-// void func_800E9CFC(void);
-// void func_800E9DBC(void);
-// void func_800E9E94(void);
-void func_800E9F78(GraphicsContext* gfxCtx);
 
 void Cutscene_Init(PlayState* play, CutsceneContext* csCtx);
 void Cutscene_Start(PlayState* play, CutsceneContext* csCtx);
