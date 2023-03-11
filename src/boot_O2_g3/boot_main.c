27baccb5f6f429d8bc1997ed781c81a9cc8002b6
angie <angheloalf95@gmail.com>
2023-03-11 09:40:42 -0300

    format

--- src/boot_O2_g3/boot_main.c
+++ src/boot_O2_g3/boot_main.c
@@ -17,7 +17,6 @@ void bootproc(void) {
     osUnmapTLBAll();
     gCartHandle = osCartRomInit();
     StackCheck_Init(&sIdleStackInfo, sIdleStack, STACK_TOP(sIdleStack), 0, 0x100, "idle");
-    osCreateThread(&sIdleThread, Z_THREAD_ID_IDLE, Idle_ThreadEntry, NULL, STACK_TOP(sIdleStack),
-                   Z_PRIORITY_IDLE);
+    osCreateThread(&sIdleThread, Z_THREAD_ID_IDLE, Idle_ThreadEntry, NULL, STACK_TOP(sIdleStack), Z_PRIORITY_IDLE);
     osStartThread(&sIdleThread);
 }
