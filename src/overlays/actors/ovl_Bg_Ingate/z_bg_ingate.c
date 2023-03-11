d6fb1452b045d90a11764766b66f9e13c8a4b58e
Derek Hensley <hensley.derek58@gmail.com>
2023-03-11 09:34:05 -0800

    Uno mas

--- src/overlays/actors/ovl_Bg_Ingate/z_bg_ingate.c
+++ src/overlays/actors/ovl_Bg_Ingate/z_bg_ingate.c
@@ -190,7 +190,7 @@ void func_80953F9C(BgIngate* this, PlayState* play) {
 
         if (this->unk160 & 2) {
 
-            if (this->timePath->additionalPathIndex != 0xFF) {
+            if (this->timePath->additionalPathIndex != PATH_INDEX_END) {
                 func_80953E38(play);
                 func_800B7298(play, &this->dyna.actor, PLAYER_CSMODE_7);
                 this->dyna.actor.textId = 0x9E4;
