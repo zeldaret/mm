ead6e01b500daacfcb47876d44a90b24215eb56f
angie <angheloalf95@gmail.com>
2023-03-12 12:12:29 -0300

    format

--- src/code/z_effect_soft_sprite_dlftbls.c
+++ src/code/z_effect_soft_sprite_dlftbls.c
@@ -18,14 +18,18 @@
 #undef DEFINE_EFFECT_SS
 #undef DEFINE_EFFECT_SS_UNSET
 
-#define DEFINE_EFFECT_SS(name, _enumValue)                                                                \
-    {                                                                                          \
-        SEGMENT_ROM_START(ovl_##name), SEGMENT_ROM_END(ovl_##name), SEGMENT_START(ovl_##name), \
-            SEGMENT_END(ovl_##name), NULL, &name##_InitVars, 1,                                \
+#define DEFINE_EFFECT_SS(name, _enumValue) \
+    {                                      \
+        SEGMENT_ROM_START(ovl_##name),     \
+        SEGMENT_ROM_END(ovl_##name),       \
+        SEGMENT_START(ovl_##name),         \
+        SEGMENT_END(ovl_##name),           \
+        NULL,                              \
+        &name##_InitVars,                  \
+        1,                                 \
     },
 
-#define DEFINE_EFFECT_SS_UNSET(_enumValue) \
-    { 0 },
+#define DEFINE_EFFECT_SS_UNSET(_enumValue) { 0 },
 
 EffectSsOverlay gParticleOverlayTable[] = {
 #include "tables/effect_ss_table.h"
