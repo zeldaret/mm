2cb01b5b6a69635f7c8f554cf97442268aff28be
engineer124 <47598039+engineer124@users.noreply.github.com>
2023-03-23 09:36:09 +1100

    z_room (1 non-matching) (#1143)
    * begin room docs and decomp
    
    * match Room_DrawImageSingle
    
    * match Room_DrawImageMulti
    
    * Room_AllocateAndLoad non-matching
    
    * big improvements on Room_DrawCullable
    
    * match Room_AllocateAndLoad
    
    * improvement?
    
    * cleanup
    
    * comments
    
    * cleanup
    
    * small pr suggestions
    
    * PR Suggestions
    
    * cleanup sceneCmds
    
    * some PR
    
    * PR Suggestions
    
    * more PR Suggestions
    
    * col to collision

--- include/variables.h
+++ include/variables.h
@@ -592,8 +592,6 @@ extern Gfx gCullFrontDList[];
 extern Gfx sSetupDL[438];
 extern Gfx sFillSetupDL[12];
 extern Gfx gEmptyDL[1];
-extern Vec3f D_801C1D10;
-extern room_draw_func roomDrawFuncs[4];
 extern u32 gBitFlags[32];
 extern u16 gEquipMasks[];
 extern u16 gEquipNegMasks[];
