9e2b399dacd630c4ff0feb88a8fc6125c62f4554
angie <angheloalf95@gmail.com>
2023-03-23 00:08:02 -0300

    Revert "ovl_Item_B_Heart OK (#110)"
    This reverts commit f0cd2db05579c0237b2fa7836ac696de824844e1.

--- src/overlays/actors/ovl_Item_B_Heart/z_item_b_heart.h
+++ src/overlays/actors/ovl_Item_B_Heart/z_item_b_heart.h
@@ -7,9 +7,7 @@ struct ItemBHeart;
 
 typedef struct ItemBHeart {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ UNK_TYPE1 unk_144[0x20];
-    /* 0x164 */ f32 unk_164;
-    /* 0x168 */ f32 unk_168;
+    /* 0x144 */ char unk_144[0x28];
 } ItemBHeart; // size = 0x16C
 
 #endif // Z_ITEM_B_HEART_H
