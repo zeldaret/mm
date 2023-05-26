9fe3c2c1f63dea3076927fb44112b0e457612b53
angie <angheloalf95@gmail.com>
2023-05-26 10:17:27 -0400

    format

--- include/gfx.h
+++ include/gfx.h
@@ -6,21 +6,20 @@
 #include "thga.h"
 #include "alignment.h"
 
-
 struct GameState;
 
 typedef struct {
     /* 0x000 */ Gfx taskStart[9];
-    /* 0x048 */ Gfx clearZBuffer[8];       // original name: clear_zb_dl
-    /* 0x088 */ Gfx clearFrameBuffer[5];   // original name: clear_fb_dl
-    /* 0x0B0 */ Gfx setupBuffers[6];       // setup framebuffer and zbuffer
-    /* 0x0E0 */ Gfx unk_E0[12];            // unused
+    /* 0x048 */ Gfx clearZBuffer[8];     // original name: clear_zb_dl
+    /* 0x088 */ Gfx clearFrameBuffer[5]; // original name: clear_fb_dl
+    /* 0x0B0 */ Gfx setupBuffers[6];     // setup framebuffer and zbuffer
+    /* 0x0E0 */ Gfx unk_E0[12];          // unused
     /* 0x140 */ Gfx syncSegments[17];
     /* 0x1C8 */ Gfx setScissor[2];
-    /* 0x1D8 */ Gfx unk_1D8[25];           // unused
+    /* 0x1D8 */ Gfx unk_1D8[25]; // unused
     /* 0x2A0 */ Gfx disps[5];
-    /* 0x2C8 */ Gfx clearFillRect[3];      // fillrect for clearing buffers
-    /* 0x2E0 */ Gfx fillRect[3];           // fillrect for general purpose
+    /* 0x2C8 */ Gfx clearFillRect[3]; // fillrect for clearing buffers
+    /* 0x2E0 */ Gfx fillRect[3];      // fillrect for general purpose
     /* 0x2F8 */ Gfx debugDisp[1];
 } GfxMasterList; // size = 0x300
 
@@ -39,43 +38,42 @@ typedef struct GfxPool {
 } GfxPool; // size = 0x20310
 
 typedef struct GraphicsContext {
-    /* 0x000 */ Gfx*        polyOpaBuffer;  // Pointer to "Zelda 0"
-    /* 0x004 */ Gfx*        polyXluBuffer;  // Pointer to "Zelda 1"
-    /* 0x008 */ char        unk_8[0x8];     // Unused, could this be pointers to "Zelda 2" / "Zelda 3"
-    /* 0x010 */ Gfx*        overlayBuffer;  // Pointer to "Zelda 4"
-    /* 0x014 */ u32         unk_14;
-    /* 0x018 */ char        unk_18[0x20];
-    /* 0x038 */ OSMesg      msgBuff[8];
+    /* 0x000 */ Gfx* polyOpaBuffer; // Pointer to "Zelda 0"
+    /* 0x004 */ Gfx* polyXluBuffer; // Pointer to "Zelda 1"
+    /* 0x008 */ char unk_8[0x8];    // Unused, could this be pointers to "Zelda 2" / "Zelda 3"
+    /* 0x010 */ Gfx* overlayBuffer; // Pointer to "Zelda 4"
+    /* 0x014 */ u32 unk_14;
+    /* 0x018 */ char unk_18[0x20];
+    /* 0x038 */ OSMesg msgBuff[8];
     /* 0x058 */ OSMesgQueue* schedMsgQ;
     /* 0x05C */ OSMesgQueue queue;
-    /* 0x074 */ char        unk_74[0x04];
-    /* 0x078 */ OSScTask    task;
-    /* 0x0E0 */ char        unk_E0[0xD0];
-    /* 0x1B0 */ Gfx*        workBuffer;
+    /* 0x074 */ char unk_74[0x04];
+    /* 0x078 */ OSScTask task;
+    /* 0x0E0 */ char unk_E0[0xD0];
+    /* 0x1B0 */ Gfx* workBuffer;
     /* 0x1B4 */ TwoHeadGfxArena work;
-    /* 0x1C4 */ Gfx*        debugBuffer;
+    /* 0x1C4 */ Gfx* debugBuffer;
     /* 0x1C8 */ TwoHeadGfxArena debug;
     /* 0x1D8 */ UNK_TYPE1 unk_1D8[0xAC];
-    /* 0x284 */ OSViMode*   viMode;
-    /* 0x288 */ void*       zbuffer;
-    /* 0x28C */ char        unk_28C[0x1C];
-    /* 0x2A8 */ TwoHeadGfxArena overlay;    // "Zelda 4"
-    /* 0x2B8 */ TwoHeadGfxArena polyOpa;    // "Zelda 0"
-    /* 0x2C8 */ TwoHeadGfxArena polyXlu;    // "Zelda 1"
-    /* 0x2D8 */ u32         gfxPoolIdx;
-    /* 0x2DC */ u16*        curFrameBuffer;
-    /* 0x2E0 */ char        unk_2E0[0x4];
-    /* 0x2E4 */ u32         viConfigFeatures;
-    /* 0x2E8 */ char        unk_2E8[0x2];
-    /* 0x2EA */ u8          updateViMode;
-    /* 0x2EB */ u8          framebufferIndex;
-    /* 0x2EC */ void        (*callback)(struct GraphicsContext*, u32);
-    /* 0x2F0 */ u32         callbackArg;
-    /* 0x2F4 */ f32         xScale;
-    /* 0x2F8 */ f32         yScale;
+    /* 0x284 */ OSViMode* viMode;
+    /* 0x288 */ void* zbuffer;
+    /* 0x28C */ char unk_28C[0x1C];
+    /* 0x2A8 */ TwoHeadGfxArena overlay; // "Zelda 4"
+    /* 0x2B8 */ TwoHeadGfxArena polyOpa; // "Zelda 0"
+    /* 0x2C8 */ TwoHeadGfxArena polyXlu; // "Zelda 1"
+    /* 0x2D8 */ u32 gfxPoolIdx;
+    /* 0x2DC */ u16* curFrameBuffer;
+    /* 0x2E0 */ char unk_2E0[0x4];
+    /* 0x2E4 */ u32 viConfigFeatures;
+    /* 0x2E8 */ char unk_2E8[0x2];
+    /* 0x2EA */ u8 updateViMode;
+    /* 0x2EB */ u8 framebufferIndex;
+    /* 0x2EC */ void (*callback)(struct GraphicsContext*, u32);
+    /* 0x2F0 */ u32 callbackArg;
+    /* 0x2F4 */ f32 xScale;
+    /* 0x2F8 */ f32 yScale;
     /* 0x2FC */ GfxMasterList* masterList;
 } GraphicsContext; // size = 0x300
-
 
 Gfx* Gfx_SetFog(Gfx* gfx, s32 r, s32 g, s32 b, s32 a, s32 n, s32 f);
 Gfx* Gfx_SetFogWithSync(Gfx* gfx, s32 r, s32 g, s32 b, s32 a, s32 n, s32 f);
@@ -158,10 +156,8 @@ void func_8012CF0C(GraphicsContext* gfxCtx, s32 clearFb, s32 clearZb, u8 r, u8 g
 void func_8012D374(GraphicsContext* gfxCtx, u8 r, u8 g, u8 b);
 void func_8012D40C(f32* param_1, f32* param_2, s16* param_3);
 
-
 extern Gfx sSetupDL[73 * 6];
 extern Gfx gEmptyDL[];
-
 
 #define WORK_DISP __gfxCtx->work.p
 #define POLY_OPA_DISP __gfxCtx->polyOpa.p
@@ -181,7 +177,6 @@ extern Gfx gEmptyDL[];
     }                       \
     (void)0
 
-
 #define GRAPH_ALLOC(gfxCtx, size) ((void*)((gfxCtx)->polyOpa.d = (Gfx*)((u8*)(gfxCtx)->polyOpa.d - ALIGN16(size))))
 
 // Custom gbi macro
@@ -198,7 +193,5 @@ extern Gfx gEmptyDL[];
                        ((height)-1) << G_TEXTURE_IMAGE_FRAC);                                                          \
     }                                                                                                                  \
     (void)0
-
-
 
 #endif
