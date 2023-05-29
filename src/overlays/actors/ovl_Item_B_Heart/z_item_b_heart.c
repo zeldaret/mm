566946de975b3aeba0301e5e90c701a604aec212
StickyThwomp <stickythwomp@protonmail.com>
2023-05-28 20:29:42 -0500

    Fixed function names after master merge.

--- src/overlays/actors/ovl_Item_B_Heart/z_item_b_heart.c
+++ src/overlays/actors/ovl_Item_B_Heart/z_item_b_heart.c
@@ -105,12 +105,12 @@ void ItemBHeart_Draw(Actor* thisx, PlayState* play) {
     }
 
     if (drawTranslucent || (this->actor.world.rot.y != 0)) {
-        func_8012C2DC(play->state.gfxCtx);
+        Gfx_SetupDL25_Xlu(play->state.gfxCtx);
         gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPDisplayList(POLY_XLU_DISP++, &gGiHeartBorderDL);
         gSPDisplayList(POLY_XLU_DISP++, &gGiHeartContainerDL);
     } else {
-        func_8012C28C(play->state.gfxCtx);
+        Gfx_SetupDL25_Opa(play->state.gfxCtx);
         gSPMatrix(POLY_OPA_DISP++, Matrix_NewMtx(play->state.gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
         gSPDisplayList(POLY_OPA_DISP++, &gGiHeartBorderDL);
         gSPDisplayList(POLY_OPA_DISP++, &gGiHeartContainerDL);
