dd897f4f2e825babe0f119043f3f6228224afb37
Angie <angheloalf95@gmail.com>
2023-06-24 11:49:19 -0400

    engo

--- src/overlays/actors/ovl_En_Go/z_en_go.c
+++ src/overlays/actors/ovl_En_Go/z_en_go.c
@@ -353,7 +353,7 @@ static AnimationInfoS sAnimationInfo[] = {
  * @param maxFrames Maximum number of frames the effect will last. Actual lifetime will be between 'maxFrames / 3' and
  * 'maxFrames'
  */
-EnGoEffect* EnGo_InitSteam(EnGoEffect effect[], Vec3f pos, Vec3f accel, Vec3f velocity, f32 scale, f32 deltaScale,
+EnGoEffect* EnGo_InitSteam(EnGoEffect effect[ENGO_OTHER_EFFECT_COUNT], Vec3f pos, Vec3f accel, Vec3f velocity, f32 scale, f32 deltaScale,
                            s32 maxFrames) {
     s32 i;
 
@@ -381,7 +381,7 @@ EnGoEffect* EnGo_InitSteam(EnGoEffect effect[], Vec3f pos, Vec3f accel, Vec3f ve
  *
  * @param effect The EnGoEffect table
  */
-void EnGo_DrawSteam(EnGoEffect effect[], PlayState* play2) {
+void EnGo_DrawSteam(EnGoEffect effect[ENGO_EFFECT_COUNT], PlayState* play2) {
     PlayState* play = play2;
     s32 i;
     s32 isMaterialSet = false;
@@ -435,7 +435,7 @@ void EnGo_DrawSteam(EnGoEffect effect[], PlayState* play2) {
  * @param maxFrames Maximum number of frames the effect will last. Actual lifetime will be 1/3 * maxFrames -> maxFrames
  * @param parentEffectType Type of the parent effect, determines which of the possible dust effects correspond
  */
-void EnGo_InitDust(EnGoEffect effect[], Vec3f pos, Vec3f accel, Vec3f vel, f32 scale, f32 deltaScale, s32 maxFrames,
+void EnGo_InitDust(EnGoEffect effect[ENGO_OTHER_EFFECT_COUNT], Vec3f pos, Vec3f accel, Vec3f vel, f32 scale, f32 deltaScale, s32 maxFrames,
                    EnGoEffectType parentEffectType) {
     s32 i;
 
@@ -462,7 +462,7 @@ void EnGo_InitDust(EnGoEffect effect[], Vec3f pos, Vec3f accel, Vec3f vel, f32 s
  *
  * @param effect The EnGoEffect table
  */
-void EnGo_DrawDust(EnGoEffect effect[], PlayState* play2) {
+void EnGo_DrawDust(EnGoEffect effect[ENGO_EFFECT_COUNT], PlayState* play2) {
     static TexturePtr sDustTextures[] = {
         gEffDust8Tex, gEffDust7Tex, gEffDust6Tex, gEffDust5Tex, gEffDust4Tex, gEffDust3Tex, gEffDust2Tex, gEffDust1Tex,
     };
@@ -644,7 +644,7 @@ void EnGo_UpdateSnow(EnGoEffect* effect, f32 dustConversionHeight) {
  * @param model Snow Model
  * @param type Snow Effect type to draw
  */
-void EnGo_DrawSnow(EnGoEffect effect[], PlayState* play, Gfx* material, Gfx* model, u8 effectType) {
+void EnGo_DrawSnow(EnGoEffect effect[ENGO_SNOW_EFFECT_COUNT], PlayState* play, Gfx* material, Gfx* model, u8 effectType) {
     s32 i;
     u8 isMaterialSet = false;
 
