40d1640078d307237015b39935fe5e5c1b797b52
louist103 <louist103@gmail.com>
2023-06-25 19:28:33 -0400

    PR Fixes

--- src/overlays/actors/ovl_Obj_Wind/z_obj_wind.c
+++ src/overlays/actors/ovl_Obj_Wind/z_obj_wind.c
@@ -12,6 +12,9 @@
 
 #define THIS ((ObjWind*)thisx)
 
+#define OBJ_WIND_GET_TYPE(thisx) (((thisx)->params >> 7) & 0x1F)
+#define OBJ_WIND_GET_SWITCH_FLAG(thisx) ((thisx)->params & 0x7F)
+
 void ObjWind_Init(Actor* thisx, PlayState* play);
 void ObjWind_Destroy(Actor* thisx, PlayState* play);
 void ObjWind_Update(Actor* thisx, PlayState* play);
@@ -45,57 +48,52 @@ void ObjWind_Init(Actor* thisx, PlayState* play) {
     if (WaterBox_GetSurface1(play, &play->colCtx, this->actor.world.pos.x, this->actor.world.pos.z, &ySurface,
                              &waterbox) &&
         (this->actor.world.pos.y < ySurface)) {
-        this->underWater = 1;
+        this->isUnderWater = 1;
     }
 }
 
 void ObjWind_Destroy(Actor* thisx, PlayState* play) {
 }
 
-typedef struct sD_80B2448C {
-    s16 unk_0;
-    s16 unk_2;
-    s16 unk_4;
-    s16 unk_6;
-    s16 unk_8;
-} sD_80B2448C;
+typedef struct ObjWindStruct {
+    /* 0x0 */ s16 unk_0;
+    /* 0x2 */ s16 unk_2;
+    /* 0x4 */ s16 unk_4;
+    /* 0x6 */ s16 unk_6;
+    /* 0x8 */ s16 unk_8;
+} ObjWindStruct;
 
-static sD_80B2448C D_80B2448C[] = {
-    { 0x0212, 0x005A, 0x0BB8, 0x07D0, 0x0000 }, { 0x0460, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x026C, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0654, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 }, { 0x0190, 0x0064, 0x07D0, 0x03E8, 0x0000 },
-    { 0x0258, 0x0078, 0x0BB8, 0x07D0, 0x0000 }, { 0x01F4, 0x0050, 0x0BB8, 0x07D0, 0x0000 },
-    { 0x0190, 0x003C, 0x07D0, 0x07D0, 0x0000 }, { 0x01D6, 0x0046, 0x07D0, 0x03E8, 0x0000 },
+static ObjWindStruct D_80B2448C[] = {
+    { 530, 90, 3000, 2000, 0 },   { 1120, 100, 2000, 1000, 0 }, { 620, 100, 2000, 1000, 0 },
+    { 1620, 100, 2000, 1000, 0 }, { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },  { 400, 100, 2000, 1000, 0 },
+    { 400, 100, 2000, 1000, 0 },  { 600, 120, 3000, 2000, 0 },  { 500, 80, 3000, 2000, 0 },
+    { 400, 60, 2000, 2000, 0 },   { 470, 70, 2000, 1000, 0 },
 };
 
-static s32 D_80B245CC = 0xFFFFFFFF;
+static s32 D_80B245CC = -1;
 
 void ObjWind_Update(Actor* thisx, PlayState* play) {
     ObjWind* this = (ObjWind*)thisx;
-    sD_80B2448C* entry = &D_80B2448C[(this->actor.params >> 7) & 0x1F];
+    ObjWindStruct* entry = &D_80B2448C[OBJ_WIND_GET_TYPE(thisx)];
     Player* player;
-    f32 cosYTimesX; // sp80
-    f32 cosX;       // sp7C
-    f32 sinYTimesX; // sp78
-    Vec3f posCopy;  // sp6C;
-    Vec3f sp60;
+    f32 cosYTimesX;
+    f32 cosX;
+    f32 sinYTimesX;
+    Vec3f posCopy;
+    Vec3f nearestPoint;
     Vec3f sp54;
-    f32 dist;     // sp50
-    f32 temp_fv0; // sp4C
+    f32 dist;
+    f32 distToNearestPoint;
 
     if (D_80B245CC != DREG(85)) {
-        if ((DREG(85) >= 0) && (DREG(85) < 0x20)) {
+        if ((DREG(85) >= 0) && (DREG(85) < ARRAY_COUNT(D_80B2448C))) {
             DREG(80) = D_80B2448C[DREG(85)].unk_0;
             DREG(81) = D_80B2448C[DREG(85)].unk_2;
             DREG(82) = D_80B2448C[DREG(85)].unk_4;
@@ -110,30 +108,30 @@ void ObjWind_Update(Actor* thisx, PlayState* play) {
         D_80B2448C[DREG(85)].unk_6 = DREG(83);
         D_80B2448C[DREG(85)].unk_8 = DREG(84);
     }
-    if (((this->actor.params & 0x7F) == 0x7F) || !Flags_GetSwitch(play, this->actor.params & 0x7F)) {
+    if (OBJ_WIND_GET_SWITCH_FLAG(thisx) == 0x7F || !Flags_GetSwitch(play, OBJ_WIND_GET_SWITCH_FLAG(thisx))) {
         player = GET_PLAYER(play);
         Math_Vec3f_Copy(&posCopy, &this->actor.world.pos);
         cosX = Math_CosS(this->actor.shape.rot.x);
         dist = Math_SinS(this->actor.shape.rot.x);
         sinYTimesX = Math_SinS(this->actor.shape.rot.y) * dist;
         cosYTimesX = Math_CosS(this->actor.shape.rot.y) * dist;
-        temp_fv0 = func_80179A44(&posCopy, &player->actor.world, &sp60);
-        if ((temp_fv0 >= 0.0f) && (temp_fv0 < entry->unk_0)) {
-            dist = Math_Vec3f_DistXYZAndStoreDiff(&player->actor.world.pos, &sp60, &sp54);
+        distToNearestPoint = func_80179A44(&posCopy, &player->actor.world, &nearestPoint);
+        if ((distToNearestPoint >= 0.0f) && (distToNearestPoint < entry->unk_0)) {
+            dist = Math_Vec3f_DistXYZAndStoreDiff(&player->actor.world.pos, &nearestPoint, &sp54);
             if (dist < entry->unk_2) {
                 f32 var_fa0;
-                f32 temp_ft4 = 1.0f;
+                f32 windSpeedXZ = 1.0f;
                 f32 sp40;
-                f32 temp_ft0;   // sp3C
-                f32 temp_fa1_2; // sp38
-                f32 temp_ft4_2; // sp34
-                f32 temp_fa0;   // sp30
+                f32 temp_ft0;
+                f32 windSpeedX;
+                f32 windSpeedY;
+                f32 windSpeedZ;
 
-                var_fa0 = 1.0f - temp_fv0 / entry->unk_0;
+                var_fa0 = 1.0f - distToNearestPoint / entry->unk_0;
                 sp40 = ((f32)entry->unk_4 / 100.0f) *
                        ((var_fa0 * (1.0f - dist / entry->unk_2)) + ((f32)entry->unk_8 / 100.0f));
-                if (0.8f < temp_fv0 / entry->unk_0) {
-                    var_fa0 = temp_ft4 - 1.0f;
+                if (0.8f < distToNearestPoint / entry->unk_0) {
+                    var_fa0 = windSpeedXZ - 1.0f;
                 }
                 temp_ft0 = ((f32)entry->unk_6 / 100.0f) * (dist / entry->unk_2 * var_fa0);
                 if (dist != 0.0f) {
@@ -142,14 +140,14 @@ void ObjWind_Update(Actor* thisx, PlayState* play) {
                     dist = 1.0f / dist;
                 }
                 temp_ft0 *= dist;
-                temp_fa1_2 = (sinYTimesX * sp40) + (sp54.x * temp_ft0);
-                temp_ft4_2 = (cosX * sp40) + (sp54.y * temp_ft0);
-                temp_fa0 = (cosYTimesX * sp40) + (sp54.z * temp_ft0);
-                player->windSpeed = sqrtf(SQ(temp_fa1_2) + SQ(temp_ft4_2) + SQ(temp_fa0));
-                player->windAngleY = Math_Atan2S_XY(temp_fa0, temp_fa1_2);
+                windSpeedX = (sinYTimesX * sp40) + (sp54.x * temp_ft0);
+                windSpeedY = (cosX * sp40) + (sp54.y * temp_ft0);
+                windSpeedZ = (cosYTimesX * sp40) + (sp54.z * temp_ft0);
+                player->windSpeed = sqrtf(SQ(windSpeedX) + SQ(windSpeedY) + SQ(windSpeedZ));
+                player->windAngleY = Math_Atan2S_XY(windSpeedZ, windSpeedX);
 
-                temp_ft4 = sqrtf(SQ(temp_fa1_2) + SQ(temp_fa0));
-                player->windAngleX = Math_Atan2S_XY(temp_ft4, temp_ft4_2);
+                windSpeedXZ = sqrtf(SQ(windSpeedX) + SQ(windSpeedZ));
+                player->windAngleX = Math_Atan2S_XY(windSpeedXZ, windSpeedY);
             }
         }
     }
--- src/overlays/actors/ovl_Obj_Wind/z_obj_wind.h
+++ src/overlays/actors/ovl_Obj_Wind/z_obj_wind.h
@@ -7,7 +7,7 @@ struct ObjWind;
 
 typedef struct ObjWind {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char underWater;
+    /* 0x144 */ u8 isUnderWater;
 } ObjWind; // size = 0x148
 
 #endif // Z_OBJ_WIND_H
