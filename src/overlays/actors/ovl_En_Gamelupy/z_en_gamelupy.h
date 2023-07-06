89319f4b1aa75aa1e28701e1dd28e9f64003aeb0
Derek Hensley <hensley.derek58@gmail.com>
2023-07-05 20:16:26 -0700

    EnGameLupy

--- src/overlays/actors/ovl_En_Gamelupy/z_en_gamelupy.h
+++ src/overlays/actors/ovl_En_Gamelupy/z_en_gamelupy.h
@@ -5,17 +5,25 @@
 
 struct EnGamelupy;
 
+#define ENGAMELUPY_COLLECTED 50
+
 typedef void (*EnGamelupyActionFunc)(struct EnGamelupy*, PlayState*);
+
+typedef enum {
+    /* 0 */ ENGAMELUPY_TYPE_GREEN,
+    /* 1 */ ENGAMELUPY_TYPE_BLUE,
+    /* 2 */ ENGAMELUPY_TYPE_MAX
+} EnGamelupyType;
 
 typedef struct EnGamelupy {
     /* 0x000 */ Actor actor;
     /* 0x144 */ ColliderCylinder collider;
     /* 0x190 */ EnGamelupyActionFunc actionFunc;
-    /* 0x194 */ s32 rupeeIndex;
-    /* 0x198 */ s16* unk_198;
-    /* 0x19C */ s16 unk_19C;
-    /* 0x19E */ s16 unk_19E;
-    /* 0x1A0 */ s16 unk_1A0;
+    /* 0x194 */ s32 type;
+    /* 0x198 */ s16* sharedMem; // Shared memory location with ACTOR_EN_LIFT_NUTS
+    /* 0x19C */ s16 collectedFrames;
+    /* 0x19E */ s16 sparklesAngle;
+    /* 0x1A0 */ s16 sparklesAngleStep;
 } EnGamelupy; // size = 0x1A4
 
 #endif // Z_EN_GAMELUPY_H
