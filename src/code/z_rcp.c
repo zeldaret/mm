312fcafe2db9379a01c1b7f1f3ea4cd783ca48fe
Derek Hensley <hensley.derek58@gmail.com>
2023-08-20 01:50:55 -0700

    Small General cleanup (#1360)
    * Draw things
    
    * EffectSsFhgFlash_SpawnShock enum
    
    * gohtIndex
    
    * colliders
    
    * ObjGhaka_Init
    
    * func_80B3C39C
    
    * undef syms

--- src/code/z_rcp.c
+++ src/code/z_rcp.c
@@ -1539,10 +1539,10 @@ void func_8012CF0C(GraphicsContext* gfxCtx, s32 clearFb, s32 clearZb, u8 r, u8 g
     gSPDisplayList(DEBUG_DISP++, gGfxMasterDL->setupBuffers);
 
     if (clearZb) {
-        gSPDisplayList(gfxCtx->polyOpa.p++, D_0E000000.clearZBuffer);
+        gSPDisplayList(POLY_OPA_DISP++, D_0E000000.clearZBuffer);
     }
     if (clearFb) {
-        gSPDisplayList(gfxCtx->polyOpa.p++, D_0E000000.clearFrameBuffer);
+        gSPDisplayList(POLY_OPA_DISP++, D_0E000000.clearFrameBuffer);
     }
 
     CLOSE_DISPS(gfxCtx);
