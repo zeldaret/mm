5b16f786ffbc9312732c72761b4276ff3b734342
CodeOverFlow <adrien.bodineau@gmail.com>
2023-09-05 19:38:02 +0200

    some cleanup

--- src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.c
+++ src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.c
@@ -16,76 +16,15 @@ void func_809A5DE0(Actor* actor, Actor* thisx);
 void func_809A5E14(Actor* arg0, Actor* this);
 Actor* ObjBoyo_FindCollidedActor(ObjBoyo* this, PlayState* play, u32* index);
 
-f32 D_809A61D0 = 0.03f;
-f32 D_809A61D4 = 0.01f;
-f32 D_809A61D8 = 0.1f;
-f32 D_809A61DC = 0.1f;
-f32 D_809A61E0 = 0.033333335f;
-f32 D_809A61E4 = 0.012f;
-f32 D_809A61E8[2] = { 0.006f, 0.0f };
-/*
- * From Z64Utils
- * Segment: 0xFF (0x08)
- * Type: Two Tex Scroll
- * Params Seg: 0x06000E80
- */
-extern AnimatedMaterial* D_06000E88;
-
-#if 0
-const ActorInit Obj_Boyo_InitVars = {
-    ACTOR_OBJ_BOYO,
-    ACTORCAT_PROP,
-    FLAGS,
-    OBJECT_BOYO,
-    sizeof(ObjBoyo),
-    (ActorFunc)ObjBoyo_Init,
-    (ActorFunc)ObjBoyo_Destroy,
-    (ActorFunc)ObjBoyo_Update,
-    (ActorFunc)ObjBoyo_Draw
-};
-
-#endif
-
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_Init.s")
-// D_809A6170
-ColliderCylinderInit gColliderCylinderInit = {
-    {
-        /* ColliderInit base */
-        0xA,  /* u8 coltype */
-        0,    /* u8 atFlags */
-        0xD,  /* u8 acFlags */
-        0x39, /* u8 ocFlags1 */
-        0x20, /* u8 ocFlags2 */
-        1     /* u8 shape */
-    },
-    {
-        /* ColliderInfoInit info */
-        0, /* u8 elemType */
-        {  /* ColliderTouchInit toucher */
-          0 /* u32 dmgFlags */, 0 /* u8 effect */, 0 /* u8 defense */ },
-        {
-            /* ColliderBumpInit  bumper */
-            0x01CBFFBE /* u32 dmgFlags */, 0 /* u8 effect */, 0 /* u8 defense */
-        },
-        0, /* u8 toucherFlags */
-        1, /* u8 bumperFlags */
-        1  /* u8 ocElemFlags */
-    },
-    { /* Cylinder16 dim */
-      0x3C /* s16 radius */,
-      0x8C /* s16 height */,
-      0 /* yShift */,
-      {
-          /* Vec3s pos */
-          0, /* s16 x */
-          0, /* s16 y */
-          0  /*s16 z */
-      } },
-};
-
-// D_809A619C
-InitChainEntry gInitChainEntry[5] = { 0xB0FC0FA0, 0xB100012C, 0xB104012C, 0x48580064,
-                                      0x00000000 }; /* unable to generate initializer */
+const ActorInit Obj_Boyo_InitVars = { ACTOR_OBJ_BOYO,
+                                      ACTORCAT_PROP,
+                                      FLAGS,
+                                      OBJECT_BOYO,
+                                      sizeof(ObjBoyo),
+                                      (ActorFunc)ObjBoyo_Init,
+                                      (ActorFunc)ObjBoyo_Destroy,
+                                      (ActorFunc)ObjBoyo_Update,
+                                      (ActorFunc)ObjBoyo_Draw };
 
 typedef void (*ObjBoyoCollisionHandler)(Actor* actor_a, Actor* actor_b);
 
@@ -94,19 +33,42 @@ typedef struct ObjBoyoUnkStruct {
     /* 0x4 */ s16 id;
 } ObjBoyoUnkStruct; // size = 0x8
 
-ObjBoyoUnkStruct gCollisionHandling[] = {
+static AnimatedMaterial* D_06000E88;
+
+static ColliderCylinderInit gColliderCylinderInit = {
+    {
+        /* ColliderInit base */
+        COLTYPE_NONE,                                       /* u8 coltype */
+        AT_NONE,                                            /* u8 atFlags */
+        AC_TYPE_PLAYER | AC_HARD | AC_ON,                   /* u8 acFlags */
+        OC1_TYPE_2 | OC1_TYPE_1 | OC1_TYPE_PLAYER | OC1_ON, /* u8 ocFlags1 */
+        OC2_TYPE_2,                                         /* u8 ocFlags2 */
+        COLSHAPE_CYLINDER                                   /* u8 shape */
+    },
+    {
+        /* ColliderInfoInit info */
+        ELEMTYPE_UNK0, /* u8 elemType */
+        { 0x00000000 /* u32 dmgFlags */, 0x00 /* u8 effect */, 0x00 /* u8 defense */ },
+        { 0x01CBFFBE /* u32 dmgFlags */, 0x00 /* u8 effect */, 0x00 /* u8 defense */ },
+        TOUCH_NONE, /* u8 toucherFlags */
+        BUMP_ON,    /* u8 bumperFlags */
+        OCELEM_ON   /* u8 ocElemFlags */
+    },
+    { 60 /* s16 radius */, 140 /* s16 height */, 0 /* yShift */, { 0 /* s16 x */, 0 /* s16 y */, 0 /*s16 z */ } },
+};
+
+static InitChainEntry gInitChainEntry[5] = { 0xB0FC0FA0, 0xB100012C, 0xB104012C, 0x48580064,
+                                             0x00000000 }; /* unable to generate initializer */
+
+static objBoyoUnkStruct gCollisionHandling[] = {
     { ObjBoyo_PushPlayer, 0x021D0000 }, { func_809A5DE0, 0x00090000 }, { func_809A5E14, 0x0 }, { NULL, 0x0 }
 };
 
-/********************** INIT **********************/
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_Init.s")
-// MATCHING
 void ObjBoyo_Init(ObjBoyo* this, PlayState* play) {
     ColliderCylinder* temp_a1;
 
     Actor_ProcessInitChain(&this->actor, gInitChainEntry);
     temp_a1 = &this->collider;
-    // sp20 = temp_a1;
     Collider_InitCylinder(play, temp_a1);
     Collider_SetCylinder(play, temp_a1, &this->actor, &gColliderCylinderInit);
     Collider_UpdateCylinder(&this->actor, temp_a1);
@@ -114,41 +76,28 @@ void ObjBoyo_Init(ObjBoyo* this, PlayState* play) {
     this->animatedMaterial = Lib_SegmentedToVirtual((void*)&D_06000E88);
 }
 
-/********************** DESTROY **********************/
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_Destroy.s")
-// MATCHING
 void ObjBoyo_Destroy(ObjBoyo* thisx, PlayState* play) {
     Collider_DestroyCylinder(play, &THIS->collider);
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_PushPlayer.s")
-// ObjBoyo_PushPlayer
 void ObjBoyo_PushPlayer(Actor* thisx, Actor* player) {
     ((Player*)player)->pushedSpeed = 30.0f;
     ((Player*)player)->pushedYaw = (s16)THIS->actor.yawTowardsPlayer;
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/func_809A5DE0.s")
-// MATCHING
 void func_809A5DE0(Actor* actor, Actor* thisx) {
     THIS->pushedSpeed = 30.0f;                                       // push speed
     THIS->yawTowardsActor = Actor_WorldYawTowardActor(actor, thisx); // push direction
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/func_809A5E14.s")
 void func_809A5E14(Actor* arg0, Actor* thisx) {
     // some kind of reset ?
     THIS->unk1F0 = 0;
 }
 
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/func_809A5E24.s")
 /*
- * Return first actor of type 2 (supposing actorList is deterministic)
- * or
- * look for the address A in D_809A61B4 matching the address in *this->unk150
- * arg2 is the index of the address into D_809A61B4.
+ * Find the Actor we collided with.
  */
-// MATCHING
 Actor* ObjBoyo_FindCollidedActor(ObjBoyo* this, PlayState* play, u32* index) {
     Actor* collidedActor;
     ObjBoyoUnkStruct* data;
@@ -174,8 +123,6 @@ Actor* ObjBoyo_FindCollidedActor(ObjBoyo* this, PlayState* play, u32* index) {
     return 0;
 }
 
-/********************** UPDATE **********************/
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_Update.s")
 void ObjBoyo_Update(Actor* thisx, PlayState* play2) {
     PlayState* play = play2;
     ObjBoyo* this = THIS;
@@ -207,8 +154,6 @@ void ObjBoyo_Update(Actor* thisx, PlayState* play2) {
         this->unk1AA += this->unk1AC;
         this->unk1A8 += this->unk1AA;
 
-        // this->actor.scale.z = (( this->unk19C * this->unk198 * (f32)this->unk194) * Math_CosS((s16)(this->unk1A8 +
-        // this->unk1A4))) + 0.1f;
         this->actor.scale.x = this->actor.scale.z =
             (Math_CosS((s16)(this->unk1A8 + this->unk1A4)) * (f32)this->unk194 * this->unk19C * this->unk198) + 0.1f;
 
@@ -239,10 +184,8 @@ void ObjBoyo_Update(Actor* thisx, PlayState* play2) {
         CollisionCheck_SetAC(play, &play->colChkCtx, &this->collider.base);
     }
 }
-// #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Boyo/ObjBoyo_Draw.s")
-// MATCHING
-extern Gfx D_06000300;
+static Gfx* D_06000300;
 void ObjBoyo_Draw(ObjBoyo* this, PlayState* play) {
     AnimatedMat_Draw(play, this->animatedMaterial);
-    Gfx_DrawDListOpa(play, &D_06000300);
+    Gfx_DrawDListOpa(play, D_06000300);
 }
