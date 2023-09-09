0222e7f761d751987a36978318af5dbf1eafb331
nuss <escabiaspablo21@gmail.com>
2023-09-09 20:06:10 +0200

    ovl_Bg_Lotus decompiled

--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
@@ -9,9 +9,11 @@ void BgLotus_Init(Actor* thisx, PlayState* play);
 void BgLotus_Destroy(Actor* thisx, PlayState* play);
 void BgLotus_Update(Actor* thisx, PlayState* play);
 void BgLotus_Draw(Actor* thisx, PlayState* play);
+void func_80AD68DC(BgLotus* thisx, PlayState* play);
+void func_80AD6A88(Actor* thisx, PlayState* play);
+void func_80AD6B68(Actor* thisx, PlayState* play);
 
-/*
-const ActorInit Bg_Lotus_InitVars = {
+ActorInit Bg_Lotus_InitVars = {
     ACTOR_BG_LOTUS,
     ACTORCAT_BG,
     FLAGS,
@@ -20,22 +22,154 @@ const ActorInit Bg_Lotus_InitVars = {
     (ActorFunc)BgLotus_Init,
     (ActorFunc)BgLotus_Destroy,
     (ActorFunc)BgLotus_Update,
-    (ActorFunc)BgLotus_Draw
+    (ActorFunc)BgLotus_Draw,
 };
-*/
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/BgLotus_Init.s")
+s32 D_80AD6D10[] = { 0x48580064 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/BgLotus_Destroy.s")
+void BgLotus_Init(Actor* thisx, PlayState* play) {
+    BgLotus* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/func_80AD6830.s")
+    s32 pad;
+    s32 sp2C;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/func_80AD68DC.s")
+    Actor_ProcessInitChain(&this->dyna.actor, (InitChainEntry*)D_80AD6D10);
+    DynaPolyActor_Init((DynaPolyActor*)this, 1);
+    DynaPolyActor_LoadMesh(play, (DynaPolyActor*)this, &gLilyPadCol);
+    this->dyna.actor.floorHeight = BgCheck_EntityRaycastFloor5(&play->colCtx, &this->dyna.actor.floorPoly, &sp2C,
+                                                               &this->dyna.actor, &this->dyna.actor.world.pos);
+    this->unk168 = 0x60;
+    this->dyna.actor.world.rot.y = ((s32)Rand_Next() >> 0x10);
+    this->unk15C = &func_80AD68DC;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/func_80AD6A88.s")
+void BgLotus_Destroy(Actor* thisx, PlayState* play) {
+    BgLotus* this = THIS;
+    DynaPoly_DeleteBgActor(play, &play->colCtx.dyna, this->dyna.bgId);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/func_80AD6B68.s")
+void func_80AD6830(Actor* thisx) {
+    BgLotus* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/BgLotus_Update.s")
+    f32 temp_fv1;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Bg_Lotus/BgLotus_Draw.s")
+    if (thisx->params == 0) {
+        temp_fv1 = Math_SinF(this->unk166 * (M_PI / 4)) * ((0.014f * (this->unk166 / (80.0f * 0.1f))) + 0.01f);
+        thisx->scale.x = ((1.0f + temp_fv1) * 0.1f);
+        thisx->scale.z = ((1.0f - temp_fv1) * 0.1f);
+    }
+}
+
+void func_80AD68DC(BgLotus* thisx, PlayState* play) {
+    f32 sp34;
+
+    thisx->unk168 = (thisx->unk168 - 1);
+    sp34 = Math_SinF(thisx->unk168 * 0.06544985f) * 6.0f;
+    if (thisx->dyna.actor.params == 0) {
+        thisx->dyna.actor.world.pos.x =
+            (Math_SinS(thisx->dyna.actor.world.rot.y) * sp34) + thisx->dyna.actor.home.pos.x;
+        thisx->dyna.actor.world.pos.z =
+            (Math_CosS(thisx->dyna.actor.world.rot.y) * sp34) + thisx->dyna.actor.home.pos.z;
+        if (thisx->unk168 == 0) {
+            thisx->unk168 = 0x60;
+            thisx->dyna.actor.world.rot.y += (s16)((s32)Rand_Next() >> 0x12);
+        }
+    }
+    if (thisx->unk160 < thisx->dyna.actor.floorHeight) {
+        thisx->dyna.actor.world.pos.y = thisx->dyna.actor.floorHeight;
+        goto block_12;
+    }
+    thisx->dyna.actor.world.pos.y = thisx->unk160;
+    if (DynaPolyActor_IsPlayerOnTop((DynaPolyActor*)thisx) != 0) {
+        if (thisx->unk164 == 0) {
+            EffectSsGRipple_Spawn(play, &thisx->dyna.actor.world.pos, 0x3E8, 0x578, 0);
+            EffectSsGRipple_Spawn(play, &thisx->dyna.actor.world.pos, 0x3E8, 0x578, 8);
+            thisx->unk166 = 0x28;
+        }
+        if (gSaveContext.save.playerForm != 3) {
+            thisx->unk166 = 0x28;
+            thisx->dyna.actor.flags |= 0x10;
+            thisx->unk15C = func_80AD6A88;
+            return;
+        }
+        thisx->unk164 = 1U;
+        goto block_12;
+    }
+    thisx->unk164 = 0U;
+block_12:
+    if (thisx->unk166 > 0) {
+        thisx->unk166 = (thisx->unk166 - 1);
+    }
+    func_80AD6830(&thisx->dyna.actor);
+}
+
+void func_80AD6A88(Actor* thisx, PlayState* play) {
+    BgLotus* this = THIS;
+
+    if (this->unk160 < thisx->world.pos.y) {
+        thisx->world.pos.y = this->unk160;
+    }
+    thisx->world.pos.y -= 1.0f;
+    if (thisx->world.pos.y <= thisx->floorHeight) {
+        thisx->world.pos.y = thisx->floorHeight;
+        this->unk166 = 0;
+    }
+    if (this->unk166 > 0) {
+        this->unk166 = (this->unk166 - 1);
+        func_80AD6830(thisx);
+        return;
+    }
+    if (Math_StepToF(&thisx->scale.x, 0.0f, 0.0050000004f) != 0) {
+        thisx->draw = (void*)0;
+        this->unk166 = 0x64;
+        DynaPoly_DisableCollision(play, &play->colCtx.dyna, this->dyna.bgId);
+        this->unk15C = func_80AD6B68;
+    }
+    thisx->scale.z = thisx->scale.x;
+}
+
+void func_80AD6B68(Actor* thisx, PlayState* play) {
+    BgLotus* this = THIS;
+
+    f32 temp_fv0;
+    f32 temp_fv1;
+
+    if (this->unk166 > 0) {
+        this->unk166--;
+        return;
+    }
+    if ((thisx->xzDistToPlayer > 100.0f) && (thisx->projectedPos.z < 0.0f)) {
+        thisx->draw = BgLotus_Draw;
+        DynaPoly_EnableCollision(play, &play->colCtx.dyna, this->dyna.bgId);
+        Actor_SetScale(thisx, 0.1f);
+        temp_fv0 = this->unk160;
+        temp_fv1 = thisx->floorHeight;
+        if (temp_fv0 < temp_fv1) {
+            thisx->world.pos.y = temp_fv1;
+        } else {
+            thisx->world.pos.y = temp_fv0;
+        }
+        thisx->flags &= ~ACTOR_FLAG_10;
+        this->unk168 = 0x60;
+        this->unk15C = &func_80AD68DC;
+        thisx->world.pos.x = thisx->home.pos.x;
+        thisx->world.pos.z = thisx->home.pos.z;
+    }
+}
+
+void BgLotus_Update(Actor* thisx, PlayState* play) {
+    BgLotus* this = THIS;
+
+    s32 pad;
+    WaterBox* sp2C;
+
+    WaterBox_GetSurface1_2(play, &play->colCtx, this->dyna.actor.world.pos.x, this->dyna.actor.world.pos.z,
+                           &this->unk160, &sp2C);
+    ((UNK_TYPE(*)(BgLotus*, PlayState*))(this->unk15C))(this, play);
+}
+
+void BgLotus_Draw(Actor* thisx, PlayState* play) {
+
+    BgLotus* this = THIS;
+    Gfx_DrawDListOpa(play, gLilyPadDL);
+}
--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
@@ -6,8 +6,13 @@
 struct BgLotus;
 
 typedef struct BgLotus {
-    /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x28];
-} BgLotus; // size = 0x16C
+    /* 0x000 */ DynaPolyActor dyna;
+    /* 0x15C */ s32 unk15C;
+    /* 0x160 */ f32 unk160;
+    /* 0x164 */ u8 unk164;
+    /* 0x166 */ s16 unk166;
+    /* 0x168 */ s16 unk168;
+    /* 0x16A */ char pad16A[2];
+} BgLotus; /* size = 0x16C */
 
 #endif // Z_BG_LOTUS_H
