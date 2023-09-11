9a9ba40ca1ec0f957d05beb2ae3b0d1e3b16752c
nuss <escabiaspablo21@gmail.com>
2023-09-11 10:42:47 +0200

    padding removed

--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.h
@@ -14,7 +14,6 @@ typedef struct BgLotus {
     /* 0x164 */ u8 unk164;
     /* 0x166 */ s16 unk166;
     /* 0x168 */ s16 unk168;
-    /* 0x16A */ char pad16A[2];
 } BgLotus; /* size = 0x16C */
 
 #endif // Z_BG_LOTUS_H
