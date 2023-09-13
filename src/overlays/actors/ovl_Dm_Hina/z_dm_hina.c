8f11a60b614f205841d70e6233eec0583ca25d05
nuss <escabiaspablo21@gmail.com>
2023-09-13 20:19:01 +0200

    all functions without func_80A1F75C

--- src/overlays/actors/ovl_Dm_Hina/z_dm_hina.c
+++ src/overlays/actors/ovl_Dm_Hina/z_dm_hina.c
@@ -32,22 +32,146 @@ ActorInit Dm_Hina_InitVars = {
     (ActorFunc)DmHina_Draw,
 };
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Init.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Init.s")
+void DmHina_Init(Actor* thisx, PlayState* play) {
+    DmHina* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Destroy.s")
+    this->unk180 = 1;
+    this->actionFunc = func_80A1F470;
+    this->unk_158 = this->actor.world.pos.y;
+    this->actor.focus.pos.y = this->actor.world.pos.y;
+    this->unk148 = 0.0f;
+    this->unk_15C = 1.0f;
+    this->actor.focus.pos.x = this->actor.world.pos.x;
+    this->actor.focus.pos.z = this->actor.world.pos.z;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F470.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Destroy.s")
+void DmHina_Destroy(Actor* thisx, PlayState* play) {
+    DmHina* this = THIS;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F56C.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F470.s")
+void func_80A1F470(DmHina* this, PlayState* play) {
+    Player* player = GET_PLAYER(play);
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F5AC.s")
+    Math_SmoothStepToF(&this->unk148, 0.6f, 0.5f, 0.05f, 0.001f);
+    this->unk154 = Math_SinS((s16)(play->gameplayFrames * 0x708)) * 8.0f;
+    if ((player->stateFlags1 & 0x400) && (this->actor.xzDistToPlayer < 80.0f)) {
+        this->unk180 = 0;
+        this->unk154 = 0.0f;
+        this->actor.world.pos.y += 40.0f;
+        this->actionFunc = func_80A1F56C;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F63C.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F56C.s")
+void func_80A1F56C(DmHina* this, PlayState* play) {
+    if (Message_GetState(&play->msgCtx) == (u8)2) {
+        this->unk17C = 2;
+        this->actionFunc = func_80A1F5AC;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F5AC.s")
+void func_80A1F5AC(DmHina* this, PlayState* play) {
+    this->unk17C--;
+    if (this->unk17C == 0) {
+        this->unk180 = 1;
+        Cutscene_StartManual(play, &play->csCtx);
+        this->unk17A = Play_CreateSubCamera(play);
+        Play_ChangeCameraStatus(play, 0, 1);
+        Play_ChangeCameraStatus(play, this->unk17A, 7);
+        this->actionFunc = func_80A1F63C;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F63C.s")
+void func_80A1F63C(DmHina* this, PlayState* play) {
+    this->unk160 = this->actor.world.pos.x + 100.0f;
+    this->unk164 = this->unk_158 + 260.0f;
+    this->unk168 = this->actor.world.pos.z + 100.0f;
+    this->unk16C = this->actor.world.pos.x;
+    this->unk174 = this->actor.world.pos.z;
+    this->unk170 = this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C);
+    Play_SetCameraAtEye(play, this->unk17A, (Vec3f*)&this->unk16C, (Vec3f*)&this->unk160);
+    Math_SmoothStepToF(&this->actor.world.pos.y, this->unk_158 + 300.0f, 0.5f, 2.0f, 0.1f);
+    if (((this->unk_158 + 240.0f) < this->actor.world.pos.y) && (this->unk17E != 1)) {
+        this->unk17E = 1;
+        Actor_PlaySfx(&this->actor, 0x580BU);
+    }
+    Actor_PlaySfx(&this->actor, 0x219BU);
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F75C.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Update.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Update.s")
+void func_80A1F75C(DmHina* this, PlayState* play); /* extern */
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F9AC.s")
+void DmHina_Update(Actor* thisx, PlayState* play) {
+    DmHina* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Draw.s")
+    this->actionFunc(this, play);
+    func_80A1F75C(this, play);
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/func_80A1F9AC.s")
+extern UNK_TYPE D_04023348;
+extern UNK_TYPE D_04023428;
+
+void func_80A1F9AC(DmHina* this, PlayState* play) {
+    GraphicsContext* gfxCtx;
+    gfxCtx = play->state.gfxCtx;
+
+    if (this->unk17E != 0) {
+        OPEN_DISPS(gfxCtx);
+
+        Gfx_SetupDL25_Xlu(play->state.gfxCtx);
+
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 255, 255, (u8)(this->unk14C * 100.0f) + 0x9B, this->unk17F);
+        gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, (u8)(this->unk150 * 100.0f) + 0x32, 0);
+        gSPDisplayList(POLY_XLU_DISP++, &D_04023348);
+
+        Matrix_Translate(this->actor.world.pos.x,
+                         this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C),
+                         this->actor.world.pos.z, MTXMODE_NEW);
+        Matrix_ReplaceRotation(&play->billboardMtxF);
+        Matrix_Scale(this->unk14C * 20.0f, this->unk14C * 20.0f, this->unk14C * 20.0f, MTXMODE_APPLY);
+        Matrix_RotateZF(Rand_ZeroFloat(6.2831855f), MTXMODE_APPLY);
+
+        gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPDisplayList(POLY_XLU_DISP++, &D_04023428);
+
+        CLOSE_DISPS(gfxCtx);
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Dm_Hina/DmHina_Draw.s")
+void DmHina_Draw(Actor* thisx, PlayState* play) {
+    DmHina* this = THIS;
+
+    if ((u8)this->unk180 != 0) {
+        Matrix_Translate(this->actor.world.pos.x,
+                         this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C),
+                         this->actor.world.pos.z, MTXMODE_NEW);
+        Matrix_RotateZYX(0, (s16)(play->gameplayFrames * 0x3E8), 0, MTXMODE_APPLY);
+        Matrix_Scale((this->unk148 * (1.0f - this->unk14C) * this->unk_15C),
+                     (this->unk148 * (1.0f - this->unk14C) * this->unk_15C),
+                     (this->unk148 * (1.0f - this->unk14C) * this->unk_15C), MTXMODE_APPLY);
+        switch (this->actor.params) {
+            case 0:
+                GetItem_Draw(play, 0x5C);
+                break;
+            case 1:
+                GetItem_Draw(play, 0x63);
+                break;
+            case 2:
+                GetItem_Draw(play, 0x64);
+                break;
+            case 3:
+                GetItem_Draw(play, 0x65);
+                break;
+        }
+        func_80A1F9AC(this, play);
+    }
+}
--- src/overlays/actors/ovl_Dm_Hina/z_dm_hina.h
+++ src/overlays/actors/ovl_Dm_Hina/z_dm_hina.h
@@ -10,10 +10,25 @@ typedef void (*DmHinaActionFunc)(struct DmHina*, PlayState*);
 typedef struct DmHina {
     /* 0x000 */ Actor actor;
     /* 0x144 */ DmHinaActionFunc actionFunc;
-    /* 0x148 */ char unk_148[0x10];
+    /* 0x148 */ f32 unk148;
+    /* 0x14C */ f32 unk14C;
+    /* 0x150 */ f32 unk150;
+    /* 0x154 */ f32 unk154;
     /* 0x158 */ f32 unk_158;
     /* 0x15C */ f32 unk_15C;
-    /* 0x160 */ char unk_160[0x24];
+    /* 0x160 */ f32 unk160;
+    /* 0x164 */ f32 unk164;
+    /* 0x168 */ f32 unk168;
+    /* 0x16C */ f32 unk16C;
+    /* 0x170 */ f32 unk170;
+    /* 0x174 */ f32 unk174;
+    /* 0x178 */ char pad178[2];
+    /* 0x17A */ s16 unk17A;
+    /* 0x17C */ s16 unk17C;
+    /* 0x17E */ u8 unk17E;
+    /* 0x17F */ u8 unk17F;
+    /* 0x180 */ s8 unk180;
+    /* 0x181 */ char pad181[3];
 } DmHina; // size = 0x184
 
 #endif // Z_DM_HINA_H
