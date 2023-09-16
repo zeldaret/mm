f3b2e11aa7cf0bd6d63e917061a487175d6d6000
nuss <escabiaspablo21@gmail.com>
2023-09-16 11:43:04 +0200

    improvements

--- src/overlays/actors/ovl_Dm_Hina/z_dm_hina.c
+++ src/overlays/actors/ovl_Dm_Hina/z_dm_hina.c
@@ -5,6 +5,7 @@
  */
 
 #include "z_dm_hina.h"
+#include "objects/gameplay_keep/gameplay_keep.h"
 
 #define FLAGS (ACTOR_FLAG_10 | ACTOR_FLAG_20)
 
@@ -37,23 +38,22 @@ void DmHina_Init(Actor* thisx, PlayState* play) {
 
     this->unk180 = 1;
     this->actionFunc = func_80A1F470;
-    this->unk_158 = this->actor.world.pos.y;
+    this->unk158 = this->actor.world.pos.y;
     this->actor.focus.pos.y = this->actor.world.pos.y;
     this->unk148 = 0.0f;
-    this->unk_15C = 1.0f;
+    this->unk15C = 1.0f;
     this->actor.focus.pos.x = this->actor.world.pos.x;
     this->actor.focus.pos.z = this->actor.world.pos.z;
 }
 
 void DmHina_Destroy(Actor* thisx, PlayState* play) {
-    DmHina* this = THIS;
 }
 
 void func_80A1F470(DmHina* this, PlayState* play) {
     Player* player = GET_PLAYER(play);
 
     Math_SmoothStepToF(&this->unk148, 0.6f, 0.5f, 0.05f, 0.001f);
-    this->unk154 = Math_SinS((s16)(play->gameplayFrames * 0x708)) * 8.0f;
+    this->unk154 = Math_SinS(play->gameplayFrames * 0x708) * 8.0f;
     if ((player->stateFlags1 & 0x400) && (this->actor.xzDistToPlayer < 80.0f)) {
         this->unk180 = 0;
         this->unk154 = 0.0f;
@@ -75,43 +75,44 @@ void func_80A1F5AC(DmHina* this, PlayState* play) {
         this->unk180 = 1;
         Cutscene_StartManual(play, &play->csCtx);
         this->unk17A = Play_CreateSubCamera(play);
-        Play_ChangeCameraStatus(play, 0, 1);
-        Play_ChangeCameraStatus(play, this->unk17A, 7);
+        Play_ChangeCameraStatus(play, CAM_ID_MAIN, CAM_STATUS_WAIT);
+        Play_ChangeCameraStatus(play, this->unk17A, CAM_STATUS_ACTIVE);
         this->actionFunc = func_80A1F63C;
     }
 }
 
 void func_80A1F63C(DmHina* this, PlayState* play) {
-    this->unk160 = this->actor.world.pos.x + 100.0f;
-    this->unk164 = this->unk_158 + 260.0f;
-    this->unk168 = this->actor.world.pos.z + 100.0f;
-    this->unk16C = this->actor.world.pos.x;
-    this->unk174 = this->actor.world.pos.z;
-    this->unk170 = this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C);
-    Play_SetCameraAtEye(play, this->unk17A, (Vec3f*)&this->unk16C, (Vec3f*)&this->unk160);
-    Math_SmoothStepToF(&this->actor.world.pos.y, this->unk_158 + 300.0f, 0.5f, 2.0f, 0.1f);
-    if (((this->unk_158 + 240.0f) < this->actor.world.pos.y) && (this->unk17E != 1)) {
+    this->unk160.x = this->actor.world.pos.x + 100.0f;
+    this->unk160.y = this->unk158 + 260.0f;
+    this->unk160.z = this->actor.world.pos.z + 100.0f;
+    this->unk16C.x = this->actor.world.pos.x;
+    this->unk16C.z = this->actor.world.pos.z;
+    this->unk16C.y = this->actor.world.pos.y + this->unk154 * this->unk15C + 40.0f * this->unk15C;
+    Play_SetCameraAtEye(play, this->unk17A, &this->unk16C, &this->unk160);
+    Math_SmoothStepToF(&this->actor.world.pos.y, this->unk158 + 300.0f, 0.5f, 2.0f, 0.1f);
+    if (((this->unk158 + 240.0f) < this->actor.world.pos.y) && (this->unk17E != 1)) {
         this->unk17E = 1;
-        Actor_PlaySfx(&this->actor, 0x580BU);
+        Actor_PlaySfx(&this->actor, NA_SE_OC_WHITE_OUT_INTO_KYOJIN);
     }
-    Actor_PlaySfx(&this->actor, 0x219BU);
+    Actor_PlaySfx(&this->actor, NA_SE_EV_MASK_RISING - SFX_FLAG);
 }
 
 void func_80A1F75C(DmHina* this, PlayState* play) {
-    s32 var_a0;
+    s32 i;
 
     switch (this->unk17E) {
         case 0:
             this->unk17F = 0;
             this->unk14C = 0.0f;
             break;
+
         case 1:
             Math_SmoothStepToF(&this->unk14C, 1.0f, 0.4f, 0.05f, 0.001f);
             this->unk17F = this->unk14C * 255.0f;
             this->unk150 = Math_SinS(play->state.frames * 0x1F40);
-            for (var_a0 = 0; var_a0 < 3; var_a0++) {
-                play->envCtx.lightSettings.ambientColor[var_a0] = play->envCtx.lightSettings.fogColor[var_a0] =
-                    play->envCtx.lightSettings.diffuseColor1[var_a0] = -255.0f * this->unk14C;
+            for (i = 0; i < 3; i++) {
+                play->envCtx.lightSettings.ambientColor[i] = play->envCtx.lightSettings.fogColor[i] =
+                    play->envCtx.lightSettings.diffuseColor1[i] = -255.0f * this->unk14C;
             }
             play->envCtx.lightSettings.fogNear = -500.0f * this->unk14C;
             if (play->envCtx.lightSettings.fogNear < -300) {
@@ -128,9 +129,6 @@ void DmHina_Update(Actor* thisx, PlayState* play) {
     func_80A1F75C(this, play);
 }
 
-extern UNK_TYPE D_04023348;
-extern UNK_TYPE D_04023428;
-
 void func_80A1F9AC(DmHina* this, PlayState* play) {
     GraphicsContext* gfxCtx = play->state.gfxCtx;
 
@@ -139,19 +137,19 @@ void func_80A1F9AC(DmHina* this, PlayState* play) {
 
         Gfx_SetupDL25_Xlu(play->state.gfxCtx);
 
-        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 255, 255, (u8)(this->unk14C * 100.0f) + 0x9B, this->unk17F);
-        gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, (u8)(this->unk150 * 100.0f) + 0x32, 0);
-        gSPDisplayList(POLY_XLU_DISP++, &D_04023348);
+        gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, 255, 255, (u8)(this->unk14C * 100.0f) + 155, this->unk17F);
+        gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, (u8)(this->unk150 * 100.0f) + 50, 0);
+        gSPDisplayList(POLY_XLU_DISP++, gLightOrbMaterial1DL);
 
         Matrix_Translate(this->actor.world.pos.x,
-                         this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C),
+                         this->actor.world.pos.y + (this->unk154 * this->unk15C) + (40.0f * this->unk15C),
                          this->actor.world.pos.z, MTXMODE_NEW);
         Matrix_ReplaceRotation(&play->billboardMtxF);
         Matrix_Scale(this->unk14C * 20.0f, this->unk14C * 20.0f, this->unk14C * 20.0f, MTXMODE_APPLY);
-        Matrix_RotateZF(Rand_ZeroFloat(6.2831855f), MTXMODE_APPLY);
+        Matrix_RotateZF(Rand_ZeroFloat(2 * M_PI), MTXMODE_APPLY);
 
         gSPMatrix(POLY_XLU_DISP++, Matrix_NewMtx(gfxCtx), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
-        gSPDisplayList(POLY_XLU_DISP++, &D_04023428);
+        gSPDisplayList(POLY_XLU_DISP++, gLightOrbModelDL);
 
         CLOSE_DISPS(gfxCtx);
     }
@@ -160,26 +158,29 @@ void func_80A1F9AC(DmHina* this, PlayState* play) {
 void DmHina_Draw(Actor* thisx, PlayState* play) {
     DmHina* this = THIS;
 
-    if ((u8)this->unk180 != 0) {
+    if (this->unk180 != 0) {
         Matrix_Translate(this->actor.world.pos.x,
-                         this->actor.world.pos.y + (this->unk154 * this->unk_15C) + (40.0f * this->unk_15C),
+                         this->actor.world.pos.y + (this->unk154 * this->unk15C) + (40.0f * this->unk15C),
                          this->actor.world.pos.z, MTXMODE_NEW);
-        Matrix_RotateZYX(0, (s16)(play->gameplayFrames * 0x3E8), 0, MTXMODE_APPLY);
-        Matrix_Scale((this->unk148 * (1.0f - this->unk14C) * this->unk_15C),
-                     (this->unk148 * (1.0f - this->unk14C) * this->unk_15C),
-                     (this->unk148 * (1.0f - this->unk14C) * this->unk_15C), MTXMODE_APPLY);
+        Matrix_RotateZYX(0, play->gameplayFrames * 0x3E8, 0, MTXMODE_APPLY);
+        Matrix_Scale((this->unk148 * (1.0f - this->unk14C) * this->unk15C),
+                     (this->unk148 * (1.0f - this->unk14C) * this->unk15C),
+                     (this->unk148 * (1.0f - this->unk14C) * this->unk15C), MTXMODE_APPLY);
         switch (this->actor.params) {
             case 0:
-                GetItem_Draw(play, 0x5C);
+                GetItem_Draw(play, GID_REMAINS_ODOLWA);
                 break;
+
             case 1:
-                GetItem_Draw(play, 0x63);
+                GetItem_Draw(play, GID_REMAINS_GOHT);
                 break;
+
             case 2:
-                GetItem_Draw(play, 0x64);
+                GetItem_Draw(play, GID_REMAINS_GYORG);
                 break;
+
             case 3:
-                GetItem_Draw(play, 0x65);
+                GetItem_Draw(play, GID_REMAINS_TWINMOLD);
                 break;
         }
         func_80A1F9AC(this, play);
--- src/overlays/actors/ovl_Dm_Hina/z_dm_hina.h
+++ src/overlays/actors/ovl_Dm_Hina/z_dm_hina.h
@@ -14,20 +14,16 @@ typedef struct DmHina {
     /* 0x14C */ f32 unk14C;
     /* 0x150 */ f32 unk150;
     /* 0x154 */ f32 unk154;
-    /* 0x158 */ f32 unk_158;
-    /* 0x15C */ f32 unk_15C;
-    /* 0x160 */ f32 unk160;
-    /* 0x164 */ f32 unk164;
-    /* 0x168 */ f32 unk168;
-    /* 0x16C */ f32 unk16C;
-    /* 0x170 */ f32 unk170;
-    /* 0x174 */ f32 unk174;
-    /* 0x178 */ char pad178[2];
+    /* 0x158 */ f32 unk158;
+    /* 0x15C */ f32 unk15C;
+    /* 0x160 */ Vec3f unk160;
+    /* 0x16C */ Vec3f unk16C;
+    /* 0x178 */ UNK_TYPE1 pad178[2];
     /* 0x17A */ s16 unk17A;
     /* 0x17C */ s16 unk17C;
     /* 0x17E */ u8 unk17E;
     /* 0x17F */ u8 unk17F;
-    /* 0x180 */ s8 unk180;
+    /* 0x180 */ u8 unk180;
 } DmHina; // size = 0x184
 
 #endif // Z_DM_HINA_H
