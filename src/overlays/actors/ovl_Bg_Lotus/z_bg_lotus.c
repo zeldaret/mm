2c070361a411673d810cff70dcc20153352e333c
Nuss <escabiaspablo21@gmail.com>
2023-09-23 07:33:19 +0200

    line

--- src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
+++ src/overlays/actors/ovl_Bg_Lotus/z_bg_lotus.c
@@ -29,6 +29,7 @@ ActorInit Bg_Lotus_InitVars = {
 static InitChainEntry sInitChain[] = {
     ICHAIN_VEC3F_DIV1000(scale, 100, ICHAIN_STOP),
 };
+
 void BgLotus_Init(Actor* thisx, PlayState* play) {
     BgLotus* this = THIS;
     s32 pad;
