1c5205eeac3df9ab0cb21cbc68faa51727c1b67e
CodeOverFlow <adrien.bodineau@gmail.com>
2023-09-23 17:23:47 +0200

    review: clean z_obj_boyo.h

--- src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.h
+++ src/overlays/actors/ovl_Obj_Boyo/z_obj_boyo.h
@@ -6,21 +6,20 @@
 
 typedef struct ObjBoyo {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ ColliderCylinder collider;          /* inferred */
-    /* 0x190 */ AnimatedMaterial* animatedMaterial; /* inferred */
+    /* 0x144 */ ColliderCylinder collider;
+    /* 0x190 */ AnimatedMaterial* animatedMaterial;
     /* 0x194 */ s16 unk194;
     /* 0x196 */ s16 unk196;
-    /* 0x198 */ f32 unk198;        /* inferred */
-    /* 0x19C */ f32 unk19C;        /* inferred */
-    /* 0x1A0 */ f32 unk1A0;        /* inferred */
-    /* 0x1A4 */ s16 unk1A4;        /* inferred */
-    /* 0x1A6 */ s16 unk1A6;        /* inferred */
-    /* 0x1A8 */ s16 unk1A8;        /* inferred */
-    /* 0x1AA */ s16 unk1AA;        /* inferred */
-    /* 0x1AC */ s16 unk1AC;        /* inferred */
-    /* 0x1AE */ char pad1AE[0x42]; /* maybe part of unk1AC[0x22]? */
-    /* 0x1F0 */ s16 unk1F0;
-    /* 0x1F4 */ s16 unk1F4;
+    /* 0x198 */ f32 unk198;
+    /* 0x19C */ f32 unk19C;
+    /* 0x1A0 */ f32 unk1A0;
+    /* 0x1A4 */ s16 unk1A4;
+    /* 0x1A6 */ s16 unk1A6;
+    /* 0x1A8 */ s16 unk1A8;
+    /* 0x1AA */ s16 unk1AA;
+    /* 0x1AC */ s16 unk1AC;
+    /* 0x1AE */ s16 unk1F0;
+    /* 0x1B0 */ s16 unk1F4;
     /* 0x1F6 */ s16 unk1F6;
     /* 0x1F8 */ f32 unk1F8;
     /* 0x1FC */ f32 unk1FC;
@@ -30,9 +29,8 @@ typedef struct ObjBoyo {
     /* 0x208 */ s16 unk208;
     /* 0x20A */ s16 unk20A;
     /* 0x20C */ s16 unk20C;
-    /* 0x20E */ char pad20E[225];
     /* 0x2F0 */ f32 pushedSpeed;
     /* 0x2F0 */ s16 yawTowardsActor;
-} ObjBoyo; /* size = 0x20F */
+} ObjBoyo; // size = 0x1B0
 
 #endif // Z_OBJ_BOYO_H
