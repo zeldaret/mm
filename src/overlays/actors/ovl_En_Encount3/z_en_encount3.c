cecafa0d9516b04336e5949c8fcafe7d02fb40b1
Nuss <escabiaspablo21@gmail.com>
2023-09-24 08:53:25 +0200

    some matches

--- src/overlays/actors/ovl_En_Encount3/z_en_encount3.c
+++ src/overlays/actors/ovl_En_Encount3/z_en_encount3.c
@@ -15,6 +15,8 @@ void EnEncount3_Destroy(Actor* thisx, PlayState* play);
 void EnEncount3_Update(Actor* thisx, PlayState* play);
 void EnEncount3_Draw(Actor* thisx, PlayState* play);
 
+void func_809AD058(EnEncount3* this);
+
 void func_809AD084(EnEncount3* this, PlayState* play);
 void func_809AD194(EnEncount3* this, PlayState* play);
 void func_809AD1EC(EnEncount3* this, PlayState* play);
@@ -31,21 +33,86 @@ ActorInit En_Encount3_InitVars = {
     (ActorFunc)EnEncount3_Draw,
 };
 
-UNK_TYPE D_809AD810 = { 0 };
+u32 D_809AD810 = false;
 
 extern UNK_TYPE D_060009A0;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/EnEncount3_Init.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/EnEncount3_Init.s")
+void EnEncount3_Init(Actor* thisx, PlayState* play) {
+    EnEncount3* this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/EnEncount3_Destroy.s")
+    this->unk14A = (this->actor.params >> 0xC) & 0xF;
+    this->unk158 = (this->actor.params >> 7) & 0x1F;
+    this->unk15C = this->actor.params & 0x7F;
+    this->unk15A = this->actor.csId;
+    this->unk16C = this->actor.world.rot.z * 10.0f;
+    if (this->unk16C < 10.0f) {
+        this->unk16C = 10.0f;
+    } else if (this->unk16C > 1000.0f) {
+        this->unk16C = 1000.0f;
+    }
+    if (this->unk15C == 0x7F) {
+        this->unk15C = -1;
+    }
+    if ((this->unk15C >= 0) && (Flags_GetSwitch(play, this->unk15C) != 0)) {
+        Actor_Kill(&this->actor);
+    }
+    this->actor.flags |= 0x08000000;
+    this->actor.flags &= ~ACTOR_FLAG_TARGETABLE;
+    func_809AD058(this);
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD058.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/EnEncount3_Destroy.s")
+void EnEncount3_Destroy(Actor* thisx, PlayState* play) {
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD084.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD058.s")
+void func_809AD058(EnEncount3* this) {
+    this->unk154 = 0x113;
+    this->unk150 = 1;
+    this->unk15E = 0x1E;
+    this->actionFunc = func_809AD084;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD194.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD084.s")
+void func_809AD084(EnEncount3* this, PlayState* play) {
+    if ((this->unk15C >= 0) && (Flags_GetSwitch(play, this->unk15C) != 0)) {
+        Actor_Kill(&this->actor);
+        return;
+    }
+    if (!(this->unk16C < this->actor.xzDistToPlayer) && (Player_GetMask(play) == 6) && (!D_809AD810)) {
+        if (this->unk15E > 0) {
+            this->unk15E--;
+            return;
+        }
+        this->unk17C = Actor_SpawnAsChild(&play->actorCtx, &this->actor, play, this->unk154, this->actor.world.pos.x,
+                                          this->actor.world.pos.y, this->actor.world.pos.z, 0, 0, 0, this->unk158);
+        if (this->unk17C != NULL) {
+            this->unk14E++;
+            D_809AD810 = true;
+            this->actionFunc = func_809AD194;
+        }
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD1EC.s")
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD194.s")
+void func_809AD194(EnEncount3* this, PlayState* play) {
+    if (this->unk14E == 0) {
+        this->unk178 = 0.0f;
+        if (this->unk15C >= 0) {
+            Flags_SetSwitch(play, this->unk15C);
+        }
+        this->actionFunc = func_809AD1EC;
+    }
+}
+
+//#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/func_809AD1EC.s")
+void func_809AD1EC(EnEncount3* this, PlayState* play) {
+    if (this->unk174 < 0.002f) {
+        D_809AD810 = false;
+        Actor_Kill(&this->actor);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Encount3/EnEncount3_Update.s")
 
--- src/overlays/actors/ovl_En_Encount3/z_en_encount3.h
+++ src/overlays/actors/ovl_En_Encount3/z_en_encount3.h
@@ -10,7 +10,27 @@ typedef void (*EnEncount3ActionFunc)(struct EnEncount3*, PlayState*);
 typedef struct EnEncount3 {
     /* 0x000 */ Actor actor;
     /* 0x144 */ EnEncount3ActionFunc actionFunc;
-    /* 0x148 */ char unk_148[0x84];
-} EnEncount3; // size = 0x1CC
+    /* 0x148 */ s16 unk148;
+    /* 0x14A */ s16 unk14A;                         /* inferred */
+    /* 0x14C */ char pad14C[2];
+    /* 0x14E */ s16 unk14E;
+    /* 0x150 */ s16 unk150;
+    /* 0x152 */ s16 unk152;
+    /* 0x154 */ s16 unk154;
+    /* 0x156 */ s16 unk156;
+    /* 0x158 */ s16 unk158;
+    /* 0x15A */ s16 unk15A;
+    /* 0x15C */ s16 unk15C;
+    /* 0x15E */ s16 unk15E;
+    /* 0x160 */ f32 unk160;
+    /* 0x164 */ f32 unk164;
+    /* 0x168 */ f32 unk168;
+    /* 0x16C */ f32 unk16C;
+    /* 0x170 */ f32 unk170;
+    /* 0x174 */ f32 unk174;
+    /* 0x178 */ f32 unk178;
+    /* 0x17C */ Actor *unk17C;
+    /* 0x180 */ char pad180[0x4C];                  /* maybe part of unk17C[0x14]? */
+} EnEncount3;                                       /* size = 0x1CC */
 
 #endif // Z_EN_ENCOUNT3_H
