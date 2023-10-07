c0f2bf878342034bbccf9c22d3e94b38abc201af
nuss <escabiaspablo21@gmail.com>
2023-10-07 13:12:44 +0200

    naming

--- src/overlays/actors/ovl_Bg_Icicle/z_bg_icicle.c
+++ src/overlays/actors/ovl_Bg_Icicle/z_bg_icicle.c
@@ -69,7 +69,7 @@ void BgIcicle_Init(Actor* thisx, PlayState* play) {
 
     Actor_ProcessInitChain(thisx, sInitChain);
     DynaPolyActor_Init(&this->dyna, 0);
-    DynaPolyActor_LoadMesh(play, &this->dyna, &gIciclesCol);
+    DynaPolyActor_LoadMesh(play, &this->dyna, &gIcicleCol);
 
     Collider_InitAndSetCylinder(play, &this->collider, thisx, &sCylinderInit);
     Collider_UpdateCylinder(thisx, &this->collider);
@@ -242,5 +242,5 @@ void BgIcicle_Update(Actor* thisx, PlayState* play) {
 }
 
 void BgIcicle_Draw(Actor* thisx, PlayState* play) {
-    Gfx_DrawDListOpa(play, gIciclesDL);
+    Gfx_DrawDListOpa(play, gIcicleDL);
 }
