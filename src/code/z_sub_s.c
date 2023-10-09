599cad03269275d51beb68d43d87f7e3d9e98890
engineer124 <engineer124engineer124@gmail.com>
2023-10-10 00:24:14 +1100

    simplify enum

--- src/code/z_sub_s.c
+++ src/code/z_sub_s.c
@@ -121,10 +121,10 @@ Gfx* SubS_DrawTransformFlex(PlayState* play, void** skeleton, Vec3s* jointTable,
     gSPSegment(gfx++, 0x0D, mtx);
     Matrix_Push();
     rootLimb = Lib_SegmentedToVirtual(skeleton[0]);
-    pos.x = jointTable[LIMB_INDEX_MODEL_POS].x;
-    pos.y = jointTable[LIMB_INDEX_MODEL_POS].y;
-    pos.z = jointTable[LIMB_INDEX_MODEL_POS].z;
-    rot = jointTable[LIMB_INDEX_MODEL_ROT];
+    pos.x = jointTable[LIMB_INDEX_POS].x;
+    pos.y = jointTable[LIMB_INDEX_POS].y;
+    pos.z = jointTable[LIMB_INDEX_POS].z;
+    rot = jointTable[LIMB_INDEX_ROT];
     newDlist = rootLimb->dList;
     limbDList = rootLimb->dList;
 
