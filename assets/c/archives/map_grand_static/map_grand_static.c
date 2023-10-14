ac31bcc7fc203899aeaf82bf14bb565c7a8436a9
angie <angheloalf95@gmail.com>
2023-10-13 23:10:49 -0300

    Rename symbols to follow style

--- assets/c/archives/map_grand_static/map_grand_static.c
+++ assets/c/archives/map_grand_static/map_grand_static.c
@@ -5,395 +5,395 @@
 #include "assets/misc/link_animetion/link_animetion.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 
-u64 map_grand_static_100[] = {
+u64 gMapGrandStatic100Tex[] = {
 #include "assets/archives/map_grand_static/map_100.i4.inc.c"
 };
 
-u64 map_grand_static_101[] = {
+u64 gMapGrandStatic101Tex[] = {
 #include "assets/archives/map_grand_static/map_101.i4.inc.c"
 };
 
-u64 map_grand_static_102[] = {
+u64 gMapGrandStatic102Tex[] = {
 #include "assets/archives/map_grand_static/map_102.i4.inc.c"
 };
 
-u64 map_grand_static_103[] = {
+u64 gMapGrandStatic103Tex[] = {
 #include "assets/archives/map_grand_static/map_103.i4.inc.c"
 };
 
-u64 map_grand_static_104[] = {
+u64 gMapGrandStatic104Tex[] = {
 #include "assets/archives/map_grand_static/map_104.i4.inc.c"
 };
 
-u64 map_grand_static_105[] = {
+u64 gMapGrandStatic105Tex[] = {
 #include "assets/archives/map_grand_static/map_105.i4.inc.c"
 };
 
-u64 map_grand_static_106[] = {
+u64 gMapGrandStatic106Tex[] = {
 #include "assets/archives/map_grand_static/map_106.i4.inc.c"
 };
 
-u64 map_grand_static_107[] = {
+u64 gMapGrandStatic107Tex[] = {
 #include "assets/archives/map_grand_static/map_107.i4.inc.c"
 };
 
-u64 map_grand_static_108[] = {
+u64 gMapGrandStatic108Tex[] = {
 #include "assets/archives/map_grand_static/map_108.i4.inc.c"
 };
 
-u64 map_grand_static_109[] = {
+u64 gMapGrandStatic109Tex[] = {
 #include "assets/archives/map_grand_static/map_109.i4.inc.c"
 };
 
-u64 map_grand_static_10A[] = {
+u64 gMapGrandStatic10ATex[] = {
 #include "assets/archives/map_grand_static/map_10A.i4.inc.c"
 };
 
-u64 map_grand_static_10B[] = {
+u64 gMapGrandStatic10BTex[] = {
 #include "assets/archives/map_grand_static/map_10B.i4.inc.c"
 };
 
-u64 map_grand_static_10C[] = {
+u64 gMapGrandStatic10CTex[] = {
 #include "assets/archives/map_grand_static/map_10C.i4.inc.c"
 };
 
-u64 map_grand_static_10D[] = {
+u64 gMapGrandStatic10DTex[] = {
 #include "assets/archives/map_grand_static/map_10D.i4.inc.c"
 };
 
-u64 map_grand_static_10E[] = {
+u64 gMapGrandStatic10ETex[] = {
 #include "assets/archives/map_grand_static/map_10E.i4.inc.c"
 };
 
-u64 map_grand_static_10F[] = {
+u64 gMapGrandStatic10FTex[] = {
 #include "assets/archives/map_grand_static/map_10F.i4.inc.c"
 };
 
-u64 map_grand_static_110[] = {
+u64 gMapGrandStatic110Tex[] = {
 #include "assets/archives/map_grand_static/map_110.i4.inc.c"
 };
 
-u64 map_grand_static_111[] = {
+u64 gMapGrandStatic111Tex[] = {
 #include "assets/archives/map_grand_static/map_111.i4.inc.c"
 };
 
-u64 map_grand_static_112[] = {
+u64 gMapGrandStatic112Tex[] = {
 #include "assets/archives/map_grand_static/map_112.i4.inc.c"
 };
 
-u64 map_grand_static_113[] = {
+u64 gMapGrandStatic113Tex[] = {
 #include "assets/archives/map_grand_static/map_113.i4.inc.c"
 };
 
-u64 map_grand_static_114[] = {
+u64 gMapGrandStatic114Tex[] = {
 #include "assets/archives/map_grand_static/map_114.i4.inc.c"
 };
 
-u64 map_grand_static_115[] = {
+u64 gMapGrandStatic115Tex[] = {
 #include "assets/archives/map_grand_static/map_115.i4.inc.c"
 };
 
-u64 map_grand_static_116[] = {
+u64 gMapGrandStatic116Tex[] = {
 #include "assets/archives/map_grand_static/map_116.i4.inc.c"
 };
 
-u64 map_grand_static_117[] = {
+u64 gMapGrandStatic117Tex[] = {
 #include "assets/archives/map_grand_static/map_117.i4.inc.c"
 };
 
-u64 map_grand_static_118[] = {
+u64 gMapGrandStatic118Tex[] = {
 #include "assets/archives/map_grand_static/map_118.i4.inc.c"
 };
 
-u64 map_grand_static_119[] = {
+u64 gMapGrandStatic119Tex[] = {
 #include "assets/archives/map_grand_static/map_119.i4.inc.c"
 };
 
-u64 map_grand_static_11A[] = {
+u64 gMapGrandStatic11ATex[] = {
 #include "assets/archives/map_grand_static/map_11A.i4.inc.c"
 };
 
-u64 map_grand_static_11B[] = {
+u64 gMapGrandStatic11BTex[] = {
 #include "assets/archives/map_grand_static/map_11B.i4.inc.c"
 };
 
-u64 map_grand_static_11C[] = {
+u64 gMapGrandStatic11CTex[] = {
 #include "assets/archives/map_grand_static/map_11C.i4.inc.c"
 };
 
-u64 map_grand_static_11D[] = {
+u64 gMapGrandStatic11DTex[] = {
 #include "assets/archives/map_grand_static/map_11D.i4.inc.c"
 };
 
-u64 map_grand_static_11E[] = {
+u64 gMapGrandStatic11ETex[] = {
 #include "assets/archives/map_grand_static/map_11E.i4.inc.c"
 };
 
-u64 map_grand_static_11F[] = {
+u64 gMapGrandStatic11FTex[] = {
 #include "assets/archives/map_grand_static/map_11F.i4.inc.c"
 };
 
-u64 map_grand_static_120[] = {
+u64 gMapGrandStatic120Tex[] = {
 #include "assets/archives/map_grand_static/map_120.i4.inc.c"
 };
 
-u64 map_grand_static_121[] = {
+u64 gMapGrandStatic121Tex[] = {
 #include "assets/archives/map_grand_static/map_121.i4.inc.c"
 };
 
-u64 map_grand_static_122[] = {
+u64 gMapGrandStatic122Tex[] = {
 #include "assets/archives/map_grand_static/map_122.i4.inc.c"
 };
 
-u64 map_grand_static_123[] = {
+u64 gMapGrandStatic123Tex[] = {
 #include "assets/archives/map_grand_static/map_123.i4.inc.c"
 };
 
-u64 map_grand_static_124[] = {
+u64 gMapGrandStatic124Tex[] = {
 #include "assets/archives/map_grand_static/map_124.i4.inc.c"
 };
 
-u64 map_grand_static_125[] = {
+u64 gMapGrandStatic125Tex[] = {
 #include "assets/archives/map_grand_static/map_125.i4.inc.c"
 };
 
-u64 map_grand_static_126[] = {
+u64 gMapGrandStatic126Tex[] = {
 #include "assets/archives/map_grand_static/map_126.i4.inc.c"
 };
 
-u64 map_grand_static_127[] = {
+u64 gMapGrandStatic127Tex[] = {
 #include "assets/archives/map_grand_static/map_127.i4.inc.c"
 };
 
-u64 map_grand_static_128[] = {
+u64 gMapGrandStatic128Tex[] = {
 #include "assets/archives/map_grand_static/map_128.i4.inc.c"
 };
 
-u64 map_grand_static_129[] = {
+u64 gMapGrandStatic129Tex[] = {
 #include "assets/archives/map_grand_static/map_129.i4.inc.c"
 };
 
-u64 map_grand_static_12A[] = {
+u64 gMapGrandStatic12ATex[] = {
 #include "assets/archives/map_grand_static/map_12A.i4.inc.c"
 };
 
-u64 map_grand_static_12B[] = {
+u64 gMapGrandStatic12BTex[] = {
 #include "assets/archives/map_grand_static/map_12B.i4.inc.c"
 };
 
-u64 map_grand_static_12C[] = {
+u64 gMapGrandStatic12CTex[] = {
 #include "assets/archives/map_grand_static/map_12C.i4.inc.c"
 };
 
-u64 map_grand_static_12D[] = {
+u64 gMapGrandStatic12DTex[] = {
 #include "assets/archives/map_grand_static/map_12D.i4.inc.c"
 };
 
-u64 map_grand_static_12E[] = {
+u64 gMapGrandStatic12ETex[] = {
 #include "assets/archives/map_grand_static/map_12E.i4.inc.c"
 };
 
-u64 map_grand_static_12F[] = {
+u64 gMapGrandStatic12FTex[] = {
 #include "assets/archives/map_grand_static/map_12F.i4.inc.c"
 };
 
-u64 map_grand_static_130[] = {
+u64 gMapGrandStatic130Tex[] = {
 #include "assets/archives/map_grand_static/map_130.i4.inc.c"
 };
 
-u64 map_grand_static_131[] = {
+u64 gMapGrandStatic131Tex[] = {
 #include "assets/archives/map_grand_static/map_131.i4.inc.c"
 };
 
-u64 map_grand_static_132[] = {
+u64 gMapGrandStatic132Tex[] = {
 #include "assets/archives/map_grand_static/map_132.i4.inc.c"
 };
 
-u64 map_grand_static_133[] = {
+u64 gMapGrandStatic133Tex[] = {
 #include "assets/archives/map_grand_static/map_133.i4.inc.c"
 };
 
-u64 map_grand_static_134[] = {
+u64 gMapGrandStatic134Tex[] = {
 #include "assets/archives/map_grand_static/map_134.i4.inc.c"
 };
 
-u64 map_grand_static_135[] = {
+u64 gMapGrandStatic135Tex[] = {
 #include "assets/archives/map_grand_static/map_135.i4.inc.c"
 };
 
-u64 map_grand_static_136[] = {
+u64 gMapGrandStatic136Tex[] = {
 #include "assets/archives/map_grand_static/map_136.i4.inc.c"
 };
 
-u64 map_grand_static_137[] = {
+u64 gMapGrandStatic137Tex[] = {
 #include "assets/archives/map_grand_static/map_137.i4.inc.c"
 };
 
-u64 map_grand_static_138[] = {
+u64 gMapGrandStatic138Tex[] = {
 #include "assets/archives/map_grand_static/map_138.i4.inc.c"
 };
 
-u64 map_grand_static_139[] = {
+u64 gMapGrandStatic139Tex[] = {
 #include "assets/archives/map_grand_static/map_139.i4.inc.c"
 };
 
-u64 map_grand_static_13A[] = {
+u64 gMapGrandStatic13ATex[] = {
 #include "assets/archives/map_grand_static/map_13A.i4.inc.c"
 };
 
-u64 map_grand_static_13B[] = {
+u64 gMapGrandStatic13BTex[] = {
 #include "assets/archives/map_grand_static/map_13B.i4.inc.c"
 };
 
-u64 map_grand_static_13C[] = {
+u64 gMapGrandStatic13CTex[] = {
 #include "assets/archives/map_grand_static/map_13C.i4.inc.c"
 };
 
-u64 map_grand_static_13D[] = {
+u64 gMapGrandStatic13DTex[] = {
 #include "assets/archives/map_grand_static/map_13D.i4.inc.c"
 };
 
-u64 map_grand_static_13E[] = {
+u64 gMapGrandStatic13ETex[] = {
 #include "assets/archives/map_grand_static/map_13E.i4.inc.c"
 };
 
-u64 map_grand_static_13F[] = {
+u64 gMapGrandStatic13FTex[] = {
 #include "assets/archives/map_grand_static/map_13F.i4.inc.c"
 };
 
-u64 map_grand_static_140[] = {
+u64 gMapGrandStatic140Tex[] = {
 #include "assets/archives/map_grand_static/map_140.i4.inc.c"
 };
 
-u64 map_grand_static_141[] = {
+u64 gMapGrandStatic141Tex[] = {
 #include "assets/archives/map_grand_static/map_141.i4.inc.c"
 };
 
-u64 map_grand_static_142[] = {
+u64 gMapGrandStatic142Tex[] = {
 #include "assets/archives/map_grand_static/map_142.i4.inc.c"
 };
 
-u64 map_grand_static_143[] = {
+u64 gMapGrandStatic143Tex[] = {
 #include "assets/archives/map_grand_static/map_143.i4.inc.c"
 };
 
-u64 map_grand_static_144[] = {
+u64 gMapGrandStatic144Tex[] = {
 #include "assets/archives/map_grand_static/map_144.i4.inc.c"
 };
 
-u64 map_grand_static_145[] = {
+u64 gMapGrandStatic145Tex[] = {
 #include "assets/archives/map_grand_static/map_145.i4.inc.c"
 };
 
-u64 map_grand_static_146[] = {
+u64 gMapGrandStatic146Tex[] = {
 #include "assets/archives/map_grand_static/map_146.i4.inc.c"
 };
 
-u64 map_grand_static_147[] = {
+u64 gMapGrandStatic147Tex[] = {
 #include "assets/archives/map_grand_static/map_147.i4.inc.c"
 };
 
-u64 map_grand_static_148[] = {
+u64 gMapGrandStatic148Tex[] = {
 #include "assets/archives/map_grand_static/map_148.i4.inc.c"
 };
 
-u64 map_grand_static_149[] = {
+u64 gMapGrandStatic149Tex[] = {
 #include "assets/archives/map_grand_static/map_149.i4.inc.c"
 };
 
-u64 map_grand_static_14A[] = {
+u64 gMapGrandStatic14ATex[] = {
 #include "assets/archives/map_grand_static/map_14A.i4.inc.c"
 };
 
-u64 map_grand_static_14B[] = {
+u64 gMapGrandStatic14BTex[] = {
 #include "assets/archives/map_grand_static/map_14B.i4.inc.c"
 };
 
-u64 map_grand_static_14C[] = {
+u64 gMapGrandStatic14CTex[] = {
 #include "assets/archives/map_grand_static/map_14C.i4.inc.c"
 };
 
-u64 map_grand_static_14D[] = {
+u64 gMapGrandStatic14DTex[] = {
 #include "assets/archives/map_grand_static/map_14D.i4.inc.c"
 };
 
-u64 map_grand_static_14E[] = {
+u64 gMapGrandStatic14ETex[] = {
 #include "assets/archives/map_grand_static/map_14E.i4.inc.c"
 };
 
-u64 map_grand_static_14F[] = {
+u64 gMapGrandStatic14FTex[] = {
 #include "assets/archives/map_grand_static/map_14F.i4.inc.c"
 };
 
-u64 map_grand_static_150[] = {
+u64 gMapGrandStatic150Tex[] = {
 #include "assets/archives/map_grand_static/map_150.i4.inc.c"
 };
 
-u64 map_grand_static_151[] = {
+u64 gMapGrandStatic151Tex[] = {
 #include "assets/archives/map_grand_static/map_151.i4.inc.c"
 };
 
-u64 map_grand_static_152[] = {
+u64 gMapGrandStatic152Tex[] = {
 #include "assets/archives/map_grand_static/map_152.i4.inc.c"
 };
 
-u64 map_grand_static_153[] = {
+u64 gMapGrandStatic153Tex[] = {
 #include "assets/archives/map_grand_static/map_153.i4.inc.c"
 };
 
-u64 map_grand_static_154[] = {
+u64 gMapGrandStatic154Tex[] = {
 #include "assets/archives/map_grand_static/map_154.i4.inc.c"
 };
 
-u64 map_grand_static_155[] = {
+u64 gMapGrandStatic155Tex[] = {
 #include "assets/archives/map_grand_static/map_155.i4.inc.c"
 };
 
-u64 map_grand_static_156[] = {
+u64 gMapGrandStatic156Tex[] = {
 #include "assets/archives/map_grand_static/map_156.i4.inc.c"
 };
 
-u64 map_grand_static_157[] = {
+u64 gMapGrandStatic157Tex[] = {
 #include "assets/archives/map_grand_static/map_157.i4.inc.c"
 };
 
-u64 map_grand_static_158[] = {
+u64 gMapGrandStatic158Tex[] = {
 #include "assets/archives/map_grand_static/map_158.i4.inc.c"
 };
 
-u64 map_grand_static_159[] = {
+u64 gMapGrandStatic159Tex[] = {
 #include "assets/archives/map_grand_static/map_159.i4.inc.c"
 };
 
-u64 map_grand_static_15A[] = {
+u64 gMapGrandStatic15ATex[] = {
 #include "assets/archives/map_grand_static/map_15A.i4.inc.c"
 };
 
-u64 map_grand_static_15B[] = {
+u64 gMapGrandStatic15BTex[] = {
 #include "assets/archives/map_grand_static/map_15B.i4.inc.c"
 };
 
-u64 map_grand_static_15C[] = {
+u64 gMapGrandStatic15CTex[] = {
 #include "assets/archives/map_grand_static/map_15C.i4.inc.c"
 };
 
-u64 map_grand_static_15D[] = {
+u64 gMapGrandStatic15DTex[] = {
 #include "assets/archives/map_grand_static/map_15D.i4.inc.c"
 };
 
-u64 map_grand_static_15E[] = {
+u64 gMapGrandStatic15ETex[] = {
 #include "assets/archives/map_grand_static/map_15E.i4.inc.c"
 };
 
-u64 map_grand_static_15F[] = {
+u64 gMapGrandStatic15FTex[] = {
 #include "assets/archives/map_grand_static/map_15F.i4.inc.c"
 };
 
-u64 map_grand_static_160[] = {
+u64 gMapGrandStatic160Tex[] = {
 #include "assets/archives/map_grand_static/map_160.i4.inc.c"
 };
 
-u64 map_grand_static_161[] = {
+u64 gMapGrandStatic161Tex[] = {
 #include "assets/archives/map_grand_static/map_161.i4.inc.c"
 };
 
--- assets/c/archives/map_i_static/map_i_static.c
+++ assets/c/archives/map_i_static/map_i_static.c
@@ -5,235 +5,235 @@
 #include "assets/misc/link_animetion/link_animetion.h"
 #include "objects/gameplay_keep/gameplay_keep.h"
 
-u64 map_i_static_00[] = {
+u64 gMapIStatic00Tex[] = {
 #include "assets/archives/map_i_static/map_00.i4.inc.c"
 };
 
-u64 map_i_static_01[] = {
+u64 gMapIStatic01Tex[] = {
 #include "assets/archives/map_i_static/map_01.i4.inc.c"
 };
 
-u64 map_i_static_02[] = {
+u64 gMapIStatic02Tex[] = {
 #include "assets/archives/map_i_static/map_02.i4.inc.c"
 };
 
-u64 map_i_static_03[] = {
+u64 gMapIStatic03Tex[] = {
 #include "assets/archives/map_i_static/map_03.i4.inc.c"
 };
 
-u64 map_i_static_04[] = {
+u64 gMapIStatic04Tex[] = {
 #include "assets/archives/map_i_static/map_04.i4.inc.c"
 };
 
-u64 map_i_static_05[] = {
+u64 gMapIStatic05Tex[] = {
 #include "assets/archives/map_i_static/map_05.i4.inc.c"
 };
 
-u64 map_i_static_06[] = {
+u64 gMapIStatic06Tex[] = {
 #include "assets/archives/map_i_static/map_06.i4.inc.c"
 };
 
-u64 map_i_static_07[] = {
+u64 gMapIStatic07Tex[] = {
 #include "assets/archives/map_i_static/map_07.i4.inc.c"
 };
 
-u64 map_i_static_08[] = {
+u64 gMapIStatic08Tex[] = {
 #include "assets/archives/map_i_static/map_08.i4.inc.c"
 };
 
-u64 map_i_static_09[] = {
+u64 gMapIStatic09Tex[] = {
 #include "assets/archives/map_i_static/map_09.i4.inc.c"
 };
 
-u64 map_i_static_0A[] = {
+u64 gMapIStatic0ATex[] = {
 #include "assets/archives/map_i_static/map_0A.i4.inc.c"
 };
 
-u64 map_i_static_0B[] = {
+u64 gMapIStatic0BTex[] = {
 #include "assets/archives/map_i_static/map_0B.i4.inc.c"
 };
 
-u64 map_i_static_0C[] = {
+u64 gMapIStatic0CTex[] = {
 #include "assets/archives/map_i_static/map_0C.i4.inc.c"
 };
 
-u64 map_i_static_0D[] = {
+u64 gMapIStatic0DTex[] = {
 #include "assets/archives/map_i_static/map_0D.i4.inc.c"
 };
 
-u64 map_i_static_0E[] = {
+u64 gMapIStatic0ETex[] = {
 #include "assets/archives/map_i_static/map_0E.i4.inc.c"
 };
 
-u64 map_i_static_0F[] = {
+u64 gMapIStatic0FTex[] = {
 #include "assets/archives/map_i_static/map_0F.i4.inc.c"
 };
 
-u64 map_i_static_10[] = {
+u64 gMapIStatic10Tex[] = {
 #include "assets/archives/map_i_static/map_10.i4.inc.c"
 };
 
-u64 map_i_static_11[] = {
+u64 gMapIStatic11Tex[] = {
 #include "assets/archives/map_i_static/map_11.i4.inc.c"
 };
 
-u64 map_i_static_12[] = {
+u64 gMapIStatic12Tex[] = {
 #include "assets/archives/map_i_static/map_12.i4.inc.c"
 };
 
-u64 map_i_static_13[] = {
+u64 gMapIStatic13Tex[] = {
 #include "assets/archives/map_i_static/map_13.i4.inc.c"
 };
 
-u64 map_i_static_14[] = {
+u64 gMapIStatic14Tex[] = {
 #include "assets/archives/map_i_static/map_14.i4.inc.c"
 };
 
-u64 map_i_static_15[] = {
+u64 gMapIStatic15Tex[] = {
 #include "assets/archives/map_i_static/map_15.i4.inc.c"
 };
 
-u64 map_i_static_16[] = {
+u64 gMapIStatic16Tex[] = {
 #include "assets/archives/map_i_static/map_16.i4.inc.c"
 };
 
-u64 map_i_static_17[] = {
+u64 gMapIStatic17Tex[] = {
 #include "assets/archives/map_i_static/map_17.i4.inc.c"
 };
 
-u64 map_i_static_18[] = {
+u64 gMapIStatic18Tex[] = {
 #include "assets/archives/map_i_static/map_18.i4.inc.c"
 };
 
-u64 map_i_static_19[] = {
+u64 gMapIStatic19Tex[] = {
 #include "assets/archives/map_i_static/map_19.i4.inc.c"
 };
 
-u64 map_i_static_1A[] = {
+u64 gMapIStatic1ATex[] = {
 #include "assets/archives/map_i_static/map_1A.i4.inc.c"
 };
 
-u64 map_i_static_1B[] = {
+u64 gMapIStatic1BTex[] = {
 #include "assets/archives/map_i_static/map_1B.i4.inc.c"
 };
 
-u64 map_i_static_1C[] = {
+u64 gMapIStatic1CTex[] = {
 #include "assets/archives/map_i_static/map_1C.i4.inc.c"
 };
 
-u64 map_i_static_1D[] = {
+u64 gMapIStatic1DTex[] = {
 #include "assets/archives/map_i_static/map_1D.i4.inc.c"
 };
 
-u64 map_i_static_1E[] = {
+u64 gMapIStatic1ETex[] = {
 #include "assets/archives/map_i_static/map_1E.i4.inc.c"
 };
 
-u64 map_i_static_1F[] = {
+u64 gMapIStatic1FTex[] = {
 #include "assets/archives/map_i_static/map_1F.i4.inc.c"
 };
 
-u64 map_i_static_20[] = {
+u64 gMapIStatic20Tex[] = {
 #include "assets/archives/map_i_static/map_20.i4.inc.c"
 };
 
-u64 map_i_static_21[] = {
+u64 gMapIStatic21Tex[] = {
 #include "assets/archives/map_i_static/map_21.i4.inc.c"
 };
 
-u64 map_i_static_22[] = {
+u64 gMapIStatic22Tex[] = {
 #include "assets/archives/map_i_static/map_22.i4.inc.c"
 };
 
-u64 map_i_static_23[] = {
+u64 gMapIStatic23Tex[] = {
 #include "assets/archives/map_i_static/map_23.i4.inc.c"
 };
 
-u64 map_i_static_24[] = {
+u64 gMapIStatic24Tex[] = {
 #include "assets/archives/map_i_static/map_24.i4.inc.c"
 };
 
-u64 map_i_static_25[] = {
+u64 gMapIStatic25Tex[] = {
 #include "assets/archives/map_i_static/map_25.i4.inc.c"
 };
 
-u64 map_i_static_26[] = {
+u64 gMapIStatic26Tex[] = {
 #include "assets/archives/map_i_static/map_26.i4.inc.c"
 };
 
-u64 map_i_static_27[] = {
+u64 gMapIStatic27Tex[] = {
 #include "assets/archives/map_i_static/map_27.i4.inc.c"
 };
 
-u64 map_i_static_28[] = {
+u64 gMapIStatic28Tex[] = {
 #include "assets/archives/map_i_static/map_28.i4.inc.c"
 };
 
-u64 map_i_static_29[] = {
+u64 gMapIStatic29Tex[] = {
 #include "assets/archives/map_i_static/map_29.i4.inc.c"
 };
 
-u64 map_i_static_2A[] = {
+u64 gMapIStatic2ATex[] = {
 #include "assets/archives/map_i_static/map_2A.i4.inc.c"
 };
 
-u64 map_i_static_2B[] = {
+u64 gMapIStatic2BTex[] = {
 #include "assets/archives/map_i_static/map_2B.i4.inc.c"
 };
 
-u64 map_i_static_2C[] = {
+u64 gMapIStatic2CTex[] = {
 #include "assets/archives/map_i_static/map_2C.i4.inc.c"
 };
 
-u64 map_i_static_2D[] = {
+u64 gMapIStatic2DTex[] = {
 #include "assets/archives/map_i_static/map_2D.i4.inc.c"
 };
 
-u64 map_i_static_2E[] = {
+u64 gMapIStatic2ETex[] = {
 #include "assets/archives/map_i_static/map_2E.i4.inc.c"
 };
 
-u64 map_i_static_2F[] = {
+u64 gMapIStatic2FTex[] = {
 #include "assets/archives/map_i_static/map_2F.i4.inc.c"
 };
 
-u64 map_i_static_30[] = {
+u64 gMapIStatic30Tex[] = {
 #include "assets/archives/map_i_static/map_30.i4.inc.c"
 };
 
-u64 map_i_static_31[] = {
+u64 gMapIStatic31Tex[] = {
 #include "assets/archives/map_i_static/map_31.i4.inc.c"
 };
 
-u64 map_i_static_32[] = {
+u64 gMapIStatic32Tex[] = {
 #include "assets/archives/map_i_static/map_32.i4.inc.c"
 };
 
-u64 map_i_static_33[] = {
+u64 gMapIStatic33Tex[] = {
 #include "assets/archives/map_i_static/map_33.i4.inc.c"
 };
 
-u64 map_i_static_34[] = {
+u64 gMapIStatic34Tex[] = {
 #include "assets/archives/map_i_static/map_34.i4.inc.c"
 };
 
-u64 map_i_static_35[] = {
+u64 gMapIStatic35Tex[] = {
 #include "assets/archives/map_i_static/map_35.i4.inc.c"
 };
 
-u64 map_i_static_36[] = {
+u64 gMapIStatic36Tex[] = {
 #include "assets/archives/map_i_static/map_36.i4.inc.c"
 };
 
-u64 map_i_static_37[] = {
+u64 gMapIStatic37Tex[] = {
 #include "assets/archives/map_i_static/map_37.i4.inc.c"
 };
 
-u64 map_i_static_38[] = {
+u64 gMapIStatic38Tex[] = {
 #include "assets/archives/map_i_static/map_38.i4.inc.c"
 };
 
-u64 map_i_static_39[] = {
+u64 gMapIStatic39Tex[] = {
 #include "assets/archives/map_i_static/map_39.i4.inc.c"
 };
 
