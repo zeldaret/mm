552a941d74132722c4d5a5975670216cb2a7cd80
angie <angheloalf95@gmail.com>
2023-10-13 22:55:57 -0300

    commit c files

---
+++ assets/c/archives/map_grand_static/map_grand_static.c
@@ -0,0 +1,515 @@
+#include "ultra64.h"
+#include "z64.h"
+#include "macros.h"
+#include "map_grand_static.h"
+#include "assets/misc/link_animetion/link_animetion.h"
+#include "objects/gameplay_keep/gameplay_keep.h"
+
+u64 map_grand_static_100[] = {
+#include "assets/archives/map_grand_static/map_100.i4.inc.c"
+};
+
+u64 map_grand_static_101[] = {
+#include "assets/archives/map_grand_static/map_101.i4.inc.c"
+};
+
+u64 map_grand_static_102[] = {
+#include "assets/archives/map_grand_static/map_102.i4.inc.c"
+};
+
+u64 map_grand_static_103[] = {
+#include "assets/archives/map_grand_static/map_103.i4.inc.c"
+};
+
+u64 map_grand_static_104[] = {
+#include "assets/archives/map_grand_static/map_104.i4.inc.c"
+};
+
+u64 map_grand_static_105[] = {
+#include "assets/archives/map_grand_static/map_105.i4.inc.c"
+};
+
+u64 map_grand_static_106[] = {
+#include "assets/archives/map_grand_static/map_106.i4.inc.c"
+};
+
+u64 map_grand_static_107[] = {
+#include "assets/archives/map_grand_static/map_107.i4.inc.c"
+};
+
+u64 map_grand_static_108[] = {
+#include "assets/archives/map_grand_static/map_108.i4.inc.c"
+};
+
+u64 map_grand_static_109[] = {
+#include "assets/archives/map_grand_static/map_109.i4.inc.c"
+};
+
+u64 map_grand_static_10A[] = {
+#include "assets/archives/map_grand_static/map_10A.i4.inc.c"
+};
+
+u64 map_grand_static_10B[] = {
+#include "assets/archives/map_grand_static/map_10B.i4.inc.c"
+};
+
+u64 map_grand_static_10C[] = {
+#include "assets/archives/map_grand_static/map_10C.i4.inc.c"
+};
+
+u64 map_grand_static_10D[] = {
+#include "assets/archives/map_grand_static/map_10D.i4.inc.c"
+};
+
+u64 map_grand_static_10E[] = {
+#include "assets/archives/map_grand_static/map_10E.i4.inc.c"
+};
+
+u64 map_grand_static_10F[] = {
+#include "assets/archives/map_grand_static/map_10F.i4.inc.c"
+};
+
+u64 map_grand_static_110[] = {
+#include "assets/archives/map_grand_static/map_110.i4.inc.c"
+};
+
+u64 map_grand_static_111[] = {
+#include "assets/archives/map_grand_static/map_111.i4.inc.c"
+};
+
+u64 map_grand_static_112[] = {
+#include "assets/archives/map_grand_static/map_112.i4.inc.c"
+};
+
+u64 map_grand_static_113[] = {
+#include "assets/archives/map_grand_static/map_113.i4.inc.c"
+};
+
+u64 map_grand_static_114[] = {
+#include "assets/archives/map_grand_static/map_114.i4.inc.c"
+};
+
+u64 map_grand_static_115[] = {
+#include "assets/archives/map_grand_static/map_115.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_010E68[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_116[] = {
+#include "assets/archives/map_grand_static/map_116.i4.inc.c"
+};
+
+u64 map_grand_static_117[] = {
+#include "assets/archives/map_grand_static/map_117.i4.inc.c"
+};
+
+u64 map_grand_static_118[] = {
+#include "assets/archives/map_grand_static/map_118.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_012C08[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_119[] = {
+#include "assets/archives/map_grand_static/map_119.i4.inc.c"
+};
+
+u64 map_grand_static_11A[] = {
+#include "assets/archives/map_grand_static/map_11A.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_013DF8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_11B[] = {
+#include "assets/archives/map_grand_static/map_11B.i4.inc.c"
+};
+
+u64 map_grand_static_11C[] = {
+#include "assets/archives/map_grand_static/map_11C.i4.inc.c"
+};
+
+u64 map_grand_static_11D[] = {
+#include "assets/archives/map_grand_static/map_11D.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_014FF8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_11E[] = {
+#include "assets/archives/map_grand_static/map_11E.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_015588[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_11F[] = {
+#include "assets/archives/map_grand_static/map_11F.i4.inc.c"
+};
+
+u64 map_grand_static_120[] = {
+#include "assets/archives/map_grand_static/map_120.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_0162C8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_121[] = {
+#include "assets/archives/map_grand_static/map_121.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_016A68[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_122[] = {
+#include "assets/archives/map_grand_static/map_122.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_017858[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_123[] = {
+#include "assets/archives/map_grand_static/map_123.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_0180A8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_124[] = {
+#include "assets/archives/map_grand_static/map_124.i4.inc.c"
+};
+
+u64 map_grand_static_125[] = {
+#include "assets/archives/map_grand_static/map_125.i4.inc.c"
+};
+
+u64 map_grand_static_126[] = {
+#include "assets/archives/map_grand_static/map_126.i4.inc.c"
+};
+
+u64 map_grand_static_127[] = {
+#include "assets/archives/map_grand_static/map_127.i4.inc.c"
+};
+
+u64 map_grand_static_128[] = {
+#include "assets/archives/map_grand_static/map_128.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_019F08[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_129[] = {
+#include "assets/archives/map_grand_static/map_129.i4.inc.c"
+};
+
+u64 map_grand_static_12A[] = {
+#include "assets/archives/map_grand_static/map_12A.i4.inc.c"
+};
+
+u64 map_grand_static_12B[] = {
+#include "assets/archives/map_grand_static/map_12B.i4.inc.c"
+};
+
+u64 map_grand_static_12C[] = {
+#include "assets/archives/map_grand_static/map_12C.i4.inc.c"
+};
+
+u64 map_grand_static_12D[] = {
+#include "assets/archives/map_grand_static/map_12D.i4.inc.c"
+};
+
+u64 map_grand_static_12E[] = {
+#include "assets/archives/map_grand_static/map_12E.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_01D9F8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_12F[] = {
+#include "assets/archives/map_grand_static/map_12F.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_01E798[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_130[] = {
+#include "assets/archives/map_grand_static/map_130.i4.inc.c"
+};
+
+u64 map_grand_static_131[] = {
+#include "assets/archives/map_grand_static/map_131.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_01F398[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_132[] = {
+#include "assets/archives/map_grand_static/map_132.i4.inc.c"
+};
+
+u64 map_grand_static_133[] = {
+#include "assets/archives/map_grand_static/map_133.i4.inc.c"
+};
+
+u64 map_grand_static_134[] = {
+#include "assets/archives/map_grand_static/map_134.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_020DD8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_135[] = {
+#include "assets/archives/map_grand_static/map_135.i4.inc.c"
+};
+
+u64 map_grand_static_136[] = {
+#include "assets/archives/map_grand_static/map_136.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_021908[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_137[] = {
+#include "assets/archives/map_grand_static/map_137.i4.inc.c"
+};
+
+u64 map_grand_static_138[] = {
+#include "assets/archives/map_grand_static/map_138.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_0225B8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_139[] = {
+#include "assets/archives/map_grand_static/map_139.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_022D88[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_13A[] = {
+#include "assets/archives/map_grand_static/map_13A.i4.inc.c"
+};
+
+u64 map_grand_static_13B[] = {
+#include "assets/archives/map_grand_static/map_13B.i4.inc.c"
+};
+
+u64 map_grand_static_13C[] = {
+#include "assets/archives/map_grand_static/map_13C.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_024B68[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_13D[] = {
+#include "assets/archives/map_grand_static/map_13D.i4.inc.c"
+};
+
+u64 map_grand_static_13E[] = {
+#include "assets/archives/map_grand_static/map_13E.i4.inc.c"
+};
+
+u64 map_grand_static_13F[] = {
+#include "assets/archives/map_grand_static/map_13F.i4.inc.c"
+};
+
+u64 map_grand_static_140[] = {
+#include "assets/archives/map_grand_static/map_140.i4.inc.c"
+};
+
+u64 map_grand_static_141[] = {
+#include "assets/archives/map_grand_static/map_141.i4.inc.c"
+};
+
+u64 map_grand_static_142[] = {
+#include "assets/archives/map_grand_static/map_142.i4.inc.c"
+};
+
+u64 map_grand_static_143[] = {
+#include "assets/archives/map_grand_static/map_143.i4.inc.c"
+};
+
+u64 map_grand_static_144[] = {
+#include "assets/archives/map_grand_static/map_144.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_0274D8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_145[] = {
+#include "assets/archives/map_grand_static/map_145.i4.inc.c"
+};
+
+u64 map_grand_static_146[] = {
+#include "assets/archives/map_grand_static/map_146.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_027E88[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_147[] = {
+#include "assets/archives/map_grand_static/map_147.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_028388[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_148[] = {
+#include "assets/archives/map_grand_static/map_148.i4.inc.c"
+};
+
+u64 map_grand_static_149[] = {
+#include "assets/archives/map_grand_static/map_149.i4.inc.c"
+};
+
+u64 map_grand_static_14A[] = {
+#include "assets/archives/map_grand_static/map_14A.i4.inc.c"
+};
+
+u64 map_grand_static_14B[] = {
+#include "assets/archives/map_grand_static/map_14B.i4.inc.c"
+};
+
+u64 map_grand_static_14C[] = {
+#include "assets/archives/map_grand_static/map_14C.i4.inc.c"
+};
+
+u64 map_grand_static_14D[] = {
+#include "assets/archives/map_grand_static/map_14D.i4.inc.c"
+};
+
+u64 map_grand_static_14E[] = {
+#include "assets/archives/map_grand_static/map_14E.i4.inc.c"
+};
+
+u64 map_grand_static_14F[] = {
+#include "assets/archives/map_grand_static/map_14F.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_02C1E8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_150[] = {
+#include "assets/archives/map_grand_static/map_150.i4.inc.c"
+};
+
+u64 map_grand_static_151[] = {
+#include "assets/archives/map_grand_static/map_151.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_02D4D8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_152[] = {
+#include "assets/archives/map_grand_static/map_152.i4.inc.c"
+};
+
+u64 map_grand_static_153[] = {
+#include "assets/archives/map_grand_static/map_153.i4.inc.c"
+};
+
+u64 map_grand_static_154[] = {
+#include "assets/archives/map_grand_static/map_154.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_02EAD8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_155[] = {
+#include "assets/archives/map_grand_static/map_155.i4.inc.c"
+};
+
+u64 map_grand_static_156[] = {
+#include "assets/archives/map_grand_static/map_156.i4.inc.c"
+};
+
+u64 map_grand_static_157[] = {
+#include "assets/archives/map_grand_static/map_157.i4.inc.c"
+};
+
+u64 map_grand_static_158[] = {
+#include "assets/archives/map_grand_static/map_158.i4.inc.c"
+};
+
+u64 map_grand_static_159[] = {
+#include "assets/archives/map_grand_static/map_159.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_031C28[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_15A[] = {
+#include "assets/archives/map_grand_static/map_15A.i4.inc.c"
+};
+
+u64 map_grand_static_15B[] = {
+#include "assets/archives/map_grand_static/map_15B.i4.inc.c"
+};
+
+u64 map_grand_static_15C[] = {
+#include "assets/archives/map_grand_static/map_15C.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_033698[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_15D[] = {
+#include "assets/archives/map_grand_static/map_15D.i4.inc.c"
+};
+
+u64 map_grand_static_15E[] = {
+#include "assets/archives/map_grand_static/map_15E.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_034768[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_15F[] = {
+#include "assets/archives/map_grand_static/map_15F.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_034BA8[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_160[] = {
+#include "assets/archives/map_grand_static/map_160.i4.inc.c"
+};
+
+u8 map_grand_static.unarchive_possiblePadding_034F48[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+};
+
+u64 map_grand_static_161[] = {
+#include "assets/archives/map_grand_static/map_161.i4.inc.c"
+};
+
---
+++ assets/c/archives/map_i_static/map_i_static.c
@@ -0,0 +1,239 @@
+#include "ultra64.h"
+#include "z64.h"
+#include "macros.h"
+#include "map_i_static.h"
+#include "assets/misc/link_animetion/link_animetion.h"
+#include "objects/gameplay_keep/gameplay_keep.h"
+
+u64 map_i_static_00[] = {
+#include "assets/archives/map_i_static/map_00.i4.inc.c"
+};
+
+u64 map_i_static_01[] = {
+#include "assets/archives/map_i_static/map_01.i4.inc.c"
+};
+
+u64 map_i_static_02[] = {
+#include "assets/archives/map_i_static/map_02.i4.inc.c"
+};
+
+u64 map_i_static_03[] = {
+#include "assets/archives/map_i_static/map_03.i4.inc.c"
+};
+
+u64 map_i_static_04[] = {
+#include "assets/archives/map_i_static/map_04.i4.inc.c"
+};
+
+u64 map_i_static_05[] = {
+#include "assets/archives/map_i_static/map_05.i4.inc.c"
+};
+
+u64 map_i_static_06[] = {
+#include "assets/archives/map_i_static/map_06.i4.inc.c"
+};
+
+u64 map_i_static_07[] = {
+#include "assets/archives/map_i_static/map_07.i4.inc.c"
+};
+
+u64 map_i_static_08[] = {
+#include "assets/archives/map_i_static/map_08.i4.inc.c"
+};
+
+u64 map_i_static_09[] = {
+#include "assets/archives/map_i_static/map_09.i4.inc.c"
+};
+
+u64 map_i_static_0A[] = {
+#include "assets/archives/map_i_static/map_0A.i4.inc.c"
+};
+
+u64 map_i_static_0B[] = {
+#include "assets/archives/map_i_static/map_0B.i4.inc.c"
+};
+
+u64 map_i_static_0C[] = {
+#include "assets/archives/map_i_static/map_0C.i4.inc.c"
+};
+
+u64 map_i_static_0D[] = {
+#include "assets/archives/map_i_static/map_0D.i4.inc.c"
+};
+
+u64 map_i_static_0E[] = {
+#include "assets/archives/map_i_static/map_0E.i4.inc.c"
+};
+
+u64 map_i_static_0F[] = {
+#include "assets/archives/map_i_static/map_0F.i4.inc.c"
+};
+
+u64 map_i_static_10[] = {
+#include "assets/archives/map_i_static/map_10.i4.inc.c"
+};
+
+u64 map_i_static_11[] = {
+#include "assets/archives/map_i_static/map_11.i4.inc.c"
+};
+
+u64 map_i_static_12[] = {
+#include "assets/archives/map_i_static/map_12.i4.inc.c"
+};
+
+u64 map_i_static_13[] = {
+#include "assets/archives/map_i_static/map_13.i4.inc.c"
+};
+
+u64 map_i_static_14[] = {
+#include "assets/archives/map_i_static/map_14.i4.inc.c"
+};
+
+u64 map_i_static_15[] = {
+#include "assets/archives/map_i_static/map_15.i4.inc.c"
+};
+
+u64 map_i_static_16[] = {
+#include "assets/archives/map_i_static/map_16.i4.inc.c"
+};
+
+u64 map_i_static_17[] = {
+#include "assets/archives/map_i_static/map_17.i4.inc.c"
+};
+
+u64 map_i_static_18[] = {
+#include "assets/archives/map_i_static/map_18.i4.inc.c"
+};
+
+u64 map_i_static_19[] = {
+#include "assets/archives/map_i_static/map_19.i4.inc.c"
+};
+
+u64 map_i_static_1A[] = {
+#include "assets/archives/map_i_static/map_1A.i4.inc.c"
+};
+
+u64 map_i_static_1B[] = {
+#include "assets/archives/map_i_static/map_1B.i4.inc.c"
+};
+
+u64 map_i_static_1C[] = {
+#include "assets/archives/map_i_static/map_1C.i4.inc.c"
+};
+
+u64 map_i_static_1D[] = {
+#include "assets/archives/map_i_static/map_1D.i4.inc.c"
+};
+
+u64 map_i_static_1E[] = {
+#include "assets/archives/map_i_static/map_1E.i4.inc.c"
+};
+
+u64 map_i_static_1F[] = {
+#include "assets/archives/map_i_static/map_1F.i4.inc.c"
+};
+
+u64 map_i_static_20[] = {
+#include "assets/archives/map_i_static/map_20.i4.inc.c"
+};
+
+u64 map_i_static_21[] = {
+#include "assets/archives/map_i_static/map_21.i4.inc.c"
+};
+
+u64 map_i_static_22[] = {
+#include "assets/archives/map_i_static/map_22.i4.inc.c"
+};
+
+u64 map_i_static_23[] = {
+#include "assets/archives/map_i_static/map_23.i4.inc.c"
+};
+
+u64 map_i_static_24[] = {
+#include "assets/archives/map_i_static/map_24.i4.inc.c"
+};
+
+u64 map_i_static_25[] = {
+#include "assets/archives/map_i_static/map_25.i4.inc.c"
+};
+
+u64 map_i_static_26[] = {
+#include "assets/archives/map_i_static/map_26.i4.inc.c"
+};
+
+u64 map_i_static_27[] = {
+#include "assets/archives/map_i_static/map_27.i4.inc.c"
+};
+
+u64 map_i_static_28[] = {
+#include "assets/archives/map_i_static/map_28.i4.inc.c"
+};
+
+u64 map_i_static_29[] = {
+#include "assets/archives/map_i_static/map_29.i4.inc.c"
+};
+
+u64 map_i_static_2A[] = {
+#include "assets/archives/map_i_static/map_2A.i4.inc.c"
+};
+
+u64 map_i_static_2B[] = {
+#include "assets/archives/map_i_static/map_2B.i4.inc.c"
+};
+
+u64 map_i_static_2C[] = {
+#include "assets/archives/map_i_static/map_2C.i4.inc.c"
+};
+
+u64 map_i_static_2D[] = {
+#include "assets/archives/map_i_static/map_2D.i4.inc.c"
+};
+
+u64 map_i_static_2E[] = {
+#include "assets/archives/map_i_static/map_2E.i4.inc.c"
+};
+
+u64 map_i_static_2F[] = {
+#include "assets/archives/map_i_static/map_2F.i4.inc.c"
+};
+
+u64 map_i_static_30[] = {
+#include "assets/archives/map_i_static/map_30.i4.inc.c"
+};
+
+u64 map_i_static_31[] = {
+#include "assets/archives/map_i_static/map_31.i4.inc.c"
+};
+
+u64 map_i_static_32[] = {
+#include "assets/archives/map_i_static/map_32.i4.inc.c"
+};
+
+u64 map_i_static_33[] = {
+#include "assets/archives/map_i_static/map_33.i4.inc.c"
+};
+
+u64 map_i_static_34[] = {
+#include "assets/archives/map_i_static/map_34.i4.inc.c"
+};
+
+u64 map_i_static_35[] = {
+#include "assets/archives/map_i_static/map_35.i4.inc.c"
+};
+
+u64 map_i_static_36[] = {
+#include "assets/archives/map_i_static/map_36.i4.inc.c"
+};
+
+u64 map_i_static_37[] = {
+#include "assets/archives/map_i_static/map_37.i4.inc.c"
+};
+
+u64 map_i_static_38[] = {
+#include "assets/archives/map_i_static/map_38.i4.inc.c"
+};
+
+u64 map_i_static_39[] = {
+#include "assets/archives/map_i_static/map_39.i4.inc.c"
+};
+
