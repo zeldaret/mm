6946b9825f409412353cc1c7554d4eb893d12708
Angie <angheloalf95@gmail.com>
2023-11-12 21:52:26 -0300

    rewrite SYSTEM_HEAP_END_ADDR in terms of other symbols

--- include/buffers.h
+++ include/buffers.h
@@ -16,6 +16,14 @@ extern GfxPool gGfxPools[2];
 extern u8 gAudioHeap[0x138000];
 extern u8 gSystemHeap[];
 
+
+/**
+ * The following variables are part of the `framebuffers` segment
+ */
+extern u8 gPictoPhotoI8[PICTO_PHOTO_SIZE];
+extern u8 D_80784600[0x56200];
+extern u16 gFramebuffer0[SCREEN_HEIGHT][SCREEN_WIDTH];
+
 /**
  * The system heap is an area of memory that covers all the RAM that is not
  * taken by compiled code/data.
@@ -25,14 +33,11 @@ extern u8 gSystemHeap[];
  * a fixed size.
  * This address is calculated by doing `0x80800000 - (size of framebuffers)`,
  * where 0x80800000 is the end of the Expansion Pak address range.
+ * In the vanilla game this value expands to `0x80780000`.
  */
 #ifndef SYSTEM_HEAP_END_ADDR
-#define SYSTEM_HEAP_END_ADDR 0x80780000
+#define SYSTEM_HEAP_END_ADDR (PHYS_TO_K0(0x800000) - sizeof(gFramebuffer0) - sizeof(D_80784600) - sizeof(gPictoPhotoI8))
 #endif
-
-extern u8 gPictoPhotoI8[PICTO_PHOTO_SIZE];
-extern u8 D_80784600[0x56200];
-extern u16 gFramebuffer0[SCREEN_HEIGHT][SCREEN_WIDTH];
 
 
 #endif
