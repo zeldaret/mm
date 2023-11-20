839d564a8501800c1c2c20ba4c66938cb62072c1
angie <angheloalf95@gmail.com>
2023-11-19 22:32:23 -0300

    Move out some stuff from macros.h

--- include/gfx.h
+++ include/gfx.h
@@ -283,4 +283,21 @@ extern Gfx gEmptyDL[];
         _g->words.w1 = (unsigned int)(d);                                         \
     })
 
+/**
+ * `x` vertex x
+ * `y` vertex y
+ * `z` vertex z
+ * `s` texture s coordinate
+ * `t` texture t coordinate
+ * `crnx` red component of color vertex, or x component of normal vertex
+ * `cgny` green component of color vertex, or y component of normal vertex
+ * `cbnz` blue component of color vertex, or z component of normal vertex
+ * `a` alpha
+ */
+#define VTX(x, y, z, s, t, crnx, cgny, cbnz, a) \
+    { { { x, y, z }, 0, { s, t }, { crnx, cgny, cbnz, a } }, }
+
+#define VTX_T(x, y, z, s, t, cr, cg, cb, a) \
+    { { x, y, z }, 0, { s, t }, { cr, cg, cb, a }, }
+
 #endif
--- include/macros.h
+++ include/macros.h
@@ -21,8 +21,6 @@
 
 #define ARRAY_COUNT_2D(arr) (ARRAY_COUNT(arr) * ARRAY_COUNT(arr[0]))
 
-#define SEGMENTED_TO_K0(addr) (void*)((gSegments[SEGMENT_NUMBER(addr)] + K0BASE) + SEGMENT_OFFSET(addr))
-
 #define GET_ACTIVE_CAM(play) ((play)->cameraPtrs[(play)->activeCamId])
 
 #define GET_PLAYER(play) ((Player*)(play)->actorCtx.actorLists[ACTORCAT_PLAYER].first)
@@ -66,23 +64,6 @@
     (flag & 0x2) ? 1 : \
     (flag & 0x1) ? 0 : \
     0)
-
-/**
- * `x` vertex x
- * `y` vertex y
- * `z` vertex z
- * `s` texture s coordinate
- * `t` texture t coordinate
- * `crnx` red component of color vertex, or x component of normal vertex
- * `cgny` green component of color vertex, or y component of normal vertex
- * `cbnz` blue component of color vertex, or z component of normal vertex
- * `a` alpha
- */
-#define VTX(x, y, z, s, t, crnx, cgny, cbnz, a) \
-    { { { x, y, z }, 0, { s, t }, { crnx, cgny, cbnz, a } }, }
-
-#define VTX_T(x, y, z, s, t, cr, cg, cb, a) \
-    { { x, y, z }, 0, { s, t }, { cr, cg, cb, a }, }
 
 #define SQ(x) ((x) * (x))
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
