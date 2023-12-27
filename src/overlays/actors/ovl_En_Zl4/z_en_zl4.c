ade776e1746765e4f05a290919e6b7ae0d9c0e57
Iván Peñarando Martínez <ipenyarando@gmail.com>
2023-12-27 10:39:09 +0100

    Tweaked code with new names to make it compile

--- src/overlays/actors/ovl_En_Zl4/z_en_zl4.c
+++ src/overlays/actors/ovl_En_Zl4/z_en_zl4.c
@@ -16,7 +16,7 @@ void EnZl4_Destroy(Actor* thisx, PlayState* play);
 void EnZl4_Update(Actor* thisx, PlayState* play);
 void EnZl4_Draw(Actor* thisx, PlayState* play);
 
-void func_809A1BB0(SkelAnime*, AnimationInfo*, u16);
+void EnZl4_ChangeAnim(SkelAnime*, AnimationInfo*, u16);
 void func_809A1D0C(EnZl4* this, PlayState* play);
 void func_809A1DBC(PlayState* play, s32 limbIndex, Actor* thisx);
 s32 func_809A1D60(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx);
@@ -56,11 +56,11 @@ void EnZl4_Init(Actor* thisx, PlayState* play) {
     EnZl4* this = THIS;
 
     this->unk2E0 = 0;
-    this->unk2F0 = 0xFF;
+    this->envColorAlpha = 0xFF;
     this->actor.targetArrowOffset = 3000.0f;
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 24.0f);
-    SkelAnime_InitFlex(play, &this->unk144, &gSkullKidSkel, NULL, NULL, NULL, 0);
-    func_809A1BB0(&this->unk144, &D_809A1F80, 0);
+    SkelAnime_InitFlex(play, &this->skelAnime, &gSkullKidSkel, NULL, NULL, NULL, 0);
+    EnZl4_ChangeAnim(&this->skelAnime, &D_809A1F80, 0);
     Actor_SetScale(&this->actor, 0.01f);
     this->actionFunc = func_809A1D0C;
 }
@@ -74,8 +74,8 @@ void func_809A1D0C(EnZl4* this, PlayState* play) {
 void EnZl4_Update(Actor* thisx, PlayState* play) {
     EnZl4* this = THIS;
 
-    SkelAnime_Update(&this->unk144);
-    this->unk2F0 = this->unk2F0;
+    SkelAnime_Update(&this->skelAnime);
+    this->envColorAlpha = this->envColorAlpha;
     this->actionFunc(this, play);
 }
 
@@ -117,13 +117,13 @@ void EnZl4_Draw(Actor* thisx, PlayState* play) {
 
     OPEN_DISPS(play->state.gfxCtx);
     Gfx_SetupDL25_Opa(play->state.gfxCtx);
-    if (this->unk2F0 < 0xFFU) {
-        gSPSegment(POLY_OPA_DISP++, 0x0C, func_809A1DD0(play->state.gfxCtx, this->unk2F0));
+    if (this->envColorAlpha < 0xFFU) {
+        gSPSegment(POLY_OPA_DISP++, 0x0C, func_809A1DD0(play->state.gfxCtx, this->envColorAlpha));
 
     } else {
-        gSPSegment(POLY_OPA_DISP++, 0x0C, func_809A1E28(play->state.gfxCtx, this->unk2F0));
+        gSPSegment(POLY_OPA_DISP++, 0x0C, func_809A1E28(play->state.gfxCtx, this->envColorAlpha));
     }
-    SkelAnime_DrawTransformFlexOpa(play, this->unk144.skeleton, this->unk144.jointTable, this->unk144.dListCount,
+    SkelAnime_DrawTransformFlexOpa(play, this->skelAnime.skeleton, this->skelAnime.jointTable, this->skelAnime.dListCount,
                                    func_809A1D60, func_809A1DA4, func_809A1DBC, &this->actor);
     CLOSE_DISPS(play->state.gfxCtx);
 }
