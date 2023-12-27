4a1d3f1ac8caedac6e59abebc32da0b27648af04
Iván Peñarando Martínez <ipenyarando@gmail.com>
2023-12-27 12:32:02 +0100

    Documented some functions/vars names

--- src/overlays/actors/ovl_En_Zl4/z_en_zl4.c
+++ src/overlays/actors/ovl_En_Zl4/z_en_zl4.c
@@ -18,8 +18,8 @@ void EnZl4_Draw(Actor* thisx, PlayState* play);
 
 void EnZl4_ChangeAnim(SkelAnime*, AnimationInfo*, u16);
 void func_809A1D0C(EnZl4* this, PlayState* play);
-void func_809A1DBC(PlayState* play, s32 limbIndex, Actor* thisx);
-s32 func_809A1D60(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx);
+void EnZl4_transformLimbDraw(PlayState* play, s32 limbIndex, Actor* thisx);
+s32 EnZl4_overrideLimbDraw(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx);
 Gfx* func_809A1DD0(GraphicsContext*, u32);
 Gfx* func_809A1E28(GraphicsContext*, u32);
 
@@ -35,7 +35,7 @@ ActorInit En_Zl4_InitVars = {
     /**/ EnZl4_Draw,
 };
 
-static AnimationInfo D_809A1F80 = { &gSkullKidTPoseAnim, 1.0f, 0.0f, -1.0f, 0, 0.0f };
+static AnimationInfo sAnimationInfo[] = { &gSkullKidTPoseAnim, 1.0f, 0.0f, -1.0f, 0, 0.0f };
 static Vec3f D_809A1F98 = { 0.0f, 0.0f, 0.0f };
 
 void EnZl4_ChangeAnim(SkelAnime* skelAnime, AnimationInfo* animInfo, u16 animIndex) {
@@ -60,7 +60,7 @@ void EnZl4_Init(Actor* thisx, PlayState* play) {
     this->actor.targetArrowOffset = 3000.0f;
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 24.0f);
     SkelAnime_InitFlex(play, &this->skelAnime, &gSkullKidSkel, NULL, NULL, NULL, 0);
-    EnZl4_ChangeAnim(&this->skelAnime, &D_809A1F80, 0);
+    EnZl4_ChangeAnim(&this->skelAnime, sAnimationInfo, 0);
     Actor_SetScale(&this->actor, 0.01f);
     this->actionFunc = func_809A1D0C;
 }
@@ -79,16 +79,16 @@ void EnZl4_Update(Actor* thisx, PlayState* play) {
     this->actionFunc(this, play);
 }
 
-s32 func_809A1D60(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx) {
+s32 EnZl4_overrideLimbDraw(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* thisx) {
     Vec3f sp4 = D_809A1F98;
 
     return false;
 }
 
-void func_809A1DA4(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* pos, Actor* thisx) {
+void EnZl4_postLimbDraw(PlayState* play, s32 limbIndex, Gfx** dList, Vec3s* pos, Actor* thisx) {
 }
 
-void func_809A1DBC(PlayState* play, s32 limbIndex, Actor* thisx) {
+void EnZl4_transformLimbDraw(PlayState* play, s32 limbIndex, Actor* thisx) {
 }
 
 Gfx* func_809A1DD0(GraphicsContext* gfxCtx, u32 alpha) {
@@ -124,7 +124,7 @@ void EnZl4_Draw(Actor* thisx, PlayState* play) {
         gSPSegment(POLY_OPA_DISP++, 0x0C, func_809A1E28(play->state.gfxCtx, this->envColorAlpha));
     }
     SkelAnime_DrawTransformFlexOpa(play, this->skelAnime.skeleton, this->skelAnime.jointTable,
-                                   this->skelAnime.dListCount, func_809A1D60, func_809A1DA4, func_809A1DBC,
-                                   &this->actor);
+                                   this->skelAnime.dListCount, EnZl4_overrideLimbDraw, EnZl4_postLimbDraw,
+                                   EnZl4_transformLimbDraw, &this->actor);
     CLOSE_DISPS(play->state.gfxCtx);
 }
