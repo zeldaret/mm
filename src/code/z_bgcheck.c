db8f5f5ccc89304a28aa754087a6c2bf72e55717
Derek Hensley <hensley.derek58@gmail.com>
2024-01-28 16:27:50 -0800

    General Cleanup 2 (#1538)
    * Cleanup
    
    * Cleanup variables.h
    
    * Cleanup fault headers
    
    * Some includes/headers cleanup
    
    * Libultra btn wrappers
    
    * Mark differences to original libultra headers
    
    * Remove duplicate include in fault
    
    * format
    
    * Update include/PR/os_voice.h
    
    Co-authored-by: Tom Overton <tom-overton@users.noreply.github.com>
    
    * align
    
    ---------
    
    Co-authored-by: Tom Overton <tom-overton@users.noreply.github.com>

--- src/code/z_bgcheck.c
+++ src/code/z_bgcheck.c
@@ -1,8 +1,13 @@
 #include "prevent_bss_reordering.h"
-#include "fault.h"
+#include "z64bgcheck.h"
+
 #include "libc64/fixed_point.h"
 #include "libc64/sprintf.h"
+
+#include "fault.h"
 #include "vt.h"
+#include "z64actor.h"
+
 #include "overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope.h"
 
 #define DYNA_RAYCAST_FLOORS 1
