7e716e120f882afdb3eec9c68b9f8cb16489fde1
Tharo <17233964+Thar0@users.noreply.github.com>
2024-02-07 13:21:54 +0000

    ovl_En_Kitan (#1535)
    * Decompiled
    
    * Documented
    
    * Suggested changes, mention that on debug there is an assert(false) in EnKitan_GetQuestionMessageId
    
    * Format
    
    * Update

--- src/overlays/actors/ovl_En_Kitan/z_en_kitan.h
+++ src/overlays/actors/ovl_En_Kitan/z_en_kitan.h
@@ -2,6 +2,9 @@
 #define Z_EN_KITAN_H
 
 #include "global.h"
+#include "objects/object_kitan/object_kitan.h"
+
+#define ENKITAN_GET_COLLECT_FLAG(thisx) (((thisx)->params & 0xFE00) >> 9)
 
 struct EnKitan;
 
@@ -9,7 +12,13 @@ typedef void (*EnKitanActionFunc)(struct EnKitan*, PlayState*);
 
 typedef struct EnKitan {
     /* 0x000 */ Actor actor;
-    /* 0x144 */ char unk_144[0x194];
+    /* 0x144 */ SkelAnime skelAnime;
+    /* 0x188 */ Vec3s jointTable[KEATON_LIMB_MAX];
+    /* 0x206 */ Vec3s morphTable[KEATON_LIMB_MAX];
+    /* 0x284 */ ColliderCylinder collider;
+    /* 0x2D0 */ s32 textBitSet;
+    /* 0x2D4 */ UNK_TYPE1 unk2D4[2];
+    /* 0x2D6 */ s16 timer;
     /* 0x2D8 */ EnKitanActionFunc actionFunc;
 } EnKitan; // size = 0x2DC
 
