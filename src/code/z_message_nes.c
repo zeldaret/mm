250be53cde9e9cc91e6b268b25e2f09e4ddb8125
Derek Hensley <hensley.derek58@gmail.com>
2024-03-31 20:24:06 -0700

    Add Shift-JIS support for some voice and message files (#1500)
    * z_message iconv
    
    * voicecheckword iconv
    
    * format
    
    * TODO
    
    * voice_external iconv
    
    * Missed a few in message
    
    * Use shiftjis_conv instead
    
    * space comment in message_nes
    
    * Use chars for cases
    
    * Add ending single quote check as well
    
    * table comments
    
    * No look up table
    
    * align makefile tools

--- src/code/z_message_nes.c
+++ src/code/z_message_nes.c
@@ -867,23 +867,24 @@ void Message_DrawTextNES(PlayState* play, Gfx** gfxP, u16 textDrawPos) {
 
             default:
                 switch (character) {
-                    case 0x8169:
-                    case 0x8175:
+                    case '（':
+                    case '「':
                         msgCtx->textPosX -= TRUNCF_BINANG(6.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x8145:
+                    case '・':
                         msgCtx->textPosX -= TRUNCF_BINANG(3.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x8148:
-                    case 0x8149:
+                    case '？':
+                    case '！':
                         msgCtx->textPosX -= TRUNCF_BINANG(2.0f * msgCtx->textCharScale);
                         break;
 
                     default:
                         break;
                 }
+
                 if ((msgCtx->msgMode == MSGMODE_TEXT_DISPLAYING) && ((i + 1) == msgCtx->textDrawPos)) {
                     Audio_PlaySfx(NA_SE_NONE);
                 }
@@ -907,28 +908,27 @@ void Message_DrawTextNES(PlayState* play, Gfx** gfxP, u16 textDrawPos) {
                 }
                 charTexIndex += FONT_CHAR_TEX_SIZE;
 
-                //! @TODO: u8 character but > 0x255 cases
                 switch (character) {
-                    case 0x8144:
+                    case '．':
                         msgCtx->textPosX += TRUNCF_BINANG(8.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x816A:
-                    case 0x8176:
+                    case '）':
+                    case '」':
                         msgCtx->textPosX += TRUNCF_BINANG(10.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x8141:
-                    case 0x8142:
-                    case 0x8168:
+                    case '、':
+                    case '。':
+                    case '”':
                         msgCtx->textPosX += TRUNCF_BINANG(12.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x8194:
+                    case '＃':
                         msgCtx->textPosX += TRUNCF_BINANG(14.0f * msgCtx->textCharScale);
                         break;
 
-                    case 0x8145:
+                    case '・':
                         msgCtx->textPosX += TRUNCF_BINANG(15.0f * msgCtx->textCharScale);
                         break;
 
