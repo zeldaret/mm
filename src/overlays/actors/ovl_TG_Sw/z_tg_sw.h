b7e5468ca16315a7e322055eff3d97fe980bbc25
Derek Hensley <hensley.derek58@gmail.com>
2024-05-19 11:02:20 -0700

    General cleanup (#1636)
    * totalLoadedActors comparision
    
    * OBJMINE_PARAMS
    
    * Sort z64.h header includes
    
    * MapSelect_LoadConsoleLogo
    
    * Combine notebook tables into 1
    
    * TGSw clean up

--- src/overlays/actors/ovl_TG_Sw/z_tg_sw.h
+++ src/overlays/actors/ovl_TG_Sw/z_tg_sw.h
@@ -6,6 +6,8 @@
 struct TGSw;
 typedef void (*TGSwActionFunc)(struct TGSw*, PlayState*);
 
+#define TGSW_GET_FC(thisx) ((((thisx)->params & 0xFC) >> 2) & 0xFF)
+
 typedef struct TGSw {
     /* 0x000 */ Actor actor;
     /* 0x144 */ TGSwActionFunc actionFunc;
