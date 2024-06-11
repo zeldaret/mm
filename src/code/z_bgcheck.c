e4c6ffb0d6ed407dec46fe43118426e2348f3018
Derek Hensley <hensley.derek58@gmail.com>
2024-06-10 22:07:16 -0700

    bgcheck includes

--- src/code/z_bgcheck.c
+++ src/code/z_bgcheck.c
@@ -1,11 +1,10 @@
 #include "prevent_bss_reordering.h"
-
 #include "z64bgcheck.h"
-#include "global.h"
 
 #include "libc64/fixed_point.h"
 #include "libc64/sprintf.h"
 
+#include "global.h"
 #include "fault.h"
 #include "vt.h"
 #include "z64actor.h"
