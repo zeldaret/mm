aec741be09c0551ba944758524c6561b5b80a600
Derek Hensley <hensley.derek58@gmail.com>
2024-07-27 07:28:53 -0700

    initialize typo

--- include/z64keyframe.h
+++ include/z64keyframe.h
@@ -70,7 +70,7 @@ typedef struct {
     // Array of bitflags for each limb indicating whether to do keyframe interpolation
     // or pull from fixed values that do not change throughout the animation.
     union {
-        // Used to initalize bitflags without warnings
+        // Used to initialize bitflags without warnings
     /* 0x00 */ void* data;
         // For standard the bit layout in each array element is:
         // [5] X Translation (root limb only)
