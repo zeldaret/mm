2a152a9676ed57b7b913ad80b556b5e569ada908
Tharo <17233964+Thar0@users.noreply.github.com>
2024-12-22 23:24:47 +0000

    Apply the NORETURN attribute to functions that do not return (#1768)
    * Apply the NORETURN attribute to functions that do not return
    
    * Add NORETURN to debug.c functions

--- src/boot/libu64/debug.c
+++ src/boot/libu64/debug.c
@@ -1,11 +1,11 @@
 #include "global.h"
 #include "fault.h"
 
-void _dbg_hungup(const char* file, int lineNum) {
+NORETURN void _dbg_hungup(const char* file, int lineNum) {
     osGetThreadId(NULL);
     Fault_AddHungupAndCrash(file, lineNum);
 }
 
-void Reset(void) {
+NORETURN void Reset(void) {
     Fault_AddHungupAndCrash("Reset", 0);
 }
