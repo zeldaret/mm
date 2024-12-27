b12258cd6d1ba2ac63df518c8c40f08b34370614
Derek Hensley <hensley.derek58@gmail.com>
2024-12-26 21:45:51 -0800

    gGfxSPTaskStack size macro

--- src/buffers/gfxbuffers.c
+++ src/buffers/gfxbuffers.c
@@ -3,6 +3,6 @@
 
 u64 gGfxSPTaskYieldBuffer[OS_YIELD_DATA_SIZE / sizeof(u64)] ALIGNED(16);
 
-STACK(gGfxSPTaskStack, 0x400) ALIGNED(16);
+STACK(gGfxSPTaskStack, SP_DRAM_STACK_SIZE8) ALIGNED(16);
 
 GfxPool gGfxPools[2] ALIGNED(16);
