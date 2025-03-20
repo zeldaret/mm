6d3a8aa30f3b3afb084d79bca844631542fbb4b6
Derek Hensley <hensley.derek58@gmail.com>
2025-03-20 09:15:53 -0700

    split micrcode into their own files

--- src/microcode/gspF3DZEX2_NoN_PosLight_fifo.s
+++ src/microcode/gspF3DZEX2_NoN_PosLight_fifo.s
@@ -2,22 +2,14 @@
 
 .section .rodata
 
-dlabel aspMainDataStart
-    .incbin "incbin/aspMainData"
-dlabel aspMainDataEnd
+.balign 16
 
 dlabel gspF3DZEX2_NoN_PosLight_fifoTextStart
     .incbin "incbin/gspF3DZEX2_NoN_PosLight_fifoText"
 dlabel gspF3DZEX2_NoN_PosLight_fifoTextEnd
 
+.balign 16
+
 dlabel gspF3DZEX2_NoN_PosLight_fifoDataStart
     .incbin "incbin/gspF3DZEX2_NoN_PosLight_fifoData"
 dlabel gspF3DZEX2_NoN_PosLight_fifoDataEnd
-
-dlabel gspS2DEX2_fifoDataStart
-    .incbin "incbin/gspS2DEX2_fifoData"
-dlabel gspS2DEX2_fifoDataEnd
-
-dlabel njpgdspMainDataStart
-    .incbin "incbin/njpgdspMainData"
-dlabel njpgdspMainDataEnd
