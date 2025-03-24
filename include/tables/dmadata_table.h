03f12527e178143bb0f1e3bd41ec748476c77999
Derek Hensley <hensley.derek58@gmail.com>
2025-03-24 07:01:04 -0700

    Remove us specific dmadata table (#1801)

--- include/tables/dmadata_table.h
+++ include/tables/dmadata_table.h
@@ -1,9 +1,5 @@
 /**
  * Select dmadata table for version
  */
-#ifdef NON_MATCHING
-// For non-matching builds, dmadata is generated from the specfile segments
+// dmadata is generated from the specfile segments
 #include "dmadata/dmadata_table_spec.h"
-#else
-#include "dmadata/dmadata_table_us.h"
-#endif
