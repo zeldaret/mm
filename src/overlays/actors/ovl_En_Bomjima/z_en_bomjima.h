37ad3668aeb49346ce91332ca2122cda5a163e8b
louist103 <35883445+louist103@users.noreply.github.com>
2025-04-19 12:53:55 -0400

    Fix OOB access

--- src/overlays/actors/ovl_En_Bomjima/z_en_bomjima.h
+++ src/overlays/actors/ovl_En_Bomjima/z_en_bomjima.h
@@ -42,8 +42,8 @@ typedef struct EnBomjima {
     /* 0x2CA */ s16 unk_2CA;
     /* 0x2CC */ f32 animEndFrame;
     /* 0x2D0 */ f32 unk_2D0;
-    /* 0x2D4 */ s16 csIdList[2];
-    /* 0x2D8 */ UNK_TYPE1 unk2D8[4]; // maybe a part of the above?
+    /* 0x2D4 */ s16 csIdList[3];
+    /* 0x2D8 */ UNK_TYPE1 unk2D8[3]; // maybe a part of the above?
     /* 0x2DC */ s16 unk_2DC;
     /* 0x2DE */ s16 cutsceneEnded;
     /* 0x2E0 */ s16 unk_2E0;
