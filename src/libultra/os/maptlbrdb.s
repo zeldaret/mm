76f8d6b670f071ebb93c133a24b88aef223b802d
Tharo <17233964+Thar0@users.noreply.github.com>
2025-08-16 17:36:22 +0100

    Handwritten asm: OS functions, libm_vals

---
+++ src/libultra/os/maptlbrdb.s
@@ -0,0 +1,31 @@
+#include "PR/asm.h"
+#include "PR/regdef.h"
+#include "PR/R4300.h"
+#include "PR/rdb.h"
+
+.text
+
+LEAF(osMapTLBRdb)
+    MFC0(   t0, C0_ENTRYHI)
+    li      t1, NTLBENTRIES
+    MTC0(   t1, C0_INX)
+    MTC0(   zero, C0_PAGEMASK)
+    li      t2, (TLBLO_UNCACHED | TLBLO_D | TLBLO_V | TLBLO_G)
+    li      t1, (RDB_BASE_REG & TLBHI_VPN2MASK)
+    MTC0(   t1, C0_ENTRYHI)
+    /* Possible bug? Virtual address instead of physical address set as page frame number */
+    li      t1, RDB_BASE_VIRTUAL_ADDR
+    srl     t3, t1, TLBLO_PFNSHIFT
+    or      t3, t3, t2
+    MTC0(   t3, C0_ENTRYLO0)
+    li      t1, TLBLO_G
+    MTC0(   t1, C0_ENTRYLO1)
+    NOP
+    TLBWI
+    NOP
+    NOP
+    NOP
+    NOP
+    MTC0(   t0, C0_ENTRYHI)
+    jr      ra
+END(osMapTLBRdb)
---
+++ src/libultra/os/writebackdcache.s
@@ -0,0 +1,54 @@
+#include "PR/asm.h"
+#include "PR/regdef.h"
+#include "PR/R4300.h"
+
+.text
+
+/**
+ *  void osWritebackDCache(void* vaddr, s32 nbytes);
+ *
+ *  Writes back the contents of the data cache to main memory for `nbytes` at `vaddr`.
+ *  If `nbytes` is as large as or larger than the data cache size, the entire cache is
+ *  written back.
+ */
+LEAF(osWritebackDCache)
+    /* If the amount to write back is less than or equal to 0, return immediately */
+    blez    a1, 2f
+
+    /* If the amount to write back is as large as or larger than */
+    /* the data cache size, write back all */
+    li      t3, DCACHE_SIZE
+    bgeu    a1, t3, 3f
+
+    /* ensure end address doesn't wrap around and end up smaller */
+    /* than the start address */
+    move    t0, a0
+    addu    t1, a0, a1
+    bgeu    t0, t1, 2f
+
+    /* Mask and subtract to align to cache line */
+    addiu   t1, t1, -DCACHE_LINESIZE
+    andi    t2, t0, DCACHE_LINEMASK
+    subu    t0, t0, t2
+1:
+    CACHE(  (CACH_PD | C_HWB), (t0))
+.set noreorder
+    bltu    t0, t1, 1b
+     addiu  t0, t0, DCACHE_LINESIZE
+.set reorder
+2:
+    jr      ra
+
+/* same as osWritebackDCacheAll in operation */
+3:
+    li      t0, K0BASE
+    addu    t1, t0, t3
+    addiu   t1, t1, -DCACHE_LINESIZE
+4:
+    CACHE(  (CACH_PD | C_IWBINV), (t0))
+.set noreorder
+    bltu    t0, t1, 4b
+     addiu  t0, DCACHE_LINESIZE
+.set reorder
+    jr      ra
+END(osWritebackDCache)
