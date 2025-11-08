5cf4f6bbbdcfa953e8a9e743d1838d88a653cc7a
isghj <isghj5@gmail.com>
2025-11-08 11:46:11 -0800

    rename_sym: fixed to work with new disasm functions/variables locations

--- tools/rename_sym.sh
+++ tools/rename_sym.sh
@@ -2,12 +2,17 @@
 
 shopt -s globstar
 
+FUNCTIONS_FILES="tools/disasm/**/functions.txt"
+VARIABLES_FILES="tools/disasm/**/variables.txt"
+ASM_FILES="extracted/**/asm/**/*s"
+
 if [ "$#" -ne "2" ];
 then
     echo "usage: $0 old_name new_name"
     exit 1
 fi
 
-#echo "Replace $1 with $2?"
-#read
-grep -rl "$1" asm/**/*.s src/**/*.{c,h} include/**/*.h assets/xml/* tools/disasm/functions.txt tools/disasm/variables.txt tools/sizes/*.csv | xargs sed -i "s/\b$1\b/$2/g"
+grep -rl "$1" src/**/*.{c,h} include/**/*.h assets/xml/* $FUNCTIONS_FILES $VARIABLES_FILES tools/sizes/*.csv | xargs sed -i "s/\b$1\b/$2/g"
+
+# so many files it overloads xargs, and takes much longer
+# grep -rl "$1" $ASM_FILES | xargs sed -i "s/\b$1\b/$2/g"
