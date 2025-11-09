8caa567144eb3835fecbff0ffb4ab77e0e94bdd5
isghj <isghj5@gmail.com>
2025-11-09 08:57:27 -0800

    rename_sym: re-enabled asm

--- tools/rename_sym.sh
+++ tools/rename_sym.sh
@@ -12,7 +12,9 @@ then
     exit 1
 fi
 
+echo "Replacing symbol in most files ..."
 grep -rl "$1" src/**/*.{c,h} include/**/*.h assets/xml/* $FUNCTIONS_FILES $VARIABLES_FILES tools/sizes/*.csv | xargs sed -i "s/\b$1\b/$2/g"
 
 # so many files it overloads xargs, and takes much longer
-# grep -rl "$1" $ASM_FILES | xargs sed -i "s/\b$1\b/$2/g"
+echo "Replacing symbol in asm files ..."
+grep -rl "$1" $ASM_FILES | xargs sed -i "s/\b$1\b/$2/g"
