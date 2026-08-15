da2a2da85aac1a7cf10a007850c8b5d1b7771a40
Derek Hensley <hensley.derek58@gmail.com>
2026-08-15 12:53:32 -0700

    gPoeComposerEyeTex

--- tools/assets/extract/extase_oot64/dlist_resources.py
+++ tools/assets/extract/extase_oot64/dlist_resources.py
@@ -1617,7 +1617,7 @@ class DListResource(Resource, can_size_be_unknown=True):
                     "gSunDL",  # DList loads bigger chunks than the individual texture pieces (overlaps)
                 }
                 HACK_no_warn_bad_dims_Texs = {
-                    "gPoeComposerFlatHeadDL_000060E0_Tex",  # used as both rgba16 16x16 and rgba16 8x8
+                    "gPoeComposerEyeTex",  # used as both rgba16 16x16 and rgba16 8x8
                     "gDekuStickTex",  # used as both i8 8x8 and i8 16x16
                     "gHilite1Tex",  # used as both rgba16 16x16 and rgba16 32x32
                     "gHilite2Tex",  # used as both rgba16 16x16 and rgba16 32x32
